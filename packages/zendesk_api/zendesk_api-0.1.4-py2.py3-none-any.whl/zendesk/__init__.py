b94edb43ed093ce766dbeb2817470e013c1c141c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:38 +0000

    Add package zendesk_api/0.1.4/zendesk_api-0.1.4-py2.py3-none-any.whl

---
+++ packages/zendesk_api/zendesk_api-0.1.4-py2.py3-none-any.whl/zendesk/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Zendesk API
+# Copyright (c) 2008-2015 Hive Solutions Lda.
+#
+# This file is part of Hive Zendesk API.
+#
+# Hive Zendesk API is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Zendesk API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Zendesk API. If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+from . import base
+from . import ticket
+from . import ticket_field
+from . import user
+
+from .base import DOMAIN, Api
+from .ticket import TicketApi
+from .ticket_field import TicketFieldApi
+from .user import UserApi
