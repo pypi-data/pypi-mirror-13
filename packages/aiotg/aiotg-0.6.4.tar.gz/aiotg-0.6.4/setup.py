d135bd1d48716602039dfbbbef598463ddb7c9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:53 +0000

    Add package aiotg/0.6.4/aiotg-0.6.4.tar.gz

---
+++ packages/aiotg/aiotg-0.6.4.tar.gz/aiotg-0.6.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiotg',
+    version='0.6.4',
+    description='Asynchronous Python API for building Telegram bots',
+    url='https://github.com/szastupov/aiotg',
+
+    author='Stepan Zastupov',
+    author_email='stepan.zastupov@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries'
+    ],
+
+    keywords='asyncio telegram',
+
+    packages=find_packages(exclude=['examples', 'docs', 'tests*']),
+
+    install_requires=['aiohttp>=0.17.2']
+)
