7148b0be413c9f78f9a1bf31c4c3ab9e7f73fae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package VCheck/1.3.0/VCheck-1.3.0.tar.gz

---
+++ packages/VCheck/VCheck-1.3.0.tar.gz/VCheck-1.3.0/tests/versionmod_test.py
@@ -0,0 +1,40 @@
+import unittest.mock as mock
+import vcheck
+import git
+from .base import base as _base
+from .base import *
+
+
+class versionmod_test(base):
+    def version_noversion_test(self):
+        with self.assertRaisesRegex(vcheck.VersionError, 'Unable to return version: not at a tag.') as cm:
+            ver = vcheck.version(self.mod2check)  # noqa
+
+        self.assertEqual(cm.exception.errno, vcheck.VersionError.NOT_AT_TAG)
+
+    def version_valid_test(self):
+        on_version_ind = -1
+        self.mockrepo_real(on_version_ind=on_version_ind)
+        ver = vcheck.version(self.mod2check)
+        self.assertEqual(ver, current_versions[on_version_ind])
+
+    def version_dirty_test(self):
+        self.mockrepo_real(is_dirty=True)
+        
+        with self.assertRaises(vcheck.VersionError) as cm:
+            ver = vcheck.version(self.mod2check)
+
+        self.assertEqual(cm.exception.errno, vcheck.VersionError.DIRTY)
+
+    def hexsha_dirty_test(self):
+        self.mockrepo_real(is_dirty=True)
+        
+        with self.assertRaises(vcheck.VersionError) as cm:
+            ver = vcheck.hexsha(self.mod2check)
+
+        self.assertEqual(cm.exception.errno, vcheck.VersionError.DIRTY)
+
+    def hexsha_valid_test(self):
+        hexsha = vcheck.hexsha(self.mod2check)
+
+        self.assertEqual(hexsha, current_hexsha)
