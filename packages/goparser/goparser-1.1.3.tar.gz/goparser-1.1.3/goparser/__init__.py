f887373a0a1f6f8f2943e290c2f75ef1c7ec2dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:30 +0000

    Add package goparser/1.1.3/goparser-1.1.3.tar.gz

---
+++ packages/goparser/goparser-1.1.3.tar.gz/goparser-1.1.3/goparser/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+
+from goparser.term import GOTerm
+from goparser.annotation import GOAnnotation
+from goparser.parser import GOParser
+
+__version__ = pkg_resources.require('goparser')[0].version
+
+__all__ = ['GOTerm', 'GOAnnotation', 'GOParser']
