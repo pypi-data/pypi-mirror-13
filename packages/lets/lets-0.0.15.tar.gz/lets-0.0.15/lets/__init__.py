9f75ecaa721c129b4162953de42187039bf49262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package lets/0.0.15/lets-0.0.15.tar.gz

---
+++ packages/lets/lets-0.0.15.tar.gz/lets-0.0.15/lets/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+   lets
+   ~~~~
+
+   Utilities for gevent_.
+
+   .. _gevent: http://gevent.org/
+
+   :copyright: (c) 2013-2015 by Heungsub Lee
+   :license: BSD, see LICENSE for more details.
+
+"""
+from __future__ import absolute_import
+
+from .__about__ import __version__  # noqa
+from .jobqueue import JobQueue
+from .objectpool import ObjectPool
+from .processlet import ProcessExit, Processlet, ProcessPool
+from .slavery import (
+    join_slaves, link_partner, link_slave, MasterGreenletExit, spawn_partner,
+    spawn_partner_later, spawn_slave, spawn_slave_later)
+from .transparentlet import TransparentGroup, Transparentlet
+
+
+__all__ = ['JobQueue', 'join_slaves', 'link_partner', 'link_slave',
+           'MasterGreenletExit', 'ObjectPool', 'Processlet', 'ProcessExit',
+           'ProcessPool', 'spawn_partner', 'spawn_partner_later',
+           'spawn_slave', 'spawn_slave_later', 'Transparentlet',
+           'TransparentGroup']
