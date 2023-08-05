10019df800030259dbc8351e2ae58248ba64cabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:45 +0000

    Add package recursionlist/1.0.0/recursionlist-1.0.0.zip

---
+++ packages/recursionlist/recursionlist-1.0.0.zip/recursionlist-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'recursionlist',
+	version = '1.0.0',
+	py_modules = ['recursionlist'],
+	author = 'erocfort',
+	author_email = 'erocfort@gmail.com',
+	url = 'www.forteric.com',
+	description = 'A simple printer of nested lists',
+	)
+	
