6fd3b3c6b3de7763090c796b516753a490f59fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package upyflashed/0.1.1/upyflashed-0.1.1.tar.gz

---
+++ packages/upyflashed/upyflashed-0.1.1.tar.gz/upyflashed-0.1.1/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='upyflashed',
+
+    version='0.1.1',
+
+    description='A command to watch for new hex files from upyed and flash the micro:bit immediately',
+    long_description=read('README.rst') + read('CHANGES.rst'),
+
+    url='https://github.com/tomviner/upyflashed',
+
+    author='Tom Viner',
+    author_email='upyflashed@viner.tv',
+
+    license='Apache',
+
+    packages=find_packages(
+        exclude=['tests']
+    ),
+
+    install_requires=[
+        'psutil',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'upyflashed = upyflashed:main',
+        ],
+    }
+)
