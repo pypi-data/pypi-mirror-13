7f60248f40a6990c79c898c9e793e77900dfbecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:39 +0000

    Add package trytrylook/1.2.0/trytrylook-1.2.0.zip

---
+++ packages/trytrylook/trytrylook-1.2.0.zip/trytrylook-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         ='trytrylook',
+       version      ='1.2.0',
+       py_modules   =['trytrylook'],
+       author       ='rick199006',
+       author_email ='rick199006@gmail.com',
+       url          ='http://www.rick.com',
+       description  ='A simple printer of nested list',
+     )
