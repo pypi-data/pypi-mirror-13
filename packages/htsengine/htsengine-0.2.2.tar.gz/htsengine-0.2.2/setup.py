5a861c1514cc65429c47b2f9aa0760c2e0cb355b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:31 +0000

    Add package htsengine/0.2.2/htsengine-0.2.2.tar.gz

---
+++ packages/htsengine/htsengine-0.2.2.tar.gz/htsengine-0.2.2/setup.py
@@ -0,0 +1,57 @@
+from distutils.core import setup
+import os
+from distutils.extension import Extension
+
+current_directory = os.path.dirname(__file__)
+
+
+def long_description(filename):
+    return '''
+This package is an extension for whose want to use hts engine by Python 3.
+
+htsengine will update when hts_engine updating.
+
+## Installation
+```bash
+pip install htsengine
+```
+'''
+
+sources = ['hts_engine_python.c']
+library_path = os.path.join(current_directory, 'src', 'lib')
+for filename in os.listdir(library_path):
+    if filename.endswith('.c'):
+        sources.append(os.path.join(library_path, filename))
+
+module = Extension(
+    'htsengine',
+    sources=sources,
+    include_dirs=[library_path, os.path.join(
+        current_directory, 'src', 'include'), ],
+    library_dirs=[library_path, ],
+)
+
+setup(
+    name='htsengine',
+    version='0.2.2',
+    description='An extension for whose want to use hts engine by Python 3',
+    long_description=long_description('README.md'),
+    url='http://hts-engine.sourceforge.net/',
+    download_url='https://github.com/sih4sing5hong5/hts_engine_python',
+    keywords=[
+        '語言合成',
+        'Text to Speech',
+        'TTS',
+        'HTS',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    ext_modules=[module],
+)
