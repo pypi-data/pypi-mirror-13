0fef828f37d73071686fee9cf65ea70860e7e868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:18 +0000

    Add package lektor-surge/0.1/lektor-surge-0.1.tar.gz

---
+++ packages/lektor-surge/lektor-surge-0.1.tar.gz/lektor-surge-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='lektor-surge',
+    version='0.1',
+    author=u'A. Jesse Jiryu Davis',
+    author_email='jesse@emptysquare.net',
+    license='MIT',
+    py_modules=['lektor_surge'],
+    install_requires=['Lektor'],
+    url='https://github.com/ajdavis/lektor-surge',
+    entry_points={
+        'lektor.plugins': [
+            'surge = lektor_surge:SurgePlugin',
+        ]
+    }
+)
