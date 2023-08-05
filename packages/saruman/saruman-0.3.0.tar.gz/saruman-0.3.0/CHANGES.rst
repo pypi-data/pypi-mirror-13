d4f4eb551769db262915a99557bdaf278d15868f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package saruman/0.3.0/saruman-0.3.0.tar.gz

---
+++ packages/saruman/saruman-0.3.0.tar.gz/saruman-0.3.0/CHANGES.rst
@@ -0,0 +1,53 @@
+Changelog for Saruman
+=====================
+
+0.3.0 (2016-01-03)
+------------------
+
+- big modifications of the documentation structure
+
+
+0.2.3 (2016-01-03)
+------------------
+
+- fixing badges and coverage in testing
+
+
+0.2.2 (2016-01-03)
+------------------
+
+- testing works
+
+
+0.2.1 (2016-01-03)
+------------------
+
+- fixing a lot of nasty issues
+
+
+
+0.2.0 (2016-01-03)
+------------------
+
+- adding sphinx documentation
+- adding CI coverage
+- fixing nasty unpack in modprobe.py
+
+
+0.1.2 (2016-01-03)
+------------------
+
+- fixing some typo.
+
+
+0.1.1 (2016-01-03)
+------------------
+
+- remove download urls as we use sdist
+
+
+0.1.0 (2016-01-03)
+------------------
+
+- add zest.release to perform check on release an better automation
+- add some yaml config files
---
+++ packages/saruman/saruman-0.3.0.tar.gz/saruman-0.3.0/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+import saruman
+
+
+def read(filename):
+    try:
+        with open(filename) as f:
+            return f.read()
+    except NameError:
+        with open(filename, 'r') as f:
+            return f.read()
+
+long_description = u'\n\n'.join([read('README.rst'),
+                                 read('CREDITS.rst'),
+                                 read('CHANGES.rst')])
+
+
+class Install(_install):
+    def run(self):
+        _install.run(self)
+        print("Post Install")
+
+
+setup(
+        name='saruman',
+        version=saruman.__version__,
+        packages=find_packages(),
+        url='https://github.com/tychota/saruman',
+        license='MIT',
+        author='tychota',
+        author_email='tycho.tatitscheff+saruman@gadz.org',
+        description='A firewall that leverage AMQP workqueue ! Build by iresam for iresam !',
+        long_description=long_description,
+        install_requires=[
+            'celery==3.1.19',
+            'plumbum==1.6.1',
+            'click==6.2',
+            'colorlog==2.6.0',
+            'zest.releaser==6.4',
+            'coverage==4.0.3',
+            'pyyaml==3.11',
+        ],
+        test_suite="tests",
+        entry_points='''
+            [console_scripts]
+            saruman=saruman.__main__:cli
+        ''',
+        cmdclass={'install': Install},
+        zip_safe=False,
+        keywords=['firewall', 'amqp', 'nftables', 'dhcp, reverse-proxy'],
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Console',
+            'Intended Audience :: System Administrators',
+            'Natural Language :: English',
+            'Natural Language :: French',
+            'Operating System :: POSIX :: Linux',
+            'Topic :: Internet :: Name Service (DNS)',
+            'Topic :: Internet :: Proxy Servers',
+            'Topic :: System :: Networking',
+            'Topic :: System :: Networking :: Firewalls',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+)
