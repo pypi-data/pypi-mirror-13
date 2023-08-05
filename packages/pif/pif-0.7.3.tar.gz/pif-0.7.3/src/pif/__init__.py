e78bd18c59cb5312d35dce8622730a1d6d281f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:58 +0000

    Add package pif/0.7.3/pif-0.7.3.tar.gz

---
+++ packages/pif/pif-0.7.3.tar.gz/pif-0.7.3/src/pif/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'pif'
+__version__ = '0.7.3'
+__build__ = 0x00000a
+__author__ = 'Artur Barseghyan'
+__copyright__ = 'Copyright (c) 2013-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('get_public_ip',)
+
+from pif.utils import get_public_ip
