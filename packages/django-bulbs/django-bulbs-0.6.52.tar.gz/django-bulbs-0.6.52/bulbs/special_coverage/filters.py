f423d96348c829d22e03f88f0d33aaae05d4f2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:15 +0000

    Add package django-bulbs/0.6.52/django-bulbs-0.6.52.tar.gz

---
+++ packages/django-bulbs/django-bulbs-0.6.52.tar.gz/django-bulbs-0.6.52/bulbs/special_coverage/filters.py
@@ -0,0 +1,25 @@
+from bulbs.utils.filters import CaseInsensitiveBooleanFilter
+
+from bulbs.utils.methods import get_query_params, today_as_datetime
+
+
+class SpecialCoverageFilter(CaseInsensitiveBooleanFilter):
+    """
+    Includes the default boolean_filter, but replaces the active and inactive states to represent
+    whether the current_date is within a SpecialCoverage's start_date & end_date range.
+    """
+    def filter_queryset(self, request, queryset, view):
+        queryset = super(SpecialCoverageFilter, self).filter_queryset(request, queryset, view)
+        query_params = get_query_params(request)
+        if "active" in query_params:
+            today_filter = today_as_datetime()
+            value = query_params.get("active").lower()
+            if value == "true":
+                queryset = queryset.filter(
+                    start_date__lte=today_filter, end_date__gte=today_filter
+                )
+            elif value == "false":
+                queryset == queryset.exclude(
+                    start_date__lte=today_filter, end_date__gte=today_filter
+                )
+        return queryset
