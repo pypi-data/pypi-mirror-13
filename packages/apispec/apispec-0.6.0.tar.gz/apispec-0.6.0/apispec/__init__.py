8ab83027ff69c509562994714f05441222a78363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package apispec/0.6.0/apispec-0.6.0.tar.gz

---
+++ packages/apispec/apispec-0.6.0.tar.gz/apispec-0.6.0/apispec/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Contains the main `APISpec` class.
+"""
+
+__version__ = '0.6.0'
+__author__ = 'Steven Loria, Josh Carp, and contributors'
+__license__ = 'MIT'
+
+from .core import APISpec, Path
+
+__all__ = [
+    'APISpec',
+    'Path',
+]
