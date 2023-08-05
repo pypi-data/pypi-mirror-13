ef817c676f09bd79d473082777a797799ce85191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:08 +0000

    Add package nesterSPC/1.3.0/nesterSPC-1.3.0.tar.gz

---
+++ packages/nesterSPC/nesterSPC-1.3.0.tar.gz/nesterSPC-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name         = 'nesterSPC',
+      version      = '1.3.0',
+      py_modules   = ['nester'],
+      author       = 'chubbzzz90',
+      author_email = 'chubbzzz90@yahoo.com',
+      url          = 'nothing',
+      description  = 'A simple printer of nested lists',
+      )
