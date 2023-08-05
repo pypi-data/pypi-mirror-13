b94959809fd3e174fb7e3e88d02aae3b21310707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package preshed/0.45.0/preshed-0.45.0.tar.gz

---
+++ packages/preshed/preshed-0.45.0.tar.gz/preshed-0.45.0/preshed/about.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.45.0'
+version = '0.45.0'
+full_version = '0.45.0'
+git_revision = 'c9351185b96196e1e4d60202cb1877b3c15042f3'
+release = True
+if not release:
+    version = full_version
