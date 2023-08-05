0044f96e1f44c540da9148d59d669e252ac0323d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:46 +0000

    Add package nealpan_nester/1.2.3/nealpan_nester-1.2.3.tar.gz

---
+++ packages/nealpan_nester/nealpan_nester-1.2.3.tar.gz/nealpan_nester-1.2.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nealpan_nester',
+    version = '1.2.3',
+    py_modules = ['nealpan_nester'],
+    author = 'nealpan21',
+    author_email = '920878286@qq.com',
+    url = 'http://www.baidu.com',
+    description = 'A simple printer of nested lists',
+)
