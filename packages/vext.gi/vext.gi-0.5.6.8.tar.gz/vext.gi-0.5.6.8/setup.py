f8f3a402dc7a7e658031ce31d1698a553fddc9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:52 +0000

    Add package vext.gi/0.5.6.8/vext.gi-0.5.6.8.tar.gz

---
+++ packages/vext.gi/vext.gi-0.5.6.8.tar.gz/vext.gi-0.5.6.8/setup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+info="""
+Allow use of system gi.repository (Gtk3) from a virtualenv
+Should work on all platforms.
+
+report bugs to https://github.com/stuaxo/vext
+"""
+
+version="0.5.6.8"
+vext_version="vext>=%s" % version
+
+
+from glob import glob
+from subprocess import call
+
+from setuptools import setup
+from setuptools.command.install import install
+import os
+
+vext_files = glob("*.vext")
+
+def _post_install():
+    import os
+    if 'VEXT_DEBUG' in os.environ:
+        import ipdb; ipdb.set_trace()
+    cmd = ["vext", "-e", "-i" + (" -i".join(vext_files))]
+    print("run cmd:")
+    print(cmd)
+    call(cmd)
+
+class Install(install):
+    def run(self):
+        import sys
+        self.do_egg_install()
+        self.execute(_post_install, [], msg="Install vext files:")
+
+setup(
+    name='vext.gi',
+    zip_safe=False,
+
+    cmdclass={
+          'install': Install,
+    },
+
+    version=version,
+
+    description='Use system python packages in a virtualenv',
+    long_description=info,
+
+    # The project's main homepage.
+    url='https://github.com/stuaxo/vext',
+
+    # Author details
+    author='Stuart Axon',
+    author_email='stuaxo2@yahoo.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='virtualenv gi vext gtk',
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    setup_requires=["setuptools>=0.18.8"],
+    install_requires=[vext_version],
+)
