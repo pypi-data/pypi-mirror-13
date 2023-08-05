d78408e502b16c6ab3e05f60db9ec0fff330bcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package saruman/0.2.1/saruman-0.2.1.tar.gz

---
+++ packages/saruman/saruman-0.2.1.tar.gz/saruman-0.2.1/saruman/tasks/iproute2/interfaces.py
@@ -0,0 +1,21 @@
+from celery import Task
+from celery.utils.log import get_task_logger
+
+# noinspection PyUnresolvedReferences
+from plumbum.cmd import lsmod, modprobe
+
+from saruman.helpers.error_handling import error_handling
+from saruman.helpers.exceptions import FirewallNotAllowedError
+
+__all__ = ['check']
+
+logger = get_task_logger(__name__)
+
+
+class Bridge(Task):
+    name = 'kernel.modules.check'
+
+    @error_handling(logger)
+    def run(self, bridge_name, interface_name):
+        logger.info("Création d'un nouveau bridge `{}`".format(bridge_name))
+        logger.debug("Execution de ip")
