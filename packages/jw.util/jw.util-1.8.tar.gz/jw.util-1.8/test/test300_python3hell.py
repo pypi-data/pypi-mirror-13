6df10c8d1bf758d20b9b4b801d2514680bf6b4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:33 +0000

    Add package jw.util/1.8/jw.util-1.8.tar.gz

---
+++ packages/jw.util/jw.util-1.8.tar.gz/jw.util-1.8/test/test300_python3hell.py
@@ -0,0 +1,21 @@
+"""
+Tests for python3hell module
+
+Only meaningfull with tox
+"""
+from __future__ import unicode_literals
+import sys
+from jw.util.python3hell import Bytes2Str, SetDefaultEncoding, Open
+from nose.tools import eq_
+
+def test10_Bytes2Str():
+    eq_(Bytes2Str(b'x'), 'x')
+
+def test20_SetDefaultEncoding():
+    SetDefaultEncoding('utf-8')
+    eq_(sys.getdefaultencoding(), 'utf-8')
+
+def test30_Open():
+    f = Open('/dev/stdout', 'a')
+    f.write('')
+    f.close()
