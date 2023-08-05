53a9b800669e98a2daf4bccb6ea774548f2d4fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:28 +0000

    Add package ptk/1.2.0/ptk-1.2.0.tar.gz

---
+++ packages/ptk/ptk-1.2.0.tar.gz/ptk-1.2.0/tests/test_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import unittest
+
+from test_utils import *
+from test_regex import *
+from test_regex_tokenizer import *
+from test_regex_parser import *
+from test_lexer import *
+from test_grammar import *
+from test_parser import *
+
+
+if __name__ == '__main__':
+    unittest.main()
