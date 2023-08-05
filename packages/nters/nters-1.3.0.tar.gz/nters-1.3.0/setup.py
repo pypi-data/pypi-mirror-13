d773ae4868774257aa501660365d5e6453681ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:31 +0000

    Add package nters/1.3.0/nters-1.3.0.tar.gz

---
+++ packages/nters/nters-1.3.0.tar.gz/nters-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nters',
+        version = '1.3.0',
+        py_modules = ['nters'],
+        author = 'hello',
+        author_email = '2028560095@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+     )
