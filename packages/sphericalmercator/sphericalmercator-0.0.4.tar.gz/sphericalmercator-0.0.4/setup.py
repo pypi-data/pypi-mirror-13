d53f90bb3bde19dbd429780643e8f97c96f11109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:25 +0000

    Add package sphericalmercator/0.0.4/sphericalmercator-0.0.4.tar.gz

---
+++ packages/sphericalmercator/sphericalmercator-0.0.4.tar.gz/sphericalmercator-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='sphericalmercator',
+    version='0.0.4',
+    py_modules = ['sphericalmercator',],
+    description = 'Tool for calculate XYZ-tile bounding box in geodesic coordinates',
+    long_description=open('README.rst').read(),
+    url='https://bitbucket.org/lighter/spherical-mercator/',
+)
