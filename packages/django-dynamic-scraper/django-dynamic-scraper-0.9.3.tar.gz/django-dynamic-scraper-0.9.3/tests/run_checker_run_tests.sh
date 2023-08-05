913b1d19d308f3e0f58c64fe853fa80ddcf34a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:14 +0000

    Add package django-dynamic-scraper/0.9.3/django-dynamic-scraper-0.9.3.tar.gz

---
+++ packages/django-dynamic-scraper/django-dynamic-scraper-0.9.3.tar.gz/django-dynamic-scraper-0.9.3/tests/run_checker_run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+suite='scraper.checker_run_test.CheckerRunTest'
+tests="
+test_no_checker
+test_x_path_type_keep
+test_x_path_type_keep_double
+test_x_path_type_blank_result_field_keep
+test_x_path_type_404_delete
+test_x_path_type_404_delete_with_zero_actions
+test_x_path_type_x_path_delete
+test_x_path_type_x_path_first_delete_double
+test_x_path_type_x_path_second_delete_double
+test_x_path_type_blank_result_field_x_path_delete
+test_delete_with_img_flat_no_thumbs
+test_delete_with_img_flat_with_thumbs
+test_delete_with_img_all_no_thumbs
+test_delete_with_img_all_with_thumbs
+test_delete_with_img_thumbs_with_thumbs
+test_404_type_404_delete
+test_checker_test_wrong_checker_config
+"
+
+for test in `echo $tests`
+do
+    echo $suite.$test
+    python manage.py test $suite.$test
+done
