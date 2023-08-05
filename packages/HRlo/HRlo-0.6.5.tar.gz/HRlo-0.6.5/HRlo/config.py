03226c5d18554d7ca07d4d3539c1a452187c25f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:09 +0000

    Add package HRlo/0.6.5/HRlo-0.6.5.tar.gz

---
+++ packages/HRlo/HRlo-0.6.5.tar.gz/HRlo-0.6.5/HRlo/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+name    = 'HRlo'
+version = '0.6.5'
+
+description = 'HR manager'
+
+url = 'https://github.com/so07/HRlo'
+
+author=''
+author_email=''
---
+++ packages/HRlo/HRlo-0.6.5.tar.gz/HRlo-0.6.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+try:
+   from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+   from distutils.command.build_py import build_py
+
+from HRlo import config
+
+setup(
+
+   name=config.name,
+   version=config.version,
+   description=config.description,
+   author=config.author,
+   author_email=config.author_email,
+   url=config.url,
+   packages=['HRlo', 'HRlo/logs', 'HRlo/logs/dayutils'],
+   scripts = ['bin/accaerralo', 'bin/HRlo',
+              'bin/HRday', 'bin/HRget',
+              'bin/HRpresence', 'bin/HRcompany',
+              'bin/HRtotalizator',
+              'bin/HRutils'],
+   cmdclass = {'build_py': build_py},
+
+   classifiers=[
+       'Development Status :: 3 - Alpha',
+       'Programming Language :: Python :: 3',
+       'Programming Language :: Python :: 3.2',
+       'Programming Language :: Python :: 3.3',
+       'Programming Language :: Python :: 3.4',
+   ],
+
+)
