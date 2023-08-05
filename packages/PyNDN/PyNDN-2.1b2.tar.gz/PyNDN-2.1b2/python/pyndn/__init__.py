d566d1f807182bb4fd7c6c6b580e470b4011f368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:58 +0000

    Add package PyNDN/2.1b2/PyNDN-2.1b2.tar.gz

---
+++ packages/PyNDN/PyNDN-2.1b2.tar.gz/PyNDN-2.1b2/python/pyndn/__init__.py
@@ -0,0 +1,48 @@
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+#
+# Copyright (C) 2014-2016 Regents of the University of California.
+# Author: Derek Kulinski <takeda@takeda.tk>
+# Author: Jeff Burke <jburke@ucla.edu>
+# Author: Jeff Thompson <jefft0@remap.ucla.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# A copy of the GNU Lesser General Public License is in the file COPYING.
+
+from pyndn import control_parameters, data, exclude, face
+from pyndn import forwarding_flags, interest, interest_filter, key_locator
+from pyndn import meta_info, name, digest_sha256_signature
+from pyndn import sha256_with_rsa_signature, signature
+__all__ = ['control_parameters', 'data', 'exclude', 'face', 'forwarding_flags', 
+           'interest', 'interest_filter', 'key_locator', 'meta_info', 'name',
+           'digest_sha256_signature', 'sha256_with_rsa_signature', 'signature']
+
+import sys as _sys
+
+try:
+    from pyndn.control_parameters import *
+    from pyndn.data import *
+    from pyndn.exclude import *
+    from pyndn.face import *
+    from pyndn.forwarding_flags import *
+    from pyndn.interest import *
+    from pyndn.interest_filter import *
+    from pyndn.key_locator import *
+    from pyndn.meta_info import *
+    from pyndn.name import *
+    from pyndn.digest_sha256_signature import *
+    from pyndn.sha256_with_rsa_signature import *
+    from pyndn.signature import *
+except ImportError:
+    del _sys.modules[__name__]
+    raise
