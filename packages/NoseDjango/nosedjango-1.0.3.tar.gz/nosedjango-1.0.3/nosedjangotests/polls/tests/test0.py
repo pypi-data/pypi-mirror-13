eaf301fdbcdc7c9470e5e8e562b9b20bdebf7c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package NoseDjango/1.0.3/nosedjango-1.0.3.tar.gz

---
+++ packages/NoseDjango/nosedjango-1.0.3.tar.gz/nosedjango-1.0.3/nosedjangotests/polls/tests/test0.py
@@ -0,0 +1,25 @@
+
+from django.test import TestCase
+
+from nosedjangotests.polls.models import Choice
+
+
+def _test_using_content_types(self):
+    self.assertEqual(Choice.objects.all().count(), 1)
+
+
+class ContentTypeTestCase(TestCase):
+    fixtures = [
+        'polls1.json',
+        'choices.json',
+    ]
+
+    def test_using_content_types_1(self):
+        _test_using_content_types(self)
+
+    def test_using_content_types_2(self):
+        _test_using_content_types(self)
+
+
+class CherryPyTestCase(ContentTypeTestCase):
+    start_live_server = True
