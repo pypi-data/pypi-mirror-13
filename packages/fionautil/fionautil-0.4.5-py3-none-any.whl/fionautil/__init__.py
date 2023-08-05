623bbba41df0ee1023a872341c6d02b774698cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:56 +0000

    Add package fionautil/0.4.5/fionautil-0.4.5-py3-none-any.whl

---
+++ packages/fionautil/fionautil-0.4.5-py3-none-any.whl/fionautil/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of fionautil.
+# http://github.com/fitnr/fionautil
+
+# Licensed under the GPLv3 license:
+# http://http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
+from . import feature
+from . import geometry
+from . import layer
+from . import measure
+from . import coords
+from . import round
+from . import scale
+
+__version__ = '0.4.5'
+
+__all__ = [
+    'feature',
+    'geometry',
+    'layer',
+    'measure',
+    'round',
+    'scale'
+]
---
+++ packages/fionautil/fionautil-0.4.5-py3-none-any.whl/fionautil/coords.py
@@ -0,0 +1,25 @@
+from .round import round_ring as roundring
+
+def max_x(coords):
+    return max((c[0] for c in coords))
+
+def max_y(coords):
+    return max((c[1] for c in coords))
+
+def min_x(coords):
+    return min((c[0] for c in coords))
+
+def min_y(coords):
+    return min((c[1] for c in coords))
+
+def segmentize(ring):
+    for i, point in enumerate(ring[:-1]):
+        yield point, ring[i + 1]
+
+def bounds(ring):
+    '''Return minimum bounding rectangle for a ring'''
+    return min_x(ring), min_y(ring), max_x(ring), max_y(ring)
+
+def roundpolyring(polyring, precision=None):
+    return [roundring(ring, precision) for ring in polyring]
+
