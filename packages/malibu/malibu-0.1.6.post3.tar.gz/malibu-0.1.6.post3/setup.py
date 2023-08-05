207402b4f06e77ac2a3f9e9569e68eac8aaf66cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:31 +0000

    Add package malibu/0.1.6.post3/malibu-0.1.6.post3.tar.gz

---
+++ packages/malibu/malibu-0.1.6.post3.tar.gz/malibu-0.1.6.post3/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+import malibu
+
+setup(
+    name='malibu',
+    version=malibu.__version__,
+    description="maiome library of utilities",
+
+    url="https://phabricator.ramcloud.io/tag/malibu",
+    author="Sean Johnson",
+    author_email="sean.johnson@maio.me",
+
+    license="Unlicense",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: Public Domain",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    packages=['malibu',
+              'malibu.command',
+              'malibu.command.builtins',
+              'malibu.config',
+              'malibu.database',
+              'malibu.design',
+              'malibu.text',
+              'malibu.util'],
+    package_dir={'malibu': 'malibu'},
+    install_requires=[
+        'dill'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'coverage'
+    ],
+    zip_safe=True
+)
