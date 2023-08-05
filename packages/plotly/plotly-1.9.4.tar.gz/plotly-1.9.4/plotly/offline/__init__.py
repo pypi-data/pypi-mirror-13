ff959f3aa28becdcb1e3eef8d1856705653757ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:38 +0000

    Add package plotly/1.9.4/plotly-1.9.4.tar.gz

---
+++ packages/plotly/plotly-1.9.4.tar.gz/plotly-1.9.4/plotly/offline/__init__.py
@@ -0,0 +1,11 @@
+"""
+offline
+======
+This module provides offline functionality.
+"""
+from . offline import (
+    download_plotlyjs,
+    init_notebook_mode,
+    iplot,
+    plot
+)
---
+++ packages/plotly/plotly-1.9.4.tar.gz/plotly-1.9.4/plotly/offline/offline.py
@@ -0,0 +1,300 @@
+""" Plotly Offline
+    A module to use Plotly's graphing library with Python
+    without connecting to a public or private plotly enterprise
+    server.
+"""
+from __future__ import absolute_import
+
+import json
+import os
+import uuid
+import warnings
+from pkg_resources import resource_string
+import webbrowser
+
+import plotly
+from plotly import tools, utils
+from plotly.exceptions import PlotlyError
+
+
+__PLOTLY_OFFLINE_INITIALIZED = False
+
+
+def download_plotlyjs(download_url):
+    warnings.warn('''
+        `download_plotlyjs` is deprecated and will be removed in the
+        next release. plotly.js is shipped with this module, it is no
+        longer necessary to download this bundle separately.
+    ''', DeprecationWarning)
+    pass
+
+
+def get_plotlyjs():
+    path = os.path.join('offline', 'plotly.min.js')
+    plotlyjs = resource_string('plotly', path).decode('utf-8')
+    return plotlyjs
+
+
+def init_notebook_mode():
+    """
+    Initialize Plotly Offline mode in an IPython Notebook.
+    Run this function at the start of an IPython notebook
+    to load the necessary javascript files for creating
+    Plotly graphs with plotly.offline.iplot.
+    """
+    if not tools._ipython_imported:
+        raise ImportError('`iplot` can only run inside an IPython Notebook.')
+    from IPython.display import HTML, display
+
+    global __PLOTLY_OFFLINE_INITIALIZED
+    __PLOTLY_OFFLINE_INITIALIZED = True
+    display(HTML('<script type="text/javascript">' +
+                 # ipython's includes `require` as a global, which
+                 # conflicts with plotly.js. so, unrequire it.
+                 'require=requirejs=define=undefined;' +
+                 '</script>' +
+                 '<script type="text/javascript">' +
+                 get_plotlyjs() +
+                 '</script>'))
+
+
+def _plot_html(figure_or_data, show_link, link_text,
+               validate, default_width, default_height):
+
+    figure = tools.return_figure_from_figure_or_data(figure_or_data, validate)
+
+    width = figure.get('layout', {}).get('width', default_width)
+    height = figure.get('layout', {}).get('height', default_height)
+
+    try:
+        float(width)
+    except (ValueError, TypeError):
+        pass
+    else:
+        width = str(width) + 'px'
+
+    try:
+        float(width)
+    except (ValueError, TypeError):
+        pass
+    else:
+        width = str(width) + 'px'
+
+    plotdivid = uuid.uuid4()
+    jdata = json.dumps(figure.get('data', []), cls=utils.PlotlyJSONEncoder)
+    jlayout = json.dumps(figure.get('layout', {}), cls=utils.PlotlyJSONEncoder)
+
+    config = {}
+    config['showLink'] = show_link
+    config['linkText'] = link_text
+    jconfig = json.dumps(config)
+
+    # TODO: The get_config 'source of truth' should
+    # really be somewhere other than plotly.plotly
+    plotly_platform_url = plotly.plotly.get_config().get('plotly_domain',
+                                                         'https://plot.ly')
+    if (plotly_platform_url != 'https://plot.ly' and
+            link_text == 'Export to plot.ly'):
+
+        link_domain = plotly_platform_url\
+            .replace('https://', '')\
+            .replace('http://', '')
+        link_text = link_text.replace('plot.ly', link_domain)
+
+    script = 'Plotly.newPlot("{id}", {data}, {layout}, {config})'.format(
+        id=plotdivid,
+        data=jdata,
+        layout=jlayout,
+        config=jconfig)
+
+    plotly_html_div = (
+        ''
+        '<div id="{id}" style="height: {height}; width: {width};" '
+        'class="plotly-graph-div">'
+        '</div>'
+        '<script type="text/javascript">'
+        'window.PLOTLYENV=window.PLOTLYENV || {{}};'
+        'window.PLOTLYENV.BASE_URL="' + plotly_platform_url + '";'
+        '{script}'
+        '</script>'
+        '').format(
+        id=plotdivid, script=script,
+        height=height, width=width)
+
+    return plotly_html_div, plotdivid, width, height
+
+
+def iplot(figure_or_data, show_link=True, link_text='Export to plot.ly',
+          validate=True):
+    """
+    Draw plotly graphs inside an IPython notebook without
+    connecting to an external server.
+    To save the chart to Plotly Cloud or Plotly Enterprise, use
+    `plotly.plotly.iplot`.
+    To embed an image of the chart, use `plotly.image.ishow`.
+
+    figure_or_data -- a plotly.graph_objs.Figure or plotly.graph_objs.Data or
+                      dict or list that describes a Plotly graph.
+                      See https://plot.ly/python/ for examples of
+                      graph descriptions.
+
+    Keyword arguments:
+    show_link (default=True) -- display a link in the bottom-right corner of
+                                of the chart that will export the chart to
+                                Plotly Cloud or Plotly Enterprise
+    link_text (default='Export to plot.ly') -- the text of export link
+    validate (default=True) -- validate that all of the keys in the figure
+                               are valid? omit if your version of plotly.js
+                               has become outdated with your version of
+                               graph_reference.json or if you need to include
+                               extra, unnecessary keys in your figure.
+
+    Example:
+    ```
+    from plotly.offline import init_notebook_mode, iplot
+    init_notebook_mode()
+
+    iplot([{'x': [1, 2, 3], 'y': [5, 2, 7]}])
+    ```
+    """
+    if not __PLOTLY_OFFLINE_INITIALIZED:
+        raise PlotlyError('\n'.join([
+            'Plotly Offline mode has not been initialized in this notebook. '
+            'Run: ',
+            '',
+            'import plotly',
+            'plotly.offline.init_notebook_mode() '
+            '# run at the start of every ipython notebook',
+        ]))
+    if not tools._ipython_imported:
+        raise ImportError('`iplot` can only run inside an IPython Notebook.')
+
+    from IPython.display import HTML, display
+
+    plot_html, plotdivid, width, height = _plot_html(
+        figure_or_data, show_link, link_text, validate,
+        '100%', 525)
+
+    display(HTML(plot_html))
+
+
+def plot(figure_or_data,
+         show_link=True, link_text='Export to plot.ly',
+         validate=True, output_type='file',
+         include_plotlyjs=True,
+         filename='temp-plot.html',
+         auto_open=True):
+    """ Create a plotly graph locally as an HTML document or string.
+
+    Example:
+    ```
+    from plotly.offline import plot
+    import plotly.graph_objs as go
+
+    plot([
+        go.Scatter(x=[1, 2, 3], y=[3, 2 6])
+    ], filename='my-graph.html')
+    ```
+    More examples below.
+
+    figure_or_data -- a plotly.graph_objs.Figure or plotly.graph_objs.Data or
+                      dict or list that describes a Plotly graph.
+                      See https://plot.ly/python/ for examples of
+                      graph descriptions.
+
+    Keyword arguments:
+    show_link (default=True) -- display a link in the bottom-right corner of
+        of the chart that will export the chart to Plotly Cloud or
+        Plotly Enterprise
+    link_text (default='Export to plot.ly') -- the text of export link
+    validate (default=True) -- validate that all of the keys in the figure
+        are valid? omit if your version of plotly.js has become outdated
+        with your version of graph_reference.json or if you need to include
+        extra, unnecessary keys in your figure.
+    output_type ('file' | 'div' - default 'file') -- if 'file', then
+        the graph is saved as a standalone HTML file and `plot`
+        returns None.
+        If 'div', then `plot` returns a string that just contains the
+        HTML <div> that contains the graph and the script to generate the
+        graph.
+        Use 'file' if you want to save and view a single graph at a time
+        in a standalone HTML file.
+        Use 'div' if you are embedding these graphs in an HTML file with
+        other graphs or HTML markup, like a HTML report or an website.
+    include_plotlyjs (default=True) -- If True, include the plotly.js
+        source code in the output file or string.
+        Set as False if your HTML file already contains a copy of the plotly.js
+        library.
+    filename (default='temp-plot.html') -- The local filename to save the
+        outputted chart to. If the filename already exists, it will be
+        overwritten. This argument only applies if `output_type` is 'file'.
+    auto_open (default=True) -- If True, open the saved file in a
+        web browser after saving.
+        This argument only applies if `output_type` is 'file'.
+    """
+    if output_type not in ['div', 'file']:
+        raise ValueError(
+            "`output_type` argument must be 'div' or 'file'. "
+            "You supplied `" + output_type + "``")
+    if not filename.endswith('.html') and output_type == 'file':
+        warnings.warn(
+            "Your filename `" + filename + "` didn't end with .html. "
+            "Adding .html to the end of your file.")
+        filename += '.html'
+
+    plot_html, plotdivid, width, height = _plot_html(
+        figure_or_data, show_link, link_text, validate,
+        '100%', '100%')
+
+    figure = tools.return_figure_from_figure_or_data(figure_or_data, validate)
+
+    resize_script = ''
+    if width == '100%' or height == '100%':
+        resize_script = (
+            ''
+            '<script type="text/javascript">'
+            'window.removeEventListener("resize");'
+            'window.addEventListener("resize", function(){{'
+            'Plotly.Plots.resize(document.getElementById("{id}"));}});'
+            '</script>'
+        ).format(id=plotdivid)
+
+    if output_type == 'file':
+        with open(filename, 'w') as f:
+            if include_plotlyjs:
+                plotly_js_script = ''.join([
+                    '<script type="text/javascript">',
+                    get_plotlyjs(),
+                    '</script>',
+                ])
+            else:
+                plotly_js_script = ''
+
+            f.write(''.join([
+                '<html>',
+                '<head><meta charset="utf-8" /></head>',
+                '<body>',
+                plotly_js_script,
+                plot_html,
+                resize_script,
+                '</body>',
+                '</html>']))
+
+        url = 'file://' + os.path.abspath(filename)
+        if auto_open:
+            webbrowser.open(url)
+
+        return url
+
+    elif output_type == 'div':
+        if include_plotlyjs:
+            return ''.join([
+                '<div>',
+                '<script type="text/javascript">',
+                get_plotlyjs(),
+                '</script>',
+                plot_html,
+                '</div>'
+            ])
+        else:
+            return plot_html
