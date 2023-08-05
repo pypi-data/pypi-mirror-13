530c14a330657bd2c52e75042bf60e4d5e4be2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package cpenv/0.2.34/cpenv-0.2.34-py2.py3-none-any.whl

---
+++ packages/cpenv/cpenv-0.2.34-py2.py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,43 @@
+import os
+from contextlib import contextmanager
+from functools import partial
+
+data_path = partial(os.path.join, os.path.dirname(__file__), 'data')
+
+
+@contextmanager
+def cwd(new_cwd):
+
+    old_cwd = os.getcwd()
+
+    try:
+        os.chdir(new_cwd)
+        yield
+    except:
+        raise
+    finally:
+        os.chdir(old_cwd)
+
+
+def touch(filepath):
+
+    with open(filepath, 'a'):
+        os.utime(filepath, None)
+
+
+def make_files(*filepaths, **kwargs):
+    data = kwargs.get('text', None)
+
+    for filepath in filepaths:
+        d = os.path.dirname(filepath)
+
+        try:
+            os.makedirs(d)
+        except:
+            pass
+
+        if not data:
+            touch(filepath)
+        else:
+            with open(filepath, 'w') as f:
+                f.write(data)
