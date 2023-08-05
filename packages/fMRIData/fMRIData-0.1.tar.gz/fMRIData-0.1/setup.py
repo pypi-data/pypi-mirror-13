2c678432088af56765ec46f47bb8795ed4a94aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:48 +0000

    Add package fMRIData/0.1/fMRIData-0.1.tar.gz

---
+++ packages/fMRIData/fMRIData-0.1.tar.gz/fMRIData-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(name="fMRIData",
+      version="0.1",
+      description="An fMRIData utiliy",
+      author="Babak Afshin-Pour",
+      author_email='contact@babakafshinpour.com',
+      platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+      license="BSD",
+      url="http://www.babakafshinpour.com",
+      packages=find_packages(),
+      )
+      
