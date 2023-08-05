e6a1d934b5725c6f7cdb820099e52bcafb40bf22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:12 +0000

    Add package bg/1.5.2/bg-1.5.2.tar.gz

---
+++ packages/bg/bg-1.5.2.tar.gz/bg-1.5.2/bg/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__author__ = "Sergey Aganezov"
+__email__ = "aganezov(at)gwu.edu"
+__status__ = "production"
+
+version = "1.5.2"
+
+__all__ = ["bg_io",
+           "breakpoint_graph",
+           "edge",
+           "genome",
+           "kbreak",
+           "multicolor",
+           "tree",
+           "vertices",
+           "utils",
+           "distances"]
+
+from bg.breakpoint_graph import BreakpointGraph
+from bg.vertices import BGVertex, BlockVertex, InfinityVertex
+from bg.edge import BGEdge
+from bg.multicolor import Multicolor
+from bg.bg_io import GRIMMReader
+from bg.genome import BGGenome
+from bg.kbreak import KBreak
+from bg.tree import BGTree, NewickReader
