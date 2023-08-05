055b1ac6ce5286282788766833a376cecd25eda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:40 +0000

    Add package myNester_gptjdwkd89/1.1.0/myNester_gptjdwkd89-1.1.0.tar.gz

---
+++ packages/myNester_gptjdwkd89/myNester_gptjdwkd89-1.1.0.tar.gz/myNester_gptjdwkd89-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'myNester_gptjdwkd89',
+        version         = '1.1.0',
+        py_modules      = ['myNester_gptjdwkd89'],
+        author          = 'gptjdwkd89 ',
+        author_email    = 'jjrrtt@naver.com',
+        url             = 'http://kimtajo.cafe24.com',
+        description     = 'A simple printer of nesterd lists',
+    )
