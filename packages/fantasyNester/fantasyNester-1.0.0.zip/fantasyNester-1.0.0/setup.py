afd92845a953ba91aee71e7fdaba0deca6760cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package fantasyNester/1.0.0/fantasyNester-1.0.0.zip

---
+++ packages/fantasyNester/fantasyNester-1.0.0.zip/fantasyNester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='fantasyNester',
+    version='1.0.0',
+    py_modules=['fantasyNester'],
+    author='haohonglee',
+    author_email='haohonglee@163.com',
+    url='',
+    description='A simple printer of nested lists'
+)
