43cb60bdb3cd99edd68cd90ea0f895ed5ed145f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:54 +0000

    Add package sqlalchemy_validation/0.4.0a0/sqlalchemy_validation-0.4.0a0-py3.4.egg

---
+++ packages/sqlalchemy_validation/sqlalchemy_validation-0.4.0a0-py3.4.egg/sqlalchemy_validation/error.py
@@ -0,0 +1,174 @@
+"""
+"""
+
+from collections import UserDict
+
+
+class BaseValidationError(Exception):
+    """Generic Error Class.
+    """
+    pass
+
+
+class ValidationError(UserDict, BaseValidationError):
+    """
+    key: A tuple of column names.
+    value: A BaseValidationError instance.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        init_dict = kwargs.pop("init_dict_", {})
+        UserDict.__init__(self, dict=init_dict, **kwargs)
+        BaseValidationError.__init__(self, *args)
+
+    def __str__(self):
+        return "The following Errors have raised!\n\n{}".format(
+            "\n\n".join("{}\n{}".format(key, val) for key, val in self.items())
+        )
+
+
+class ValidatesError(BaseValidationError):
+    """
+    Attributes:
+      model: A Model instance.
+      column: A Column instance.
+      value: A value tried to set.
+      model_name: A Model name.
+    """
+    def __init__(self, model, column, value):
+        """
+        Args:
+          model: A Model instance.
+          column: A Column instance.
+          value: A value tried to set.
+          model_name: A Model name.
+        """
+        self.model = model
+        self.column = column
+        self.value = value
+        self.model_name = model.__class__.__name__
+        super(ValidatesError, self).__init__()
+
+
+class EnumError(ValidatesError):
+    """Enum Constraint.
+    """
+    def __str__(self):
+        return ("EnumError!\nTable.column: {}.{}\n"
+                "Enum: {}\nvalue: {}").format(
+            self.model_name, self.column.name,
+            self.column.type.enums, self.value
+        )
+
+
+class TooShortError(ValidatesError):
+    """Length Constraint.
+    """
+    def __str__(self):
+        return ("TooShortError!\n"
+                "Table.column: {}.{}\n"
+                "length limitation: {}\n"
+                "value(length): {}({})").format(
+            self.model_name, self.column.name,
+            self.column.length, self.value, len(self.value)
+        )
+
+
+class TooLongError(ValidatesError):
+    """Length Constraint.
+    """
+    def __str__(self):
+        return ("TooLongError!\n"
+                "Table.column: {}.{}\n"
+                "length limitation: {}\n"
+                "value(length): {}({})").format(
+            self.model_name, self.column.name,
+            self.column.length, self.value, len(self.value)
+        )
+
+
+class OverMaxError(ValidatesError):
+    """Size Constraint.
+    """
+    def __str__(self):
+        return ("OverMaxError!\n"
+                "Table.column: {}.{}\n"
+                "size limitation: {}\n"
+                "value: {}").format(
+            self.model_name, self.column.name,
+            self.column.size, self.value
+        )
+
+
+class OverMinError(ValidatesError):
+    """Size Constraint.
+    """
+    def __str__(self):
+        value = getattr(self.model, self.column.name)
+        return ("OverMinError!\n"
+                "Table.column: {}.{}\n"
+                "size limitation: {}\n"
+                "value: {}").format(
+            self.model_name, self.column.name,
+            self.column.size, self.value
+        )
+
+
+class NotNullError(ValidatesError):
+    """Not Null Constraint.
+
+    Attributes:
+      model: A Model instance.
+      column: A Column instance.
+      model_name: A Model name.
+    """
+    def __init__(self, model, column):
+        """
+        Args:
+          model: A Model instance.
+          column: A Column instance.
+          model_name: A Model name.
+        """
+        super(NotNullError, self).__init__(model, column, None)
+
+    def __str__(self):
+        return ("NotNullError!\n""{}.{} can't be None.").format(
+            self.model_name, self.column.name
+        )
+
+
+class InvalidTypeError(ValidatesError):
+    """Type Constraint.
+    """
+    def __str__(self):
+        return ("InvalidTypeError!\n"
+                "Table.column: {}.{}\n"
+                "expected type: {}\n"
+                "value(type): {}({})").format(
+            self.model_name, self.column.name,
+            self.column.type.python_type, self.value, type(self.value)
+        )
+
+
+class EmailError(ValidatesError):
+    """Email Format Constraint.
+    """
+    def __str__(self):
+        return ("EmailError!\n"
+                "Table.column: {}.{}\n"
+                "value: {}").format(
+            self.model_name, self.column.name, self.value
+        )
+
+
+class RegExpError(ValidatesError):
+    """RegExp Constraint.
+    """
+    def __str__(self):
+        return ("RegExpError!\n"
+                "Table.column: {}.{}\n"
+                "RegExp: {}\n"
+                "value: {}").format(
+            self.model_name, self.column.name, self.column.regexp, self.value
+        )
---
+++ packages/sqlalchemy_validation/sqlalchemy_validation-0.4.0a0-py3.4.egg/sqlalchemy_validation/model.py
@@ -0,0 +1,82 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative.api import DeclarativeMeta
+
+from .error import BaseValidationError, ValidationError
+from .attribute import validate_attribute
+
+
+class MetaClass(DeclarativeMeta):
+    """
+    """
+
+    def __init__(cls, classname, bases, dict_):
+        """
+        """
+        super(MetaClass, cls).__init__(classname, bases, dict_)
+        if hasattr(cls, "__table__"):
+            validate_attribute(cls)
+
+
+def _table_columns(constraint, table):
+    return "{}__{}".format(
+        table.name, "__".join(sorted(constraint.columns.keys()))
+    )
+
+
+metadata = MetaData(naming_convention={
+    "table_columns": _table_columns,
+    "ix": "ix_%(table_columns)s",
+    "uq": "uq_%(table_columns)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s__%(column_0_name)s__%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+})
+
+
+class BaseModel(object):
+    """
+    """
+    def __init__(self, **kwargs):
+        """
+        Args:
+          kwargs:
+
+        Raises:
+          TypeError:
+          sqlalchemy_validation.ValidationError
+        """
+        cls_ = type(self)
+        errors = ValidationError()
+        for column_name, value in kwargs.items():
+            if not hasattr(cls_, column_name):
+                raise TypeError(
+                    "{!r} is an invalid keyword argument for {!s}".format
+                    (column_name, cls_.__name__))
+            try:
+                setattr(self, column_name, value)
+            except BaseValidationError as e:
+                errors[(column_name,)] = e
+        if errors:
+            raise errors
+
+    def __call__(self, **kwargs):
+        """Sets kwargs to model's columns and returns self.
+
+        Args:
+
+        Returns:
+          self
+
+        Raises:
+          TypeError:
+          sqlalchemy_validation.ValidationError
+        """
+        self.__init__(**kwargs)
+        return self
+
+
+Model = declarative_base(
+    cls=BaseModel, constructor=None, metaclass=MetaClass,
+    metadata=metadata
+)
