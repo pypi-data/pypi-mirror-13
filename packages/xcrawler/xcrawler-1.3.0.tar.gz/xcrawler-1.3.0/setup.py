9a83f1edb6bd1e950e600654be1aed67d23b689c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:03 +0000

    Add package xcrawler/1.3.0/xcrawler-1.3.0.tar.gz

---
+++ packages/xcrawler/xcrawler-1.3.0.tar.gz/xcrawler-1.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    long_description = readme_file.read()
+
+setup(name='xcrawler',
+      packages=find_packages(),
+      install_requires=['lxml', 'cssselect', 'six', 'requests>=2.4.0'],
+      version='1.3.0',
+      description='A multi-threaded, open source web crawler',
+      long_description=long_description,
+      url='https://github.com/cardsurf/xcrawler',
+      author='cardsurf',
+      author_email='cardsurf@email.com',
+      license='GNU GPL v2.0',
+      platforms=['Any'],
+      keywords=['crawler', 'spider', 'scraper'],
+      classifiers=[
+                    'Programming Language :: Python',
+                    'Programming Language :: Python :: 2',
+                    'Programming Language :: Python :: 2.7',
+                    'Programming Language :: Python :: 3',
+                    'Programming Language :: Python :: 3.4',
+                    'Operating System :: OS Independent',
+                    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                    'Topic :: Internet :: WWW/HTTP',
+                    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                  ],
+      )
+
