95a044614462401fbfc9d80bc86c89ccabfa0276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:19 +0000

    Add package tryp/2.1.0/tryp-2.1.0.tar.gz

---
+++ packages/tryp/tryp-2.1.0.tar.gz/tryp-2.1.0/tryp/__init__.py
@@ -0,0 +1,15 @@
+from fn import _  # type: ignore
+
+from tryp.maybe import Maybe, Just, Empty, may, flat_may
+from tryp.list import List
+from tryp.map import Map
+from tryp.future import Future
+from tryp.boolean import Boolean
+from tryp.func import curried
+from tryp.anon import __
+
+development = False
+
+
+__all__ = ('Maybe', 'Just', 'Empty', 'may', 'List', 'Map', '_', 'Future',
+           'Boolean', 'development', 'flat_may', 'curried', '__')
