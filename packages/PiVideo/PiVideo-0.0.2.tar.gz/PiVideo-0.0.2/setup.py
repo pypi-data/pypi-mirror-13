47c3658506c8545ccca7e93a9b319e8798ac478a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package PiVideo/0.0.2/PiVideo-0.0.2.tar.gz

---
+++ packages/PiVideo/PiVideo-0.0.2.tar.gz/PiVideo-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="PiVideo",
+
+    # Version number (initial):
+    version="0.0.2",
+
+    # Application author details:
+    author="Lintest Systems LLC",
+    author_email="lintestsystems@gmail.com",
+
+    # Packages
+    packages=["pivideo"],
+
+    # Include additional files into the package
+    #include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/pivideo_v002/",
+
+    # License
+    license="LICENSE.txt",
+
+    description="Raspberry Pi PiVideo Utilities",
+    long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "python-smbus",
+    ],
+)
