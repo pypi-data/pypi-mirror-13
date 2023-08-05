16db83dd6c19f5e7a3dfdf7678164de6841db65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package bash-toolbelt/0.0.2/bash-toolbelt-0.0.2.tar.gz

---
+++ packages/bash-toolbelt/bash-toolbelt-0.0.2.tar.gz/bash-toolbelt-0.0.2/bash/os.py
@@ -0,0 +1,54 @@
+import codecs
+import json
+import os
+import platform
+import subprocess
+import time
+
+
+def run(cmd):
+    try:
+        # Python 3.5
+        # result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE).stdout.strip()
+        # return result.stdout, result.returncode
+        return subprocess.check_output(cmd, shell=True).strip()
+    except subprocess.CalledProcessError as e:
+        return ''
+
+def wait_for(limit, seconds, func, *args, **kwargs):
+    c = 0
+    while c < limit and func(*args, **kwargs):
+        c += 1
+        time.sleep(seconds)
+    return func(*args, **kwargs)
+
+def read_config_file(filename):
+    """
+    Example of the file localhost.json:
+    {
+        "ami": "123",
+        "hosts": ["a.com", "b.com"]
+    }
+    """
+    if os.path.exists(filename):
+        with codecs.open(filename, 'r', 'utf-8') as f:
+           return json.loads(f.read())
+
+def str2bool(string):
+    "It returns True, False or None"
+    if not string:
+        return None
+    if isinstance(string, (str,)):
+        string = string.lower()
+        options = ['none', 'null', 'nil']
+        if string in options:
+            return None
+        options = ['false', 'f', 'no', '0']
+        return not (string in options)
+    return string
+
+def is_mac():
+    return platform.system().lower() == 'darwin'
+
+def is_linux():
+    return platform.system().lower() == 'linux'
---
+++ packages/bash-toolbelt/bash-toolbelt-0.0.2.tar.gz/bash-toolbelt-0.0.2/setup.py
@@ -0,0 +1,53 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+# http://guide.python-distribute.org/quickstart.html
+# python setup.py sdist
+# python setup.py register
+# python setup.py sdist upload
+# pip install django-dynamic-fixture
+# pip install django-dynamic-fixture --upgrade --no-deps
+# Manual upload to PypI
+# http://pypi.python.org/pypi/django-dynamic-fixture
+# Go to 'edit' link
+# Update version and save
+# Go to 'files' link and upload the file
+
+VERSION = '0.0.2'
+
+tests_require = [
+    'pytest==2.8.5',
+    'tox==2.3.1',
+]
+
+install_requires = [
+]
+
+# from pip.req import parse_requirements
+# install_requires = parse_requirements('requirements.txt')
+# tests_require = parse_requirements('requirements-dev.txt')
+
+setup(name='bash-toolbelt',
+      url='https://github.com/paulocheque/bash-toolbelt',
+      author="paulocheque",
+      author_email='paulocheque@gmail.com',
+      keywords='python bach utility library git os',
+      description='',
+      license='MIT',
+      classifiers=[
+          'Operating System :: OS Independent',
+          'Topic :: Software Development',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: PyPy',
+      ],
+
+      version=VERSION,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      test_suite='runtests.runtests',
+      extras_require={'test': tests_require},
+
+      packages=find_packages(),
+)
+
