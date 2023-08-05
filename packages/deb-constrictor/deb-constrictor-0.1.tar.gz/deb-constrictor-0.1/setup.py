87b8c7f457c3a3f662db8fac20e9892f314c63c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:43 +0000

    Add package deb-constrictor/0.1/deb-constrictor-0.1.tar.gz

---
+++ packages/deb-constrictor/deb-constrictor-0.1.tar.gz/deb-constrictor-0.1/setup.py
@@ -0,0 +1,54 @@
+import os
+import re
+import sys
+
+from codecs import open
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'constrictor',
+]
+
+requires = []
+
+version = ''
+with open('constrictor/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='deb-constrictor',
+    version=version,
+    description='Build DPKGs natively with Python.',
+    long_description=readme,
+    author='Ben Shaw',
+    author_email='ben@bbit.co.nz',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'constrictor': 'constrictor'},
+    include_package_data=True,
+    install_requires=requires,
+    license='BSD 3-Clause',
+    zip_safe=True,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    )
+)
