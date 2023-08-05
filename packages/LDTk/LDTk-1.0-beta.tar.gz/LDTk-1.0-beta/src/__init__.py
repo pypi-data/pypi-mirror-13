5ef66e2991118ee747441f463e702c83ce7be924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:32 +0000

    Add package LDTk/1.0-beta/LDTk-1.0-beta.tar.gz

---
+++ packages/LDTk/LDTk-1.0-beta.tar.gz/LDTk-1.0-beta/src/__init__.py
@@ -0,0 +1,4 @@
+from ldtk import LDPSetCreator, LDPSet
+from filters import BoxcarFilter, TabulatedFilter
+
+__all__ = ['LDPSetCreator','LDPSet','BoxcarFilter','TabulatedFilter','ldtk_cache','ldtk_root']
