b8b98bd5988c6297f8e93bf4cad0c95263f4e2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package ascetic/0.7.2.28/ascetic-0.7.2.28.tar.gz

---
+++ packages/ascetic/ascetic-0.7.2.28.tar.gz/ascetic-0.7.2.28/ascetic/contrib/modeltranslation.py
@@ -0,0 +1,81 @@
+import collections
+from ..models import Field
+
+# We can't use TranslationRegistry, because Mapper can be inherited, and we need to fix hierarchy???
+
+
+class TranslationColumnDescriptor(object):
+
+    def __get__(self, instance, owner):
+        if not instance:
+            return self
+        return instance._mapper.translate_column(instance.original_column)
+
+    def __set__(self, instance, value):
+        instance.original_column = value.rsplit('_', 1)[0]
+
+
+class TranslationField(Field):
+    column = TranslationColumnDescriptor()
+
+
+class TranslationMapper(object):
+
+    translated_fields = ()
+
+    def create_translation_field(self, name, data, declared_fields=None):
+        field = super(TranslationMapper, self).create_field(name, data, declared_fields)
+        if name in self.translated_fields and not isinstance(field, TranslationField):
+            NewField = type("Translation{}".format(field.__class__.__name__), (TranslationField, field.__class__), {})
+            field = NewField(**field.__dict__)
+            # field.__class__ = NewField
+        return field
+
+    def create_fields(self, columns, declared_fields):
+        fields = collections.OrderedDict()
+        rmap = {field.column: name for name, field in declared_fields.items() if hasattr(field, 'column')}
+
+        original_columns = {}
+        for name in self.translated_fields:
+            if name in declared_fields and hasattr(declared_fields[name], 'column'):
+                field = declared_fields[name]
+                column = field.original_column if isinstance(field, TranslationField) else field.column
+            else:
+                column = name
+            for lang in self.get_languages():
+                original_columns[self.translate_column(column, lang)] = column
+
+        for data in columns:
+            column_name = data['column']
+            column_name = original_columns.get(column_name, column_name)
+            name = rmap.get(column_name, column_name)
+            if name in fields:
+                continue
+            fields[name] = self.create_translation_field(name, data, declared_fields)
+
+        for name, field in declared_fields.items():
+            if name not in fields:
+                fields[name] = self.create_translation_field(name, {'virtual': True}, declared_fields)
+        return fields
+
+    def add_field(self, name, field):
+        field.name = name
+        field._mapper = self
+        self.fields[name] = field
+        if isinstance(field, TranslationField):
+            for lang in self.get_languages():
+                self.columns[self.translate_column(field.original_column, lang)] = field
+        else:
+            self.columns[field.column] = field
+
+    def _make_identity_key(self, model, pk):
+        return super(TranslationMapper, self)._make_identity_key(model, pk) + (self.get_language(),)
+
+    def translate_column(self, name, lang=None):
+        return '{}_{}'.format(name, lang or self.get_language())
+
+    def get_language(self):
+        raise NotImplementedError
+
+    def get_languages(self):
+        raise NotImplementedError
