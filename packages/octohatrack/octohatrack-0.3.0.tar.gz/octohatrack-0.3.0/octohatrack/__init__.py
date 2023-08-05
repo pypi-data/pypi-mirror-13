148a625cfa5c00ac2a1f64a64ac6f4e2ba097637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:28 +0000

    Add package octohatrack/0.3.0/octohatrack-0.3.0.tar.gz

---
+++ packages/octohatrack/octohatrack-0.3.0.tar.gz/octohatrack-0.3.0/octohatrack/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+from .helpers import * 
+
+def main(): 
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("repo_name", help="githubuser/repo")
+  parser.add_argument("-g", "--generate-html", action='store_true', help="Generate output as HTML")
+  parser.add_argument("-l", "--limit", help="Limit to the last x Issues/Pull Requests", type=int, default=0)
+  parser.add_argument("-c", "--show-contributors", action='store_true', help="Output the code contributors")
+  parser.add_argument("-n", "--show-names", action='store_true', help="Show the user's display name")
+  args = parser.parse_args()  
+
+  repo_name = args.repo_name
+
+  try:
+    if not repo_exists(repo_name):
+      print("Repo does not exist: %s" % repo_name)
+      sys.exit(1)
+
+    code_contributors = get_code_contributors(repo_name)
+    code_commentors = get_code_commentors(repo_name, args.limit)
+  except ValueError as e:
+    print(e)
+    sys.exit(1)
+
+  non_code_contributors = []
+  for user in code_commentors:
+    user_name, avatar, name = user 
+    if user not in code_contributors:
+      non_code_contributors.append(user)
+
+  code_contributors = sorted(code_contributors, key=lambda k: k['user_name'].lower()) 
+  non_code_contributors = sorted(non_code_contributors, key=lambda k: k['user_name'].lower()) 
+
+  print("\nCode contributions: %d" % len(code_contributors))
+
+  if args.show_contributors:
+    for user in code_contributors: 
+      display_user_name(user, args)
+  
+  print("\nNon-code contributions: %d" % len(non_code_contributors))
+
+  for user in non_code_contributors: 
+    display_user_name(user, args)
+
+  if args.generate_html:
+    generate_html(code_contributors, non_code_contributors, args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/octohatrack/octohatrack-0.3.0.tar.gz/octohatrack-0.3.0/octohatrack/helpers.py
@@ -0,0 +1,170 @@
+import os
+import sys
+import atexit
+import json
+from .connection import Connection, Pager
+from .exceptions import ResponseError
+from .generatehtml import generate_html
+from functools import wraps
+
+# Always run on start import
+cache_file = "cache_file.json"
+cache = {}
+
+if os.path.isfile(cache_file):
+    with open(cache_file, "r") as f:
+        try: 
+            cache = json.load(f) 
+        except ValueError:
+            pass 
+
+# Always run on exit
+def save_cache():
+    with open(cache_file, 'w') as f:
+        json.dump(cache, f)
+
+atexit.register(save_cache)
+
+def memoise(wrapped):
+
+    @wraps(wrapped)
+    def wrapper(*args, **kwargs):
+        key = args[0]
+        if key not in cache:
+            cache[key] = wrapped(*args, **kwargs)
+        return cache[key]
+
+    return wrapper
+
+token = os.environ.get("GITHUB_TOKEN")
+debug = os.environ.get("DEBUG")
+
+if (token == None):
+    print("Warning: No GITHUB_TOKEN found - Unauthenticated requests are rate limited to 60 requests per hour.")
+else:
+  if debug:
+    print("GITHUB_TOKEN found of length %d" % len(token))
+
+conn = Connection(token)
+  
+def unique(array): 
+  array = [x for x in array if x is not None]
+  return list({v['user_name']:v for v in array}.values())
+
+def flatten(array):
+  return [item for sublist in array for item in sublist]
+
+@memoise
+def get_paged_json(uri):
+  json = []
+  try: 
+      pager = Pager(conn, uri, params={}, max_pages=0)
+      for response in pager:
+          progress_advance()
+          json += response.json()
+  except ResponseError:
+        pass
+
+  return json
+
+def get_code_contributors(repo_name): 
+  progress("Collecting contributors")
+  users = []
+  response = get_paged_json("/repos/%s/contributors" % repo_name)
+  for entry in response:
+      users.append(get_user_data(entry))
+  progress_complete()
+  return unique(users)
+
+def get_code_commentors(repo_name, limit):
+  progress("Collecting commentors")
+
+  pri_count = get_pri_count(repo_name)
+  if limit == 0:
+     minimum = 1
+  else: 
+    minimum = max(1, pri_count - limit)
+
+  users = []
+  for index in range(minimum, pri_count + 1):
+      users.append(get_user("/repos/%s/pulls/%d" % (repo_name, index)))
+      users.append(get_user("/repos/%s/issues/%d" % (repo_name, index)))
+
+      for entry in get_paged_json("/repos/%s/pulls/%d/comments" % (repo_name, index)):
+            users.append(get_user_data(entry))
+
+      for entry in get_paged_json("/repos/%s/issues/%d/comments" % (repo_name, index)):
+            users.append(get_user_data(entry))
+
+  progress_complete()
+
+  return unique(users)
+
+@memoise
+def get_data(uri):
+    try: 
+      resp = conn.send("GET", uri)
+      return resp.json()
+    except ResponseError as e: 
+      return None
+
+def get_pri_count(repo_name):
+    prs = get_data("/repos/%s/pulls?state=all" % repo_name)
+    issues = get_data("/repos/%s/issues?state=all" % repo_name)
+
+    if not prs:
+        pr_count = 0
+    else:
+        pr_count = prs[0]["number"]
+
+    if not issues:
+        issue_count = 0
+    else:
+        issue_count = issues[0]["number"]
+
+    return max(pr_count, issue_count)
+
+def get_user_data(entry):
+    if "user" in entry.keys():
+      return {"user_name": entry["user"]["login"], "avatar": "%s&s=128" % entry["user"]["avatar_url"],
+              "name": get_user_name(entry["user"]["login"])}
+    else:
+      return {"user_name": entry["login"], "avatar": "%s&s=128" % entry["avatar_url"], "name": get_user_name(entry["login"])}
+
+@memoise
+def get_user(uri):
+    progress_advance()
+    entry = get_data(uri)
+    if entry is not None:
+        return get_user_data(entry)
+
+@memoise
+def repo_exists(repo_name):
+    try:
+        repo = conn.send("GET", "/repos/%s" % repo_name)
+        return True 
+    except ResponseError as e:
+        return False
+
+def progress(message):
+    sys.stdout.write("%s..." % message)
+    sys.stdout.flush()
+
+def progress_advance():
+    sys.stdout.write(".")
+    sys.stdout.flush()
+
+def progress_complete():
+    sys.stdout.write("\n")
+
+def get_user_name(login):
+    user = get_data("/users/%s" % login)
+    if user["name"] is None: user["name"] = login
+    return user["name"]
+
+def display_user_name(user, args):
+    if args.show_names and user["name"] != user["user_name"]:
+      print("%s (%s)" % (user["user_name"], user['name']))
+    else: 
+      print(user["user_name"])
+
