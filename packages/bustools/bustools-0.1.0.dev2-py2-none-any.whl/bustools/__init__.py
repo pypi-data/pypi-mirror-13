89d660fb09557b60ea7ebd926449cdaa616a9657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:02 +0000

    Add package bustools/0.1.0.dev2/bustools-0.1.0.dev2-py2-none-any.whl

---
+++ packages/bustools/bustools-0.1.0.dev2-py2-none-any.whl/bustools/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#==========================================================================
+# DISTRIBUTION VERSION
+#--------------------------------------------------------------------------
+# This is a PEP 0008 and PEP 0440 compliant version string, i.e.
+# "major.minor.micro".  It is read by setup.py to determine the
+# distribution version for public release on PyPI.
+#
+# For a discussion on single-sourcing the version across setup.py and
+# the project code, see
+# https://packaging.python.org/en/latest/single_source_version.html
+#==========================================================================
+__version__ = "0.1.0.dev2"
+
+def version():
+    return __version__
