4639b793ab42350339773d17b25859168f067619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:17 +0000

    Add package Flask-Z3950/0.1.1/Flask-Z3950-0.1.1.tar.gz

---
+++ packages/Flask-Z3950/Flask-Z3950-0.1.1.tar.gz/Flask-Z3950-0.1.1/flask_z3950/dataset.py
@@ -0,0 +1,65 @@
+# -*- coding: utf8 -*-
+"""Dataset module for Flask-Z3950."""
+
+import json
+import os
+import pymarc
+from lxml import etree
+
+
+class Dataset(object):
+    """Dataset class with functions for transforming raw record data.
+
+    :param record_data: A list of raw record data.
+    """
+
+    def __init__(self, record_data):
+        self.record_data = record_data
+
+
+    def to_str(self):
+        """Return a string representation of all raw record data."""
+        return ''.join([r for r in self.record_data])
+
+
+    def to_marcxml(self):
+        """Return a MARCXML representation of any MARC records."""
+        records = [pymarc.Record(data=r) for r in self.record_data]
+        xmllist = [pymarc.record_to_xml(r) for r in records]
+        xmlstr = "".join(xmllist)
+        xmldoc = """<?xml version="1.0" encoding="utf-8"?>
+                    <collection xmlns="http://www.loc.gov/MARC21/slim">
+                        {0}
+                    </collection>""".format(xmlstr)
+        return self._transform(xmldoc, 'format-xml.xsl')
+
+
+    def to_html(self):
+        """Return an HTML representation of any MARC records."""
+        records = [pymarc.Record(data=r) for r in self.record_data]
+        xmllist = [pymarc.record_to_xml(r) for r in records]
+        xslt = 'marcxml-to-html.xsl'
+        html_list = [self._transform(xml, xslt) for xml in xmllist]
+        return "".join(html_list)
+
+
+    def to_json(self, **kwargs):
+        """Return a JSON representation of any MARC records.
+
+        :param **kwargs: Arbitrary keyword arguments to add to the JSON."""
+        reclist = [pymarc.Record(data=r).as_dict() for r in self.record_data]
+        recdict = {"records": reclist}
+        recdict.update(kwargs)
+        return json.dumps(recdict)
+
+
+
+    def _transform(self, xml, xslt_fn):
+        """Return the result of an XSLT transformation."""
+        here = os.path.dirname(os.path.abspath(__file__))
+        xslt_path = os.path.join(here, 'xsl', xslt_fn)
+        xslt = etree.parse(xslt_path)
+        doc = etree.fromstring(xml)
+        transform = etree.XSLT(xslt)
+        newdoc = transform(doc)
+        return etree.tostring(newdoc, pretty_print=True)
---
+++ packages/Flask-Z3950/Flask-Z3950-0.1.1.tar.gz/Flask-Z3950-0.1.1/setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf8 -*-
+"""
+Flask-Z3950
+-----------
+
+A Flask plugin that provides Z39.50 integration.
+"""
+
+import os
+from setuptools import setup, find_packages
+
+try:
+    here = os.path.dirname(__file__)
+    long_description = open(os.path.join(here, 'docs', 'index.rst')).read()
+except:
+    long_description = ""
+
+requirements=[
+    "Flask>=0.10.1",
+    "lxml>=3.5.0, <4.0",
+    "ply>=3.8.0, <4.0",
+    "pymarc>=3.0.4, <4.0",
+    "PyZ3950",
+    ]
+
+dependency_links=[
+    "git+https://github.com/asl2/PyZ3950.git#egg=PyZ3950"
+    ]
+
+tests_require = [
+    "pytest-cov>=2.2.0, <3.0",
+    "pytest>=2.8.4, <3.0",
+]
+
+setup(
+    name="Flask-Z3950",
+    version="0.1.1",
+    author="Alexander Mendes",
+    author_email="alexanderhmendes@gmail.com",
+    description="A Flask plugin that provides Z39.50 integration.",
+    license="BSD",
+    url="https://github.com/alexandermendes/Flask-Z3950",
+    packages=['flask_z3950'],
+    long_description=long_description,
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=requirements,
+    dependency_links=dependency_links,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: Z39.50",
+    ],
+)
