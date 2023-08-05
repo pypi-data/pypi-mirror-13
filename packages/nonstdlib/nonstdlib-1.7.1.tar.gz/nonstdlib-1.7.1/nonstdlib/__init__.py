1b541ec5e28d0c7f13c0d08798101e17ad2e280f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package nonstdlib/1.7.1/nonstdlib-1.7.1.tar.gz

---
+++ packages/nonstdlib/nonstdlib-1.7.1.tar.gz/nonstdlib-1.7.1/nonstdlib/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__version__ = '1.7.1'
+
+from .approx import *
+from .debug import *
+from .io import *
+from .fmt import *
+from .meta import *
+from .misc import *
+from .text import *
