fb73ec8e363030f8e5bdde68b89b74adc4ea46f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:53 +0000

    Add package ANSI-Color/1.0.0/ANSI-Color-1.0.0.tar.gz

---
+++ packages/ANSI-Color/ANSI-Color-1.0.0.tar.gz/ANSI-Color-1.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 *-*
+__author__ = 'Naitiz'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ANSI-Color",
+    version="1.0.0",
+    author=__author__,
+    author_email="bunnyswe@qq.com",
+    packages=find_packages(),
+    license="The MIT License",
+    url="https://github.com/bunnyswe/PythonAnsiColor",
+    description="print colorful on your *sh",
+    keywords="asni color term shell",
+    zip_safe=True
+)
