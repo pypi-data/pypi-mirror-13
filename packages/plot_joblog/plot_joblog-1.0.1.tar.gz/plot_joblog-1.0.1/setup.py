dc3886b5f5ab63bbd8c7bed2e5dddaa8e9409621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:00 +0000

    Add package plot_joblog/1.0.1/plot_joblog-1.0.1.tar.gz

---
+++ packages/plot_joblog/plot_joblog-1.0.1.tar.gz/plot_joblog-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='plot_joblog',
+    version='1.0.1',
+    description='Produce a time chart of the jobs ran by GNU parallel',
+    author='Maurizio Tomasi',
+    author_email='ziotom78@gmail.com',
+    py_modules=['plot_joblog'],
+    install_requires=[
+        'Click',
+    ],
+    url='https://github.com/ziotom78/plot_joblog',
+    license='MIT',
+    keywords='plot cli gnuparallel',
+    entry_points='''
+        [console_scripts]
+        plot_joblog=plot_joblog:main
+    ''',
+)
