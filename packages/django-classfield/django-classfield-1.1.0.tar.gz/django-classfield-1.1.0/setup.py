ce422d71cafbde5e71001d746f07a1a586fc9e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:17 +0000

    Add package django-classfield/1.1.0/django-classfield-1.1.0.tar.gz

---
+++ packages/django-classfield/django-classfield-1.1.0.tar.gz/django-classfield-1.1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='django-classfield',
+    version='1.1.0',
+    description='Adds a class field to django',
+    author='Mike Harris, Mike Amy',
+    author_email='michael.amy@wfp.org',
+    url='https://github.com/MikeAmy/django-classfield',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=find_packages('.')
+)
