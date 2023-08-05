824abb2fb4eb047d2363713d37cc258879898aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:56 +0000

    Add package donut/0.2/donut-0.2.tar.gz

---
+++ packages/donut/donut-0.2.tar.gz/donut-0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('donut.py') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split('=')[-1].strip()
+
+long_description = open('README.md').read()
+
+setup(name='donut',
+      license='MIT',
+      version=version,
+      description='Makes symlinks to dotfiles.',
+      long_description=long_description,
+      author='Luke Olson',
+      author_email='luke.olson@gmail.com',
+      url='https://github.com/lukeolson/donut',
+      py_modules=['donut'],
+      entry_points={'console_scripts': ['donut = donut:main']},
+      classifiers=['Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Utilities'])
