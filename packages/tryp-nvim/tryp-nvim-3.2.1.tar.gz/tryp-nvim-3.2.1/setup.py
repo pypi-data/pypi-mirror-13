1a175d4b647f8d8a790455424d1c1b3dcf8002ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:19 +0000

    Add package tryp-nvim/3.2.1/tryp-nvim-3.2.1.tar.gz

---
+++ packages/tryp-nvim/tryp-nvim-3.2.1.tar.gz/tryp-nvim-3.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages  # type: ignore
+
+setup(
+    name='tryp-nvim',
+    description='neovim helpers',
+    version='3.2.1',
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/tryp-nvim',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'tryp',
+        'neovim',
+        'pyrsistent',
+    ]
+)
