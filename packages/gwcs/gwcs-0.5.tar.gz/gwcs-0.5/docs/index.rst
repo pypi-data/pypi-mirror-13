a0610f58d7f76410fd783b2e21f1e00817c70f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package gwcs/0.5/gwcs-0.5.tar.gz

---
+++ packages/gwcs/gwcs-0.5.tar.gz/gwcs-0.5/docs/index.rst
@@ -0,0 +1,126 @@
+GWCS Documentation
+====================
+
+`GWCS <https://github.com/spacetelescope/gwcs>`__ is a package for constructing and managing
+the World Coordinate System (WCS) of astronomical data.
+
+Introduction
+------------
+
+
+`GWCS <https://github.com/spacetelescope/gwcs>`__ takes a general approach to WCS.
+It supports a data model which includes the entire transformation pipeline from
+input coordinates (detector by default)  to world cooridnates.
+Transformations can be chained, joined or combined with arithmetic operators
+using the flexible framework of compound models in `~astropy.modeling`.
+Transformations can be chained, joined or combined with arithmetic operators.
+In the case of a celestial output frame `~astropy.coordinates` provides automatically
+further transformations between standard coordinate frames.
+Spectral output coordinates are instances of `~astropy.units.Quantity`  and are
+transformed to other units with the tools in that package.
+The goal is to provide a flexible toolkit which is easily extendable by adding new
+transforms and frames.
+
+
+Installation
+------------
+
+`gwcs <https://github.com/spacetelescope/gwcs>`__ requires:
+
+- `numpy <http://www.numpy.org/>`__ 1.6 or later
+
+- `astropy <http://www.astropy.org/>`__ 1.1 or later
+
+- `pyasdf <http://pyasdf.readthedocs.org/en/latest/>`__
+
+
+Getting Started
+---------------
+
+The simplest way to initialize a WCS object is to pass a `forward_transform` and an `output_frame`
+to `~gwcs.wcs.WCS`. As an example, consider a typical basic FITS WCS of an image.
+The following imports are generally useful:
+
+  >>> from astropy.modeling.models import (Shift, Scale, Rotation2D,
+      Pix2Sky_TAN, RotateNative2Celestial, Mapping, Polynomial2D)
+  >>> from astropy import coordinates as coord
+  >>> from astropy import units as u
+  >>> from gwcs import wcs
+  >>> from gwcs import coordinate_frames
+
+The `forward_transform` is constructed as a combined model using `astropy.modeling`. The frames
+can be strings or subclasses of `~gwcs.coordinate_frames.CoordinateFrame`.
+
+  >>> transform = Shift(-10.5) & Shift(-13.2) | Rotation2D(0.0023) | \
+      Scale(.01) & Scale(.04) | Pix2Sky_TAN() | RotateNative2Celestial(5.6, -72.05, 180)
+  >>> sky_frame = coordinate_frames.CelestialFrame(reference_frame=coord.ICRS(), name='icrs')
+  >>> w = wcs.WCS(forward_transform=transform, output_frame=sky_frame)
+
+To convert a pixel (x, y) = (1, 2) to sky coordinates, call the WCS object as a function:
+
+  >>> sky = w(1, 2)
+  >>> print(sky)
+      (5.284139265842845, -72.49775640633503)
+
+The :meth:`~gwcs.wcs.WCS.invert` method evaluates the :meth:`~gwcs.wcs.WCS.backward_transform`
+if available, otherwise applies an iterative method to calculate the reverse coordinates.
+
+  >>> w.invert(*sky)
+      (1.000000000009388, 2.0000000000112728)
+
+Some methods allow managing the transforms in a more detailed manner.
+
+Transforms between frames can be retrieved and evaluated separately.
+
+  >>> dist = w.get_transform('detector', 'focal')
+  >>> dist(1, 2)
+      (0.16, 0.8)
+
+Transforms in the pipeline can be replaced by new transforms.
+
+  >>> new_transform = Shift(1) & Shift(1.5) | distortion
+  >>> w.set_transform('detector', 'focal', new_transform)
+  >>> w(1, 2)
+      (5.257230028926096, -72.53171157138964)
+
+A transform can be inserted before or after a frame in the pipeline.
+
+  >>> scale = Scale(2) & Scale(1)
+  >>> w.insert_transform('icrs', scale, after=False)
+  >>> w(1, 2)
+      (10.514460057852192, -72.53171157138964)
+
+
+Using `gwcs`
+------------
+
+
+.. toctree::
+  :maxdepth: 2
+
+  gwcs/create_wcs
+  gwcs/selector_model.rst
+
+
+
+See also
+--------
+
+- The `modeling  package in astropy
+  <http://docs.astropy.org/en/stable/modeling/>`__
+
+- The `Coordinates package in astropy
+  <http://docs.astropy.org/en/stable/coordinates/>`__
+
+- The `Advanced Scientific Data Format (ASDF) standard
+  <http://asdf-standard.readthedocs.org/>`__
+  and its `Python implementation
+  <http://pyasdf.readthedocs.org/>`__
+
+
+Reference/API
+-------------
+
+.. automodapi:: gwcs.wcs
+.. automodapi:: gwcs.coordinate_frames
+.. automodapi:: gwcs.selector
---
+++ packages/gwcs/gwcs-0.5.tar.gz/gwcs-0.5/gwcs/tests/test_wcs.py
@@ -0,0 +1,201 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_allclose
+from astropy.modeling import models
+from astropy import coordinates as coord
+from astropy.io import fits
+from astropy import units as u
+from astropy.tests.helper import pytest
+from astropy.utils.data import get_pkg_data_filename
+from astropy import wcs as astwcs
+
+from .. import wcs
+from .. import coordinate_frames as cf
+from ..utils import ModelDimensionalityError, CoordinateFrameError
+
+
+m1 = models.Shift(12.4) & models.Shift(-2)
+m2 = models.Scale(2) & models.Scale(-2)
+icrs = cf.CelestialFrame(reference_frame=coord.ICRS())
+det = cf.Frame2D(name='detector', axes_order=(0, 1))
+focal = cf.Frame2D(name='focal', axes_order=(0, 1), unit=(u.m, u.m))
+pipe = [(det, m1),
+        (focal, m2),
+        (icrs, None)
+        ]
+
+
+def test_create_wcs():
+    """
+    Tests initializing a WCS object with frames of type str or CoorindtaeFrame.
+    """
+    icrs = cf.CelestialFrame(name='icrs', reference_frame=coord.ICRS())
+    det = cf.Frame2D(name='detector', axes_order=(0,1))
+    gw1 = wcs.WCS(output_frame='icrs', input_frame='detector', forward_transform=m1)
+    gw2 = wcs.WCS(output_frame='icrs', forward_transform=m1)
+    gw3 = wcs.WCS(output_frame=icrs, input_frame=det, forward_transform=m1)
+    assert(gw1.input_frame == gw2.input_frame == gw3.input_frame == 'detector')
+    assert(gw1.output_frame == gw2.output_frame == gw3.output_frame == 'icrs')
+    assert(gw1.forward_transform.__class__ == gw2.forward_transform.__class__ ==
+           gw3.forward_transform.__class__ == m1.__class__)
+    assert np.in1d(gw1._available_frames, gw2._available_frames).all()
+
+
+def test_init_no_transform():
+    """
+    Tests initializing a WCS object without a forward_transform.
+    """
+    gw = wcs.WCS(output_frame='icrs')
+    assert gw._pipeline == [('detector', None), ('icrs', None)]
+    assert np.in1d(gw.available_frames, ['detector', 'icrs']).all()
+    icrs = cf.CelestialFrame(reference_frame=coord.ICRS())
+    det = cf.Frame2D(name='detector', axes_order=(0, 1))
+    gw = wcs.WCS(output_frame=icrs, input_frame=det)
+    assert gw._pipeline == [('detector', None), ('CelestialFrame', None)]
+    assert np.in1d(gw.available_frames, ['detector', 'CelestialFrame']).all()
+
+
+def test_pipeline_init():
+    """ Tests initializing a WCS object with a pipeline list."""
+
+    gw = wcs.WCS(input_frame=det, output_frame=icrs, forward_transform=pipe)
+    assert np.in1d(gw.available_frames, ['detector', 'focal', 'CelestialFrame']).all()
+    assert_allclose(gw(1, 2), [26.8, 0] )
+
+
+def test_insert_transform():
+    """ Tests inserting a transform."""
+    m1 = models.Shift(12.4)
+    m2 = models.Scale(3.1)
+    gw = wcs.WCS(output_frame='icrs', forward_transform=m1)
+    assert(gw.forward_transform(1.2) == m1(1.2))
+    gw.insert_transform(frame='icrs', transform=m2)
+    assert(gw.forward_transform(1.2) == (m1 | m2)(1.2))
+
+
+def test_wrong_ndim():
+    """
+    Tests that exception is raised if n_inputs/n_outputs does not
+    match number of input/output axes.
+    """
+    det = cf.Frame2D(name='detector')
+    icrs = cf.CelestialFrame(reference_frame=coord.ICRS(), name='icrs')
+    m = models.Shift(1)
+    with pytest.raises(ModelDimensionalityError):
+        w = wcs.WCS(output_frame='icrs', forward_transform=m, input_frame=det)
+    with pytest.raises(ModelDimensionalityError):
+        w = wcs.WCS(output_frame=icrs, forward_transform=m)
+
+
+def test_set_transform():
+    """ Tests setting a transform between two frames in the pipeline."""
+    w = wcs.WCS(input_frame=det, output_frame=icrs, forward_transform=pipe)
+    w.set_transform('detector', 'focal', models.Identity(2))
+    assert_allclose(w(1, 1), (2, -2))
+    with pytest.raises(CoordinateFrameError):
+        w.set_transform('detector1', 'focal', models.Identity(2))
+    with pytest.raises(CoordinateFrameError):
+        w.set_transform('detector', 'focal1', models.Identity(2))
+
+
+def test_get_transform():
+    w = wcs.WCS(pipe)
+    tr_forward = w.get_transform('detector', 'focal')
+    tr_back = w.get_transform('CelestialFrame', 'detector')
+    x, y = 1, 2
+    fx, fy = tr_forward(1, 2)
+    assert_allclose(w.pipeline[0][1](x, y), (fx, fy))
+    assert_allclose((x, y), tr_back(*w(x, y)))
+    assert( w.get_transform('detector', 'detector') is None)
+
+
+class TestImaging(object):
+
+    def setup_class(self):
+        hdr = fits.Header.fromtextfile(get_pkg_data_filename("data/acs.hdr"), endcard=False)
+        self.fitsw = astwcs.WCS(hdr)
+        a_coeff = hdr['A_*']
+        a_order = a_coeff.pop('A_ORDER')
+        b_coeff = hdr['B_*']
+        b_order = b_coeff.pop('B_ORDER')
+
+        crpix = [hdr['CRPIX1'], hdr['CRPIX2']]
+        distortion = models.SIP(
+            crpix, a_order, b_order, a_coeff, b_coeff, name='sip_distorion') + models.Identity(2)
+
+        cdmat = np.array([[hdr['CD1_1'], hdr['CD1_2']], [hdr['CD2_1'], hdr['CD2_2']]])
+        aff = models.AffineTransformation2D(matrix=cdmat, name='rotation')
+
+        offx = models.Shift(-hdr['CRPIX1'], name='x_translation')
+        offy = models.Shift(-hdr['CRPIX2'], name='y_translation')
+
+        wcslin = (offx & offy) | aff
+
+        phi = hdr['CRVAL1']
+        lon = hdr['CRVAL2']
+        theta = 180
+        n2c = models.RotateNative2Celestial(phi, lon, theta, name='sky_rotation')
+
+        tan = models.Pix2Sky_TAN(name='tangent_projection')
+        sky_cs = cf.CelestialFrame(reference_frame=coord.ICRS())
+        wcs_forward = wcslin | tan | n2c
+        pipeline = [('detector', distortion),
+                    ('focal', wcs_forward),
+                    (sky_cs, None)
+                    ]
+
+        self.wcs = wcs.WCS(input_frame='detector',
+                           output_frame=sky_cs,
+                           forward_transform=pipeline)
+        nx, ny = (5, 2)
+        x = np.linspace(0, 1, nx)
+        y = np.linspace(0, 1, ny)
+        self.xv, self.yv = np.meshgrid(x, y)
+
+    def test_distortion(self):
+        sipx, sipy = self.fitsw.sip_pix2foc(self.xv, self.yv, 1)
+        sipx = np.array(sipx) + 2048
+        sipy = np.array(sipy) + 1024
+        sip_coord = self.wcs.get_transform('detector', 'focal')(self.xv, self.yv)
+        assert_allclose(sipx, sip_coord[0])
+        assert_allclose(sipy, sip_coord[1])
+
+    def test_wcslinear(self):
+        ra, dec = self.fitsw.wcs_pix2world(self.xv, self.yv, 1)
+        sky = self.wcs.get_transform('focal', 'CelestialFrame')(self.xv, self.yv)
+        assert_allclose(ra, sky[0])
+        assert_allclose(dec, sky[1])
+
+    def test_forward(self):
+        sky_coord = self.wcs(self.xv, self.yv)
+        ra, dec = self.fitsw.all_pix2world(self.xv, self.yv, 1)
+        assert_allclose(sky_coord[0], ra)
+        assert_allclose(sky_coord[1], dec)
+
+    def test_backward(self):
+        transform = self.wcs.get_transform(from_frame='focal', to_frame=self.wcs.output_frame)
+        sky_coord = self.wcs.transform('focal', self.wcs.output_frame, self.xv, self.yv)
+        px_coord = transform.inverse(*sky_coord)
+        assert_allclose(px_coord[0], self.xv, atol=10**-6)
+        assert_allclose(px_coord[1], self.yv, atol=10**-6)
+
+    def test_footprint(self):
+        footprint = self.wcs.footprint((4096, 2048))
+        fits_footprint = self.fitsw.calc_footprint(axes=(4096, 2048))
+        assert_allclose(footprint, fits_footprint)
+
+    def test_inverse(self):
+        sky_coord = self.wcs(1, 2)
+        with pytest.raises(NotImplementedError) as exc:
+            detector_coord = self.wcs.invert(sky_coord[0], sky_coord[1])
+
+    def test_units(self):
+        assert(self.wcs.unit == (u.degree, u.degree))
+
+    def test_get_transform(self):
+        with pytest.raises(wcs.CoordinateFrameError):
+            assert(self.wcs.get_transform('x_translation', 'sky_rotation').submodel_names ==
+                   self.wcs.forward_transform[1:].submodel_names)
+
