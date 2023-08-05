03f3363f8ebdc67b0e2465da5f3d1fdc03070502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:18 +0000

    Add package frigg-worker/3.2.0/frigg_worker-3.2.0-py3-none-any.whl

---
+++ packages/frigg-worker/frigg_worker-3.2.0-py3-none-any.whl/frigg_worker/deployments.py
@@ -0,0 +1,96 @@
+import logging
+import os
+
+import yaml
+
+from frigg_worker.errors import GitCloneError
+
+from .jobs import Job, Result
+
+logger = logging.getLogger(__name__)
+
+
+class Deployment(Job):
+
+    preset = None
+
+    MAIN_TASKS_KEY = 'deploy_tasks'
+
+    def run_deploy(self):
+        task = None
+        self.delete_working_dir()
+
+        try:
+            self.clone_repo()
+        except GitCloneError as error:
+            return self.handle_clone_error(error)
+
+        self.load_preset()
+
+        try:
+            self.finished = False
+            self.create_pending_tasks()
+            self.start_services()
+            self.report_run()
+
+            for task in self.settings.tasks['setup']:
+                self.run_setup_task(task)
+                self.report_run()
+
+            if self.preset:
+                for task in self.preset['tasks']:
+                    self.run_task(task)
+                    self.report_run()
+
+            for task in self.settings.preview['tasks']:
+                self.run_task(task)
+                self.report_run()
+
+            if self.preset:
+                self.run_task(self.preset['daemon_task'])
+                self.report_run()
+
+        except Exception as e:
+            self.error(task or '', e)
+            logger.exception(e)
+            logger.info('Build nr. {build.id} failed\n{0}'.format(str(e), build=self))
+        finally:
+            self.finished = True
+            self.report_run()
+
+            logger.info('Run of deploy {build.id} finished.'.format(build=self))
+
+    def create_pending_tasks(self):
+        """
+        Creates pending task results in a dict on self.result with task string as key. It will also
+        create a list on self.tasks that is used to make sure the serialization of the results
+        creates a correctly ordered list.
+        """
+        for task in self.settings.tasks['setup']:
+            self.setup_tasks.append(task)
+            self.setup_results[task] = Result(task)
+
+        if self.preset:
+            for task in self.preset['tasks']:
+                self.tasks.append(task)
+                self.results[task] = Result(task)
+
+        for task in self.settings.preview['tasks']:
+            self.tasks.append(task)
+            self.results[task] = Result(task)
+
+        if self.preset:
+            self.tasks.append(self.preset['daemon_task'])
+            self.results[self.preset['daemon_task']] = Result(self.preset['daemon_task'])
+
+    def load_preset(self):
+        """
+        Loads preset if it is specified in the .frigg.yml
+        """
+        if 'preset' in self.settings.preview:
+            with open(os.path.join(os.path.dirname(__file__), 'presets.yaml')) as f:
+                presets = yaml.load(f.read())
+
+            if self.settings.preview['preset'] in presets:
+                self.preset = presets[self.settings.preview['preset']]
+            return self.preset
---
+++ packages/frigg-worker/frigg_worker-3.2.0-py3-none-any.whl/frigg_worker/fetcher.py
@@ -0,0 +1,148 @@
+# -*- coding: utf8 -*-
+import json
+import logging
+import random
+import socket
+import sys
+import time
+
+import docker
+import frigg_coverage
+import frigg_settings
+import frigg_test_discovery
+import pip
+import requests
+from docker.manager import Docker
+
+import frigg_worker
+
+from .builds import Build
+from .deployments import Deployment
+from .errors import ApiError
+
+logger = logging.getLogger(__name__)
+
+
+def fetcher(options, callback):
+    notify_of_upstart(options)
+    while options['dispatcher_url']:
+        try:
+            task = fetch_task(options['dispatcher_url'], options['dispatcher_token'])
+            if task:
+                callback(task, options)
+        except ApiError as error:
+            if error.code == 'OUTDATED':
+                return upgrade()
+
+        time.sleep(5.0 + random.randint(1, 100) / 100)
+
+
+def fetch_builds(**options):
+    return fetcher(options, start_build)
+
+
+def fetch_deployments(**options):
+    return fetcher(options, start_deployment)
+
+
+def valid_image(job, task):
+    if not task['image']:
+        job.error('', 'No image specified for docker')
+        return False
+
+    if not task['image'].startswith('frigg/'):
+        job.error('', 'Only frigg/ images are allowed to use')
+        return False
+
+    return True
+
+
+def start_build(task, options):
+    docker_options = {
+        'image': task['image'],
+        'combine_outputs': True,
+        'privilege': True,
+        'env_variables': {'CI': 'frigg', 'GH_TOKEN': task['gh_token']},
+        'name_prefix': 'build'
+    }
+
+    with Docker(**docker_options) as docker:
+        try:
+            build = Build(task['id'], task, docker=docker, worker_options=options)
+            if valid_image(build, task):
+                logger.info('Starting {0}'.format(task))
+                build.run_tests()
+
+            return build
+        except Exception as e:
+            logger.exception(e)
+
+
+def start_deployment(task, options):
+    docker_options = {
+        'image': task['image'],
+        'timeout': task['ttl'],
+        'combine_outputs': True,
+        'privilege': True,
+        'ports_mapping': ['{port}:8000'.format(**task)],
+        'env_variables': {'CI': 'frigg', 'GH_TOKEN': task['gh_token']},
+        'name_prefix': 'preview'
+    }
+    docker = Docker(**docker_options)
+    docker.start()
+    try:
+        deployment = Deployment(task['id'], task, docker=docker, worker_options=options)
+
+        if valid_image(deployment, task):
+            deployment.run_deploy()
+
+        return deployment
+    except Exception as e:
+        docker.stop()
+        raise e
+
+
+def fetch_task(dispatcher_url, dispatcher_token):
+    logger.debug('Fetching new job from {0}'.format(dispatcher_url))
+    try:
+        response = requests.get(
+            dispatcher_url,
+            headers={
+                'x-frigg-worker-token': dispatcher_token,
+                'x-frigg-worker-host': socket.getfqdn(),
+                'x-frigg-worker-version': getattr(frigg_worker, '__version__', ''),
+                'x-frigg-settings-version': getattr(frigg_settings, '__version__', ''),
+                'x-frigg-coverage-version': getattr(frigg_coverage, '__version__', ''),
+                'x-frigg-test-discovery-version': getattr(frigg_test_discovery, '__version__', ''),
+                'x-docker-wrapper-version': getattr(docker, '__version__', ''),
+            }
+        )
+
+        if response.status_code == 200:
+            return response.json()['job']
+        else:
+            raise ApiError(response.json()['error'])
+    except requests.exceptions.ConnectionError as e:
+        logger.exception(e)
+
+    time.sleep(20)
+
+
+def notify_of_upstart(options):
+    if options['slack_url']:
+        host = socket.getfqdn()
+        requests.post(options['slack_url'], json.dumps({
+            'icon_emoji': options['slack_icon'],
+            'channel': options['slack_channel'],
+            'text': 'I just started on host {host}, looking for work now...'.format(host=host),
+            'username': 'frigg-worker',
+        }))
+
+
+def upgrade():
+    logger.info('Upgrading worker')
+    pip.main(['install', '-U', 'frigg-worker'])
+    pip.main(['install', '-U', 'frigg-settings'])
+    pip.main(['install', '-U', 'frigg-test-discovery'])
+    pip.main(['install', '-U', 'docker-wrapper'])
+    sys.exit(1)
