bfd719739f5ff239ac23e7199f0a5a6584bc2a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:58 +0000

    Add package python-gcm/0.4/python-gcm-0.4.tar.gz

---
+++ packages/python-gcm/python-gcm-0.4.tar.gz/python-gcm-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='python-gcm',
+    version='0.4',
+    packages=['gcm'],
+    license='The MIT License (MIT)',
+    author='Nam Ngo',
+    author_email='nam@kogan.com.au',
+    url='http://blog.namis.me/python-gcm/',
+    description='Python client for Google Cloud Messaging for Android (GCM)',
+    long_description=open('README.rst').read(),
+    keywords='android gcm push notification google cloud messaging',
+    install_requires=['requests'],
+    tests_require=['mock'],
+    test_suite='gcm.test',
+)
