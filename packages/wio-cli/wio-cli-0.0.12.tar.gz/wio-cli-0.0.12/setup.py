a65f49ba6c8cc07dfcbb99ffc5f12c04e0655126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:33 +0000

    Add package wio-cli/0.0.12/wio-cli-0.0.12.tar.gz

---
+++ packages/wio-cli/wio-cli-0.0.12.tar.gz/wio-cli-0.0.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wio-cli',
+    version='0.0.12',
+    description='CLI for Wio Link',
+    url='https://github.com/awong1900/wio_cli',
+    author='Ten Wong',
+    author_email='wangtengoo7@gmail.com',
+    license='MIT',
+    packages=['wio'],
+    # data_files=[('config', ['wio/config.json'])],
+    # include_package_data=True,
+    install_requires=[
+        'click',
+        'requests',
+        'pyserial',
+    ],
+    entry_points='''
+        [console_scripts]
+        wio=wio.wio:cli
+    ''',
+)
