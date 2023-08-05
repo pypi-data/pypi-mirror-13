4fcedac9c0ccd452eb72ba042d7382bf29865771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package helloIPV/1.0/helloIPV-1.0.tar.gz

---
+++ packages/helloIPV/helloIPV-1.0.tar.gz/helloIPV-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+setup(name='helloIPV',
+      version='1.0',
+      author='panthotanvir',
+      author_email='gward@python.net',
+      py_modules=['ipv']
+
+    )
