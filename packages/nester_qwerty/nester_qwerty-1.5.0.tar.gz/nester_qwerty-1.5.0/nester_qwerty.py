eb20fad53c825e20956a8a88396b767f31dbe473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:49 +0000

    Add package nester_qwerty/1.5.0/nester_qwerty-1.5.0.tar.gz

---
+++ packages/nester_qwerty/nester_qwerty-1.5.0.tar.gz/nester_qwerty-1.5.0/nester_qwerty.py
@@ -0,0 +1,12 @@
+"""Comment #1"""
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+    """Comment #2"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='', file=fh)
+            print(each_item, file=fh)
+
