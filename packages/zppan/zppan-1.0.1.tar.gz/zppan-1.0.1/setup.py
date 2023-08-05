190403f4dbead2448c90cd6725a3aafe234f5189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package zppan/1.0.1/zppan-1.0.1.tar.gz

---
+++ packages/zppan/zppan-1.0.1.tar.gz/zppan-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+svar = {'name'          :   'zppan',
+        'version'       :   '1.0.1',
+        'py_modules'    :   ['zppan.base',],
+        'author'        :   'Jeff',
+        'author_email'  :   'jeff@zppan.com',
+        'url'           :   'www.zppan.com',
+        'description'   :   'A simple demo'
+        }
+
+setup(**svar)
+
---
+++ packages/zppan/zppan-1.0.1.tar.gz/zppan-1.0.1/zppan/base.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def version():
+    print("Version zppan V0.01")
+
+def name():
+    return "Jeff"
