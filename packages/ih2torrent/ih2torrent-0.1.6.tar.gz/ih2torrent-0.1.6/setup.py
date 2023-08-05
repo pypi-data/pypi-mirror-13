1a56405ec97e75ed638c33c090c6eaffbf69570c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package ih2torrent/0.1.6/ih2torrent-0.1.6.tar.gz

---
+++ packages/ih2torrent/ih2torrent-0.1.6.tar.gz/ih2torrent-0.1.6/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+from ih2torrent import __version__ as version
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+import pypandoc
+
+# convert markdown to reStructured Text
+rst = pypandoc.convert('README.md', 'rst', format='markdown')
+
+# writes converted file
+with open('README.rst','w') as outfile:
+    outfile.write(rst)
+
+# read requirements from requirements.txt
+requirements = parse_requirements('requirements.txt', session=PipSession())
+requirements = [str(r.req) for r in requirements]
+
+setup(
+    name = 'ih2torrent',
+    py_modules = ["ih2torrent"],
+    install_requires = requirements,
+    version = version,
+    description = 'Convert a torrent infohash or magnet URI to a .torrent file using DHT and metadata protocol. Asyncio based.',
+    author = 'Mostafa Razavi',
+    author_email = 'mostafa@sepent.com',
+    url = 'https://github.com/elektito/ih2torrent',
+    download_url = 'https://github.com/elektito/ih2torrent/tarball/' + version,
+    keywords = ['bittorrent', 'torrent', 'infohash', 'magnet', 'dht', 'metadata', 'metainfo', 'asyncio'],
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "ih2torrent=ih2torrent:main",
+        ],
+    },
+)
