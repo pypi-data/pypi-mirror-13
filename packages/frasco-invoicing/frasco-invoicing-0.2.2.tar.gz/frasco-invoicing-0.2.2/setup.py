905a7524aec86f164dac6a59748359da26d98ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package frasco-invoicing/0.2.2/frasco-invoicing-0.2.2.tar.gz

---
+++ packages/frasco-invoicing/frasco-invoicing-0.2.2.tar.gz/frasco-invoicing-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='frasco-invoicing',
+    version='0.2.2',
+    url='http://github.com/frascoweb/frasco-invoicing',
+    license='MIT',
+    author='Maxime Bouroumeau-Fuseau',
+    author_email='maxime.bouroumeau@gmail.com',
+    description="Invoicing for Frasco",
+    packages=find_packages(),
+    package_data={
+        'frasco_invoicing': [
+            'emails/*.html']
+    },
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'frasco',
+        'frasco-models>=0.4'
+    ]
+)
