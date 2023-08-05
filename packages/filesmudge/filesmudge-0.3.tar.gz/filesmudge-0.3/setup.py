192ac251569d8ba4b608aeb8822269b82fbc4e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:19 +0000

    Add package filesmudge/0.3/filesmudge-0.3.tar.gz

---
+++ packages/filesmudge/filesmudge-0.3.tar.gz/filesmudge-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='filesmudge',
+    description='A silly file \'smudger\'',
+    author='Leon Jacobs',
+    author_email='leonja511@gmail.com',
+    url='https://github.com/leonjza/filesmudge',
+    download_url = 'https://github.com/leonjza/filesmudge/tarball/0.3',
+    keywords=['file', 'magic', 'bytes', 'edit', 'smudge'],
+    version='0.3',
+    py_modules=['smudge'],
+    install_requires=[
+        'Click', 'python-magic'
+    ],
+    entry_points='''
+        [console_scripts]
+        filesmudge=filesmudge.smudge:cli
+    '''
+)
