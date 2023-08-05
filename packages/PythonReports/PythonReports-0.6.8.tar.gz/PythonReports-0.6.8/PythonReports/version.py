98d733ab832142ebc7af22ec88f98dbf2d186f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package PythonReports/0.6.8/PythonReports-0.6.8.tar.gz

---
+++ packages/PythonReports/PythonReports-0.6.8.tar.gz/PythonReports-0.6.8/PythonReports/version.py
@@ -0,0 +1,9 @@
+"""PythonReports package version declaration"""
+
+from datetime import date
+
+__version__ = "0.6.8"
+__date__ = date(2015, 12, 21)
+__all__ = ["__version__", "__date__"]
+
+# vim: set et sts=4 sw=4 :
