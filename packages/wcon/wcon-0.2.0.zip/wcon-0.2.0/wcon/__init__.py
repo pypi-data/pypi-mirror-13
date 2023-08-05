605ca5081d1c6bd13466cce820ca720cb9dfa12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:29 +0000

    Add package wcon/0.2.0/wcon-0.2.0.zip

---
+++ packages/wcon/wcon-0.2.0.zip/wcon-0.2.0/wcon/__init__.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+wcon: Worm tracker Commons Object Notation
+https://github.com/openworm/tracker-commons
+
+"""
+
+
+from .wcon_parser import WCONWorms
+from .measurement_unit import MeasurementUnit
+
+__all__ = ['WCONWorms', 'MeasurementUnit']
