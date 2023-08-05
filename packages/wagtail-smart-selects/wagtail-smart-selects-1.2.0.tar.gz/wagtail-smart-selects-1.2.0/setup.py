3931addf0b326607ff3407ab09d01f4afdc461c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:28 +0000

    Add package wagtail-smart-selects/1.2.0/wagtail-smart-selects-1.2.0.tar.gz

---
+++ packages/wagtail-smart-selects/wagtail-smart-selects-1.2.0.tar.gz/wagtail-smart-selects-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="wagtail-smart-selects",
+      version="1.2.0",
+      description="Django application to handle chained model fields.",
+      author="InfoPortugal S.A.",
+      packages=find_packages(),
+      include_package_data=True,
+      )
