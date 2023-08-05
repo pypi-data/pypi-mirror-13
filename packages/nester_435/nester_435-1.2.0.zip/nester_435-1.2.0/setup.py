81b217a102cb6a33adf12c7c9a03e504ece2f48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:21 +0000

    Add package nester_435/1.2.0/nester_435-1.2.0.zip

---
+++ packages/nester_435/nester_435-1.2.0.zip/nester_435-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_435",
+    version = "1.2.0",
+    py_modules = ["nester_435"],
+    author = "hfpython",
+    author_email = "hfpython@headfirstlabs.com",
+    url = "http://www.headfirstlabs.com",
+    description = "A simple printer os nested lists / Um simples impressor de listas aninhadas",
+    )
