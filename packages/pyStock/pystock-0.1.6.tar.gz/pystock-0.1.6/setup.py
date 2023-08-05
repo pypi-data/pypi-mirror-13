75b434a44592fdaf2aa3a8a9d37381a8836804da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package pyStock/0.1.6/pystock-0.1.6.tar.gz

---
+++ packages/pyStock/pystock-0.1.6.tar.gz/pystock-0.1.6/setup.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'sqlalchemy',
+    'alembic',
+    'py-moneyed',
+]
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='pystock',
+    version='0.1.6',
+    description='A stock market model for persistence using SQLAlchemy',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Office/Business :: Financial"
+    ],
+    author='Leonardo Lazzaro',
+    author_email='lazzaroleonardo@gmail.com',
+    url='www.lazzaroleonardo.com.ar',
+    keywords='stock market model persistence',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=[
+        'pytest',
+    ],
+    test_suite="tests",
+
+)
