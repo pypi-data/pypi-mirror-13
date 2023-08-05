23e360332ddc043e83ced973d39edede0a9a7760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package ch-frontend/0.1.2/ch-frontend-0.1.2.tar.gz

---
+++ packages/ch-frontend/ch-frontend-0.1.2.tar.gz/ch-frontend-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import os, json
+from distutils.core import setup
+
+package = json.load(open("./package.json"))
+
+setup(
+        name = "ch-frontend",
+        packages = ["ch-frontend"],
+        package_dir = {"ch-frontend": "."},
+        package_data = {
+            "ch-frontend": [
+                "dist/*.js",
+                "data/*.json",
+                "package.json"
+            ]
+        },
+        version = package["version"],
+        description = "PyPI package for frontend files",
+        author = "Diwank Singh",
+        author_email = "diwank.singh@gmail.com",
+        url = "https://github.com/creatorrr/ch-frontend")
+
