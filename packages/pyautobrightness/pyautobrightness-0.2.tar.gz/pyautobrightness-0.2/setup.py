db33655adda62bd47c9c7657a6d97b5ff76d8021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package pyautobrightness/0.2/pyautobrightness-0.2.tar.gz

---
+++ packages/pyautobrightness/pyautobrightness-0.2.tar.gz/pyautobrightness-0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from pyautobrightness import __version__
+
+setup(
+    name='pyautobrightness',
+    version=__version__,
+    author='Everton Correia',
+    author_email='evertonjcorreia@gmail.com',
+    packages=['pyautobrightness'],
+    keywords=['Brightness', 'Auto', 'Webcam'],
+    url='https://github.com/evertonstz/py-auto-brightness',
+    platforms='Unix',
+    download_url='https://github.com/evertonstz/py-auto-brightness/tarball/0.2',
+    license='GNU',
+    description='A very simple software to change the screen brightness using a webcam as light sensor.',
+    entry_points={'console_scripts': ['pyautobrightness = pyautobrightness.pyautobrightnessmain:main']},
+)
