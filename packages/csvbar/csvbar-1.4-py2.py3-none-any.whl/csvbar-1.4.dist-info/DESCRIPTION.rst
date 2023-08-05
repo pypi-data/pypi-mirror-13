25abd32954b92d92846ed549865290a383328fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:32 +0000

    Add package csvbar/1.4/csvbar-1.4-py2.py3-none-any.whl

---
+++ packages/csvbar/csvbar-1.4-py2.py3-none-any.whl/csvbar-1.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,61 @@
+CSV Bar
+=======
+
+Draw bar charts from CSV files in the terminal.
+
+Requires either version 2 or 3 of [Python] (https://www.python.org/), including `pip`.
+
+
+Installing
+----------
+
+Install with Pip: `pip install csvbar`.
+
+
+Usage
+-----
+
+Say you have a CSV file such as:
+
+```
+Donor,Value
+Trust,93167.74
+Limited Liability Partnership,100522.93
+Friendly Society,111428.84
+Registered Political Party,382227.01
+Unincorporated Association,2846016.31
+Company,3696180.22
+Individual,11021726.04
+Trade Union,44483505.54
+```
+
+You can then:
+
+```bash
+$ csvbar data.csv
+
+Trust                          ▌ 93,167.74 (0.15%)
+Limited Liability Partnership  ▌ 100,522.93 (0.16%)
+Friendly Society               ▌ 111,428.84 (0.18%)
+Registered Political Party     ▌ 382,227.01 (0.61%)
+Unincorporated Association     ███ 2,846,016.31 (4.54%)
+Company                        ████ 3,696,180.22 (5.89%)
+Individual                     ████████████ 11,021,726.04 (17.57%)
+Trade Union                    ██████████████████████████████████████████████████ 44,483,505.54 (70.91%)
+
+Total: 62,734,774.63
+```
+
+It also accepts data on STDIN, useful if you're using something like [CSV Kit] (https://github.com/onyxfish/csvkit) or [Q] (https://github.com/harelba/q).
+
+
+Inspiration
+-----------
+
+There are similar tools:
+
+ * [data_hacks] (https://github.com/bitly/data_hacks), by [Bitly] (https://bitly.com/)
+ * [termgraph] (https://github.com/mkaz/termgraph), by [Marcus Kazmierczak] (https://twitter.com/mkaz)
+ * [spark] (https://github.com/holman/spark), by [Zach Holman] (https://twitter.com/holman)
+
+
---
+++ packages/csvbar/csvbar-1.4-py2.py3-none-any.whl/csvbar.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+import argparse
+import locale
+import csv
+import sys
+import io
+import os
+
+def main():
+    locale.setlocale(locale.LC_ALL, '')
+    try:
+        args = arguments()
+        labels, data = read(args['FILE'])
+        display(labels, data, args['width'])
+    except BaseException as e: sys.exit(e)
+
+def arguments():
+    parser = argparse.ArgumentParser(description='draw bar graphs from CSV files in the terminal')
+    parser.add_argument('FILE', nargs='?', default='-', help='the CSV file to operate on -- if omitted, will accept input on STDIN')
+    parser.add_argument('-w', '--width', type=int, default=50, help='width of graph in characters (default is 50)')
+    args = vars(parser.parse_args())
+    if args['FILE'] == '-' and sys.stdin.isatty():
+        parser.print_help(sys.stderr)
+        parser.exit(1)
+    return args
+
+def read(filename):
+    labels = []
+    data = []
+    if not os.path.isfile(filename) and filename != '-': raise Exception(filename + ': no such file')
+    file = sys.stdin if filename == '-' else io.open(filename, 'r')
+    reader = csv.reader(file)
+    headers = next(reader) # skip these
+    for line in reader:
+        labels.append(line[0])
+        try: data.append(float(line[1]))
+        except ValueError: raise Exception(line[1] + ' is not a number')
+    file.close()
+    return labels, data
+
+def display(labels, data, width):
+    full_tick = '█'
+    half_tick = '▌'
+    total = sum(data)
+    step = max(data) / width
+    spacing = max(len(label) for label in labels)
+    print('')
+    for i in range(len(data)):
+        label = labels[i]
+        count = data[i]
+        padding = ' ' * (spacing - len(label) + 2)
+        bar = half_tick if count < step else int(count / step) * full_tick
+        number = locale.format('%.2f', count, grouping=True)
+        percentage = locale.format('%.2f', count / total * 100, grouping=True)
+        print(label + padding + bar + ' ' + number + ' (' + percentage + '%)')
+    print('')
+    print('Total: ' + locale.format('%.2f', total, grouping=True))
+
+if __name__ == '__main__':
+    main()
