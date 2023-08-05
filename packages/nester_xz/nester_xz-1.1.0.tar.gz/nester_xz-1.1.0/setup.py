c56a2d9e769bf15fe8918f9e11feea3e7ed942ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:08 +0000

    Add package nester_xz/1.1.0/nester_xz-1.1.0.tar.gz

---
+++ packages/nester_xz/nester_xz-1.1.0.tar.gz/nester_xz-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name             = 'nester_xz',
+    version          = '1.1.0',
+    py_modules       = ['nester'],
+    author           = 'chendg',
+    author_email     = '1334415215@qq.com',
+    url              = 'http://www.headfirstlabs.com',
+    description      = 'A simple',
+    )
