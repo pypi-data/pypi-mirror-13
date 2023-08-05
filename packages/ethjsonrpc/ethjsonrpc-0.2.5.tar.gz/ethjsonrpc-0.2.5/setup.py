4509bb732db6e70a1c3179fa5226967364d47f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:23 +0000

    Add package ethjsonrpc/0.2.5/ethjsonrpc-0.2.5.tar.gz

---
+++ packages/ethjsonrpc/ethjsonrpc-0.2.5.tar.gz/ethjsonrpc-0.2.5/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='ethjsonrpc',
+    version='0.2.5',
+    description='Ethereum JSON-RPC client',
+    long_description=open('README.rst').read(),
+    author='ConsenSys',
+    author_email=['info@consensys.net'],
+    url='https://github.com/ConsenSys/ethjsonrpc',
+    packages=['ethjsonrpc'],
+    license='Unlicense',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    install_requires=[
+        'bitcoin==1.1.39',  # work around 1.1.40 bug (required by ethereum library)
+        'ethereum==1.0.8',
+        'requests==2.9.1',
+    ],
+)
