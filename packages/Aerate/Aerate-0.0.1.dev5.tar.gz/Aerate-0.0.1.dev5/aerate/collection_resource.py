4beb5b72a8cd50c25e5498df9cc0bb1ee18d2ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:14 +0000

    Add package Aerate/0.0.1.dev5/Aerate-0.0.1.dev5.tar.gz

---
+++ packages/Aerate/Aerate-0.0.1.dev5.tar.gz/Aerate-0.0.1.dev5/aerate/collection_resource.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    aerate.collection_resource
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Implements a CollectionResource for use in defining Falcon routes.
+    The CollectionResource must be subclassed in a user's application
+
+    :copyright: (c) 2016 by Kelly Caylor.
+    :license: BSD, see LICENSE for more details.
+"""
+
+
+class CollectionResource(object):
+
+    def __init__(self):
+        pass
+
+    def on_get(**kwargs):
+        pass
