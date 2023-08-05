510e7ed2f188730f3994332ff2c5d33d8367f560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package df2gspread/0.0.6/df2gspread-0.0.6.tar.gz

---
+++ packages/df2gspread/df2gspread-0.0.6.tar.gz/df2gspread-0.0.6/df2gspread/_version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Eduard Trott
+# @Date:   2015-09-15 08:57:35
+# @Email:  etrott@redhat.com
+# @Last modified by:   etrott
+# @Last Modified time: 2015-12-21 10:30:04
+
+
+
+
+version_info = ('0', '0', '6')
+__version__ = '.'.join(version_info[0:3])  # + '-' + version_info[3]
