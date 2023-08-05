ea3f6db198f7f88d5d7116bfc1473450e46cb58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:44 +0000

    Add package collective.dms.mailcontent/0.4/collective.dms.mailcontent-0.4.tar.gz

---
+++ packages/collective.dms.mailcontent/collective.dms.mailcontent-0.4.tar.gz/collective.dms.mailcontent-0.4/src/collective/dms/mailcontent/indexers.py
@@ -0,0 +1,9 @@
+from five import grok
+
+from plone.indexer import indexer
+
+from Products.PluginIndexes.common.UnIndex import _marker
+
+from collective.dms.mailcontent.dmsmail import IDmsIncomingMail
+
+
