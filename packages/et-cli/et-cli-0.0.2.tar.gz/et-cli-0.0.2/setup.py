fa03a0fc357c35c3acfa0a24ac8309bef09be413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package et-cli/0.0.2/et-cli-0.0.2.tar.gz

---
+++ packages/et-cli/et-cli-0.0.2.tar.gz/et-cli-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name = "et-cli",
+    version = "0.0.2",
+    packages=['etcli'],
+    description='ExactTarget CLI Tool',
+    long_description=readme,
+    url='https://github.com/tzmfreedom/et-cli',
+    author='makoto tajitsu',
+    author_email='makoto_tajitsu@hotmail.co.jp',
+    license='MIT',
+    scripts=['bin/et'],
+    install_requires=[
+        'FuelSDK',
+    ],
+)
