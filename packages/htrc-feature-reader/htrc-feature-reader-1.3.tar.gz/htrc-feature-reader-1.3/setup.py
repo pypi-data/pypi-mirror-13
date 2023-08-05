1342251c60cbd720e1abc12acbe9df7509db3490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package htrc-feature-reader/1.3/htrc-feature-reader-1.3.tar.gz

---
+++ packages/htrc-feature-reader/htrc-feature-reader-1.3.tar.gz/htrc-feature-reader-1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='htrc-feature-reader',
+      version='1.3',
+      description='Library for working with the HTRC Extracted Features dataset',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.2',
+          'Natural Language :: English',
+      ],
+      keywords='hathitrust text-mining text-analysis features',
+      url='https://github.com/organisciak/htrc-feature-reader',
+      author='Peter Organisciak',
+      author_email='organisciak@gmail.com',
+      license='NCSA',
+      packages=['htrc_features'],
+      install_requires=['six', 'pandas', 'numpy'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
