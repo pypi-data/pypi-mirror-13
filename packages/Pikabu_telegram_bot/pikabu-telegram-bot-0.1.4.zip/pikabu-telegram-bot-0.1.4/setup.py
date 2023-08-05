0cda97d3cbcf009f852a59af519c0565455c3360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:06 +0000

    Add package Pikabu_telegram_bot/0.1.4/pikabu-telegram-bot-0.1.4.zip

---
+++ packages/Pikabu_telegram_bot/pikabu-telegram-bot-0.1.4.zip/pikabu-telegram-bot-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def requirements():
+    """Build the requirements list for this project"""
+    requirements_list = []
+
+    with open('requirements.txt') as requirements:
+        for install in requirements:
+            requirements_list.append(install.strip())
+
+    return requirements_list
+
+
+setup(name='pikabu-telegram-bot',
+      version='0.1.4',
+      description='Pikabu bot for telegram. Powered by python-telegram-bot',
+      author='Alexey Kalyaganov',
+      author_email='admin@futurobot.ru',
+      license='LGPLv3',
+      include_package_data=True,
+      packages=find_packages(exclude=['tests*']),
+      install_requires=requirements(),
+      )
