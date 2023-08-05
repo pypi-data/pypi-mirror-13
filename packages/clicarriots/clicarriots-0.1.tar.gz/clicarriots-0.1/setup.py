abc273dee7dffe4a677f15a691d2cd7125ce2505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:16 +0000

    Add package clicarriots/0.1/clicarriots-0.1.tar.gz

---
+++ packages/clicarriots/clicarriots-0.1.tar.gz/clicarriots-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='clicarriots',
+      version='0.1',
+      description = 'The unofficial simple client for carriots platform (in progress) (python 2.x/3.x)',
+      author='Samuel de Ancos',
+      author_email='sdeancos@gmail.com',
+      url = 'https://github.com/sdeancos/clicarriots',
+      download_url = 'https://github.com/sdeancos/clicarriots/tarball/0.1',
+      packages=['clicarriots'],
+      )
