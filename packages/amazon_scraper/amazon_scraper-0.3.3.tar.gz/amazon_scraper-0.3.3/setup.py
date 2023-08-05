1c7d4836db867a5c1b7dd410457cfe0f08c5c293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:02 +0000

    Add package amazon_scraper/0.3.3/amazon_scraper-0.3.3.tar.gz

---
+++ packages/amazon_scraper/amazon_scraper-0.3.3.tar.gz/amazon_scraper-0.3.3/setup.py
@@ -0,0 +1,49 @@
+# fix building inside a virtualbox VM
+# http://bugs.python.org/issue8876#msg208792
+try:
+    import os
+    del os.link
+except:
+    pass
+
+from setuptools import setup
+
+# import the README
+with open('README.rst') as f:
+    long_description = f.read()
+
+__version__ = None
+with open('amazon_scraper/version.py') as f:
+    exec(f.read())
+
+setup(
+    name="amazon_scraper",
+    version=__version__,
+    description="Provides content not accessible through the standard Amazon API",
+    long_description=long_description,
+    license='BSD',
+    author="Adam Griffiths",
+    url='https://github.com/adamlwgriffiths/amazon_scraper',
+    test_suite='tests',
+    packages=['amazon_scraper'],
+    install_requires=[
+        'python-amazon-simple-product-api',
+        'xmltodict',
+        'python-dateutil',
+        'beautifulsoup4',
+        'requests',
+    ],
+    platforms=['any'],
+    classifiers=(
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+    ),
+)
