3bc514876f68e14080a2282e4b8c2cae445850f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:33 +0000

    Add package rot13-cipher/0.1.1/rot13-cipher-0.1.1.tar.gz

---
+++ packages/rot13-cipher/rot13-cipher-0.1.1.tar.gz/rot13-cipher-0.1.1/setup.py
@@ -0,0 +1,54 @@
+"""A setuptools-based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rot13-cipher',
+
+    version='0.1.1',
+
+    description=('Alphabetical substitution cipher',),
+    long_description=long_description,
+
+    url='https://github.com/ashleyblackmore/rot13-cipher',
+
+    author='Ashley Blackmore',
+    author_email='oss@sent.com',
+
+    license='MIT',
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='rot13 cipher crypto obfuscation',
+)
