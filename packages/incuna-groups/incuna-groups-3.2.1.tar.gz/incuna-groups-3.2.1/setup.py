21b3cb788e47cf597463a30c9f4005b62c6bffef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:47 +0000

    Add package incuna-groups/3.2.1/incuna-groups-3.2.1.tar.gz

---
+++ packages/incuna-groups/incuna-groups-3.2.1.tar.gz/incuna-groups-3.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    version='3.2.1',
+    name='incuna-groups',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django_crispy_forms>=1.4.0,<2',
+        'django-polymorphic>=0.7.2,<0.8',
+        'incuna-pagination>=0.1.1,<1',
+    ],
+    description='Generic group/forum framework.',
+    author='Incuna Ltd',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/incuna-groups',
+)
