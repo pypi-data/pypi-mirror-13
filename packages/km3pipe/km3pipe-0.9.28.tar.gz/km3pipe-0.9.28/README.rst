d0ac7a05d8197626a686ac06fdaa84066ddce424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:23 +0000

    Add package km3pipe/0.9.28/km3pipe-0.9.28.tar.gz

---
+++ packages/km3pipe/km3pipe-0.9.28.tar.gz/km3pipe-0.9.28/README.rst
@@ -0,0 +1,14 @@
+KM3Pipe
+=======
+
+KM3Pipe is a framework for KM3NeT related stuff including MC, data files, live access to detectors and databases, parsers for different file formats and an easy to use framework for batch processing.
+
+Read the docs at http://km3pipe.readthedocs.org
+
+KM3NeT related documentation at http://wiki.km3net.de/index.php/KM3Pipe
+
+.. image:: https://travis-ci.org/tamasgal/km3pipe.svg?branch=develop
+    :target: https://travis-ci.org/tamasgal/km3pipe
+
+.. image:: https://img.shields.io/badge/docs-latest-brightgreen.svg?style=flat
+    :target: http://km3pipe.readthedocs.org/en/latest/
---
+++ packages/km3pipe/km3pipe-0.9.28.tar.gz/km3pipe-0.9.28/km3pipe/db.py
@@ -0,0 +1,177 @@
+# coding=utf-8
+# Filename: db.py
+# pylint: disable=locally-disabled
+"""
+Database utilities.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from datetime import datetime
+import ssl
+import urllib
+from urllib2 import (Request, build_opener, HTTPCookieProcessor, HTTPHandler)
+import cookielib
+import json
+import sys
+
+import pandas as pd
+
+from km3pipe.tools import Timer
+from km3pipe.config import Config
+from km3pipe.logger import logging
+
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+__author__ = 'tamasgal'
+
+log = logging.getLogger(__name__)  # pylint: disable=C0103
+
+
+# Ignore invalid certificate error
+try:
+    ssl._create_default_https_context = ssl._create_unverified_context
+except AttributeError:
+    log.warn("Your SSL support is outdate. "
+             "Please update your Python installation!")
+
+LOGIN_URL = 'https://km3netdbweb.in2p3.fr/home.htm'
+BASE_URL = 'https://km3netdbweb.in2p3.fr'
+
+
+class DBManager(object):
+    """A wrapper for the KM3NeT Web DB"""
+    def __init__(self, username=None, password=None):
+        "Create database connection"
+        self.cookies = []
+        self._parameters = None
+        self._opener = None
+        if username is None:
+            config = Config()
+            username, password = config.db_credentials
+        self.login(username, password)
+
+    def datalog(self, parameter, run, maxrun=None, detid='D_ARCA001'):
+        "Retrieve datalogs for given parameter, run(s) and detector"
+        parameter = parameter.lower()
+        if maxrun is None:
+            maxrun = run
+        with Timer('Database lookup'):
+            return self._datalog(parameter, run, maxrun, detid)
+
+    def _datalog(self, parameter, run, maxrun, detid):
+        "Extract data from database"
+        values = {'parameter_name': parameter,
+                  'minrun': run,
+                  'maxrun': maxrun,
+                  'detid': detid,
+                  }
+        data = urllib.urlencode(values)
+        content = self._get_content('streamds/datalognumbers.txt?' + data)
+        if content.startswith('ERROR'):
+            log.error(content)
+            return None
+        try:
+            dataframe = pd.read_csv(StringIO(content), sep="\t")
+        except ValueError:
+            log.warning("Empty dataset")
+            return None
+        else:
+            def convert_data(timestamp):
+                return datetime.fromtimestamp(float(timestamp) / 1e3)
+            dataframe['DATETIME'] = dataframe['UNIXTIME'].apply(convert_data)
+            convert_unit = self.parameters.get_converter(parameter)
+            dataframe['VALUE'] = dataframe['DATA_VALUE'].apply(convert_unit)
+            dataframe.unit = self.parameters.unit(parameter)
+            return dataframe
+
+    @property
+    def parameters(self):
+        "Return the parameters container for quick access to their details"
+        if self._parameters is None:
+            self._load_parameters()
+        return self._parameters
+
+    def _load_parameters(self):
+        "Retrieve a list of available parameters from the database"
+        parameters = self._get_json('allparam/s')
+        if parameters['Result'] != 'OK':
+            raise ValueError('Error while retrieving the parameter list.')
+        data = {}
+        for parameter in parameters['Data']:
+            data[parameter['Name'].lower()] = parameter
+        self._parameters = ParametersContainer(data)
+
+    def _get_json(self, url):
+        "Get JSON-type content"
+        content = self._get_content('jsonds/' + url)
+        return json.loads(content)
+
+    def _get_content(self, url):
+        "Get HTML content"
+        f = self.opener.open(BASE_URL + '/' + url)
+        content = f.read()
+        return content
+
+    @property
+    def opener(self):
+        "A reusable connection manager"
+        if self._opener is None:
+            opener = build_opener()
+            for cookie in self.cookies:
+                cookie_str = cookie.name + '=' + cookie.value
+                opener.addheaders.append(('Cookie', cookie_str))
+            self._opener = opener
+        return self._opener
+
+    def login(self, username, password):
+        "Login to the databse and store cookies for upcoming requests."
+        cj = cookielib.CookieJar()
+        opener = build_opener(HTTPCookieProcessor(cj), HTTPHandler())
+        values = {'usr': username, 'pwd': password}
+        data = urllib.urlencode(values)
+        req = Request(LOGIN_URL, data)
+        f = opener.open(req)
+        html = f.read()
+        if 'Bad username or password' in html:
+            log.error("Bad username or password!")
+        self.cookies = cj
+
+
+class ParametersContainer(object):
+    """Provides easy access to parameters"""
+    def __init__(self, parameters):
+        self._parameters = parameters
+        self._converters = {}
+
+    @property
+    def names(self):
+        "A list of parameter names"
+        return self._parameters.keys()
+
+    def get_parameter(self, parameter):
+        "Return a dict of given parameter"
+        return self._parameters[parameter]
+
+    def get_converter(self, parameter):
+        """Generate unit conversion function for given parameter"""
+        if parameter not in self._converters:
+            param = self.get_parameter(parameter)
+            try:
+                scale = float(param['Scale'])
+            except KeyError:
+                scale = 1
+
+            def convert(value):
+                # easy_scale = float(param['EasyScale'])
+                # easy_scale_multiplier = float(param['EasyScaleMultiplier'])
+                return value * scale
+
+            return convert
+
+    def unit(self, parameter):
+        "Get the unit for given parameter"
+        return self._parameters[parameter.lower()]['Unit']
