6a5a6cd107ba7e379e61d31c14929ea380bfeab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:19 +0000

    Add package termdic/0.1.2/termdic-0.1.2.tar.gz

---
+++ packages/termdic/termdic-0.1.2.tar.gz/termdic-0.1.2/setup.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+version = '0.1.2'
+
+setup(
+    name = 'termdic',
+    version = version,
+    author = 'hzwer',
+    author_mail = '598460606@163.com',
+    url = 'https://github.com/hzwer/termdic',
+    description = 'Dictionary in your terminal',
+    keywords = 'dictionary, terminal',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'requests',
+        'termcolor',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'tdic = termdic.termdic:main'
+        ]
+    },
+)
