b33b5dbaf50f64d487fcbb41fb16abda3328f0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package warriors/0.2.2/warriors-0.2.2.zip

---
+++ packages/warriors/warriors-0.2.2.zip/warriors-0.2.2/README.md
@@ -0,0 +1,2 @@
+#Warriors-OTS-PythonSDK
+Warriors-python-sdk is based on the development of ALIYUN OTS Python-SDK, he solved many of the shortcomings of Aliyun OTS system. Use the package must be installed protobuf and urllib3.
---
+++ packages/warriors/warriors-0.2.2.zip/warriors-0.2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'warriors',
+    version = "0.2.2",
+    packages = find_packages(),
+    author = 'Simon_L.Y.B',
+    author_email = '1289498399@qq.com',
+    url = '',
+    license = 'MIT',
+    description = 'warriors-python-SDK'
+)
