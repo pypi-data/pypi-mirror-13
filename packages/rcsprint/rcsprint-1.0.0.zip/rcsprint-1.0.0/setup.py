4287139504bc9bffe9fef40a8dc350eab139f18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package rcsprint/1.0.0/rcsprint-1.0.0.zip

---
+++ packages/rcsprint/rcsprint-1.0.0.zip/rcsprint-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		='rcsprint',
+	version		='1.0.0',
+	py_modules	=['rcsprint'],
+	author		='dfcpython',
+	author_email='bigmeal@yeah.net',
+	url			='',
+	description	='A simple printer of nested lists'
+)
