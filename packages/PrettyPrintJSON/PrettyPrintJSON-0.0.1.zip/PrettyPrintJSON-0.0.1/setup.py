ceb06c6a4b62883a45cccdfcf4de21af92a62381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:12 +0000

    Add package PrettyPrintJSON/0.0.1/PrettyPrintJSON-0.0.1.zip

---
+++ packages/PrettyPrintJSON/PrettyPrintJSON-0.0.1.zip/PrettyPrintJSON-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PrettyPrintJSON',
+    version='0.0.1',
+    packages=('pretty_print', ),
+    url='http://pythonhosted.org',
+    license='',
+    author='Ian Davis',
+    author_email='thekewlstore@gmail.com',
+    description='Simple pretty printer interface using JSON format'
+)
