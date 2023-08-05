5a48632ac509c4efe83042fce469033bb119fd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:06 +0000

    Add package remotecv/2.0.0rc4/remotecv-2.0.0rc4.tar.gz

---
+++ packages/remotecv/remotecv-2.0.0rc4.tar.gz/remotecv-2.0.0rc4/remotecv/image_processor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from remotecv.detectors.face_detector import FaceDetector
+from remotecv.detectors.feature_detector import FeatureDetector
+from remotecv.detectors.glasses_detector import GlassesDetector
+from remotecv.detectors.profile_detector import ProfileDetector
+from remotecv.detectors.complete_detector import CompleteDetector
+from remotecv.image import Image
+
+
+class ImageProcessor:
+    def __init__(self):
+        self.detectors = {
+            'all': CompleteDetector(),
+            'face': FaceDetector(),
+            'feature': FeatureDetector(),
+            'glass': GlassesDetector(),
+            'profile': ProfileDetector()
+        }
+
+    def detect(self, detector, image_data):
+        detector = self.detectors.get(detector, None)
+        if detector is None:
+            raise AttributeError('Detector unavailable')
+
+        image = Image.create_from_buffer(image_data)
+        if image is None:
+            return []
+
+        return detector.detect(image)
