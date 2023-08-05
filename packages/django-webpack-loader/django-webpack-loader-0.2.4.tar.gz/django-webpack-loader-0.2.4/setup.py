2cac295e24d53c9ece6494957dd2fe205d2baf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package django-webpack-loader/0.2.4/django-webpack-loader-0.2.4.tar.gz

---
+++ packages/django-webpack-loader/django-webpack-loader-0.2.4.tar.gz/django-webpack-loader-0.2.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+version = '0.2.4'
+
+setup(
+  name = 'django-webpack-loader',
+  packages = ['webpack_loader', 'webpack_loader/templatetags', 'webpack_loader/contrib'],
+  version = version,
+  description = 'Transparently use webpack with django',
+  author = 'Owais Lone',
+  author_email = 'hello@owaislone.org',
+  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{}'.format(version),
+  url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
+  keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
+  data_files = [("", ["LICENSE"])],
+  classifiers = [
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Framework :: Django',
+    'Environment :: Web Environment',
+    'License :: OSI Approved :: MIT License',
+  ],
+)
