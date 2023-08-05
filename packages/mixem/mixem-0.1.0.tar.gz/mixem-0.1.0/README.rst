e1506a3682e4123ac6c9244fe3383c3c02929d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:33 +0000

    Add package mixem/0.1.0/mixem-0.1.0.tar.gz

---
+++ packages/mixem/mixem-0.1.0.tar.gz/mixem-0.1.0/README.rst
@@ -0,0 +1,23 @@
+mix'EM 
+======
+
+mixem is a pure-python implementation of the Expectation-Maximization (EM) algorithm for fitting mixtures of probability distributions. It works in Python 2 and Python 3 (tested with 2.7 and 3.5.1).
+
+Features
+--------
+
+* Built-in support for several probability distributions
+* Easily define custom probability distributions implementing their PDF and weighted log-likelihood
+
+Dependencies
+------------
+
+* NumPy v1.7.0 or newer
+* SciPy v0.14.0 or newer
+
+Installation
+------------
+
+::
+
+    pip install mixem
---
+++ packages/mixem/mixem-0.1.0.tar.gz/mixem-0.1.0/mixem/distribution/normal.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+import numpy as np
+import scipy.stats
+
+from mixem.distribution.distribution import Distribution
+
+
+class NormalDistribution(Distribution):
+    """Univariate normal distribution with parameters (mu, sigma)."""
+
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+
+    def density(self, data):
+        assert(len(data.shape) == 1), "Expect 1D data!"
+
+        return scipy.stats.norm.pdf(data, self.mu, self.sigma)
+
+    def estimate_parameters(self, data, weights):
+        assert(len(data.shape) == 1), "Expect 1D data!"
+
+        wsum = np.sum(weights)
+
+        self.mu = np.sum(weights * data) / wsum
+        self.sigma = np.sqrt(np.sum(weights * (data - self.mu) ** 2) / wsum)
+
+    def __repr__(self):
+        return "Normal[μ={mu}, σ={sigma}]".format(mu=self.mu, sigma=self.sigma)
+
+
+class MultivariateNormalDistribution(Distribution):
+    """Multivariate normal distribution with parameters (mu, Sigma)."""
+
+    def __init__(self, mu, sigma):
+        mu = np.array(mu)
+        sigma = np.array(sigma)
+
+        assert len(mu.shape) == 1, "Expect mu to be 1D vector!"
+        assert len(sigma.shape) == 2, "Expect sigma to be 2D matrix!"
+
+        assert sigma.shape[0] == sigma.shape[1], "Expect sigma to be a square matrix!"
+
+        self.mu = mu
+        self.sigma = sigma
+
+    def density(self, data):
+        return scipy.stats.multivariate_normal.pdf(data, self.mu, self.sigma)
+
+    def estimate_parameters(self, data, weights):
+        self.mu = np.sum(data * weights[:, np.newaxis], axis=0) / np.sum(weights)
+
+        center_x = data - self.mu[np.newaxis, :]
+
+        # sigma = (np.diag(weights) @ center_x).T @ center_x / np.sum(weights)
+        self.sigma = np.dot(
+            np.dot(
+                np.diag(weights),
+                center_x
+            ).T,
+            center_x
+        ) / np.sum(weights)
+
+    def __repr__(self):
+        return "MultiNormal[μ={mu}, σ={sigma}]".format(mu=self.mu, sigma=self.sigma)
