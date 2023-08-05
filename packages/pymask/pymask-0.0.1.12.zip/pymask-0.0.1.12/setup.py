6f66c379d094a7e5d9bee4d1dbf6b3cee99edbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:35 +0000

    Add package pymask/0.0.1.12/pymask-0.0.1.12.zip

---
+++ packages/pymask/pymask-0.0.1.12.zip/pymask-0.0.1.12/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pymask',  # 打包后的包文件名
+      version='0.0.1.12',
+      description='Python Distribution Utilities',
+      author='skywalker',
+      author_email='cooglex@qq.com',
+      url='http://www.skywalker.studio',
+      py_modules=['utils'],  # 与前面的新建文件名一致
+      packages=['pymask'],
+      )
