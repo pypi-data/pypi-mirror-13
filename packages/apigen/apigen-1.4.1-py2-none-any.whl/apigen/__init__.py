3481b2465f0ea7bf433425c4402d4b908a8eec2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package apigen/1.4.1/apigen-1.4.1-py2-none-any.whl

---
+++ packages/apigen/apigen-1.4.1-py2-none-any.whl/apigen/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Copyright (c) 2015 Fabian Barkhau <fabian.barkhau@gmail.com>
+# License: MIT (see LICENSE file)
+
+
+from .version import __version__  # NOQA
+from . apigen import ( # NOQA
+    VarargsFound,
+    KeywordsFound,
+    command,
+    Definition,
+    run
+)
