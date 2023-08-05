d846071e08c4f0b8f017edc555eec1dff98cb156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:06 +0000

    Add package MwasUtilities/0.0.2/MwasUtilities-0.0.2.tar.gz

---
+++ packages/MwasUtilities/MwasUtilities-0.0.2.tar.gz/MwasUtilities-0.0.2/utilities.py
@@ -0,0 +1,28 @@
+__author__ = 'Mwaaas'
+__email__ = 'francismwangi152@gmail.com'
+__phone_number__ = '+254702729654'
+
+import logging
+import os
+
+
+class NotFound(object):
+    def get(self, k, d=None):
+        if d == self:
+            return d
+        return None
+
+
+not_found = NotFound()
+
+
+class LogDjangoEnvironment(logging.Filter):
+
+    def __init__(self):
+        settings = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "")
+        self.environment = settings.split('.')[-1]
+        super(LogDjangoEnvironment, self).__init__()
+
+    def filter(self, record):
+        record.environment = self.environment
+        return True
