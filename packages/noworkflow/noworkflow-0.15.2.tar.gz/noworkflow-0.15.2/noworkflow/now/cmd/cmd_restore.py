ebbda23cec7a90c16f340e06ada68579893de0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:28 +0000

    Add package noworkflow/0.15.2/noworkflow-0.15.2.tar.gz

---
+++ packages/noworkflow/noworkflow-0.15.2.tar.gz/noworkflow-0.15.2/noworkflow/now/cmd/cmd_restore.py
@@ -0,0 +1,101 @@
+# Copyright (c) 2015 Universidade Federal Fluminense (UFF)
+# Copyright (c) 2015 Polytechnic Institute of New York University.
+# This file is part of noWorkflow.
+# Please, consult the license terms in the LICENSE file.
+""" 'now restore' command """
+from __future__ import (absolute_import, print_function,
+                        division, unicode_literals)
+
+import os
+import sys
+from datetime import datetime
+
+from .command import Command
+from .types import trial_reference
+from .. import prov_deployment
+from ..cross_version import items
+from ..models.trial import Trial
+from ..persistence import persistence
+from ..utils.io import print_msg
+from ..metadata import Metascript
+
+
+class Restore(Command):
+    """ Restore the files of a trial """
+
+    def __init__(self, *args, **kwargs):
+        super(Restore, self).__init__(*args, **kwargs)
+        self.print_msg = True
+
+    def add_arguments(self):
+        add_arg = self.add_argument
+        add_arg('trial', type=str, nargs='?',
+                help='trial id or none for last trial')
+        add_arg('-s', '--script',
+                help='python script to be restored')
+        add_arg('-b', '--bypass-modules', action='store_true',
+                help='bypass module dependencies analysis, assuming that no '
+                     'module changes occurred since last execution')
+        add_arg('-l', '--local', action='store_true',
+                help='restore local modules')
+        add_arg('-i', '--input', action='store_true',
+                help='restore input files')
+        add_arg('--dir', type=str,
+                help='set project path where is the database. Default to '
+                     'current directory')
+
+    def create_backup(self, metascript):
+        """ Create a backup trial if the script were changed """
+        trial = metascript.trial
+        if not os.path.isfile(trial.script):
+            return
+
+        head = trial.head_trial()
+        code_hash = persistence.put(metascript.code)
+
+        if code_hash != head.code_hash:
+            now = datetime.now()
+            metascript.create_trial(args='<restore {}>'.format(trial.id),
+                                    run=False)
+            prov_deployment.collect_provenance(metascript)
+            print_msg('Backup Trial {} created'.format(metascript.trial_id),
+                      self.print_msg)
+
+    def restore(self, path, code_hash, trial_id):
+        """ Restore file with <code_hash> from <trial_id> """
+        load_file = persistence.get(code_hash)
+        with open(path, 'wb') as fil:
+            fil.write(load_file)
+        print_msg('File {} from trial {} restored'.format(path, trial_id),
+                  self.print_msg)
+
+    def restore_script(self, trial):
+        """ Restore the main script from <trial> """
+        self.restore(trial.script, trial.code_hash, trial.id)
+
+    def execute(self, args):
+        persistence.connect_existing(args.dir or os.getcwd())
+        metascript = Metascript().read_restore_args(args)
+        trial = metascript.trial = Trial(trial_ref=metascript.trial_id,
+                                         script=metascript.name,
+                                         exit=True)
+        metascript.path = trial.script
+        metascript.name = trial.script
+
+        self.create_backup(metascript)
+
+        self.restore_script(trial)
+
+        persistence.store_parent(trial.script, trial.id)
+        if args.local:
+            local_modules, _ = trial.modules()
+            for module in local_modules:
+                self.restore(module['path'], module['code_hash'], trial.id)
+
+        if args.input:
+            file_accesses = trial.file_accesses()
+            files = {}
+            for faccess in reversed(file_accesses):
+                files[faccess['name']] = faccess['content_hash_before']
+            for name, content_hash in items(files):
+                self.restore(name, content_hash, trial.id)
---
+++ packages/noworkflow/noworkflow-0.15.2.tar.gz/noworkflow-0.15.2/noworkflow/resources/demo/1/step3/simulator.py
@@ -0,0 +1,6 @@
+import time
+
+def simulate(a, b):
+    time.sleep(0.01)
+    data = a + b
+    return data
