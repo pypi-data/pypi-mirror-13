eeef47206736db345073b3676d26268374974528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:20 +0000

    Add package smysql/0.2.6/smysql-0.2.6.tar.gz

---
+++ packages/smysql/smysql-0.2.6.tar.gz/smysql-0.2.6/setup.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+from setuptools import setup,find_packages
+
+PACKAGE = "smysql"
+NAME = "smysql"
+DESCRIPTION = "simple mysql orm"
+AUTHOR = "lovedboy"
+AUTHOR_EMAIL = "lovedboy.tk@qq.com"
+URL = "https://github.com/lovedboy/smysql"
+VERSION = '0.2.6'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    url=URL,
+    include_package_data = True,
+    packages = find_packages(),
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=False,
+)
