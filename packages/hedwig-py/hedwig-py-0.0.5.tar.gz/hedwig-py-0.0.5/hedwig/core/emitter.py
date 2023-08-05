c0466f15c854999e362131d9a176cf445052c94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:39 +0000

    Add package hedwig-py/0.0.5/hedwig-py-0.0.5.tar.gz

---
+++ packages/hedwig-py/hedwig-py-0.0.5.tar.gz/hedwig-py-0.0.5/hedwig/core/emitter.py
@@ -0,0 +1,32 @@
+from .base import Base
+import logging
+from pika.exceptions import ConnectionClosed
+LOGGER = logging.getLogger(__name__)
+
+
+class Emitter(Base):
+    def emit(self, key, message):
+        """
+         Emits a message to RabbitMQ
+
+        :param key: Routing key to emit on
+        :param message: json serialized msg body
+        :return:
+        :raises Exception: If RAISE_EXCEPTION is true in emitter settings
+        """
+        try:
+            self._emit(key, message)
+        except ConnectionClosed:
+            self.shutdown()
+            self._emit(key, message)
+        except Exception as e:
+            LOGGER.exception("Emitter exception %s" % str(e))
+            if self.settings.EMITTER['RAISE_EXCEPTION']:
+                raise e
+
+    def _emit(self, key, message):
+        pub_channel = self.get_channel()
+        LOGGER.debug("Trying to emit - %s" % message)
+        pub_channel.basic_publish(exchange=self.settings.EXCHANGE, routing_key=key, body=message)
+        logging.info("Emitted - %s" % message)
+
