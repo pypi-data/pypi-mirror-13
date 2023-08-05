f448388bc88cbe25d52d95f12d3005e2ed078ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:11 +0000

    Add package lint381/0.5.1/lint381-0.5.1.tar.gz

---
+++ packages/lint381/lint381-0.5.1.tar.gz/lint381-0.5.1/lint381/linter.py
@@ -0,0 +1,54 @@
+"""Linter tools."""
+import collections
+
+from .tokenizer import tokenize
+
+
+Error = collections.namedtuple("Error", [
+    "message",
+    "tokens",
+])
+"""A linter error.
+
+:ivar str message: The error message.
+:ivar list tokens: A list of relevant tokens, in the order that they appear in
+    the source code.
+"""
+
+
+class Linter:
+    """A registry of linters for a specific language."""
+
+    def __init__(self):
+        """Initialize the linter registry to be empty."""
+        self._linters = []
+
+    def register(self, func):
+        """Register the provided function as a linter.
+
+        This should be used as a decorator:
+
+        ```
+        linters = Linter()
+
+        @linters.register
+        def linter():
+            ...
+        ```
+
+        :param function func: The linting function.
+        """
+        self._linters.append(func)
+        return func
+
+    def lint(self, code):
+        """Find linting errors on the specified lines of source code.
+
+        :param str code: The source code.
+        """
+        errors = []
+
+        for func in self._linters:
+            errors.extend(func(tokenize(code)))
+
+        return errors
---
+++ packages/lint381/lint381-0.5.1.tar.gz/lint381-0.5.1/test/test_matcher.py
@@ -0,0 +1,77 @@
+"""Test the token matcher."""
+from lint381.matcher import (
+    match_regex,
+    match_tokens,
+    match_type,
+    with_matched_tokens,
+)
+from lint381.tokenizer import Token, tokenize
+
+
+def test_match_regex():
+    """Ensure that the regex matcher matches tokens correctly."""
+    token = Token(type="identifier", value="foo", start=None, end=None)
+    assert match_regex("^foo$")(token)
+    assert not match_regex("^bar$")(token)
+
+
+def test_match_type():
+    """Ensure that we match on identifier types."""
+    token = Token(type="identifier", value=None, start=None, end=None)
+    assert match_type("identifier")(token)
+    assert not match_type("binary_operator")(token)
+
+
+def test_match_tokens():
+    """Ensure that we match tokens correctly."""
+    code = """
+foo qux bar
+foo grault bar
+foo bar
+"""
+
+    expected = [["foo", "qux", "bar"],
+                ["foo", "grault", "bar"],
+                ["foo", "bar"]]
+    assert expected == [
+        [token.value for token in match]
+        for match in match_tokens(tokenize(code),
+                                  start=match_regex("foo"),
+                                  end=match_regex("bar"))]
+
+
+def test_no_matching_token():
+    """Ensure that we handle cases when there is no match."""
+    code = """
+foo
+"""
+
+    assert not list(match_tokens(tokenize(code),
+                                 start=match_regex("foo"),
+                                 end=match_regex("bar")))
+
+
+def test_not_enough_lookahead():
+    """Ensure that we don't return a match if there's not enough lookahead."""
+    code = """
+foo bar
+"""
+
+    assert not list(match_tokens(tokenize(code),
+                                 start=match_regex("foo"),
+                                 end=match_regex("bar"),
+                                 lookahead=1))
+
+
+def test_with_matching_tokens():
+    """Ensure that we can use the decorator to match tokens."""
+    code = """
+foo bar
+"""
+
+    @with_matched_tokens(start=match_regex("foo"),
+                         end=match_regex("bar"))
+    def func(tokens, match):
+        yield "baz"
+
+    assert list(func(tokenize(code))) == ["baz"]
