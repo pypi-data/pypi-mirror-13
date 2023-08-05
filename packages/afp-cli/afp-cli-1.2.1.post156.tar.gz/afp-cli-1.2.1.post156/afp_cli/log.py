7c9dd457e7cce183e6617f8937789ff476810148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package afp-cli/1.2.1.post156/afp-cli-1.2.1.post156.tar.gz

---
+++ packages/afp-cli/afp-cli-1.2.1.post156.tar.gz/afp-cli-1.2.1.post156/afp_cli/log.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from __future__ import print_function, absolute_import, division
+
+
+import sys
+
+
+DEBUG = False
+
+
+class CMDLineExit(Exception):
+    pass
+
+
+def info(message):
+    print(message)
+
+
+def error(message):
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def debug(message):
+    if DEBUG:
+        print(message)
