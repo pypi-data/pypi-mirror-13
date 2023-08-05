4e5cbf648b5363a1edbede0ccbf143e3111d9f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:29 +0000

    Add package uncompyle6/2.1.1/uncompyle6-2.1.1.tar.gz

---
+++ packages/uncompyle6/uncompyle6-2.1.1.tar.gz/uncompyle6-2.1.1/test/base_tests/python2.7/test_print_to.py
@@ -0,0 +1,13 @@
+
+import sys
+
+print >>sys.stdout, 1,2,3,4,5
+
+print >>sys.stdout, 1,2,3,4,5,
+print >>sys.stdout
+
+print >>sys.stdout, 1,2,3,4,5,
+print >>sys.stdout, 1,2,3,4,5,
+print >>sys.stdout
+
+print >>sys.stdout
