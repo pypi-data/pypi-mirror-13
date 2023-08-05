6294ab18509df185acb16fb0dce0af7f0e660ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:10 +0000

    Add package django-mail-templated/1.0.0/django-mail-templated-1.0.0.tar.gz

---
+++ packages/django-mail-templated/django-mail-templated-1.0.0.tar.gz/django-mail-templated-1.0.0/mail_templated/__init__.py
@@ -0,0 +1,24 @@
+from django.core import mail
+
+from .message import EmailMessage
+
+
+def send_mail(template_name, context, from_email, recipient_list,
+              fail_silently=False, auth_user=None, auth_password=None,
+              connection=None, *args, **kwargs):
+    """
+    Easy wrapper for sending a single message to a recipient list using
+    django template system.
+    All members of the recipient list will see the other recipients in
+    the 'To' field.
+
+    If auth_user is None, the EMAIL_HOST_USER setting is used.
+    If auth_password is None, the EMAIL_HOST_PASSWORD setting is used.
+    """
+
+    connection = connection or mail.get_connection(username=auth_user,
+                                                   password=auth_password,
+                                                   fail_silently=fail_silently)
+    return EmailMessage(
+        template_name, context, None, None, from_email, recipient_list,
+        connection=connection, *args, **kwargs).send()
