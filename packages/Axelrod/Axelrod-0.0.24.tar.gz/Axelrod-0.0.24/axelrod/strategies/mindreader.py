537e7db1c83a5a5a1327b9488ef0412f4087e1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:37 +0000

    Add package Axelrod/0.0.24/Axelrod-0.0.24.tar.gz

---
+++ packages/Axelrod/Axelrod-0.0.24.tar.gz/Axelrod-0.0.24/axelrod/strategies/mindreader.py
@@ -0,0 +1,132 @@
+import copy
+import inspect
+
+from axelrod import Actions, Player, RoundRobin, update_history
+
+C, D = Actions.C, Actions.D
+
+def simulate_match(player_1, player_2, strategy, rounds=10):
+    """Simulates a number of matches."""
+    for match in range(rounds):
+        play_1, play_2 = strategy, player_2.strategy(player_1)
+        # Update histories and counts
+        update_history(player_1, play_1)
+        update_history(player_2, play_2)
+
+def roll_back_history(player, rounds):
+    """Undo the last `rounds` rounds as sufficiently as possible."""
+    for i in range(rounds):
+        play = player.history.pop(-1)
+        if play == C:
+            player.cooperations -= 1
+        elif play == D:
+            player.defections -= 1
+
+def look_ahead(player_1, player_2, game, rounds=10):
+    """Looks ahead for `rounds` and selects the next strategy appropriately."""
+    results = []
+
+    # Simulate plays for `rounds` rounds
+    strategies = [C, D]
+    for strategy in strategies:
+        opponent_ = copy.deepcopy(player_2) # need deepcopy here
+        round_robin = RoundRobin(players=[player_1, opponent_], game=game,
+                                 turns=rounds)
+        simulate_match(player_1, opponent_, strategy, rounds)
+        results.append(round_robin._calculate_scores(player_1, opponent_)[0])
+
+        # Restore histories and counts
+        roll_back_history(player_1, rounds)
+
+    return strategies[results.index(max(results))]
+
+
+class MindReader(Player):
+    """A player that looks ahead at what the opponent will do and decides what to do."""
+
+    name = 'Mind Reader'
+    classifier = {
+        'memory_depth': -10,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': True,  # Finds out what opponent will do
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent):
+        """Pretends to play the opponent a number of times before each match.
+        The primary purpose is to look far enough ahead to see if a defect will
+        be punished by the opponent.
+
+        If the MindReader attempts to play itself (or another similar
+        strategy), then it will cause a recursion loop, so this is also handled
+        in this method, by defecting if the method is called by strategy
+        """
+
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        calname = calframe[1][3]
+
+        if calname in ('strategy', 'simulate_match'):
+            return D
+
+        game = self.tournament_attributes["game"]
+
+        best_strategy = look_ahead(self, opponent, game)
+
+        return best_strategy
+
+
+class ProtectedMindReader(MindReader):
+    """A player that looks ahead at what the opponent will do and decides what to do.
+    It is also protected from mind control strategies"""
+
+    name = 'Protected Mind Reader'
+    classifier = {
+        'memory_depth': -10,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': True,  # Finds out what opponent will do
+        'manipulates_source': True,  # Stops opponent's strategy
+        'manipulates_state': False
+    }
+
+    def __setattr__(self, name, val):
+        """Stops any other strategy altering the methods of this class """
+
+        if name == 'strategy':
+            pass
+        else:
+            self.__dict__[name] = val
+
+class MirrorMindReader(ProtectedMindReader):
+    """A player that will mirror whatever strategy it is playing against by cheating
+    and calling the opponent's strategy function instead of its own."""
+
+    name = 'Mirror Mind Reader'
+
+    classifier = {
+        'memory_depth': -10,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': True, # reading and copying the source of the component
+        'manipulates_source': True, # changing own source dynamically
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent):
+        """Will read the mind of the opponent and play the opponent's strategy.
+
+        Also avoid infinite recursion when called by itself or another mind reader
+        or bender by cooperating.
+        """
+
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        calname = calframe[1][3]
+
+        if calname in ('strategy', 'simulate_match'):
+            return C
+
+        return opponent.strategy(self)
---
+++ packages/Axelrod/Axelrod-0.0.24.tar.gz/Axelrod-0.0.24/axelrod/tournament.py
@@ -0,0 +1,146 @@
+from __future__ import absolute_import
+
+import logging
+import multiprocessing
+
+from .game import *
+from .result_set import *
+from .round_robin import *
+
+
+class Tournament(object):
+    game = Game()
+
+    def __init__(self, players, name='axelrod', game=None, turns=200,
+                 repetitions=10, processes=None, prebuilt_cache=False,
+                 noise=0, with_morality=True):
+        self.name = name
+        self.turns = turns
+        self.noise = noise
+        if game is not None:
+            self.game = game
+        self.players = players
+        self.nplayers = len(self.players)
+        self.repetitions = repetitions
+        self.prebuilt_cache = prebuilt_cache
+        self.deterministic_cache = {}
+        self._with_morality = with_morality
+        self._parallel_repetitions = repetitions
+        self._processes = processes
+        self._logger = logging.getLogger(__name__)
+        self._outcome = {'payoff': [], 'cooperation': []}
+
+    @property
+    def players(self):
+        return self._players
+
+    @players.setter
+    def players(self, players):
+        newplayers = []
+        for player in players:
+            player.set_tournament_attributes(length=self.turns,
+                                             game=self.game,
+                                             noise=self.noise)
+            newplayers.append(player)
+        self._players = newplayers
+
+    def play(self):
+        if self._processes is None:
+            self._run_serial_repetitions(self._outcome)
+        else:
+            if self._build_cache_required():
+                self._build_cache(self._outcome)
+            self._run_parallel_repetitions(self._outcome)
+
+        self.result_set = ResultSet(
+            players=self.players,
+            turns=self.turns,
+            repetitions=self.repetitions,
+            outcome=self._outcome,
+            with_morality=self._with_morality)
+        return self.result_set
+
+    def _build_cache_required(self):
+        return (
+            not self.noise and (
+                len(self.deterministic_cache) == 0 or
+                not self.prebuilt_cache))
+
+    def _build_cache(self, outcome):
+        self._logger.debug('Playing first round robin to build cache')
+        self._run_single_repetition(outcome)
+        self._parallel_repetitions -= 1
+
+    def _run_single_repetition(self, outcome):
+        output = self._play_round_robin()
+        outcome['payoff'].append(output['payoff'])
+        outcome['cooperation'].append(output['cooperation'])
+
+    def _run_serial_repetitions(self, outcome):
+        self._logger.debug('Playing %d round robins' % self.repetitions)
+        for repetition in range(self.repetitions):
+            self._run_single_repetition(outcome)
+        return True
+
+    def _run_parallel_repetitions(self, outcome):
+        # At first sight, it might seem simpler to use the multiprocessing Pool
+        # Class rather than Processes and Queues. However, Pool can only accept
+        # target functions which can be pickled and instance methods cannot.
+        work_queue = multiprocessing.Queue()
+        done_queue = multiprocessing.Queue()
+        workers = self._n_workers()
+
+        for repetition in range(self._parallel_repetitions):
+            work_queue.put(repetition)
+
+        self._logger.debug(
+            'Playing %d round robins with %d parallel processes' %
+            (self._parallel_repetitions, workers))
+        self._start_workers(workers, work_queue, done_queue)
+        self._process_done_queue(workers, done_queue, outcome)
+
+        return True
+
+    def _n_workers(self):
+        if (2 <= self._processes <= multiprocessing.cpu_count()):
+            n_workers = self._processes
+        else:
+            n_workers = multiprocessing.cpu_count()
+        return n_workers
+
+    def _start_workers(self, workers, work_queue, done_queue):
+        for worker in range(workers):
+            process = multiprocessing.Process(
+                target=self._worker, args=(work_queue, done_queue))
+            work_queue.put('STOP')
+            process.start()
+        return True
+
+    @staticmethod
+    def _process_done_queue(workers, done_queue, outcome):
+        stops = 0
+        while stops < workers:
+            output = done_queue.get()
+            if output == 'STOP':
+                stops += 1
+            else:
+                outcome['payoff'].append(output['payoff'])
+                outcome['cooperation'].append(output['cooperation'])
+        return True
+
+    def _worker(self, work_queue, done_queue):
+        for repetition in iter(work_queue.get, 'STOP'):
+            output = self._play_round_robin(cache_mutable=False)
+            done_queue.put(output)
+        done_queue.put('STOP')
+        return True
+
+    def _play_round_robin(self, cache_mutable=True):
+        round_robin = RoundRobin(
+            players=self.players,
+            game=self.game,
+            turns=self.turns,
+            deterministic_cache=self.deterministic_cache,
+            cache_mutable=cache_mutable,
+            noise=self.noise)
+        return round_robin.play()
