81ff5e7b8fca716b3139aebf6ea2eda86bd0bd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package scikit-chainer/0.3/scikit-chainer-0.3.tar.gz

---
+++ packages/scikit-chainer/scikit-chainer-0.3.tar.gz/scikit-chainer-0.3/skchainer/mlp.py
@@ -0,0 +1,22 @@
+__author__ = 'du'
+
+from chainer import Chain, functions as F
+from . import ChainerClassifier
+
+
+class MultiLayerPerceptron(ChainerClassifier):
+    def _setup_network(self, **params):
+        network = Chain(
+            l1=F.Linear(params["input_dim"], params["hidden_dim"]),
+            l2=F.Linear(params["hidden_dim"], params["n_classes"])
+        )
+
+        return network
+
+    def _forward(self, x, train=True):
+        h = F.relu(self.network.l1(x))
+        y = self.network.l2(h)
+        return y
+
+    def _loss_func(self, y, t):
+        return F.softmax_cross_entropy(y, t)
