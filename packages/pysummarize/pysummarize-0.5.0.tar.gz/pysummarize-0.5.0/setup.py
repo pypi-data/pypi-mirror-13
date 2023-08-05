bdca3550563dfbd816324cb1c09d7e139d31fcc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package pysummarize/0.5.0/pysummarize-0.5.0.tar.gz

---
+++ packages/pysummarize/pysummarize-0.5.0.tar.gz/pysummarize-0.5.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysummarize',
+    version='0.5.0',
+    description='Simple Python and NLTK-based implementation of text summarization',
+    url='https://github.com/despawnerer/summarize',
+    author='Aleksei Voronov',
+    author_email='despawn@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='language nltk linguistics nlp',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        'Distance>=0.1.3',
+        'networkx>=1.9.1',
+        'nltk>=3.0.3',
+        'funcy>=1.5',
+    ],
+)
