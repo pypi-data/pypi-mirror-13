777e4b7faa1397d55226c11fa94a5188d8719b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:01 +0000

    Add package django-tables2/1.1.1/django-tables2-1.1.1.tar.gz

---
+++ packages/django-tables2/django-tables2-1.1.1.tar.gz/django-tables2-1.1.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import re
+
+from setuptools import find_packages, setup
+
+with open('django_tables2/__init__.py', 'rb') as f:
+    version = str(re.search('__version__ = "(.+?)"', f.read().decode('utf-8')).group(1))
+
+setup(
+    name='django-tables2',
+    version=version,
+    description='Table/data-grid framework for Django',
+
+    author='Bradley Ayers',
+    author_email='bradley.ayers@gmail.com',
+    license='Simplified BSD',
+    url='https://github.com/bradleyayers/django-tables2/',
+
+    packages=find_packages(exclude=['tests.*', 'tests', 'example.*', 'example']),
+    include_package_data=True,  # declarations in MANIFEST.in
+
+    install_requires=['Django >=1.7', 'six'],
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
