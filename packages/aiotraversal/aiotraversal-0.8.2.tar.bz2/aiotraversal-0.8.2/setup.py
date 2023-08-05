43b262b96f8a72da47dd2c8e79cc7c0154b83dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:00 +0000

    Add package aiotraversal/0.8.2/aiotraversal-0.8.2.tar.bz2

---
+++ packages/aiotraversal/aiotraversal-0.8.2.tar.bz2/aiotraversal-0.8.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='aiotraversal',
+    version='0.8.2',
+    description='Traversal based asyncronious web framework',
+    # long_description=README,
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    author='Alexander Zelenyak',
+    author_email='zzz.sochi@gmail.com',
+    url='',
+    keywords=['asyncio', 'aiohttp', 'traversal', 'pyramid'],
+    packages=['aiotraversal'],
+    install_requires=[
+        'aiohttp',
+        'aiohttp_traversal>=0.8',
+        'aiohttp_exc_handlers',
+        'resolver-deco',
+        'includer',
+    ],
+    tests_require=['pytest'],
+)
