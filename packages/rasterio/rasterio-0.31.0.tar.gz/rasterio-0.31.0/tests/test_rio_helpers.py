06fbb041235eb3f3e04724ad63db3c451a0c4b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package rasterio/0.31.0/rasterio-0.31.0.tar.gz

---
+++ packages/rasterio/rasterio-0.31.0.tar.gz/rasterio-0.31.0/tests/test_rio_helpers.py
@@ -0,0 +1,23 @@
+from rasterio.rio import helpers
+
+
+def test_resolve_files_inout__output():
+    assert helpers.resolve_inout(input='in', output='out') == ('out', ['in'])
+
+
+def test_resolve_files_inout__input():
+    assert helpers.resolve_inout(input='in') == (None, ['in'])
+
+
+def test_resolve_files_inout__inout_files():
+    assert helpers.resolve_inout(files=('a', 'b', 'c')) == ('c', ['a', 'b'])
+
+
+def test_resolve_files_inout__inout_files_output_o():
+    assert helpers.resolve_inout(
+        files=('a', 'b', 'c'), output='out') == ('out', ['a', 'b', 'c'])
+
+
+def test_to_lower():
+    
+    assert helpers.to_lower(None, None, 'EPSG:3857') == 'epsg:3857'
