36ffe895a15fae32bb79c056b935e37d4dce13fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:15 +0000

    Add package hotfix/1.0.3/hotfix-1.0.3.zip

---
+++ packages/hotfix/hotfix-1.0.3.zip/hotfix-1.0.3/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='hotfix',
+    packages=['hotfix'],
+    version='1.0.3',
+    author='Simon Lessnick',
+    author_email='s_lessnick@yahoo.com',
+    license='MIT',
+    install_requires=['django'],
+    include_package_data=True,
+)
