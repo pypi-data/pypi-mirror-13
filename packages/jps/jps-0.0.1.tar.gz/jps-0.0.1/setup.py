72542638ec24780a42219a21b3ec1f59293f45e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:26 +0000

    Add package jps/0.0.1/jps-0.0.1.tar.gz

---
+++ packages/jps/jps-0.0.1.tar.gz/jps-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='jps', version='0.0.1', description='json pub/sub using zmq',
+      author='Takashi Ogura', author_email='t.ogura@gmail.com',
+      url='http://github.com/OTL/jps', packages=find_packages(),
+      install_requires=[
+        'zmq',
+        'tornado'
+        ],
+      entry_points= {
+        'console_scripts': [
+            'jps_forwarder = jps.forwarder:main',
+            'jps_echo = jps.tools:echo_command',
+            'jps_pub = jps.tools:pub_command',
+            ]
+        }
+      )
