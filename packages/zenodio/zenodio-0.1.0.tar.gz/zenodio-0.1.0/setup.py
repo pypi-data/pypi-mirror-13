f45c7be21f9de34c0577945d7a759ca34f776f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:53 +0000

    Add package zenodio/0.1.0/zenodio-0.1.0.tar.gz

---
+++ packages/zenodio/zenodio-0.1.0.tar.gz/zenodio-0.1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+
+packagename = 'zenodio'
+description = 'I/O with Zenodo.'
+author = 'Jonathan Sick'
+author_email = 'jsick@lsst.org'
+license = 'MIT'
+url = 'https://github.com/lsst-sqre/zenodio'
+version = '0.1.0'
+
+
+def read(filename):
+    full_filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        filename)
+    return open(full_filename, mode='r', encoding='utf8').read()
+
+long_description = read('README.rst')
+
+
+setup(
+    name=packagename,
+    version=version,
+    description=description,
+    long_description=long_description,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='aas',
+    packages=find_packages(exclude=['docs', 'tests*', 'data', 'notebooks']),
+    install_requires=['future', 'requests', 'xmltodict'],
+    tests_require=['pytest'],
+    # package_data={},
+)
