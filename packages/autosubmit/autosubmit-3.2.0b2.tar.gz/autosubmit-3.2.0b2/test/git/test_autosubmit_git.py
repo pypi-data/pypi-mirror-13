6bd254a74c2560d1745bfbce1f5d51c38c6d3fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package autosubmit/3.2.0b2/autosubmit-3.2.0b2.tar.gz

---
+++ packages/autosubmit/autosubmit-3.2.0b2.tar.gz/autosubmit-3.2.0b2/test/git/test_autosubmit_git.py
@@ -0,0 +1,10 @@
+# from unittest import TestCase
+#
+#
+# class TestAutosubmitGit(TestCase):
+#
+#     def test_clean_git(self):
+#         self.fail()
+#
+#     def test_clone_repository(self):
+#         self.fail()
