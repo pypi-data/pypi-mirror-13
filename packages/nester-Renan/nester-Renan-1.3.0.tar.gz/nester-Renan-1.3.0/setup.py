be90951a9a71ad8f2d596739d43381bfc12a31fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:48 +0000

    Add package nester-Renan/1.3.0/nester-Renan-1.3.0.tar.gz

---
+++ packages/nester-Renan/nester-Renan-1.3.0.tar.gz/nester-Renan-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester-Renan',
+        version         = '1.3.0',
+        py_modules      = ['nester'],
+        author          = 'RenanT',
+        author_email    = 'r.turrissi@gmail.com',
+        url             = '',
+        description     = 'A simple printer of nested lists',
+    )
