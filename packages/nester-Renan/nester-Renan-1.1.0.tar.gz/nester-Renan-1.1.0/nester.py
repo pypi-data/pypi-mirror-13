6946ad71a6e7baf00976fc1bbb3409ada97a5d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package nester-Renan/1.1.0/nester-Renan-1.1.0.tar.gz

---
+++ packages/nester-Renan/nester-Renan-1.1.0.tar.gz/nester-Renan-1.1.0/nester.py
@@ -0,0 +1,12 @@
+"""nester.py Module"""
+
+def print_lol(the_list,level):
+    """print_lol function"""
+    
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
