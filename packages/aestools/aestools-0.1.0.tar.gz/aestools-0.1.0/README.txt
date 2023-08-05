affc20939c837a002de8840fc3dc9cb54748b7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:21 +0000

    Add package aestools/0.1.0/aestools-0.1.0.tar.gz

---
+++ packages/aestools/aestools-0.1.0.tar.gz/aestools-0.1.0/README.txt
@@ -0,0 +1,15 @@
+aestools
+========
+
+aestools check KEY
+------------------
+
+Test for weak AES key when used in Galois Counter Mode with a block size
+of 128 bits, as explained in the paper "Cycling Attacks on GCM, GHASH and 
+Other Polynomial MACs and Hashes": https://eprint.iacr.org/2011/202.pdf
+
+aestools generate BITS
+----------------------
+
+Generate safe key of BITS length which is above a specific strength threshold.
+
---
+++ packages/aestools/aestools-0.1.0.tar.gz/aestools-0.1.0/setup.py
@@ -0,0 +1,46 @@
+# coding=utf8
+
+from setuptools import setup, find_packages
+
+with open('README.txt') as f:
+    long_description = f.read()
+
+
+setup_args = dict(
+    name="aestools",
+    version='0.1.0',
+    description='AES tools (GCM weak key checker / safe key generator)',
+    long_description=long_description,
+    author="Jesko Hüttenhain",
+    author_email="rattle@nullteilerfrei.de",
+    url="https://github.com/lichtkegel/WeakAESGCM",
+    license="PSF License",
+    keywords="crypto aes gcm weak key checker safe key generator",
+    platforms="any",
+    classifiers="""\
+Development Status :: 3 - Alpha
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: Python Software Foundation License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Topic :: Security :: Cryptography""".splitlines(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=['pycrypto', ],
+    entry_points={
+        'console_scripts': [
+            'aestools = aestools.cli:main',
+        ]
+    },
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
