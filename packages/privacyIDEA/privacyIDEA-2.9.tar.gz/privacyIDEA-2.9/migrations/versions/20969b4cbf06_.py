26b0951bba44d99626f625508f5722a22b205769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package privacyIDEA/2.9/privacyIDEA-2.9.tar.gz

---
+++ packages/privacyIDEA/privacyIDEA-2.9.tar.gz/privacyIDEA-2.9/migrations/versions/20969b4cbf06_.py
@@ -0,0 +1,48 @@
+"""Add column revoked to Token table
+
+Revision ID: 20969b4cbf06
+Revises: 4d9178fa8336
+Create Date: 2015-08-27 12:19:57.272525
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '20969b4cbf06'
+down_revision = '4d9178fa8336'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.exc import OperationalError
+
+
+def upgrade():
+    try:
+        op.add_column('token', sa.Column('revoked', sa.Boolean(),
+                                         nullable=False,
+                                         default=False))
+    except OperationalError as exx:
+        if exx.orig.message.lower().startswith("duplicate column name"):
+            print("Good. Column revoked already exists.")
+        else:
+            print(exx)
+    except Exception as exx:
+        print ("Could not add column 'revoked' to table 'token'")
+        print (exx)
+
+    try:
+        op.add_column('token', sa.Column('locked', sa.Boolean(),
+                                         nullable=False,
+                                         default=False))
+    except OperationalError as exx:
+        if exx.orig.message.lower().startswith("duplicate column name"):
+            print("Good. Column locked already exists.")
+        else:
+            print(exx)
+    except Exception as exx:
+        print ("Could not add column 'locked' to table 'token'")
+        print (exx)
+
+
+def downgrade():
+    op.drop_column('token', 'revoked')
+    op.drop_column('token', 'locked')
