ed3e947a9d5dafa9268d54c20c1a87cceaf841c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:14 +0000

    Add package jokes/0.1.6/jokes-0.1.6.tar.gz

---
+++ packages/jokes/jokes-0.1.6.tar.gz/jokes-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+VERSION = '0.1.6'
+setup(
+  name = 'jokes',
+  packages = ['jokes',
+              'jokes.modules',
+              'jokes.core',],
+  package_data = {'jokes':['scripts/*']},
+  scripts = ['jokes/scripts/jokes',],
+  version = VERSION,
+  description = 'Experimental language written in python',
+  author = 'Kumseok Jung',
+  author_email = 'jungkumseok@gmail.com',
+  license = 'LICENSE',
+  url = 'https://github.com/jungkumseok/jokes',
+  download_url = 'https://github.com/jungkumseok/jokes/archive/'+VERSION+'.tar.gz',
+  keywords = ['testing', 'language'], 
+  classifiers = [],
+)
