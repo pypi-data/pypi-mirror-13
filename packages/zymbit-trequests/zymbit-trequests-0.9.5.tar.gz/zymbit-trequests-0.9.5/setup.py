43384ebcc6017ddeb699a458ca73d9176a6ae23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:03 +0000

    Add package zymbit-trequests/0.9.5/zymbit-trequests-0.9.5.tar.gz

---
+++ packages/zymbit-trequests/zymbit-trequests-0.9.5.tar.gz/zymbit-trequests-0.9.5/setup.py
@@ -0,0 +1,32 @@
+"""Installer for trequests
+"""
+
+from os import path
+try:
+        from setuptools import setup, find_packages
+except ImportError:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup, find_packages
+
+
+cwd = path.dirname(__file__)
+__version__ = open(path.join(cwd, 'trequests/trequests_version.txt'),
+                   'r').read().strip()
+
+setup(
+    name='zymbit-trequests',
+    description='A Tornado async HTTP/HTTPS client '
+                'adaptor for python-requests',
+    long_description=open('README.rst').read(),
+    version=__version__,
+    author='Wes Mason',
+    author_email='wes@1stvamp.org',
+    url='https://github.com/1stvamp/trequests',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=open('requirements.txt').readlines(),
+    package_data={'': ['trequests_version.txt']},
+    include_package_data=True,
+    test_suite="trequests_tests",
+    license='BSD'
+)
