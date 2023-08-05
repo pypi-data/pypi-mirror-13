b3ed61cad8220c821eb2c8ab9b920678829bbcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package VCheck/1.3.2/VCheck-1.3.2.tar.gz

---
+++ packages/VCheck/VCheck-1.3.2.tar.gz/VCheck-1.3.2/vcheck/__init__.py
@@ -0,0 +1,18 @@
+# Author: Joel Frederico
+"""
+This project is designed to make version checking easier in scripts. A common development problem of scripts is that they depend on older versions of packages that have evolved.  These scripts may have incompatibilities with the newer modules that result in the scripts breaking. It is then very difficult to track down which version of the module the script depends on if it was never recorded which version of the module the script is built against.
+"""
+__version__ = '1.3.2'
+
+__all__ = [
+    'CheckMod',
+    'vcheck',
+    'version',
+    'hexsha'
+    ]
+__all__.sort()
+
+from .checkmod import CheckMod
+from .vcheckmod import *
+from .versionerror import VersionError
+from .versionmod import version, hexsha
