7ad8767f410d7f4c1cc39ded9c2f81cddd74d855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:13 +0000

    Add package ding_nester/1.0.0/ding_nester-1.0.0.zip

---
+++ packages/ding_nester/ding_nester-1.0.0.zip/ding_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'ding_nester',
+	version		= '1.0.0',
+	py_modules	= ['ding_nester'],
+	author		= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description 	= 'A simple printer of nested lists',
+	)
