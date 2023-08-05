9a128cfc40fd21b418d78d9cdbd9449e7d97cf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package tgbotplug/1.2.2/tgbotplug-1.2.2.tar.gz

---
+++ packages/tgbotplug/tgbotplug-1.2.2.tar.gz/tgbotplug-1.2.2/tgbot/webserver.py
@@ -0,0 +1,29 @@
+from bottle import route, run, request, abort
+from botapi import Update
+
+tg_bot = None
+
+
+@route('/ping/')
+def ping():
+    return '<b>Pong!</b>'
+
+
+@route('/update/<token>', method='POST')
+def update(token):
+    if token != tg_bot._token:
+        abort(404, 'Not found: \'/update/%s\'' % token)
+    tg_bot.process_update(Update.from_dict(request.json))
+    return None
+
+
+def wsgi_app(bot):
+    global tg_bot
+    tg_bot = bot
+    from bottle import default_app
+    return default_app()
+
+
+def run_server(bot, **kwargs):
+    wsgi_app(bot)
+    run(**kwargs)
