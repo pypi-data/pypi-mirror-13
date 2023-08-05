9407a7a51fadda7539b3e5758b5c3602144c3c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:20 +0000

    Add package usergrid/0.0.3/usergrid-0.0.3.tar.gz

---
+++ packages/usergrid/usergrid-0.0.3.tar.gz/usergrid-0.0.3/setup.py
@@ -0,0 +1,21 @@
+__author__ = 'Jeff West @ ApigeeCorporation'
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+
+setup(
+    name='usergrid',
+    version=VERSION,
+    description='Usergrid SDK for Python',
+    url='http://usergrid.apache.org',
+    download_url="https://codeload.github.com/jwest-apigee/usergrid-python/zip/v0.0.2",
+    author='Jeff West',
+    author_email='jwest@apigee.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+    }
+)
