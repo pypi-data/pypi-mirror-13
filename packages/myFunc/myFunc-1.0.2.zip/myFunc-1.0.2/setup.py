63e5a2f168d000bfe893ae93d01f00eed66f0a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package myFunc/1.0.2/myFunc-1.0.2.zip

---
+++ packages/myFunc/myFunc-1.0.2.zip/myFunc-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name =  'myFunc',
+	version =  '1.0.2',
+	py_modules = ['myFunc'],
+	author = 'lqjaddls',
+	author_email = 'lqjaddls@126.com',
+	url = 'http://www.headfirstlabs.com',
+	description = '调整参数为 list,indent,level,firstRun',
+	)
