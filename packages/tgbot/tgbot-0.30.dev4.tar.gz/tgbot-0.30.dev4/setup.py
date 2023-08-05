088b5188588a3827250cfe64e7e5d55439800b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:35 +0000

    Add package tgbot/0.30.dev4/tgbot-0.30.dev4.tar.gz

---
+++ packages/tgbot/tgbot-0.30.dev4.tar.gz/tgbot-0.30.dev4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+try:
+    import pypandoc
+    description = pypandoc.convert('tgbot/README.md','rst')
+except (IOError, ImportError):
+    description = ''
+requirements = ["uwsgidecorators >= 1.1.0","requests >= 2.7.0",
+                        "pytz >= 2015.4","feedparser >= 5.2.1",
+                        "redis >= 2.10.3","setuptools >= 18.3.1"]
+setup(name="tgbot",
+      version = "0.30",
+      description="Framework to build a Telegram Bot based on a UWSGI Server",
+      author="Thomas Eberle",
+      author_email="Thomas@tommyelroy.com",
+      url="https://github.com/T-Eberle/tgbot/",
+      license="The MIT License (MIT)",
+      keywords="tgbot bot",
+      packages=find_packages(exclude=["example","dist"]),
+      package_data ={"":["*.ini","*.md","*.txt"]},
+      long_description=description,
+      install_requires=requirements,
+      setup_requires=requirements)
