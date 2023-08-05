616b94b24bb20ac6413980f5d08ec839e24a17d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:55 +0000

    Add package django-mail-templated/2.4.2/django-mail-templated-2.4.2.tar.gz

---
+++ packages/django-mail-templated/django-mail-templated-2.4.2.tar.gz/django-mail-templated-2.4.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+DESCRIPTION = 'Send emails with Django template system'
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+
+setup(
+    name='django-mail-templated',
+    version='2.4.2',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Artem Rizhov',
+    author_email='artem.rizhov@gmail.com',
+    url='https://github.com/artemrizhov/django-mail-templated',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+    test_suite='runtests.runtests',
+)
