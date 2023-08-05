4eeb4df43b04f021108fa8a5c63c2230fefdca0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:36 +0000

    Add package succubus/1.0-17/succubus-1.0-17.tar.gz

---
+++ packages/succubus/succubus-1.0-17.tar.gz/succubus-1.0-17/setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'succubus',
+        version = '1.0-17',
+        description = '''Lightweight Python module for daemonizing''',
+        long_description = '''.. image:: https://travis-ci.org/ImmobilienScout24/succubus.svg
+    :alt: Travis build status image
+    :align: left
+    :target: https://travis-ci.org/ImmobilienScout24/succubus
+
+.. image:: https://coveralls.io/repos/ImmobilienScout24/succubus/badge.svg?branch=master
+  :alt: Coverage status
+  :target: https://coveralls.io/github/ImmobilienScout24/succubus?branch=master
+
+
+========
+succubus
+========
+
+Description
+===========
+succubus is a lightweight python module for a fast and easy creation of
+python daemons.
+
+Examples
+========
+
+.. code-block:: python
+
+    import logging
+    import sys
+
+    from logging.handlers import WatchedFileHandler
+
+    from succubus import Daemon
+
+
+    class MyDaemon(Daemon):
+        def __init__(self, *args, **kwargs):
+            super(MyDaemon, self).__init__(*args, **kwargs)
+
+        def run(self):
+            """Overwrite the run function of the daemon class"""
+            handler = WatchedFileHandler('succubus.log')
+            self.logger = logging.getLogger('succubus')
+            self.logger.addHandler(handler)
+            while True:
+                time.sleep(1)
+                self.logger.warn('Hello world')
+
+
+    def main():
+        daemon = MyDaemon(pid_file='succubus.pid')
+        sys.exit(daemon.action())
+
+
+    if __name__ == '__main__':
+        main()
+''',
+        author = "Stefan Neben, Stefan Nordhausen",
+        author_email = "stefan.neben@immobilienscout24.de, stefan.nordhausen@immobilienscout24.de",
+        license = 'Apache License 2.0',
+        url = 'https://github.com/ImmobilienScout24/succubus',
+        scripts = [],
+        packages = ['succubus'],
+        py_modules = [],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = ['psutil'],
+        dependency_links = [],
+        zip_safe=True,
+        cmdclass={'install': install},
+    )
