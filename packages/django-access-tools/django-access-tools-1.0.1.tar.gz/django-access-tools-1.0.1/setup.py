47dac4d0ec01827e215e1427d07c1a6af8d1f11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package django-access-tools/1.0.1/django-access-tools-1.0.1.tar.gz

---
+++ packages/django-access-tools/django-access-tools-1.0.1.tar.gz/django-access-tools-1.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import access
+
+setup(
+    name='django-access-tools',
+    version=".".join(map(str, access.__version__)),
+    author='Anton Agestam',
+    author_email="msn@antonagestam.se",
+    url='http://antonagestam.github.io/django-access-tools',
+    install_requires=[
+        'Django>=1.8.2',
+    ],
+    description='An abstract access manager for Django.',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+    ],
+)
