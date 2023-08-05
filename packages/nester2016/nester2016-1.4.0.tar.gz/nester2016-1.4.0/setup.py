820841472ffb04d2ec5d5a7b20c952a5b19dd367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:57 +0000

    Add package nester2016/1.4.0/nester2016-1.4.0.tar.gz

---
+++ packages/nester2016/nester2016-1.4.0.tar.gz/nester2016-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			=	'nester2016',
+		version			=	'1.4.0',
+		py_modules		=	['nester2016'],
+		author			=	'Rapadon',
+		author_email	=	'sailftnw@naver.com',
+		url				=	'http://blog.naver.com/sailftnw',
+		description		=	'A simple printer of nested lists',
+	)
