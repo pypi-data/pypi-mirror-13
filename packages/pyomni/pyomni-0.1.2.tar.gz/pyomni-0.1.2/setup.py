6b5ebca6173a245a4fe7a7677a0ff33e4b316522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package pyomni/0.1.2/pyomni-0.1.2.tar.gz

---
+++ packages/pyomni/pyomni-0.1.2.tar.gz/pyomni-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+import pyomni
+
+setup(
+    name=pyomni.__name__,
+    packages=find_packages(exclude=['tests*']),
+    version=pyomni.__version__,
+    author=pyomni.__author__,
+    author_email="taxpon@gmail.com",
+    description="Python library to handle omnifocus tasks.",
+    url=pyomni.__url__,
+    license=pyomni.__license__,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+
+    ]
+)
