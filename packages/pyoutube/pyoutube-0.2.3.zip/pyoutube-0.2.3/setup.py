7b28c3d083c8467ac2a4f57a9c9b1996934a4e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package pyoutube/0.2.3/pyoutube-0.2.3.zip

---
+++ packages/pyoutube/pyoutube-0.2.3.zip/pyoutube-0.2.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return(f.read())
+
+setup(name='pyoutube',
+      version='0.2.3',
+      description='Upload videos to youtube using OAuth2.0 and the youtube API',
+      long_description="asdf",
+      classifieres=['Development Status :: 1 - Alpha',
+                    'Programming Language :: Python :: 3.4'],
+      keywords='youtube upload uploader', 
+      author='lpbrown999',
+      author_email='lpbrown999@gmail.com',
+      packages=['pyoutube'],
+      install_requires = ['google-api-python-client'],
+      zip_safe=False)
