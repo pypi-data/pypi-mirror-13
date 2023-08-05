1b6b8439dfade3aec948c7d8346b91e23d064a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:03 +0000

    Add package mengxing/1.0.0/mengxing-1.0.0.zip

---
+++ packages/mengxing/mengxing-1.0.0.zip/mengxing-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name        ='mengxing',
+    version     ='1.0.0',
+    py_modules  =['nester'],
+    author      ='liqin',
+    author_email='hfpython@headfirstlabs.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='a simple printer of nested lists',
+    )
