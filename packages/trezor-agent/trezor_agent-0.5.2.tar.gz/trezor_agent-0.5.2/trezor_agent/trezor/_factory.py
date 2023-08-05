de881ca0687e37a5dc8a1052c1c81bbc126a8066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package trezor-agent/0.5.2/trezor_agent-0.5.2.tar.gz

---
+++ packages/trezor-agent/trezor_agent-0.5.2.tar.gz/trezor_agent-0.5.2/trezor_agent/trezor/_factory.py
@@ -0,0 +1,23 @@
+''' Thin wrapper around trezorlib. '''
+
+
+def client():
+    # pylint: disable=import-error
+    from trezorlib.client import TrezorClient
+    from trezorlib.transport_hid import HidTransport
+    from trezorlib.messages_pb2 import PassphraseAck
+
+    devices = list(HidTransport.enumerate())
+    if len(devices) != 1:
+        msg = '{:d} Trezor devices found'.format(len(devices))
+        raise IOError(msg)
+
+    t = TrezorClient(HidTransport(devices[0]))
+    t.callback_PassphraseRequest = lambda msg: PassphraseAck(passphrase='')
+    return t
+
+
+def identity_type(**kwargs):
+    # pylint: disable=import-error
+    from trezorlib.types_pb2 import IdentityType
+    return IdentityType(**kwargs)
