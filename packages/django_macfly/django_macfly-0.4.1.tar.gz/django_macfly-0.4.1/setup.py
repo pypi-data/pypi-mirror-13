482a65cb7efe5bc8fb2000e25e794e1f8445b456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:13 +0000

    Add package django_macfly/0.4.1/django_macfly-0.4.1.tar.gz

---
+++ packages/django_macfly/django_macfly-0.4.1.tar.gz/django_macfly-0.4.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION = '0.4.1'
+
+setup(
+  name = 'django_macfly',
+  packages = ['django_macfly',
+              "django_macfly.management",
+              "django_macfly.management.commands"],
+  version = VERSION,
+  description = 'Django helpers to run SQL schema migrations ahead of time.',
+  author = 'Bruno Dupuis',
+  author_email = 'bruno.dupuis@people-doc.com',
+  url = 'https://github.com/novafloss/django-MacFly',
+  download_url = 'https://github.com/novafloss/django-MacFly/tarball/%s' % VERSION,
+  keywords = ['django', 'migrations', 'DBA'],
+  classifiers = [],
+)
+
