57ce154d1c988677d4646525b9392e2766af6d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package twittergeo/0.2.0/twittergeo-0.2.0-py2.py3-none-any.whl

---
+++ packages/twittergeo/twittergeo-0.2.0-py2.py3-none-any.whl/twittergeo/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of twittergeo.
+# https://github.com/fitnr/twittergeo
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
+__version__ = '0.2.0'
+__author__ = 'Neil Freeman'
+__license__ = 'GPL-3.0'
+__all__ = ['twittergeo']
+
+from .twittergeo import twittergeo, collection, feature
