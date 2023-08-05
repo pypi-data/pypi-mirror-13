2fdcf19fe6a48423737e63038304ecd4aa70206e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:08 +0000

    Add package mdownload/0.85/mdownload-0.85.tar.gz

---
+++ packages/mdownload/mdownload-0.85.tar.gz/mdownload-0.85/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'mdownload',
+  packages = ['mdownload'], 
+  version = '0.85',
+  description = 'It is a small library to multi-threaded downloads.',
+  author = 'Diego Siqueira',
+  author_email = 'dieg0@live.com',
+  url = 'https://github.com/DiSiqueira/mdownload', 
+  download_url = 'https://github.com/DiSiqueira/mdownload/tarball/0.85', 
+  keywords = ['download', 'thread', 'speed', 'resume', 'multi', 'simple'],
+  classifiers = [],
+  license='MIT',
+)
