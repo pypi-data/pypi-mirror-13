1f9f5795ec1413906172d9d7931707b071a24d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:10 +0000

    Add package tfatool/1.3.3/tfatool-1.3.3.tar.gz

---
+++ packages/tfatool/tfatool-1.3.3.tar.gz/tfatool-1.3.3/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+import tfatool._version
+
+
+version = tfatool._version.__version__
+url = "https://github.com/TadLeonard/tfatool"
+download = "{}/archive/{}.tar.gz".format(url, version)
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="tfatool",
+      version=tfatool._version.__version__,
+      scripts=["flashair-util"],
+      install_requires=["requests>=2.9.0", "tqdm>=3.7.1"],
+      licence="MIT",
+      packages=["tfatool"],
+      description=("Tools for syncing files with the "
+                   "Toshiba FlashAir wireless SD card"),
+      long_description=read("README.md"),
+      include_package_data=True,
+      package_data={"": "README.md"},
+      author="Tad Leonard",
+      author_email="tadfleonard@gmail.com",
+      keywords="wireless sd card sync toshiba flashair", 
+      url=url,
+      download_url=download,
+)
+
