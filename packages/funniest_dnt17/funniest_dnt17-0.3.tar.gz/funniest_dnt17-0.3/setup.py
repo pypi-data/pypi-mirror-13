6e6082913c3aa5de8975b81ca5f2f808a32abc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:13 +0000

    Add package funniest_dnt17/0.3/funniest_dnt17-0.3.tar.gz

---
+++ packages/funniest_dnt17/funniest_dnt17-0.3.tar.gz/funniest_dnt17-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='funniest_dnt17',
+        version='0.3',
+        description='The funniest joke in the world',
+        long_description='Really, the funniest around.',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Text Processing :: Linguistic',
+        ],
+        keywords='funniest joke comedy flying circus tutorial',
+        url='https://github.com/dnt17/funniest_dnt17',
+        author='Devin Turner',
+        author_email='devinturner17@gmail.com',
+        license='MIT',
+        packages=['funniest_dnt17'],
+        install_requires=[
+            'markdown',
+        ],
+        include_package_data=True,
+        zip_save=False)
