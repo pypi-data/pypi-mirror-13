d2ba4bf471dcdc193a7e2a71a65420e45691308c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package django-toolware/0.0.10/django-toolware-0.0.10.tar.gz

---
+++ packages/django-toolware/django-toolware-0.0.10.tar.gz/django-toolware-0.0.10/toolware/compat.py
@@ -0,0 +1,27 @@
+import six
+
+if six.PY2:
+    import basestring
+elif six.PY3:
+    basestring = (str, bytes)
+
+if six.PY2:
+    import urlparse
+elif six.PY3:
+    import urllib.parse as urlparse
+
+try:
+    from .urlparse import parse_qs
+except ImportError:
+    from cgi import parse_qs
+
+try:
+    import HTMLParser
+    unescape = HTMLParser.HTMLParser()
+except ImportError:
+    from html import unescape
+
+if six.PY2:
+    import urllib
+elif six.PY3:
+    import urllib.parse as urllib
