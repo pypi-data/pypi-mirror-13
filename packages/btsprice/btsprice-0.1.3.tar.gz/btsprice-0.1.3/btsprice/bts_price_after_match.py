38ac7dd4a88d5eb0811ab06582c689dbc50d14fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package btsprice/0.1.3/btsprice-0.1.3.tar.gz

---
+++ packages/btsprice/btsprice-0.1.3.tar.gz/btsprice-0.1.3/btsprice/bts_price_after_match.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+from btsprice.exchanges import Exchanges
+import time
+from math import fabs
+from btsprice.misc import get_median
+# from pprint import pprint
+
+
+class BTSPriceAfterMatch(object):
+
+    def __init__(self, bts_market=None, exchanges=None):
+        self.bts_market = bts_market
+        if exchanges:
+            self.exchanges = exchanges
+        else:
+            self.exchanges = Exchanges()
+        self.order_book = {}
+        self.timeout = 300  # order book can't use after 300 seconds
+        self.timestamp_rate_yahoo = 0
+        self.timestamp = 0
+        self.rate_yahoo = {}
+        self.rate_btc = {"BTC": 1.0}
+        self.order_types = ["bids", "asks"]
+        self.need_cover_order = True
+
+    def set_orderbook_timeout(self, timeout):
+        self.timeout = timeout
+
+    def get_rate_from_yahoo(self):
+        _rate_yahoo = self.exchanges.fetch_from_yahoo()
+        if _rate_yahoo:
+            if "USD" not in self.rate_btc:
+                self.rate_btc["USD"] = 1/_rate_yahoo["USD"]["BTC"]
+                self.rate_btc["CNY"] = \
+                    _rate_yahoo["USD"]["CNY"]/_rate_yahoo["USD"]["BTC"]
+
+            del _rate_yahoo["USD"]["BTC"]
+            del _rate_yahoo["USD"]["CNY"]
+            self.timestamp_rate_yahoo = self.timestamp
+            self.rate_yahoo = _rate_yahoo
+
+    def change_order_with_rate(self, _order_book, rate):
+        for order_type in self.order_types:
+            for order in _order_book[order_type]:
+                order[0] = order[0] * rate
+        return True
+
+    def set_need_cover(self, need_cover):
+        self.need_cover_order = need_cover
+
+    def get_order_book_from_wallet(self):
+        if self.bts_market is None:
+            return
+        need_cover = self.need_cover_order
+        _order_book = self.bts_market.get_order_book(
+            "CNY", "BTS", cover=need_cover)
+        if _order_book:
+            self.change_order_with_rate(_order_book, self.rate_btc["CNY"])
+            self.order_book["wallet_cny"] = _order_book
+            self.order_book["wallet_cny"]["asset"] = "CNY"
+            self.order_book["wallet_cny"]["timestamp"] = self.timestamp
+        _order_book = self.bts_market.get_order_book(
+            "USD", "BTS", cover=need_cover)
+        if _order_book:
+            self.change_order_with_rate(_order_book, self.rate_btc["USD"])
+            self.order_book["wallet_usd"] = _order_book
+            self.order_book["wallet_usd"]["asset"] = "USD"
+            self.order_book["wallet_usd"]["timestamp"] = self.timestamp
+
+    def get_order_book_from_exchanges(self):
+        _order_book = self.exchanges.fetch_from_poloniex("btc", "bts")
+        if _order_book:
+            self.order_book["poloniex_btc"] = _order_book
+            self.order_book["poloniex_btc"]["asset"] = "BTC"
+            self.order_book["poloniex_btc"]["timestamp"] = self.timestamp
+        _order_book = self.exchanges.fetch_from_btc38()
+        if _order_book:
+            self.change_order_with_rate(_order_book, self.rate_btc["CNY"])
+            self.order_book["btc38_cny"] = _order_book
+            self.order_book["btc38_cny"]["asset"] = "CNY"
+            self.order_book["btc38_cny"]["timestamp"] = self.timestamp
+        _order_book = self.exchanges.fetch_from_btc38("btc", "bts")
+        if _order_book:
+            self.order_book["btc38_btc"] = _order_book
+            self.order_book["btc38_btc"]["asset"] = "BTC"
+            self.order_book["btc38_btc"]["timestamp"] = self.timestamp
+
+        _order_book = self.exchanges.fetch_from_yunbi()
+        if _order_book:
+            self.change_order_with_rate(_order_book, self.rate_btc["CNY"])
+            self.order_book["yunbi_cny"] = _order_book
+            self.order_book["yunbi_cny"]["asset"] = "CNY"
+            self.order_book["yunbi_cny"]["timestamp"] = self.timestamp
+        _order_book = self.exchanges.fetch_from_bter()
+        if _order_book:
+            self.change_order_with_rate(_order_book, self.rate_btc["CNY"])
+            self.order_book["bter_cny"] = _order_book
+            self.order_book["bter_cny"]["asset"] = "CNY"
+            self.order_book["bter_cny"]["timestamp"] = self.timestamp
+
+    def test_valid(self):
+        valid_price_queue = []
+        valid_price_dict = {}
+        for market in self.order_book:
+            _price = (
+                self.order_book[market]["bids"][0][0] + self.
+                order_book[market]["asks"][0][0])/2
+            valid_price_queue.append(_price)
+            valid_price_dict[market] = _price
+        valid_price = get_median(valid_price_queue)
+        counter = 0
+        for market in self.order_book:
+            change = fabs((valid_price_dict[market] - valid_price)/valid_price)
+            # if offset more than 10%, this market is invalid
+            if change > 0.1:
+                self.order_book[market]["valid"] = False
+            else:
+                self.order_book[market]["valid"] = True
+                counter += 1
+        # all market is invalid if we got valid market less than 2
+        if counter < 2:
+            for market in self.order_book:
+                self.order_book[market]["valid"] = False
+
+    def get_rate_btc(self):
+        price_btc = self.exchanges.get_btcprice_in_usd()
+        if price_btc:
+            self.rate_btc["USD"] = 1/price_btc
+        price_btc = self.exchanges.get_btcprice_in_cny()
+        if price_btc:
+            self.rate_btc["CNY"] = 1/price_btc
+        for base in ["USD", "CNY"]:
+            for asset in self.rate_yahoo[base]:
+                self.rate_btc[asset] = \
+                    self.rate_yahoo[base][asset] * self.rate_btc[base]
+
+    def get_order_book_all(self):
+        self.timestamp = time.time()
+        self.order_book_all = {"bids": [], "asks": []}
+        if self.timestamp_rate_yahoo == 0:
+            self.get_rate_from_yahoo()
+        self.get_rate_btc()
+        self.get_order_book_from_exchanges()
+        self.get_order_book_from_wallet()
+        self.test_valid()
+        for market in self.order_book:
+            if self.timestamp - self.order_book[market]["timestamp"] \
+                    >= self.timeout:
+                continue
+            if not self.order_book[market]["valid"]:
+                continue
+            for order_type in self.order_types:
+                self.order_book_all[order_type].extend(
+                    self.order_book[market][order_type])
+        self.order_book_all["bids"] = sorted(
+            self.order_book_all["bids"], reverse=True)
+        self.order_book_all["asks"] = sorted(self.order_book_all["asks"])
+
+    def is_order_book_valid(self):
+        if len(self.order_book_all["bids"]) == 0 or \
+                len(self.order_book_all["asks"]) == 0:
+            return False
+        else:
+            return True
+
+    def get_spread_order_book(self, spread=0.0):
+        order_bids = []
+        order_asks = []
+        for order in self.order_book_all["bids"]:
+            order_bids.append([order[0]*(1 + spread), order[1]])
+        for order in self.order_book_all["asks"]:
+            order_asks.append([order[0]*(1 - spread), order[1]])
+        return order_bids, order_asks
+
+    def get_price_list(self, order_bids, order_asks):
+        price_list = []
+        for order in order_bids:
+            if order[0] < order_asks[0][0]:
+                break
+            if len(price_list) == 0 or order[0] != price_list[-1]:
+                price_list.append(order[0])
+        for order in order_asks:
+            if order[0] < order_bids[0][0]:
+                break
+            if len(price_list) == 0 or order[0] != price_list[-1]:
+                price_list.append(order[0])
+        price_list = sorted(price_list)
+        return price_list
+
+    def get_match_result(self, order_bids, order_asks, price_list):
+        bid_volume = ask_volume = 0
+        median_price = get_median(price_list)
+        for order in order_bids:
+            if order[0] < median_price:
+                break
+            bid_volume += order[1]
+        for order in order_asks:
+            if order[0] > median_price:
+                break
+            ask_volume += order[1]
+        return ([bid_volume, ask_volume, median_price])
+
+    def get_real_price(self, spread=0.0):
+        if not self.is_order_book_valid():
+            return [0.0, 0.0, None]
+        order_bids, order_asks = self.get_spread_order_book(spread)
+        price_list = self.get_price_list(order_bids, order_asks)
+        if len(price_list) < 1:
+            return [0.0, 0.0, (order_bids[0][0] + order_asks[0][0]) / 2]
+        match_result = []
+        while True:
+            bid_volume, ask_volume, median_price = self.get_match_result(
+                order_bids, order_asks, price_list)
+            # we need to find a price, which can match the max volume
+            match_result.append([min(bid_volume, ask_volume),
+                                 -(bid_volume+ask_volume), median_price])
+            if len(price_list) <= 1:
+                break
+            if(bid_volume <= ask_volume):
+                price_list = price_list[:int(len(price_list) / 2)]
+            else:
+                price_list = price_list[int(len(price_list) / 2):]
+
+        match_result = sorted(match_result, reverse=True)
+        # pprint(match_result)
+        return match_result[0]
+
+    def get_valid_depth(self, price, spread=0.0):
+        valid_depth = {}
+        bid_price = price / (1+spread)
+        ask_price = price / (1-spread)
+        for market in self.order_book:
+            if self.timestamp - self.order_book[market]["timestamp"] \
+                    >= self.timeout:
+                continue
+            if not self.order_book[market]["valid"]:
+                continue
+            quote = self.order_book[market]["asset"]
+            valid_depth[market] = {
+                "bid_price": bid_price / self.rate_btc[quote], "bid_volume": 0,
+                "ask_price": ask_price / self.rate_btc[quote], "ask_volume": 0}
+            for order in sorted(self.order_book[market]["bids"], reverse=True):
+                if order[0] < bid_price:
+                    break
+                # valid_depth[market]["bid_price"] = \
+                #     order[0] * self.rate_btc[quote]
+                valid_depth[market]["bid_volume"] += order[1]
+            for order in sorted(self.order_book[market]["asks"]):
+                if order[0] > ask_price:
+                    break
+                # valid_depth[market]["ask_price"] = \
+                #     order[0] * self.rate_btc[quote]
+                valid_depth[market]["ask_volume"] += order[1]
+        return valid_depth
+
+if __name__ == "__main__":
+    bts_price = BTSPriceAfterMatch()
+    # updat rate cny
+    bts_price.get_rate_from_yahoo()
+
+    # get all order book
+    bts_price.get_order_book_all()
+    volume, volume_sum, real_price = bts_price.get_real_price(
+        spread=0.01)
+    if real_price:
+        valid_depth = bts_price.get_valid_depth(
+            price=real_price,
+            spread=0.01)
+        price_cny = real_price / bts_price.rate_btc["CNY"]
+        print(
+            "price is %.5f CNY/BTS, volume is %.3f" % (price_cny, volume))
+        print("efficent depth : %s" % valid_depth)
+    else:
+        print("can't get valid market order")
---
+++ packages/btsprice/btsprice-0.1.3.tar.gz/btsprice-0.1.3/btsprice/feedprice.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from btsprice.bts_price_after_match import BTSPriceAfterMatch
+from btsprice.feedapi import FeedApi
+import time
+import logging
+import logging.handlers
+import os
+from prettytable import PrettyTable
+from datetime import datetime
+from math import fabs
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+
+class FeedPrice(object):
+
+    def __init__(self, config=None):
+        self.init_config(config)
+        self.init_bts_price()
+        self.setup_log()
+        self.init_mpa_info()
+        self.sample = self.config["price_limit"]["filter_minute"] / \
+            self.config["timer_minute"]
+        if self.sample < 1:
+            self.sample = 1
+        self.feedapi = FeedApi(config)
+
+    def init_config(self, config):
+        if config:
+            self.config = config
+            return
+        config = {}
+        config["witness"] = None
+        config["timer_minute"] = 3
+        config["max_update_hours"] = 23.5
+        config["price_limit"] = {
+            "change_min": 0.5, "change_max": 50, "spread": 0.01,
+            "filter_minute": 30}
+        config["market_weight"] = {
+            "poloniex_btc": 1, "yunbi_cny": 1, "btc38_cny": 1, "btc38_btc": 1}
+
+        self.config = config
+
+    def init_bts_price(self):
+        self.bts_price = BTSPriceAfterMatch()
+        # don't use cover order, because it's feed price related,
+        # if there is a big order, feed price will down without stop
+        self.bts_price.set_need_cover(False)
+
+    def setup_log(self):
+        # Setting up Logger
+        self.logger = logging.getLogger('bts')
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s[%(levelname)s]: %(message)s')
+        fh = logging.handlers.RotatingFileHandler("/tmp/bts_delegate_task.log")
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+    def init_mpa_info(self):
+        peg_asset_list = ["KRW", "BTC", "SILVER", "GOLD", "TRY",
+                          "SGD", "HKD", "RUB", "SEK", "NZD", "CNY",
+                          "MXN", "CAD", "CHF", "AUD", "GBP", "JPY",
+                          "EUR", "USD", "SHENZHEN", "NASDAQC", "NIKKEI",
+                          "HANGSENG", "SHANGHAI", "TCNY"]
+        self.price_queue = {}
+        for asset in peg_asset_list:
+            self.price_queue[asset] = []
+        self.time_publish_feed = 0
+        self.adjust_scale = 1.00
+
+    def fetch_bts_price(self):
+        # updat rate cny
+        self.bts_price.get_rate_from_yahoo()
+
+        # get all order book
+        self.bts_price.get_order_book_all()
+
+        self.bts_price.rate_btc["TCNY"] = self.bts_price.rate_btc["CNY"]
+        # can add weight here
+        for order_type in self.bts_price.order_types:
+            for market in self.bts_price.order_book:
+                if market not in self.config["market_weight"]:
+                    _weight = 0.0
+                else:
+                    _weight = self.config["market_weight"][market]
+                for _order in self.bts_price.order_book[market][order_type]:
+                    _order[1] *= _weight
+
+        # calculate real price
+        volume, volume_sum, real_price = self.bts_price.get_real_price(
+            spread=self.config["price_limit"]["spread"])
+        if real_price is None:
+            return real_price, volume
+        self.valid_depth = self.bts_price.get_valid_depth(
+            price=real_price,
+            spread=self.config["price_limit"]["spread"])
+        price_cny = real_price / self.bts_price.rate_btc["CNY"]
+        self.logger.info("fetch price is %.5f CNY/BTS, volume is %.3f",
+                         price_cny, volume)
+        self.logger.info("efficent depth : %s" % self.valid_depth)
+        return real_price, volume
+
+    # these MPA's precision is 100, it's too small,
+    # have to change the price
+    # but we should fixed these at BTS2.0
+    def patch_nasdaqc(self, price):
+        if "SHENZHEN" in price:
+            price["SHENZHEN"] /= price["CNY"]
+        if "SHANGHAI" in price:
+            price["SHANGHAI"] /= price["CNY"]
+        if "NASDAQC" in price:
+            price["NASDAQC"] /= price["USD"]
+        if "NIKKEI" in price:
+            price["NIKKEI"] /= price["JPY"]
+        if "HANGSENG" in price:
+            price["HANGSENG"] /= price["HKD"]
+
+    def price_filter(self, bts_price_in_btc):
+        self.filter_price = self.get_average_price(bts_price_in_btc)
+
+    def get_median_price(self, bts_price_in_btc):
+        median_price = {}
+        for asset in self.price_queue:
+            if asset not in self.bts_price.rate_btc or \
+                    self.bts_price.rate_btc[asset] is None:
+                continue
+            self.price_queue[asset].append(bts_price_in_btc
+                                           / self.bts_price.rate_btc[asset])
+            if len(self.price_queue[asset]) > self.sample:
+                self.price_queue[asset].pop(0)
+            median_price[asset] = sorted(
+                self.price_queue[asset])[int(len(self.price_queue[asset]) / 2)]
+        self.patch_nasdaqc(median_price)
+        return median_price
+
+    def get_average_price(self, bts_price_in_btc):
+        average_price = {}
+        for asset in self.price_queue:
+            if asset not in self.bts_price.rate_btc or \
+                    self.bts_price.rate_btc[asset] is None:
+                continue
+            self.price_queue[asset].append(bts_price_in_btc
+                                           / self.bts_price.rate_btc[asset])
+            if len(self.price_queue[asset]) > self.sample:
+                self.price_queue[asset].pop(0)
+            average_price[asset] = sum(
+                self.price_queue[asset])/len(self.price_queue[asset])
+        self.patch_nasdaqc(average_price)
+        return average_price
+
+    def display_depth(self, volume):
+        t = PrettyTable([
+            "market", "bid price", "bid_volume", "ask price", "ask_volume"])
+        t.align = 'r'
+        t.border = True
+        for market in sorted(self.valid_depth):
+            _bid_price = "%.8f" % self.valid_depth[market]["bid_price"]
+            _bid_volume = "{:,.0f}".format(
+                self.valid_depth[market]["bid_volume"])
+            _ask_price = "%.8f" % self.valid_depth[market]["ask_price"]
+            _ask_volume = "{:,.0f}".format(
+                self.valid_depth[market]["ask_volume"])
+            t.add_row([
+                market, _bid_price, _bid_volume, _ask_price, _ask_volume])
+        print(t.get_string())
+
+    def display_price(self):
+        t = PrettyTable([
+            "asset", "current(/BTC)", "current(/BTS)", "current(BTS/)",
+            "median(/BTS)", "median(BTS/)", "my feed"])
+        t.align = 'r'
+        t.border = True
+        for asset in sorted(self.filter_price):
+            _price_btc = "%.3f" % (1/self.bts_price.rate_btc[asset])
+            _price_bts1 = "%.8f" % self.price_queue[asset][-1]
+            _price_bts2 = "%.3f" % (1/self.price_queue[asset][-1])
+            _median_bts1 = "%.8f" % self.filter_price[asset]
+            _median_bts2 = "%.3f" % (1/self.filter_price[asset])
+            if self.feedapi.my_feeds and asset in self.feedapi.my_feeds:
+                _my_feed = "%.8f" % self.feedapi.my_feeds[asset]["price"]
+            else:
+                _my_feed = 'x'
+            t.add_row([
+                asset, _price_btc, _price_bts1,
+                _price_bts2, _median_bts1, _median_bts2, _my_feed])
+        print(t.get_string())
+
+    def task_get_price(self):
+        bts_price_in_btc, volume = self.fetch_bts_price()
+        if bts_price_in_btc is None or volume <= 0.0:
+            return
+        self.price_filter(bts_price_in_btc)
+        os.system("clear")
+        cur_t = time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(time.time()))
+        bts_price_in_cny = bts_price_in_btc / self.bts_price.rate_btc["CNY"]
+        print("[%s] efficent price: %.5f CNY/BTS, depth: %s BTS" % (
+            cur_t, bts_price_in_cny, "{:,.0f}".format(volume)))
+        self.display_depth(volume)
+        print()
+        self.display_price()
+
+    def check_publish(self, asset_list, my_feeds, real_price):
+        need_publish = {}
+        for asset in asset_list:
+            change = fabs(my_feeds[asset]["price"] - real_price[asset]) * \
+                100.0 / my_feeds[asset]["price"]
+            if change >= self.config["price_limit"]["change_max"]:
+                continue
+            if asset not in my_feeds:
+                need_publish[asset] = real_price[asset]
+                continue
+            if (datetime.utcnow() - my_feeds[asset]["timestamp"]). \
+                    total_seconds() > \
+                    self.feedapi.asset_info[asset]["feed_lifetime_sec"] - 600:
+                need_publish[asset] = real_price[asset]
+                continue
+            if change > self.config["price_limit"]["change_min"]:
+                need_publish[asset] = real_price[asset]
+                continue
+        return need_publish
+
+    def task_publish_price(self):
+        if not self.config["witness"]:
+            return
+        self.feedapi.fetch_feed()
+        feed_need_publish = self.check_publish(
+            self.feedapi.asset_list, self.feedapi.my_feeds, self.filter_price)
+        if feed_need_publish:
+            self.logger.info("publish feeds: %s" % feed_need_publish)
+            self.feedapi.publish_feed(feed_need_publish)
+
+    def excute(self):
+        while True:
+            try:
+                self.task_get_price()
+                self.task_publish_price()
+            except Exception as e:
+                print(e)
+                self.logger.exception(e)
+            time.sleep(int(self.config["timer_minute"]*60))
+
+if __name__ == '__main__':
+    feedprice = FeedPrice()
+    feedprice.excute()
