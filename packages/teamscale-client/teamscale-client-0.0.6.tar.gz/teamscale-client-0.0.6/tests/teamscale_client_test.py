801a7ab382449bab8203a044e4b663021f9dd44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:25 +0000

    Add package teamscale-client/0.0.6/teamscale-client-0.0.6.tar.gz

---
+++ packages/teamscale-client/teamscale-client-0.0.6.tar.gz/teamscale-client-0.0.6/tests/teamscale_client_test.py
@@ -0,0 +1,46 @@
+import requests
+import responses
+import re
+import datetime
+
+from teamscale_client.teamscale_client import TeamscaleClient
+
+URL = "http://localhost:8080"
+
+def get_client():
+    return TeamscaleClient(URL, "admin", "admin", "foo")
+
+def get_project_service_mock(service_id):
+    return re.compile(r'%s/p/foo/%s/.*' % (URL, service_id))
+
+def get_global_service_mock(service_id):
+    return re.compile(r'%s/%s/.*' % (URL, service_id))
+
+@responses.activate
+def test_put():
+    responses.add(responses.PUT, 'http://localhost:8080',
+                      body='success', status=200)
+    resp = get_client().put("http://localhost:8080", "[]", {})
+    assert resp.text == "success"
+
+@responses.activate
+def test_upload_findings():
+    responses.add(responses.PUT, get_project_service_mock('add-external-findings'),
+                      body='success', status=200)
+    resp = get_client().upload_findings([], datetime.datetime.now(), "Test message", "partition-name")
+    assert resp.text == "success"
+
+@responses.activate
+def test_upload_metrics():
+    responses.add(responses.PUT, get_project_service_mock('add-external-metrics'),
+                      body='success', status=200)
+    resp = get_client().upload_metrics([], datetime.datetime.now(), "Test message", "partition-name")
+    assert resp.text == "success"
+
+@responses.activate
+def test_upload_metric_description():
+    responses.add(responses.PUT, get_global_service_mock('add-external-metric-description'),
+                      body='success', status=200)
+    resp = get_client().upload_metric_definitions([])
+    assert resp.text == "success"
+
