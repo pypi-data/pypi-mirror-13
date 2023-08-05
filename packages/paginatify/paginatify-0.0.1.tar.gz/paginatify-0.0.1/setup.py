e1ba95b179d46447c90646ba6c14222593c752e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package paginatify/0.0.1/paginatify-0.0.1.tar.gz

---
+++ packages/paginatify/paginatify-0.0.1.tar.gz/paginatify-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='paginatify',
+        version='0.0.1',
+        packages=['paginatify'],
+        zip_safe=False,
+        install_requires=[]
+)
