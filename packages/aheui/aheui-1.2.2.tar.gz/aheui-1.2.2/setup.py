39d0f35f4194990148a96778df8744bd520809d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:07 +0000

    Add package aheui/1.2.2/aheui-1.2.2.tar.gz

---
+++ packages/aheui/aheui-1.2.2.tar.gz/aheui-1.2.2/setup.py
@@ -0,0 +1,53 @@
+from __future__ import with_statement
+
+from setuptools import setup
+
+
+def get_version():
+    from aheui import version
+    return version.VERSION
+
+
+def get_readme():
+    try:
+        with open('README.md') as f:
+            return f.read().strip()
+    except IOError:
+        return ''
+
+
+setup(
+    name='aheui',
+    version=get_version(),
+    description='Aheui compiler & assembler toolkit.',
+    long_description=get_readme(),
+    author='Jeong YunWon',
+    author_email='aheui@youknowone.org',
+    url='https://github.com/aheui/rpaheui',
+    packages=(
+        'aheui',
+        'aheui/int',
+    ),
+    package_data={
+        'aheui': ['version.py']
+    },
+    install_requires=[
+    ],
+    tests_require=[
+        'flake8', 'tox', 'pytest',
+    ],
+    scripts=[
+        'bin/aheui-py',
+        'bin/aheui',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
