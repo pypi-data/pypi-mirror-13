94f8a71e34dc7b40d4831a5906eb7fccb469d9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package SuperGLU/0.1.5/SuperGLU-0.1.5.tar.gz

---
+++ packages/SuperGLU/SuperGLU-0.1.5.tar.gz/SuperGLU-0.1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'SuperGLU',
+  packages = find_packages(),
+  version = '0.1.5',
+  description = 'Base Generalized Learning Utilities (GLU) library for communicating data between different learning technologies and intelligent systems.',
+  author = 'Benjamin D. Nye',
+  author_email = 'benjamin.nye@gmail.com',
+  url = 'https://github.com/GeneralizedLearningUtilities/SuperGLU',
+  download_url = 'https://github.com/GeneralizedLearningUtilities/SuperGLU/archive/0.1.tar.gz',
+  include_package_data = True,
+  keywords = ['ITS', "Adaptive Learning", 'Messaging', 'HTML5', "Websockets", "Service"],
+  classifiers = [          
+		  "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+		  "Programming Language :: JavaScript",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Science/Research"],
+)
