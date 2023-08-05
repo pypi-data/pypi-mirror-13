a641a2dee11f312b235ebf672a8b86742587857a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package django_ballads/0.3/django_ballads-0.3.tar.gz

---
+++ packages/django_ballads/django_ballads-0.3.tar.gz/django_ballads-0.3/setup.py
@@ -0,0 +1,30 @@
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+PACKAGE = 'django_ballads'
+VERSION = '0.3'
+
+setup(
+    name=PACKAGE, version=VERSION,
+    description="Django library for coordinating multi-system transactions (eg database, filesystem, remote API calls).",
+    packages=[
+        'django_ballads',
+    ],
+    license='MIT',
+    author='James Aylett',
+    author_email='james@tartarus.org',
+    install_requires=[
+        'Django~=1.8.0',
+    ],
+    url = 'https://github.com/jaylett/django-ballads',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
