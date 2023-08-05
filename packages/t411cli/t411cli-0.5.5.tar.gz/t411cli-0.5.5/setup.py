35c37361f0c4eb4ea784d02d5adb500549ee5f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:07 +0000

    Add package t411cli/0.5.5/t411cli-0.5.5.tar.gz

---
+++ packages/t411cli/t411cli-0.5.5.tar.gz/t411cli-0.5.5/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from t411cli import __version__
+
+setup(
+        name='t411cli',
+        version=__version__,
+        description='Lightweight command line interface for T411 (french torrent website)',
+        long_description='Documentation is available `here <https://github.com/Xide/t411cli/blob/master/README.md>`_',
+        url='https://github.com/Xide/t411cli',
+        author='Germain Gau',
+        author_email='germain.gau@gmail.com',
+        license='THE BEER-WARE LICENSE',
+        packages=['t411cli'],
+        entry_points={
+            'console_scripts': ['t411=t411cli.t411cli:main'],
+        },
+        zip_safe=False,
+        test_suite='tests',
+        install_requires=[
+            't411api>=0.1.1',
+            'colorama'
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: End Users/Desktop',
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.5",
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Programming Language :: Python',
+            'Operating System :: Unix'
+        ]
+)
