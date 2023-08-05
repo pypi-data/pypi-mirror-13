e86a877ff5506e5c43937c73a9a1a0aceba1c971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package pypavlok/0.3/pypavlok-0.3.tar.gz

---
+++ packages/pypavlok/pypavlok-0.3.tar.gz/pypavlok-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pypavlok',
+      version='0.3',
+      description='Unofficial Bluetooth controller for Pavlok',
+      url='http://github.com/flagist0/pypavlok',
+      author='Alexander Presnyakov',
+      author_email='flagist0@gmail.com',
+      license='MIT',
+      packages=['pypavlok'],
+      install_requires=['gattlib>=0.20150805'],
+      include_package_data=True,
+      zip_safe=False)
