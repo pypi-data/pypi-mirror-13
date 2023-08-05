7580b3386c701f7b9135204656ab463586bd1d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package pylend/0.0.4/pylend-0.0.4.tar.gz

---
+++ packages/pylend/pylend-0.0.4.tar.gz/pylend-0.0.4/pylend/__init__.py
@@ -0,0 +1,6 @@
+from .lendingclub_connection import LendingClub_Connection
+from .exceptions import (AuthorizationException,
+                         ResourceNotFoundException,
+                         ExecutionFailureException,
+                         UnexpectedStatusCodeException)
+from .loans import Loans
