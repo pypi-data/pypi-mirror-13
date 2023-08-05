923d125513dba602baab394b4db7479a4515b28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:25 +0000

    Add package sphericalmercator/0.0.2/sphericalmercator-0.0.2.tar.gz

---
+++ packages/sphericalmercator/sphericalmercator-0.0.2.tar.gz/sphericalmercator-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='sphericalmercator',
+    version='0.0.2',
+    py_modules = ['sphericalmercator',],
+    long_description=open('README.rst').read(),
+    url='https://bitbucket.org/lighter/spherical-mercator/',
+)
