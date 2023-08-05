41fbf50156f386391bd46930cc916b9639a540ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:23 +0000

    Add package stronglib/0.1.3/stronglib-0.1.3-py2.py3-none-any.whl

---
+++ packages/stronglib/stronglib-0.1.3-py2.py3-none-any.whl/strongarm/__init__.py
@@ -0,0 +1,19 @@
+"""
+stronglib - a Python library for the STRONGARM API
+
+"""
+
+
+__author__ = 'Percipient Networks, LLC'
+__version__ = '0.1.3'
+__license__ = 'Apache 2.0'
+
+
+host = 'https://strongarm.io'
+api_key = None
+api_version = '0.1.0'  # Developers generally should not change this.
+
+
+from strongarm.common import (StrongarmException, StrongarmHttpError,
+                              StrongarmUnauthorized)
+from strongarm.resources import Domain
