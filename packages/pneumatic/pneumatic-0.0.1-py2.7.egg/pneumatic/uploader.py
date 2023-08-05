b3b718804641a275d2066b333007b38f4b9cedcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package pneumatic/0.0.1/pneumatic-0.0.1-py2.7.egg

---
+++ packages/pneumatic/pneumatic-0.0.1-py2.7.egg/pneumatic/uploader.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import json
+import requests
+from .db import Database
+from .utils import Utils
+
+
+class DocumentCloudUploader(object):
+    """
+    The main thing.
+    """
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.base_uri = 'https://' + username + ':' + password +\
+                        '@www.documentcloud.org/api/'
+
+        if self.credential_test() == 200:
+            pass
+        else:
+            print('Your username or password is incorrect.\n' +
+                  'Please check them and try again.')
+            sys.exit()
+
+        self.db = Database()
+        self.db.print_db_name()
+        self.utils = Utils()
+
+    def credential_test(self):
+        """
+        Let's check the credentials with a simple search before
+        lighting the fuse.
+        """
+        r = requests.get(self.base_uri + 'search.json?q=test')
+        return r.status_code
+
+    def request(self, full_path, payload, upload):
+        """
+        Post the request.
+        """
+        r = requests.post(self.base_uri + 'upload.json', data=payload, files=upload)
+        upload_response = json.loads(r.text)
+        timestamp = self.utils.timestamp()
+        self.db.insert_row(
+            upload_response['title'],
+            full_path,
+            timestamp,
+            r.status_code,
+            upload_response['canonical_url'],
+            None,
+            None)
+
+    def log_exclusion(self, name, full_path, exclude_reason):
+        timestamp = self.utils.timestamp()
+        self.db.insert_row(
+            name,
+            full_path,
+            timestamp,
+            None,
+            None,
+            'Y',
+            exclude_reason)
+
+    def upload(self, file_directory=None, title=None, source=None,
+               description=None, language=None, related_article=None,
+               published_url=None, access='private', project=None,
+               data=None, secure=False):
+        """
+        Upload one or more documents with associated metadata and options.
+        """
+
+        # Walk the supplied or current directory, including sub-directories,
+        # and build a list of files for the upload
+        documents = []
+        if not file_directory:
+            file_directory = '.'
+
+        # Create a list of dictionaries that includes file name and full path,
+        # plus placeholders for exclusion information.
+        files_dict_list = []
+        for root, dir, files in os.walk(file_directory):
+            for f in files:
+                file_dict = {}
+                file_dict['name'] = f
+                file_dict['full_path'] = os.path.join(root, f)
+                file_dict['exclude_flag'] = None
+                file_dict['exclude_reason'] = None
+                files_dict_list.append(file_dict)
+        print(files_dict_list)
+
+        # Check for files with prohibited formats, size
+        documents = self.utils.sanitize_uploads(files_dict_list)
+
+        for doc in documents:
+            if doc['exclude_flag']:
+                self.log_exclusion(
+                    doc['name'],
+                    doc['full_path'],
+                    doc['exclude_reason'])
+            else:
+                print('Uploading ' + doc['full_path'])
+                full_path = doc['full_path']
+                payload = {
+                    'title': doc['name'],
+                    'source': source,
+                    'description': description,
+                    'language': language,
+                    'related_article': related_article,
+                    'published_url': published_url,
+                    'access': access,
+                    'project': project,
+                    'data': data,
+                    'secure': secure
+                }
+                upload = {'file': open(doc['full_path'], 'rb')}
+                print(payload)
+                self.request(full_path, payload, upload)
---
+++ packages/pneumatic/pneumatic-0.0.1-py2.7.egg/pneumatic/utils.py
@@ -0,0 +1,43 @@
+import os
+import time
+
+
+class Utils(object):
+    """
+    A few things we'll (eventually) use.
+    """
+
+    def __init__(self):
+        # These are file types we do not want to send to DocumentCloud.
+        self.file_excludes = (
+            'aiff',
+            'DS_Store',
+            'flac',
+            'mid',
+            'mdb',
+            'mp3',
+            'ogg',
+            'pst',
+            'wav',
+            'wma'
+        )
+
+    def sanitize_uploads(self, doc_list):
+        """
+        Flag prohibited file types and files over 400MB upload size limit.
+        """
+        for x in doc_list:
+            file_split = x['name'].split('.')
+            if file_split[-1] in self.file_excludes:
+                x['exclude_flag'] = True
+                x['exclude_reason'] = 'Prohibited file type'
+            elif os.path.getsize(x['full_path']) > 5000000:
+                x['exclude_flag'] = True
+                x['exclude_reason'] = 'File over 400MB'
+            else:
+                x['exclude_flag'] = False
+        return doc_list
+
+    def timestamp(self):
+        self.time = time.strftime("%Y%m%dT%H%M%S")
+        return self.time
