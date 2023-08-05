77a5067e1f2098fd6d3dc84621967d1f458d0891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package define/1.72/define-1.72.tar.gz

---
+++ packages/define/define-1.72.tar.gz/define-1.72/setup.py
@@ -0,0 +1,22 @@
+try:
+            from setuptools import setup
+except ImportError:
+            from distutils.core import setup
+
+setup(name='define',
+      description='Terminal Dictionary',
+      long_description=open('README.md').read(),
+      version='1.72',
+      author='SethDusek',
+      author_email='shibe@openmailbox.org',
+      url='https://github.com/SethDusek/define',
+      data_files=[
+          ("share/man/man1", ["define.1"]),
+          ("share/doc/define", ["LICENSE"])
+          ],
+      scripts=['define'],
+      install_requires=['requests'],
+      classifiers=['Intended Audience :: End Users/Desktop',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3'],
+      license='BSD')
