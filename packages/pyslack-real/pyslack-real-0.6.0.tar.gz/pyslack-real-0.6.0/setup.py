36850a54594c49f11a496ba3f8a6861b587618ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package pyslack-real/0.6.0/pyslack-real-0.6.0.tar.gz

---
+++ packages/pyslack-real/pyslack-real-0.6.0.tar.gz/pyslack-real-0.6.0/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+
+setup(
+    author="@LoisaidaSam",
+    author_email="sam.sandberg@gmail.com",
+    description="A Python wrapper for Slack's API",
+    install_requires=["requests"],
+    keywords="slack api wrapper",
+    license="MIT",
+    name="pyslack-real",
+    packages=["pyslack"],
+    test_suite="tests",
+    url="https://github.com/loisaidasam/pyslack",
+    version="0.6.0",
+)
