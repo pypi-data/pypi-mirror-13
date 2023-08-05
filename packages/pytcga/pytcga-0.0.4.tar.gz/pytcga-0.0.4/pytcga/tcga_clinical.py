da0a3c83752748e4fe2eeff8dae636b222d30f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:19 +0000

    Add package pytcga/0.0.4/pytcga-0.0.4.tar.gz

---
+++ packages/pytcga/pytcga-0.0.4.tar.gz/pytcga-0.0.4/pytcga/tcga_clinical.py
@@ -0,0 +1,111 @@
+import os
+import logging
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from .tcga_requests import PYTCGA_BASE_DIRECTORY
+from .clinical_data_dictionary import clinical_data_dictionary
+
+TCGA_CLINICAL_URL = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/{}/bcr/biotab/clin/"
+
+PATIENT_DATA_FILE_CODE = 'clinical_patient'
+def request_clinical_data(disease_code,
+                  cache=True,
+                  block_size=1024):
+    """Downloads TCGA public clinical data from the TCGA FTP site
+    
+    Parameters
+    ----------
+    disease_code : str
+        TCGA disease type, i.e. 'LUAD', 'BLCA', 'BRCA' etc.
+    cache : bool, optional
+        Whether to cache the results of the request
+    block_size : int, optional
+        Block size for file downloads
+    
+    Returns
+    -------
+    patient_data_path : str
+        Path to TCGA patient data file after downloading
+    """
+    # Create directory to save clinical data
+    disease_code_dir = os.path.join(PYTCGA_BASE_DIRECTORY, disease_code)
+
+    if cache and os.path.exists(disease_code_dir):
+        patient_data_file = [f for f in os.listdir(disease_code_dir) if PATIENT_DATA_FILE_CODE in f]
+
+        if len(patient_data_file) == 1:
+            return os.path.join(disease_code_dir, patient_data_file[0])
+
+    if not os.path.exists(disease_code_dir):
+        os.makedirs(disease_code_dir)
+
+    clinical_data_directory = TCGA_CLINICAL_URL.format(disease_code.lower())
+    r = requests.get(clinical_data_directory)
+    soup = BeautifulSoup(r.content)
+
+    # Retrieve list of files and filter to txt files
+    file_links = [link.get('href') 
+                    for link in soup.find_all('a')]
+    clinical_files = [link for link in file_links if link.endswith('.txt')]
+
+    # Download all clinical data files
+    patient_data_path = None
+    for clinical_file in clinical_files:
+        output_file = os.path.join(disease_code_dir, clinical_file)
+        logging.debug('Saving {} clinical data request to {}'.format(clinical_file, output_file))
+
+        if PATIENT_DATA_FILE_CODE in output_file:
+            patient_data_path = output_file
+
+        with open(output_file, 'wb') as archive:
+            archive_response = requests.get(clinical_data_directory + '/' + clinical_file, stream=True)
+
+            for block in archive_response.iter_content(block_size):
+                archive.write(block)
+
+    return patient_data_path
+
+
+def load_clinical_data(disease_code, recode_columns=True):
+    """Downloads and loads the TCGA clinical data into a Pandas dataframe
+
+    Parameters
+    ----------
+    disease_code : str
+        TCGA disease type, i.e. 'LUAD', 'BLCA', 'BRCA' etc.
+
+    Returns
+    -------
+    patient_data_df : Dataframe
+        Returns a Pandas dataframe with the patient data
+    """
+    patient_data_path = request_clinical_data(disease_code, cache=True)
+
+    columns = pd.read_csv(patient_data_path,
+                            sep='\t',
+                            skiprows=1,
+                            nrows=10).columns
+
+    patient_data_df = pd.read_csv(patient_data_path,
+                    sep='\t', 
+                    skiprows=2,
+                    header=0,
+                    names=columns,
+                    na_values='[Not Available]')
+
+    if recode_columns:
+        for column in clinical_data_dictionary:
+            if column in patient_data_df.columns:
+                patient_data_df[column] = patient_data_df[column].map(
+                        lambda val: clinical_data_dictionary[column].get(val, val)
+                    )
+
+    logging.info("Loaded {} rows of clinical data from {} patients".format(
+            len(patient_data_df),
+            patient_data_df['bcr_patient_barcode'].nunique()
+        )
+    )
+
+    return patient_data_df
---
+++ packages/pytcga/pytcga-0.0.4.tar.gz/pytcga-0.0.4/pytcga/tcga_mutations.py
@@ -0,0 +1,88 @@
+import os
+import logging
+import tarfile
+import pandas as pd 
+
+from pytcga.tcga_requests import tcga_request
+from pytcga.tcga_clinical import load_clinical_data
+
+def prefetch_mutation_data(disease_code,
+                          wait_time=30,
+                          cache=True,):
+
+    archive_path = tcga_request(disease=disease_code,
+                               level='2',
+                               center='BI',
+                               platformType='Somatic Mutations',
+                               platform='Automated Mutation Calling',
+                               wait_time=wait_time,
+                               cache=cache)
+
+    return archive_path
+
+def load_mutation_data(disease_code,
+                       with_clinical=False,
+                       variant_type='all',
+                       wait_time=30):
+
+    archive_path = prefetch_mutation_data(disease_code,
+                                      wait_time=wait_time,
+                                      cache=True)
+
+    # Unpack tar file
+    archive = tarfile.open(archive_path)
+
+    result_dir = os.path.join(os.path.dirname(archive_path), disease_code, 'mutations')
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+
+    archive.extractall(path=result_dir)
+
+    # Filter to MAF files
+    maf_files = [f 
+                  for f in os.listdir(result_dir) 
+                  if f.endswith('.maf')]
+
+    mutation_df = pd.concat([pd.read_csv(os.path.join(result_dir, maf_file),
+                                    sep='\t', 
+                                    na_values='[Not Available]') 
+                    for maf_file in maf_files])
+
+    # Expand out the TCGA barcode to retrieve the TCGA ID
+    tcga_info = mutation_df['Tumor_Sample_Barcode'].str.rsplit('-', n=4, expand=True)
+    tcga_info.columns = ['TCGA_ID', 'SampleID', 'PortionID', 'PlateID', 'CenterID']
+
+    mutations = mutation_df.join(tcga_info, how='left')
+
+    if variant_type != 'all':
+        if variant_type == 'indel':
+            mutations = mutations[
+                            (mutations['Variant_Type'] == 'INS') |
+                            (mutations['Variant_Type'] == 'DEL')
+                        ]          
+        else:     
+            mutations = mutations[mutations['Variant_Type'] == variant_type]
+
+    logging.info("Loaded {} mutations for {} tumors from {} patients".format(
+                    len(mutations),
+                    mutations['Tumor_Sample_Barcode'].nunique(),
+                    mutations['TCGA_ID'].nunique()
+                )
+    )
+
+    if with_clinical:
+        patient_data_df = load_clinical_data(disease_code)
+        merged = mutations.merge(patient_data_df,
+                            how='outer',
+                            left_on='TCGA_ID', 
+                            right_on='bcr_patient_barcode')
+
+        logging.info("Patients: {}, Tumor Samples: {}, Mutations {}".format(
+                    merged['bcr_patient_barcode'].nunique(),
+                    merged['Tumor_Sample_Barcode'].nunique(),
+                    len(merged[~merged['TCGA_ID'].isnull()])
+                )
+        )
+        return merged
+    else: 
+        return mutations
