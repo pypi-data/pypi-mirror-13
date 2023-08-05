d547e504e0a6574c41b47c878de8f2ab50622d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:23 +0000

    Add package justplot/0.1.2a3/justplot-0.1.2a3.tar.gz

---
+++ packages/justplot/justplot-0.1.2a3.tar.gz/justplot-0.1.2a3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='justplot',
+    version='0.1.2a3',
+    packages=['justplot',],
+    license='MIT',
+    author='Uğur Çayoğlu',
+    author_email='urcyglu@gmail.com',
+    url='https://github.com/OnionNinja/justplot',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+    ],
+    classifiers=[
+        # More at http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+      ],
+    keywords=['plots','matplotlib','wrapper']
+)
+
