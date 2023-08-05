2c99d514ff5d3d9e33c42ecc3d5010e9982c8093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:29 +0000

    Add package Furtive/0.3.0/Furtive-0.3.0.tar.gz

---
+++ packages/Furtive/Furtive-0.3.0.tar.gz/Furtive-0.3.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+__VERSION__ = '0.3.0'
+
+long_description = """
+Github: https://github.com/dbryant4/furtive
+"""
+
+setup(name='Furtive',
+      version=__VERSION__,
+      description='File Integrity Verification System',
+      author='Derrick Bryant',
+      author_email='dbryant4@gmail.com',
+      long_description=long_description,
+      license='MIT',
+      packages=['furtive'],
+      entry_points={
+        'console_scripts': [
+            'furtive=furtive.cli:main',
+        ]
+      },
+      url='https://furtive.readthedocs.org',
+      download_url='https://github.com/dbryant4/furtive',
+      install_requires=[
+        'PyYAML==3.11',
+        'argparse==1.4.0'
+      ]
+     )
