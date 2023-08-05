3c7273935bc88d74ba8a3a8d0f62e3ba49638627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:20 +0000

    Add package nester123/1.1.0/nester123-1.1.0.tar.gz

---
+++ packages/nester123/nester123-1.1.0.tar.gz/nester123-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'nester123',
+version = '1.1.0',
+py_modules = ['nester123'],
+author = 'vineet',
+author_email = 'vntpaul@hotmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+)
