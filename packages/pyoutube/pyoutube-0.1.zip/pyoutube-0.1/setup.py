d52eac595400ac840e4d7baa7044e97fb2432208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package pyoutube/0.1/pyoutube-0.1.zip

---
+++ packages/pyoutube/pyoutube-0.1.zip/pyoutube-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyoutube',
+      version='0.1',
+      description='Upload videos to youtube',
+      author='lpbrown999',
+      author_email='lpbrown999@gmail.com',
+      packages=['pyoutube'],
+      zip_safe=False
+      )
