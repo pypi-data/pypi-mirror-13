300805691102db0bf325c71c70f7a25e472e0361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package python-semantic-release/3.2.1/python_semantic_release-3.2.1-py3.5.egg

---
+++ packages/python-semantic-release/python_semantic_release-3.2.1-py3.5.egg/semantic_release/errors.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class SemanticReleaseBaseError(Exception):
+    pass
+
+
+class ImproperConfigurationError(SemanticReleaseBaseError):
+    pass
+
+
+class UnknownCommitMessageStyleError(SemanticReleaseBaseError):
+    pass
+
+
+class GitError(SemanticReleaseBaseError):
+    pass
