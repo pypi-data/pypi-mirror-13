98a58e88cd3d8d2bd04f0daab2bd609bcbf65847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package bhargav_mac_nester/1.1.0/bhargav_mac_nester-1.1.0.tar.gz

---
+++ packages/bhargav_mac_nester/bhargav_mac_nester-1.1.0.tar.gz/bhargav_mac_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup 
+
+setup(
+	name = 'bhargav_mac_nester',
+	version = '1.1.0',
+	py_modules = ['bhargav_mac_nester'],
+	author = 'bhargav_jhaveri',
+	author_email = 'bhargavjhaveri@gmail.com',
+	url = 'http://www.wikipedia.org',
+	description = 'A simple printer of nested lists built by Bhargav',
+)
