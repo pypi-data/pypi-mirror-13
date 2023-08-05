e8b07321e12c2e0511e8bb33c5beb6386ea3b370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package profiling/0.1.1/profiling-0.1.1.tar.gz

---
+++ packages/profiling/profiling-0.1.1.tar.gz/profiling-0.1.1/profiling/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+   profiling
+   ~~~~~~~~~
+"""
+from __future__ import absolute_import
+
+from .__about__ import __version__  # noqa
+from .profiler import Profiler
+
+
+__all__ = ['Profiler']
