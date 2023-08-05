0226b66a9a65b21732f81ce5939dbb13c99b095e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:16 +0000

    Add package pyloudness/1.1.0/pyloudness-1.1.0.tar.gz

---
+++ packages/pyloudness/pyloudness-1.1.0.tar.gz/pyloudness-1.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pyloudness',
+      version='1.1.0',
+      description='Find out how loud that file is',
+      long_description=long_description,
+      url='https://github.com/jrigden/pyloudness',
+      author='Jason Rigden',
+      author_email='jasonrigden@gmail.com',
+      keywords='loudness audio ebu r128',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Multimedia :: Sound/Audio :: Analysis',
+          'License :: OSI Approved :: MIT License',
+      ],
+      packages=['pyloudness'],
+      zip_safe=False)
