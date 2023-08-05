899a721eba242f67ce2717a9404d162780ca5fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package ipython-animated-array/0.2.0/ipython-animated-array-0.2.0.tar.gz

---
+++ packages/ipython-animated-array/ipython-animated-array-0.2.0.tar.gz/ipython-animated-array-0.2.0/ipython_animated_array/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from os import path
+from IPython.display import HTML, display
+from jinja2 import Environment, PackageLoader
+from vizarray import vizarray
+
+class AnimateArray(object):
+    def __init__(self, viz_list, cmap='cool'):
+        super(AnimateArray, self).__init__()
+        self.env = Environment(loader=PackageLoader('ipython_animated_array', 'template', encoding='utf8'))
+        self.cmap = cmap
+        self.htmls = self.get_htmls(viz_list)
+
+    def get_htmls(self, viz_list):
+        return [
+            vizarray(viz, cmap=self.cmap)._repr_html_()
+            for viz in viz_list
+        ]
+
+    def show(self, reflesh=1000):
+        print(reflesh)
+        template = self.env.get_template('animatetag.tpl.html')
+        display(HTML(template.render({
+            'htmls': self.htmls,
+            'id': id(self.htmls),
+            'reflesh': reflesh
+        })))
---
+++ packages/ipython-animated-array/ipython-animated-array-0.2.0.tar.gz/ipython-animated-array-0.2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='ipython-animated-array',
+    version='0.2.0',
+    description='animated array rendering on ipython-notebook (jupyter)',
+    long_description=long_description,
+    author='Ryoji Ishii',
+    author_email='airtoxin@icloud.com',
+    license='MIT',
+    url='https://github.com/airtoxin/ipython-animated-array',
+    packages=find_packages(exclude=['build', 'doc', 'template']),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Framework :: IPython',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
+    keywords='ipython jupyter animation array',
+    install_requires=[
+        'ipython',
+        'Jinja2',
+        'vizarray',
+        'ipythonblocks', # vizarray dependency
+        'numpy', # vizarray dependency
+        'matplotlib<1.5', # vizarray dependency
+    ]
+)
