ddef36854b27860e17e3ee57f17dd6026ed42e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:18 +0000

    Add package PyWavelets/0.4.0/PyWavelets-0.4.0-cp34-none-win32.whl

---
+++ packages/PyWavelets/PyWavelets-0.4.0-cp34-none-win32.whl/pywt/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYWAVELETS SETUP.PY
+short_version = '0.4.0'
+version = '0.4.0'
+full_version = '0.4.0'
+git_revision = '0f269d5f71294955cbd63467e41ee3d25b76d081'
+release = True
+
+if not release:
+    version = full_version
