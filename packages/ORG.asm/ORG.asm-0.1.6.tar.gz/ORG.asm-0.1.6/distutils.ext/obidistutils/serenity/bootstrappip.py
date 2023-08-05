1e349d1106003ba828e39519dc5bc56259c7555f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:38 +0000

    Add package ORG.asm/0.1.6/ORG.asm-0.1.6.tar.gz

---
+++ packages/ORG.asm/ORG.asm-0.1.6.tar.gz/ORG.asm-0.1.6/distutils.ext/obidistutils/serenity/bootstrappip.py
@@ -0,0 +1,32 @@
+'''
+Created on 22 janv. 2016
+
+@author: coissac
+'''
+
+import sys
+from urllib import request
+import os.path
+
+from obidistutils.serenity.util import get_serenity_dir
+from obidistutils.serenity.rerun import rerun_with_anothe_python
+
+getpipurl="https://bootstrap.pypa.io/get-pip.py"
+
+def bootstrap():
+
+    getpipfile=os.path.join(get_serenity_dir(),"get-pip.py --user")
+    
+    with request.urlopen(getpipurl) as getpip:
+        with open(getpipfile,"wb") as out:
+            for l in getpip:
+                out.write(l)
+                
+    python = sys.executable
+    
+    command= "%s %s" % (python,getpipfile)
+    
+    os.system(command)
+    
+    rerun_with_anothe_python(python)
+    
