c4b8a7684d7ee00b76a781b1c8ddcd5cbad24ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:59 +0000

    Add package jsim/1.0/jsim-1.0.tar.gz

---
+++ packages/jsim/jsim-1.0.tar.gz/jsim-1.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="jsim",
+
+    # Version number (initial):
+    version="1.0",
+
+    # Application author details:
+    author="Anna Wasewicz",
+  
+
+    # Packages
+    packages=["jsim"],
+
+    url = "https://pypi.python.org/pypi/jsim/1.0",
+    author_email="wasewicz@umich.edu",
+    #
+    license="GPL",
+    description="Jaccard similarity document manager",
+
+    # long_description=open("README.txt").read(),
+
+    
+)
