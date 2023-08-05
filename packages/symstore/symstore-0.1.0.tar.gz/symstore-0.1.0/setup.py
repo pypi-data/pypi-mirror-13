45a6459bcd3fdbac6b8e57b911930e0aeefacee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:25 +0000

    Add package symstore/0.1.0/symstore-0.1.0.tar.gz

---
+++ packages/symstore/symstore-0.1.0.tar.gz/symstore-0.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import setuptools
+from os import path
+
+VERSION = open(path.join("symstore", "VERSION")).read().strip()
+
+setuptools.setup(
+    name="symstore",
+    version=VERSION,
+    description="publish PDB and PE files to symbols store",
+    long_description=open("README.rst", "r").read(),
+    url="https://github.com/elmirjagudin/symstore",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords="development symstore pdb",
+    install_requires=[
+        "pdbparse",
+        "construct"
+    ],
+    packages=["symstore"],
+    package_data={"symstore": ["VERSION"]},
+    scripts=["symstore/bin/symstore"],
+    extras_require={
+        "develop":
+            [
+                "coverage",
+                "flake8",
+                "mock"
+            ]
+    }
+)
