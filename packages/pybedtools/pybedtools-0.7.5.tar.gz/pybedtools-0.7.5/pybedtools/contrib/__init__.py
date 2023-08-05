d22bf95148a05186ae56f6b20e9fabc7c3a28263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package pybedtools/0.7.5/pybedtools-0.7.5.tar.gz

---
+++ packages/pybedtools/pybedtools-0.7.5.tar.gz/pybedtools-0.7.5/pybedtools/contrib/__init__.py
@@ -0,0 +1,5 @@
+from . import bigwig
+from . import bigbed
+from . import venn_maker
+from .classifier import MultiClassifier
+from .intersection_matrix import IntersectionMatrix
---
+++ packages/pybedtools/pybedtools-0.7.5.tar.gz/pybedtools-0.7.5/pybedtools/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.7.5'
+version = '0.7.5'
+full_version = '0.7.5'
+git_revision = '4717d9b4f52b595d1d7482237bb0c5c63326c7d5'
+release = True
+
+__version__ = version
+if not release:
+    version = full_version
