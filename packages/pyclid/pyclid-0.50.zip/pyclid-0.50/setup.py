23a0340c9451ad065104cfb0d93763373dd6a3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package pyclid/0.50/pyclid-0.50.zip

---
+++ packages/pyclid/pyclid-0.50.zip/pyclid-0.50/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pyclid',
+    packages=['pyclid'],
+    version='0.50',
+    description='A simple vector mathematics library',
+    author='Gavin Jones',
+    author_email='gav.j.jones@googlemail.com',
+    url='https://github.com/Kaundur/pyclid',
+    download_url='https://github.com/Kaundur/pyclid/tarball/0.50',
+    keywords=['Vector', 'Euclid', 'Matrix', 'Quaternion'],
+    classifiers=[],
+)
