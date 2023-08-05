23951661029cc9c65ba8ea701db713a42630168a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package django-echo/0.1.1/django-echo-0.1.1.tar.gz

---
+++ packages/django-echo/django-echo-0.1.1.tar.gz/django-echo-0.1.1/echo/skill.py
@@ -0,0 +1,45 @@
+import logging
+import re
+
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+from .request import EchoRequest
+
+
+log = logging.getLogger(__name__)
+
+LAUNCH_NOT_IMPLEMENTED_ERROR = "'launch' is not implemented. Implement this handler in a base class."
+SESSION_ENDED_NOT_IMPLEMENTED_ERROR = "'session ended' is not implemented. Implement this handler in a base class."
+
+
+class EchoSkill(generic.View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, http_request):
+        self.request = EchoRequest(http_request)
+
+        handlers = {
+            EchoRequest.LAUNCH_REQUEST: self.launch,
+            EchoRequest.INTENT_REQUEST: self.intent,
+            EchoRequest.SESSION_ENDED_REQUEST: self.session_ended,
+        }
+
+        return handlers[self.request.type]()
+
+    def launch(self):
+        raise NotImplementedError(LAUNCH_NOT_IMPLEMENTED_ERROR)
+
+    def session_ended(self):
+        raise NotImplementedError(SESSION_ENDED_NOT_IMPLEMENTED_ERROR)
+
+    def intent(self):
+        requested_intent = self.request.intent
+        handler_name = self.get_intent_handler_name(requested_intent['name'])
+        return getattr(self, handler_name)(
+            slots=requested_intent.get('slots', {}),
+            session=self.request.session
+        )
+
+    def get_intent_handler_name(self, intent_name):
+        return re.sub('(?!^)([A-Z])', r'_\1', intent_name).lower()
---
+++ packages/django-echo/django-echo-0.1.1.tar.gz/django-echo-0.1.1/echo/tests/test_skill.py
@@ -0,0 +1,71 @@
+import mock
+
+from echo.skill import (
+    LAUNCH_NOT_IMPLEMENTED_ERROR,
+    SESSION_ENDED_NOT_IMPLEMENTED_ERROR,
+    EchoSkill,
+)
+from echo.tests import BaseEchoTestCase
+
+
+class TestEchoSkill(BaseEchoTestCase):
+    def setUp(self):
+        super(TestEchoSkill, self).setUp()
+        self.skill = EchoSkill()
+
+    def test_routes_to_launch(self):
+        """A LaunchRequest should raise a NotImplementedError"""
+        expected = (NotImplementedError, LAUNCH_NOT_IMPLEMENTED_ERROR)
+
+        http_request = self._generate_launch_request()
+        with self.assertRaisesMessage(*expected):
+            self.skill.dispatch(http_request)
+
+    def test_routes_to_session_ended(self):
+        """A SessionEndedRequest should raise a NotImplementedError"""
+        expected = (NotImplementedError, SESSION_ENDED_NOT_IMPLEMENTED_ERROR)
+
+        http_request = self._generate_session_ended_request()
+        with self.assertRaisesMessage(*expected):
+            self.skill.dispatch(http_request)
+
+    def test_get_intent_handler_name(self):
+        """get_intent_handler_name should know how to un-camelcase a string"""
+        expected = "this_is_a_camel_cased_string"
+        assert self.skill.get_intent_handler_name("ThisIsACamelCasedString") == expected
+
+    def test_routes_to_intent(self):
+        """A IntentRequest should be routed to the proper intent handler"""
+        expected = 1
+
+        http_request = self._generate_intent_request(intent_name="TheIntent")
+
+        self.skill.the_intent = None
+        with mock.patch.object(self.skill, 'the_intent') as mock_intent:
+            self.skill.dispatch(http_request)
+            assert mock_intent.call_count == expected
+
+    def test_routes_to_intent_with_session_and_slot_data(self):
+        """A IntentRequest should be routed to the proper intent handler with session and slot data"""
+        expected = {
+            'slots': {
+                'Sign': {
+                    'name': 'Sign',
+                    'value': 'Virgo'
+                }
+            },
+            'session': {
+                'saved_value': 123
+            }
+        }
+
+        http_request = self._generate_intent_request(
+            intent_name="TheIntent",
+            slots=expected['slots'],
+            session=expected['session']
+        )
+
+        self.skill.the_intent = None
+        with mock.patch.object(self.skill, 'the_intent') as mock_intent:
+            self.skill.dispatch(http_request)
+            mock_intent.assert_called_once_with(**expected)
