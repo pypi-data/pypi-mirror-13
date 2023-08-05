8d25afcd3ce0fffc5fb99daaacc2fd9cbb3be927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:04 +0000

    Add package lgc_nester/1.0.0/lgc_nester-1.0.0.zip

---
+++ packages/lgc_nester/lgc_nester-1.0.0.zip/lgc_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'lgc_nester',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'lgc',
+	author_email = 'guochao_liu@foxmail.com',
+	url = 'http//www.guochao_liu@foxmail.com',
+	description = 'A simple printer of nested lists',
+	)
