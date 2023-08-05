41b89176fa0322d3aa74137267817c1293b62f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package vannistell/1.3.1/vannistell-1.3.1.zip

---
+++ packages/vannistell/vannistell-1.3.1.zip/vannistell-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'vannistell',
+	version = '1.3.1',
+	py_modules = ['nester'],
+	author = 'hfpython',
+	author_email = 'hfpython@headfirstlabs.com',
+	url = 'http://www.hehadfirstlabs.com',
+	description = 'A simple printer of nested lists that adds indentation for nested lists, with an optional second argument, and an additional option whether indentation is needed',
+	)
