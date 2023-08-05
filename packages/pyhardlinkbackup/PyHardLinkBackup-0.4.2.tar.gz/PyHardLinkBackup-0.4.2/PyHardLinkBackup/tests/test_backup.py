b214b42665efab2e09559f98a21e963fa882f9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:42 +0000

    Add package pyhardlinkbackup/0.4.2/PyHardLinkBackup-0.4.2.tar.gz

---
+++ packages/pyhardlinkbackup/PyHardLinkBackup-0.4.2.tar.gz/PyHardLinkBackup-0.4.2/PyHardLinkBackup/tests/test_backup.py
@@ -0,0 +1,163 @@
+import os
+import unittest
+import pprint
+
+import pathlib
+
+import sys
+
+from PyHardLinkBackup.tests.base import BaseCreatedTwoBackupsTestCase, BaseCreatedOneBackupsTestCase, \
+    BaseSourceDirTestCase, BaseWithSourceFilesTestCase
+from PyHardLinkBackup.tests.utils import UnittestFileSystemHelper
+from PyHardLinkBackup.phlb.config import phlb_config
+
+
+class TestBackup(BaseSourceDirTestCase):
+    """
+    Tests with empty "source unittests files" directory under /temp/
+    """
+    def test_same_size_different_content(self):
+        test_file1=pathlib.Path(self.source_path, "dirA", "file.txt")
+        os.mkdir(str(test_file1.parent))
+        with test_file1.open("w") as f:
+            f.write("content1")
+
+        test_file2=pathlib.Path(self.source_path, "dirB", "file.txt")
+        os.mkdir(str(test_file2.parent))
+        with test_file2.open("w") as f:
+            f.write("content2")
+
+        self.assertEqual(os.stat(str(test_file1)).st_size, 8)
+        self.assertEqual(os.stat(str(test_file2)).st_size, 8)
+
+        result = self.invoke_cli("backup", self.source_path)
+        print(result.output)
+
+        self.assertIn("16 Bytes in 2 files to backup.", result.output)
+        self.assertIn("new content saved: 2 files (16 Bytes 100.0%)", result.output)
+        self.assertIn("stint space via hardlinks: 0 files (0 Bytes 0.0%)", result.output)
+
+        self.assert_backup_fs_count(1) # there are tree backups in filesystem
+
+
+class WithSourceFilesTestCase(BaseWithSourceFilesTestCase):
+    def test_print_update(self):
+
+        first_run_result = self.invoke_cli("backup", self.source_path)
+        print(first_run_result.output)
+
+        # We should not have in between update info with default settings and duration
+        self.assertNotIn("Update info:", first_run_result.output)
+        self.assertNotIn("Slow down speed for unittest!", first_run_result.output)
+
+        self.assertIn("new content saved: 5 files (106 Bytes 100.0%)", first_run_result.output)
+        self.assertIn("stint space via hardlinks: 0 files (0 Bytes 0.0%)", first_run_result.output)
+
+
+        self.simulate_slow_speed(0.1) # slow down backup
+        phlb_config.print_update_interval=0.1 # Very often status infos
+
+        second_run_result = self.invoke_cli("backup", self.source_path)
+        print(second_run_result.output)
+
+        # Now we should have in between update info
+        self.assertIn("Update info:", second_run_result.output)
+        self.assertIn("Slow down speed for unittest!", second_run_result.output)
+
+        self.assertIn("new content saved: 0 files (0 Bytes 0.0%)", second_run_result.output)
+        self.assertIn("stint space via hardlinks: 5 files (106 Bytes 100.0%)", second_run_result.output)
+
+
+class TestTwoBackups(BaseCreatedTwoBackupsTestCase):
+    def test_changed_file(self):
+        filepath = os.path.join(self.source_path, "sub dir A", "dir_A_file_B.txt")
+        with open(filepath, "w") as f:
+            f.write(">>> The new content! <<<")
+
+        # run backup
+        result = self.invoke_cli("backup", self.source_path)
+
+        print(result.output)
+
+        self.assertIn("110 Bytes in 5 files to backup.", result.output)
+        self.assertIn("new content saved: 1 files (24 Bytes 21.8%)", result.output)
+        self.assertIn("stint space via hardlinks: 4 files (86 Bytes 78.2%)", result.output)
+
+        self.assert_backup_fs_count(3) # there are tree backups in filesystem
+        backup_path = self.get_newest_backup_path()
+
+        fs_helper = UnittestFileSystemHelper()
+        #fs_helper.print_tree(self.backup_path)
+        tree_list = fs_helper.pformat_tree(backup_path, with_timestamps=False)
+        pprint.pprint(tree_list,indent=0, width=200)
+        self.assertListEqual(tree_list, [
+            backup_path,
+            'root_file_A.txt                L - The root file A content.',
+            'root_file_A.txt.sha512         F - 13e3e...d7df6',
+            'root_file_B.txt                L - The root file B content.',
+            'root_file_B.txt.sha512         F - 4bb47...5a181',
+            'sub dir A                      D',
+            'sub dir A/dir_A_file_A.txt     L - File A in sub dir A.',
+            'sub dir A/dir_A_file_A.txt.sha512 F - 89091...f8669',
+            'sub dir A/dir_A_file_B.txt     F - >>> The new content! <<<', # <<-- new file
+            'sub dir A/dir_A_file_B.txt.sha512 F - b7203...eb68f', # <<-- new hash
+            'sub dir B                      D',
+            'sub dir B/sub_file.txt         L - File in sub dir B.',
+            'sub dir B/sub_file.txt.sha512  F - bbe59...dbdbb'
+        ])
+
+        # first + second data must be untouched:
+        self.assert_first_backup()
+        self.assert_second_backup()
+
+
+class TestOneBackups(BaseCreatedOneBackupsTestCase):
+    def test_summary(self):
+        summary_filepath = pathlib.Path(self.first_run_path + " summary.txt")
+        self.assertTrue(summary_filepath.is_file(), "%s doesn't exist" % summary_filepath)
+
+        with summary_filepath.open("r") as f: # Path().read_text() is new in Py 2.5
+            summary_content = f.read()
+        print(summary_content)
+
+        self.assertIn("Backup done:", summary_content)
+        self.assertIn("Source file sizes: 106 Bytes", summary_content)
+        self.assertIn("new content saved: 5 files (106 Bytes 100.0%)", summary_content)
+        self.assertIn("stint space via hardlinks: 0 files (0 Bytes 0.0%)", summary_content)
+
+
+    def test_log_file(self):
+        log_content = self.get_log_content(self.first_run_log)
+
+        print(log_content)
+        self.assertIn("Backup", log_content)
+        self.assertIn("Start low level logging", log_content)
+
+    @unittest.skipIf(sys.platform.startswith("win"), "TODO: work-a-round for os.chmod()")
+    def test_skip_files(self):
+        """
+        Test if not open able source files, will be skipped
+        and the backup will save the other files.
+        """
+        filepath1 = os.path.join(self.source_path, "root_file_B.txt")
+        filepath2 = os.path.join(self.source_path, "sub dir B", "sub_file.txt")
+
+        os.chmod(filepath1, 0o000)
+        os.chmod(filepath2, 0o000)
+
+        result = self.invoke_cli("backup", self.source_path)
+        print(result.output)
+
+        self.assertIn("106 Bytes in 5 files to backup.", result.output)
+        self.assertIn("WARNING: Skipped 2 files", result.output)
+        self.assertIn("new content saved: 0 files (0 Bytes 0.0%)", result.output)
+        self.assertIn("stint space via hardlinks: 3 files (64 Bytes 60.4%)", result.output)
+
+        self.assertEqual(os.listdir(self.backup_path), ["source unittests files"])
+        self.assert_backup_fs_count(2)
+
+        log_content = self.get_log_content(self.first_run_log)
+        print(log_content)
+        self.assertIn("Skip file", log_content)
+        self.assertIn("/root_file_B.txt error:", log_content)
+        self.assertIn("/sub dir B/sub_file.txt error:", log_content)
