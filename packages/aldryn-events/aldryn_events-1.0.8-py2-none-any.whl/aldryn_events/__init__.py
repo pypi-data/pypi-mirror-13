4fc9c948350264cdbc36b5d70a09faba4aadb328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:40 +0000

    Add package aldryn-events/1.0.8/aldryn_events-1.0.8-py2-none-any.whl

---
+++ packages/aldryn-events/aldryn_events-1.0.8-py2-none-any.whl/aldryn_events/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.8'
+
+
+# TODO: See if this can now be removed.
+request_events_event_identifier = 'aldryn_events_current_event'
+
+ORDERING_FIELDS = (
+    'start_date', 'start_time', 'end_date', 'end_time', 'pk'
+)
+
+ARCHIVE_ORDERING_FIELDS = (
+    '-start_date', '-start_time', 'end_date', 'end_time', 'pk'
+)
