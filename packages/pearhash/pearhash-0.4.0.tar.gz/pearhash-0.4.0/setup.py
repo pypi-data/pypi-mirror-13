9222c52a5de46900745ca82f85b3335e452174d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:58 +0000

    Add package pearhash/0.4.0/pearhash-0.4.0.tar.gz

---
+++ packages/pearhash/pearhash-0.4.0.tar.gz/pearhash-0.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = 'pearhash',
+	version = '0.4.0',
+	author = 'Vita Smid',
+	author_email = 'me@ze.phyr.us',
+	py_modules = ['pearhash',],
+	url = 'https://github.com/ze-phyr-us/pearhash',
+	description = 'Pearson hashing algorithm'
+)
