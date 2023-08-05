abfbcb7f9c0bfe6555ab406f871ce5115e0bed6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:45 +0000

    Add package commis/0.1/commis-0.1.tar.gz

---
+++ packages/commis/commis-0.1.tar.gz/commis-0.1/commis/__init__.py
@@ -0,0 +1,28 @@
+# commis
+# Create command line applications like Django management commands.
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Wed Jan 20 13:47:04 2016 -0500
+#
+# Copyright (C) 2016 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Create command line applications like Django management commands.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+from .command import Command
+from .program import ConsoleProgram
+from .version import get_version
+
+##########################################################################
+## Package Version
+##########################################################################
+
+__version__ = get_version()
