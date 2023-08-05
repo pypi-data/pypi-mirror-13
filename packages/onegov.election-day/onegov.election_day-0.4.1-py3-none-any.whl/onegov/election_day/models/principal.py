d4129de779a4a1b99e7923432460a779b586e7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package onegov.election-day/0.4.1/onegov.election_day-0.4.1-py3-none-any.whl

---
+++ packages/onegov.election-day/onegov.election_day-0.4.1-py3-none-any.whl/onegov/election_day/models/principal.py
@@ -0,0 +1,47 @@
+import onegov.election_day
+import yaml
+
+from cached_property import cached_property
+from onegov.core import utils
+from pathlib import Path
+
+
+cantons = {
+    'ag', 'ai', 'ar', 'be', 'bl', 'bs', 'fr', 'ge', 'gl', 'gr', 'ju', 'lu',
+    'ne', 'nw', 'ow', 'sg', 'sh', 'so', 'sz', 'tg', 'ti', 'ur', 'vd', 'vs',
+    'zg', 'zh'
+}
+
+
+class Principal(object):
+    """ The principal is the town or municipality running the election day app.
+
+    """
+
+    def __init__(self, name, logo, canton, color, base=None):
+        assert canton in cantons
+
+        self.name = name
+        self.logo = logo
+        self.canton = canton
+        self.color = color
+        self.base = base
+
+    @classmethod
+    def from_yaml(cls, yaml_source):
+        return cls(**yaml.load(yaml_source))
+
+    @cached_property
+    def municipalities(self):
+        path = utils.module_path(onegov.election_day, 'static/municipalities')
+        paths = (p for p in Path(path).iterdir() if p.is_dir())
+
+        result = {}
+
+        for path in paths:
+            year = int(path.name)
+
+            with (path / '{}.json'.format(self.canton)).open('r') as f:
+                result[year] = {int(k): v for k, v in yaml.load(f).items()}
+
+        return result
---
+++ packages/onegov.election-day/onegov.election_day-0.4.1-py3-none-any.whl/onegov/election_day/path.py
@@ -0,0 +1,41 @@
+from onegov.core.i18n import SiteLocale
+from onegov.ballot import Ballot, BallotCollection, Vote, VoteCollection
+from onegov.election_day import ElectionDayApp
+from onegov.election_day.models import Manage, Principal
+from onegov.user import Auth
+
+
+@ElectionDayApp.path(model=Auth, path='/auth')
+def get_auth(request, to='/'):
+    return Auth.from_request(request, to)
+
+
+@ElectionDayApp.path(model=Principal, path='/')
+def get_principal(app):
+    return app.principal
+
+
+@ElectionDayApp.path(model=Manage, path='/manage')
+def get_manage(app):
+    return Manage(app.session())
+
+
+@ElectionDayApp.path(model=Vote, path='/vote/{id}')
+def get_vote(app, id):
+    return VoteCollection(app.session()).by_id(id)
+
+
+@ElectionDayApp.path(model=Ballot, path='/ballot/{id}')
+def get_ballot(app, id):
+    return BallotCollection(app.session()).by_id(id)
+
+
+@ElectionDayApp.path(model=VoteCollection, path='/votes/{year}')
+def get_votes(app, year):
+    return VoteCollection(app.session(), year)
+
+
+@ElectionDayApp.path(model=SiteLocale, path='/locale/{locale}')
+def get_locale(request, app, locale, to=None):
+    to = to or request.link(app.principal)
+    return SiteLocale.for_path(app, locale, to)
