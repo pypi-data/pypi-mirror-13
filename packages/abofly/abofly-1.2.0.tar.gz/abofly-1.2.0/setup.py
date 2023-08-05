55bd8166fc1c5430e8dfa138d0a9f3c5e7c784cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package abofly/1.2.0/abofly-1.2.0.tar.gz

---
+++ packages/abofly/abofly-1.2.0.tar.gz/abofly-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name			=	'abofly',
+	version		=	'1.2.0',
+	py_modules	=	['nester'],
+	author		=	'chonti',
+	author_email	=	'chonti@naver.com',
+	description	=	'A simple printer of nested lists',
+	)
