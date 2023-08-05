77565be122c554939b1cc4ee2e1e399e2a083c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:53 +0000

    Add package cpenv/0.2.31/cpenv-0.2.31.zip

---
+++ packages/cpenv/cpenv-0.2.31.zip/cpenv-0.2.31/cpenv/log.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+'''
+Logging Configuration
+=====================
+'''
+
+import logging
+from .utils import unipath
+
+logger = logging.getLogger('cpenv')
+logger.setLevel(logging.DEBUG)
+
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+sh_formatter = logging.Formatter('\n%(message)s')
+sh.setFormatter(sh_formatter)
+
+fh = logging.FileHandler(unipath('~/.cpenv/cpenv.log'))
+fh.setLevel(logging.ERROR)
+fh_formatter = logging.Formatter(
+    '[%(asctime)s in %(module)s.%(funcName)s @%(lineno)s]\n'
+    '%(message)s')
+fh.setFormatter(fh_formatter)
+
+logger.addHandler(sh)
+logger.addHandler(fh)
