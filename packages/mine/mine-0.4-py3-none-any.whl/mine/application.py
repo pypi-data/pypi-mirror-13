099b2d84a10913b50414528b0b44b7123d715e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package mine/0.4/mine-0.4-py3-none-any.whl

---
+++ packages/mine/mine-0.4-py3-none-any.whl/mine/application.py
@@ -0,0 +1,63 @@
+"""Data structures for application information."""
+
+import logging
+
+import yorm
+
+from .base import NameMixin
+
+
+log = logging.getLogger(__name__)
+
+
+@yorm.attr(mac=yorm.converters.NullableString)
+@yorm.attr(windows=yorm.converters.NullableString)
+@yorm.attr(linux=yorm.converters.NullableString)
+class Versions(yorm.converters.AttributeDictionary):
+    """Dictionary of OS-specific application filenames."""
+
+
+@yorm.attr(auto_queue=yorm.converters.Boolean)
+@yorm.attr(single_instance=yorm.converters.Boolean)
+class Properties(yorm.converters.AttributeDictionary):
+    """Dictionary of application management settings."""
+
+
+@yorm.attr(name=yorm.converters.String)
+@yorm.attr(properties=Properties)
+@yorm.attr(versions=Versions)
+class Application(NameMixin, yorm.converters.AttributeDictionary):
+    """Dictionary of application information."""
+
+    # pylint: disable=E1101
+
+    def __init__(self, name, filename=None):
+        super().__init__()
+        self.name = name
+        self.versions = Versions(mac=filename, windows=filename, linux=filename)
+
+    @property
+    def auto_queue(self):
+        return self.properties.auto_queue
+
+    @property
+    def no_wait(self):
+        return not self.properties.single_instance
+
+
+@yorm.attr(all=Application)
+class Applications(yorm.converters.SortedList):
+    """List of monitored applications."""
+
+    def get(self, name):
+        """Get the application with the given name."""
+        application = self.find(name)
+        assert application, name
+        return application
+
+    def find(self, name):
+        """Find the application with the given name, else None."""
+        log.debug("Finding application for '%s'...", name)
+        for application in self:
+            if application == name:
+                return application
