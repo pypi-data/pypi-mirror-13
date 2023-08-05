900bd9df07969a1d4864626375ff6f196b0e216f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package pyisbn/1.0.1/pyisbn-1.0.1-py2.py3-none-any.whl

---
+++ packages/pyisbn/pyisbn-1.0.1-py2.py3-none-any.whl/pyisbn/_version.py
@@ -0,0 +1,10 @@
+# This is pyisbn version 1.0.1 (2016-01-08)
+# pylint: disable=C0103, C0111, C0121, W0622
+
+dotted = '1.0.1'
+libtool = '10:21'
+hex = 0x010001
+date = '2016-01-08'
+tuple = (1, 0, 1)
+dict = {'micro': 0, 'major': 1, 'minor': 1}
+web = 'pyisbn/1.0.1'
