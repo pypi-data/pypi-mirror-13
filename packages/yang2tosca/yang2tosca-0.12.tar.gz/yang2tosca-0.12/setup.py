92e894f5209b9d7d9ad6a8decc77d04ee8a922e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:35 +0000

    Add package yang2tosca/0.12/yang2tosca-0.12.tar.gz

---
+++ packages/yang2tosca/yang2tosca-0.12.tar.gz/yang2tosca-0.12/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'yang2tosca',
+  packages = ['yang2tosca'], # this must be the same as the name above
+  package_data={'yang2tosca': ['*.xslt']},
+  include_package_data=True,
+  version = '0.12',
+  description = 'A random test parser',
+  author = 'HPE',
+  author_email = 'shiva-charan.m-s@hpe.com',
+  url = 'https://gerrit.opnfv.org/gerrit/parser', # use the URL to the github repo
+  classifiers = [],
+)
