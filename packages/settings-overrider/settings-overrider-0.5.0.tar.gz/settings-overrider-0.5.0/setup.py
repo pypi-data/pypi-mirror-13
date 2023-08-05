58d94fac491cb754b92e248f74647bf9b0f37ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package settings-overrider/0.5.0/settings-overrider-0.5.0.tar.gz

---
+++ packages/settings-overrider/settings-overrider-0.5.0.tar.gz/settings-overrider-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup(
+    name='settings-overrider',
+    version='0.5.0',
+    description='Override Python dict contents with YAML file and/or environment variables',
+    long_description=(
+        'Originally created for Django settings but may be useful in other cases. '
+        'Read more on `project\'s GitHub page '
+        '<https://github.com/kottenator/settings-overrider>`_.'
+    ),
+    url='https://github.com/kottenator/settings-overrider',
+    author='Rostyslav Bryzgunov',
+    author_email='kottenator@gmail.com',
+    license='MIT',
+    py_modules=['settings_overrider'],
+    install_requires=['PyYAML'],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ]
+)
