757b083fd6e1919a3b55eb8a60b00f71eaa4b379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:29 +0000

    Add package fionautil/0.4.3/fionautil-0.4.3-py3-none-any.whl

---
+++ packages/fionautil/fionautil-0.4.3-py3-none-any.whl/fionautil/geometry.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+from . import measure
+from .coords import roundring, roundpolyring
+
+__all__ = [
+    'endpoints',
+    'startpoint',
+    'endpoint',
+    'bbox',
+    'azimuth',
+    'disjointed',
+    'explodepoints',
+    'explodesegments',
+    'exploderings',
+    'countpoints',
+    'countsegments',
+    'roundgeometry'
+]
+
+
+def endpoints(geometry):
+    '''Return the first and last coordinates of a LineString or Multilinestring'''
+    coords = geometry['coordinates']
+
+    if geometry['type'] == 'MultiLineString':
+        return coords[0][0], coords[-1][-1]
+
+    elif geometry['type'] == 'LineString':
+        return coords[0], coords[-1]
+
+    raise ValueError("Improper Feature type.")
+
+
+def startpoint(geometry):
+    '''Return the first point in a linear geometry'''
+    start, _ = endpoints(geometry)
+    return start
+
+
+def endpoint(geometry):
+    '''Return the last point in a linear geometry'''
+    _, end = endpoints(geometry)
+    return end
+
+
+def bbox(geometry):
+    '''Return the bounds of a geometry (minx, miny, maxx, maxy)'''
+    x, y = list(zip(*list(explodepoints(geometry))))
+    return min(x), min(y), max(x), max(y)
+
+
+def azimuth(geometry, projection=None, radians=False, clockwise=False):
+    '''
+    return the azimuth between the start and end of a polyline geometry
+    :geometry object A geojson-like geometry object
+    :projection Proj
+    :radians bool
+    :clockwise bool
+    '''
+
+    if geometry['type'] not in ('LineString', 'MultiLineString'):
+        raise ValueError("This only works with PolyLine layers, this is: {}".format(geometry['type']))
+
+    first, last = endpoints(geometry)
+    latlong = projection.is_latlong()
+
+    if latlong:
+        x0, y0 = projection(*first, inverse=True)
+        x1, y1 = projection(*last, inverse=True)
+
+    else:
+        x0, y0 = first
+        x1, y1 = last
+
+    return measure.azimuth(x0, y0, x1, y1, radians=radians, clockwise=clockwise, latlong=latlong)
+
+
+def disjointed(shapes):
+    '''Reduce a list of shapely shapes to those that are disjoint from the others'''
+    newshapes = [shapes[0]]
+    for shape in shapes[1:]:
+        for i, other in enumerate(newshapes):
+            if shape.intersects(other):
+                newshapes[i] = other.union(shape)
+                break
+
+    return newshapes
+
+
+def explodepoints(geometry):
+    '''Generator that returns every coordinate a geometry'''
+    if geometry['type'] == 'Point':
+        yield geometry['coordinates']
+
+    else:
+        for ring in exploderings(geometry):
+            for point in ring:
+                yield point
+
+
+def explodesegments(geometry):
+    '''Generator that returns every line segment of a geometry'''
+    # Sure, could just use explodepoints, but isn't that a
+    # bit more memory-intensive, copying all those lists of coords?
+    if geometry['type'] in ('MultiLineString', 'Polygon'):
+        for ring in geometry['coordinates']:
+            for i, point in enumerate(ring[:-1]):
+                yield point, ring[i + 1]
+
+    elif geometry['type'] in ('LineString', 'MultiPoint'):
+        for i, point in enumerate(geometry['coordinates'][:-1]):
+            yield point, geometry['coordinates'][i + 1]
+
+    elif geometry['type'] == 'MultiPolygon':
+        for poly in geometry['coordinates']:
+            for ring in poly:
+                for i, point in enumerate(ring[:-1]):
+                    yield point, ring[i + 1]
+
+    else:
+        raise ValueError("Unknown or invalid geometry type: {}".format(geometry['type']))
+
+
+def exploderings(geometry):
+    '''Generator that returns every ring of a geometry.
+    A ring is a list of points'''
+    if geometry['type'] in ('MultiLineString', 'Polygon'):
+        for ring in geometry['coordinates']:
+            yield ring
+
+    elif geometry['type'] in ('LineString', 'MultiPoint'):
+        yield geometry['coordinates']
+
+    elif geometry['type'] == 'MultiPolygon':
+        for poly in geometry['coordinates']:
+            for ring in poly:
+                yield ring
+    else:
+        raise ValueError("Unkown geometry type: {}".format(geometry['type']))
+
+
+def countpoints(geometry):
+    '''Returns the number of points in a geometry'''
+    if geometry['type'] == 'Point':
+        return 1
+
+    else:
+        return sum(len(ring) for ring in exploderings(geometry))
+
+
+def countsegments(geometry):
+    '''Not guaranteed for (multi)point layers'''
+    if geometry['type'] == 'Point':
+        return 0
+
+    else:
+        return sum(len(ring) - 1 for ring in exploderings(geometry))
+
+
+def roundgeometry(geometry, precision=None):
+    '''Round all the coordinates in a geometry to a given precision. Defaults to 5.'''
+    precision = 5 if precision is None else precision
+
+    if geometry['type'] == 'Point':
+        geometry['coordinates'] = roundring([geometry['coordinates']], precision).pop(0)
+
+    elif geometry['type'] in ('LineString', 'MultiPoint'):
+        geometry['coordinates'] = roundring(geometry['coordinates'], precision)
+
+    elif geometry['type'] in ('MultiLineString', 'Polygon'):
+        geometry['coordinates'] = roundpolyring(geometry['coordinates'], precision)
+
+    elif geometry['type'] == 'MultiPolygon':
+        geometry['coordinates'] = [roundpolyring(pr, precision) for pr in geometry['coordinates']]
+
+    else:
+        raise ValueError("Unknown or invalid geometry type: {}".format(geometry['type']))
+
+    return geometry
---
+++ packages/fionautil/fionautil-0.4.3-py3-none-any.whl/fionautil/measure.py
@@ -0,0 +1,163 @@
+
+import math
+import pyproj
+
+WGS84 = pyproj.Geod(ellps='WGS84')
+
+
+def distance(x0, y0, x1, y1, latlong=True):
+    '''distance (in m) between two pairs of points'''
+    if latlong:
+        _, _, d = WGS84.inv(x0, y0, x1, y1)
+    else:
+        d = math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
+    return d
+
+
+def _projected_azimuth(x0, y0, x1, y1):
+    '''The angle of a line between two points on a cartesian plane. Always clockwise and in degrees'''
+    if y0 == y1:
+        if x0 == x1:
+            az = 0
+        elif x1 > x0:
+            az = math.pi / 2
+        else:
+            az = 3 * math.pi / 2
+    else:
+        az = math.atan2((x1 - x0), (y1 - y0))
+
+    return math.degrees(az)
+
+
+def azimuth(x0, y0, x1, y1, radians=False, clockwise=None, latlong=True):
+    '''azimuth between 2 geographic points'''
+    if latlong:
+        # this is always in angles
+        az, _, _ = WGS84.inv(x0, y0, x1, y1)
+    else:
+        az = _projected_azimuth(x0, y0, x1, y1)
+
+    if radians:
+        az = math.radians(az)
+
+    return az * (1 if clockwise else -1)
+
+
+def signed_area(coords):
+    """Return the signed area enclosed by a ring using the linear time
+    algorithm at http://www.cgafaq.info/wiki/Polygon_Area. A value >= 0
+    indicates a counter-clockwise oriented ring."""
+
+    try:
+        xs, ys = list(map(list, list(zip(*coords))))
+    except ValueError:
+        # Attempt to handle a z-dimension
+        xs, ys, _ = list(map(list, list(zip(*coords))))
+
+    xs.append(xs[1])
+    ys.append(ys[1])
+    return sum(xs[i] * (ys[i + 1] - ys[i - 1]) for i in range(1, len(coords))) / 2
+
+
+def clockwise(coords):
+    return signed_area(coords) < 0
+
+
+def counterclockwise(coords):
+    return signed_area(coords) >= 0
+
+
+def azimuth_distance(x0, y0, x1, y1, latlong=True):
+    '''Azimuth and distance between two points'''
+    if latlong:
+        az, _, dist = WGS84.inv(x0, y0, x1, y1)
+    else:
+        az = azimuth(x0, y0, x1, y1, latlong=latlong)
+        dist = distance(x0, y0, x1, y1, latlong=latlong)
+
+    return az, dist
+
+
+def det(a, b):
+    '''Determinant of a 2x2 matrix'''
+    return a[0] * b[1] - a[1] * b[0]
+
+
+def onsegment(segment, point):
+    '''Return True if point lies on segment'''
+    xs, ys = list(zip(*segment))
+    pbr = point[0], point[1], point[0], point[1]
+
+    if not intersectingbounds((min(xs), min(ys), max(xs), max(ys)), pbr):
+        return False
+
+    return (point[1] - ys[0]) * (xs[1] - xs[0]) == (ys[1] - ys[0]) * (point[0] - xs[0])
+
+
+def intersectingbounds(mbr1, mbr2):
+    '''Return True if two bounding boxes intersect, else False.'''
+    # Check if intersection is impossible
+    if mbr1[2] < mbr2[0] or mbr2[2] < mbr1[0] or mbr1[3] < mbr2[1] or mbr2[3] < mbr1[1]:
+        return False
+    else:
+        return True
+
+
+def coincidentendpoint(linem, linen):
+    '''Assuming segments linem and linem, return an overlapping point'''
+    if onsegment(linem, linen[0]):
+        return linen[0]
+    elif onsegment(linem, linen[1]):
+        return linen[1]
+    elif onsegment(linen, linem[0]):
+        return linem[0]
+    elif onsegment(linen, linem[1]):
+        return linem[1]
+    return None
+
+
+def intersect(linem, linen, detm=None):
+    '''
+    Check if two line segments intersect. Returns None or the intersection.
+    If lines are coincident, returns one of the midpoints.
+    linem sequence line segment 1, e.g. ((0, 0), (1, 1))
+    linen sequence line segment 2, e.g. [[0, 1], [0, 1]]
+    det float The determinant of linem. Precalculating might be useful if
+               calculating intersection with the same segment many times
+    '''
+    mx, my = list(zip(*linem))
+    nx, ny = list(zip(*linen))
+
+    mbrm = min(mx), min(my), max(mx), max(my)
+    mbrn = min(nx), min(ny), max(nx), max(ny)
+
+    if not intersectingbounds(mbrm, mbrn):
+        return None
+
+    xD = mx[0] - mx[1], nx[0] - nx[1]
+    yD = my[0] - my[1], ny[0] - ny[1]
+
+    div = det(xD, yD)
+
+    if div == 0:
+        try:
+            # Check if lines are parallel
+            if (xD[0] == xD[1] and yD[0] == yD[1]) or (yD[0] / xD[0] == yD[1] / xD[1]):
+                return coincidentendpoint(linem, linen)
+
+        except ZeroDivisionError:
+            pass
+
+        return None
+
+    else:
+        detm = detm or det(*linem)
+        detn = det(*linen)
+        x = det((detm, detn), xD) / div
+        y = det((detm, detn), yD) / div
+
+        if (intersectingbounds(mbrm, (x, y, x, y))
+                and intersectingbounds(mbrn, (x, y, x, y))):
+            return x, y
+        else:
+            return None
