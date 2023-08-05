ef5f7713b15f18dcb2a4d0b34c0a4468fdf57dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:37 +0000

    Add package mongoose/0.0.1/mongoose-0.0.1.tar.gz

---
+++ packages/mongoose/mongoose-0.0.1.tar.gz/mongoose-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "mongoose",
+    version = "0.0.1",
+    author = "Kale Franz",
+    author_email = "kale@franz.io",
+    description = ("Eating all the snakes."),
+)
