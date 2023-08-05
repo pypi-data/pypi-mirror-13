a263dcba7d98a4e77d1a7a5fc92e7c6e18dc3a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:51 +0000

    Add package xpibee/0.1.25/xpibee-0.1.25.tar.gz

---
+++ packages/xpibee/xpibee-0.1.25.tar.gz/xpibee-0.1.25/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='xpibee',
+    version='0.1.25',
+    packages=['xpibee'],
+    author='Sachin Patney',
+    license='MIT',
+    url='https://github.com/sachinio/pilibs/tree/master/src/xpibee',
+    description='Send commands via an XBee 2 (ZB)'
+)
