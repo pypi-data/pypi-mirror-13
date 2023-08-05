00538b0245d1ab3956223d11c26869d94e626817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:21 +0000

    Add package mdx-journal/0.1.2/mdx_journal-0.1.2.tar.gz

---
+++ packages/mdx-journal/mdx_journal-0.1.2.tar.gz/mdx_journal-0.1.2/mdx_journal/__meta__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# mdx-journal (c) 2014-2016 Ian Dennis Miller
+
+__version__ = '0.1.2'
+__author__ = 'Ian Dennis Miller'
+__email__ = 'iandennismiller@gmail.com'
+__url__ = 'https://github.com/iandennismiller/mdx_journal'
+__copyright__ = '2013-2016, Ian Dennis Miller'
