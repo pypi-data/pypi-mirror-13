d835daf45215ebcf23c4b5947fb97a5ebc9bbf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:15 +0000

    Add package my_python/1.0.0/my_python-1.0.0.zip

---
+++ packages/my_python/my_python-1.0.0.zip/my_python-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'my_python',
+        version = '1.0.0',
+        py_modules = ['my_python'],
+        author = '杨彦龙',
+        author_email = 'yangyanlongedu@163.com',
+        url  = 'e:\my_python',
+        description = 'A simple printer of nested lists',
+      )
