faa052532863142353d6b7207abe1a0304e03339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package pytest-pyramid-server/1.0.0/pytest-pyramid-server-1.0.0.tar.gz

---
+++ packages/pytest-pyramid-server/pytest-pyramid-server-1.0.0.tar.gz/pytest-pyramid-server-1.0.0/setup.py
@@ -0,0 +1,52 @@
+import os
+from setuptools import setup
+
+execfile(os.path.join(os.path.pardir, 'common_setup.py'))
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 5 - Production/Stable',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Utilities',
+    'Intended Audience :: Developers',
+    'Operating System :: POSIX',
+    'Framework :: Pyramid',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+install_requires = ['pytest-server-fixtures',
+                    'pytest',
+                    'pyramid',
+                    'waitress',
+                    ]
+
+tests_require = ['pytest-cov',
+                 'pyramid-debugtoolbar',
+                 ]
+
+entry_points = {
+    'pytest11': [
+        'pyramid_server = pytest_pyramid_server',
+    ],
+    'paste.app_factory': [
+        'pyramid_server_test = pyramid_server_test:main',
+    ],
+}
+
+if __name__ == '__main__':
+    kwargs = common_setup('pytest_pyramid_server')
+    kwargs.update(dict(
+        name='pytest-pyramid-server',
+        description='Pyramid server fixture for py.test',
+        author='Edward Easton',
+        author_email='eeaston@gmail.com',
+        classifiers=classifiers,
+        install_requires=install_requires,
+        tests_require=tests_require,
+        py_modules=['pytest_pyramid_server', 'pyramid_server_test'],
+        entry_points=entry_points,
+    ))
+    setup(**kwargs)
