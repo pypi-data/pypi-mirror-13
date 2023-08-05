49e18aba38379838a0ab845b150617a921694a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:12 +0000

    Add package PS_nester/1.1.0/PS_nester-1.1.0.zip

---
+++ packages/PS_nester/PS_nester-1.1.0.zip/PS_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'PS_nester',
+    version = '1.1.0',
+    py_modules = ['nester'],
+    author = 'purestar',
+    author_email = 'lastdesign@naver.com',
+    url = '',
+    description = '중첩된 목록의 간단한 프린터'
+    )
