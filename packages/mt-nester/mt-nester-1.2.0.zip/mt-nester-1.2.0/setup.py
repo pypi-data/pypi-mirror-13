92091a04af54ae514b16bb999a9581ea949f7e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:46 +0000

    Add package mt-nester/1.2.0/mt-nester-1.2.0.zip

---
+++ packages/mt-nester/mt-nester-1.2.0.zip/mt-nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'mt-nester',
+    version         = '1.2.0',
+    py_modules      = ['nester'],
+    author          = 'Michael Tong',
+    author_email    = 'python@minhth.com',
+    url             = 'http://minhth.com',
+    description     = 'A simple printer of nested lists',
+    )
