ae01cb5d73a0e66f393af3e576cb8396c975f23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package morpheuslib/0.2/morpheuslib-0.2.zip

---
+++ packages/morpheuslib/morpheuslib-0.2.zip/morpheuslib-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'morpheuslib',
+  packages = ['morpheuslib'], # this must be the same as the name above
+  version = '0.2',
+  description = 'The python Morpheus lib',
+  author = 'Miles Budden',
+  author_email = 'miles@budden.net',
+  url = 'https://github.com/SubjectRefresh/morpheus-python-lib', # use the URL to the github repo
+  download_url = 'https://github.com/SubjectRefresh/morpheus-python-lib/archive/master.zip', # I'll explain this in a second
+  keywords = ['morpheus', 'convert', 'node'], # arbitrary keywords
+  classifiers = [],
+)
