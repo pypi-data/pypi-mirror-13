81d32861f43135c75f4cefc4962ff202410e3931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:29 +0000

    Add package neutronpy/0.3.3/neutronpy-0.3.3.tar.gz

---
+++ packages/neutronpy/neutronpy-0.3.3.tar.gz/neutronpy-0.3.3/neutronpy/__init__.py
@@ -0,0 +1,21 @@
+r'''NeutronPy: open source python library for neutron scattering data analysis
+
+'''
+from __future__ import absolute_import
+
+__version__ = '0.3.3'
+
+from . import constants
+from . import core as tools
+from . import resolution
+from . import form_facs
+from . import functions
+from . import models
+from . import spurion
+from . import lattice
+from .core import *  # pylint: disable=wildcard-import
+
+try:
+    from .kmpfit import Fitter
+except ImportError:
+    print(ImportError(u'Fitter module is not loaded.'))
