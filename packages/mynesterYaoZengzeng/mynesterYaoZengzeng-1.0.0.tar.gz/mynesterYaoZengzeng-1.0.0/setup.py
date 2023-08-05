957dd3e4ee5ecf8e53c45bdcc8aca58d20e9e298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package mynesterYaoZengzeng/1.0.0/mynesterYaoZengzeng-1.0.0.tar.gz

---
+++ packages/mynesterYaoZengzeng/mynesterYaoZengzeng-1.0.0.tar.gz/mynesterYaoZengzeng-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'mynesterYaoZengzeng',
+	version	= '1.0.0',
+	py_modules = ['mynester'],
+	author	= 'Monster_Z',
+	author_email	= '461941420@qq.com',
+	url	= 'www.baidu.com',
+	description	= '......',
+)
