98a4ec1bc33b3605a386094f03f653298c57c160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:29 +0000

    Add package okerrclient/1.9.29/okerrclient-1.9.29.tar.gz

---
+++ packages/okerrclient/okerrclient-1.9.29.tar.gz/okerrclient-1.9.29/okerrclient/run.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+
+import okerrclient.taskseq as ts
+import sys
+import pwd
+import os
+import subprocess
+import shlex
+
+class RunTaskProcessor(ts.TaskProcessor):
+    chapter = 'External programs processor'
+
+
+class TaskRun(RunTaskProcessor):
+    help = 'run program'
+
+    enabled = 0
+
+    parse_args=False
+    store_argline='prog'
+
+
+    defargs = {
+        'prog': '',
+    }
+
+    tpconf = {
+        'enabled': '0',
+        'user': 'nobody',
+        'safebin': []
+    }
+
+    def run(self,ts,data,args):               
+            
+        callarg = shlex.split(args['prog'])
+        
+        # allowed?
+        if not ('*' in self.tpconf['safebin'] or callarg[0] in self.tpconf['safebin']):
+            ts.oc.log.error('{} is not in safebin: {}'.format(args['prog'], str(self.tpconf['safebin'])))
+            ts.stop()            
+            return None
+
+        try:
+            uid = pwd.getpwnam(self.tpconf['user']).pw_uid
+        except KeyError:
+            ts.oc.log.error('no such user {}'.format(self.tpconf['user']))
+            ts.stop()            
+            return None
+        
+               
+        p = subprocess.Popen(callarg, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn = lambda: os.setuid(uid))
+        p.wait()
+        res = p.communicate()
+        
+        if p.returncode or res[1]:
+            ts.oc.log.error('error code: {}, stderr: {}'.format(p.returncode, res[1]))
+            ts.stop()            
+            return None
+    
+        return res[0].split('\n')
+                
+TaskRun('RUN', ts.TaskSeq)
+
+        
+
