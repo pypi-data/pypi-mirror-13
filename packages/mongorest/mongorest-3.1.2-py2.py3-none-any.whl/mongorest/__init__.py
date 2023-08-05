15bcd7042045f7b53e57dd11bbd5d4543fba9286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:47 +0000

    Add package mongorest/3.1.2/mongorest-3.1.2-py2.py3-none-any.whl

---
+++ packages/mongorest/mongorest-3.1.2-py2.py3-none-any.whl/mongorest/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: UTF-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from .collection import Collection
+from .wrappers import Request, Response
+
+__version__ = '3.1.2'
