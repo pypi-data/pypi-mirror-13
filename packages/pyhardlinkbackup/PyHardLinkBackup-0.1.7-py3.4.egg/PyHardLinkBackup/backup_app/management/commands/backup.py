c43b924cdb4d2cb067f0eb431e1c397ead4eaf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:21 +0000

    Add package pyhardlinkbackup/0.1.7/PyHardLinkBackup-0.1.7-py3.4.egg

---
+++ packages/pyhardlinkbackup/PyHardLinkBackup-0.1.7-py3.4.egg/PyHardLinkBackup/backup_app/management/commands/backup.py
@@ -0,0 +1,18 @@
+
+from django.core.management.base import BaseCommand, CommandError
+
+from PyHardLinkBackup.phlb.phlb_main import HardLinkBackup
+
+
+class Command(BaseCommand):
+    help = "Start a Backup run"
+
+    def add_arguments(self, parser):
+        parser.add_argument("path",
+            help="Path to the source directory to backup"
+        )
+
+    def handle(self, *args, **options):
+        src_path=options["path"]
+        phlb = HardLinkBackup(src_path=src_path)
+        phlb.print_summary()
