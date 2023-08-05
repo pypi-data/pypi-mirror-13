f0b6b0a58eecccbcc9b1618c7bb9d77726bb1a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package evepi/0.61/evepi-0.61.tar.gz

---
+++ packages/evepi/evepi-0.61.tar.gz/evepi-0.61/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'evepi',
+      version = '0.61',
+      keywords = ('evepi', 'ultrason', 'arduino'),
+      url = 'https://github.com/zhangxiansheng',
+      description = 'evepi',
+      license = 'public domain',
+      
+      author = 'Zhang Yiwei',
+      author_email = 'Karl1991223@126.com',
+      
+      packages = find_packages(),
+      platforms = 'any',
+      )
