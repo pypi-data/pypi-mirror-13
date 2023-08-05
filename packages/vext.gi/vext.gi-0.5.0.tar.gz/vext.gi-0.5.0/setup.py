99e06360ea459a9505cb52b4d733a1d4db8dff31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:02 +0000

    Add package vext.gi/0.5.0/vext.gi-0.5.0.tar.gz

---
+++ packages/vext.gi/vext.gi-0.5.0.tar.gz/vext.gi-0.5.0/setup.py
@@ -0,0 +1,83 @@
+from codecs import open  # To use a consistent encoding
+from glob import glob
+from os.path import dirname, abspath, join
+from sys import prefix
+from subprocess import call
+
+from distutils import sysconfig
+from setuptools import setup
+from setuptools.command.install import install
+
+here=dirname(abspath(__file__))
+site_packages_path = sysconfig.get_python_lib()
+vext_files = glob("*.vext")
+
+def _post_install():
+    from vext.install import check_sysdeps, install_vexts
+    install_vexts(vext_files)  # data_files doesn't work in pip7 so do it ourselves
+    check_sysdeps(join(here, *vext_files))
+
+class Install(install):
+    def run(self):
+        self.do_egg_install()
+        self.execute(_post_install, [], msg="Check system dependencies:")
+
+# Get the long description from the relevant file
+with open(join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='vext.gi',
+    zip_safe=False,
+
+    cmdclass={
+          'install': Install,
+    },
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.5.0',
+
+    description='Use system python packages in a virtualenv',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/stuaxo/vext',
+
+    # Author details
+    author='Stuart Axon',
+    author_email='stuaxo2@yahoo.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='virtualenv gi vext gtk',
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    setup_requires=["setuptools>=0.18.8"],
+    install_requires=["vext>=0.5.0"],
+)
