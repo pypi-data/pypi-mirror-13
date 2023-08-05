336aeced5ed80eafd257a6bf6624f0377e9bfabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package notebook_cleaner/0.1.5/notebook_cleaner-0.1.5.tar.gz

---
+++ packages/notebook_cleaner/notebook_cleaner-0.1.5.tar.gz/notebook_cleaner-0.1.5/notebook_cleaner/notebook_cleaner.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+""" notebook_cleaner
+
+Removes code and prompts from an HTML export of an iPython notebook.
+
+Usage:
+  notebook_cleaner <infile> [<outfile>]
+  notebook_cleaner (-h | --help)
+
+Option:
+  -h --help  	Prints this message
+  -f --force    Don't prompt when execution will overwrite existing file
+"""
+
+import sys
+from bs4 import BeautifulSoup
+from docopt import docopt
+
+JUNK_CLASSES = ['input', 'prompt']
+
+def notebook_cleaner(infile, outfile):
+	""" Removes code and prompts from HTML exports of an iPython notebook """
+
+	with open(infile, 'r') as in_f:
+		soup = BeautifulSoup(in_f.read(), 'html.parser')
+
+	junk_tags = soup.find_all(class_=JUNK_CLASSES)
+	for tag in junk_tags:
+		tag.decompose()
+
+	with open(outfile, 'w') as out_f:
+		out_f.write(str(soup))
+
+def main():
+	""" Flow for command line usage """
+	cli_context = docopt(__doc__)
+	infile = cli_context['<infile>']
+	outfile = cli_context['<outfile>']
+
+	if infile.split('.')[-1].lower() != 'html':
+		raise Exception("Not reading from a *.html file")
+
+	if not outfile:
+		outfile = infile
+
+	if infile == outfile:
+		confirm = input("Overwrite {0}? (y/n):\t".format(infile))
+		if confirm.lower() not in ['y', 'yes']:
+			return 0
+
+	notebook_cleaner(infile, outfile)
+	print("New file {0}".format(outfile))
+	sys.exit(0)
+
+if __name__ == '__main__':
+	main()
---
+++ packages/notebook_cleaner/notebook_cleaner-0.1.5.tar.gz/notebook_cleaner-0.1.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+install_requires = ['docopt', 'beautifulsoup4']
+
+setup(
+    name='notebook_cleaner',
+    version='0.1.5',
+    description="Remove code from HTML files rendered from Jupyter notebooks",
+    url="https://github.com/RAvdek/notebook_cleaner",
+    author="Russell Avdek",
+    author_email="russell.avdek@gmail.com",
+    license="MIT",
+    keywords='ipython',
+    install_requires=install_requires,
+    packages=['notebook_cleaner'],
+    entry_points = { 'console_scripts': ['notebook_cleaner = notebook_cleaner.notebook_cleaner:main'] },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+       'Programming Language :: Python :: 3.2',
+       'Programming Language :: Python :: 3.3',
+       'Programming Language :: Python :: 3.4',
+    ],
+)
+
