500ec311d19376febe642941b131b79ab6505733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package sopaper/0.3/sopaper-0.3.tar.gz

---
+++ packages/sopaper/sopaper-0.3.tar.gz/sopaper-0.3/setup.py
@@ -0,0 +1,20 @@
+#from distutils.core import setup
+from setuptools import setup
+kwargs = dict(
+  name = 'sopaper',
+  version = '0.3',
+  description = 'Automatically search and download paper',
+  author = 'Yuxin Wu',
+  author_email = 'ppwwyyxxc@gmail.com',
+  url = 'https://github.com/ppwwyyxx/sopaper',
+  download_url = 'https://github.com/ppwwyyxx/sopaper/tarball/0.3',
+  keywords = ['Utility'],
+  packages = ['sopaper', 'sopaper.fetcher',
+              'sopaper.lib', 'sopaper.searcher'],
+  entry_points={
+      'console_scripts': ['sopaper = sopaper.__main__:main']
+  },
+  include_package_data=True,
+  install_requires=['termcolor', 'requests', 'beautifulsoup4']
+)
+setup(**kwargs)
