c0fe36377e9e6887c3588da79fdf58c19dc7f1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:59 +0000

    Add package nealpan_nester/1.0.0/nealpan_nester-1.0.0.tar.gz

---
+++ packages/nealpan_nester/nealpan_nester-1.0.0.tar.gz/nealpan_nester-1.0.0/nealpan_nester.py
@@ -0,0 +1,7 @@
+"""这是nester.py模块"""
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
