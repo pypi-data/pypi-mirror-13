c37fa55017df94c38ed2087269003c9abe76514b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package luthor/1.0.0/luthor-1.0.0.tar.gz

---
+++ packages/luthor/luthor-1.0.0.tar.gz/luthor-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+
+
+      name = 'luthor',
+      version = '1.0.0',
+	py_modules = ['nester'],
+	author ='lex',
+	author_email ='295720614@qq.com',
+	url = 'http://www.baidu.com',
+	description = 'aaaaa',
+         )
