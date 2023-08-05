15c5bb5c32cd515cad013b11cd9a92b5726bc221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package CodeFlow/0.0.2/CodeFlow-0.0.2.tar.gz

---
+++ packages/CodeFlow/CodeFlow-0.0.2.tar.gz/CodeFlow-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='CodeFlow',
+      author='Sven Wiltink',
+      author_email='sven.wiltink@hotmail.com',
+      url='https://github.com/SvenWiltink/PyFlow',
+      version='0.0.2',
+      packages=['PyFlow'],
+      package_dir={'': 'src'}
+      )
