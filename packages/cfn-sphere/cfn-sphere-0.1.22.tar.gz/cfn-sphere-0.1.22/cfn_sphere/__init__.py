68ab3375d45d7b1e801034c9d5972ada26edbf61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:29 +0000

    Add package cfn-sphere/0.1.22/cfn-sphere-0.1.22.tar.gz

---
+++ packages/cfn-sphere/cfn-sphere-0.1.22.tar.gz/cfn-sphere-0.1.22/cfn_sphere/__init__.py
@@ -0,0 +1,66 @@
+__version__ = '0.1.22'
+
+from cfn_sphere.template.transformer import CloudFormationTemplateTransformer
+from cfn_sphere.stack_configuration.dependency_resolver import DependencyResolver
+from cfn_sphere.stack_configuration.parameter_resolver import ParameterResolver
+from cfn_sphere.aws.cfn import CloudFormation
+from cfn_sphere.file_loader import FileLoader
+from cfn_sphere.aws.cfn import CloudFormationStack
+from cfn_sphere.custom_resources import CustomResourceHandler
+from cfn_sphere.util import get_logger
+
+
+class StackActionHandler(object):
+    def __init__(self, config):
+        self.logger = get_logger(root=True)
+        self.config = config
+        self.region = config.region
+        self.cfn = CloudFormation(region=self.region)
+        self.parameter_resolver = ParameterResolver(region=self.region)
+
+    def create_or_update_stacks(self):
+        existing_stacks = self.cfn.get_stack_names()
+        desired_stacks = self.config.stacks
+        stack_processing_order = DependencyResolver().get_stack_order(desired_stacks)
+
+        if len(stack_processing_order) > 1:
+            self.logger.info(
+                "Will process stacks in the following order: {0}".format(", ".join(stack_processing_order)))
+
+        for stack_name in stack_processing_order:
+            stack_config = self.config.stacks.get(stack_name)
+
+            template_url = stack_config.template_url
+            working_dir = stack_config.working_dir
+
+            raw_template = FileLoader.get_file_from_url(template_url, working_dir)
+            template = CloudFormationTemplateTransformer.transform_template(raw_template)
+
+            parameters = self.parameter_resolver.resolve_parameter_values(stack_config.parameters, stack_name)
+            stack = CloudFormationStack(template, parameters, stack_name, self.region, stack_config.timeout)
+
+            if stack_name in existing_stacks:
+
+                self.cfn.validate_stack_is_ready_for_action(stack)
+                self.cfn.update_stack(stack)
+            else:
+                self.cfn.create_stack(stack)
+
+            CustomResourceHandler.process_post_resources(stack)
+
+    def delete_stacks(self):
+        existing_stacks = self.cfn.get_stack_names()
+        stacks = self.config.stacks
+
+        stack_processing_order = DependencyResolver().get_stack_order(stacks)
+        stack_processing_order.reverse()
+
+        self.logger.info("Will delete stacks in the following order: {0}".format(", ".join(stack_processing_order)))
+
+        for stack_name in stack_processing_order:
+            if stack_name in existing_stacks:
+                stack = CloudFormationStack(None, None, stack_name, None, None)
+                self.cfn.validate_stack_is_ready_for_action(stack)
+                self.cfn.delete_stack(stack)
+            else:
+                self.logger.info("Stack {0} is already deleted".format(stack_name))
