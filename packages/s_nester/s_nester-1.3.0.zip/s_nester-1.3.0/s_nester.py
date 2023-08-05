c2c104bcfdbc28d4c343abe3291e4ae9963983c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package s_nester/1.3.0/s_nester-1.3.0.zip

---
+++ packages/s_nester/s_nester-1.3.0.zip/s_nester-1.3.0/s_nester.py
@@ -0,0 +1,29 @@
+"""This is the “nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists"""
+##for movie in movies:
+##    if isinstance(movie,list):
+##        for mov in movie:
+##            if isinstance(mov,list):
+##                for mo in mov:
+##                    print (mo)    
+##            else:
+##                print(mov)
+##    else:
+##        print (movie)
+
+def print_lol(theList,indent=False,level=0):
+        
+    """This function takes a positional argument called “the_list", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line. A second
+    argument called “level" is used to insert tab-stops when a nested list
+    is encountered """
+    for item in theList:
+        if isinstance(item,list):
+            print_lol(item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print ("\t",end='')
+            print(item)
+
