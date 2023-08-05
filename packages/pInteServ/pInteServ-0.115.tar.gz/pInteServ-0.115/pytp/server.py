4f737f04394bef971ff57602a88bca034a4637c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package pInteServ/0.115/pInteServ-0.115.tar.gz

---
+++ packages/pInteServ/pInteServ-0.115.tar.gz/pInteServ-0.115/pytp/server.py
@@ -0,0 +1,16 @@
+from pytp import connectsession
+import logging
+import os
+
+
+def main():
+    """
+    The main function for starting a server.  Usage is piserver
+    """
+    wd = os.path.join(os.path.expanduser('~'), 'pInteServer')
+    os.makedirs(wd, exist_ok=True)
+    os.chdir(wd)
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='pInteServ.log', level=logging.INFO)
+    server = connectsession.ServerSocket(46000)
+    server.activate()
+
