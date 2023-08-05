1b7224daad242c28cd5f85c919c7955ffd8b62d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package ZY_nester/1.1.0/ZY_nester-1.1.0.zip

---
+++ packages/ZY_nester/ZY_nester-1.1.0.zip/ZY_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name    ='ZY_nester',
+    version ='1.1.0',
+    py_modules  =['ZY_nester'],
+    author  ='ZhangYang',
+    author_email    ='50633651@qq.com',
+    url ='http://www.starfish.im',
+    description ='A simply printer of nested lists',
+    )
