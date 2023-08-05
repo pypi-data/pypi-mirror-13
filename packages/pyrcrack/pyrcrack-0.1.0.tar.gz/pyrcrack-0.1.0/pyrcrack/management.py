130b4eef2bc5fd6048a3ee5007c0834b913f8a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:51 +0000

    Add package pyrcrack/0.1.0/pyrcrack-0.1.0.tar.gz

---
+++ packages/pyrcrack/pyrcrack-0.1.0.tar.gz/pyrcrack-0.1.0/pyrcrack/management.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+import subprocess
+from contextlib import suppress
+from . import Air, PATH
+
+
+class Airmon(Air):
+    """
+        Airmon-ng object.
+
+        It doubles as a context manager.
+        So you can call it as
+
+        ::
+
+            wifi = Airmon().start('wlan0')
+            Airmon('smoothie0').stop()
+            Airmon('wlan0').check()
+
+        or as
+
+        ::
+            with Airmon('wlan0') as f:
+                print(f.interface)
+
+    """
+
+    def __init__(self, interface):
+        """
+            We need this to use the context manager.
+            So I decided to make interface mandatory
+        """
+        self.interface = interface  #: Wireless interface
+        super(self.__class__, self).__init__()
+
+    def _do_action(self, what):
+        """
+            Execute airmon-ng with MON_PREFIX and PATH set.
+        """
+        env = {'PATH': PATH, 'MON_PREFIX': 'smoothie'}
+        return subprocess.check_output(["airmon-ng", what,
+                                        self.interface], env=env)
+
+    def start(self):
+        """
+            Executes
+
+            ::
+
+                airmon-ng  start <WIFI>
+
+            and replaces self.interface with the monitor interface.
+        """
+        ret = self._do_action('start')
+
+        for asg in re.finditer(r'(.*) on (.*)\)', ret.decode()):
+            self.interface = asg.group(2)
+            return self.interface
+
+    def stop(self):
+        """
+            Stops monitor mode on current interface
+        """
+        with suppress(subprocess.CalledProcessError):
+            self._do_action('stop')
+
+    def __enter__(self, *args, **kwargs):
+        self.start(*args, **kwargs)
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.stop()
+        return self
