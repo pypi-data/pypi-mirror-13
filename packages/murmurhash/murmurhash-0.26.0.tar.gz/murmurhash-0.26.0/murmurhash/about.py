1080d6d6b59d29abbed1fde616fb1578204224b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package murmurhash/0.26.0/murmurhash-0.26.0.tar.gz

---
+++ packages/murmurhash/murmurhash-0.26.0.tar.gz/murmurhash-0.26.0/murmurhash/about.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.26.0'
+version = '0.26.0'
+full_version = '0.26.0'
+git_revision = '94287d50afabbfc12d717198c7ed2db3e9d09d85'
+release = True
+if not release:
+    version = full_version
