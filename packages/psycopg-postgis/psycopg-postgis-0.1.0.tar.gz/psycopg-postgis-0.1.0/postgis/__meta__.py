2d055e4519c95a8bd1580a3c9190745d1046c7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package psycopg-postgis/0.1.0/psycopg-postgis-0.1.0.tar.gz

---
+++ packages/psycopg-postgis/psycopg-postgis-0.1.0.tar.gz/psycopg-postgis-0.1.0/postgis/__meta__.py
@@ -0,0 +1,6 @@
+VERSION = (0, 1, 0)
+
+__author__ = 'Yohan Boniface'
+__contact__ = "yohan.boniface@data.gouv.fr"
+__homepage__ = "https://github.com/yohanboniface/psycopg-postgis"
+__version__ = ".".join(map(str, VERSION))
