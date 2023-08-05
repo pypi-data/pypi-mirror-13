fb3de668e61a6555955276c9f03d9ac4ddf2d728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:52 +0000

    Add package justplot/0.1.2a4/justplot-0.1.2a4.tar.gz

---
+++ packages/justplot/justplot-0.1.2a4.tar.gz/justplot-0.1.2a4/README.md
@@ -0,0 +1,2 @@
+# justplot
+A thin wrapper around matplotlib, which lets you reuse settings.
---
+++ packages/justplot/justplot-0.1.2a4.tar.gz/justplot-0.1.2a4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='justplot',
+    version='0.1.2a4',
+    packages=['justplot',],
+    license='MIT',
+    author='Uğur Çayoğlu',
+    author_email='urcyglu@gmail.com',
+    description='A thin wrapper around matplotlib, which lets you reuse settings.',
+    url='https://github.com/OnionNinja/justplot',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+    ],
+    classifiers=[
+        # More at http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+      ],
+    keywords=['plots','matplotlib','wrapper'],
+    include_package_data=True,
+)
+
