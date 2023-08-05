f58c5fd4744176889e277c06a8b8dcd87144edf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:02 +0000

    Add package testhello/0.1/testhello-0.1.tar.gz

---
+++ packages/testhello/testhello-0.1.tar.gz/testhello-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='testhello',
+      version='0.1',
+      description='Hello test program',
+      author='Aqeel',
+      author_email='testhello@example.com',
+      license='MIT',
+      packages=['testhello'],
+      zip_safe=False)
