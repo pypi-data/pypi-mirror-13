ee8039a9ef8dfc991f8e089e9662c06fb3d68bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package LJH_nester/1.3.0/LJH_nester-1.3.0.zip

---
+++ packages/LJH_nester/LJH_nester-1.3.0.zip/LJH_nester-1.3.0/test.py
@@ -0,0 +1,10 @@
+# This is my module for displaye a multiple layers list
+def print_lol(param, indent = False,tabStop = 0):
+    if isinstance(param, list):
+        for inner_list in param:
+            print_lol(inner_list, tabStop + 1)
+    else:
+        if indent:
+            for tmpNum in range(tabStop-1):
+                print("\t", end='')
+        print(param)
