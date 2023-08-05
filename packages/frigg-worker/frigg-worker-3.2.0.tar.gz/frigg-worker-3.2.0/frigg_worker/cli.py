2979f1f607ae25900fc77723213e92fff88840f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:18 +0000

    Add package frigg-worker/3.2.0/frigg-worker-3.2.0.tar.gz

---
+++ packages/frigg-worker/frigg-worker-3.2.0.tar.gz/frigg-worker-3.2.0/frigg_worker/cli.py
@@ -0,0 +1,56 @@
+# -*- coding: utf8 -*-
+import logging.config
+import socket
+
+import click
+
+from .fetcher import fetch_builds, fetch_deployments
+from .log_helpers import load_logging_config
+
+logger = logging.getLogger('frigg_worker.cli')
+
+
+@click.command()
+@click.argument('mode')
+@click.option('--dispatcher-url', default=None, help='URL to the dispatcher, overrides settings')
+@click.option('--dispatcher-token', default=None, help='Token for dispatcher, overrides settings')
+@click.option('--hq-url', default=None, help='URL for frigg-hq, overrides settings')
+@click.option('--hq-token', default=None, help='Token for frigg-hq, overrides settings')
+@click.option('--slack-url', default=None, help='URL for incoming webhook in slack')
+@click.option('--sentry-dsn', default=None, help='Sentry dsn needed to connect to the sentry API')
+@click.option('--loglevel', default='DEBUG', help='Set log level for frigg-packages')
+def start(mode, **kwargs):
+    options = {
+        'slack_icon': ':monkey_face:',
+        'slack_channel': '#workforce'
+    }
+    options.update(kwargs)
+    logging.config.dictConfig(load_logging_config(options))
+
+    try:
+        from raven import Client
+        options['sentry'] = Client(options['sentry_dsn'])
+    except ImportError:
+        options['sentry'] = None
+
+    options['worker_host'] = socket.getfqdn()
+
+    _start(mode, **options)
+
+
+def _start(mode, **options):
+    try:
+        logger.info('Starting frigg worker')
+        if mode == 'builder':
+            fetch_builds(**options)
+        elif mode == 'deployer':
+            fetch_deployments(**options)
+        else:
+            print('Unknown mode {0}'.format(mode))
+    except Exception as e:
+        logger.exception(e)
+        _start(mode, **options)
+
+
+if __name__ == '__main__':
+    start()
---
+++ packages/frigg-worker/frigg-worker-3.2.0.tar.gz/frigg-worker-3.2.0/setup.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf8 -*-
+import re
+import sys
+import codecs
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+with open('frigg_worker/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+setup(
+    name='frigg-worker',
+    version=version,
+    description='A worker application that listens to the frigg broker '
+                'an pick up builds and build them.',
+    long_description=read('README.rst'),
+    author='The frigg team',
+    author_email='hi@frigg.io',
+    license='MIT',
+    url='https://github.com/frigg/frigg-worker',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'click==5.1',
+        'frigg-coverage>=1.2.0,<2.0.0',
+        'frigg-settings>=1.1.1,<2.0.0',
+        'docker-wrapper>=2.1,<2.2',  # rq.filter: <2.0
+        'pyyaml==3.11',
+        'requests>=2.9.0,<3.0.0',
+        'raven==5.8.1'
+    ],
+    entry_points={
+        'console_scripts': ['frigg-worker = frigg_worker.cli:start']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+    ]
+)
