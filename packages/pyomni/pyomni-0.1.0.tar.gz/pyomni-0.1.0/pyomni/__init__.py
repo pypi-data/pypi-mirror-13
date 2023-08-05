43396dd210d8aef0c14419f309df11209cc3e5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package pyomni/0.1.0/pyomni-0.1.0.tar.gz

---
+++ packages/pyomni/pyomni-0.1.0.tar.gz/pyomni-0.1.0/pyomni/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from .pyomni import PyOmni
+from .omni_task import OmniTask
+
+__author__ = 'Takuro Wada'
+__version__ = '0.1.0'
+__url__ = 'https://github.com/taxpon/pyomni/blob/master/pyomni/pyomni.py'
+__license__ = 'MIT'
