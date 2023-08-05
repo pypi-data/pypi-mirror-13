ff5d29c6e6fe924aaed184a40c4ec8dd9fc8031f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package django-sp/0.1b2/django-sp-0.1b2.tar.gz

---
+++ packages/django-sp/django-sp-0.1b2.tar.gz/django-sp-0.1b2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+readme = open('README.md').read()
+
+from socialprofile import __version__ as version
+
+setup(
+    name="django-sp",
+    version=version,
+    url='https://github.com/DLRSP/django-sp',
+    license='MIT',
+    description="Django Custom Social Profile Auth/User",
+    author='DLRSP',
+    author_email='dlrsp.py@gmail.com',
+    packages=['socialprofile', ],
+    long_description=readme,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['django_nose',
+                      'pytest-runner',
+                      'django>=1.8',
+                      'python-social-auth',		#<-- Need By Auth Process
+                      'django-errors',
+                      ],
+    tests_require=['runtests.py'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP ',
+    ]
+)
