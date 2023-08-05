c0068db3953fc5abd0025261912a13683f06a948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:30 +0000

    Add package FaoFaoNon/1.0/FaoFaoNon-1.0.tar.gz

---
+++ packages/FaoFaoNon/FaoFaoNon-1.0.tar.gz/FaoFaoNon-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup, Extension
+setup(name='FaoFaoNon',
+      version='1.0',
+      author='panthotanvir',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      py_modules=['ipv'],
+    )
