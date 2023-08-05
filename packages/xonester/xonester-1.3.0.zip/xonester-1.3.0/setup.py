74281e16db70e1f8ae59c4b3e3f8fae124d39d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package xonester/1.3.0/xonester-1.3.0.zip

---
+++ packages/xonester/xonester-1.3.0.zip/xonester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'xonester',
+        version      = '1.3.0',
+        py_modules   = ['xonester'],
+        author       = 'XOrange',
+        author_email = 'y97506@126.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+    )
