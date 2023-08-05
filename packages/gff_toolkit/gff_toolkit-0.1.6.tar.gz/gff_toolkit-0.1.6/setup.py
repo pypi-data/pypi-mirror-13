a49da5689c3cb2aa438d88bb69dfb0a3bc6779f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:07 +0000

    Add package gff_toolkit/0.1.6/gff_toolkit-0.1.6.tar.gz

---
+++ packages/gff_toolkit/gff_toolkit-0.1.6.tar.gz/gff_toolkit-0.1.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+__author__ = 'rensholmer'
+__created__ = '17/08/15'
+
+import sys
+from setuptools import setup
+
+def main():
+	setup(name='gff_toolkit',
+	      packages=['gff_toolkit'],
+	      author='rens holmer',
+	      author_email='rens.holmer@wur.nl',
+	      version='0.1.6',
+	      py_modules=['gff','gffsubpart','parser','test','__init__'],
+	      url='https://github.com/holmrenser/gff_toolkit',
+	      install_requires=['intervaltree']
+	      )
+
+if __name__ == '__main__':
+	main()
