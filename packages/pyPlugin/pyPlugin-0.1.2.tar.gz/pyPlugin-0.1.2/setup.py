2569fc036360b2d7007913950af25ea3e543df83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:33 +0000

    Add package pyPlugin/0.1.2/pyPlugin-0.1.2.tar.gz

---
+++ packages/pyPlugin/pyPlugin-0.1.2.tar.gz/pyPlugin-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pyPlugin",
+    version="0.1.2",
+    description="Simple framework-less plugin loader for Python",
+    author="Source Simian",
+    author_email='sourcesimian@users.noreply.github.com',
+    url='https://github.com/sourcesimian/pyPlugin',
+    license='MIT',
+    packages=['pyplugin'],
+    install_requires=[],
+    download_url="https://github.com/sourcesimian/pyPlugin/tarball/v0.1.2",
+)
