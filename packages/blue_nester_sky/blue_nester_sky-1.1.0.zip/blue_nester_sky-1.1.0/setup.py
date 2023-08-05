6de0eccecf804c4339b7770976747cafb44a2483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package blue_nester_sky/1.1.0/blue_nester_sky-1.1.0.zip

---
+++ packages/blue_nester_sky/blue_nester_sky-1.1.0.zip/blue_nester_sky-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name          ='blue_nester_sky',
+	  version       ='1.1.0',
+	  py_moduler    =['blue_nester_sky'],
+	  author        ='bluesky',
+	  author_email  ='liuxl@dtdream.com',
+	  url           ='http://www.dtdream.com',
+	  description   ='a simple program of print function',
+	  )
