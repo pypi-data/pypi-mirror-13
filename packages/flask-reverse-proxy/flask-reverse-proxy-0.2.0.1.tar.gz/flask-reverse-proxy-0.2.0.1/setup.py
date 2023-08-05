c8897027166c3aada0a15d203d97525c3bcdca91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:48 +0000

    Add package flask-reverse-proxy/0.2.0.1/flask-reverse-proxy-0.2.0.1.tar.gz

---
+++ packages/flask-reverse-proxy/flask-reverse-proxy-0.2.0.1.tar.gz/flask-reverse-proxy-0.2.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='flask-reverse-proxy',
+    version='0.2.0.1',
+    packages=['flask_reverse_proxy'],
+    url='',
+    license='',
+    author='Wilberto Morales',
+    author_email='wilbertomorales777@gmail.com',
+    description=''
+)
