25d1547e7d37848f83cadf6969b8bac010015a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:31 +0000

    Add package hunter_nester/1.0.0/hunter_nester-1.0.0.tar.gz

---
+++ packages/hunter_nester/hunter_nester-1.0.0.tar.gz/hunter_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+name="hunter_nester",
+version="1.0.0",
+py_modules=["hunter_nester"],
+author="HunterOS",
+author_email="rahul.soshte47@gmail.com",
+description="coolio"
+)
