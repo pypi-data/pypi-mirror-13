96a6aae1bfa3299690cee5863f0297b88948906a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package gthnk/0.4.0/gthnk-0.4.0.tar.gz

---
+++ packages/gthnk/gthnk-0.4.0.tar.gz/gthnk-0.4.0/gthnk/models/day.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+# gthnk (c) 2014-2016 Ian Dennis Miller
+
+import StringIO
+import datetime
+import re
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from flask.ext.diamond.utils.mixins import CRUDMixin
+from sqlalchemy import desc
+from sqlalchemy.ext.orderinglist import ordering_list
+from wand.image import Image
+
+from .. import db
+
+
+class Day(db.Model, CRUDMixin):
+    """
+    A Day consists of the Entry objects that were created on that day.
+    This is just a convenient way of referring to Entry objects in the database.
+    This object is also capable of creating a string that is parsable by the JournalBuffer
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, unique=True)
+
+    pages = db.relationship("Page", order_by="Page.sequence",
+        collection_class=ordering_list('sequence', reorder_on_append=True),
+        backref=db.backref("day"))
+
+    def add_page(self, binary):
+        from page import Page
+        page = Page.create(day=self)
+        page.set_image(binary=binary)
+        self.pages.append(page)
+        self.pages.reorder()
+        db.session.commit()
+
+    def attach(self, binary):
+        # determine the format of the file
+        binary_format = Image(blob=binary).format
+        if binary_format == "PDF":
+            # use PyPDF2 to read the stream
+            pdf = PdfFileReader(StringIO.StringIO(binary))
+            if pdf.getNumPages() > 1:  # if it is a multi-page PDF
+                for pdf_page in pdf.pages:  # add the pages individually
+                    output = PdfFileWriter()
+                    output.addPage(pdf_page)
+
+                    pdf_page_buf = StringIO.StringIO()
+                    output.write(pdf_page_buf)
+                    self.add_page(pdf_page_buf.getvalue())
+            else:  # if it is just a single page PDF
+                # then add the original bytestream
+                self.add_page(binary)
+        elif binary_format:
+            self.add_page(binary)
+        else:  # could not recognize file
+            pass
+
+    def render_pdf(self):
+        outpdf = PdfFileWriter()
+        for page in self.pages:
+            if page.extension == "pdf":
+                outpdf.addPage(PdfFileReader(StringIO.StringIO(page.binary)).getPage(0))
+            else:
+                img = Image(blob=page.binary, resolution=72)
+                img.format = "jpg"
+                img.format = "pdf"
+                outpdf.addPage(PdfFileReader(StringIO.StringIO(img.make_blob())).getPage(0))
+
+        pdf_page_buf = StringIO.StringIO()
+        outpdf.write(pdf_page_buf)
+        return pdf_page_buf.getvalue()
+
+    def yesterday(self):
+        return self.query.filter(Day.date < self.date).order_by(desc(Day.date)).first()
+
+    def tomorrow(self):
+        return self.query.filter(Day.date > self.date).order_by(Day.date).first()
+
+    def render(self):
+        from entry import Entry
+        buf = datetime.datetime.strftime(self.date, "%Y-%m-%d")
+        for entry in self.entries.order_by(Entry.timestamp).all():
+            buf += unicode(entry)
+        buf += "\n\n"
+        return buf
+
+    def render_markdown(self):
+        buf = self.render()
+        buf = re.sub(r'(\d\d\d\d-\d\d-\d\d)\n', '# \g<1>\n', buf)
+        buf = re.sub(r'(\d\d\d\d)\n', '## \g<1>\n', buf)
+
+        img_fmt = "[![{sequence}](../thumbnail/{attachment})](../attachment/{attachment})\n"
+
+        if len(self.pages) > 0:
+            buf += "# Attachments\n\n"
+            for page in self.pages:
+                buf += img_fmt.format(
+                    sequence=page.sequence,
+                    thumbnail=page.filename(extension="jpg"),
+                    attachment=page.filename()
+                )
+
+        return buf
+
+    def __repr__(self):
+        return "<Day: {}>".format(self.date)
+
+    def __unicode__(self):
+        return repr(self)
+
+
+def latest():
+    return Day.query.order_by(desc(Day.date)).first()
---
+++ packages/gthnk/gthnk-0.4.0.tar.gz/gthnk-0.4.0/gthnk/models/page.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# gthnk (c) 2014-2016 Ian Dennis Miller
+
+from flask.ext.diamond.utils.mixins import CRUDMixin
+from .. import db
+from wand.image import Image
+from wand.color import Color
+
+
+class Page(db.Model, CRUDMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    day_id = db.Column(db.Integer, db.ForeignKey('day.id'))
+    sequence = db.Column(db.Integer)
+    binary = db.Column(db.Binary)
+    title = db.Column(db.Unicode(1024))
+    thumbnail = db.Column(db.Binary)
+    preview = db.Column(db.Binary)
+    extension = db.Column(db.String(32))
+
+    def set_image(self, binary):
+        self.binary = binary
+        with Image(blob=self.binary, resolution=150) as img:
+            self.extension = img.format.lower()
+            flattened = Image(background=Color("white"),
+                height=img.height, width=img.width)
+            flattened.composite(img, left=0, top=0)
+            flattened.format = "jpeg"
+            flattened.compression_quality = 50
+
+            thumbnail = flattened.clone()
+            thumbnail.transform(resize='150x200>')
+            self.thumbnail = thumbnail.make_blob()
+
+            preview = flattened.clone()
+            preview.gaussian_blur(radius=1, sigma=0.5)
+            preview.transform(resize='612x792>')
+            self.preview = preview.make_blob()
+        self.save()
+
+    def filename(self, extension=None):
+        if not extension:
+            extension = self.extension
+        return '{0}-{1}.{2}'.format(self.day.date, self.sequence, extension)
+
+    def content_type(self):
+        if self.extension == 'pdf':
+            return 'application/pdf'
+        elif self.extension == 'gif':
+            return 'image/gif'
+        elif self.extension == 'png':
+            return 'image/png'
+        elif self.extension == 'jpg':
+            return 'image/jpeg'
+
+    def __repr__(self):
+        if self.sequence is not None:
+            return '<Page filename: %s-%d.pdf>' % (self.day.date, self.sequence)
+        else:
+            return '<Page filename: %s-xxx.pdf>' % (self.day.date)
+
+    def __unicode__(self):
+        return repr(self)
