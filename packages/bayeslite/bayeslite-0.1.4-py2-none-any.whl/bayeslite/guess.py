58b22f4ede0ea4028cbe5b975c3c33a21a00866b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:50 +0000

    Add package bayeslite/0.1.4/bayeslite-0.1.4-py2-none-any.whl

---
+++ packages/bayeslite/bayeslite-0.1.4-py2-none-any.whl/bayeslite/guess.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2015, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""Heuristic guessing of statistical types based on data.
+
+The heuristics implemented here are ad-hoc, and do not implement any
+sort of Bayesian model selection.  They are based on crude attempts to
+parse data as numbers, and on fixed parameters for distinguishing
+categorical and numerical data.  No columns are ever guessed to be
+cyclic.
+"""
+
+import math
+
+import bayeslite.core as core
+
+from bayeslite.sqlite3_util import sqlite3_quote_name
+from bayeslite.util import casefold
+from bayeslite.util import unique
+
+def bayesdb_guess_generator(bdb, generator, table, metamodel,
+        ifnotexists=None, count_cutoff=None, ratio_cutoff=None,
+        default=None, overrides=None):
+    """Heuristically guess a generator for `table` using `metamodel`.
+
+    Based on the data in `table`, create a generator named `generator`
+    using `metamodel` for it.
+
+    :param bool ifnotexists: if true or ``None`` and `generator`
+        already exists, do nothing.
+    :param int count_cutoff: number of distinct values below which
+        columns whose values can all be parsed as numbers will be
+        considered categorical anyway
+    :param real ratio_cutoff: ratio of distinct values to total values
+        below which columns whose values can all be parsed as numbers
+        will be considered categorical anyway
+    :param list overrides: list of ``(name, stattype)``, overriding
+        any guessed statistical type for columns by those names
+
+    In addition to statistical types, the overrides may specify
+    ``key`` or ``ignore``, in which case those columns will not be
+    modelled at all.
+    """
+
+    # Fill in default arguments.
+    if ifnotexists is None:
+        ifnotexists = False
+    if default is None:
+        default = False
+
+    with bdb.savepoint():
+        if core.bayesdb_has_generator(bdb, generator):
+            if ifnotexists:
+                return
+            else:
+                raise ValueError('Generator already exists: %s' %
+                    (repr(generator),))
+        qt = sqlite3_quote_name(table)
+        cursor = bdb.sql_execute('SELECT * FROM %s' % (qt,))
+        column_names = [d[0] for d in cursor.description]
+        rows = cursor.fetchall()
+        stattypes = bayesdb_guess_stattypes(column_names, rows,
+            count_cutoff=count_cutoff, ratio_cutoff=ratio_cutoff,
+            overrides=overrides)
+        # Skip the key column.
+        column_names, stattypes = \
+            unzip([(cn, st) for cn, st in zip(column_names, stattypes)
+                if st != 'key' and st != 'ignore'])
+        if len(column_names) == 0:
+            raise ValueError('Table has no modelled columns: %s' %
+                (repr(table),))
+        qg = sqlite3_quote_name(generator)
+        qmm = sqlite3_quote_name(metamodel)
+        qcns = map(sqlite3_quote_name, column_names)
+        qsts = map(sqlite3_quote_name, stattypes)
+        qs = ','.join(qcn + ' ' + qst for qcn, qst in zip(qcns, qsts))
+        bdb.execute('CREATE %sGENERATOR %s FOR %s USING %s(%s)' %
+            ('DEFAULT ' if default else '', qg, qt, qmm, qs))
+
+def unzip(l):                   # ???
+    xs = []
+    ys = []
+    for x, y in l:
+        xs.append(x)
+        ys.append(y)
+    return xs, ys
+
+def bayesdb_guess_stattypes(column_names, rows,
+        count_cutoff=None, ratio_cutoff=None, overrides=None):
+    """Heuristically guess statistical types for the data in `rows`.
+
+    Return a list of statistical types corresponding to the columns
+    named in the list `column_names`.
+
+    :param int count_cutoff: number of distinct values below which
+        columns whose values can all be parsed as numbers will be
+        considered categorical anyway
+    :param real ratio_cutoff: ratio of distinct values to total values
+        below which columns whose values can all be parsed as numbers
+        will be considered categorical anyway
+    :param list overrides: list of ``(name, stattype)``, overriding
+        any guessed statistical type for columns by those names
+
+    In addition to statistical types, the overrides may specify
+    ``key`` or ``ignore``.
+    """
+
+    # Fill in default arguments.
+    if count_cutoff is None:
+        count_cutoff = 20
+    if ratio_cutoff is None:
+        ratio_cutoff = 0.02
+    if overrides is None:
+        overrides = []
+
+    # Build a set of the column names.
+    column_name_set = set()
+    duplicates = set()
+    for name in column_names:
+        if casefold(name) in column_name_set:
+            duplicates.add(name)
+        column_name_set.add(casefold(name))
+    if 0 < len(duplicates):
+        raise ValueError('Duplicate column names: %s' %
+            (repr(list(duplicates),)))
+
+    # Build a map for the overrides.
+    #
+    # XXX Support more than just stattype: allow arbitrary column
+    # descriptions.
+    override_map = {}
+    unknown = set()
+    duplicates = set()
+    for name, stattype in overrides:
+        if casefold(name) not in column_name_set:
+            unknown.add(name)
+            continue
+        if casefold(name) in override_map:
+            duplicates.add(name)
+            continue
+        override_map[casefold(name)] = casefold(stattype)
+    if 0 < len(unknown):
+        raise ValueError('Unknown columns overridden: %s' %
+            (repr(list(unknown)),))
+    if 0 < len(duplicates):
+        raise ValueError('Duplicate columns overridden: %s' %
+            (repr(list(duplicates)),))
+
+    # Sanity-check the inputs.
+    ncols = len(column_names)
+    assert ncols == len(unique(map(casefold, column_names)))
+    for ri, row in enumerate(rows):
+        if len(row) < ncols:
+            raise ValueError('Row %d: Too few columns: %d < %d' %
+                (ri, len(row), ncols))
+        if len(row) > ncols:
+            raise ValueError('Row %d: Too many columns: %d > %d' %
+                (ri, len(row), ncols))
+
+    # Find a key first, if it has been specified as an override.
+    key = None
+    duplicate_keys = set()
+    for ci, column_name in enumerate(column_names):
+        if casefold(column_name) in override_map:
+            if override_map[casefold(column_name)] == 'key':
+                if key is not None:
+                    duplicate_keys.add(column_name)
+                    continue
+                column = integerify(rows, ci)
+                if not column:
+                    column = [row[ci] for row in rows]
+                if not keyable_p(column):
+                    raise ValueError('Column non-unique but specified as key'
+                        ': %s' % (repr(column_name),))
+                key = column_name
+    if 0 < len(duplicate_keys):
+        raise ValueError('Multiple columns overridden as keys: %s' %
+            (repr(list(duplicate_keys)),))
+
+    # Now go through and guess the other column stattypes or use the
+    # override.
+    stattypes = []
+    for ci, column_name in enumerate(column_names):
+        if casefold(column_name) in override_map:
+            stattype = override_map[casefold(column_name)]
+        else:
+            numericable = True
+            column = integerify(rows, ci)
+            if not column:
+                column = floatify(rows, ci)
+                if not column:
+                    column = [row[ci] for row in rows]
+                    numericable = False
+            if key is None and keyable_p(column):
+                stattype = 'key'
+                key = column_name
+            elif numericable and \
+                 numerical_p(column, count_cutoff, ratio_cutoff):
+                stattype = 'numerical'
+            else:
+                stattype = 'categorical'
+        stattypes.append(stattype)
+    return stattypes
+
+def integerify(rows, ci):
+    column = [0] * len(rows)
+    try:
+        for ri, row in enumerate(rows):
+            if isinstance(row[ci], float):
+                return None
+            column[ri] = int(row[ci])
+    except (ValueError, TypeError):
+        return None
+    return column
+
+def floatify(rows, ci):
+    column = [0] * len(rows)
+    try:
+        for ri, row in enumerate(rows):
+            if row[ci] is None or row[ci] == '':
+                column[ri] = float('NaN')
+            else:
+                column[ri] = float(row[ci])
+    except (ValueError, TypeError):
+        return None
+    return column
+
+def keyable_p(column):
+    # `unique' can't cope with NaNs, so reject them early.
+    if any(v is None or (isinstance(v, float) and math.isnan(v))
+           for v in column):
+        return False
+    return len(column) == len(unique(column))
+
+def numerical_p(column, count_cutoff, ratio_cutoff):
+    nu = len(unique([v for v in column if not math.isnan(v)]))
+    if nu <= count_cutoff:
+        return False
+    if float(nu) / float(len(column)) <= ratio_cutoff:
+        return False
+    return True
---
+++ packages/bayeslite/bayeslite-0.1.4-py2-none-any.whl/bayeslite/schema.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2015, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from bayeslite.exception import BayesDBException
+from bayeslite.sqlite3_util import sqlite3_transaction
+from bayeslite.util import cursor_value
+
+bayesdb_schema_5 = '''
+PRAGMA user_version = 5;
+
+CREATE TABLE bayesdb_metamodel (
+	name		TEXT COLLATE NOCASE NOT NULL PRIMARY KEY,
+	version		INTEGER NOT NULL
+);
+
+-- Statistics type.
+CREATE TABLE bayesdb_stattype (
+	name		TEXT COLLATE NOCASE NOT NULL PRIMARY KEY
+);
+
+INSERT INTO bayesdb_stattype VALUES ('categorical');
+INSERT INTO bayesdb_stattype VALUES ('cyclic');
+INSERT INTO bayesdb_stattype VALUES ('numerical');
+
+CREATE TABLE bayesdb_column (
+	tabname		TEXT COLLATE NOCASE NOT NULL,
+	colno		INTEGER NOT NULL CHECK (0 <= colno),
+	name		TEXT COLLATE NOCASE NOT NULL,
+	shortname	TEXT,
+	description	TEXT,
+	PRIMARY KEY(tabname, colno),
+	UNIQUE(tabname, name)
+);
+
+CREATE TABLE bayesdb_column_map (
+	tabname		TEXT COLLATE NOCASE NOT NULL,
+	colno		INTEGER NOT NULL,
+	key		TEXT NOT NULL,
+	value		TEXT NOT NULL,
+	PRIMARY KEY(tabname, colno, key),
+	FOREIGN KEY(tabname, colno) REFERENCES bayesdb_column(tabname, colno)
+);
+
+CREATE TABLE bayesdb_generator (
+	-- We use AUTOINCREMENT so that generator id numbers don't get
+	-- reused and are safe to hang onto outside a transaction.
+	id		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+				CHECK (0 < id),
+	name		TEXT COLLATE NOCASE NOT NULL UNIQUE,
+	tabname		TEXT COLLATE NOCASE NOT NULL,
+				-- REFERENCES sqlite_master(name)
+	metamodel	INTEGER NOT NULL REFERENCES bayesdb_metamodel(name)
+);
+
+CREATE TABLE bayesdb_generator_column (
+	generator_id	INTEGER NOT NULL REFERENCES bayesdb_generator(id),
+	colno		INTEGER NOT NULL,
+	stattype	TEXT NOT NULL REFERENCES bayesdb_stattype(name),
+	PRIMARY KEY(generator_id, colno)
+);
+
+CREATE TABLE bayesdb_generator_model (
+	generator_id	INTEGER NOT NULL REFERENCES bayesdb_generator(id),
+	modelno		INTEGER NOT NULL,
+	iterations	INTEGER NOT NULL CHECK (0 <= iterations),
+	PRIMARY KEY(generator_id, modelno)
+);
+'''
+
+bayesdb_schema_5to6 = '''
+PRAGMA user_version = 6;
+
+ALTER TABLE bayesdb_generator
+    ADD COLUMN defaultp BOOLEAN DEFAULT 0;
+
+CREATE UNIQUE INDEX bayesdb_generator_i_default ON bayesdb_generator (tabname)
+    WHERE defaultp;
+'''
+
+bayesdb_schema_6to7 = '''
+PRAGMA user_version = 7;
+
+CREATE TABLE bayesdb_session (
+	id		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+				CHECK (0 < id),
+	sent	BOOLEAN DEFAULT 0,
+	version	TEXT NOT NULL
+);
+
+CREATE TABLE bayesdb_session_entries (
+	id		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+				CHECK (0 < id),
+	session_id	INTEGER NOT NULL REFERENCES bayesdb_session(id),
+	type		TEXT CHECK (type IN ('bql','sql')) NOT NULL,
+	data		TEXT NOT NULL,
+	-- Timing is by the local POSIX clock.
+	start_time	INTEGER NOT NULL,
+	end_time	INTEGER,
+	error		TEXT
+);
+'''
+
+### BayesDB SQLite setup
+
+def bayesdb_install_schema(db, version=None, compatible=None):
+    # Get the application id.
+    cursor = db.cursor().execute('PRAGMA application_id')
+    application_id = 0
+    try:
+        row = cursor.next()
+    except StopIteration:
+        raise EnvironmentError('Missing application_id in sqlite3.')
+    else:
+        application_id = row[0]
+        assert isinstance(application_id, int)
+
+    # Get the user version.
+    cursor = db.cursor().execute('PRAGMA user_version')
+    user_version = 0
+    try:
+        row = cursor.next()
+    except StopIteration:
+        raise EnvironmentError('Missing user_version in sqlite3.')
+    else:
+        user_version = row[0]
+        assert isinstance(user_version, int)
+
+    # Check them.  If zero, install schema.  If mismatch, fail.
+    install = False
+    if application_id == 0 and user_version == 0:
+        # Assume we just created the database.
+        #
+        # XXX What if we opened some random other sqlite file which
+        # did not have an application_id or user_version set?  Hope
+        # everyone else sets application_id and user_version too...
+        with sqlite3_transaction(db):
+            db.cursor().execute('PRAGMA application_id = %d' % (0x42594442,))
+            db.cursor().execute(bayesdb_schema_5)
+        user_version = 5
+        install = True
+    elif application_id != 0x42594442:
+        raise IOError('Wrong application id: 0x%08x' % (application_id,))
+    # 5 was the last prerelease version.
+    if user_version == 5:
+        with sqlite3_transaction(db):
+            db.cursor().execute(bayesdb_schema_5to6)
+        user_version = 6
+    if user_version != 6 and user_version != 7:
+        raise IOError('Unknown bayeslite format version: %d' % (user_version,))
+    if install or not compatible:
+        _upgrade_schema(db, version=version)
+    db.cursor().execute('PRAGMA foreign_keys = ON')
+    db.cursor().execute('PRAGMA integrity_check')
+    db.cursor().execute('PRAGMA foreign_key_check')
+
+def _upgrade_schema(db, version=None):
+    if version is None:
+        version = 7
+    assert 6 <= version
+    current_version = _schema_version(db)
+    assert 6 <= current_version
+    if current_version == 6 < version:
+        with sqlite3_transaction(db):
+            db.cursor().execute(bayesdb_schema_6to7)
+        current_version = 7
+    assert current_version == version
+    db.cursor().execute('PRAGMA integrity_check')
+    db.cursor().execute('PRAGMA foreign_key_check')
+
+def _schema_version(db):
+    return cursor_value(db.cursor().execute('PRAGMA user_version'))
+
+def bayesdb_upgrade_schema(bdb, version=None):
+    """Upgrade the BayesDB internal database schema.
+
+    If `version` is `None`, upgrade to the latest database format
+    version supported by bayeslite.  Otherwise, it may be a schema
+    version number.
+    """
+    _upgrade_schema(bdb._sqlite3, version=version)
+
+def bayesdb_schema_version(bdb):
+    """Return the version number for the BayesDB internal database schema."""
+    return _schema_version(bdb._sqlite3)
+
+def bayesdb_schema_required(bdb, version, why):
+    """Fail if `bdb`'s internal database schema version is not new enough.
+
+    The string `why` is included in the text of the error message,
+    along with advice to use `bayesdb_upgrade_schema` to upgrade it.
+    """
+    current_version = bayesdb_schema_version(bdb)
+    if current_version < version:
+        raise BayesDBException(bdb, 'BayesDB schema version too old'
+            ' to support %s: current version %r, at least %r required;'
+            ' use bayesdb_upgrade_schema to upgrade.'
+            % (why, current_version, version))
