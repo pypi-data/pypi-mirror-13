c7741aa174faedba442d70bbb990980c9598fe61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:49 +0000

    Add package digimat.nestcam/0.1.4/digimat.nestcam-0.1.4.tar.gz

---
+++ packages/digimat.nestcam/digimat.nestcam-0.1.4.tar.gz/digimat.nestcam-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digimat.nestcam',
+    version='0.1.4',
+    description='Digimat Nestcam',
+    namespace_packages=['digimat'],
+    author='Frederic Hess',
+    author_email='fhess@splust.ch',
+    license='PSF',
+    packages=find_packages('src'),
+    package_dir = {'':'src'},
+    install_requires=[
+        'pillow',
+        'requests[security]',
+        'urllib3[secure]',
+        'setuptools'
+    ],
+    dependency_links=[
+        ''
+    ],
+    zip_safe=False)
