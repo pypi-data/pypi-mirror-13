855ebb38fa111cb72c7bdaae28bb3b92a81163a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package regal/1.1/regal-1.1.tar.gz

---
+++ packages/regal/regal-1.1.tar.gz/regal-1.1/setup.py
@@ -0,0 +1,42 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+__version_info__ = (1, 1)
+__version__ = '.'.join([str(v) for v in __version_info__])
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='regal',
+    version=__version__,
+    packages=['regal'],
+    include_package_data=True,
+    license='MIT License',
+    description='A/B Testing or publish smart grouping engine.',
+    long_description=README,
+    url='https://github.com/boylegu/regal',
+    author='BoyleGu',
+    author_email='gubaoer@hotmail.com',
+    install_requires=['six==1.10.0'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
