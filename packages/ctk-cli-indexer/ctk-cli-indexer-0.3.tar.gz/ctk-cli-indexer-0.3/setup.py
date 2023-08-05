27074d804c04c8dbd583fd67b9737eb508f2fece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package ctk-cli-indexer/0.3/ctk-cli-indexer-0.3.tar.gz

---
+++ packages/ctk-cli-indexer/ctk-cli-indexer-0.3.tar.gz/ctk-cli-indexer-0.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import os, codecs
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    readme = f.read()
+
+# reqs is a list of requirement spec strings
+with file('requirements.txt') as f:
+    reqs = [req.strip() for req in f]
+
+setup(name='ctk-cli-indexer',
+    version='0.3',
+    description=('Python utilities for creating an ElasticSearch database '
+                 'containing information on available CLI modules'),
+    long_description=readme,
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'
+    ],
+    author='Hans Meine, Jean-Christophe Fillion-Robin',
+    author_email='hans_meine@gmx.net, jchris.fillionr@kitware.com',
+    url='https://github.com/commontk/ctk-cli-indexer',
+    install_requires=reqs,
+    packages=['ctk_cli_indexer'],
+    entry_points = {
+        'console_scripts': [
+            'ctk_cli_indexer = ctk_cli_indexer.main:main',
+        ],
+    },
+    package_data={
+        '': ['requirements.txt'],
+    },
+    )
