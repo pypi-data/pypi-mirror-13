889e509e704590f5c98a177fb7d563fa79811b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:27 +0000

    Add package djangocms-helper/0.9.4/djangocms_helper-0.9.4-py2.py3-none-any.whl

---
+++ packages/djangocms-helper/djangocms_helper-0.9.4-py2.py3-none-any.whl/djangocms_helper/test_utils/runners.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from django.test.runner import DiscoverRunner
+from django.utils import unittest
+
+
+class CapturedOutputRunner(DiscoverRunner):
+
+    def run_suite(self, suite, **kwargs):
+        return unittest.TextTestRunner(
+            verbosity=self.verbosity,
+            failfast=self.failfast,
+            stream=sys.stderr
+        ).run(suite)
+
+try:
+    from django.test.simple import DjangoTestSuiteRunner
+
+    class CapturedOutputSimpleRunner(DjangoTestSuiteRunner):
+
+        def run_suite(self, suite, **kwargs):
+            return unittest.TextTestRunner(
+                verbosity=self.verbosity,
+                failfast=self.failfast,
+                stream=sys.stderr
+            ).run(suite)
+
+except ImportError:
+    CapturedOutputSimpleRunner = CapturedOutputRunner
