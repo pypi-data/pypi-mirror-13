835620a975b699a5119c593cba307828dc2efd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:09 +0000

    Add package invenio-accounts/1.0.0a7/invenio-accounts-1.0.0a7.tar.gz

---
+++ packages/invenio-accounts/invenio-accounts-1.0.0a7.tar.gz/invenio-accounts-1.0.0a7/invenio_accounts/testutils.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Invenio-Accounts utility functions for tests and testing purposes.
+
+.. warning:: DO NOT USE IN A PRODUCTION ENVIRONMENT.
+
+Functions within accessing the datastore will throw an error if called outside
+of an application context. If pytest-flask is installed you don't have to worry
+about this.
+"""
+
+from __future__ import absolute_import, print_function
+
+import flask
+import flask_login
+from flask import current_app
+from flask_security import url_for_security
+from flask_security.utils import encrypt_password
+from werkzeug.local import LocalProxy
+
+# "Convenient references" (lifted from flask_security source)
+_datastore = LocalProxy(lambda: current_app.extensions['security'].datastore)
+
+
+def create_test_user(email='test@test.org',
+                     password='123456', **kwargs):
+    """Create a user in the datastore, bypassing the registration process.
+
+    Accesses the application's datastore. An error is thrown if called from
+    outside of an application context.
+
+    Returns the created user model object instance, with the plaintext password
+    as `user.password_plaintext`.
+    """
+    assert flask.current_app.testing
+    encrypted_password = encrypt_password(password)
+    user = _datastore.create_user(email=email, password=encrypted_password,
+                                  **kwargs)
+    _datastore.commit()
+    user.password_plaintext = password
+    return user
+
+
+def login_user_via_view(client, email, password, login_url=None):
+    """Attempt to log the given user in via the 'login' view on the client.
+
+    Returns the response object.
+    """
+    return client.post(login_url or url_for_security('login'),
+                       data={'email': email, 'password': password},
+                       environ_base={'REMOTE_ADDR': '127.0.0.1'})
+    # If the REMOTE_ADDR isn't set it'll throw out a ValueError as it attempts
+    # to update the User model in the database with 'untrackable' as the new
+    # `last_login_ip`.
+
+
+def client_authenticated(client, test_url=None):
+    """Attempt to get the change password page with the given client object.
+
+    Returns True if the client can get the change password page without getting
+    redirected and flask_login's `current_user` object isn't anonymous.
+    """
+    response = client.get(test_url or url_for_security('change_password'))
+
+    return (response.status_code == 200 and
+            not flask_login.current_user.is_anonymous)
+
+
+def webdriver_authenticated(webdriver, test_url=None):
+    """Attempt to get the change password page through the given webdriver.
+
+    Similar to `client_authenticated`, but for selenium webdriver objects.
+    """
+    save_url = webdriver.current_url
+
+    webdriver.get(test_url or flask.url_for('security.change_password',
+                                            _external=True))
+    result_url = webdriver.current_url
+    webdriver.get(save_url)
+    return (flask.url_for('security.login', _external=True) not in result_url)
