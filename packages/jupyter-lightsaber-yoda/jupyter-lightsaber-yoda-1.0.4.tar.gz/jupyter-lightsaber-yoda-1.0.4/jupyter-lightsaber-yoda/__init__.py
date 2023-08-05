f54d977037d6e6db91ac50029c9b378a5d773572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package jupyter-lightsaber-yoda/1.0.4/jupyter-lightsaber-yoda-1.0.4.tar.gz

---
+++ packages/jupyter-lightsaber-yoda/jupyter-lightsaber-yoda-1.0.4.tar.gz/jupyter-lightsaber-yoda-1.0.4/jupyter-lightsaber-yoda/__init__.py
@@ -0,0 +1,7 @@
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'amd',
+        'dest': 'jupyter-lightsaber-yoda',
+        'require': 'jupyter-lightsaber-yoda/index'
+    }]
