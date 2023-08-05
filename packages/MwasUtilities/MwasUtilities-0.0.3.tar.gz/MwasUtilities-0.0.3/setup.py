a87340511bc9a22c7cd78c622c90ddc11f70faa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package MwasUtilities/0.0.3/MwasUtilities-0.0.3.tar.gz

---
+++ packages/MwasUtilities/MwasUtilities-0.0.3.tar.gz/MwasUtilities-0.0.3/setup.py
@@ -0,0 +1,18 @@
+__author__ = 'Mwaaas'
+__email__ = 'francismwangi152@gmail.com'
+__phone_number__ = '+254702729654'
+
+
+from distutils.core import setup
+
+setup(
+    name='MwasUtilities',
+    version='0.0.3',
+    packages=[''],
+    url='',
+    license='',
+    author='mwas',
+    author_email='francismwangi152@gmail.com',
+    description='it is used to get object from dict recusively for with get method'
+)
+
