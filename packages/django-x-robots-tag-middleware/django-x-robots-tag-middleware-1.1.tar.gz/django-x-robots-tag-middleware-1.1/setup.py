ed54b9b0b0f7c9f313941719d78a24f2c62c9461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package django-x-robots-tag-middleware/1.1/django-x-robots-tag-middleware-1.1.tar.gz

---
+++ packages/django-x-robots-tag-middleware/django-x-robots-tag-middleware-1.1.tar.gz/django-x-robots-tag-middleware-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-x-robots-tag-middleware",
+    version="1.1",
+    url='http://timlwhite.com',
+    license='BSD',
+    description="Enables returning the X-Robots-Tag header based on Django settings.",
+    long_description=open('README.rst').read(),
+
+    author='Tim White',
+    author_email='tim@cyface.com',
+
+    packages=find_packages(exclude=('x_robots_tag_middleware_demo', 'tests', 'devscripts')),
+    include_package_data=True,
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
