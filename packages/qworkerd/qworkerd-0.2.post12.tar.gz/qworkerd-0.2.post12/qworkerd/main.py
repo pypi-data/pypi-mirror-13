b02a8d666a3c0ba508c92648a09085569d791f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:44 +0000

    Add package qworkerd/0.2.post12/qworkerd-0.2.post12.tar.gz

---
+++ packages/qworkerd/qworkerd-0.2.post12.tar.gz/qworkerd-0.2.post12/qworkerd/main.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+import django, logging, logtool, os, psutil, socket
+from celery import Celery
+from celery.signals import setup_logging
+from django.conf import settings
+
+LOG = logging.getLogger (__name__)
+DEFAULT_LOGCONF = "/etc/qworkerd/logging.conf"
+
+django.setup ()
+os.environ.setdefault ("DJANGO_SETTINGS_MODULE", "qworkerd.settings")
+app = Celery ("qworkerd")
+app.config_from_object ("django.conf:settings")
+app.autodiscover_tasks (lambda: settings.INSTALLED_APPS)
+app.set_current ()
+
+@setup_logging.connect
+@logtool.log_call
+def setup_logging_handler (**kwargs): # pylint: disable=W0613
+  logging.config.fileConfig (DEFAULT_LOGCONF,
+                             disable_existing_loggers = False)
+
+@app.task
+@logtool.log_call
+def status ():
+  rc = {
+    "hostname": socket.gethostname (),
+    "cpu": {
+      "count": psutil.cpu_count (),
+      "times": psutil.cpu_times (),
+      "percent": psutil.cpu_percent (percpu = True),
+      },
+    "memory": {
+      "virtual": psutil.virtual_memory (),
+      "swap": psutil.swap_memory (),
+      },
+    "net": psutil.net_io_counters (),
+    "uptime": {
+      "ut": psutil.boot_time (),
+      "since": logtool.time_str (psutil.boot_time (), slug = True),
+      },
+    "disk": {},
+    "process": {},
+    }
+  for part in psutil.disk_partitions ():
+    rc["disk"]["mounts"] = {
+      part.mountpoint: psutil.disk_usage (part.mountpoint),
+    }
+  for disk, io in psutil.disk_io_counters (perdisk = True).items ():
+    rc["disk"]["io"] = {
+      disk: io
+    }
+  return rc
+
+if __name__ == "__main__":
+  app.start ()
