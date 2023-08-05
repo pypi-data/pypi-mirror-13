afa801c312aad3bc8b35c334fd6f70219ca07046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:13 +0000

    Add package miniorm/1.7.9/miniorm-1.7.9.tar.gz

---
+++ packages/miniorm/miniorm-1.7.9.tar.gz/miniorm-1.7.9/setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(name='miniorm',
+      version='1.7.9',
+      keywords='mini mysql db orm :: miniorm',
+      description='mini mysql db orm :: miniorm :: with connection pool',
+      long_description=open("README.md", 'rb').read(),
+      packages=find_packages(exclude=["*.tests", "*.tests.*"]),
+      author="inwn",
+      author_email="ininwn@gmail.com",
+      include_package_data=True,
+      license="MIT",
+      zip_safe=False,
+      install_requires=[
+          'mysql-connector-python>=2.0.4'
+      ]
+      )
