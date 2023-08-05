68f94388b177703f59c4bd117ee2e7a6242d8799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:56 +0000

    Add package python-wink/0.4.2/python-wink-0.4.2.tar.gz

---
+++ packages/python-wink/python-wink-0.4.2.tar.gz/python-wink-0.4.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='python-wink',
+      version='0.4.2',
+      description='Access Wink devices via the Wink API',
+      url='http://github.com/bradsk88/python-wink',
+      author='John McLaughlin',
+      license='MIT',
+      install_requires=['requests>=2.0'],
+      tests_require=['mock'],
+      test_suite='tests',
+      packages=['pywink'],
+      zip_safe=True)
