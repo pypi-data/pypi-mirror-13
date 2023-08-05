e7252ed7d735b9fd75260b3cd8cd7839af315481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package maxwellzhou_nester/1.3.2/maxwellzhou_nester-1.3.2.tar.gz

---
+++ packages/maxwellzhou_nester/maxwellzhou_nester-1.3.2.tar.gz/maxwellzhou_nester-1.3.2/set_up.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'maxwellzhou_nester',
+    version = '1.3.2',
+    py_modules = ['maxwellzhou_nester'],
+    author = 'Maxwell',
+    author_email = 'llasionzhou@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
