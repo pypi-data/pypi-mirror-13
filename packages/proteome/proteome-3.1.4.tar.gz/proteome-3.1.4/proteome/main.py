ccb5f81ac3fe54f8fc08c52e35f1892007c4595d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package proteome/3.1.4/proteome-3.1.4.tar.gz

---
+++ packages/proteome/proteome-3.1.4.tar.gz/proteome-3.1.4/proteome/main.py
@@ -0,0 +1,41 @@
+from pathlib import Path  # type: ignore
+
+from toolz.itertoolz import cons  # type: ignore
+
+from tryp import List, Map
+
+from proteome.nvim import NvimFacade
+from proteome.env import Env
+from proteome.state import ProteomeState
+from proteome.project import Projects
+
+
+class Proteome(ProteomeState):
+
+    def __init__(
+            self,
+            vim: NvimFacade,
+            config_path: Path,
+            plugins: List[str],
+            bases: List[Path],
+            type_bases: Map[Path, List[str]],
+    ) -> None:
+        self._config_path = config_path
+        self._bases = bases
+        self._type_bases = type_bases
+        core = 'proteome.plugins.core'
+        ProteomeState.__init__(self, vim, List.wrap(cons(core, plugins)))
+
+    def init(self):
+        return Env(  # type: ignore
+            config_path=self._config_path,
+            bases=self._bases,
+            type_bases=self._type_bases,
+            projects=Projects()
+        )
+
+    @property
+    def projects(self):
+        return self.env.projects
+
+__all__ = ['Proteome']
---
+++ packages/proteome/proteome-3.1.4.tar.gz/proteome-3.1.4/proteome/plugins/history.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+from typing import Callable, Any
+from datetime import datetime
+
+from tryp import _
+from tryp.lazy import lazy
+
+from trypnv.machine import may_handle, message
+
+from proteome.state import ProteomeComponent
+from proteome.env import Env
+from proteome.git import HistoryGit
+from proteome.project import Project
+from proteome.plugins.core import Save, StageIV
+
+Commit = message('Commit')
+HistoryBufferPrev = message('HistoryBufferPrev')
+HistoryBufferNext = message('HistoryBufferNext')
+
+
+class Plugin(ProteomeComponent):
+
+    @lazy
+    def git(self):
+        base = self.vim.pdir('history_base').get_or_else(Path('/dev/null'))
+        return HistoryGit(base, self.vim)
+
+    # TODO handle broken repo
+    def _commit(self, pro: Project):
+        self.log.debug('commiting to history repo for {}'.format(pro))
+        return self.git.add_commit_all(pro, datetime.now().isoformat())
+
+    def _init(self, pro: Project):
+        self.log.debug('initializing history repo for {}'.format(pro))
+        return self.git.init(pro)
+
+    @property
+    def all_projects_history(self):
+        return self.pflags.all_projects_history
+
+    @property
+    def git_ready(self):
+        return self.git is not None and self.git.ready
+
+    def _handle(self, env: Env, handler: Callable[[Project], Any]):
+        name = handler.__name__
+        if self.git_ready:
+            projects = env.history_projects(self.all_projects_history)
+            inf = 'running history handler {} on {}'
+            self.log.verbose(inf.format(name, projects))
+            projects.foreach(handler)
+            self.git.exec()
+        else:
+            err = 'tried to run {} on history while not ready'
+            self.log.verbose(err.format(name))
+
+    @may_handle(StageIV)
+    def stage_4(self, env: Env, msg):
+        self._handle(env, self._init)
+
+    @may_handle(Commit)
+    def commit(self, env: Env, msg):
+        self._handle(env, self._commit)
+
+    @may_handle(Save)
+    def save(self, env, msg):
+        return Commit()
+
+    @may_handle(HistoryBufferPrev)
+    def history_buffer_prev(self, env, msg):
+        pass
+
+__all__ = ['Commit', 'Plugin']
