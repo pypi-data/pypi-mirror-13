2e5867a6d6061bef4159fd7d0f0616f3e16993b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package Fao/1.0/Fao-1.0.tar.gz

---
+++ packages/Fao/Fao-1.0.tar.gz/Fao-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+#from distutils.sysconfig import get_python_version
+#from sysconfig import get_python_version
+setup(name='Fao',
+      version='1.0',
+      author='panthotanvir',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      py_modules=['foo'],
+#      ext_modules=[Extension('foo', ['foo.c'])],
+#      packages=['new_package'],
+ #     package_dir={'new_package':'new_package'},
+  #    package_data={'new_package': ['data/*.dat']},
+
+    )
