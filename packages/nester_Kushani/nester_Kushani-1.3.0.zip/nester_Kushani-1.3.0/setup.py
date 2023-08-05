478194ae0a9157f434c065b3cf1fba495b07a825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:30 +0000

    Add package nester_Kushani/1.3.0/nester_Kushani-1.3.0.zip

---
+++ packages/nester_Kushani/nester_Kushani-1.3.0.zip/nester_Kushani-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name = 'nester_Kushani',
+version = '1.3.0',
+py_modules = ['nester_Kushani'],
+author = 'kushani',
+author_email = 'kushanibanu@yahoo.com',
+url = 'http://yahoomail.com',
+description = 'A simple printer of nested lists',
+)
