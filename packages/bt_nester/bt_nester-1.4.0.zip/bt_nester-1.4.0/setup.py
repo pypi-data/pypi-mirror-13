565d851b323ca14ce29f8b10abb61e101d557d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package bt_nester/1.4.0/bt_nester-1.4.0.zip

---
+++ packages/bt_nester/bt_nester-1.4.0.zip/bt_nester-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'bt_nester',
+        version = '1.4.0',
+        py_modules = ['bt_nester'],
+        author = 'bltate2',
+        author_email = 'btate_30.06@hotmail.com',
+        description = 'A simple printer of nested lists',
+        )
