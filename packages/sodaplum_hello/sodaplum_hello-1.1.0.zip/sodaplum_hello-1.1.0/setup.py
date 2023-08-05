4efaa5eab4dea090c1fcf8a23443761822a4fb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:30 +0000

    Add package sodaplum_hello/1.1.0/sodaplum_hello-1.1.0.zip

---
+++ packages/sodaplum_hello/sodaplum_hello-1.1.0.zip/sodaplum_hello-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'sodaplum_hello',
+		version = '1.1.0',
+		py_modules = ['sodaplum_hello'],
+		author = 'sodaplum',
+		author_email = 'sodaplum@163.com',
+		url = 'http://www.sodaplum.com',
+		description = 'print "Hello, world!"',
+	)
