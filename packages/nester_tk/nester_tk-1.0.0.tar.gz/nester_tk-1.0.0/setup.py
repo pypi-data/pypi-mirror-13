dc07202c8d5841730bcace767a0ce8e9824324fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:26 +0000

    Add package nester_tk/1.0.0/nester_tk-1.0.0.tar.gz

---
+++ packages/nester_tk/nester_tk-1.0.0.tar.gz/nester_tk-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_tk',
+	version = '1.0.0',
+	py_modules = ['nester_tk'],
+	author = 'tushark',
+	author_email = 'tusharkhurana99@gmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+     )
