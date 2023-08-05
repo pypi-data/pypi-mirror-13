c482066247fbb7ad5bcd5b555e960cf70abd2548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package Dex7er/1.0.0/Dex7er-1.0.0.zip

---
+++ packages/Dex7er/Dex7er-1.0.0.zip/Dex7er-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'Dex7er', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['Dex7er'],
+        author       = 'Dex7er Chen',
+        author_email = '10523797@126.com',
+        url          = 'http://dex7er.lofter.com/',
+        description  = 'A simple printer',
+     )
