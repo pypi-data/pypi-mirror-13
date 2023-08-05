d4fdc4a5860b8a8d73a1ef1dc6940666ca64d39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:24 +0000

    Add package grazyna/0.5.4/grazyna-0.5.4.tar.gz

---
+++ packages/grazyna/grazyna-0.5.4.tar.gz/grazyna-0.5.4/grazyna/__main__.py
@@ -0,0 +1,13 @@
+from sys import argv
+from .config import create_config
+from .irc import client
+
+
+def run():
+    with open(argv[1]) as f:
+        cfg = create_config(f)
+    client.connect(cfg)
+
+
+if __name__ == "__main__":
+    run()
