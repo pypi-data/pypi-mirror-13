87eb1f73cbc9e21a4fede4cd26f887852922fec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:57 +0000

    Add package django-auditlog/0.3.3/django-auditlog-0.3.3.zip

---
+++ packages/django-auditlog/django-auditlog-0.3.3.zip/django-auditlog-0.3.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='django-auditlog',
+    version='0.3.3',
+    packages=['auditlog', 'auditlog.migrations'],
+    package_dir={'': 'src'},
+    url='https://github.com/jjkester/django-auditlog',
+    license='MIT',
+    author='Jan-Jelle Kester',
+    author_email='janjelle@jjkester.nl',
+    description='Audit log app for Django',
+    install_requires=[
+        'Django>=1.7',
+        'django-jsonfield>=0.9.13',
+    ]
+)
