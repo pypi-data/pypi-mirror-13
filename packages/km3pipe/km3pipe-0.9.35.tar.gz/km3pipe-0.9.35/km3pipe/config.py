87ed773469d45b3abdd728a46805a9567e4d7bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:50 +0000

    Add package km3pipe/0.9.35/km3pipe-0.9.35.tar.gz

---
+++ packages/km3pipe/km3pipe-0.9.35.tar.gz/km3pipe-0.9.35/km3pipe/config.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+# Filename: config.py
+# pylint: disable=locally-disabled
+"""
+Tools for global configuration.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+import os
+from six.moves.configparser import ConfigParser, NoSectionError
+import getpass
+try:
+    input = raw_input
+except NameError:
+    pass
+
+from km3pipe.logger import logging
+
+__author__ = 'tamasgal'
+
+log = logging.getLogger(__name__)  # pylint: disable=C0103
+
+CONFIG_PATH = os.path.expanduser('~/.km3net')
+
+
+class Config(object):
+    def __init__(self):
+        """Configuration manager for KM3NeT stuff"""
+        self.config = ConfigParser()
+        try:
+            self._read_configuration()
+        except IOError:
+            log.warn("No configuration found at '{0}'".format(CONFIG_PATH))
+        else:
+            self._check_config_file_permissions()
+
+    def _check_config_file_permissions(self):
+        """Make sure that the configuration file is 0600"""
+        if os.stat(CONFIG_PATH).st_mode & 0777 != 0600:
+            log.critical("Your config file is readable to others!\n" +
+                         "Please execute `chmod 0600 {0}`".format(CONFIG_PATH))
+            return False
+        return True
+
+    def _read_configuration(self):
+        """Parse configuration file"""
+        self.config.readfp(open(CONFIG_PATH))
+
+    @property
+    def db_credentials(self):
+        """Return username and password for the KM3NeT WebDB."""
+        try:
+            username = self.config.get('DB', 'username')
+            password = self.config.get('DB', 'password')
+        except NoSectionError:
+            username = input("Please enter your KM3NeT DB username: ")
+            password = getpass.getpass("Password: ")
+        return username, password
