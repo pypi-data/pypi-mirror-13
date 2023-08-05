b02c8df0bd711498ad875663ee2aab73689abf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package gendobot/2.1.2/gendobot-2.1.2.tar.gz

---
+++ packages/gendobot/gendobot-2.1.2.tar.gz/gendobot-2.1.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+try:
+    from itertools import ifilter
+except:
+    ifilter = filter
+from os import path
+from ast import parse
+import pip
+requirements = pip.req.parse_requirements("requirements.txt",
+                                          session=pip.download.PipSession())
+
+pip_requirements = [str(r.req) for r in requirements]
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('LICENSE.txt') as readme_file:
+    license = readme_file.read()
+
+with open(path.join('gendo', '__init__.py')) as f:
+    __version__ = parse(next(ifilter(
+        lambda line: line.startswith('__version__'), f))).body[0].value.s
+
+setup(
+    name='gendobot',
+    author='Nick Ficano',
+    author_email='nficano@gmail.com',
+    version=__version__,
+    packages=find_packages(exclude=['tests*']),
+    url='http://nickficano.com',
+    description="a lightweight Slackbot framework for Python",
+    zip_safe=False,
+    install_requires=pip_requirements,
+    long_description=readme,
+    license=license,
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+    ]
+)
