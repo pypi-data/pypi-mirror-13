efd6be2f75fcf124df7237b0faba0fe94f783faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:19 +0000

    Add package LeonaM_nester/1.1.0/LeonaM_nester-1.1.0.tar.gz

---
+++ packages/LeonaM_nester/LeonaM_nester-1.1.0.tar.gz/LeonaM_nester-1.1.0/nester.py
@@ -0,0 +1,17 @@
+"""This is the "nester.py" module and it provides on function called print_lol()
+    which prints lists that may or may not include nested lists."""
+def print_lol(the_list, level):
+    """This funcion takes one positional argument called "the_list" and one int 
+        argument called "level", where "the_list" is any Python list (of - possibly 
+        - nested lists), "level" is the level of the nester
+        Each data item in theprovided list is (recursively) printed to the screen
+        on it's own line, and the items in the i-th level will be printed after i 
+        tabs"""
+        
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end = '')
+            print(each_item)
