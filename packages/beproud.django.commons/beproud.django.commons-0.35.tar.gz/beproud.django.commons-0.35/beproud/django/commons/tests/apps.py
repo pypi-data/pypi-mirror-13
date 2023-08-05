6d9e6bf19a64ad9b14baabf07c243a0bc8efb1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:11 +0000

    Add package beproud.django.commons/0.35/beproud.django.commons-0.35.tar.gz

---
+++ packages/beproud.django.commons/beproud.django.commons-0.35.tar.gz/beproud.django.commons-0.35/beproud/django/commons/tests/apps.py
@@ -0,0 +1,16 @@
+import django.apps import AppConfig
+
+
+class TestFieldsApp(AppConfig):
+    name = 'fields'
+    label = 'fields'
+
+
+class TestShortcutsApp(AppConfig):
+    name = 'shortcuts_app'
+    label = 'shortcuts_app'
+
+
+class TestBaseApp(AppConfig):
+    name = 'base'
+    label = 'base'
