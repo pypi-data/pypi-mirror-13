ff3ca2c52bcff08c30913ba58b5e048caa286a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:49 +0000

    Add package vpnotebook/0.1.2/vpnotebook-0.1.2.zip

---
+++ packages/vpnotebook/vpnotebook-0.1.2.zip/vpnotebook-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+try:
+    from vpnotebook import cmdclass
+except:
+    import pip, importlib
+    pip.main(['install', 'vpnotebook']); cmdclass = importlib.import_module('vpnotebook').cmdclass
+
+setup(
+    name='vpnotebook',
+    packages=['vpnotebook'],
+    version='0.1.02',
+    description='pip installable VPython kernel for Jupyter Notebook',
+    long_description=open('README.txt').read(),
+    author='Bruce Sherwood, John Coady, Ruth Chabay',
+    author_email='johncoady@shaw.ca',
+    url='http://pypi.python.org/pypi/vpnotebook/',
+    license='New BSD License',
+    keywords='vpython kernel',
+    classifiers=[
+          'Framework :: IPython',
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+    ],
+    cmdclass=cmdclass('vpnotebook/data'),
+    package_data={'vpnotebook': ['data/kernel.json']},
+    
+)
