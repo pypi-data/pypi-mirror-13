59076637ce7d73fdbb8a844b51e3a05c2b89709c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package NumRep/0.1.3/NumRep-0.1.3.tar.gz

---
+++ packages/NumRep/NumRep-0.1.3.tar.gz/NumRep-0.1.3/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+NAME = "NumRep"
+
+import re
+
+vre = re.compile(r"Version: ([\d.]+)")
+def read_file(fil):
+    with open(fil) as f:
+        a = f.read()
+    return a
+
+def getversion():
+    fil = read_file('README.rst')
+    ver = vre.search(fil)
+    vers = ver.group(1)
+    return vers
+
+dct = dict(
+    name=NAME,
+    version=getversion(),
+    description='A module to represent numbers by their place value.',
+    long_description=read_file("README.rst")+ '\n\n'+ read_file('CHANGELOG.rst'),
+    keywords='number representation order place value denomination',
+    author='Muhammed Shamil K',
+    url = 'https://github.com/necessary129/NumRep',
+    author_email='note@noteness.cf',
+    license='2-clause Simplified BSD',
+    packages=[NAME],
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Plugins',
+    'Intended Audience :: Customer Service',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Utilities',
+    ],
+    )
+if __name__ == '__main__':
+    setup(**dct)
