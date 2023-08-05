1ca02c70255df85b7cb7a95f6227ecad934f73bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:16 +0000

    Add package t411api/0.1.1/t411api-0.1.1.tar.gz

---
+++ packages/t411api/t411api-0.1.1.tar.gz/t411api-0.1.1/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = '0.0.0'
+exec(open('t411api/version.py').read())
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+        name='t411api',
+        version=__version__,
+        description='Lightweight API for T411 (french torrent website)',
+        long_description='Documentation is available `here <https://github.com/Xide/t411api/blob/master/README.md>`_',
+        url='https://github.com/Xide/t411api',
+        author='Germain Gau',
+        author_email='germain.gau@gmail.com',
+        license='THE BEER-WARE LICENSE',
+        packages=['t411api'],
+        zip_safe=False,
+        test_suite='tests',
+        install_requires=required,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Programming Language :: Python',
+            'Operating System :: Unix'
+        ]
+)
