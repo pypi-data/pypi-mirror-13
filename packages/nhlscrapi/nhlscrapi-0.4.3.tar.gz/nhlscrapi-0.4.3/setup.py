733bc7eaab0ad44ae86c79671c5d7c812c9eab7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package nhlscrapi/0.4.3/nhlscrapi-0.4.3.tar.gz

---
+++ packages/nhlscrapi/nhlscrapi-0.4.3.tar.gz/nhlscrapi-0.4.3/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+import nhlscrapi
+
+setup(
+  name="nhlscrapi",
+    
+  version=nhlscrapi.__version__,
+    
+  description='NHL Scrapr API for Python',
+  long_description=open('README.rst').read(),
+    
+  author='Rob Howley',
+  author_email='howley.robert@gmail.com',
+  url='https://github.com/robhowley/nhlscrapi',
+    
+  packages=find_packages(),
+    
+  include_package_data=True,
+    
+  scripts=['bin/gamedata.py'],
+    
+  license="Apache Software License version 2.0",
+    
+  platforms='any',
+    
+  zip_safe=False,
+    
+  keywords='nhlscrapi',
+    
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
+    
+  test_suite='tests',
+    
+  # Dependent packages (distributions)
+  install_requires=['lxml', 'requests']
+)
