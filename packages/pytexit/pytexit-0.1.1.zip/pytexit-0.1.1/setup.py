fe743b4156956e85f78bbb4bc08c7afbe6b2cb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:51 +0000

    Add package pytexit/0.1.1/pytexit-0.1.1.zip

---
+++ packages/pytexit/pytexit-0.1.1.zip/pytexit-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+long_description = 'Convert a Python expression in a LaTeX formula'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+    
+setup(name='pytexit',
+      version='0.1.1',
+      description='Convert a Python expression in a LaTeX formula',
+	long_description=long_description,
+      url='https://github.com/rainwear/pytexit',
+      author='Erwan Pannier',
+      author_email='erwan.pannier@gmail.com',
+      license='BSD-3',
+      packages=['pytexit'],
+      install_requires=[],
+      scripts=[
+          'scripts/py2tex'],
+	include_package_data=True,
+      zip_safe=False)
