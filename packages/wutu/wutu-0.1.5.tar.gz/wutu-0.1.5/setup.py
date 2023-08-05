8d5b61c83647a21e9fcf78f6ec667b82756cbbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package wutu/0.1.5/wutu-0.1.5.tar.gz

---
+++ packages/wutu/wutu-0.1.5.tar.gz/wutu-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from pip.req import parse_requirements
+import sys, os
+sys.path.append("wutu/")
+sys.path.append("wutu/compiler/")
+
+install_reqs = list(parse_requirements("requirements.txt", session={}))
+
+
+def version():
+    import version
+    return version.get_version()
+
+
+setup(name="wutu",
+        version=version(),
+        description="A minimalistic python-angular framework",
+        author="Šarūnas Navickas",
+        author_email="zaibacu@gmail.com",
+        url="https://github.com/zaibacu/wutu",
+        license="MIT",
+        packages=["wutu", "wutu.compiler"],
+        install_requires=[str(ir.req) for ir in install_reqs],
+        test_suite="nose.collector",
+        tests_require=["nose"])
