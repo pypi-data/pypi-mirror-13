8b4d45c8c8110e1ac7aab07d146563f440147d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:02 +0000

    Add package pyWorkFlow/0.0.2/pyWorkFlow-0.0.2.tar.gz

---
+++ packages/pyWorkFlow/pyWorkFlow-0.0.2.tar.gz/pyWorkFlow-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name='pyWorkFlow',
+      version='0.0.2',
+      description='A lightweight parallel task engine',
+      author='Chris Saunders',
+      author_email='csaunders@illumina.com',
+      packages=["pyWorkFlow"],
+)
