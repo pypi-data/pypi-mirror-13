578683d4873e3d9a2c43ae2365e64c3fad10ecf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:04 +0000

    Add package nece/0.3/nece-0.3.tar.gz

---
+++ packages/nece/nece-0.3.tar.gz/nece-0.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+description = "A content translation framework using Postgresql's " +\
+    "jsonb field in the background",
+
+setup(
+    name='nece',
+    version='0.3',
+    description=description,
+    long_description=description,
+    author='Can Mustafa Özdemir',
+    author_email='canmustafaozdemir@gmail.com',
+    url='https://github.com/tatterdemalion/django-nece',
+    download_url='https://github.com/tatterdemalion/django-nece/tarball/0.3',
+    keywords=['translations', 'i18n', 'language', 'multilingual'],
+    packages=['nece', 'nece.fields', 'nece.fields.pgjson',
+              'nece.fields.pgjson.forms'],
+    install_requires=[
+        'Django>=1.8',
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Database",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+)
