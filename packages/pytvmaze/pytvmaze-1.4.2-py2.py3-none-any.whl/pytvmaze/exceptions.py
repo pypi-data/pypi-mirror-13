2e89dbb2410b24dabd61a0aacb386fe479cca928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package pytvmaze/1.4.2/pytvmaze-1.4.2-py2.py3-none-any.whl

---
+++ packages/pytvmaze/pytvmaze-1.4.2-py2.py3-none-any.whl/pytvmaze/exceptions.py
@@ -0,0 +1,74 @@
+class BaseError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class ShowNotFound(BaseError):
+    pass
+
+
+class IDNotFound(BaseError):
+    pass
+
+
+class ScheduleNotFound(BaseError):
+    pass
+
+
+class EpisodeNotFound(BaseError):
+    pass
+
+
+class NoEpisodesForAirdate(BaseError):
+    pass
+
+
+class CastNotFound(BaseError):
+    pass
+
+
+class ShowIndexError(BaseError):
+    pass
+
+
+class PersonNotFound(BaseError):
+    pass
+
+
+class CreditsNotFound(BaseError):
+    pass
+
+
+class UpdateNotFound(BaseError):
+    pass
+
+
+class AKASNotFound(BaseError):
+    pass
+
+
+class GeneralError(BaseError):
+    pass
+
+
+class MissingParameters(BaseError):
+    pass
+
+
+class SeasonNotFound(BaseError):
+    pass
+
+
+class IllegalAirDate(BaseError):
+    pass
+
+
+class ConnectionError(BaseError):
+    pass
+
+
+class BadRequest(BaseError):
+    pass
