6324845da005f2aa460f537ddbc0946c483b03f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package saruman/0.0.1/saruman-0.0.1.tar.gz

---
+++ packages/saruman/saruman-0.0.1.tar.gz/saruman-0.0.1/saruman/tasks/kernel/modprobe.py
@@ -0,0 +1,59 @@
+from celery.utils.log import get_task_logger
+from celery import Task, states
+from celery.exceptions import Ignore
+# noinspection PyUnresolvedReferences
+from plumbum.cmd import lsmod, modprobe
+
+
+from saruman.app.queue import queue
+
+from saruman.helpers.exceptions.plumbum import ProcessExecutionError
+from saruman.helpers.exceptions.firewall import FirewallGenericError, FirewallNotAllowedError
+
+__all__ = ['check']
+
+logger = get_task_logger(__name__)
+
+allowed_mod = ['dummy']
+
+
+class Check(Task):
+    name = 'kernel.modules.check'
+
+    def run(self, module_name, **kwargs):
+        logger.info("Check pour voir si le module `{}` du kernel est activé".format(module_name))
+        try:
+            if module_name not in allowed_mod:
+                raise FirewallNotAllowedError("Le module `{}` du kernel n'est pas whitelisté".format(module_name))
+            logger.debug("Execution de lsmod")
+            mod = lsmod()
+            return module_name in mod
+        except (ProcessExecutionError, FirewallGenericError) as e:
+            logger.error(e)
+        except Exception as e:
+            logger.exception(e)
+
+
+class Add(Task):
+    name = 'kernel.modules.add'
+
+    def run(self, module_name, module_args, **kwargs):
+        logger.info("Activation du module `{}` du kernel".format(module_name))
+        try:
+            if module_name not in allowed_mod:
+                raise FirewallNotAllowedError("Le module `{}` du kernel n'est pas whitelisté".format(module_name))
+            logger.debug("Execution de modprobe")
+            if module_args is not None:
+                mod = modprobe[module_name, module_args]()
+            else:
+                mod = modprobe[module_name]()
+            actif = mod
+            logger.info("Le module `{}` du kernel est {}".format(module_name, "activé" if actif else "désactivé"))
+            return actif
+        except (ProcessExecutionError, FirewallGenericError) as e:
+            logger.error(e)
+        except Exception as e:
+            logger.exception(e)
+
+if __name__ == '__main__':
+    Check().delay("test")
---
+++ packages/saruman/saruman-0.0.1.tar.gz/saruman-0.0.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+version = __import__('saruman').__version__
+
+
+class Install(_install):
+    def run(self):
+        _install.run(self)
+        print("Post Install")
+
+setup(
+        name='saruman',
+        version=version,
+        packages=find_packages(),
+        url='https://github.com/tychota/saruman',
+        download_url='https://github.com/tychota/saruman/tarball/0.1',
+        license='MIT',
+        author='tychota',
+        author_email='tycho.tatitscheff+saruman@gadz.org',
+        description='A firewall that leverage workqueue ! Build by iresam for iresam !',
+        install_requires=['celery', 'python-iptables', 'plumbum', 'click', 'colorlog'],
+        entry_points='''
+            [console_scripts]
+            saruman=saruman.__main__:cli
+        ''',
+        cmdclass={'install': Install},
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Console',
+            'Intended Audience :: System Administrators',
+            'Natural Language :: English',
+            'Natural Language :: French',
+            'Operating System :: POSIX :: Linux',
+            'Topic :: Internet :: Name Service (DNS)',
+            'Topic :: Internet :: Proxy Servers',
+            'Topic :: System :: Networking',
+            'Topic :: System :: Networking :: Firewalls',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+)
