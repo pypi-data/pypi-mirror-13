dc59bba77b9c322722a3349bcee8394099c2bb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package Pylsy/2.71/Pylsy-2.71.tar.gz

---
+++ packages/Pylsy/Pylsy-2.71.tar.gz/Pylsy-2.71/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    # Use setuptools if available, for install_requires (among other things).
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='Pylsy',
+    packages=['pylsy'],
+    install_requires=['wcwidth'],
+    version='2.71',
+    description='Pylsy is a simple library that draws tables in the Terminal.',
+    long_description=open('README.md').read(),
+    author='leviathan',
+    author_email='leviathan1995@outlook.com',
+    url='https://github.com/Leviathan1995/Pylsy',
+    license='MIT',
+    test_suite='pylsy.tests',
+)
