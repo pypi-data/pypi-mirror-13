6e54e6aacb0b4f73ba0ed47265cebc8f6f3c2ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package black-magic/0.0.11/black-magic-0.0.11.tar.gz

---
+++ packages/black-magic/black-magic-0.0.11.tar.gz/black-magic-0.0.11/black_magic/__init__.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+__title__ = 'black-magic'
+__version__ = '0.0.11'
+
+__summary__ = 'Decorator utility that operates on black magic'
+__uri__ = 'https://github.com/coldfix/black-magic'
+
+__author__ = 'Thomas Gläßle'
+__author_email__ = 't_glaessle@gmx.de'
+
+__license__ = 'Unlicense'
+__copyright__ = '(c) 2013-2015 Thomas Gläßle'
+
+__classifiers__ = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development',
+    'License :: Public Domain',
+]
