ef797f627aa26c6f436504405ac62323f6f4e2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package 652ga/1.3.1/652ga-1.3.1.zip

---
+++ packages/652ga/652ga-1.3.1.zip/652ga-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+				
+setup(
+    name = '652ga',
+    version = '1.3.1',
+    py_modules = ['652ga'],
+    author = 'Learning',
+    author_email = 'animation_pursuit@yahoo.com',
+    url = '',
+    description = 'A simple printer of nested lists',
+    )
