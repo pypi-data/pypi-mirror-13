33f2d9c96ca72faae574a7cd5989aef1cbbb56df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:35 +0000

    Add package n1/1.4.0/n1-1.4.0.zip

---
+++ packages/n1/n1-1.4.0.zip/n1-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+        name        = 'n1',
+        version     ='1.4.0',
+        py_modules  =['n1'],
+        author      ='heal_w',
+        author_email='z1z2bz3@163.com',
+        description ='A simple printer of nested lists',
+        )
