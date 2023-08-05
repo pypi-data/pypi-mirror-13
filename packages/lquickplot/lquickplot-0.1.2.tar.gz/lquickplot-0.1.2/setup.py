20ab7cfd43c963ffe029cbaa8064aa72b4f51a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:27 +0000

    Add package lquickplot/0.1.2/lquickplot-0.1.2.tar.gz

---
+++ packages/lquickplot/lquickplot-0.1.2.tar.gz/lquickplot-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'lquickplot',
+        version = '0.1.2',
+        keywords = ('simple', 'test'),
+        description = 'just a simple test',
+        license = 'MIT License',
+        author = 'ap9035',
+        author_email = 'ap9035@gmail.com',
+
+        scripts = ['quickplot.py'],
+        package = ['Interpolation'],
+
+        platforms = 'any',
+)
