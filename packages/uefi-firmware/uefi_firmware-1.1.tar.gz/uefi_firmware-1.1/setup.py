c5496bcb185fca793a3bb53d6845c39c205c3cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package uefi-firmware/1.1/uefi_firmware-1.1.tar.gz

---
+++ packages/uefi-firmware/uefi_firmware-1.1.tar.gz/uefi_firmware-1.1/setup.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import os
+import re
+from setuptools import setup, find_packages, Extension, Command
+
+
+class LintCommand(Command):
+    """Run pylint on implementation and test code"""
+
+    description = "Run pylint on implementation and test code"
+    user_options = []
+
+    _pylint_options = [
+        "--max-line-length 80",
+        "--ignore-imports yes"
+    ]
+
+    _lint_paths = [
+        "uefi_firmware/*.py",
+        "uefi_firmware/*/*.py",
+        "tests/*.py",
+        "scripts/*.py",
+        "scripts/contrib/*.py",
+    ]
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        """Run the command"""
+        os.system("pylint %s %s" % (
+            " ".join(self._pylint_options),
+            " ".join(self._lint_paths),
+        ))
+
+with open('README.rst') as f:
+    README = f.read()
+
+with open('LICENSE') as f:
+    LICENSE = f.read()
+
+with open("uefi_firmware/__init__.py", "r") as f:
+    __INIT__ = f.read()
+
+VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    __INIT__, re.MULTILINE).group(1)
+AUTHOR = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                   __INIT__, re.MULTILINE).group(1)
+
+setup(
+    name='uefi_firmware',
+    version=VERSION,
+    description='Various data structures and parsing tools for UEFI firmware.',
+    long_description=README,
+    author=AUTHOR,
+    author_email='teddy@prosauce.org',
+    license=LICENSE,
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite="tests",
+    cmdclass={
+        "lint": LintCommand,
+    },
+
+    ext_modules=[
+        Extension(
+            'uefi_firmware.efi_compressor',
+            sources=[
+                os.path.join(
+                    "uefi_firmware", "compression", "Tiano", "EfiCompress.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "Tiano", "TianoCompress.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "Tiano", "Decompress.c"),
+
+                os.path.join(
+                    "uefi_firmware", "compression", "LZMA", "SDK", "C", "Bra86.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "LZMA", "SDK", "C", "LzFind.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "LZMA", "SDK", "C", "LzmaDec.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "LZMA", "SDK", "C", "LzmaEnc.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "LZMA", "LzmaCompress.c"),
+                os.path.join(
+                    "uefi_firmware", "compression", "LZMA", "LzmaDecompress.c"),
+
+                os.path.join("uefi_firmware", "compression", "EfiCompressor.c")
+            ],
+            include_dirs=[
+                os.path.join("uefi_firmware", 'compression', 'Include')
+            ],
+        )
+    ],
+
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Topic :: Security',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords="security uefi firmware parsing bios",
+)
---
+++ packages/uefi-firmware/uefi_firmware-1.1.tar.gz/uefi_firmware-1.1/uefi_firmware/base.py
@@ -0,0 +1,120 @@
+'''Base provides basic firmware object structures.
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import ctypes
+
+from .utils import dump_data, sguid, blue
+
+
+class BaseObject(object):
+    '''A base object can be used to access direct content.'''
+
+
+class FirmwareObject(object):
+    def __init__(self):
+        self.data = None
+        self.name = None
+        self.attrs = None
+        self.guid = None
+
+    @property
+    def content(self):
+        if hasattr(self, "data") and self.data is not None:
+            return self.data
+        return ""
+
+    @property
+    def objects(self):
+        return []
+
+    @property
+    def label(self):
+        if hasattr(self, "name") and self.name is not None:
+            if self.name is None:
+                return ""
+            return self.name
+        return ""
+
+    @property
+    def guid_label(self):
+        if not hasattr(self, "guid") or self.guid is None:
+            return ""
+        return sguid(self.guid)
+
+    @property
+    def type_label(self):
+        return self.__class__.__name__
+
+    @property
+    def attrs_label(self):
+        if hasattr(self, "attrs") and self.attrs is not None:
+            return self.attrs
+        return {}
+
+    def info(self, include_content=False):
+        return {
+            "_self": self,
+            "label": self.label,
+            "guid": self.guid_label,
+            "type": self.type_label,
+            "content": self.content if include_content else "",
+            "attrs": self.attrs_label
+        }
+
+    def iterate_objects(self, include_content=False):
+        objects = []
+        for _object in self.objects:
+            if _object is None:
+                continue
+            _info = _object.info(include_content)
+            _info["objects"] = _object.iterate_objects(include_content)
+            for _object in _info["objects"]:
+                _object["parent"] = _info
+            objects.append(_info)
+        return objects
+
+
+class StructuredObject(object):
+    def __init__(self):
+        self.fields = []
+
+    def parse_structure(self, data, structure):
+        '''Construct an instance object of the provided structure.'''
+        struct_instance = structure()
+        struct_size = ctypes.sizeof(struct_instance)
+
+        struct_data = data[:struct_size]
+        struct_length = min(len(struct_data), struct_size)
+        ctypes.memmove(
+            ctypes.addressof(struct_instance), struct_data, struct_length)
+        self.structure = struct_instance
+        self.structure_data = struct_data
+        self.structure_fields = [field[0] for field in structure._fields_]
+        self.structure_size = struct_size
+
+    def show_structure(self):
+        for field in self.fields:
+            print ("%s: %s" % (field, getattr(self.structure, field, None)))
+
+
+class RawObject(FirmwareObject, BaseObject):
+
+    def __init__(self, data):
+        self.data = data
+
+    def build(self, generate_checksum, debug=False):
+        return self.data
+
+    def showinfo(self, ts='', index=None):
+        print ("%s%s size= %d " % (
+            ts, blue("RawObject:"), len(self.data)
+        ))
+
+    def dump(self, parent='', index=None):
+        path = os.path.join(parent, "object.raw")
+        dump_data(path, self.data)
