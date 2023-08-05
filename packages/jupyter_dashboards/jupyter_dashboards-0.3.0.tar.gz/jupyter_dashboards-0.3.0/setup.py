27ad270e77478a776727f6e08e6e31169912d1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package jupyter_dashboards/0.3.0/jupyter_dashboards-0.3.0.tar.gz

---
+++ packages/jupyter_dashboards/jupyter_dashboards-0.3.0.tar.gz/jupyter_dashboards-0.3.0/setup.py
@@ -0,0 +1,88 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import os
+import errno
+from setuptools import setup
+from setuptools.command.install import install
+
+from notebook.nbextensions import install_nbextension
+from notebook.services.config import ConfigManager
+
+# Get location of this file at runtime
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Eval the version tuple and string from the source
+VERSION_NS = {}
+with open(os.path.join(HERE, 'urth/dashboard/_version.py')) as f:
+    exec(f.read(), {}, VERSION_NS)
+
+EXT_DIR = os.path.join(HERE, 'urth_dash_js')
+
+def makedirs(path):
+    '''
+    mkdir -p and ignore existence errors compatible with Py2/3.
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+class InstallCommand(install):
+    def run(self):
+        js_cm = ConfigManager()
+        makedirs(js_cm.config_dir)
+
+        print('Installing Python server extension')
+        install.run(self)
+        
+        print('Installing notebook JS extension')
+        install_nbextension(EXT_DIR, overwrite=True, user=True)
+
+        print('Enabling notebook JS extension')
+        js_cm.update('notebook', {"load_extensions": {'urth_dash_js/notebook/main': True}})
+
+setup(
+    name='jupyter_dashboards',
+    author='Jupyter Development Team',
+    author_email='jupyter@googlegroups.com',
+    description='Extension for Jupyter Notebook 4.0.x for laying out, viewing, and deploying notebooks as dynamic dashboards',
+    long_description = '''
+    This package adds the following features to Jupyter Notebook:
+
+* Dashboard layout mode for arranging notebook cell outputs in a grid-like fashion
+* Dashboard view mode for interacting with an assembled dashboard within the Jupyter Notebook
+* Ability to share notebooks with dashboard layout metadata in them with other Jupyter Notebook users
+* Ability to nbconvert a notebook to a separate dashboard web application
+
+See `the project README <https://github.com/jupyter-incubator/dashboards>`_
+for more information. 
+''',
+    url='https://github.com/jupyter-incubator/dashboards',
+    version=VERSION_NS['__version__'],
+    license='BSD',
+    platforms=['Jupyter Notebook 4.0.x'],
+    packages=[
+        'urth', 
+        'urth.dashboard'
+    ],
+    include_package_data=True,
+    install_requires=[],
+    cmdclass={
+        'install': InstallCommand
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
