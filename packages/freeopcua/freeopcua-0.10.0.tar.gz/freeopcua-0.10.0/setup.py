c544a5399e1c855167e428e8420d4a8de1e251f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:26 +0000

    Add package freeopcua/0.10.0/freeopcua-0.10.0.tar.gz

---
+++ packages/freeopcua/freeopcua-0.10.0.tar.gz/freeopcua-0.10.0/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+import sys
+
+if sys.version_info[0] < 3:
+    install_requires = ["enum34", "trollius", "futures", "cryptography"]
+else:
+    install_requires = ["cryptography"]
+
+setup(name="freeopcua", 
+      version="0.10.0",
+      description="Pure Python OPC-UA client and server library",
+      author="Olivier Roulet-Dubonnet",
+      author_email="olivier.roulet@gmail.com",
+      url='http://freeopcua.github.io/',
+      packages=["opcua"],
+      provides=["opcua"],
+      license="GNU Lesser General Public License v3 or later",
+      install_requires=install_requires,
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 2",
+                   "Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "Operating System :: OS Independent",
+                   "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      entry_points={'console_scripts': 
+                    [
+                        'uaread = opcua.tools:uaread',
+                        'uals = opcua.tools:uals',
+                        'uabrowse = opcua.tools:uals',
+                        'uawrite = opcua.tools:uawrite',
+                        'uasubscribe = opcua.tools:uasubscribe',
+                        'uahistoryread = opcua.tools:uahistoryread',
+                        'uaclient = opcua.tools:uaclient',
+                        'uaserver = opcua.tools:uaserver',
+                        'uadiscover = opcua.tools:uadiscover'
+                    ]
+                    }
+      )
+
