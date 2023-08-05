fc57a6acfd6b8bdb7e6c1e70159ad9c9a7dfa9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package bibtexparser/0.6.2/bibtexparser-0.6.2.tar.gz

---
+++ packages/bibtexparser/bibtexparser-0.6.2.tar.gz/bibtexparser-0.6.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError as ex:
+    print('[python-bibtexparser] setuptools not found. Falling back to distutils.core')
+    from distutils.core import setup
+from bibtexparser import __version__ as version
+
+setup(
+    name         = 'bibtexparser',
+    version      = version,
+    url          = "https://github.com/sciunto-org/python-bibtexparser",
+    author       = "Francois Boulogne and other contributors",
+    license      = "LGPLv3 or BSD",
+    author_email = "fboulogne@sciunto.org",
+    description  = "Bibtex parser for python 2.7 and 3.3 and newer",
+    packages = ['bibtexparser'],
+)
