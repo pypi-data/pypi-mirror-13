533017ebe8fca79eb50c1db430d823d9ffb8c851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package pyprogressbar/0.0.2/pyprogressbar-0.0.2.zip

---
+++ packages/pyprogressbar/pyprogressbar-0.0.2.zip/pyprogressbar-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pyprogressbar",
+    version="0.0.2",
+    author="Gefu Tang",
+    author_email="tanggefu@gmail.com",
+    description=("pyprogressbar is a progress bar control for python."),
+    license="GNU",
+    keywords="progress bar",
+    url="https://github.com/primetang/pyprogressbar",
+    packages=['pyprogressbar'],
+    package_dir={'pyprogressbar': 'src'},
+)
