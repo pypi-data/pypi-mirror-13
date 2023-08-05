c05789f5afd931d79f75716b55d429329d8eaeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:54 +0000

    Add package pInteServ/0.116/pInteServ-0.116.tar.gz

---
+++ packages/pInteServ/pInteServ-0.116.tar.gz/pInteServ-0.116/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('./README.rst') as f:
+        return f.read()
+
+setup(name='pInteServ',
+      version='0.116',
+      description='Module and cli for client/server directory sync.',
+      long_description=readme(),
+      url='https://github.com/paulcrook726/PiCloud.git',
+      author='Paul Crook',
+      author_email='paulcrook726@gmail.com',
+      license='GNU',
+      entry_points={
+          'console_scripts':
+              ['picli = pytp.client_cli:main',
+               'piserver = pytp.server:main'],
+      },
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Internet :: File Transfer Protocol (FTP)',
+      ],
+      install_requires=[
+          'pynacl',]
+      )
