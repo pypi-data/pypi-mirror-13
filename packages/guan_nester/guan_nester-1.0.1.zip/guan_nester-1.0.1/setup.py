fbae0fbaf7a379b50d64c8f7864cce638535a683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:09 +0000

    Add package guan_nester/1.0.1/guan_nester-1.0.1.zip

---
+++ packages/guan_nester/guan_nester-1.0.1.zip/guan_nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name           = "guan_nester",
+    version        = "1.0.1",
+    py_modules     = ["guan_nester"],
+    author         = "Guan",
+    author_email   = "gzq960412@163.com",
+    url            = " ",
+    description    = "A simple printer of guan_nested lists"
+    )
