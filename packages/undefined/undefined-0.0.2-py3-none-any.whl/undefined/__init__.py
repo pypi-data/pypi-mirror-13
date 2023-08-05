bb422a59e7a5efa670c7dd5472294f36f01860f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package undefined/0.0.2/undefined-0.0.2-py3-none-any.whl

---
+++ packages/undefined/undefined-0.0.2-py3-none-any.whl/undefined/__init__.py
@@ -0,0 +1,44 @@
+"""
+a simple package
+"""
+
+
+__version__ = '0.0.2'
+
+import sys
+import types
+
+class Undefined(types.ModuleType):
+    """
+    Simply a global object that act as undefined.
+    """
+
+    __version__ = __version__
+
+
+    @property
+    def Undefined(self):
+        return self
+
+    def __call__(self, value):
+        return value is self
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    __str__ = __repr__
+
+
+Undefined.__name__ = 'Undefined'
+Undefined = Undefined('undefined', """
+    Simply a global object that act as undefined.
+    """
+)
+
+Undefined.__version__ = __version__
+
+if sys.modules[__name__] is Undefined:
+    print('doing nothing')
+    pass
+else:
+   sys.modules[__name__] = Undefined
