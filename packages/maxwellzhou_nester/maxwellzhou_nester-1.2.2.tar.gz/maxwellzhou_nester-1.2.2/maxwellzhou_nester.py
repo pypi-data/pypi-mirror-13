981a5d38b150f3882ae0df60f219ec05232161fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:23 +0000

    Add package maxwellzhou_nester/1.2.2/maxwellzhou_nester-1.2.2.tar.gz

---
+++ packages/maxwellzhou_nester/maxwellzhou_nester-1.2.2.tar.gz/maxwellzhou_nester-1.2.2/maxwellzhou_nester.py
@@ -0,0 +1,8 @@
+def print_lol(the_list, level):
+        for each_itme in the_list:
+                if isinstance(each_itme, list):
+                        print_lol(each_itme, level + 1)
+                else:
+                        for tap_stop in range(level):
+                                print("\t", end= ' ')
+                        print(each_itme)
