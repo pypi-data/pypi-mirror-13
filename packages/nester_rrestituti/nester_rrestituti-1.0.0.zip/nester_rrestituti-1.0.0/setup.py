ae08addbb43dd66981a91d269ef92b16411b7af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package nester_rrestituti/1.0.0/nester_rrestituti-1.0.0.zip

---
+++ packages/nester_rrestituti/nester_rrestituti-1.0.0.zip/nester_rrestituti-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_rrestituti',
+        version = '1.0.0',
+        py_modules = ['nester_rrestituti'],
+        author = 'rrestituti',
+        author_email = 'rrestituti@uol.com.br',
+        url = '',
+        description = 'A simple printer of nested lists'
+     )
