702dd0e3056f4da602d0d07fa6d4c8bf63d0a42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package easytrader/0.1/easytrader-0.1.tar.gz

---
+++ packages/easytrader/easytrader-0.1.tar.gz/easytrader-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+      name = 'easytrader',
+      packages = ['easytrader'], # this must be the same as the name above
+      version = '0.1',
+      description = 'A utility for China Stock Trade',
+      author = 'shidenggui',
+      author_email = 'longlyshidenggui@gmail.com',
+      url = 'https://github.com/shidenggui/easytrader', # use the URL to the github repo
+      download_url = 'https://github.com/shidenggui/easytrader', # I'll explain this in a second
+      keywords = ['China', 'stock', 'trade'], # arbitrary keywords
+      classifiers = [],
+
+)
