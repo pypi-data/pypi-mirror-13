66a9af0da1f478b328a06fea30ff35f122a7f694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package nylas/1.2.0/nylas-1.2.0.tar.gz

---
+++ packages/nylas/nylas-1.2.0.tar.gz/nylas-1.2.0/tests/credentials.py
@@ -0,0 +1,3 @@
+APP_ID = '59f6jb9vc3clikrlly1rw3es2'
+APP_SECRET = '38y8aw1qog3doehmnlekx304e'
+AUTH_TOKEN = 'PvNFQhNiSu9mDND7FJGLLrHOtaAO1v' # The access token to a local account (i.e: running on your machine).
