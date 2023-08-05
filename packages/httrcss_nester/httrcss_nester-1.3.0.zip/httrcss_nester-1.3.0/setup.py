a0fcdc38bc29f2c9acaa6ff6e5fbf8c7253ec0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package httrcss_nester/1.3.0/httrcss_nester-1.3.0.zip

---
+++ packages/httrcss_nester/httrcss_nester-1.3.0.zip/httrcss_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'httrcss_nester',
+        version='1.3.0',
+        py_modules=['httrcss_nester'],
+        author='httrcss',
+        author_email='httrcss@126.com',
+        url='http://www.126.com',
+        description='a simple printer of nested lists',
+    )
