4ac4da531237c92ef0e5b0f5905a1a1875a2e28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package fabu/1.3.0/fabu-1.3.0.tar.gz

---
+++ packages/fabu/fabu-1.3.0.tar.gz/fabu-1.3.0/qiantao.py
@@ -0,0 +1,12 @@
+
+"""这是一个嵌套函数"""
+def print_qiantao(every,indent=False,level=0):
+    """这是一个递归函数，用于显示出一个数组重嵌套的多个数组重的每个元素"""
+    for everyone in every:
+        if isinstance(everyone,list):
+            print_qiantao(everyone,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+        print(everyone)
