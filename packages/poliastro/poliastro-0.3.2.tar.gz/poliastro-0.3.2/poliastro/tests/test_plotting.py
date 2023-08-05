c7286fbee93da07bfaa4590ec8d33e463fb4da42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:29 +0000

    Add package poliastro/0.3.2/poliastro-0.3.2.tar.gz

---
+++ packages/poliastro/poliastro-0.3.2.tar.gz/poliastro-0.3.2/poliastro/tests/test_plotting.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import pytest
+
+import astropy.units as u
+
+import matplotlib
+matplotlib.use("Agg", warn=False) # use Agg backend for these tests
+
+from poliastro import plotting
+from poliastro.plotting import OrbitPlotter
+
+
+def test_OrbitPlotter_has_axes():
+    ax = "Unused axes"
+    op = OrbitPlotter(ax)
+    assert op.ax is ax
+
+
+def test_set_frame_raises_error_if_frame_exists():
+    op = OrbitPlotter()
+    p = [1, 0, 0] * u.one
+    q = [0, 1, 0] * u.one
+    w = [0, 0, 1] * u.one
+    op.set_frame(p, q, w)
+    with pytest.raises(NotImplementedError):
+        op.set_frame(p, q, w)
