e8bce54f4f8bc70a4de52bbbb4e2098b55b70c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package popt/0.3/popt-0.3.tar.gz

---
+++ packages/popt/popt-0.3.tar.gz/popt-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='popt',
+      packages=['popt'],
+      version='0.3',
+      description='Tool for converting Robot Framework xml output to a human-readable log file',
+      author='Janne Kohvakka',
+      author_email='jkohvakk@gmail.com',
+      url='https://github.com/jkohvakk/popt',
+      keywords=['Robot Framework', 'log'],
+      classifiers=[],
+      entry_points={'console_scripts': ['popt=popt:read_arguments']})
