55d5d020eca35f12f1a04efaea80dfc36104df98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:14 +0000

    Add package grabfeed/0.3.2/grabfeed-0.3.2.tar.gz

---
+++ packages/grabfeed/grabfeed-0.3.2.tar.gz/grabfeed-0.3.2/grabfeed/test.py
@@ -0,0 +1,40 @@
+import unittest
+from grabber import return_rss
+
+
+class TestGrabber(unittest.TestCase):
+
+    def test_wordpress(self):
+        link = "http://techcrunch.com"
+        self.assertEqual(return_rss(link), 'http://techcrunch.com/feed/')
+
+    def test_blogger(self):
+        link = "http://google.blogspot.com/"
+        self.assertEqual(
+            return_rss(link),
+            'http://google.blogspot.com/feeds/posts/default?alt=rss'
+        )
+
+    def test_svtle(self):
+        link = 'http://blog.svbtle.com/'
+        self.assertEqual(
+            return_rss(link),
+            'http://blog.svbtle.com/feed'
+        )
+
+    def test_ghost(self):
+        link = 'https://blog.ghost.org/'
+        self.assertEqual(
+            return_rss(link),
+            'https://blog.ghost.org/rss/'
+        )
+
+    def test_medium(self):
+        link = 'https://medium.com/stories-from-nlocate'
+        self.assertEqual(
+            return_rss(link),
+            'https://medium.com/feed/stories-from-nlocate'
+        )
+
+if __name__ == '__main__':
+    unittest.main()
