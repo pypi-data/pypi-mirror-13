85548fa46762e66ad85b35f1ee837260f52f2a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package bottlecap/0.3/bottlecap-0.3.tar.gz

---
+++ packages/bottlecap/bottlecap-0.3.tar.gz/bottlecap-0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+setup(
+    name="bottlecap",
+    description="Extras for Bottle",
+    author='Cal Leeming',
+    author_email='cal@iops.io',
+    url='https://github.com/foxx/bottlecap',
+    keywords=['bottle', 'bottlecap'],
+    version="0.3",
+    py_modules=['bottlecap'],
+    setup_requires=[
+        'pytest-runner>=2.6',
+        'yanc>=0.3'
+    ],
+    install_requires=[
+        'bottle',
+        'click',
+        'werkzeug',
+        'six'
+    ],
+    tests_require=[
+        'bottle>=0.12',
+        'pytest-benchmark>=3.0',
+        'pytest-raisesregexp>=2.1',
+        'pytest-cov>=2.2',
+        'pytest>=2.8',
+        'webtest>=2.0',
+        'python-coveralls',
+        'tox'
+    ]
+)
