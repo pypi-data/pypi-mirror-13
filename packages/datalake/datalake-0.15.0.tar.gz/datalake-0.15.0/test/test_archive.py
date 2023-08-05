3df578c5e40227facbe36ee89b5f53e2924a75b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:28 +0000

    Add package datalake/0.15.0/datalake-0.15.0.tar.gz

---
+++ packages/datalake/datalake-0.15.0.tar.gz/datalake-0.15.0/test/test_archive.py
@@ -0,0 +1,28 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import simplejson as json
+
+
+def test_push_file(archive, random_metadata, tmpfile, s3_key):
+    expected_content = 'mwahaha'
+    f = tmpfile(expected_content)
+    url = archive.prepare_metadata_and_push(f, **random_metadata)
+    from_s3 = s3_key(url)
+    assert from_s3.get_contents_as_string() == expected_content
+    metadata = from_s3.get_metadata('datalake')
+    assert metadata is not None
+    metadata = json.loads(metadata)
+    common_keys = set(metadata.keys()).intersection(random_metadata.keys())
+    assert common_keys == set(random_metadata.keys())
