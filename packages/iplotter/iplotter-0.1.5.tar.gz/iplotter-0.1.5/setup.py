f9bff1a9d29950c3e73f1975db585328fe74042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:53 +0000

    Add package iplotter/0.1.5/iplotter-0.1.5.tar.gz

---
+++ packages/iplotter/iplotter-0.1.5.tar.gz/iplotter-0.1.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from setuptools import find_packages
+import os
+
+from iplotter import __version__
+
+if __name__ == '__main__':
+
+    setup(name='iplotter',
+          version=__version__,
+          description='C3.js and plotly.js for iPython/Jupyter notebooks',
+          setup_requires=['setuptools-markdown'],
+          long_description_markdown_filename='README.md',
+          url='https://github.com/niloch/iplotter',
+          author='csulliva',
+          author_email='csulliva@brandeis.edu',
+          license='MIT',
+          packages=find_packages(),
+          install_requires=[
+              'Jinja2>=2.0',
+              'ipython>=3.0',
+              'notebook>=3.0'
+          ],
+          zip_safe=False,
+          include_package_data=True,
+          keywords=['ipython', 'plotly', 'c3', 'plot', 'chart'],
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Topic :: Scientific/Engineering :: Visualization',
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+              "Programming Language :: Python :: 3.3",
+              "Programming Language :: Python :: 3.4",
+              "Programming Language :: Python :: 3.5",
+          ])
