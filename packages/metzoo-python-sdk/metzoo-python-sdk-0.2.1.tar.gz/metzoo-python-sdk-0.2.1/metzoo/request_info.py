a2bb97add3a9180f1a0fd3f7672b3559b35543df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:20 +0000

    Add package metzoo-python-sdk/0.2.1/metzoo-python-sdk-0.2.1.tar.gz

---
+++ packages/metzoo-python-sdk/metzoo-python-sdk-0.2.1.tar.gz/metzoo-python-sdk-0.2.1/metzoo/request_info.py
@@ -0,0 +1,14 @@
+class RequestInfo(object):
+    """Used to send requests"""
+
+    def __init__(self, method, route):
+        self._method = method
+        self._route = route
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def route(self):
+        return self._route
