86928776b67e9b963b6e0811e71738babdff2af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:00 +0000

    Add package python-lunrclient/1.1.0/python-lunrclient-1.1.0.tar.gz

---
+++ packages/python-lunrclient/python-lunrclient-1.1.0.tar.gz/python-lunrclient-1.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-lunrclient',
+    version='1.1.0',
+    description='LunrClient',
+    license='Apache License (2.0)',
+    author='Derrick J Wippler',
+    author_email='thrawn01@gmail.com',
+    url='rackspace.com',
+    packages=find_packages(exclude=['test']),
+    test_suite='nose.collector',
+    install_requires=['requests', 'prettytable>=0.7'],
+    entry_points={
+        'console_scripts': ['storage = lunrclient.storage_shell:main',
+                            'lunr = lunrclient.lunr_shell:main']
+    }
+)
