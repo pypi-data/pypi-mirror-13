bc31fa5dfa76157032ea2b2b2260ae8611976d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package python-awk/0.0.1/python-awk-0.0.1.tar.gz

---
+++ packages/python-awk/python-awk-0.0.1.tar.gz/python-awk-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'python-awk',
+    packages = ['python_awk'],
+    version = "0.0.1",
+    description = 'Placeholder description',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/pawk",
+    download_url = 'https://github.com/jasontrigg0/pawk/tarball/0.0.1',
+    scripts=["python_awk/pawk"],
+    install_requires=[],
+    keywords = [],
+    classifiers = [],
+)
