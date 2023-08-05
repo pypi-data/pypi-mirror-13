25ef8cdb77e4f2e0364642e8b69ffd5afe7a1b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package hp_nester/1.0.0/hp_nester-1.0.0.tar.gz

---
+++ packages/hp_nester/hp_nester-1.0.0.tar.gz/hp_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='hp_nester',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='How Peng',
+    author_email='howpeng@foxmail.com',
+    url='http://howpeng.com',
+    description='A simple printer of nested lists.',
+)
