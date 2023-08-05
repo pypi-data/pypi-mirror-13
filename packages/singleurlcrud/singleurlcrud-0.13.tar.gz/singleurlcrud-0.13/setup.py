1b4f5abbd66facf064711d9a4d56adb8af4efdde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:15 +0000

    Add package singleurlcrud/0.13/singleurlcrud-0.13.tar.gz

---
+++ packages/singleurlcrud/singleurlcrud-0.13.tar.gz/singleurlcrud-0.13/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='singleurlcrud',
+    description='Django CRUD, implemented using a single view and consequently a single URL.',
+    long_description=open('README.md').read(),
+    version='0.13',
+    author='Hari Mahadevan',
+    author_email='hari@hari.xyz',
+    url='https://www.github.com/harikvpy/crud/tarball/0.13',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['django-pure-pagination', 'django-bootstrap3'],
+    license='BSD-3',
+    keywords=['django', 'crud'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )
