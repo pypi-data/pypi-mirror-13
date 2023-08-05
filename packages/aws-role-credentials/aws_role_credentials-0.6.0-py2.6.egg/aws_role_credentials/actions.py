db370e9604987a16123043d565029c28fe68e992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:13 +0000

    Add package aws-role-credentials/0.6.0/aws_role_credentials-0.6.0-py2.6.egg

---
+++ packages/aws-role-credentials/aws_role_credentials-0.6.0-py2.6.egg/aws_role_credentials/actions.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import os
+import boto.sts
+import shlex
+from subprocess import Popen
+
+from aws_role_credentials.models import SamlAssertion, AwsCredentialsFile
+
+
+class Actions:
+
+    def __init__(self,
+                 credentials_filename,
+                 profile,
+                 region,
+                 quiet,
+                 **kwargs):
+
+        self.credentials_filename = credentials_filename
+        self.profile = profile
+        self.region = region
+        self.quiet = quiet
+
+    @staticmethod
+    def persist_credentials(credentials_filename,
+                            profile, region, token, quiet, **kwargs):
+        AwsCredentialsFile(credentials_filename).add_profile(profile,
+                                                             region,
+                                                             token.credentials,
+                                                             quiet)
+
+    @staticmethod
+    def credentials_handler(credentials_filename,
+                            profile,
+                            region, quiet, **kwargs):
+
+        return lambda token: Actions.persist_credentials(credentials_filename,
+                                                         profile,
+                                                         region,
+                                                         token,
+                                                         quiet)
+
+    @staticmethod
+    def exec_with_credentials(region, command, token):
+        env = os.environ.copy()
+
+        env["AWS_ACCESS_KEY_ID"] = token.credentials.access_key
+        env["AWS_SECRET_ACCESS_KEY"] = token.credentials.secret_key
+        env["AWS_SESSION_TOKEN"] = token.credentials.session_token
+        env["AWS_DEFAULT_REGION"] = region
+
+        Popen(shlex.split(command),
+              env=env,
+              shell=False).wait()
+
+    @staticmethod
+    def exec_handler(region, exec_command, **kwargs):
+        return lambda token: Actions.exec_with_credentials(region, exec_command, token)
+
+    @staticmethod
+    def saml_token(region, assertion, **kwargs):
+        assertion = SamlAssertion(assertion)
+        role = assertion.roles()[0]
+
+        conn = boto.sts.connect_to_region(region, anon=True)
+        return conn.assume_role_with_saml(role['role'], role['principle'],
+                                          assertion.encode())
+
+    @staticmethod
+    def user_token(region, role_arn, session_name,
+                   mfa_serial_number=None, mfa_token=None,
+                   **kwargs):
+        conn = boto.sts.connect_to_region(region)
+
+        return conn.assume_role(role_arn, session_name,
+                                mfa_serial_number=mfa_serial_number,
+                                mfa_token=mfa_token)
---
+++ packages/aws-role-credentials/aws_role_credentials-0.6.0-py2.6.egg/aws_role_credentials/models.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+import ConfigParser
+import base64
+
+
+class SamlAssertion:
+
+    def __init__(self, assertion):
+        self.assertion = assertion
+
+    @staticmethod
+    def split_roles(roles):
+        return [(y.strip())
+                for y
+                in roles.text.split(',')]
+
+    @staticmethod
+    def sort_roles(roles):
+        return sorted(roles,
+                      key=lambda role: 'saml-provider' in role)
+
+    def roles(self):
+        attributes = ET.fromstring(self.assertion).getiterator('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute')
+
+        roles_attributes = [x for x
+                            in attributes
+                            if x.get('Name') == 'https://aws.amazon.com/SAML/Attributes/Role']
+
+        roles_values = [(x.getiterator('{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue'))
+                        for x
+                        in roles_attributes]
+
+        return [(dict(zip(['role', 'principle'],
+                          self.sort_roles(self.split_roles(x)))))
+                for x
+                in roles_values[0]]
+
+    def encode(self):
+        return base64.b64encode(self.assertion)
+
+
+class AwsCredentialsFile:
+
+    def __init__(self, filename):
+        self.filename = filename
+        return
+
+    def _add_profile(self, name, profile):
+        config = ConfigParser.RawConfigParser()
+        config.read(self.filename)
+
+        if not config.has_section(name):
+            config.add_section(name)
+
+        [(config.set(name, k, v))
+         for k, v in profile.iteritems()]
+
+        with open(self.filename, 'w+') as configfile:
+            config.write(configfile)
+
+    def add_profile(self, name, region, credentials, quiet):
+        self._add_profile(name, {'output': 'json',
+                                 'region': region,
+                                 'aws_access_key_id': credentials.access_key,
+                                 'aws_secret_access_key': credentials.secret_key,
+                                 'aws_security_token': credentials.session_token,
+                                 'aws_session_token': credentials.session_token})
+
+        # Tell user token expiration and basic profile usage
+        if not quiet:
+            print '\n\n----------------------------------------------------------------'
+            print 'Your credentials have been stored in the AWS configuration file {0} under the {1} profile.'.format(self.filename, name)
+            print 'Note that they will expire at {0}.'.format(credentials.expiration)
+            print 'You may safely rerun this script at any time to refresh your credentials.'
+            print 'To use this credential, call the AWS CLI with the --profile option (e.g. aws --profile {0} ec2 describe-instances).'.format(name)
+            print '----------------------------------------------------------------\n\n'
