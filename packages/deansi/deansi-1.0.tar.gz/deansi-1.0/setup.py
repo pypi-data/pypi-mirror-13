104820cd2bf960b21eaed374ec03d1f120a3aa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package deansi/1.0/deansi-1.0.tar.gz

---
+++ packages/deansi/deansi-1.0.tar.gz/deansi-1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+readme = open("README.rst").read()
+
+setup(
+	name = "deansi",
+	version = "1.0",
+	description = "ANSI codes to HTML converter",
+	author = "David García Garzón",
+	author_email = "voki@canvoki.net",
+	url = 'https://github.com/GuifiBaix/python-deansi',
+	long_description = readme,
+	license = 'GNU General Public License v3 or later (GPLv3+)',
+	packages=find_packages(exclude=['*[tT]est*']),
+	py_modules=[
+		'deansi',
+		],
+	scripts=[
+		'deansi.py',
+		],
+	include_package_data = True,
+	test_suite = 'deansi_test',
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: System :: Logging',
+		'Topic :: Text Processing :: Filters',
+		'Topic :: Text Processing :: Markup :: HTML',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+		'Operating System :: OS Independent',
+	],
+)
+
