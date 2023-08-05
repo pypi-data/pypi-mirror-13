5d3fc015b0140468286307345e77241de64df7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:28 +0000

    Add package evepi/0.66/evepi-0.66.tar.gz

---
+++ packages/evepi/evepi-0.66.tar.gz/evepi-0.66/evepi/snd_01.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import socket
+import json
+
+fuck_json_path = open( "/etc/snd_01/fuckpath.md" ).read()
+fuck_json_file = file( fuck_json_path )
+fuck_json = json.load( fuck_json_file )
+
+def get_res( name, cmd="fuck" ):
+    global fuck_json
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # 定义socket类型，网络通信
+    s.connect( (fuck_json[name][0], fuck_json[name][1]) )       # 要连接的IP与端口
+    s.sendall( cmd )      # 把命令发送给对端
+    data=s.recv(1024)     # 把接收的数据定义为变量
+    s.close()   # 关闭连接
+    return data
+
+def get_5ms_res( name ):
+    return int(get_res( name, cmd="whether" ) )
+
+def get_1s_res( name ):
+    return 100 - float( get_res(name, cmd="sum") )
