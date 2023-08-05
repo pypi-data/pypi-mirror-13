cbe1206c5332862e68bccd95d9925ab898921eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package GoDaddyPy/0.1.1/GoDaddyPy-0.1.1.tar.gz

---
+++ packages/GoDaddyPy/GoDaddyPy-0.1.1.tar.gz/GoDaddyPy-0.1.1/godaddypy/accounts.py
@@ -0,0 +1,17 @@
+accounts = [
+    {
+        'domains': ['juliancoy.com', 'r00ted.info'],
+        'api_key': 'AEPypMxY8xw_LdV5mhXjqdsRMbfPCfhEhm',
+        'api_secret': 'LdVCC1qNJqtqX34mnrfH6F'
+    }
+]
+
+"""
+from godaddypy import Account, GoDaddyAPI
+my_acct = Account('AEPypMxY8xw_LdV5mhXjqdsRMbfPCfhEhm','LdVCC1qNJqtqX34mnrfH6F')
+bad_acct = Account('AEPypMxY8xw_LdV5mhXjqdsRMbfPCfhEhm', 'badkey')
+
+client = GoDaddyAPI(my_acct)
+bad_client = GoDaddyAPI(bad_acct)
+client.get_domains()
+"""
