02c7c935a9d0818de0b4209183e97f192696de2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package netflix_roulette/0.22/netflix_roulette-0.22.tar.gz

---
+++ packages/netflix_roulette/netflix_roulette-0.22.tar.gz/netflix_roulette-0.22/netflix_roulette/util.py
@@ -0,0 +1,22 @@
+import six
+
+
+class Element(object):
+    __attrs__ = []
+    repr_data_items = []
+
+    def __init__(self):
+        self._populate()
+
+    def __repr__(self):
+        return six.u('<{0.__class__.__name__} {1}>').format(
+            self, six.u(' ').join([six.u("{0}='{1}'").format(
+                k, getattr(self, k, None)) for k in self.repr_data_items]),
+        ).encode('utf-8')
+
+    def _populate(self):
+        self._data_set = True
+
+    def _set_attrs_none(self):
+        for attr in self.__attrs__:
+            setattr(self, attr, None)
