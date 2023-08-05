5ee5a5b30e70f508c5ae88482461fab7bcafa62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package Centaur/0.0.5/Centaur-0.0.5.tar.gz

---
+++ packages/Centaur/Centaur-0.0.5.tar.gz/Centaur-0.0.5/centaur/datatypes/__init__.py
@@ -0,0 +1,38 @@
+from .context import _Context, _Module
+from .utils import select_items, with_items, without_items
+from .decorators import validate_before_call
+from .exceptions import ValidationError
+
+
+def def_datatypes(dt_definitions, _ctx=None):
+    ctx = _ctx or _Context.create_empty()
+    return ctx.def_datatypes(dt_definitions)
+
+
+def def_datatype(dt_definition, _ctx=None):
+    ctx = _ctx or _Context.create_empty()
+    dt = ctx.def_datatype(dt_definition)
+    return dt
+
+
+def load_module(module_def):
+    if isinstance(module_def, dict):
+        return _Module.from_dict(module_def)
+    elif isinstance(module_def, str):
+        return _Module.from_file(module_def)
+
+
+def fulfill(value, datatype):
+    return datatype.fulfill(value)
+
+
+def guard(value, datatype):
+    return datatype.guard(value)
+
+
+__all__ = ['def_datatypes', 'def_datatype', 'load_module', 'fulfill', 'guard',
+           'select_items', 'with_items', 'without_items',
+           '_Context', '_Module',
+           'validate_before_call',
+           'ValidationError',
+           ]
