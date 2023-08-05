a6965e4ef9ee77a139e00a637824dcebd9c85388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:39 +0000

    Add package wb_vandalism/0.1.8/wb_vandalism-0.1.8.tar.gz

---
+++ packages/wb_vandalism/wb_vandalism-0.1.8.tar.gz/wb_vandalism-0.1.8/README.rst
@@ -0,0 +1,28 @@
+.. image:: https://travis-ci.org/wiki-ai/wb-vandalism.svg?branch=master
+    :target: https://travis-ci.org/wiki-ai/wb-vandalism
+
+.. image:: http://codecov.io/github/wiki-ai/wb-vandalism/coverage.svg?branch=master
+    :target: http://codecov.io/github/wiki-ai/wb-vandalism?branch=master
+
+
+wb-wandalism: Wikidata anti-vandalism system
+============================================
+This library provides a set of utilities for building and maintaining
+edit quality prediction models for WikiBase wikis.
+
+* **Installation:** ``pip install wb-vandalism``
+* **Repository:** https://github.com/Ladsgroup/wb-vandalism
+* **Report bugs:** `wb_vandalism @ phabricator <https://phabricator.wikimedia.org/maniphest/task/create/?projects=wb_vandalism>`_
+* **License:** MIT
+
+**Compatible with Python 3.x only.**
+
+Authors
+-------
+    Aaron Halfaker
+        * ahalfaker@wikimedia.org
+        * `<http://halfaker.info>`_
+
+    Amir Sarabadani
+        * Ladsgroup AT gmail.com
+        * `<https://wikimediafoundation.org/wiki/User:Ladsgroup>`_
---
+++ packages/wb_vandalism/wb_vandalism-0.1.8.tar.gz/wb_vandalism-0.1.8/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def requirements(fname):
+    for line in open(os.path.join(os.path.dirname(__file__), fname)):
+        yield line.strip()
+
+version = "0.1.8"
+
+setup(
+    name="wb_vandalism",
+    version=version,
+    author="Amir Sarabadani",
+    author_email="Ladsgroup@gmail.com",
+    description=("A library for performing automatic detection of vandalism in Wikidata."),
+    license="MIT",
+    url="https://github.com/Ladsgroup/wb-vandalism",
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=list(requirements('requirements.txt')),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering"
+    ],
+)
