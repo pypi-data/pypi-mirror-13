0b010afa8d6da9eafa7393c8a407b331fe3433bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:52 +0000

    Add package tibas.fountain/0.1.3/tibas.fountain-0.1.3.tar.gz

---
+++ packages/tibas.fountain/tibas.fountain-0.1.3.tar.gz/tibas.fountain-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+long_description = """
+Sphinx extension and directive for embeding Fountain marked screenplay fragments.
+
+Works for HTML representations. 
+Designed to work with ablog
+"""
+
+setup(
+    name='tibas.fountain',
+    author='gabriel montangé láscaris-comneno',
+    author_email='gabriel@tibas.london',
+    description='Sphinx extension for embeding Fountain screenplay fragments',
+    install_requires = [ 'fountain' ],
+    license='MIT',
+    long_description=long_description,
+    namespace_packages = [ 'tibas' ],
+    packages=find_packages(),
+    platforms=[ 'any' ],
+    url='bitbucket.org/gabriel.montagne/tibas.fountain',
+    version='0.1.3',
+)
