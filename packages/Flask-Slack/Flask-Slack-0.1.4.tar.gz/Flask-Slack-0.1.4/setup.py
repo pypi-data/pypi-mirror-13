80a4440052cdee81db36f6f566b24dd2932f4ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:41 +0000

    Add package Flask-Slack/0.1.4/Flask-Slack-0.1.4.tar.gz

---
+++ packages/Flask-Slack/Flask-Slack-0.1.4.tar.gz/Flask-Slack-0.1.4/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup
+
+
+def fread(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath) as f:
+        return f.read()
+
+
+setup(
+    name='Flask-Slack',
+    version='0.1.4',
+    url='https://github.com/verycb/flask-slack',
+    author='VeryCB',
+    author_email='imcaibin@gmail.com',
+    description='Slack extension for Flask.',
+    long_description=fread('README.rst'),
+    license='BSD',
+    py_modules=['flask_slack'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'six',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pytest-pep8',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
