9a254daf575a0cfd22cf633695770561ba9186d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package AWS-Manager/0.2b0/AWS-Manager-0.2b0.tar.gz

---
+++ packages/AWS-Manager/AWS-Manager-0.2b0.tar.gz/AWS-Manager-0.2b0/aws_manager/settings/settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import os
+
+script_name = "AWS Manager"
+
+config_file_name = os.path.join((os.path.dirname(os.path.dirname(__file__))), '.config')
+
+pip_installation = "'%s/%s/scripts/get-pip.py'" % (os.path.dirname(os.path.realpath(__file__)), "..")
