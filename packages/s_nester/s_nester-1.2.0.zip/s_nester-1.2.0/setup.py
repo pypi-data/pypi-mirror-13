3810b6d0bf982953f20d6c7b3221a81cf37eba60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package s_nester/1.2.0/s_nester-1.2.0.zip

---
+++ packages/s_nester/s_nester-1.2.0.zip/s_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name ='s_nester',
+    version ='1.2.0',
+    py_modules=['s_nester'],
+    author= 'sameh',
+    author_email='sameh.iti@gmail.com',
+    url='',
+    description='A simple printer of nested lists'
+    )
