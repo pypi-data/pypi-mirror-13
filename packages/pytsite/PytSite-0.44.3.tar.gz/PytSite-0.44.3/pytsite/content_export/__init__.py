187a70d3d4f14b814edd5fbd3bbbbb90f28a462b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:50 +0000

    Add package pytsite/0.44.3/PytSite-0.44.3.tar.gz

---
+++ packages/pytsite/PytSite-0.44.3.tar.gz/PytSite-0.44.3/pytsite/content_export/__init__.py
@@ -0,0 +1,35 @@
+"""PytSite Content Export Plugin.
+"""
+# Public API
+from . import _model as model, _error as error
+from ._driver import Abstract as AbstractDriver
+from ._functions import register_driver
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def __init():
+    """Init wrapper.
+    """
+    from pytsite import admin, odm, events, lang, router
+    from . import _model, _eh
+
+    # Resources
+    lang.register_package(__name__)
+
+    # ODM models
+    odm.register_model('content_export', _model.ContentExport)
+
+    # Event handlers
+    events.listen('pytsite.cron.1min', _eh.cron_1min_eh)
+
+    # Sidebar menu
+    m = 'content_export'
+    admin.sidebar.add_menu('misc', m, __name__ + '@export',
+                           href=router.ep_url('pytsite.odm_ui.ep.browse', {'model': m}),
+                           icon='fa fa-bullhorn',
+                           permissions=('pytsite.odm_ui.browse.' + m, 'pytsite.odm_ui.browse_own.' + m))
+
+__init()
