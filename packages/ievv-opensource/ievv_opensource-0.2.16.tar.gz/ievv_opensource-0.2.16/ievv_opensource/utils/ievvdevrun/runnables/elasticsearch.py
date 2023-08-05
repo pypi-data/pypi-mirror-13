1c75276fb8287492ffc0309849b29ac720278eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package ievv-opensource/0.2.16/ievv_opensource-0.2.16.tar.gz

---
+++ packages/ievv-opensource/ievv_opensource-0.2.16.tar.gz/ievv_opensource-0.2.16/ievv_opensource/utils/ievvdevrun/runnables/elasticsearch.py
@@ -0,0 +1,36 @@
+import sys
+
+from ievv_opensource.utils.ievvdevrun.runnables import base
+
+
+class RunnableThread(base.ShellCommandRunnableThread):
+    """
+    Elasticsearch runnable thread.
+
+    Examples:
+
+        You can just add it to your Django development settings with::
+
+            IEVVTASKS_DEVELOPRUN_THREADLIST = {
+                'default': ievvdevrun.config.RunnableThreadList(
+                    ievvdevrun.runnables.elasticsearch.RunnableThread(
+                        configpath='not_for_deploy/elasticsearch.develop.yml')
+                )
+            }
+
+    """
+    default_autorestart_on_crash = True
+
+    def __init__(self, configpath, elasticsearch_executable='elasticsearch', **kwargs):
+        self.configpath = configpath
+        self.elasticsearch_executable = elasticsearch_executable
+        super(RunnableThread, self).__init__(**kwargs)
+
+    def get_logger_name(self):
+        return 'ElasticSearch {}'.format(self.configpath)
+
+    def get_command_config(self):
+        return {
+            'executable': self.elasticsearch_executable,
+            'args': ['--config={}'.format(self.configpath)]
+        }
