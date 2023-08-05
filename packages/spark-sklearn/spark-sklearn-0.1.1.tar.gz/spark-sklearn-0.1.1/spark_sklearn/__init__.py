f6d48b69c9662dab9128cda5c17edd79400f9634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:56 +0000

    Add package spark-sklearn/0.1.1/spark-sklearn-0.1.1.tar.gz

---
+++ packages/spark-sklearn/spark-sklearn-0.1.1.tar.gz/spark-sklearn-0.1.1/spark_sklearn/__init__.py
@@ -0,0 +1,9 @@
+from scipy.sparse import csr_matrix
+
+from converter import Converter
+from grid_search import GridSearchCV
+from udt import CSRVectorUDT
+
+__all__ = ['Converter', 'CSRVectorUDT', 'GridSearchCV']
+
+csr_matrix.__UDT__ = CSRVectorUDT()
