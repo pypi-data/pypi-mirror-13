da9e5207671d5fb4b80bf09b13a5e411724ba9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package propyte/2015.12.20.2242/propyte-2015.12.20.2242.tar.gz

---
+++ packages/propyte/propyte-2015.12.20.2242.tar.gz/propyte-2015.12.20.2242/propyte.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+"""
+################################################################################
+#                                                                              #
+# propyte                                                                      #
+#                                                                              #
+################################################################################
+#                                                                              #
+# LICENCE INFORMATION                                                          #
+#                                                                              #
+# This program provides template utilities for programs.                       #
+#                                                                              #
+# copyright (C) 2015 Will Breaden Madden, w.bm@cern.ch                         #
+#                                                                              #
+# This software is released under the terms of the GNU General Public License  #
+# version 3 (GPLv3).                                                           #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify it      #
+# under the terms of the GNU General Public License as published by the Free   #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# This program is distributed in the hope that it will be useful, but WITHOUT  #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for     #
+# more details.                                                                #
+#                                                                              #
+# For a copy of the GNU General Public License, see                            #
+# <http://www.gnu.org/licenses/>.                                              #
+#                                                                              #
+################################################################################
+"""
+
+name    = "propyte"
+version = "2015-12-20T2242Z"
+
+import os
+import sys
+import logging
+import technicolor
+import shijian
+import pyprel
+import urllib
+import imp
+import time
+import docopt
+import contextlib
+
+class Program(object):
+
+    def __init__(
+        self,
+        parent       = None,
+        options      = None,
+        name         = None,
+        version      = None,
+        logo         = None,
+        engageLog    = True
+        ):
+
+        global clock
+        clock = shijian.Clock(name = "program run time")
+
+        self.options         = options
+        self.userName        = self.options["--username"]
+        self.verbose         = self.options["--verbose"]
+        self.silent          = self.options["--silent"]
+
+        self.name            = name
+        self.version         = version
+        self.logo            = logo
+
+        if self.userName is None:
+            self.userName    = os.getenv("USER")
+        if self.logo is not None:
+            self.displayLogo     = True
+        elif self.logo is None and self.name is not None:
+            self.logo        = pyprel.renderBanner(
+                                  text = self.name.upper()
+                               )
+            self.displayLogo = True
+        else:
+            self.displayLogo = False
+
+        # logging
+        if engageLog:
+            global log
+            log = logging.getLogger(__name__)
+            logging.root.addHandler(technicolor.ColorisingStreamHandler())
+
+            # logging level
+            if self.verbose:
+                logging.root.setLevel(logging.DEBUG)
+            else:
+                logging.root.setLevel(logging.INFO)
+
+        self.engage()
+
+    def engage(
+        self
+        ):
+        if not self.silent:
+            pyprel.printLine()
+        # logo
+        if self.displayLogo:
+            if not self.silent:
+                log.info(pyprel.centerString(text = self.logo))
+                pyprel.printLine()
+        # engage alert
+        if self.name:
+            if not self.silent:
+                log.info("initiate {name}".format(
+                    name = self.name
+                ))
+        # version
+        if self.version:
+            if not self.silent:
+                log.info("version: {version}".format(
+                    version = self.version
+                ))
+        if not self.silent:
+            log.info("initiation time: {time}".format(
+                time = clock.startTime()
+            ))
+
+    def terminate(
+        self
+        ):
+        clock.stop()
+        if not self.silent:
+            log.info("termination time: {time}".format(
+                time = clock.stopTime()
+            ))
+            log.info("time statistics report:\n{report}".format(
+                report = shijian.clocks.report()
+            ))
+            log.info("terminate {name}".format(
+                name = self.name
+            ))
+            pyprel.printLine()
+        sys.exit()
+
+def smuggle(
+    moduleName = None,
+    URL        = None
+    ):
+    if moduleName is None:
+        moduleName = URL
+    try:
+        module = __import__(moduleName)
+        return(module)
+    except:
+        try:
+            moduleString = urllib.urlopen(URL).read()
+            module = imp.new_module("module")
+            exec moduleString in module.__dict__
+            return(module)
+        except: 
+            raise(
+                Exception(
+                    "module {moduleName} import error".format(
+                        moduleName = moduleName
+                    )
+                )
+            )
+            sys.exit()
+
+@contextlib.contextmanager
+def import_ganzfeld():
+    """
+    Create a context for importing a module such that the module is isolated
+    from command line options and arguments.
+    """
+    tmp = sys.argv
+    sys.argv = []
+    yield
+    sys.argv = tmp
+
+if __name__ == "__main__":
+    options = docopt.docopt(__doc__)
+    if options["--version"]:
+        print(version)
+        exit()
+    main(options)
