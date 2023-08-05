b5f779d2dffdab7470830457223348885121423e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:00 +0000

    Add package PyNMA/1.0/PyNMA-1.0.tar.gz

---
+++ packages/PyNMA/PyNMA-1.0.tar.gz/PyNMA-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="PyNMA",
+      version="1.0",
+      description="Python Module for NotifyMyAndroid API",
+      url="https://github.com/uskr/pynma",
+      packages=find_packages()
+      )
