4c40897eff35ad3030e39fabc7799651785a6159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package holviapi/0.1.20160123/holviapi-0.1.20160123.tar.gz

---
+++ packages/holviapi/holviapi-0.1.20160123.tar.gz/holviapi-0.1.20160123/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import subprocess
+
+setup(
+    name='holviapi',
+    version='0.1.20160123',
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=['holviapi', 'holviapi.errors', ],
+    license='MIT',
+    long_description=open('README.md').read(),
+    description='Implement Pythonic wrappers for Holvi JSON-REST API',
+    install_requires=list(filter(bool, (x.strip() for x in open('requirements.txt').readlines()))),
+    url='https://github.com/rambo/python-holviapi',
+)
