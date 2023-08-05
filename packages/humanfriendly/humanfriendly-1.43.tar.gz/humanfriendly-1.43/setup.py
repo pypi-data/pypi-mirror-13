f370879ca4c3dc5ed38049f39f97559bcd6b0fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:03 +0000

    Add package humanfriendly/1.43/humanfriendly-1.43.tar.gz

---
+++ packages/humanfriendly/humanfriendly-1.43.tar.gz/humanfriendly-1.43/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+"""Setup script for the `humanfriendly` package."""
+
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: January 17, 2016
+# URL: https://humanfriendly.readthedocs.org
+
+# Standard library modules.
+import codecs
+import os
+import sys
+
+# De-facto standard solution for Python packaging.
+from setuptools import find_packages, setup
+
+# Find the directory where the source distribution was unpacked.
+source_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Add the directory with the source distribution to the search path.
+sys.path.append(source_directory)
+
+# Import the module to find the version number (this is safe because we don't
+# have any external dependencies).
+from humanfriendly import __version__ as version_string  # noqa
+
+# Fill in the long description (for the benefit of PyPI)
+# with the contents of README.rst (rendered by GitHub).
+readme_file = os.path.join(source_directory, 'README.rst')
+with codecs.open(readme_file, 'r', 'utf-8') as handle:
+    readme_text = handle.read()
+
+# Conditional importlib dependency for Python 2.6.
+install_requires = []
+if sys.version_info[:2] <= (2, 6) or sys.version_info[:2] == (3, 0):
+    install_requires.append('importlib')
+
+setup(
+    name='humanfriendly',
+    version=version_string,
+    description="Human friendly output for text interfaces using Python",
+    long_description=readme_text,
+    url='https://humanfriendly.readthedocs.org',
+    author='Peter Odding',
+    author_email='peter@peterodding.com',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points=dict(console_scripts=[
+        'humanfriendly = humanfriendly.cli:main'
+    ]),
+    test_suite='humanfriendly.tests',
+    tests_require=[
+        'capturer >= 2.1',
+        'coloredlogs >= 2.0',
+    ],
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Communications',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: System :: Shells',
+        'Topic :: System :: System Shells',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Terminals',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Utilities',
+    ])
