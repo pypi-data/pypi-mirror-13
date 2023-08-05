0ac0dfa2c4367a65dd199738f1721abf8a2482ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:26 +0000

    Add package raspberry_pi.python/0.1.2/raspberry_pi.python-0.1.2.tar.gz

---
+++ packages/raspberry_pi.python/raspberry_pi.python-0.1.2.tar.gz/raspberry_pi.python-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'raspberry_pi.python',
+  packages = ['raspberry_pi.python'],
+  version = '0.1.2',
+  description = 'Countly IOT SDK for Raspberry Pi Python',
+  author = 'Countly',
+  author_email = 'hello@count.ly',
+  url = 'https://github.com/countly/countly-sdk-iot',
+  download_url = 'https://github.com/countly/countly-sdk-iot/tarball/0.1',
+  keywords = ['countly', 'iot', 'pyhon'],
+  classifiers = [],
+)
