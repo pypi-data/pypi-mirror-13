ae531e4047a5170e736e71c4ce8fb9250b912421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package malibu/0.1.6.post2/malibu-0.1.6.post2.tar.gz

---
+++ packages/malibu/malibu-0.1.6.post2.tar.gz/malibu-0.1.6.post2/malibu/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from malibu import command
+from malibu import config
+from malibu import database
+from malibu import design
+from malibu import text
+from malibu import util
+
+
+__version__ = '0.1.6-2'
+__doc__ = """
+malibu is a collection of classes and utilities that make writing code
+a little bit easier and a little less tedious.
+
+The whole point of this library is to have a small codebase that could
+be easily reused across projects with nice, easily loadable chunks that
+can be used disjointly.
+"""
