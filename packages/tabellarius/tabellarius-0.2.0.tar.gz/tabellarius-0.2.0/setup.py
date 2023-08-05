f280a3a0341846e5b4d9782eb6255fd780d7e06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package tabellarius/0.2.0/tabellarius-0.2.0.tar.gz

---
+++ packages/tabellarius/tabellarius-0.2.0.tar.gz/tabellarius-0.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(name='tabellarius',
+      version='0.2.0',
+      description='A mail-sorting tool that is less annoying',
+      author='Arnold Bechtoldt',
+      author_email='mail@arnoldbechtoldt.com',
+      url='https://github.com/bechtoldt/tabellarius',
+      packages=['tabellarius'],
+      install_requires=[
+          'PyYAML==3.11',
+          'gnupg==2.0.2',
+          'IMAPClient==1.0.0',
+          'backports.ssl==0.0.9',
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',])
