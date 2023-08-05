db275d19ad9a9228b80615e47bb51dbfa2f398f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:45 +0000

    Add package tradingWithPython/0.0.14.2/tradingWithPython-0.0.14.2.tar.gz

---
+++ packages/tradingWithPython/tradingWithPython-0.0.14.2.tar.gz/tradingWithPython-0.0.14.2/tradingWithPython/__init__.py
@@ -0,0 +1,1 @@
+__docformat__ = 'restructuredtext'__version__ = '0.0.14.2'from lib import yahooFinance as yahoofrom lib.classes import *from lib.functions import *from lib.csvDatabase import HistDataCsvfrom lib.backtest import Backtest
