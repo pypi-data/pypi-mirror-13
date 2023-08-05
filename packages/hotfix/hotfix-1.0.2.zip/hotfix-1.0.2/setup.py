44c97d3d7a5ba9537a5a9274b8d8a3be1640dc2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:15 +0000

    Add package hotfix/1.0.2/hotfix-1.0.2.zip

---
+++ packages/hotfix/hotfix-1.0.2.zip/hotfix-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+__author__ = 'snake'
+
+
+setup(
+    name='hotfix',
+    packages=['hotfix'],  # this must be the same as the name above
+    version='1.0.2',
+    author='Simon Lessnick',
+)
