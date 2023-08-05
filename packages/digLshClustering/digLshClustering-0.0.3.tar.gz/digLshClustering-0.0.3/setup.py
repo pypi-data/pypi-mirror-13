810e0ea613f2b80775992c0c2a6646f9624a3314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package digLshClustering/0.0.3/digLshClustering-0.0.3.tar.gz

---
+++ packages/digLshClustering/digLshClustering-0.0.3.tar.gz/digLshClustering-0.0.3/setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+config = {
+    'name': 'digLshClustering',
+    'description': 'code to cluster based on lsh algorithm',
+    'author': 'Dipsy Kapoor',
+    'url': 'https://github.com/usc-isi-i2/dig-lsh-clustering.git',
+    'download_url': 'https://github.com/usc-isi-i2/dig-lsh-clustering.git',
+    'author_email': 'dipsykapoor@gmail.com',
+    'install_requires': ['nose2',
+                         'digSparkUtil',
+                         'jq'],
+    'version':'0.0.3',
+    'packages': ['digLshClustering.hasher','digLshClustering.gen_int_input',
+                 'digLshClustering.clusterer','digLshClustering.utils'],
+    'scripts': []
+}
+
+setup(**config)
