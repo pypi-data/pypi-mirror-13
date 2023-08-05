fd70cc1c06f0004a6535f3bdaa8dfd668256c75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package pyramid-notebook/0.1.1/pyramid_notebook-0.1.1.tar.gz

---
+++ packages/pyramid-notebook/pyramid_notebook-0.1.1.tar.gz/pyramid_notebook-0.1.1/CHANGES.rst
@@ -0,0 +1,12 @@
+Changelog for pyramid_notebook
+==============================
+
+0.1.1 (2015-12-19)
+------------------
+
+- Fixed broken setup.py classifiers
+
+0.1 (2015-12-19)
+----------------
+
+- Initial release.
---
+++ packages/pyramid-notebook/pyramid_notebook-0.1.1.tar.gz/pyramid_notebook-0.1.1/droneio.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Need to upgrade to Python 3.4
+sudo add-apt-repository ppa:fkrull/deadsnakes > /dev/null 2>&1
+sudo apt-get -qq update > /dev/null 2>&1
+sudo apt-get -qq install python3.4-dev > /dev/null 2>&1
+
+# Creteat test virtualenv
+python3.4 -m venv venv
+. venv/bin/activate
+
+# Make sure pip itself is up to date
+pip install -U --quiet pip
+pip install --quiet -r test-requirements.txt
+
+# Make venv aware of our own package
+python setup.py develop
+
+sudo start xvfb
+
+# Run tests using py.test test runner
+echo "Running tests"
+py.test tests/* --cov pyramid_notebook --cov-report xml --splinter-webdriver=chrome --splinter-screenshot-dir=screenshots --ini=pyramid_notebook/demo/development.ini
+
+# Update data to codecov.io
+codecov --token="$CODECOV_TOKEN"
+
+echo "Done with tests"
