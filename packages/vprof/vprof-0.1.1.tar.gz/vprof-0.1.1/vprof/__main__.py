fb5aedb04c64b82c509fc204a9f989f14ee567fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package vprof/0.1.1/vprof-0.1.1.tar.gz

---
+++ packages/vprof/vprof-0.1.1.tar.gz/vprof-0.1.1/vprof/__main__.py
@@ -0,0 +1,48 @@
+"""Main module for visual profiler."""
+import argparse
+import os
+import sys
+
+from vprof import profile_wrappers
+from vprof import stats_server
+
+
+_MODULE_DESC = 'Python visual profiler.'
+_HOST = 'localhost'
+_PORT = 8000
+
+_PROFILE_MAP = {
+    'c': profile_wrappers.RuntimeProfile,
+    'm': profile_wrappers.MemoryProfile,
+}
+
+def main():
+    """Visual profiler main function."""
+    parser = argparse.ArgumentParser(description=_MODULE_DESC)
+    parser.add_argument('profilers', metavar='opts',
+                        help='Profilers configuration')
+    parser.add_argument('source', metavar='src', nargs=1,
+                        help='Python program to profile.')
+    args = parser.parse_args()
+
+    if len(args.profilers) > len(set(args.profilers)):
+        print('Profiler configuration is ambiguous. Remove duplicates.')
+        sys.exit(1)
+
+    for option in args.profilers:
+        if option not in _PROFILE_MAP:
+            print('Unrecognized option: %s' % option)
+            sys.exit(2)
+
+    sys.argv[:] = args.source
+    program_name, program_stats = args.source[0], {}
+    for option in args.profilers:
+        curr_profiler = _PROFILE_MAP[option](program_name)
+        print('Running %s...' % curr_profiler.__class__.__name__)
+        program_stats[option] = curr_profiler.run()
+    sys.stderr = open(os.devnull, "w")
+    print('Starting HTTP server...')
+    stats_server.start(_HOST, _PORT, program_stats)
+
+if __name__ == "__main__":
+    main()
