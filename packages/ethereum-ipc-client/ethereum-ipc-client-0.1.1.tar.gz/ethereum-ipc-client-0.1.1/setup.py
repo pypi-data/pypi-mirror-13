eaf218acf15b948e0145db53c8d1bcf8abb6ccb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:06 +0000

    Add package ethereum-ipc-client/0.1.1/ethereum-ipc-client-0.1.1.tar.gz

---
+++ packages/ethereum-ipc-client/ethereum-ipc-client-0.1.1.tar.gz/ethereum-ipc-client-0.1.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+version = '0.1.1'
+
+readme = open(os.path.join(DIR, 'README.md')).read()
+
+
+setup(
+    name='ethereum-ipc-client',
+    version=version,
+    description="""Ethereum IPC Client""",
+    long_description=readme,
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/pipermerriam/ethereum-ipc-client',
+    include_package_data=True,
+    py_modules=['eth_ipc_client'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
