00f7f4a7fa8a764e50ae7eb8a4dcebc4a5495324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:31 +0000

    Add package Flask-Personal-Access-Token/0.1.1/Flask-Personal-Access-Token-0.1.1.tar.gz

---
+++ packages/Flask-Personal-Access-Token/Flask-Personal-Access-Token-0.1.1.tar.gz/Flask-Personal-Access-Token-0.1.1/setup.py
@@ -0,0 +1,36 @@
+"""
+Flask Personal Access Token Managerment Extension.
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-Personal-Access-Token',
+    version='0.1.1',
+    url='https://github.com/soasme/flask-personal-access-token',
+    license='MIT',
+    author='Ju Lin',
+    author_email='soasme@gmail.com',
+    description='Flask Personal Access Token Management Extension',
+    long_description=__doc__,
+    packages=['flask_personal_access_token'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+    ],
+    classifiers=[
+        'Framework :: Flask',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
