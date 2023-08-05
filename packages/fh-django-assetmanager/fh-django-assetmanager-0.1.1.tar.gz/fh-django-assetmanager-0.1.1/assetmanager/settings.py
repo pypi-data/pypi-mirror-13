110e11c40cacc404606e0952eda5d2ed546479de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:37 +0000

    Add package fh-django-assetmanager/0.1.1/fh-django-assetmanager-0.1.1.tar.gz

---
+++ packages/fh-django-assetmanager/fh-django-assetmanager-0.1.1.tar.gz/fh-django-assetmanager-0.1.1/assetmanager/settings.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+STORAGE = getattr(settings, 'ASSETMANAGER_STORAGE',
+                  'django.core.files.storage.FileSystemStorage')
+PAGE_SIZE = getattr(settings, 'ASSETMANAGER_PAGE_SIZE', 10)
+IMAGE_CONTENT_TYPES = getattr(settings, 'ASSETMANAGER_IMAGE_CONTENT_TYPES',
+                              ('image/jpeg', 'image/png', 'image/gif'
+                               'image/svg+xml'))
+MEDIA_ROOT = getattr(settings, 'ASSETMANAGER_MEDIA_ROOT', '/assetmanager/')
+MEDIA_URL = getattr(settings, 'ASSETMANAGER_MEDIA_URL', '')
