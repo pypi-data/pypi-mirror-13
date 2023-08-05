3e9e5afd66bceb5b5e51bd145946517138138770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package bquick/0.1.5/bquick-0.1.5.tar.gz

---
+++ packages/bquick/bquick-0.1.5.tar.gz/bquick-0.1.5/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='bquick',
+      version='0.1.5',
+      description='The command line tool for BigQuery management.',
+      url='http://github.com/visualskyrim/bquick',
+      author='visualskyrim',
+      author_email='visual.skyrim@gmail.com',
+      license='MIT',
+      packages=['bquick', 'bquick.bigquery'],
+      install_requires=[
+        'google-api-python-client', 'oauth2client'
+      ],
+      scripts=['bin/bquick'],
+      zip_safe=False)
