b46302774ea97e62296fdadafb987f8800b5586e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:35 +0000

    Add package yolodb/0.3.0/yolodb-0.3.0.tar.gz

---
+++ packages/yolodb/yolodb-0.3.0.tar.gz/yolodb-0.3.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+with open('VERSION.txt', 'r') as v:
+    version = v.read().strip()
+
+# Requirements
+# install_reqs = parse_requirements('requirements.txt', session='dummy')
+# reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='yolodb',
+    url='https://github.com/gdraynz/yolodb',
+    description='A simple key/value storage DB (well... json)',
+    author='gdraynz',
+    author_email='gd.raynz@gmail.com',
+    version=version,
+    # install_requires=reqs,
+    packages=find_packages(exclude=['tests']),
+    license='WTFPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
