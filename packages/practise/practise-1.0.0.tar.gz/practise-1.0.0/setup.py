b66a87edc9961f4abe6b1b53872de417e8963ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package practise/1.0.0/practise-1.0.0.tar.gz

---
+++ packages/practise/practise-1.0.0.tar.gz/practise-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'practise',
+        version     = '1.0.0',
+        py_modules  = ['practise'],
+        author      = 'zephyr',
+        author_email= 'zephyr.nju@gmail.com',
+        url         = 'http://zzli.me',
+        description = 'A simple printer of nested lists',
+     )
