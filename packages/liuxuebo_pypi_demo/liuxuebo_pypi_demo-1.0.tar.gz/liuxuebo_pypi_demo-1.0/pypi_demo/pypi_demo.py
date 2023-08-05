5e1fa52e5b3040606033d5424fcaa865526632e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:56 +0000

    Add package liuxuebo_pypi_demo/1.0/liuxuebo_pypi_demo-1.0.tar.gz

---
+++ packages/liuxuebo_pypi_demo/liuxuebo_pypi_demo-1.0.tar.gz/liuxuebo_pypi_demo-1.0/pypi_demo/pypi_demo.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = 'liuxuebo'
+
+
+def pypi_demo():
+    print('this is a demo')
---
+++ packages/liuxuebo_pypi_demo/liuxuebo_pypi_demo-1.0.tar.gz/liuxuebo_pypi_demo-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='liuxuebo_pypi_demo',
+        version='1.0',
+        keywords=('demo'),
+        description='a library for learning pypi',
+        license='MIT License',
+        author='liuxuebo',
+        author_email='lufo816@gmail.com',
+        packages=find_packages(),
+        include_package_data=True,
+        platforms='any',
+        install_requires=[],
+)
