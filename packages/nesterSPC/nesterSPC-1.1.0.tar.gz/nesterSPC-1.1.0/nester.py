a6de18e5ebbf11df9ba06f3685f7e37604079e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package nesterSPC/1.1.0/nesterSPC-1.1.0.tar.gz

---
+++ packages/nesterSPC/nesterSPC-1.1.0.tar.gz/nesterSPC-1.1.0/nester.py
@@ -0,0 +1,14 @@
+"""This module can be used to print inner lists in a list"""
+def print_lol(the_list, level):
+    """Iterate through each item in the list
+       if the item is a list print the contents
+       of that list. Else just print the non-listed
+       items. A value can be assigned to indent
+       each inner list."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end="")
+            print(each_item)
