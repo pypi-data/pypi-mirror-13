cdd3a2a324673a2161b3a17d80e70b236f13ebca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package drf-tracking/0.2.4/drf_tracking-0.2.4-py2.py3-none-any.whl

---
+++ packages/drf-tracking/drf_tracking-0.2.4-py2.py3-none-any.whl/rest_framework_tracking/mixins.py
@@ -0,0 +1,52 @@
+from .models import APIRequestLog
+from django.utils.timezone import now
+
+
+class LoggingMixin(object):
+    """Mixin to log requests"""
+    def initial(self, request, *args, **kwargs):
+        """Set current time on request"""
+        # get data dict
+        try:
+            data_dict = request.data.dict()
+        except AttributeError:  # if already a dict, can't dictify
+            data_dict = request.data
+
+        # save to log without user
+        # self.request is already present
+        self.request.log = APIRequestLog.objects.create(
+            requested_at=now(),
+            path=request.path,
+            remote_addr=request.META['REMOTE_ADDR'],
+            host=request.get_host(),
+            method=request.method,
+            query_params=request.query_params.dict(),
+            data=data_dict,
+        )
+
+        # regular intitial, including auth check
+        super(LoggingMixin, self).initial(request, *args, **kwargs)
+
+        # add user to log after auth
+        user = request.user
+        if user.is_anonymous():
+            user = None
+        self.request.log.user = user
+        self.request.log.save()
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        # regular finalize response
+        response = super(LoggingMixin, self).finalize_response(request, response, *args, **kwargs)
+
+        # compute response time
+        response_timedelta = now() - self.request.log.requested_at
+        response_ms = int(response_timedelta.total_seconds() * 1000)
+
+        # save to log
+        self.request.log.response = response.rendered_content
+        self.request.log.status_code = response.status_code
+        self.request.log.response_ms = response_ms
+        self.request.log.save()
+
+        # return
+        return response
