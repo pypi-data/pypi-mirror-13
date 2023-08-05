67723e7ef87fc8ff7222096c9b6feda06bb578c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package WASH/1.5/WASH-1.5.tar.gz

---
+++ packages/WASH/WASH-1.5.tar.gz/WASH-1.5/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='WASH',
+		version='1.5', 
+		description='Wolfram|Alpha interactive shell', 
+		author='Cody Brocious', 
+		author_email='cody.brocious@gmail.com', 
+		scripts=['wash'], 
+		install_requires=[
+			'requests',
+			'beautifulsoup4', 
+			'mako'
+		], 
+		license='CC0', 
+		url='https://pypi.python.org/pypi/WASH'
+	)
