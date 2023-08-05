1f1a0be299be971c96c10622864c8a6804d925cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package jupyter-tree-filter/1.4.1/jupyter-tree-filter-1.4.1.tar.gz

---
+++ packages/jupyter-tree-filter/jupyter-tree-filter-1.4.1.tar.gz/jupyter-tree-filter-1.4.1/jupyter-tree-filter/__init__.py
@@ -0,0 +1,7 @@
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'tree',
+        'src': 'amd',
+        'dest': 'jupyter-tree-filter',
+        'require': 'jupyter-tree-filter/index'
+    }]
---
+++ packages/jupyter-tree-filter/jupyter-tree-filter-1.4.1.tar.gz/jupyter-tree-filter-1.4.1/jupyter-tree-filter/amd/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+define(['exports'], function (exports) {
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    exports.load_ipython_extension = load_ipython_extension;
+
+    function filterRows(filterText) {
+        var rows = Array.prototype.concat.apply([], document.querySelectorAll('.list_item.row'));
+        rows.forEach(function (row) {
+            if (!filterText || row.querySelector('.item_name').innerText.indexOf(filterText) !== -1) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+    }
+
+    function load_ipython_extension() {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        document.querySelector('#notebook_list_header').appendChild(input);
+
+        input.onkeyup = function () {
+            filterRows(input.value);
+        };
+    }
+});
