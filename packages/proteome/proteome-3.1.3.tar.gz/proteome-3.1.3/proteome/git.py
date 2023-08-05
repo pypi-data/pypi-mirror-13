ad698e45ad31f5a62c950eb28f83a09683f8151a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package proteome/3.1.3/proteome-3.1.3.tar.gz

---
+++ packages/proteome/proteome-3.1.3.tar.gz/proteome-3.1.3/proteome/git.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from proteome.project import Project
+from proteome.nvim import NvimFacade
+
+from trypnv import ProcessExecutor, Job  # type: ignore
+
+
+class Git(ProcessExecutor):
+
+    def pre_args(self, project: Project):
+        return []
+
+    def command(self, project: Project, name: str, *cmd_args):
+        args = self.pre_args(project) + [name] + list(cmd_args)
+        self.log.debug('running git {}'.format(' '.join(args)))
+        return self.run(Job(project, 'git', args, self.loop))
+
+    # TODO remove dangling lock file
+    # and set the excludesfile
+    def init(self, project: Project):
+        return self.command(project, 'init')
+
+    def add_commit_all(self, project: Project, message: str):
+        return self.command(project, 'add', '-A', str(project.root))\
+            .flat_map(lambda a: self.command(project, 'commit', '-m', message))
+
+
+class HistoryGit(Git):
+
+    def __init__(self, base: Path, vim: NvimFacade, loop=None) -> None:
+        self.base = base
+        super(HistoryGit, self).__init__(vim, loop)
+
+    def pre_args(self, project: Project):
+        d = str(project.root)
+        h = str(self._history_dir(project))
+        return [
+            '--git-dir',
+            h,
+            '--work-tree',
+            d,
+        ]
+
+    def _history_dir(self, project: Project):
+        return self.base / project.fqn
+
+    @property
+    def ready(self):
+        return self.base.is_dir() and super(HistoryGit, self).ready
+
+__all__ = ['Git']
---
+++ packages/proteome/proteome-3.1.3.tar.gz/proteome-3.1.3/proteome/logging.py
@@ -0,0 +1,22 @@
+import logging  # type: ignore
+
+import tryp.logging
+from trypnv.logging import trypnv_logger
+
+from tryp.lazy import lazy  # type: ignore
+
+
+log = proteome_root_logger = trypnv_logger('proteome')
+
+
+def proteome_logger(name: str):
+    return proteome_root_logger.getChild(name)
+
+
+class Logging(tryp.logging.Logging):
+
+    @lazy
+    def _log(self) -> tryp.logging.Logger:
+        return proteome_logger(self.__class__.__name__)
+
+__all__ = ['proteome_logger', 'Logging']
