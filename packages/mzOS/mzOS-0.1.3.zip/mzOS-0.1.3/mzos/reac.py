3ebde4504c1a201176c0d733bb3c8c0b1275bc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:42 +0000

    Add package mzOS/0.1.3/mzOS-0.1.3.zip

---
+++ packages/mzOS/mzOS-0.1.3.zip/mzOS-0.1.3/mzos/reac.py
@@ -0,0 +1,10 @@
+class RPrecord(object):
+    def __init__(self):
+        """
+        @type self:
+        """
+        self.as_r = set()
+        self.as_p = set()
+
+    def get(self):
+        return self.as_r, self.as_p
---
+++ packages/mzOS/mzOS-0.1.3.zip/mzOS-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+entry_points = {
+    "console_scripts": [
+        'mzos = mzos.scripts.mzos_script:main'
+    ]
+}
+
+setup(
+    name='mzOS',
+    version='0.1.3',
+    packages=find_packages(),
+    package_data={'mzos.ressources': ['*'],
+                  'mzos.third_party.emass': ['*']
+                  },
+    url='http://github.com/jerkos/mzOS',
+    license='MIT',
+    author='Marco Dubois',
+    author_email='cram@hotmail.fr',
+    description='Heuristic based feature annotations/identifications of LC-MS metabolomics dataset.',
+    entry_points=entry_points,
+    long_description=open('README').read(),
+    requires=['bioservices', 'scipy', 'numpy', 'six', 'sklearn', 'pandas'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Topic :: Scientific/Engineering :: Bio-Informatics'
+                 ]
+)
