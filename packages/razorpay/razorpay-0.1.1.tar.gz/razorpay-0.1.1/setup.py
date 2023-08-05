97b9abd442fa2ea330b4e720c52ccce3ee678ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:35 +0000

    Add package razorpay/0.1.1/razorpay-0.1.1.tar.gz

---
+++ packages/razorpay/razorpay-0.1.1.tar.gz/razorpay-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="razorpay",
+    version="0.1.1",
+    description="Razorpay Python Client",
+    url="https://github.com/decached/razorpay",
+    author="Akash Kothawale",
+    author_email="akash@decached.com",
+    license="MIT",
+    install_requires=["requests"],
+    packages=["razorpay"],
+    keywords='razorpay payment gateway india',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
