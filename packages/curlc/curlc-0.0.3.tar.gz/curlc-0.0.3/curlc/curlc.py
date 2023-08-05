943a1449c86ecfdc9fb7de705d91c01c4a6b2b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package curlc/0.0.3/curlc-0.0.3.tar.gz

---
+++ packages/curlc/curlc-0.0.3.tar.gz/curlc-0.0.3/curlc/curlc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from pycookiecheat import chrome_cookies
+import subprocess
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: curlc [url] <curl args...>")
+        sys.exit(1)
+
+    curlargs = sys.argv[1:]
+    for arg in curlargs:
+        if arg[0] != '-':
+            url = arg
+            break
+
+    chrome = chrome_cookies(url)
+
+    cargs = []
+    carg = "cookie: "
+    for k, v in chrome.items():
+        cargs.append(k + "=" + v)
+    carg += "; ".join(cargs)
+
+    args = ["curl", "-H", carg]
+    args.extend(curlargs)
+    subprocess.run(args)
+
+
+if __name__ == '__main__':
+    main()
