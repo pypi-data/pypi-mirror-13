acb20c07bcb7434dd67231ee18e5de7b1b6ce181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:01 +0000

    Add package TwistedExiftool/0.1.2/TwistedExiftool-0.1.2.tar.gz

---
+++ packages/TwistedExiftool/TwistedExiftool-0.1.2.tar.gz/TwistedExiftool-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='TwistedExiftool',
+    version='0.1.2',
+    description='Exiftool protocol and stream endpoint plugin to be used with twisted',
+    author='Lorenz Schori',
+    author_email='lo@znerol.ch',
+    url='https://github.com/znerol/twisted-exiftool',
+    packages=['twisted_exiftool', 'twisted.plugins'],
+    package_data={
+        'twisted.plugins': [
+            'twisted/plugins/twisted_exiftool.py',
+        ]
+    },
+    install_requires=[
+        'Twisted>=13.1',
+        'zope.interface'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Graphics :: Capture'
+    ],
+)
