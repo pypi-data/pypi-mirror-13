bb3a05b7dba93b83eb74f7f23f20af380f156d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:05 +0000

    Add package zipdep.py/1.0.1/zipdep.py-1.0.1.tar.gz

---
+++ packages/zipdep.py/zipdep.py-1.0.1.tar.gz/zipdep.py-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='zipdep.py',
+    version='1.0.1',
+    packages=[''],
+    url='https://git.veriny.tf/SunDwarf/zipdep.py',
+    license='MIT',
+    author='Isaac Dickinson',
+    author_email='eyes@veriny.tf',
+    description='Packaging dependencies into your app',
+    scripts=['zipdep.py']
+)
