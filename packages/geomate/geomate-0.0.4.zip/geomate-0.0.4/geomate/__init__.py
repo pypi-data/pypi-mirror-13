0159eafc6c2fca1b15be286c2e5ca08b3edcceef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package geomate/0.0.4/geomate-0.0.4.zip

---
+++ packages/geomate/geomate-0.0.4.zip/geomate-0.0.4/geomate/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .batch import BatchGeocoder
+from .geocoderAPI.google import GoogleGeocoder 
+
+__version__ = "0.0.4"
+__short_description__ = ("Batch google geocoding tool. Automatically handle "
+                         "API key, save result to disk and manage your todo list.")
