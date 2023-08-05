c4662876786d69c1bdcc6fc2914195910a4dd145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package ipbec/0.1.2/ipbec-0.1.2.zip

---
+++ packages/ipbec/ipbec-0.1.2.zip/ipbec-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='ipbec',
+      version='0.1.2',
+      description='A PyQT application analyze absorption images of Bose-Einstein condensates',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: X11 Applications :: Qt',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Multimedia :: Graphics :: Viewers',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Scientific/Engineering :: Visualization'],
+
+      url='http://github.com/shreyaspotnis/ipbec',
+      author='Shreyas Potnis',
+      author_email='shreyaspotnis@gmail.com',
+      license='MIT',
+      packages=['ipbec'],
+      install_requires=['pyqtgraph', 'numpy'],
+      zip_safe=False)
