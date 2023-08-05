a583940611bc42f786e9e3a7732af8011465da95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:45 +0000

    Add package parthsql/0.1.0/parthsql-0.1.0.tar.gz

---
+++ packages/parthsql/parthsql-0.1.0.tar.gz/parthsql-0.1.0/tests/test_main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_main
+----------------------------------
+
+Tests for `main` module.
+"""
+
+import unittest
+
+from parthsql import main
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
