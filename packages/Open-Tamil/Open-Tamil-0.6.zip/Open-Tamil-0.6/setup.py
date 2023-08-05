698ea23bd6fc90f0d1e6d60a90508a01d09bf37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package Open-Tamil/0.6/Open-Tamil-0.6.zip

---
+++ packages/Open-Tamil/Open-Tamil-0.6.zip/Open-Tamil-0.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# (C) 2013-2015 முத்தையா அண்ணாமலை 
+# open-tamil project
+
+from distutils.core import setup
+from codecs import open
+
+setup(name='Open-Tamil',
+      version='0.6',
+      description='Tamil language text processing tools for Python v2, v3',
+      author='M. Annamalai, T. Arulalan, and other contributors',
+      author_email='ezhillang@gmail.com',
+      url='https://github.com/arcturusannamalai/open-tamil',
+      packages=['tamil','transliterate','ngram','tamil.txt2ipa','tamil.txt2unicode','tamil.utils', 'solthiruthi'],
+      package_dir={'solthiruthi': 'solthiruthi'},
+      package_data={'solthiruthi': ['data/*.txt']},
+      license='MIT',
+      platforms='PC,Linux,Mac',
+      classifiers=['Natural Language :: Tamil',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4'],
+      long_description=open('README.md','r','UTF-8').read(),
+      download_url='https://github.com/arcturusannamalai/open-tamil/archive/master.zip',#pip
+      )
---
+++ packages/Open-Tamil/Open-Tamil-0.6.zip/Open-Tamil-0.6/tamil/__init__.py
@@ -0,0 +1,16 @@
+# * coding: utf8 *
+# 
+# (C) 2013-2015 Muthiah Annamalai <ezhillang@gmail.com>
+# Library provides various encoding services for Tamil libraries
+
+from . import utf8
+from . import tscii
+
+from . import txt2unicode
+from . import txt2ipa
+from . import numeral
+from . import regexp
+from . import wordutils
+
+VERSION = '0.51'
+__all__ = ['utf8','txt2unicode','num2tamilstr','txt2unicode','txt2ipa','numeral','regexp']
