0d37e9bee42c898fab105970d1d270c80381d3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package tc-aws/2.0.11/tc_aws-2.0.11.tar.gz

---
+++ packages/tc-aws/tc_aws-2.0.11.tar.gz/tc_aws-2.0.11/tc_aws/loaders/s3_loader.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+# Copyright (c) 2015, thumbor-community
+# Use of this source code is governed by the MIT license that can be
+# found in the LICENSE file.
+
+from thumbor.utils import logger
+from tornado.concurrent import return_future
+
+import thumbor.loaders.http_loader as http_loader
+
+from . import *
+from ..aws.bucket import Bucket
+
+@return_future
+def load(context, url, callback):
+    """
+    Loads image
+    :param Context context: Thumbor's context
+    :param string url: Path to load
+    :param callable callback: Callback method once done
+    """
+    if _use_http_loader(context, url):
+        http_loader.load_sync(context, url, callback, normalize_url_func=_normalize_url)
+        return
+
+    bucket, key = _get_bucket_and_key(context, url)
+
+    if _validate_bucket(context, bucket):
+        bucket_loader = Bucket(bucket, context.config.get('TC_AWS_REGION'))
+
+        def handle_data(file_key):
+            if not file_key or 'Error' in file_key or 'Body' not in file_key:
+                logger.warn("ERROR retrieving image from S3 {0}: {1}".format(key, str(file_key)))
+            else:
+                callback(file_key['Body'].read())
+
+        bucket_loader.get(key, callback=handle_data)
+    else:
+        callback(None)
