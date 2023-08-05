07ace71a2af56ddc166834ddd74c22117187069b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:31 +0000

    Add package onegov.election-day/0.4.0/onegov.election_day-0.4.0.tar.gz

---
+++ packages/onegov.election-day/onegov.election_day-0.4.0.tar.gz/onegov.election_day-0.4.0/onegov/election_day/views/homepage.py
@@ -0,0 +1,34 @@
+from onegov.core.security import Public
+from onegov.core.utils import groupbylist
+from onegov.ballot import VoteCollection
+from onegov.election_day import ElectionDayApp
+from onegov.election_day.layout import DefaultLayout
+from onegov.election_day.models import Principal
+
+
+@ElectionDayApp.html(model=Principal, template='homepage.pt',
+                     permission=Public)
+def view_principal(self, request):
+
+    collection = VoteCollection(request.app.session())
+    votes = collection.get_latest()
+
+    if votes:
+        votes_by_domain_and_date = groupbylist(
+            votes, lambda v: (v.domain, v.date))
+    else:
+        votes_by_domain_and_date = None
+
+    return {
+        'layout': DefaultLayout(self, request),
+        'votes_by_domain_and_date': votes_by_domain_and_date,
+        'show_base_link': True
+    }
+
+
+@ElectionDayApp.html(model=Principal, template='opendata.pt', name='opendata',
+                     permission=Public)
+def view_opendata(self, request):
+    return {
+        'layout': DefaultLayout(self, request),
+    }
