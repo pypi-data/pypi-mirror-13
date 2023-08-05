157517c60f61fa575dcf1008afa31cd9b5a268e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:07 +0000

    Add package okscraper/0.0.6/okscraper-0.0.6.tar.gz

---
+++ packages/okscraper/okscraper-0.0.6.tar.gz/okscraper-0.0.6/okscraper/sources.py
@@ -0,0 +1,46 @@
+import re, urllib2
+
+class BaseSource(object):
+    """
+        Abstract BaseSource
+        
+        extending classes must implement a fetch method which returns the input data
+    """
+
+class BaseStringParamsSource(BaseSource):
+
+    def __init__(self, source_string):
+        self._source_string = source_string
+
+    def get_source_string(self, *args, **kwargs):
+        args = list(args)
+        src = self._source_string
+        identifiers = re.findall(r"<<(\w*)>>", src)
+        while len(args) > 0 and len(identifiers) > 0:
+            identifier = identifiers.pop(0)
+            arg = args.pop(0)
+            src = src.replace('<<{}>>'.format(identifier), str(arg))
+        return src
+
+    def fetch(self, *args, **kwargs):
+        return self._fetch(self.get_source_string(*args, **kwargs))
+
+class UrlSource(BaseStringParamsSource):
+    """fetch data from a url"""
+    def _fetch(selfself, url):
+        return urllib2.urlopen(url).read()
+
+class FileSource(BaseStringParamsSource):
+    """fetch data from a file"""
+    def _fetch(self, filepath):
+        with file(filepath) as f:
+            return f.read()
+
+class ScraperSource(BaseSource):
+    """fetch data from an okscraper"""
+
+    def __init__(self, scraper):
+        self._scraper = scraper
+
+    def fetch(self, *args, **kwargs):
+        return self._scraper.scrape(*args, **kwargs)
---
+++ packages/okscraper/okscraper-0.0.6.tar.gz/okscraper-0.0.6/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='okscraper',
+    version='0.0.6',
+    description='module for scraping websites and documents',
+    author='Ori Hoch',
+    author_email='ori@uumpa.com',
+    license='GPLv3',
+    url='https://github.com/hasadna/okscraper',
+    packages=find_packages(exclude=['tests', 'tests.*'])
+)
