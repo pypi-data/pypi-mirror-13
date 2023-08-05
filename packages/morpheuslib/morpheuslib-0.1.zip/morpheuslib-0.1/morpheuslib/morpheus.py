7ca30daebbe3b64dda62c02027d17bb5145f45fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package morpheuslib/0.1/morpheuslib-0.1.zip

---
+++ packages/morpheuslib/morpheuslib-0.1.zip/morpheuslib-0.1/morpheuslib/morpheus.py
@@ -0,0 +1,20 @@
+import urllib.parse
+import urllib.request
+import json
+
+def convert(fr, to, ur):
+    """ this function returns the converted text """
+
+    url = 'http://subjectrefresh.info/morpheus/api/convert'
+    values = {'from' : fr,
+              'to' : to,
+              'url' : ur }
+
+    data = urllib.parse.urlencode(values)
+    full_url = url + '?' + data
+    
+    content = urllib.request.urlopen(full_url)
+    output = content.read().decode('utf-8')
+    
+    ser = json.loads(output)
+    return ser[to]
