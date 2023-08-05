8006d323325ea19ef18810e1ad4ddddf1b29ed31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:25 +0000

    Add package MesoPy/2.0.0/MesoPy-2.0.0.tar.gz

---
+++ packages/MesoPy/MesoPy-2.0.0.tar.gz/MesoPy-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='MesoPy',
+        py_modules=['MesoPy'],
+        version='2.0.0',
+        description='A pure python wrapper for the MesoWest API',
+        author='Josh Clark',
+        author_email='joshua.m.clark@utah.edu',
+        url='https://github.com/mesowx/MesoPy',
+        download_url='https://github.com/mesowx/MesoPy/tarball/2.0.0',
+        keywords=['weather API climate MesoWest meteorology'],
+        classifiers=[], requires=[]
+)
