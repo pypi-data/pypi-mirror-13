aa325970f810605be9b989dde80a63019a0bec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:50 +0000

    Add package vjy_nester/1.2.0/vjy_nester-1.2.0.tar.gz

---
+++ packages/vjy_nester/vjy_nester-1.2.0.tar.gz/vjy_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'vjy_nester',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'victor',
+    author_email = 'victorjyan@outlook.com',
+    url = '',
+    description = 'A simple printer of nested lists. Just practise for PypI.',
+)
