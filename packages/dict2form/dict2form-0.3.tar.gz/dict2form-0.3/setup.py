08c7b7a08bd0d19e1555b76fef91715354d6794e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:43 +0000

    Add package dict2form/0.3/dict2form-0.3.tar.gz

---
+++ packages/dict2form/dict2form-0.3.tar.gz/dict2form-0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dict2form',
+    version='0.3',
+    packages=[],
+    url='https://mehmetkose.github.io',
+    license='Apache2',
+    author='Mehmet Kose',
+    author_email='mehmetkose.py@gmail.com',
+    description='python dictionary object to html5 json form generator.'
+)
