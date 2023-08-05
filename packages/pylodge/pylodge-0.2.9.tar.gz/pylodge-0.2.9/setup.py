4653cc42dce054a7d4cdda6714e1e8146ca84d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:14 +0000

    Add package pylodge/0.2.9/pylodge-0.2.9.tar.gz

---
+++ packages/pylodge/pylodge-0.2.9.tar.gz/pylodge-0.2.9/setup.py
@@ -0,0 +1,39 @@
+__author__ = 'ashwin'
+"""A Test Lodge based pylodge module.
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='pylodge',
+    version='0.2.9',
+
+    description='Test Automation framework for TestLodge',
+
+    url='https://github.com/gettalent/pylodge',
+
+    # Author details
+    author='Ashwin Kondapalli',
+    author_email='ashwin@gettalent.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Quality Assurance',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+
+    ],
+
+    keywords='TestLodge, test automation, pylodge',
+
+    packages=['pylodge'],
+
+    install_requires=['requests'],
+
+)
