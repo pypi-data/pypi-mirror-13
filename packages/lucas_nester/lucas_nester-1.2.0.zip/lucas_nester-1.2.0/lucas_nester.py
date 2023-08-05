8f5969289bb14f761ab5afc0ca0ac7da6b60c42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:06 +0000

    Add package lucas_nester/1.2.0/lucas_nester-1.2.0.zip

---
+++ packages/lucas_nester/lucas_nester-1.2.0.zip/lucas_nester-1.2.0/lucas_nester.py
@@ -0,0 +1,19 @@
+
+"""Este é o módulo "lucas_nester.py", e fornece uma função chamada print_org()
+que imprime listas que podem ou não incluir listas aninhadas."""
+
+def print_org(the_list,level=0):
+    """Esta função requer um argumento posicional chamado "the_list", que é 
+        qualquer lista Python(de possíveis listas aninhadas). Cada item de dados na
+        lista fornecida é(recursivamente) impresso na tela em sua própria lista.
+        Um segundo argumento chamado "level" é usado para inseir tabulações
+        quando uma lista aninhada é encontrada."""
+
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_org(each_item, level+1)
+        else:
+             for tab_stop in range(level):
+                 print("\t",end=" ")
+             print(each_item)
+
---
+++ packages/lucas_nester/lucas_nester-1.2.0.zip/lucas_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'lucas_nester', 
+        version      = '1.2.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['lucas_nester'],
+        author       = 'Lucas Neves',
+        author_email = 'lucas_done@hotmail.com',
+        url          = 'http://www.canaaessencias.com',
+        description  = 'Simples printer de listas aninhadas',
+     )
