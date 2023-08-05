0754dac49aead9ba8c3851f9f2c3f7a57641d65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:38 +0000

    Add package FukuML/0.0.4/FukuML-0.0.4-py2.py3-none-any.whl

---
+++ packages/FukuML/FukuML-0.0.4-py2.py3-none-any.whl/FukuML/__init__.py
@@ -0,0 +1,12 @@
+#encoding=utf8
+
+__version__ = '0.0.4'
+__all__ = [
+    'MLBase',
+    'PLA',
+    'PLA.BinaryClassifier',
+    'PocketPLA',
+    'PocketPLA.BinaryClassifier',
+    'Utility',
+    'Utility.DatasetLoader'
+]
