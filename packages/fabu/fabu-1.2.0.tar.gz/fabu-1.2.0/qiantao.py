a9357cd7085aae5e2b64531fb2c9a004cc387781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package fabu/1.2.0/fabu-1.2.0.tar.gz

---
+++ packages/fabu/fabu-1.2.0.tar.gz/fabu-1.2.0/qiantao.py
@@ -0,0 +1,11 @@
+
+"""这是一个嵌套函数"""
+def print_qiantao(every,level):
+    """这是一个递归函数，用于显示出一个数组重嵌套的多个数组重的每个元素"""
+    for everyone in every:
+        if isinstance(everyone,list=0):
+            print_qiantao(everyone,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(everyone)
---
+++ packages/fabu/fabu-1.2.0.tar.gz/fabu-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup   (
+	name		='fabu',
+	version		='1.2.0',
+	py_modules	=['qiantao'],
+	author		='hfpython',
+	author_email	='hfpython@headfirstlabs.com',
+	url		='http://www.headfirstlabs.com',
+	description	='A simple printer of nester lists',
+	)
