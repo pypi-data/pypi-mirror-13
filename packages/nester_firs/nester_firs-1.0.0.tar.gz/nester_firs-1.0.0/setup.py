ec757d90180d9ead22d7669f5b76f89445febe94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:40 +0000

    Add package nester_firs/1.0.0/nester_firs-1.0.0.tar.gz

---
+++ packages/nester_firs/nester_firs-1.0.0.tar.gz/nester_firs-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'nester_firs',
+		version = '1.0.0',
+		py_modules = ['nester'],
+		author = 'lijianhui',
+		author_email = 'lijianui@37.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nested lists',
+	)
