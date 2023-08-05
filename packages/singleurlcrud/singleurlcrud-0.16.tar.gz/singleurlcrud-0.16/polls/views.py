9ccaa767b1ac61f628054df469c4b68f154dee2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:35 +0000

    Add package singleurlcrud/0.16/singleurlcrud-0.16.tar.gz

---
+++ packages/singleurlcrud/singleurlcrud-0.16.tar.gz/singleurlcrud-0.16/polls/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+
+from singleurlcrud.views import CRUDView
+from .models import *
+
+# Create your views here.
+class AuthorCRUDView(CRUDView):
+    model = Author
+    list_display = ('name',)
+
+class QuestionCRUDView(CRUDView):
+    model = Question
+    list_display = ('question_text', 'pub_date', 'author')
+    related_field_crud_urls = {
+            'author': reverse_lazy("polls:authors")
+            }
+
+    def get_actions(self):
+        self.related_field_crud_urls = {
+            'author': reverse_lazy('polls:authors') +"?o=add",
+            }
+        return [
+            ('Delete', self.delete_multiple_items)
+            ]
+
+    def delete_multiple_items(self, request, items):
+        Question.objects.filter(pk__in=items).delete()
+
+    class VoteItemAction(object):
+        title = 'Vote'
+        key = 'vote1'
+        css = 'glyphicon glyphicon-envelope'
+
+        def doAction(self, item):
+            import logging
+            logging.getLogger('general').info("VoteItemAction invoked!")
+            pass
+
+    def get_item_actions(self):
+        return [self.VoteItemAction()]
+
---
+++ packages/singleurlcrud/singleurlcrud-0.16.tar.gz/singleurlcrud-0.16/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='singleurlcrud',
+    description='Django CRUD, implemented using a single view and hence a single URL.',
+    long_description=readme,
+    version='0.16',
+    author='Hari Mahadevan',
+    author_email='hari@hari.xyz',
+    url='https://www.github.com/harikvpy/crud/tarball/0.16',
+    packages=[
+        'singleurlcrud',
+        ],
+    include_package_data=True,
+    install_requires=[
+        'django-pure-pagination',
+        'django-bootstrap3'
+        ],
+    license='BSD-3',
+    keywords=[
+        'django',
+        'crud',
+        'singleurlcrud',
+        ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )
