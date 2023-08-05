d12561e551f04bcf26e2b038c93c5128b0bf520c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package yieldfrom.urllib3/0.1.4/yieldfrom.urllib3-0.1.4.zip

---
+++ packages/yieldfrom.urllib3/yieldfrom.urllib3-0.1.4.zip/yieldfrom.urllib3-0.1.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import os
+import re
+
+
+setup(name='yieldfrom.urllib3',
+
+      version='0.1.4',
+
+      description="Asyncio HTTP library with thread-safe connection pooling, file post, and more.",
+      long_description=open('README.rst').read() + '\n\n' + open('CHANGES.rst').read(),
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Software Development :: Libraries',
+      ],
+      keywords='urllib httplib asyncio filepost http https ssl pooling',
+
+      author='Andrey Petrov',
+      author_email='andrey.petrov@shazow.net',
+      maintainer='David Keeney',
+      maintainer_email='dkeeney@rdbhost.com',
+
+      url='http://urllib3.readthedocs.org/',
+      license='MIT',
+
+      packages=['yieldfrom', 'yieldfrom.urllib3',
+                'yieldfrom.urllib3.packages', 'yieldfrom.urllib3.packages.ssl_match_hostname',
+                'yieldfrom.urllib3.util',
+                ],
+      package_dir={'yieldfrom': 'yieldfrom'},
+      install_requires=['yieldfrom.http.client<0.2', 'setuptools'],
+      namespace_packages=['yieldfrom'],
+      zip_safe=False,
+      )
