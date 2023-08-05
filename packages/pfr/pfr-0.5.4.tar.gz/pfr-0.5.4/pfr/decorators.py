06545596da86ee2d3bc6e7d76e4a46166ea8a610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:15 +0000

    Add package pfr/0.5.4/pfr-0.5.4.tar.gz

---
+++ packages/pfr/pfr-0.5.4.tar.gz/pfr-0.5.4/pfr/decorators.py
@@ -0,0 +1,108 @@
+import collections
+import datetime
+import functools
+import os
+import time
+import urlparse
+
+import appdirs
+
+def switchToDir(dirPath):
+    """
+    Decorator that switches to given directory before executing function, and
+    then returning to orignal directory.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            orig_cwd = os.getcwd()
+            os.chdir(dirPath)
+            ret = func(*args, **kwargs)
+            os.chdir(orig_cwd)
+            return ret
+        return wrapper
+
+    return decorator
+
+def cacheHTML(func):
+    """Caches the HTML returned by the specified function `func`. Caches it in
+    the user cache determined by the appdirs package.
+    """
+
+    CACHE_DIR = appdirs.user_cache_dir('pfr', 'mgoldberg')
+    if not os.path.isdir(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+
+    @functools.wraps(func)
+    def wrapper(url):
+        parsed = urlparse.urlparse(url)
+        relURL = parsed.path
+        if parsed.query:
+            relURL += '?' + parsed.query
+        noPathFN = relURL.replace('/', '')
+        fn = '{}/{}'.format(CACHE_DIR, noPathFN)
+
+        # TODO: fix this problem?
+        if len(noPathFN) > 255:
+            # filename is too long, just evaluate the function
+            return func(url).decode('utf-8', 'ignore')
+        
+        # set time variables (in seconds)
+        if os.path.isfile(fn):
+            modtime = int(os.path.getmtime(fn))
+            curtime = int(time.time())
+
+        def cacheValid(ct, mt, fn):
+            # first, if the filetype is a boxscore, then we're safe caching it
+            if any(kw in fn for kw in ('boxscore',)):
+                return True
+
+            # otherwise, check if it's currently the offseason
+            today = datetime.date.today()
+            endOfSeason = datetime.date(today.year, 2, 10)
+            startOfSeason = datetime.date(today.year, 9, 1)
+            # if we're in the offseason, don't worry about it
+            if endOfSeason < today < startOfSeason:
+                return True
+
+            # otherwise, check if new data could have been updated since mod
+            # (assumed that new game data is added the day after that game)
+            modDay = today - datetime.timedelta(seconds=ct-mt)
+            lastGameDay = today
+            while (lastGameDay.weekday() + 1) % 7 not in (0, 1, 2, 5):
+                lastGameDay = lastGameDay - datetime.timedelta(days=1)
+            return modDay >= lastGameDay
+
+        # if file found and caching is valid, read from file
+        if os.path.isfile(fn) and cacheValid(curtime, modtime, fn):
+            with open(fn, 'r') as f:
+                text = f.read()
+            return text
+        # otherwise, download html and cache it
+        else:
+            text = func(url)#.encode('ascii', 'replace')
+            with open(fn, 'w+') as f:
+                f.write(text)
+            return text
+    
+    return wrapper
+
+def memoized(fun):
+    """A simple memoize decorator."""
+    @functools.wraps(fun)
+    def wrapper(*args, **kwargs):
+        # TODO: deal with dicts in args
+        key = (args, frozenset(sorted(kwargs.items())))
+        try:
+            ret = cache[key]
+            return ret
+        except KeyError:
+            ret = cache[key] = fun(*args, **kwargs)
+            return ret
+        except TypeError:
+            print 'memoization type error here', fun.__name__, key
+            return fun(*args, **kwargs)
+
+    cache = {}
+    return wrapper
---
+++ packages/pfr/pfr-0.5.4.tar.gz/pfr-0.5.4/pfr/players.py
@@ -0,0 +1,192 @@
+import datetime
+import re
+import urlparse
+
+import numpy as np
+import pandas as pd
+from pyquery import PyQuery as pq
+
+import pfr
+
+__all__ = [
+    'Player',
+]
+
+yr = datetime.datetime.now().year
+
+class Player:
+
+    def __init__(self, playerID):
+        self.pID = playerID
+        self.mainURL = urlparse.urljoin(
+            pfr.BASE_URL, '/players/{0[0]}/{0}.htm'
+        ).format(self.pID)
+
+    def __eq__(self, other):
+        return self.pID == other.pID
+
+    def __hash__(self):
+        return hash(self.pID)
+
+    @pfr.decorators.memoized
+    def getDoc(self):
+        doc = pq(pfr.utils.getHTML(self.mainURL))
+        return doc
+
+    @pfr.decorators.memoized
+    def name(self):
+        doc = self.getDoc()
+        name = doc('div#info_box h1:first').text()
+        return name
+
+    @pfr.decorators.memoized
+    def age(self, year=yr, month=9, day=1):
+        doc = self.getDoc()
+        span = doc('div#info_box span#necro-birth')
+        birthstring = span.attr('data-birth')
+        dateargs = re.match(r'(\d{4})\-(\d{2})\-(\d{2})', birthstring).groups()
+        dateargs = map(int, dateargs)
+        birthDate = datetime.date(*dateargs)
+        delta = datetime.date(year=year, month=month, day=day) - birthdate
+        age = delta.days / 365.
+        return age
+
+    @pfr.decorators.memoized
+    def position(self):
+        doc = self.getDoc()
+        rawText = (doc('div#info_box p')
+                   .filter(lambda i,e: 'Position' in e.text_content())
+                   .text())
+        rawPos = re.search(r'Position: (\S+)', rawText, re.I).group(1)
+        allPositions = rawPos.split('-')
+        # right now, returning just the primary position for those with
+        # multiple positions
+        return allPositions[0]
+
+    @pfr.decorators.memoized
+    def height(self):
+        doc = self.getDoc()
+        try:
+            rawText = (doc('div#info_box p')
+                       .filter(
+                           lambda i,e: 'height:' in e.text_content().lower()
+                       ).text())
+            rawHeight = (re.search(r'Height: (\d\-\d{1,2})', rawText, re.I)
+                         .group(1))
+        except AttributeError:
+            return np.nan
+        feet, inches = map(int, rawHeight.split('-'))
+        return feet*12 + inches
+
+    @pfr.decorators.memoized
+    def weight(self):
+        doc = self.getDoc()
+        rawText = (doc('div#info_box p')
+                   .filter(lambda i,e: 'Weight:' in e.text_content())
+                   .text())
+        rawWeight = re.search(r'Weight: (\S+)', rawText, re.I).group(1)
+        return int(rawWeight)
+
+    @pfr.decorators.memoized
+    def hand(self):
+        doc = self.getDoc()
+        rawText = (doc('div#info_box p')
+                   .filter(lambda i,e: 'Position' in e.text_content())
+                   .text())
+        rawHand = re.search(r'Throws: (\S+)', rawText, re.I).group(1)
+        return rawHand[0] # 'L' or 'R'
+
+    @pfr.decorators.memoized
+    def pick(self):
+        doc = self.getDoc()
+        rawDraft = doc('div#info_box > p:first').text()
+        m = re.search(r'Drafted .*? round \((\d+).*?overall\)', rawDraft, re.I)
+        # if not drafted or taken in supplemental draft, return NaN
+        if not m or 'Supplemental' in rawDraft:
+            return np.nan
+        else:
+            return int(m.group(1))
+
+    @pfr.decorators.memoized
+    def draftClass(self):
+        doc = self.getDoc()
+        rawDraft = doc('div#info_box > p:first').text()
+        m = re.search(r'Drafted.*?of the (\d{4}) NFL', rawDraft, re.I)
+        if not m:
+            return np.nan
+        else:
+            return int(m.group(1))
+
+    @pfr.decorators.memoized
+    def draftTeam(self):
+        doc = self.getDoc()
+        rawDraft = doc('div#info_box > p:first')
+        draftStr = pfr.utils.flattenLinks(rawDraft)
+        m = re.search(r'Drafted by the (\w{3})', draftStr)
+        if not m:
+            return np.nan
+        else:
+            return m.group(1)
+
+    @pfr.decorators.memoized
+    def college(self):
+        doc = self.getDoc()
+        rawText = doc('div#info_box > p:first')
+        cleanedText = pfr.utils.flattenLinks(rawText)
+        college = re.search(r'College: (\S+)', cleanedText).group(1)
+        return college
+
+    @pfr.decorators.memoized
+    def highSchool(self):
+        doc = self.getDoc()
+        rawText = doc('div#info_box > p:first')
+        cleanedText = pfr.utils.flattenLinks(rawText)
+        hs = re.search(r'High School: (\S{8})', cleanedText).group(1)
+        return hs
+
+    @pfr.decorators.memoized
+    def av(self, year=yr):
+        doc = self.getDoc()
+        tables = doc('table[id]').filter(
+            lambda i,e: 'AV' in e.text_content()
+        )
+        # if no AV table, return NaN
+        if not tables:
+            return np.nan
+        # otherwise, extract the AV
+        table = tables.eq(0)
+        df = pfr.utils.parseTable(table)
+        df = df.query('year_id == @year')
+        # if the player has an AV for that year, return it
+        if not df.empty:
+            return df['av'].iloc[0]
+        # otherwise, return NaN
+        else:
+            return np.nan
+
+    @pfr.decorators.memoized
+    def gamelog(self):
+        """Gets the career gamelog of the given player.
+        :returns: A DataFrame with the player's career gamelog.
+        """
+        url = urlparse.urljoin(
+            pfr.BASE_URL, '/players/{0[0]}/{0}/gamelog'
+        ).format(self.pID)
+        doc = pq(pfr.utils.getHTML(url))
+        table = doc('#stats')
+        df = pfr.utils.parseTable(table)
+        return df
+
+    @pfr.decorators.memoized
+    def passing(self):
+        doc = self.getDoc()
+        table = doc('#passing')
+        df = pfr.utils.parseTable(table)
+        return df
+
+    @pfr.decorators.memoized
+    def rushing_and_receiving(self):
+        doc = self.getDoc()
+        table = doc('#rushing_and_receiving')
+        df = pfr.utils.parseTable(table)
+        return df
