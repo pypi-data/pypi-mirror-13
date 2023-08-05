192059fe0d7e9c6c083ddc43e3fa0317365f2155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:13 +0000

    Add package sentry/8.0.0/sentry-8.0.0-py27-none-any.whl

---
+++ packages/sentry/sentry-8.0.0-py27-none-any.whl/sentry/static/sentry/app/views/releaseArtifacts.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {History} from 'react-router';
+
+import ApiMixin from '../mixins/apiMixin';
+import FileSize from '../components/fileSize';
+import LoadingError from '../components/loadingError';
+import LoadingIndicator from '../components/loadingIndicator';
+import Pagination from '../components/pagination';
+import {t} from '../locale';
+
+const ReleaseArtifacts = React.createClass({
+  contextTypes: {
+    release: React.PropTypes.object
+  },
+
+  mixins: [
+    ApiMixin,
+    History
+  ],
+
+  getInitialState() {
+    return {
+      loading: true,
+      error: false,
+      fileList: [],
+      pageLinks: null
+    };
+  },
+
+  componentWillMount() {
+    this.fetchData();
+  },
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.search !== prevProps.location.search) {
+      this.fetchData();
+    }
+  },
+
+  fetchData() {
+    let params = this.props.params;
+    let endpoint = '/projects/' + params.orgId + '/' + params.projectId + '/releases/' + params.version + '/files/';
+
+    this.setState({
+      loading: true,
+      error: false
+    });
+
+    this.api.request(endpoint, {
+      method: 'GET',
+      data: this.props.location.query,
+      success: (data, _, jqXHR) => {
+        this.setState({
+          error: false,
+          loading: false,
+          fileList: data,
+          pageLinks: jqXHR.getResponseHeader('Link')
+        });
+      },
+      error: () => {
+        this.setState({
+          error: true,
+          loading: false
+        });
+      }
+    });
+  },
+
+  render() {
+    if (this.state.loading)
+      return <LoadingIndicator />;
+    else if (this.state.error)
+      return <LoadingError onRetry={this.fetchData} />;
+    else if (this.state.fileList.length === 0)
+      return (
+        <div className="box empty-stream">
+          <span className="icon icon-exclamation"></span>
+          <p>{t('There are no artifacts uploaded for this release.')}</p>
+        </div>
+      );
+
+    // TODO(dcramer): files should allow you to download and delete them
+    return (
+      <div>
+        <table className="table">
+          <tbody>
+          {this.state.fileList.map((file) => {
+            return (
+              <tr key={file.id}>
+                <td><strong>{file.name}</strong></td>
+                <td style={{width: 120}}><FileSize bytes={file.size} /></td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
+        <Pagination pageLinks={this.state.pageLinks}/>
+      </div>
+    );
+  }
+});
+
+export default ReleaseArtifacts;
