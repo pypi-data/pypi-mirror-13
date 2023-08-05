0b546f2a105a410eb1799ecd6b9ae27f14a5dd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package agora/0.5.41/agora-0.5.41.zip

---
+++ packages/agora/agora-0.5.41.zip/agora-0.5.41/agora/corelibs/onyx_utils.py
@@ -0,0 +1,75 @@
+###############################################################################
+#
+#   Agora Portfolio & Risk Management System
+#
+#   Copyright 2015 Carlo Sbraccia
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+###############################################################################
+
+from onyx.database.objdb import ObjDbBase
+
+import onyx.core
+import contextlib
+import getpass
+import os
+
+__all__ = ["OnyxInit", "OnyxStartup"]
+
+
+# -----------------------------------------------------------------------------
+def OnyxInit(objdb=None, tsdb=None, user=None, host=None):
+    """
+    Description:
+        Activate onyx databases and graph using the respective context
+        managers.
+    Inputs:
+        objdb - an instance of an ObjDb client or the name of a valid ObjDb
+        tsdb  - an instance of an TsDb client or the name of a valid TsDb
+        user  - the database user
+        host  - the database host
+    Returns:
+        A stack of context managers.
+    """
+    objdb = objdb or os.getenv("OBJDB_PROD", default="ProdDb")
+    tsdb = tsdb or os.getenv("TSDB_PROD", default="TsDb")
+    user = user or os.getenv("POSTGRESQL_USER", getpass.getuser())
+    host = host or os.getenv("POSTGRESQL_HOST", None)
+
+    if isinstance(objdb, ObjDbBase):
+        objdb_clt = objdb
+    else:
+        objdb_clt = onyx.core.ObjDbClient(objdb, user, host)
+
+    if isinstance(tsdb, onyx.core.TsDbClient):
+        tsdb_clt = tsdb
+    else:
+        tsdb_clt = onyx.core.TsDbClient(tsdb, user, host)
+
+    stack = contextlib.ExitStack()
+    stack.enter_context(onyx.core.UseDatabase(objdb_clt))
+    stack.enter_context(onyx.core.TsDbUseDatabase(tsdb_clt))
+    stack.enter_context(onyx.core.UseGraph())
+
+    return stack
+
+
+# -----------------------------------------------------------------------------
+def OnyxStartup(objdb=None, tsdb=None, user=None, host=None):
+    stack = OnyxInit(objdb, tsdb, user, host)
+    stack.__enter__()
+    print("""Onyx has been fired up... Good luck!!!""")
+    return {key: value
+            for key, value in onyx.core.__dict__.items()
+            if not key.startswith("__")}
