5f61e07d12513cee7d3b2f4573716317bd405e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package pyexcel-xls/0.1.0/pyexcel-xls-0.1.0.tar.gz

---
+++ packages/pyexcel-xls/pyexcel-xls-0.1.0.tar.gz/pyexcel-xls-0.1.0/setup.py
@@ -0,0 +1,54 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+import sys
+
+with open("README.rst", 'r') as readme:
+    README_txt = readme.read()
+
+dependencies = [
+    'pyexcel-io>=0.1.0',
+    'xlrd',
+    'xlwt-future'
+]
+
+with open("VERSION", "r") as version:
+    version_txt = version.read().rstrip()
+
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    dependencies.append('ordereddict')
+
+setup(
+    name='pyexcel-xls',
+    author="C. W.",
+    version=version_txt,
+    author_email="wangc_2011@hotmail.com",
+    url="https://github.com/chfw/pyexcel-xls",
+    description='A wrapper library to read, manipulate and write data in xls format. It reads xlsx and xlsm format',
+    install_requires=dependencies,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    long_description=README_txt,
+    zip_safe=False,
+    tests_require=['nose'],
+    keywords=['excel', 'xls', 'xlsx'],
+    license='GNU GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Office/Business',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
