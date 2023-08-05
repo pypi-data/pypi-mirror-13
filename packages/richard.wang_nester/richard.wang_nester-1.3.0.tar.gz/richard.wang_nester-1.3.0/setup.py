63122a2d677ac2d56f61eff36b8b7f2ea91ff303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package richard.wang_nester/1.3.0/richard.wang_nester-1.3.0.tar.gz

---
+++ packages/richard.wang_nester/richard.wang_nester-1.3.0.tar.gz/richard.wang_nester-1.3.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+        name='richard.wang_nester',
+        version='1.3.0',
+        py_modules=['nester'],
+        author='richard.wang',
+        author_email='13811666390@139.com',
+	url='taosim.cnblogs.com',
+        description='A simple printer of nested list',
+)
