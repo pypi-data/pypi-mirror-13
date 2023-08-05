2f65bb5533d5b7b6c4fe6f47dd8b02a07814a35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:28 +0000

    Add package spm1d/0.3.1.8/spm1d-0.3.1.8.tar.gz

---
+++ packages/spm1d/spm1d-0.3.1.8.tar.gz/spm1d-0.3.1.8/spm1d/stats/anova/__init__.py
@@ -0,0 +1,8 @@
+
+import designs
+import factors
+import models
+
+from ui import anova1,anova1rm
+from ui import anova2,anova2nested,anova2rm,anova2onerm
+from ui import anova3,anova3nested,anova3rm,anova3tworm,anova3onerm
