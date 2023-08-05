00f52f9bf5030e2cd3539276d24eb0e1cc1ece28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package djasmine/0.2.3/djasmine-0.2.3.tar.gz

---
+++ packages/djasmine/djasmine-0.2.3.tar.gz/djasmine-0.2.3/djasmine/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os.path
+import glob2
+from django.conf import settings
+from django.shortcuts import render
+
+
+def find_specfiles():
+    for filepath in glob2.iglob(
+            settings.JASMINE_SPEC_ROOT + '/**/*.[Ss]pec.js'):
+        yield os.path.relpath(filepath, settings.JASMINE_SPEC_ROOT)
+
+
+def jasmine_run_spec(request, spec=None):
+    if spec:
+        specfiles = [spec]
+    else:
+        specfiles = find_specfiles()
+    return render(
+        request,
+        'djasmine/specrunner.html',
+        {'specfiles': specfiles},
+    )
