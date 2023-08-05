fa64ed989a67ee35690a3e994feedaf06586f3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:57 +0000

    Add package oneID-cli/1.0.0/oneID-cli-1.0.0.tar.gz

---
+++ packages/oneID-cli/oneID-cli-1.0.0.tar.gz/oneID-cli-1.0.0/src/oneid_cli/services/__init__.py
@@ -0,0 +1,8 @@
+
+from .configure import Configure
+from .list_projects import ListProjects
+from .create_project import CreateProject
+from .provision import Provision
+from .revoke import Revoke
+
+__all__ = [Configure, ListProjects, CreateProject, Provision, Revoke]
