e42c1cbc684a33abe22d4cf6903ecc1e5909a37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:58 +0000

    Add package django-iprestrict/0.4.1/django-iprestrict-0.4.1.tar.gz

---
+++ packages/django-iprestrict/django-iprestrict-0.4.1.tar.gz/django-iprestrict-0.4.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='django-iprestrict',
+    version='0.4.1',
+    description='Django app + middleware to restrict access to all or sections of a Django project by client IP ranges',
+    long_description='Django app + middleware to restrict access to all or sections of a Django project by client IP ranges',
+    author='Tamas Szabo, CCG, Murdoch University',
+    author_email='devops@ccg.murdoch.edu.au',
+    url='https://github.com/muccg/django-iprestrict',
+    download_url='https://github.com/muccg/django-iprestrict/releases',
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    packages=[
+        'iprestrict',
+        'iprestrict.management',
+        'iprestrict.management.commands',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django>=1.7',
+        'django-templatetag-handlebars',
+    ],
+    test_suite='tests.runtests.main',
+)
