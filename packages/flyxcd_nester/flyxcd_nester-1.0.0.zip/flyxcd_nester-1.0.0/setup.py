74f74c187e738627d4b10d99a7af15736f95bb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package flyxcd_nester/1.0.0/flyxcd_nester-1.0.0.zip

---
+++ packages/flyxcd_nester/flyxcd_nester-1.0.0.zip/flyxcd_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'flyxcd_nester',
+    version = '1.0.0',
+    py_modules = ['flyxcd_nester'],
+    author = 'xcdpython',
+    author_email = 'xcd3306@126.com',
+    url = '',
+    description = 'A simple printer of nested lists',
+)
