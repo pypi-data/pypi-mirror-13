2746692ec1b1fb33110bcb1f9e687c4326e0a5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:56 +0000

    Add package updoc/1.7.5/updoc-1.7.5.tar.gz

---
+++ packages/updoc/updoc-1.7.5.tar.gz/updoc-1.7.5/updoc/defaults.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import os
+from djangofloor.utils import DirectoryPath
+
+__author__ = 'Matthieu Gallet'
+PROTOCOL = 'http'
+HOST = '{PROTOCOL}://{SERVER_NAME}'
+PUBLIC_BOOKMARKS = True
+PUBLIC_BOOKMARKS_HELP = 'Are bookmarks publicly available?'
+PUBLIC_PROXIES = True
+PUBLIC_PROXIES_HELP = 'Is proxy.pac file publicly available?'
+PUBLIC_INDEX = True
+PUBLIC_INDEX_HELP = 'Is the list of all documentations publicly available?'
+PUBLIC_DOCS = True
+PUBLIC_DOCS_HELP = 'Are documentations publicly available?'
+WS4REDIS_EMULATION_INTERVAL = 5000
+LOCAL_PATH = os.path.join(os.path.dirname(__file__), '..', 'django_data')
+BIND_ADDRESS = 'localhost:8129'
+ES_HOSTS = None
+ES_HOSTS_HELP = 'IP:port of your ElasticSearch database, leave it empty if you do not use ElasticSearch'
+ES_INDEX = 'updoc_index'
+ES_INDEX_HELP = 'name of your ElasticSearch index'
+ES_TIKA_EXTENSIONS = 'pdf,html,doc,odt,rtf,epub'
+ES_MAX_SIZE = 30 * 1024 * 1024
+ES_DOC_TYPE = 'document'
+ES_PLAIN_EXTENSIONS = 'txt,csv,md,rst'
+ES_EXCLUDED_DIR = '_sources,_static'
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.request',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'djangofloor.context_processors.context_base',
+    'updoc.context_processors.most_checked',
+    ]
+
+# FLOOR_AUTHENTICATION_HEADER = None
+
+########################################################################################################################
+# sessions
+########################################################################################################################
+# SESSION_ENGINE = 'redis_sessions.session'
+# SESSION_REDIS_PREFIX = 'session'
+# SESSION_REDIS_HOST = '{REDIS_HOST}'
+# SESSION_REDIS_PORT = '{REDIS_PORT}'
+# SESSION_REDIS_DB = 10
+
+
+########################################################################################################################
+# caching
+########################################################################################################################
+CACHES = {
+    'default': {'BACKEND': 'django_redis.cache.RedisCache', 'LOCATION': 'redis://{REDIS_HOST}:{REDIS_PORT}/11',
+                'OPTIONS': {'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+                            'PARSER_CLASS': 'redis.connection.HiredisParser', }, },
+    }
+
+########################################################################################################################
+# django-redis-websocket
+########################################################################################################################
+
+########################################################################################################################
+# celery
+########################################################################################################################
+USE_CELERY = True
+FLOOR_INSTALLED_APPS = ['updoc', ]
+FLOOR_INDEX = 'updoc.views.index'
+FLOOR_URL_CONF = 'updoc.root_urls.urls'
+FLOOR_PROJECT_NAME = 'UpDoc!'
+FILE_UPLOAD_TEMP_DIR = DirectoryPath('{LOCAL_PATH}/tmp')
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '5I0zJQuHzqcACuzGIwTAC3cV6RlZpjV8MNUETYd5KZXg6UoI4G'
+
+FLOOR_EXTRA_JS = ['js/jquery.ui.widget.js', 'js/jquery.iframe-transport.js', 'js/jquery.fileupload.js',
+                  'js/fuelux.min.js', 'js/updoc.js', ]
+FLOOR_EXTRA_CSS = ['css/fuelux.min.css', ]
---
+++ packages/updoc/updoc-1.7.5.tar.gz/updoc-1.7.5/updoc/templates/updoc/dev/doc/source/installation.rst
@@ -0,0 +1,60 @@
+{% extends 'djangofloor/dev/doc/source/installation.rst' %}
+
+{% block post_dependencies %}
+  * elasticsearch>=2.0.0
+  * requests
+  * markdown
+  * hiredis{% endblock %}
+
+{% block webserver_ssl_extra %}            <Location /updoc/show_alt/>
+                Order deny,allow
+                Allow from all
+                Satisfy any
+            </Location>
+{% endblock %}
+
+{% block post_application %}    moneta-manage createsuperuser
+    echo "CACHES = {'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}}" > $VIRTUAL_ENV/etc/updoc/settings.py
+    updoc-manage init_es
+{% endblock %}
+
+{% block other_application %}Elasticsearch
+=============
+
+UpDoc uses ElasticSearch to index documents.
+If you have a recent Debian/Ubuntu distribution, you can directly install ElasticSearch.
+
+.. code-block:: bash
+
+    sudo apt-get install elasticsearch
+
+Otherwise, you should install a more recent version from their official repository:
+
+.. code-block:: bash
+
+    wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+    echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sudo tee /etc/apt/sources.list.d/elasticsearch.list
+    sudo apt-get update
+    sudo apt-get install openjdk-7-jre-headless elasticsearch
+    sudo /bin/systemctl daemon-reload
+    sudo /bin/systemctl enable elasticsearch.service
+    sudo service elasticsearch start
+
+sudo chown elasticsearch:elasticsearch /usr/share/elasticsearch
+sudo sed -i -s 's%#LOG_DIR=/var/log/elasticsearch%LOG_DIR=/var/log/elasticsearch%' /etc/default/elasticsearch
+sudo sed -i -s 's%#DATA_DIR=/var/lib/elasticsearch%DATA_DIR=/var/lib/elasticsearch%' /etc/default/elasticsearch
+sudo sed -i -s 's%#WORK_DIR=/tmp/elasticsearch%WORK_DIR=/tmp/elasticsearch%' /etc/default/elasticsearch
+sudo sed -i -s 's%#CONF_DIR=/etc/elasticsearch%CONF_DIR=/etc/elasticsearch%' /etc/default/elasticsearch
+sudo sed -i -s 's%#CONF_FILE=/etc/elasticsearch/elasticsearch.yml%CONF_FILE=/etc/elasticsearch/elasticsearch.yml%' /etc/default/elasticsearch
+sudo sed -i -s 's%#network.bind_host: 192.168.0.1%network.bind_host: 127.0.0.1%' /etc/elasticsearch/elasticsearch.yml
+# if you still use IP v.4
+echo 'JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"' | sudo tee -a /usr/share/elasticsearch/bin/elasticsearch.in.sh
+
+On Debian 7, you probably should use something like:
+
+.. code-block:: bash
+
+    sudo update-rc.d elasticsearch defaults 95 10
+    sudo /etc/init.d/elasticsearch start
+
+{% endblock %}
