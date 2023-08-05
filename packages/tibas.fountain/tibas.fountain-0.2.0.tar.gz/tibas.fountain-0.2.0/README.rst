0e23f6ab7371836b71fcb71f40cca72329304505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:33 +0000

    Add package tibas.fountain/0.2.0/tibas.fountain-0.2.0.tar.gz

---
+++ packages/tibas.fountain/tibas.fountain-0.2.0.tar.gz/tibas.fountain-0.2.0/README.rst
@@ -0,0 +1,66 @@
+sphinx extension for rendering fountain screenplays
+======================================================
+:version: 0.2.0
+
+Simple `Sphinx <http://www.sphinx-doc.org/en/stable/>`_ HTML extension for embedding screenplay fragments written in `Fountain <http://fountain.io>`_
+
+This project uses the `fountain python module <https://bitbucket.org/gabriel.montagne/fountain>`_ to parse the markup.
+
+Installation
+--------------
+
+Install the package by running::
+
+    python3 setup.py install
+
+Then add the ``tibas.fountain`` package to your ``conf.py`` ``extensions`` module names array. For example::
+
+    extensions = [
+        'sphinx.ext.extlinks',
+        'sphinx.ext.intersphinx',
+        'sphinx.ext.todo',
+        'alabaster',
+        'sphinxcontrib.blockdiag',
+        'ablog',
+        'tibas.fountain',
+    ]
+
+Usage
+-------
+
+The extension provides a ``fountain`` directive::
+
+    .. fountain::
+
+      EXT. MOTORWAY VERGE - NIGHT
+
+      Sometime later, as the crowd disperses and the traffic begins to flow normally, James kneels besides the Lincoln and shows Vaughn the blood on his door.
+      Catherine sits in the back seat.
+
+      JAMES
+      We must have driven through a pool of blood.
+      If the police stops you again, they may impound the car while they have the blood analyzed.
+
+      Vaughan kneels besides him and inspects the smears of blood.
+
+      VAUGHAN
+      You're right Ballard. 
+      There's an all-night car-wash in the airport service area.
+
+      Vaughan rises and holds the door open for James, who sits behind the wheel, expecting Vaughan to walk around the car and sit beside him.
+      Instead, Vaughan pulls open the rear door and and climbs in beside Catherine.
+
+      As they set off, Vaughan's camera lands on the front seat.
+
+      INT. VAUGHAN'S CAR - NIGHT
+
+    Etc.
+
+You can also use external files::
+
+    .. fountain:: somewhere/screenplay.fountain
+
+This will produce HTML with classes you can then style however you prefer,
+
+.. image:: screenshot-cronenberg.png
+
---
+++ packages/tibas.fountain/tibas.fountain-0.2.0.tar.gz/tibas.fountain-0.2.0/tibas/fountain/__init__.py
@@ -0,0 +1,77 @@
+from docutils import nodes
+from docutils.parsers.rst import Directive
+from fountain import Fountain
+
+def append_dialogue(container, token):
+    char = nodes.line()
+    char += nodes.Text(token['character'])
+    char.attributes['classes'] = [ 'dialog-character' ]
+    container += char
+
+    line = nodes.line()
+    line += nodes.Text('\n'.join(token['text']))
+    line.attributes['classes'] = [ 'dialog-text' ]
+    container += line
+
+def append_action(container, token):
+    line = nodes.line()
+    line += nodes.Text('\n'.join(token['lines']))
+    line.attributes['classes'] = [ 'action' ]
+    container += line
+
+def append_transition(container, token):
+    line = nodes.line()
+    line += nodes.Text(token['transition'] + ':')
+    container += line
+
+def append_slugline(container, token):
+    line = nodes.line()
+    forced = token.get('forced')
+    if forced:
+        line += nodes.Text(token['description'])
+    else:
+        line += nodes.Text(token.get('place', '') + '. ')
+        line += nodes.Text(token.get('location', ''))
+        time = token.get('time', '')
+        if time:
+            line += nodes.Text(' -- ')
+        line += nodes.Text(time)
+
+    line.attributes['classes'] = [ 'slugline' ]
+    container.append(line)
+
+def nothing(container, token):
+    print('do nothing with', token, token['type'])
+
+token_builder_by_type = {
+    'dialogue': append_dialogue,
+    'action': append_action,
+    'slugline': append_slugline,
+    'transition': append_transition,
+}
+
+def setup(app):
+    app.add_directive('fountain', FountainDirective)
+    return { 'version': '0.2.0' }
+
+class FountainDirective(Directive):
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    def run(self):
+        if self.arguments and self.content:
+            raise Exception('Only source argument or content can be provided.')
+
+        if self.arguments:
+            with open(self.arguments[0]) as script:
+                content = script.readlines()
+        else:
+            content = self.content
+
+        parser = Fountain()
+        tokens = parser.tokenize(content)
+        container = nodes.container()
+        for token in tokens:
+            token_builder_by_type.get(token['type'], nothing)(container, token)
+        container.attributes['classes'] = [ 'screenplay' ]
+        return [ container ]
