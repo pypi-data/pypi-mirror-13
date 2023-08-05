b255ee3f95678d3299a71a7f13b262229789ce26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package nesterCece/1.3.0/nesterCece-1.3.0.tar.gz

---
+++ packages/nesterCece/nesterCece-1.3.0.tar.gz/nesterCece-1.3.0/nesterCece.py
@@ -0,0 +1,19 @@
+"""
+Given a list, this recorsive function prints out on the shell
+the items of the list.
+If there are nested lists, then this function will recurse and
+iterate through the items of the list, so that all the elements
+will be printed out.
+"""
+
+def print_lol(the_list, level=0):
+    for item in the_list:
+        if isinstance(item, list):
+            if level != 0:
+                print_lol(item, level+1)
+            else:
+                print_lol(item, level)
+        else:
+            for num in range(level):
+                print("\t", end='')
+            print(item)
