6f7b0174d809a8084bf63a9abe5bff23794b4e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:40 +0000

    Add package nester_qwerty/1.1.0/nester_qwerty-1.1.0.tar.gz

---
+++ packages/nester_qwerty/nester_qwerty-1.1.0.tar.gz/nester_qwerty-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='nester_qwerty',
+    version='1.1.0',
+    py_modules=['nester_qwerty'],
+    author='khainen',
+    author_email='khainen.23@gmail.com.com',
+    description='A simple printer of nested lists',
+    )
