90b66b860d4971de701c06ee902ee84c6fb3f56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package pytest-qt-app/1.0.0/pytest-qt-app-1.0.0.tar.gz

---
+++ packages/pytest-qt-app/pytest-qt-app-1.0.0.tar.gz/pytest-qt-app-1.0.0/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup
+
+execfile(os.path.join(os.path.pardir, 'common_setup.py'))
+         
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 5 - Production/Stable',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: User Interfaces',
+    'Intended Audience :: Developers',
+    'Operating System :: POSIX',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+install_requires = ['pytest',
+                    'pytest-server-fixtures',
+                    'pytest-shutil',
+                    ]
+
+tests_require = ['pytest-cov'
+                 ]
+
+entry_points = {
+    'pytest11': [
+        'qt = pytest_qt_app',
+    ]
+}
+
+if __name__ == '__main__':
+    kwargs = common_setup('pytest_qt_app')
+    kwargs.update(dict(
+        name='pytest-qt-app',
+        description='QT app fixture for py.test',
+        author='Edward Easton',
+        author_email='eeaston@gmail.com',
+        classifiers=classifiers,
+        install_requires=install_requires,
+        tests_require=tests_require,
+        py_modules=['pytest_qt_app'],
+        entry_points=entry_points,
+    ))
+    setup(**kwargs)
