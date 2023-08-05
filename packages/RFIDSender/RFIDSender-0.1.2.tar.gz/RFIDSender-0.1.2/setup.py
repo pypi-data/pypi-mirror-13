9ebff2167b94141a6894c350371cde8879b38367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package RFIDSender/0.1.2/RFIDSender-0.1.2.tar.gz

---
+++ packages/RFIDSender/RFIDSender-0.1.2.tar.gz/RFIDSender-0.1.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name="RFIDSender",
+      packages = find_packages(),
+      version="0.1.2",
+      description="Bostin Technology RFID Sender Module",
+      author="Matthew Bennett",
+      author_email="matthew.bennett@bostintechnology.com",
+      url="http://www.BostinTechnology.com",
+      install_requires=["WiringPi2", "Boto3"],
+      )
+
+
+#      py_modules = { "": "RFIDSender/*.py"},
+#      package_data = { "": "/RFIDSender/*.py"},
+
+#      packages="RFIDSender",
