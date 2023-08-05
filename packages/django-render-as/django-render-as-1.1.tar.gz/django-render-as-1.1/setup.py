a47c9993d55b117430b9e94928f416828ee8f48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package django-render-as/1.1/django-render-as-1.1.tar.gz

---
+++ packages/django-render-as/django-render-as-1.1.tar.gz/django-render-as-1.1/setup.py
@@ -0,0 +1,38 @@
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+PACKAGE = 'django-render-as'
+VERSION = '1.1'
+
+package_data = {
+    'render_as': [
+        'templates/avoid_clash_with_real_app/*.html',
+        'templates/render_as/*.html',
+        ],
+}
+
+setup(
+    name=PACKAGE, version=VERSION,
+    description="Template rendering indirector based on object class",
+    packages=[
+        'render_as',
+        'render_as/templatetags',
+        ],
+    package_data=package_data,
+    license='MIT',
+    author='James Aylett',
+    author_email='james@tartarus.org',
+    install_requires=[
+        'Django~=1.8',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
