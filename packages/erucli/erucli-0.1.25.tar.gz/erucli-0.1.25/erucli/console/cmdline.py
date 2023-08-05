f4abe4b668ba48d900de7293500e97f1728c583e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package erucli/0.1.25/erucli-0.1.25.tar.gz

---
+++ packages/erucli/erucli-0.1.25.tar.gz/erucli-0.1.25/erucli/console/cmdline.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+import os
+import click
+import yaml
+import pygit2
+
+from eruhttp import EruClient
+from erucli.console.style import error
+from erucli.console.commands import commands
+
+@click.group()
+@click.pass_context
+def eru_commands(ctx):
+    if not os.path.exists(os.path.abspath('./.git')):
+        click.echo(error('Must run inside git dir'))
+        ctx.exit(-1)
+    if not os.path.exists(os.path.abspath('./app.yaml')):
+        click.echo(error('Need app.yaml in repository'))
+        ctx.exit(-1)
+    eru_url = os.getenv('ERU_URL')
+    if not eru_url:
+        click.echo(error('Need ERU_URL set in env'))
+        ctx.exit(-1)
+
+    with open(os.path.abspath('./app.yaml')) as f:
+        appconfig = yaml.load(f)
+        ctx.obj['appconfig'] = appconfig
+        ctx.obj['appname'] = appconfig['appname']
+
+    repo = pygit2.Repository('.')
+    # 有时候app.yaml里会写version的, 就以他为主了
+    ctx.obj['sha1'] = appconfig.get('version', '') or repo.head.target.hex
+    ctx.obj['short_sha1'] = appconfig.get('version', '')[:7] or ctx.obj['sha1'][:7]
+
+    remote = ''
+    for r in repo.remotes:
+        if r.name == 'origin':
+            remote = r.url
+            if not remote.startswith('http'):
+                _, path = remote.split('@', 1)
+                # 太丢人了...
+                remote = 'http://' + path.replace(':', '/')
+    ctx.obj['remote'] = remote
+
+    ctx.obj['eru'] = EruClient(eru_url)
+
+for command, function in commands.iteritems():
+    eru_commands.command(command)(function)
+
+def main():
+    eru_commands(obj={})
+
---
+++ packages/erucli/erucli-0.1.25.tar.gz/erucli-0.1.25/erucli/console/commands.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+from erucli.console.app import (
+    register_app_version,
+    set_app_env,
+    delete_app_env,
+    list_app_env_content,
+    list_app_containers,
+    list_app_versions,
+    list_app_env_names,
+    deploy_private_container,
+    deploy_public_container,
+    build_image,
+    build_log,
+    remove_containers,
+    offline_version,
+    container_log,
+    bind_container_network,
+)
+from erucli.console.entity import (
+    create_group,
+    create_pod,
+    create_network,
+    create_host,
+    assign_pod_to_group,
+    assign_host_to_group,
+)
+
+commands = {
+    'app:register': register_app_version,
+    'app:setenv': set_app_env,
+    'app:getenv': list_app_env_content,
+    'app:delenv': delete_app_env,
+    'app:listenv': list_app_env_names,
+    'app:dpri': deploy_private_container,
+    'app:dpub': deploy_public_container,
+    'app:build': build_image,
+    'app:listcontainer': list_app_containers,
+    'app:listversion': list_app_versions,
+    'app:rmcontainer': remove_containers,
+    'app:offline': offline_version,
+
+    'group:create': create_group,
+    'pod:create':create_pod,
+    'pod:assign': assign_pod_to_group,
+    'host:create': create_host,
+    'host:assign': assign_host_to_group,
+
+    'log:task': build_log,
+    'log:container': container_log,
+
+    'net:create': create_network,
+    'net:bind': bind_container_network,
+}
+
