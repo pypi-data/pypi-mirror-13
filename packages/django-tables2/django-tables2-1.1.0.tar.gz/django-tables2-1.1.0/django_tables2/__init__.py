75284f02588b79ffe2a968211baaa268008d9607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:51 +0000

    Add package django-tables2/1.1.0/django-tables2-1.1.0.tar.gz

---
+++ packages/django-tables2/django-tables2-1.1.0.tar.gz/django-tables2-1.1.0/django_tables2/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# pylint: disable=W0611
+from .tables import Table
+from .columns import (BooleanColumn, Column, CheckBoxColumn, DateColumn,
+                      DateTimeColumn, EmailColumn, FileColumn, LinkColumn,
+                      TemplateColumn, URLColumn, TimeColumn)
+from .config import RequestConfig
+from .utils import A
+from .views import SingleTableMixin, SingleTableView
+
+
+__version__ = "1.1.0"
