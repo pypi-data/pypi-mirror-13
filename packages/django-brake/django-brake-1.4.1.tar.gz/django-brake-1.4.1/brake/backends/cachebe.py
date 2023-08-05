073cd980ef924f14e8038f2038c7c56bc3679825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package django-brake/1.4.1/django-brake-1.4.1.tar.gz

---
+++ packages/django-brake/django-brake-1.4.1.tar.gz/django-brake-1.4.1/brake/backends/cachebe.py
@@ -0,0 +1,100 @@
+import hashlib
+
+from django.core.cache import cache
+from django.core.cache.backends.base import BaseCache
+
+from brake.backends import BaseBackend
+
+
+CACHE_PREFIX = 'rl:'
+BASE_CACHE = BaseCache({})
+IP_PREFIX = 'ip:'
+KEY_TEMPLATE = 'func:%s:%s%s:%s%s'
+PERIOD_PREFIX = 'period:'
+
+
+class CacheBackend(BaseBackend):
+
+    def get_ip(self, request):
+        """This gets the IP we wish to use for ratelimiting.
+
+        It defaults to 'REMOTE_ADDR'. It's recommended that you override
+        this function if you're using loadbalancers or any kind of upstream
+        proxy service to route requests to Django.
+        """
+        return request.META['REMOTE_ADDR']
+
+    def _keys(self, func_name, request, ip=True, field=None, period=None):
+        keys = []
+        if ip:
+            keys.append(KEY_TEMPLATE % (
+                func_name, PERIOD_PREFIX, period,
+                IP_PREFIX, self.get_ip(request)
+            ))
+
+        if field is not None:
+            if not isinstance(field, (list, tuple)):
+                field = [field]
+            for f in field:
+                val = getattr(request, request.method).get(f)
+                # Convert value to hexdigest as cache backend doesn't allow
+                # certain characters
+                if val:
+                    val = hashlib.sha1(val.encode('utf-8')).hexdigest()
+                    keys.append('func:%s:%s%s:field:%s:%s' % (
+                        func_name, PERIOD_PREFIX, period, f, val
+                    ))
+
+        return [
+            BASE_CACHE.make_key(CACHE_PREFIX + k) for k in keys
+        ]
+
+    def count(self, func_name, request, ip=True, field=None, period=60):
+        """Increment counters for all relevant cache_keys given a request."""
+        cache_keys = self._keys(func_name, request, ip, field, period)
+        counters = dict((key, 1) for key in cache_keys)
+        counters_read = cache.get_many(cache_keys)
+        for key, value in counters_read.items():
+            #Handle old values.
+            if isinstance(value, tuple):
+                count, _ = value # Futureproofing for expiration values.
+            else:
+                count = value
+
+            count += 1
+
+            # These changes come from:
+            # https://github.com/gmcquillan/django-brake/pull/21
+            # However, to future proof them, we accept the new values, but
+            # continue to write the old-style values as part of an upgrade path.
+            counters[key] = count
+
+        cache.set_many(counters, timeout=period)
+
+    def limit(self, func_name, request,
+            ip=True, field=None, count=5, period=None):
+        """Return limit data about any keys relevant for requst."""
+        counters = cache.get_many(
+            self._keys(func_name, request, ip, field, period)
+        )
+
+        limits = []
+        for counter in counters:
+            ratelimited_by = 'field'
+            if ':ip:' in counter:
+                ratelimited_by = 'ip'
+
+            current_count = counters[counter]
+            if isinstance(current_count, tuple):
+                current_count = current_count[0]
+            if current_count > count:
+                limits.append({
+                    'ratelimited_by': ratelimited_by,
+                    'period': period,
+                    'field': field,
+                    'count': current_count,
+                    'cache_key': counter,
+                    'ip': self.get_ip(request)
+                })
+
+        return limits
