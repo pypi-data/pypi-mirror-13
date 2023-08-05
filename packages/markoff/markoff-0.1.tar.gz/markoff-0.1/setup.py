579f7e0c71de117dfde63280f22676d36c06c68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package markoff/0.1/markoff-0.1.tar.gz

---
+++ packages/markoff/markoff-0.1.tar.gz/markoff-0.1/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A simple phrase generating class using Markov chains',
+    'author': 'Kyle J. Kneitinger',
+    'url': 'https://github.com/kneitinger/markoff',
+    'author_email': 'kneit@pdx.edu',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['markoff','tests'],
+    'scripts': [],
+    'name': 'markoff',
+    'license' : 'BSD',
+    'classifiers' : [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Linguistic'
+    ]
+}
+
+setup(**config)
