0a105635e19ff2a2a459629bd495e72508ce8624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package pyoni/0.5/pyoni-0.5.tar.gz

---
+++ packages/pyoni/pyoni-0.5.tar.gz/pyoni-0.5/src/onitool/toolfix.py
@@ -0,0 +1,55 @@
+from . import onifile as oni
+import struct
+
+def fix(args,action,a):
+    r = oni.Patcher(a)
+    last = None
+    prelast = None	
+    mid = -1
+    needclose = False
+    print "fileheader",r.h0
+    while True:
+        h = r.next()
+        if h is None:
+            break
+        elif h["rt"] == oni.RECORD_NEW_DATA:
+            break
+        if mid < h["nid"]:
+            mid = h["nid"]
+        prelast = last
+        last = h
+        if h["rt"] == oni.RECORD_NEW_DATA:
+            hh = oni.parsedatahead(a,h)
+            q = r.stats[h["nid"]]
+            q.addframe(h,hh)
+           
+    if True:
+        print "prelast",prelast
+        print "last",last
+        print "maxnodeid",mid
+
+    if not last["nextheader"] <= filesize:
+        print "FIXER last ends at",last["nextheader"],"with file size",filesize,"delta:",last["nextheader"]-filesize," we need truncate at pre last",prelast["afteroffset"]
+        if action == "fixcut":
+            a.truncate(prelast["nextheader"])
+        last = prelast
+        needclose = True
+
+    if last["rt"] != oni.RECORD_END:
+        print "FIXER missing RECORD_END. Last Record is:",last["rt"],"appending"
+        if action == "fixcut":
+            a.seek(0,2)
+            oni.writeend(a)
+            needclose = True
+
+    if h0["maxnid"] != mid:
+        print "FIXER bad maxnid",h0["maxnid"],"vs",mid,"fixing"
+        h0["maxnid"] = mid
+        if action == "fixcut":
+            a.seek(0,0)
+            oni.writehead1(a,h0)
+            needclose = True
+    else:
+        print "header ok, no changes"
+    if needclose:
+    	a.finalize()
---
+++ packages/pyoni/pyoni-0.5.tar.gz/pyoni-0.5/src/onitool/toolinfo.py
@@ -0,0 +1,86 @@
+from . import onifile as oni
+import struct
+
+def info(args,a):
+    r = oni.Reader(a)
+    print "fileheader",r.h0
+    while True:
+        h = r.next()
+        if h is None:
+            break
+        elif h["rt"] == oni.RECORD_NEW_DATA:
+            break
+    for k,v in r.streams.iteritems():
+        print k,v
+
+def seeks(args,a):
+    """prints seeks"""
+    r = oni.Reader(a)
+    while True:
+        h = r.next()
+        if h is None:
+            break
+        if h["rt"] == oni.RECORD_SEEK_TABLE:
+            q = oni.parseseek(a,h)
+            for qq in q["data"]:
+                print qq
+
+def times(args,a):
+    """prints times info"""
+    r = oni.Reader(a)
+    while True:
+        h = r.next()
+        if h is None:
+            break
+        if h["rt"] == oni.RECORD_NEW_DATA:
+            hh = oni.parsedatahead(a,h)
+            print h["nid"],hh["frameid"],hh["timestamp"]
+
+def dump(args,a):
+    r = oni.Reader(a)
+    last = None
+    # scan all and keep pre and last
+    mid = -1
+    print "fileheader",r.h0
+    while True:
+        h = r.next()
+        pt = a.tell()
+        if h is None:
+            break
+        prelast = last
+        last = h
+        if mid < h["nid"]:
+            mid = h["nid"]
+        if h["rt"] == oni.RECORD_NEW_DATA:
+            print "newdata",h["nid"],oni.parsedatahead(a,h),h["ps"]
+        elif h["rt"] == oni.RECORD_NODE_DATA_BEGIN:
+            print "nodebegin",h["nid"],h
+        elif h["rt"] == oni.RECORD_NODE_ADDED:
+            print "nodeadded",h["nid"],h,oni.parseadded(a,h)
+        elif h["rt"] == oni.RECORD_INT_PROPERTY:
+            print "intprop",h["nid"],oni.parseprop(a,h)
+        elif h["rt"] == oni.RECORD_GENERAL_PROPERTY:
+            pp = oni.parseprop(a,h)
+            print "genprop",h["nid"],pp
+            if pp["name"] == "xnMapOutputMode":
+                (xres,yres) = struct.unpack("ii",pp["data"])
+                data = dict(xres=xres,yres=yres)
+                print "\t",data
+        elif h["rt"] == oni.RECORD_NODE_STATE_READY:
+            print "ready",h
+        elif h["rt"] == oni.RECORD_NODE_REMOVED:
+            print "removed",h
+        elif h["rt"] == oni.RECORD_SEEK_TABLE:
+            print "seektable",h
+        elif h["rt"] == oni.RECORD_END:
+            print "end",h
+        else:
+            print h["rt"]
+    if True:
+        print "blocks stats:"
+        print "\tprelast",prelast
+        print "\tlast",last
+        print "\tmaxnodeid",mid   
+    print "streams stats:"
+    for k,v in r.streams.iteritems():
+        print "\t",k,v
