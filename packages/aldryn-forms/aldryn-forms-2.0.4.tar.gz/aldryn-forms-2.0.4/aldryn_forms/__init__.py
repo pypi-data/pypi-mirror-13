02d01a5ec0ec861a2a5390b1892ee34477627e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:23 +0000

    Add package aldryn-forms/2.0.4/aldryn-forms-2.0.4.tar.gz

---
+++ packages/aldryn-forms/aldryn-forms-2.0.4.tar.gz/aldryn-forms-2.0.4/aldryn_forms/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+__version__ = '2.0.4'
+
+warnings.warn(
+    'The "aldryn_forms.FormData" model has been deprecated in '
+    'favor of aldryn_forms.FormSubmission',
+    DeprecationWarning
+)
