e1614b110cdd2dc53e9fc23b2532f69ff6c57dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package gateway-outage-responder/1.1/gateway-outage-responder-1.1.tar.gz

---
+++ packages/gateway-outage-responder/gateway-outage-responder-1.1.tar.gz/gateway-outage-responder-1.1/gateway_outage_responder/main.py
@@ -0,0 +1,81 @@
+import argparse
+import logging
+import subprocess
+import time
+import urllib2
+
+
+logger = logging.getLogger(__name__)
+
+
+def url_is_accessible(url):
+    try:
+        urllib2.urlopen(url, timeout=5)
+        return True
+    except urllib2.URLError:
+        pass
+    return False
+
+
+def unplug_cable_modem():
+    logger.debug('Turning off cable modem outlet...')
+    subprocess.call([
+        'stanley-outlet-control',
+        '1',
+        'off'
+    ])
+
+
+def plug_in_cable_modem():
+    logger.debug('Turning on cable modem outlet...')
+    subprocess.call([
+        'stanley-outlet-control',
+        '1',
+        'on'
+    ])
+
+
+def unplug_router():
+    logger.debug('Turning off router outlet...')
+    subprocess.call([
+        'stanley-outlet-control',
+        '2',
+        'off'
+    ])
+
+
+def plug_in_router():
+    logger.debug('Turning on router outlet...')
+    subprocess.call([
+        'stanley-outlet-control',
+        '2',
+        'on'
+    ])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logfile')
+    parser.add_argument('--url', default='http://google.com')
+
+    args = parser.parse_args()
+
+    logging_kwargs = {
+        'level': logging.DEBUG,
+        'format': '%(asctime)s [%(levelname)s] %(message)s'
+    }
+    if args.logfile:
+        logging_kwargs['filename'] = args.logfile
+
+    logging.basicConfig(**logging_kwargs)
+
+    if not url_is_accessible(args.url):
+        logger.warning('Internet appears to be offline.')
+        unplug_cable_modem()
+        unplug_router()
+
+        plug_in_router()
+        time.sleep(60)
+        plug_in_cable_modem()
+    else:
+        logger.info('Internet appears to be accessible.')
---
+++ packages/gateway-outage-responder/gateway-outage-responder-1.1.tar.gz/gateway-outage-responder-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gateway-outage-responder',
+    version='1.1',
+    url='https://github.com/coddingtonbear/gateway-outage-responder',  # noqa
+    description=(
+        'If it looks like the internet is out; do something!'
+    ),
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    requirements=[
+        'stanley-outlet-control',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gateway-outage-responder = gateway_outage_responder.main:main'
+        ],
+    },
+)
