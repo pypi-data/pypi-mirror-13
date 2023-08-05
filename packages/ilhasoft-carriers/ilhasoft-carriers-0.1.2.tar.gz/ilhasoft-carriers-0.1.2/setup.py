9131303b61a2a673b8a8b0eaae252547caa63889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package ilhasoft-carriers/0.1.2/ilhasoft-carriers-0.1.2.tar.gz

---
+++ packages/ilhasoft-carriers/ilhasoft-carriers-0.1.2.tar.gz/ilhasoft-carriers-0.1.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+CLASSIFIERS = [
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development',
+]
+
+setup(
+    name='ilhasoft-carriers',
+    version='0.1.2',
+    author='Teeh Amaral - Ilhasoft',
+    author_email='ed.amaral@ilhasoft.com.br',
+    packages=[''],
+    url='https://github.com/Ilhasoft/carriers',
+    license='BSD licence, see LICENCE.txt',
+    description='Library consultation phone carriers around the world.',
+    long_description=open('README.rst').read(),
+    download_url="https://github.com/Ilhasoft/carriers/archive/master.zip",
+    keywords=['carriers', 'ilhasoft', 'mobile', 'ilhasoft-carriers'],
+    classifiers=CLASSIFIERS,
+)
