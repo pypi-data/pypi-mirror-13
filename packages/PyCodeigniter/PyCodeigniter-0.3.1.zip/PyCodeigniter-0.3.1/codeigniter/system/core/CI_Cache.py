35ce8ad292df24057fb01a924db56b0dfa941429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package PyCodeigniter/0.3.1/PyCodeigniter-0.3.1.zip

---
+++ packages/PyCodeigniter/PyCodeigniter-0.3.1.zip/PyCodeigniter-0.3.1/codeigniter/system/core/CI_Cache.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+__author__ = 'xiaozhang'
+
+
+# def Cache(func):
+#     def handle_args(*args, **kwargs):
+#         print "begin"
+#         return func(*args, **kwargs)
+#         print "end"
+#     return handle_args
+
+
+import re
+import time
+import json
+
+import CI_Application 
+
+try:
+    import thread
+except ImportError as e:
+    import _thread as thread
+
+
+class CI_Memory_Cache(object):
+    def __init__(self,**kwargs):
+        self.cache_dict={}
+        self.cache_conf=kwargs['cache']
+        self.app=kwargs['app']
+        try:
+            thread.start_new_thread(self.check,())
+        except Exception as e:
+            self.app.logger.error(e)
+    def set(self,key,value,ttl=3600):
+        self.put(key,value,ttl)
+
+    def put(self,key,value,ttl=3600):
+        self.cache_dict[key]={'t':int(time.time())+ttl,'v':value}
+    def get(self,key):
+        if key in self.cache_dict.keys():
+            obj=self.cache_dict[key]
+            if int(time.time())- obj['t']>0:
+                return None
+            else:
+                return obj['v']
+    def delete(self,key):
+        if key in self.cache_dict.keys():
+            del self.cache_dict[key]
+    def check(self):
+        while True:
+            try:
+                if len( self.cache_dict)>int(self.cache_conf['max_count']):
+                    self.cache_dict.clear()
+                else:
+                    pass
+                time.sleep(30)
+            except Exception as e:
+                self.app.logger.error(e)
+
+
+class CI_Cache(object):
+    REGEX_KEY=re.compile(r'\#p\[(\w+)\]([^,}]+)?',re.IGNORECASE)
+    def __init__(self,**kwargs):
+        # globals()['ci']=kwargs['app']
+        self.cache_conf=kwargs['cache']
+        if self.cache_conf['type']=='memory':
+            self.cache_instance=CI_Memory_Cache(**kwargs)
+        else:
+            self.cache_instance=None
+
+    def set_cache(self,cache):
+        self.cache_instance=cache
+
+
+    def __getattr__(self, item):
+        if hasattr(self.cache_instance,item):
+            return getattr(self.cache_instance,item)
+
+
+
+
+
+    @staticmethod
+    def get_cache_key(prefix,tpl,func,*args,**kwargs):
+        key=prefix+'$'+func.__name__+'$'
+        # match= re.findall(r'\#p\[(\d+)\]([^,}]+)?',tpl)
+        match=CI_Cache.REGEX_KEY.findall(tpl)
+        # match=[]
+        # return key
+        if len(match)<=len(kwargs):
+            for m in match:
+                if m[0] in kwargs.keys():
+                    if isinstance(kwargs[(m[0])],dict):
+                        if m[1]!='':
+                            key=key+str(kwargs[(m[0])][m[1][1:]])+'$'
+                        else:
+                            key=key+str(kwargs[(m[0])])+'$'
+                    else:
+                        key=key+ str(kwargs[(m[0])])+'$'
+                else:
+                    continue
+        return key
+    @staticmethod
+    def Cache(prefix='', ttl=3600,key='',op='select'):
+        def handle_func(func):
+            def handle_args(*args, **kwargs):
+                # print args,kwargs
+                ci=CI_Application.CI
+                if ci!=None:
+                    if  ci.cache.cache_instance==None:
+                        ci.logger.error('cache not implment,you can implment it and setting it')
+                        return func(*args, **kwargs)
+                    else:
+                        # op dispatch
+                        ckey=CI_Cache.get_cache_key(prefix,key,func,*args,**kwargs)
+                        if op=='select':
+                            obj=ci.cache.cache_instance.get(ckey)
+                            if obj==None:
+                                result=func(*args, **kwargs)
+                                cacheresult=CI_Cache.serial(result)
+                                ci.cache.cache_instance.set(ckey,cacheresult,ttl)
+                                return result
+                            else:
+                                obj= CI_Cache.unserial(obj)
+                                return obj
+                        elif op=='del' or op=='delete' or op=='remove':
+                            ci.cache.cache_instance.delete(ckey)
+                        elif op=='insert' or op=='update':
+                            result=func(*args, **kwargs)
+                            cacheresult=CI_Cache.serial(result)
+                            ci.cache.cache_instance.set(ckey,cacheresult,ttl)
+                            return result
+                else:
+                    return func(*args, **kwargs)
+            return handle_args
+        return handle_func
+
+    @staticmethod
+    def serial(obj):
+        if CI_Application.CI.config['cache']['type']=='memory':
+            return obj
+        cacheresult=obj
+        if isinstance(obj,dict) or isinstance(obj,list) or isinstance(obj,tuple):
+            cacheresult='___obj___:'+json.dumps(obj)
+        else:
+            cacheresult=json.dumps(obj)
+        return cacheresult
+
+    @staticmethod
+    def unserial(obj):
+        if CI_Application.CI.config['cache']['type']=='memory':
+            return obj
+        if str(obj).startswith('___obj___:'):
+            obj=str(obj)[len('___obj___:'):]
+        obj=json.loads(obj)
+        return obj
+
+
+
+
+@CI_Cache.Cache('abc',key='#p[0].index,#p[1],#p[2]')
+def abc(a,b,c):
+    print (a,b,c)
+
+
+
+
+if __name__=='__main__':
+    abc({'index':'adsdf'},2,3)
---
+++ packages/PyCodeigniter/PyCodeigniter-0.3.1.zip/PyCodeigniter-0.3.1/codeigniter/system/core/CI_Router.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+__author__ = 'xiaozhang'
+
+import json
+import re
+import time
+import logging
+from logging.handlers import RotatingFileHandler
+import datetime
+
+try:
+    import web,cgi,os
+except Exception as e:
+    pass
+
+
+class CI_Router(object):
+    def __init__(self,**kwargs):
+        self.app=kwargs['app']
+        self.config=self.app.config
+        self.access_log=logging.getLogger('access_log')
+        self.error_log=logging.getLogger('error_log')
+        self.access_log.setLevel(logging.DEBUG)
+        self.error_log.setLevel(logging.DEBUG)
+        if 'access_log' in self.config['server']:
+            self.access_log_file= self.config['server']['access_log']
+        else:
+            self.access_log_file='./access.log'
+        if 'error_log' in self.config['server']:
+            self.error_log_file= self.config['server']['error_log']
+        else:
+            self.error_log_file='./error.log'
+        self._methods={}
+        acchandler = RotatingFileHandler(filename=self.access_log_file, maxBytes=1024*1024*1024, backupCount=10)
+        errhandler = RotatingFileHandler(filename=self.access_log_file, maxBytes=1024*1024*1024, backupCount=10)
+        # format='%(asctime)s %(message)s'
+        # acchandler.setFormatter(format)
+        # errhandler.setFormatter(format)
+        self.access_log.addHandler(acchandler)
+        self.error_log.addHandler(errhandler)
+
+
+    def get_func(self,ctrl,func):
+        funclist=dir(ctrl)
+        for f in funclist:
+            if(f.startswith('_')):
+                continue
+            else:
+                if f.lower()==func.lower():
+                    return f
+        return None
+
+
+    def route(self,ctrl,func,data={},request=None):
+        try:
+            ctrl_instance=self.app.loader.ctrl(ctrl)
+            f=self.get_func(ctrl_instance,func)
+            if f!=None:
+                func=f
+            if not hasattr(ctrl_instance,func) or str(func).startswith('_'):
+                 return "404 Not Found", "Not Found"
+
+            # if request!=None:
+                # if 'req' not in data.keys():
+                #     data['req']=request
+                # else:
+                #     data['__request__']=request
+
+            return '200 OK',eval('self.app.loader.ctrl(ctrl).'+func+'(**data)')
+        except TypeError as e:
+            self.app.logger.error('when call controller %s function %s error,%s'%(ctrl,func,str(e)))
+            return '200 OK',{'message':str(e),'code':500}
+        except Exception as e:
+            self.app.logger.error('when call controller %s function %s error,%s'%(ctrl,func,str(e)))
+            return  "500 Internal server error","Server Error,Please see log file"
+
+
+
+    def _log(self,env,code,stime):
+        etime=time.time()
+        etime=etime-stime
+        etime="%0.6f" % etime
+        now = datetime.datetime.now()
+        dt=now.strftime('%Y-%m-%d %H:%M:%S')
+        method=env['REQUEST_METHOD']
+        addr=env['REMOTE_ADDR']
+        path=env['PATH_INFO']
+        protol=env['SERVER_PROTOCOL']
+        message="%s - - [%s] \"%s %s %s\" %s %s" % (addr,dt,method,path,protol,str(code),etime)
+        self.access_log.info(message)
+
+
+
+    def wsgi_route(self,env):
+        stime=time.time()
+        data=self.app.input.parse(env)
+        try:
+            if 'session' in self.config.keys():
+                self.app.cookie.parse_cookie(env)
+                self.app.session.pre_parse_uuid()
+            if '__ctrl_name__' in data.keys():
+                ctrl=data['__ctrl_name__']
+                del data['__ctrl_name__']
+            if '__func_name__' in data.keys():
+                func=data['__func_name__']
+                del data['__func_name__']
+            ctrl_instance=self.app.loader.ctrl(ctrl)
+            #print(dir(ctrl_instance))
+            f=self.get_func(ctrl_instance,func)
+            if f!=None:
+                func=f
+            if not hasattr(ctrl_instance,func) or str(func).startswith('_'):
+                 return "404 Not Found", "Not Found"
+        except Exception as err:
+            self._log(env,404,stime)
+            return "404 Not Found", "Not Found"
+        try:
+            ret=eval('self.app.loader.ctrl(ctrl).'+func+'(**data)')
+            self._log(env,200,stime)
+            return '200 OK',ret
+        except TypeError as e:
+            self.app.logger.error('when call controller %s function %s error,%s'%(ctrl,func,str(e)))
+            self._log(env,500,stime)
+            return '200 OK',{'message':str(e),'code':500}
+        except Exception as e:
+            self._log(env,500,stime)
+            self.app.logger.error('when call controller %s function %s error,%s'%(ctrl,func,str(e)))
+            return  "500 Internal server error","Server Error,Please see log file"
+        finally:
+            if 'session' in self.config.keys():
+                self.app.session.release()
+            
+
+
+    def webpy_route(self):
+        try:
+            code,obj= self.wsgi_route(web.ctx['environ'])
+            if isinstance(obj,str):
+                return obj
+            else:
+                return str(json.dumps(obj))
+        except Exception as e:
+            self.app.logger.error(e)
+            return "Server Error"
+
+
+
+
+
+
+
+if __name__=='__main__':
+    r=CI_Router()
+
+
