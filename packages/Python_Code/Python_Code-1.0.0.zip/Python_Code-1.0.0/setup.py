16e20b7634a5f28a9423c3476f8ca5e047c61d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:41 +0000

    Add package Python_Code/1.0.0/Python_Code-1.0.0.zip

---
+++ packages/Python_Code/Python_Code-1.0.0.zip/Python_Code-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+       name   =  'Python_Code',
+       version=  '1.0.0',
+       py_modules = ['nester'],
+       author   = 'CJM',
+       author_email='jmcui10@fudan.edu.cn',
+       description ='A simple printer of nested lists'
+       )
