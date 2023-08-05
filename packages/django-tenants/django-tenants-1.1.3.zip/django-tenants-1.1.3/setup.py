7f43700e553f94a40b79a81b304241b53509c31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:48 +0000

    Add package django-tenants/1.1.3/django-tenants-1.1.3.zip

---
+++ packages/django-tenants/django-tenants-1.1.3.zip/django-tenants-1.1.3/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from version import get_git_version
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = "1.1.2"
+
+setup(
+    name='django-tenants',
+    version=get_git_version(),
+    author='Thomas Turner',
+    author_email='tom@twt.me.uk',
+    packages=[
+        'django_tenants',
+        'django_tenants.postgresql_backend',
+        'django_tenants.management',
+        'django_tenants.management.commands',
+        'django_tenants.migration_executors',
+        'django_tenants.templatetags',
+        'django_tenants.test',
+        'django_tenants.tests',
+    ],
+    scripts=[],
+    url='https://github.com/tomturner/django-tenants',
+    license='MIT',
+    description='Tenant support for Django using PostgreSQL schemas.',
+    long_description=open('README.rst').read() if exists("README.rst") else "",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+    ],
+    install_requires=[
+        'Django >= 1.8.0',
+        'psycopg2',
+    ],
+    zip_safe=False,
+)
