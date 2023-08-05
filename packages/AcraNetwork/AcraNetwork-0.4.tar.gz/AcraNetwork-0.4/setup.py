a430c59d691149d0e7ea7601d7df5102dc94c3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package AcraNetwork/0.4/AcraNetwork-0.4.tar.gz

---
+++ packages/AcraNetwork/AcraNetwork-0.4.tar.gz/AcraNetwork-0.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AcraNetwork',
+      version='0.4',
+      description='Classes and utilities to support Flight Test Instrumentation Ethernet networks',
+      author='Diarmuid Collins',
+      author_email='dcollins@curtisswright.com',
+      url='https://github.com/diarmuidcwc/AcraNetwork',
+      packages=['AcraNetwork'],
+      long_description=open('README', 'rt').read(),
+      classifiers =['Programming Language :: Python',
+                    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                    'Operating System :: OS Independent',
+                    'Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Libraries :: Python Modules',
+                    ],
+     )
