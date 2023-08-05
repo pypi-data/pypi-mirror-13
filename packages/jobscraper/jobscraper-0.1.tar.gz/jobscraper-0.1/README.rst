aa0db2c1cf678544b0a43ddb9b2b96d33c8eafcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:53 +0000

    Add package jobscraper/0.1/jobscraper-0.1.tar.gz

---
+++ packages/jobscraper/jobscraper-0.1.tar.gz/jobscraper-0.1/README.rst
@@ -0,0 +1,14 @@
+jobscraper
+--------
+	>>> pip install jobscraper
+
+
+To use, simply do::
+
+    >>> from jobscraper import scrape
+    >>> jobs = scrape('python')
+
+
+'jobs' is a dictionary of jobs with keys (1,2,........10) top 10 jobs.
+
+	>>> jobs['1']
---
+++ packages/jobscraper/jobscraper-0.1.tar.gz/jobscraper-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='jobscraper',
+    version='0.1',
+    description='Grab all the matching jobs from job portals',
+    long_description=readme(),
+    url='https://github.com/sijanonly/jobscraper',
+    download_url='https://github.com/sijanonly/jobscraper/tarball/master',
+    author='Sijan Bhandari',
+    author_email='sijanonly@gmail.com',
+    license='MIT',
+    packages=['jobscraper'],
+    keywords=['jobs', 'freelance'],
+    install_requires=[
+        'beautifulsoup4==4.4.1',
+        'requests==2.9.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
