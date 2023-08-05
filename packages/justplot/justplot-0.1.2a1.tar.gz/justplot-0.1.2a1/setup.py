8d26a048b01c248814b2ba0aeafbfecb5259f674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:22 +0000

    Add package justplot/0.1.2a1/justplot-0.1.2a1.tar.gz

---
+++ packages/justplot/justplot-0.1.2a1.tar.gz/justplot-0.1.2a1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='justplot',
+    version='0.1.2a1',
+    packages=['justplot',],
+    license='MIT',
+    author='Uğur Çayoğlu',
+    author_email='urcyglu@gmail.com',
+    classifiers=[
+        # More at http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+      ],
+    keywords=['plots','matplotlib','wrapper']
+)
+
