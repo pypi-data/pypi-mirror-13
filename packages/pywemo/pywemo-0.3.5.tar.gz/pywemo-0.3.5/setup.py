0e528192c6f88673e334b7d44c9f127c207e44d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package pywemo/0.3.5/pywemo-0.3.5.tar.gz

---
+++ packages/pywemo/pywemo-0.3.5.tar.gz/pywemo-0.3.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='pywemo',
+      version='0.3.5',
+      description='Access WeMo switches using their SOAP API',
+      url='http://github.com/balloob/pywemo',
+      author='Paulus Schoutsen',
+      author_email='Paulus@PaulusSchoutsen.nl',
+      license='MIT',
+      install_requires=['requests>=2.0'],
+      packages=find_packages(),
+      zip_safe=True)
