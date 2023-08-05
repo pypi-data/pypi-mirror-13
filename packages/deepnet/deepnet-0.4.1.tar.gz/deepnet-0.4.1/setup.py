55ab3fe603f14a9e392226e25312824f77d9ce4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:49 +0000

    Add package deepnet/0.4.1/deepnet-0.4.1.tar.gz

---
+++ packages/deepnet/deepnet-0.4.1.tar.gz/deepnet-0.4.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='deepnet',
+      version='0.4.1',
+      description='package for deep learning',
+      url='https://github.com/leovetter/DeepNet',
+      author='Léo Vetter',
+      author_email='vetter.leo@gmail.com',
+      packages=['deepnet'],
+      zip_safe=False)
