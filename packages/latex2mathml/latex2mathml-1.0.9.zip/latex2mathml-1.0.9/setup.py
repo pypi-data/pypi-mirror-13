6ac445dc8881c8c3b5cbc5f624d531b9006d67fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:49 +0000

    Add package latex2mathml/1.0.9/latex2mathml-1.0.9.zip

---
+++ packages/latex2mathml/latex2mathml-1.0.9.zip/latex2mathml-1.0.9/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+VERSION = open('VERSION').read()
+
+setup(
+        name='latex2mathml',
+        version=VERSION,
+        packages=['latex2mathml'],
+        url='https://github.com/Code-ReaQtor/latex2mathml',
+        download_url='https://github.com/Code-ReaQtor/latex2mathml/tarball/{}'.format(VERSION),
+        license='MIT',
+        author='Ronie Martinez',
+        author_email='ronmarti18@gmail.com',
+        description='Pure Python library for LaTeX to MathML conversion.',
+        long_description=open('README').read(),
+        keywords=[],
+        classifiers=['Development Status :: 3 - Alpha',
+                     'License :: OSI Approved :: MIT License',
+                     'Topic :: Software Development :: Libraries :: Python Modules',
+                     'Programming Language :: Python',
+                     'Topic :: Scientific/Engineering :: Mathematics',
+                     'Topic :: Text Processing :: Markup :: HTML',
+                     'Topic :: Text Processing :: Markup :: LaTeX'],
+        package_data={'latex2mathml': ['unimathsymbols.txt']}
+)
