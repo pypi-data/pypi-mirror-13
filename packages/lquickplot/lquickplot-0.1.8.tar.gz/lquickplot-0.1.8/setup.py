15e2a1b0a1acc44df4673ca23163a31dcad679dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:28 +0000

    Add package lquickplot/0.1.8/lquickplot-0.1.8.tar.gz

---
+++ packages/lquickplot/lquickplot-0.1.8.tar.gz/lquickplot-0.1.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'lquickplot',
+        version = '0.1.8',
+        keywords = ('simple', 'test'),
+        description = 'just a simple test',
+        license = 'MIT License',
+        author = 'ap9035',
+        author_email = 'ap9035@gmail.com',
+
+        scripts = ['quickplot.py'],
+
+        platforms = 'any',
+)
