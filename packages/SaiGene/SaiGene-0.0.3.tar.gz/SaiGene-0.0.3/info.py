2c8738801a14dccd7ae63afcf4de1e1857442bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package SaiGene/0.0.3/SaiGene-0.0.3.tar.gz

---
+++ packages/SaiGene/SaiGene-0.0.3.tar.gz/SaiGene-0.0.3/info.py
@@ -0,0 +1,14 @@
+# package information.
+INFO = dict(
+  name = "saigene",
+  description = "The calculator fot cat's age",
+  author = "shinriyo",
+  author_email = "shinriyo@gmail.com",
+  license = "MIT License",
+  url = "https://github.com/shinriyo/saigene",
+  keywords = ['saigene', '17sai', 'shinriyo'], # arbitrary keywords
+  classifiers = [
+    "Programming Language :: Python :: 2.7",
+    "License :: OSI Approved :: MIT License"
+  ]
+)
