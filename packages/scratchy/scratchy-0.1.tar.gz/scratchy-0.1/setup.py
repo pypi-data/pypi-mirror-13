8aceeeacde4c13ed63532b9963faf872b6cf89ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package scratchy/0.1/scratchy-0.1.tar.gz

---
+++ packages/scratchy/scratchy-0.1.tar.gz/scratchy-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='scratchy', 
+      version='0.1',
+      description='A static site generator, aimed for blogging',
+      url='http://github.com/rizaldim/scratchy',
+      author='Rizaldi Martaputra',
+      author_email='rizaldi.m@gmail.com',
+      license='MIT',
+      packages=['scratchy'],
+      zip_safe=False)
+
