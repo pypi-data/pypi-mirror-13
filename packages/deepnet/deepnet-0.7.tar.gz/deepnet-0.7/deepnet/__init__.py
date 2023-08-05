a529a12450d0c12f3b4482231324a59ca022c190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package deepnet/0.7/deepnet-0.7.tar.gz

---
+++ packages/deepnet/deepnet-0.7.tar.gz/deepnet-0.7/deepnet/__init__.py
@@ -0,0 +1,4 @@
+from .layers import FullyConnectedLayer, SoftmaxLayer, ConvLayer, PoolLayer
+from .net import Network
+from .models import ConvNet, MLP
+from .visualization import init_learning_curves, update_learning_curves
