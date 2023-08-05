2b3ab360f6699e72637b3f3ea582adc07d7c1344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:01 +0000

    Add package pyexcel-ods/0.1.0/pyexcel-ods-0.1.0.tar.gz

---
+++ packages/pyexcel-ods/pyexcel-ods-0.1.0.tar.gz/pyexcel-ods-0.1.0/setup.py
@@ -0,0 +1,51 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+import sys
+
+with open("README.rst", 'r') as readme:
+    README_txt = readme.read()
+
+with open("VERSION", "r") as version:
+    version_txt = version.read().rstrip()
+
+dependencies = [
+    'odfpy==0.9.6',
+    'pyexcel-io>=0.1.0'
+    ]
+
+with open("VERSION", "r") as version:
+    version_txt = version.read().rstrip()
+
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    dependencies.append('ordereddict')
+
+setup(
+    name='pyexcel-ods',
+    author="C. W.",
+    version=version_txt,
+    author_email="wangc_2011@hotmail.com",
+    url="https://github.com/chfw/pyexcel-ods",
+    description='A wrapper library to read, manipulate and write data in ods format',
+    install_requires=dependencies,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    long_description=README_txt,
+    zip_safe=False,
+    license='New BSD License',
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Office/Business',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
