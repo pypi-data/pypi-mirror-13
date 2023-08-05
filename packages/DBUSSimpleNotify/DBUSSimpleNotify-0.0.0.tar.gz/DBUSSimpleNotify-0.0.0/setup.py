a142656693da8a46193c20997ddb75dab12fa8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:27 +0000

    Add package DBUSSimpleNotify/0.0.0/DBUSSimpleNotify-0.0.0.tar.gz

---
+++ packages/DBUSSimpleNotify/DBUSSimpleNotify-0.0.0.tar.gz/DBUSSimpleNotify-0.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='DBUSSimpleNotify',
+    author='Egil Hasting',
+    author_email='eh@higen.org',
+    packages=['dbussimplenotify'],
+    scripts=['dbussimplenotify/dbussimplenotify.py'],
+    url='https://github.com/ehasting/DBUSSimpleNotify',
+    download_url='https://github.com/ehasting/DBUSSimpleNotify',
+    keywords="DBUS notify",
+    license='MIT',
+    description='Dead simple notify class for DBUS.',
+    long_description=open('README.txt').read(),
+    platforms=['linux'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 ],
+
+)
