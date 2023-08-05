f012e66de214ee5888d5087317779975c94aaa56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package Google-Search-API/1.1.11/Google-Search-API-1.1.11.tar.gz

---
+++ packages/Google-Search-API/Google-Search-API-1.1.11.tar.gz/Google-Search-API-1.1.11/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("requirements.txt") as f:
+    requirements = [req.strip() for req in f.readlines()]
+
+test_requirements = [
+    "nose",
+    "nose-cov"
+]
+setup(name='Google-Search-API',
+      version='1.1.11',
+      url='https://github.com/abenassi/Google-Search-API',
+      description='Search in google',
+      author='Anthony Casagrande, Agustin Benassi',
+      author_email='birdapi@gmail.com, agusbenassi@gmail.com',
+      maintainer="Agustin Benassi",
+      maintainer_email='agusbenassi@gmail.com',
+      license='MIT',
+      packages=[
+          'google',
+          'google.modules',
+          'google.tests'
+      ],
+      package_dir={'google': 'google'},
+      include_package_data=True,
+      install_requires=requirements,
+      keywords="google search images api",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7'
+      ],
+      setup_requires=['nose>=1.0'],
+      test_suite='nose.collector',
+      tests_require=test_requirements
+      )
