5d405302544ff617bc8c5d7bb9d17c1afe4392d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package nbsphinx/0.2.0/nbsphinx-0.2.0.tar.gz

---
+++ packages/nbsphinx/nbsphinx-0.2.0.tar.gz/nbsphinx-0.2.0/doc/conf.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Use sphinx-quickstart to create your own conf.py file!
+# After that, you have to edit a few things.  See below.
+
+import os
+import sys
+
+# Add relative path to nbsphinx.py (if you didn't install it with pip):
+#sys.path.insert(0, os.path.abspath('.'))
+
+# Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# *** the following lines are (hopefully) temporary,
+# see https://github.com/sphinx-doc/sphinx/issues/2162
+source_suffix = ['.rst', '.ipynb']
+source_parsers = {'ipynb': 'nbsphinx.NotebookParser'}
+# *** end of temporary lines
+
+# -- The settings below this line are not specific to nbsphinx ------------
+
+master_doc = 'index'
+
+project = 'nbsphinx'
+author = 'Matthias Geier'
+copyright = '2015, ' + author
+
+# -- Get version information from Git -------------------------------------
+
+try:
+    from subprocess import check_output
+    release = check_output(['git', 'describe', '--tags', '--always'])
+    release = release.decode().strip()
+except Exception:
+    release = '<unknown>'
+
+# -- Options for HTML output ----------------------------------------------
+
+html_title = project + ' version ' + release
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'preamble': '\setcounter{tocdepth}{2}',
+}
+
+latex_documents = [
+    (master_doc, 'nbsphinx.tex', project, author, 'howto'),
+]
---
+++ packages/nbsphinx/nbsphinx-0.2.0.tar.gz/nbsphinx-0.2.0/doc/index.rst
@@ -0,0 +1,21 @@
+.. include:: ../README.rst
+
+All content shown below -- except for the section :doc:`rst` -- was
+generated from Jupyter notebooks.
+
+.. toctree::
+    :maxdepth: 2
+    :glob:
+
+    usage
+    example
+    pre-executed
+    allow-errors
+    hidden-cells
+    subdir/*
+    rst
+    links
+
+.. only:: html
+
+    There is also :doc:`orphan`, just for the sake of it.
