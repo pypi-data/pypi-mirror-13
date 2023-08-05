bab90c975cc05e72f0167d1b9241993603a332e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package papersizes/0.6.1/papersizes-0.6.1.tar.gz

---
+++ packages/papersizes/papersizes-0.6.1.tar.gz/papersizes-0.6.1/papersizes/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+A module containing commonly used paper sizes, for design and layout.
+"""
+
+__version_info__ = (0,6,1)
+__version__ = '.'.join([str(__value) for __value in __version_info__])
+__copyright__ = '2006-2015, Ian Millington'
+__license__ = 'MIT'
+
+from .papersizes import *
