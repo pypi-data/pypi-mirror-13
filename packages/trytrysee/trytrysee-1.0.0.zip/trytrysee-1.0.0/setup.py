901f68649152e942045b2baef9c33ab7d7b6aac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:37 +0000

    Add package trytrysee/1.0.0/trytrysee-1.0.0.zip

---
+++ packages/trytrysee/trytrysee-1.0.0.zip/trytrysee-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         ='trytrysee',
+       version      ='1.0.0',
+       py_modules   =['trytrysee'],
+       author       ='rick199006',
+       author_email ='rick199006@gmail.com',
+       url          ='http://www.headfirstlabs.com',
+       description  ='A simple printer of nested list',
+     )
