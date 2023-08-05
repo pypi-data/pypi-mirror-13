31672eef828512864e40191ad32691f32728be8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:39 +0000

    Add package c00bra-nester/1.4.0/c00bra-nester-1.4.0.tar.gz

---
+++ packages/c00bra-nester/c00bra-nester-1.4.0.tar.gz/c00bra-nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'c00bra-nester',
+	version			= '1.4.0',
+	py_modules		= ['nester'],
+	author 			= 'C00bra',
+	author_email	= 'posta.reduceri@gmail.com',
+	url				= 'http://www.headfirstlabs.com',
+	description		= 'A simple printer of nested lists',
+	)
