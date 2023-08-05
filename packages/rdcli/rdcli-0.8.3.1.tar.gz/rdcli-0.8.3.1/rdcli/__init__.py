0edf99ca973c9a5740cf52865f9c66314de435e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package rdcli/0.8.3.1/rdcli-0.8.3.1.tar.gz

---
+++ packages/rdcli/rdcli-0.8.3.1.tar.gz/rdcli-0.8.3.1/rdcli/__init__.py
@@ -0,0 +1,13 @@
+"""
+    Python library to use Real-Debrid services :
+    - login
+    - link unrestriction
+
+    Tested on python 2.7
+"""
+
+from RDWorker import RDWorker, RDError, UnrestrictionError, LoginError
+
+__all__ = ['RDWorker', 'RDError', 'UnrestrictionError', 'LoginError']
+
+__author__ = 'MrMitch'
