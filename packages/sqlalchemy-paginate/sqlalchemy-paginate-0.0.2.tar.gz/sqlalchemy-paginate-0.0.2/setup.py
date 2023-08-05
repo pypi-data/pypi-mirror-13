86a93cc62b648de1f68aa72aff437aa499c5b852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package sqlalchemy-paginate/0.0.2/sqlalchemy-paginate-0.0.2.tar.gz

---
+++ packages/sqlalchemy-paginate/sqlalchemy-paginate-0.0.2.tar.gz/sqlalchemy-paginate-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='sqlalchemy-paginate',
+        version='0.0.2',
+        packages=['sqlalchemy_paginate'],
+        zip_safe=False,
+        install_requires=['sqlalchemy>=1.0.11']
+)
