cf19c2adda9929ac62531d360574ecebae6e864e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:46 +0000

    Add package corgi/0.1.22/corgi-0.1.22.tar.gz

---
+++ packages/corgi/corgi-0.1.22.tar.gz/corgi-0.1.22/corgi/visualization.py
@@ -0,0 +1,20 @@
+def sql_hist(table, column, where=''):
+    """
+    Queries a table, selecting the column and count of the values in that column. Generates
+    a histogram plot of that data for display in a jupyter notebook.
+    """
+
+    # this depends on the type of table we're looking at, mysql, postgres, etc...
+    # might want to template these queries
+    sql = """
+    SELECT something FROM %(table)s
+    %(where)s
+    """
+    pass
+
+def sql_scatter(table, x_col, y_col, coloring_dimension=None, where=''):
+    """
+    Queries a table for data in x_col and y_col, then displays them (optionally colored by
+    coloring_dimension) onto a scatterplot.
+    """
+    pass
