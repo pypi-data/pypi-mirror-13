e25c7b13ea9b1fc0b31e68b42ba54af09b031334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:12 +0000

    Add package funniest_dnt17/0.1/funniest_dnt17-0.1.tar.gz

---
+++ packages/funniest_dnt17/funniest_dnt17-0.1.tar.gz/funniest_dnt17-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_dnt17',
+        version='0.1',
+        description='The funniest joke in the world',
+        url='https://github.com/dnt17/funniest_dnt17',
+        author='Devin Turner',
+        author_email='devinturner17@gmail.com',
+        license='MIT',
+        packages=['funniest_dnt17'],
+        zip_save=False)
