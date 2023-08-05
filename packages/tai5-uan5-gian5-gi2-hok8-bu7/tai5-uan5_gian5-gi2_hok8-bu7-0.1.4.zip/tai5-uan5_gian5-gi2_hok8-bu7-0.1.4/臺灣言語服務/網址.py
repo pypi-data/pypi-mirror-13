fb081684a6e8a5e7534f70eab06b5c6c7fc73100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package tai5-uan5-gian5-gi2-hok8-bu7/0.1.4/tai5-uan5_gian5-gi2_hok8-bu7-0.1.4.zip

---
+++ packages/tai5-uan5-gian5-gi2-hok8-bu7/tai5-uan5_gian5-gi2_hok8-bu7-0.1.4.zip/tai5-uan5_gian5-gi2_hok8-bu7-0.1.4/臺灣言語服務/網址.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+from 臺灣言語服務.服務 import 服務
+
+
+__服務 = 服務()
+urlpatterns = patterns(
+    '',
+    url(r'^正規化翻譯$', __服務.正規化翻譯),
+    url(r'^語音合成$', __服務.語音合成),
+)
