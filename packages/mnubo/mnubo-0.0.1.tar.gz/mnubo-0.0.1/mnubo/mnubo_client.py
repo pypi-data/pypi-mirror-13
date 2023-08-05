685a36b21a6245e21c765ee14264d2bdb90026dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:26 +0000

    Add package mnubo/0.0.1/mnubo-0.0.1.tar.gz

---
+++ packages/mnubo/mnubo-0.0.1.tar.gz/mnubo-0.0.1/mnubo/mnubo_client.py
@@ -0,0 +1,17 @@
+
+from services import EventServices
+from services import OwnerServices
+from services import ObjectServices
+from api_manager import APIManager
+
+
+class MnuboClient(object):
+    """ Initializes the mnubo client which contains the API manager as well as the available resource services
+    """
+
+    def __init__(self, client_id, client_secret, hostname):
+        self.__api_manager = APIManager(client_id, client_secret, hostname)
+        self.owner_services = OwnerServices(self.__api_manager)
+        self.event_services = EventServices(self.__api_manager)
+        self.smart_object_services = ObjectServices(self.__api_manager)
+
---
+++ packages/mnubo/mnubo-0.0.1.tar.gz/mnubo-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="mnubo",
+    version="0.0.1",
+    description="Python SDK to access mnubo ingestion APIs",
+    author="mnubo",
+    author_email="sos@mnubo.com",
+    url="https://github.com/mnubo/mnubo-python-sdk",
+    packages=["mnubo"],
+    install_requires=['requests>=2.5,<3.0'],
+    tests_require=['nose', 'mock']
+)
