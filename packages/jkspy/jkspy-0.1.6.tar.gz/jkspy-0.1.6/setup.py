30c3cdcb438b5b635b30331ed9e4b1aea053dd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:03 +0000

    Add package jkspy/0.1.6/jkspy-0.1.6.tar.gz

---
+++ packages/jkspy/jkspy-0.1.6.tar.gz/jkspy-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'jkspy',
+  packages = ['jkspy'],
+  version = '0.1.6',
+  description = 'Python utilities for doing computer stuff',
+  author = 'Kumseok Jung',
+  author_email = 'jungkumseok@gmail.com',
+  license = 'LICENSE.txt',
+  url = 'https://github.com/jungkumseok/jkspy',
+  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.6.tar.gz',
+  keywords = ['testing', 'utility'], 
+  classifiers = [],
+)
