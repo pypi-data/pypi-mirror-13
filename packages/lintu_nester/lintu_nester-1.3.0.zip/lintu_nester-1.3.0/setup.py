4980f9227a8803c4485059dd5474579445979875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package lintu_nester/1.3.0/lintu_nester-1.3.0.zip

---
+++ packages/lintu_nester/lintu_nester-1.3.0.zip/lintu_nester-1.3.0/setup.py
@@ -0,0 +1,13 @@
+﻿#从Python发布工具导入"setup"函数。
+from distutils.core import setup
+
+setup(
+    #以下是setup函数的参数名
+    name         = 'lintu_nester',
+    version      = '1.3.0',
+    py_modules   = ['nester'], #将模块的元数据与setup函数的参数关联。
+    author       = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+    )
