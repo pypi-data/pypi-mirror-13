17a622db4219a90e1b4eb1461b749038ea7e89fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:36 +0000

    Add package py-mysql-elasticsearch-sync/0.2.1/py-mysql-elasticsearch-sync-0.2.1.tar.gz

---
+++ packages/py-mysql-elasticsearch-sync/py-mysql-elasticsearch-sync-0.2.1.tar.gz/py-mysql-elasticsearch-sync-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import src
+
+setup(
+    name='py-mysql-elasticsearch-sync',
+    version=src.__version__,
+    packages=find_packages(),
+    url='https://github.com/zhongbiaodev/py-mysql-elasticsearch-sync',
+    license='MIT',
+    author='Windfarer',
+    author_email='windfarer@gmail.com',
+    description='MySQL to Elasticsearch sync tool',
+    install_requires=[
+        'mysql-replication>=0.8',
+        'requests>=2.9.1',
+        'PyYAML>=3.11',
+        'lxml>=3.5.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'es-sync=src:start',
+        ]
+    },
+    include_package_data=True
+)
