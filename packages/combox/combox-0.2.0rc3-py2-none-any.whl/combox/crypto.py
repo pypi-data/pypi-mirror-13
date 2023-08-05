b910415cd3cc0fb997d8cbe45cec4eba48072a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package combox/0.2.0rc3/combox-0.2.0rc3-py2-none-any.whl

---
+++ packages/combox/combox-0.2.0rc3-py2-none-any.whl/combox/crypto.py
@@ -0,0 +1,168 @@
+#    Copyright (C) 2015 Combox author(s). See AUTHORS.
+#
+#    This file is part of Combox.
+#
+#   Combox is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Combox is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Combox (see COPYING).  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+## Adapted from https://gist.github.com/sekondus/4322469
+
+import base64
+import os
+
+from combox.config import get_nodedirs
+from combox.file import (read_file, write_file,
+                         read_shards, write_shards,
+                         split_data, glue_data,
+                         relative_path)
+from combox.log import log_i
+
+from Crypto.Cipher import AES
+from datetime import datetime
+from os import path
+
+BLOCK_SIZE = 32
+PAD_CHAR = '#'
+
+
+def pad(data):
+    """Pad data such that its length is a multiple of BLOCK_SIZE.
+    """
+
+    padding = (BLOCK_SIZE - (len(data) % BLOCK_SIZE)) * PAD_CHAR
+    data += padding
+
+    return data
+
+
+def encrypt(data, secret):
+    """Encrypt byestream and return cipher.
+    """
+    aes = AES.new(pad(secret))
+    cipher = base64.b64encode(aes.encrypt(pad(data)))
+    
+    return cipher
+
+
+def decrypt(cipher, secret):
+    """Decrypt cipher and return data.
+    """
+    aes = AES.new(pad(secret))
+    data = aes.decrypt(base64.b64decode(cipher)).rstrip(PAD_CHAR)
+
+    return data
+
+def encrypt_shards(shards, secret):
+    """Encrypt the shards of data and return a list of ciphers.
+
+    shards: list of shards (string, bytes).
+    secret: top secret passphrase
+    """
+
+    ciphers = []
+    for shard in shards:
+        cipher = encrypt(shard, secret)
+        ciphers.append(cipher)
+
+    return ciphers
+
+
+def decrypt_shards(ciphers, secret):
+    """Decrypt the ciphered shards and return a list of shards.
+
+    shards: list of ciphered shards.
+    secret: top secret passphrase
+    """
+
+    shards = []
+    for cipher in ciphers:
+        shard = decrypt(cipher, secret)
+        shards.append(shard)
+
+    return shards
+
+
+def split_and_encrypt(fpath, config, fcontent=None):
+    """Splits the file, encrypts the shards and writes them to the nodes.
+
+    fpath: The path to file that has to be split.
+    config: The dictonary containing the combox configuration information.
+    fcontent: Contents of the file at `fpath' (optional).
+
+    If `fcontent' is None, then this function reads the contents of
+    the file; otherwise it just assumes `fcontent' is the content of
+    the file.
+    """
+
+    start = datetime.now()
+
+    rel_path = relative_path(fpath, config)
+
+    # no. of shards = no. of nodes.
+    SHARDS = len(config['nodes_info'].keys())
+
+    f = path.join(config['combox_dir'], rel_path)
+
+    if not fcontent:
+        fcontent = read_file(f)
+
+    f_shards = split_data(fcontent, SHARDS)
+
+    # encrypt shards
+    ciphered_shards = encrypt_shards(f_shards, config['topsecret'])
+
+
+    # write ciphered shards to disk
+    f_basename =  rel_path
+    # gets the list of node' directories.
+    nodes = get_nodedirs(config)
+
+    write_shards(ciphered_shards, nodes, f_basename)
+
+    end = datetime.now()
+    duration = (end - start).total_seconds() * pow(10, 3)
+    log_i('Took %f ms  to split and encrypt %s' % (duration, fpath))
+
+
+
+def decrypt_and_glue(fpath, config, write=True):
+
+    """
+    Reads encrypted shards, decrypts and glues them.
+
+    fpath: The path to file that has to be decrypted & glued from the nodes.
+    config: The dictionary containing the combox configuration information.
+    write: If set, writes the reconstructed file's content to disk.
+    """
+
+    rel_path = relative_path(fpath, config)
+
+    f = path.abspath(fpath)
+    f_basename = rel_path
+    # gets the list of node' directories.
+    nodes = get_nodedirs(config)
+
+    ciphered_shards = read_shards(nodes, f_basename)
+
+    # decrypt shards
+    f_parts = decrypt_shards(ciphered_shards, config['topsecret'])
+
+    # glue them together
+    f_content = glue_data(f_parts)
+
+    if write:
+        # write the glued content to fpath
+        write_file(f, f_content)
+
+    return f_content
---
+++ packages/combox/combox-0.2.0rc3-py2-none-any.whl/combox/silo.py
@@ -0,0 +1,216 @@
+#    Copyright (C) 2015 Combox author(s). See AUTHORS.
+#
+#    This file is part of Combox.
+#
+#   Combox is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Combox is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Combox (see COPYING).  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+import pickledb
+
+from os import path
+from threading import Lock
+
+from combox.file import hash_file
+
+class ComboxSilo(object):
+    """The Combox silo.
+
+    Helps keep track of files in combox directory.
+    """
+
+
+    def __init__(self, config, lock):
+        """config: a dictinary which contains combox configuration.
+
+        """
+        self.config = config
+
+        self.silo_path = path.join(config['silo_dir'], 'silo.db')
+        self.db = pickledb.load(self.silo_path, True)
+
+        ## things we need for housekeep the node directory.
+        self.node_dicts = ['file_created', 'file_modified', 'file_moved',
+                           'file_deleted', 'file_moved_info']
+
+        # created the dicts if not already created.
+        for ndict in self.node_dicts:
+            if not self.db.get(ndict):
+                self.db.dcreate(ndict)
+
+        self.lock = lock
+
+
+    def reload(self):
+        """Re-loads the DB from disk."""
+        with self.lock:
+            self.db = pickledb.load(self.silo_path, True)
+
+
+    def update(self, filep):
+        """Update filep's info in db
+
+        filep and the hash of its content is written to the db.
+
+        filep: path to the file in combox directory.
+
+        """
+        self.reload()
+        with self.lock:
+            fhash = hash_file(filep)
+            return self.db.set(filep, fhash)
+
+
+    def keys(self):
+        """Returns a list of all keys in db."""
+        # this is why Redis or some other key-value DB should be used
+        # instead of PickleDB
+        self.reload()
+        with self.lock:
+            return self.db.db.keys()
+
+
+    def remove(self, filep):
+        """Removes filep from db.
+
+        filep: path to the file in combox directory.
+
+        """
+        try:
+            self.reload()
+            with self.lock:
+                return self.db.rem(filep)
+        except KeyError, e:
+            # means `filep' not present in db.
+            return False
+
+
+    def exists(self, filep):
+        """Checks if filep's info is stored in db.
+
+        Returns True if filep's info is in db; False otherwise.
+
+        filep: path to the file in combox directory.
+
+        """
+        self.reload()
+        with self.lock:
+            if self.db.get(filep) is None:
+                return False
+            else:
+                return True
+
+
+    def stale(self, filep, fhash=None):
+        """Returns True if filep's hash is different from the hash stored in db.
+
+        Returns None, if filep's info is not yet stored in db.
+        Returns False, if filep's hash has not changed it.
+
+        filep: path to the file in combox directory.
+        fhash: If not None, it is assumed to be filep's hash.
+        """
+
+        if not fhash:
+            fhash = hash_file(filep)
+
+        self.reload()
+        with self.lock:
+            fhash_in_db = self.db.get(filep)
+
+        if fhash_in_db is None:
+            return None
+        elif fhash == fhash_in_db:
+            return False
+        else:
+            return True
+
+
+    def nodedicts(self):
+        """
+        Returns a list containing the dicts related the the node directories.
+        """
+        return self.node_dicts
+
+
+    def node_set(self, type_, file_, num=-1):
+        """
+        Update information about the shard of `file_'.
+
+        type_: 'file_created', 'file_modified', 'file_moved', 'file_deleted'
+        file_: path of the file_ in combox directory.
+        num: (optional) integer associated with the `file_'.
+        """
+
+        self.reload()
+        with self.lock:
+            if num != -1:
+                self.db.dadd(type_, (file_, num))
+                return
+            try:
+                num = self.db.dget(type_, file_)
+                num += 1
+            except KeyError, e:
+                # I don't think this is the right way to do this. :|
+                #
+                # If we are here it means file_ is not already there,
+                # so:
+                num = 1
+            self.db.dadd(type_, (file_, num))
+
+
+    def node_store_moved_info(self, src_path, dest_path):
+        """
+        Update/create about file move.
+
+        type_: expected type is 'file_moved_info'.
+        src_path: usually the source path of the file being moved.
+        dest_path: usually the destination path of the file being moved.
+        """
+        self.reload()
+        with self.lock:
+            self.db.dadd('file_moved_info', (src_path, dest_path))
+
+
+    def node_get(self, type_, file_):
+        """
+        Returns a number denoting the number of node directories in which the file_'s shard was created/modified/moved/deleted.
+
+        type_: 'file_created', 'file_modified', 'file_moved', 'file_deleted'
+        file_: path of the file_ in combox directory.
+        """
+        self.reload()
+        with self.lock:
+            try:
+                return self.db.dget(type_, file_)
+            except KeyError, e:
+                # file_ info not there under type_ dict.
+                return None
+
+
+    def node_rem(self, type_, file_):
+        """
+        Removes information about the shard of `file_'.
+
+        type_: 'file_created', 'file_modified', 'file_moved', 'file_deleted'
+        file_: path of the file_ in combox directory.
+        """
+
+        self.reload()
+        with self.lock:
+            try:
+                return self.db.dpop(type_, file_)
+            except KeyError, e:
+                # means file_'s info was already removed.
+                # do nothing
+                pass
