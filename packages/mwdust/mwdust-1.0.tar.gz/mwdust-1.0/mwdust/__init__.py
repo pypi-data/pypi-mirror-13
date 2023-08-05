1c83bcb2d8d2dff95fc0c3888b378f25881e3e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package mwdust/1.0/mwdust-1.0.tar.gz

---
+++ packages/mwdust/mwdust-1.0.tar.gz/mwdust-1.0/mwdust/__init__.py
@@ -0,0 +1,7 @@
+from mwdust.SFD import SFD
+from mwdust.Marshall06 import Marshall06
+from mwdust.Drimmel03 import Drimmel03
+from mwdust.Sale14 import Sale14
+from mwdust.Green15 import Green15
+from mwdust.Combined15 import Combined15
+from mwdust.Zero import Zero
