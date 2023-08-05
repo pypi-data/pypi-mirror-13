ecb1af80275e3c87962524fcd8c87a755f26a723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package epinephrine/0.0.3/epinephrine-0.0.3.tar.gz

---
+++ packages/epinephrine/epinephrine-0.0.3.tar.gz/epinephrine-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+appname = "epinephrine"
+
+setup(
+    name = appname,
+    version = "0.0.3",
+    packages = find_packages(),
+    entry_points={
+        'console_scripts': [
+            '{appname} = {appname}.__main__:main'.format(appname=appname)
+        ]
+    },
+    install_requires = [],
+    author = "minamorl",
+    author_email = "minamorl@minamorl.com",
+)
