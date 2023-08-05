9a40986a3a6b8d8c0d53371e7145312846f0a990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package clldmpg/1.1.1/clldmpg-1.1.1.tar.gz

---
+++ packages/clldmpg/clldmpg-1.1.1.tar.gz/clldmpg-1.1.1/clldmpg/__init__.py
@@ -0,0 +1,37 @@
+from pyramid.response import Response
+
+from clld.interfaces import IOlacConfig
+from clld.web.views.olac import OlacConfig, Participant, Institution
+
+
+class MpgOlacConfig(OlacConfig):
+    def description(self, req):
+        return {
+            'archiveURL': 'http://%s/' % req.dataset.domain,
+            'participants': [
+                Participant("Admin", 'Robert Forkel', 'forkel@shh.mpg.de'),
+            ] + [
+                Participant(
+                    "Editor",
+                    ed.contributor.name,
+                    ed.contributor.email or req.dataset.contact)
+                for ed in req.dataset.editors],
+            'institution': Institution(
+                'Max Planck Institute for the Science of Human History',
+                'http://shh.mpg.de',
+                'Jena, Germany',
+            ),
+            'synopsis': req.dataset.description or '',
+        }
+
+
+def includeme(config):
+    config.include('clld.web.app')
+    config.registry.registerUtility(MpgOlacConfig(), IOlacConfig)
+    config.add_static_view('clldmpg-static', 'clldmpg:static')
+    config.add_settings({'clld.publisher_logo': 'clldmpg:static/minerva.png'})
+
+    config.add_route('google-site-verification', 'googlebbc8f4da1abdc58b.html')
+    config.add_view(
+        lambda r: Response('google-site-verification: googlebbc8f4da1abdc58b.html'),
+        route_name='google-site-verification')
