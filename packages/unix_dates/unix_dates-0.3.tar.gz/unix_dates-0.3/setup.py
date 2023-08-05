6d52ec6c0d31d49c89906e4f10e028a3ef34c1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:16 +0000

    Add package unix_dates/0.3/unix_dates-0.3.tar.gz

---
+++ packages/unix_dates/unix_dates-0.3.tar.gz/unix_dates-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='unix_dates',
+    version='0.3',
+    description='Python Unix Dates conversion utilities',
+    url='https://github.com/ophirh/python-unix-dates',
+    author='Ophir',
+    author_email='opensource@itculate.io',
+    license='MIT',
+    keywords=['unix', 'dates', 'utc', 'timestamp'],
+    packages=['unix_dates'],
+)
