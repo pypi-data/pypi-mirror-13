8e759fdb791a9443a43bfc2b72f17dfeef61ff6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:22 +0000

    Add package justplot/0.1.1dev/justplot-0.1.1dev.tar.gz

---
+++ packages/justplot/justplot-0.1.1dev.tar.gz/justplot-0.1.1dev/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='justplot',
+    version='0.1.1dev',
+    packages=['justplot',],
+    license='MIT',
+    author='Uğur Çayoğlu',
+    author_email='urcyglu@gmail.com',
+)
+
