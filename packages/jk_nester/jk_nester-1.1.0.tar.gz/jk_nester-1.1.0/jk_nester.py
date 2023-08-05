019e2bd02312aeb72f6f5e6878baf689b4c578e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package jk_nester/1.1.0/jk_nester-1.1.0.tar.gz

---
+++ packages/jk_nester/jk_nester-1.1.0.tar.gz/jk_nester-1.1.0/jk_nester.py
@@ -0,0 +1,15 @@
+"""
+Module practise
+"""
+
+
+def print_lol(some_list, level):
+    #Output all elements in a list-takes care of list of list too
+    #recursive function
+    for first_level in some_list:
+        if isinstance(first_level, list):
+            print_lol(first_level, level + 1)
+        else:
+            for tab_stop in range(level)
+                print("\t", end="")
+            print(first_level)
---
+++ packages/jk_nester/jk_nester-1.1.0.tar.gz/jk_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+        name = "jk_nester",
+        version = "1.1.0",
+        py_modules = ["jk_nester"],
+        author = "Jin Kim",
+        author_email = "storytellor@gmail.com",
+        url = "none",
+        description = "A simple printer of nested lists",
+    )
