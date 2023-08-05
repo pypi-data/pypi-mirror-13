25e9d1ad263e7ab26b708e887b535e04952d1aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:30 +0000

    Add package sphinxcontrib-osexample/0.1.1/sphinxcontrib-osexample-0.1.1.tar.gz

---
+++ packages/sphinxcontrib-osexample/sphinxcontrib-osexample-0.1.1.tar.gz/sphinxcontrib-osexample-0.1.1/CHANGES.rst
@@ -0,0 +1,15 @@
+Changelog
+=========
+
+0.1.1 (2016-01-15)
+------------------
+
+- add basic homebrew lexer [svx]
+- add basic osx pkg lexer [svx]
+- include README into setup.py [svx]
+
+
+0.1.0 (2015-12-26)
+------------------
+
+- initial fork [svx]
---
+++ packages/sphinxcontrib-osexample/sphinxcontrib-osexample-0.1.1.tar.gz/sphinxcontrib-osexample-0.1.1/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+'''
+This package contains the osexample Sphinx extension.
+
+This extension adds support for Operating systems code block
+widget to Sphinx.
+'''
+
+requires = ['Sphinx>=1.0']
+
+setup(
+    name='sphinxcontrib-osexample',
+    version='0.1.1',
+    url='https://github.com/svx/sphinxcontrib-osexample',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-osexample',
+    license='BSD',
+    author='Sven Strack',
+    author_email='sven@so36.net',
+    description='Sphinx extension for OS code-blocks',
+    keywords = "Sphinx extension",
+    long_description=readme(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+	'Programming Language :: Python :: 2.7',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
