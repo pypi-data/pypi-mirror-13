5a35e5aed39222989a85509aa4d1b76d96220312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:33 +0000

    Add package redsys-256/0.1.1/redsys-256-0.1.1.tar.gz

---
+++ packages/redsys-256/redsys-256-0.1.1.tar.gz/redsys-256-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+PACKAGES = ['redsys', ]
+
+setup(
+        name='redsys-256',
+        description='This package can generate the necessary fields to the Redsys payment form.',
+        keywords='redsys hmac sha256',
+        author='Alejandro Otero Ortiz de Cosca',
+        author_email='otero.alx@gmail.com',
+        url='http://www.aotero.es',
+        download_url="https://github.com/lexotero/python-redsys",
+        version='0.1.1',
+        license='MIT',
+        provides=[
+            'redsys'
+        ],
+        install_requires=[
+            'pycrypto'
+        ],
+        packages=PACKAGES,
+        scripts=[],
+)
