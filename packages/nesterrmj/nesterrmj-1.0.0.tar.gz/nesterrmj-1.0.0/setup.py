2e3c6ace34b36f71b87062ef41e0789a47834f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:36 +0000

    Add package nesterrmj/1.0.0/nesterrmj-1.0.0.tar.gz

---
+++ packages/nesterrmj/nesterrmj-1.0.0.tar.gz/nesterrmj-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nesterrmj',
+        version     = '1.0.0',
+        py_modules  = ['nesterrmj'],
+        author      = 'Rahul',
+        author_email= 'rahul.juliato@gmail.com',
+        url         = 'http://frankensteinlab.ddns.net/',
+        description = 'A simple printer of nested lists example',
+     )
