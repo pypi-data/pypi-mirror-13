8173e3e0b817cdab65f6c44de5b5063ec1cd8119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:19 +0000

    Add package virtualizorAPI/0.1/virtualizorAPI-0.1.zip

---
+++ packages/virtualizorAPI/virtualizorAPI-0.1.zip/virtualizorAPI-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'virtualizorAPI',
+  packages = ['virtualizorAPI'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Virtualizor End User API in Python3',
+  author = 'Damir H.',
+  license = 'MIT: https://github.com/boxcontrol/VirtualizorAPI/blob/master/LICENSE',
+  author_email = 'info@boxcontrol.net',
+  url = 'https://github.com/boxcontrol/VirtualizorAPI', # use the URL to the github repo
+  download_url = 'https://github.com/boxcontrol/VirtualizorAPI/tarball/0.1', # I'll explain this in a second
+  keywords = ['virtualizor', 'API', 'enduser'], # arbitrary keywords
+  classifiers = [],
+)
