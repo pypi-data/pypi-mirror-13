c80e3a4eb9c4b193f407213e35353ef924010770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package Boolean-Solver/0.2.0/Boolean_Solver-0.2.0-py2-none-any.whl

---
+++ packages/Boolean-Solver/Boolean_Solver-0.2.0-py2-none-any.whl/boolean_solver/test_sample.py
@@ -0,0 +1,44 @@
+__author__ = 'juan pablo isaza'
+
+import unittest
+import solver
+import start_sample
+
+
+class MyTest(unittest.TestCase):
+    """
+    1. Set the truth table of your boolean function (at least for rows where output=True)
+    2. run solver.execute(self, callable, table) where callable is the boolean function
+     with the decorator=@solve_boolean() in functions1.
+     See examples below:
+    """
+    def test_AND_function(self):
+
+        #                  b1     b0   output
+        truth_table = {((True, True), True)}
+
+        solver.execute(self, start_sample.and_function, truth_table)
+
+    def test_OR_function(self):
+
+        truth_table = {((False, True), True),
+                       ((True, False), True),
+                       ((True, True), True)}
+
+        solver.execute(self, start_sample.or_function, truth_table)
+
+    def test_XOR_function(self):
+
+        truth_table = {((False, True), True),
+                       ((True, False), True),
+                       ((True, True), False)}
+
+        solver.execute(self, start_sample.xor_function, truth_table)
+
+    def test_AND_3_VARIABLES_function(self):
+
+        truth_table = {((True, True, True), True)}
+
+        solver.execute(self, start_sample.and_function_3_variables, truth_table)
+
+print "We have solved the riddle, go run start_sample.py, again!!!"
