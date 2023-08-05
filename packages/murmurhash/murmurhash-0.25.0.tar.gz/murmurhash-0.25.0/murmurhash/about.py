1b93c9f1be9dece737abc39238dd33d139a20e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package murmurhash/0.25.0/murmurhash-0.25.0.tar.gz

---
+++ packages/murmurhash/murmurhash-0.25.0.tar.gz/murmurhash-0.25.0/murmurhash/about.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.25.0'
+version = '0.25.0'
+full_version = '0.25.0'
+git_revision = '1dbef961a8dac2a2552f913485b0bf9654004dcc'
+release = True
+if not release:
+    version = full_version
