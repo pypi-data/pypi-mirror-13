f840cd9b012c3ec05432d4a5020db1ac1a06b857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:10 +0000

    Add package asphalt/1.2.0/asphalt-1.2.0-py34.py35-none-any.whl

---
+++ packages/asphalt/asphalt-1.2.0-py34.py35-none-any.whl/asphalt/core/component.py
@@ -0,0 +1,94 @@
+from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
+from typing import Dict, Any, Union
+import asyncio
+
+from typeguard import check_argument_types
+
+from .util import PluginContainer, merge_config, asynchronous
+from .context import Context
+
+__all__ = ('Component', 'ContainerComponent', 'component_types')
+
+
+class Component(metaclass=ABCMeta):
+    """This is the base class for all Asphalt components."""
+
+    __slots__ = ()
+
+    @abstractmethod
+    def start(self, ctx: Context):
+        """
+        Perform any necessary tasks to start the services provided by this component.
+
+        This method can be a coroutine.
+
+        The context can be used to:
+          * add context event listeners (:meth:`~Context.add_listener`)
+          * publish resources (:meth:`~Context.publish_resource` and
+            :meth:`~Context.publish_lazy_resource`)
+          * request resources (:meth:`~Context.request_resource`)
+
+        It is advisable for Components to first publish all the resources they can before
+        requesting any. This will speed up the dependency resolution and prevent deadlocks.
+
+        :param ctx: the containing context for this component
+        """
+
+
+class ContainerComponent(Component):
+    __slots__ = 'child_components', 'component_config'
+
+    def __init__(self, components: Dict[str, Any]=None):
+        self.child_components = OrderedDict()
+        self.component_config = components or {}
+
+    def add_component(self, alias: str, type: Union[str, type]=None, **kwargs):
+        """
+        Instantiate a component using :func:`create_component` and adds it as a child component of
+        this container.
+
+        If the second argument is omitted, the value of ``alias`` is used as its value.
+
+        The locally given configuration can be overridden by component configuration parameters
+        supplied to the constructor (the ``components`` argument).
+
+        :param alias: a name for the component instance, unique within this container
+        :param type: entry point name or :cls:`Component` subclass or a textual reference to one
+
+        """
+        assert check_argument_types()
+        if not isinstance(alias, str) or not alias:
+            raise TypeError('component_alias must be a nonempty string')
+        if alias in self.child_components:
+            raise ValueError('there is already a child component named "{}"'.format(alias))
+
+        # Allow the external configuration to override the constructor arguments
+        kwargs = merge_config(kwargs, self.component_config.get(alias, {}))
+
+        component = component_types.create_object(type or alias, **kwargs)
+        self.child_components[alias] = component
+
+    @asynchronous
+    def start(self, ctx: Context):
+        """
+        Create child components that have been configured but not yet created and then calls their
+        :meth:`Component.start` methods in separate tasks and waits until they have completed.
+
+        """
+        for alias in self.component_config:
+            if alias not in self.child_components:
+                self.add_component(alias)
+
+        if self.child_components:
+            tasks = []
+            for component in self.child_components.values():
+                retval = component.start(ctx)
+                if retval is not None:
+                    tasks.append(retval)
+
+            if tasks:
+                yield from asyncio.gather(*tasks)
+
+
+component_types = PluginContainer('asphalt.components', Component)
---
+++ packages/asphalt/asphalt-1.2.0-py34.py35-none-any.whl/asphalt/core/event.py
@@ -0,0 +1,145 @@
+from typing import Dict, Callable, Any, Sequence, Union
+
+from typeguard import check_argument_types
+
+from .util import qualified_name, asynchronous
+
+__all__ = ('Event', 'EventListener', 'EventSource')
+
+
+class Event:
+    """
+    The base class for all events.
+
+    :ivar EventSource source: the event source where this event originated from
+    :ivar str topic: the topic
+    """
+
+    __slots__ = 'source', 'topic'
+
+    def __init__(self, source: 'EventSource', topic: str):
+        self.source = source
+        self.topic = topic
+
+
+class EventListener:
+    """A handle that can be used to remove an event listener from its :class:`EventSource`."""
+
+    __slots__ = 'source', 'topic', 'callback', 'args', 'kwargs'
+
+    def __init__(self, source: 'EventSource', topic: str, callback: Callable, args: Sequence,
+                 kwargs: Dict[str, Any]):
+        assert check_argument_types()
+        self.source = source
+        self.topic = topic
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs
+
+    def remove(self):
+        """Remove this listener from its event source."""
+
+        self.source.remove_listener(self)
+
+    def __repr__(self):
+        return ('{0.__class__.__name__}(topic={0.topic!r}, callback={1}, args={0.args!r}, '
+                'kwargs={0.kwargs!r})'.
+                format(self, qualified_name(self.callback)))
+
+
+class EventSource:
+    """A mixin class that provides support for dispatching and listening to events."""
+
+    __slots__ = '_topics'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._topics = {}
+
+    def _register_topics(self, topics: Dict[str, Any]):
+        """
+        Register a number of supported topics and their respective event classes.
+
+        :param topics: a dictionary of topic -> event class
+
+        """
+        for topic, event_class in topics.items():
+            self._topics[topic] = {'event_class': event_class, 'listeners': []}
+
+    @asynchronous
+    def add_listener(self, topic: str, callback: Callable, args: Sequence=(),
+                     kwargs: Dict[str, Any]=None) -> EventListener:
+        """
+        Start listening to events specified by ``topic``.
+
+        The callback (which can be a coroutine function) will be called with a single argument (an
+        :class:`Event` instance). The exact event class used depends on the event class mappings
+        given to the constructor.
+
+        :param topic: the topic to listen to
+        :param callback: a callable to call with the event object when the event is dispatched
+        :param args: positional arguments to call the callback with (in addition to the event)
+        :param kwargs: keyword arguments to call the callback with
+        :return: a listener handle which can be used with :meth:`remove_listener` to unlisten
+        :raises LookupError: if the named event has not been registered in this event source
+
+        """
+        assert check_argument_types()
+        if topic not in self._topics:
+            raise LookupError('no such topic registered: {}'.format(topic))
+
+        handle = EventListener(self, topic, callback, args, kwargs or {})
+        handles = self._topics[topic]['listeners']
+        handles.append(handle)
+        return handle
+
+    @asynchronous
+    def remove_listener(self, handle: EventListener):
+        """
+        Remove an event listener previously added via :meth:`add_listener`.
+
+        :param handle: the listener handle returned from :meth:`add_listener`
+        :raises LookupError: if the handle was not found among the registered listeners
+
+        """
+        assert check_argument_types()
+        try:
+            self._topics[handle.topic]['listeners'].remove(handle)
+        except (KeyError, ValueError):
+            raise LookupError('listener not found') from None
+
+    @asynchronous
+    def dispatch(self, event: Union[str, Event], *args, **kwargs):
+        """
+        Dispatch an event, optionally constructing one first.
+
+        This method has two forms: dispatch(``event``) and dispatch(``topic``, ``*args``,
+        ``**kwargs``). The former dispatches an existing event object while the latter
+        instantiates one, using this object as the source. Any extra positional and keyword
+        arguments are passed directly to the event class constructor.
+
+        Any exceptions raised by the listener callbacks are passed
+        through to the caller.
+
+        :param event: an :class:`~asphalt.core.event.Event` instance or an event topic
+        :raises LookupError: if the topic has not been registered in this event source
+
+        """
+        assert check_argument_types()
+        topic = event.topic if isinstance(event, Event) else event
+        try:
+            registration = self._topics[topic]
+        except KeyError:
+            raise LookupError('no such topic registered: {}'.format(topic)) from None
+
+        if isinstance(event, Event):
+            assert not args and not kwargs, 'passing extra arguments makes no sense here'
+            assert isinstance(event, registration['event_class']), 'event class mismatch'
+        else:
+            event_class = registration['event_class']
+            event = event_class(self, topic, *args, **kwargs)
+
+        for listener in list(registration['listeners']):
+            retval = listener.callback(event, *listener.args, **listener.kwargs)
+            if retval is not None:
+                yield from retval
