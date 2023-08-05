bb3b7188bf9bcfdc0f2aa3b7b12910aa7ab0afae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package tigerwang/1.1.1/tigerwang-1.1.1.tar.gz

---
+++ packages/tigerwang/tigerwang-1.1.1.tar.gz/tigerwang-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'tigerwang',
+		version = '1.1.1',
+		py_modules = ['tigerwang'],
+		author = 'tiger',
+		author_email = 'iemail.wy@gmail.com',
+		url = 'heep://www.baidu.com',
+		description = 'A simple printer of nested lists',
+	)
