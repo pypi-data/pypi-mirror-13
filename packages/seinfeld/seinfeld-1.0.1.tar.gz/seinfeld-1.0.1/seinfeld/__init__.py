c788a9bbc539f0e6e863518ae128d1c7273c9eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:30 +0000

    Add package seinfeld/1.0.1/seinfeld-1.0.1.tar.gz

---
+++ packages/seinfeld/seinfeld-1.0.1.tar.gz/seinfeld-1.0.1/seinfeld/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2016 John Reese
+# Licensed under the MIT license
+# flake8: noqa
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+try:
+    from .seinfeld import Seinfeld
+
+except ImportError:
+    pass
+
+__version__ = '1.0.1'
