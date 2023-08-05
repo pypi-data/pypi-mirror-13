965aeec63917f3d42776e5d8b376f8f0f88df080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:20 +0000

    Add package gnuplotlib/0.11/gnuplotlib-0.11.tar.gz

---
+++ packages/gnuplotlib/gnuplotlib-0.11.tar.gz/gnuplotlib-0.11/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from distutils.core          import setup
+from distutils.command.clean import clean
+import subprocess
+
+subprocess.call( ['make', 'README', 'README.org'] )
+
+
+class MoreClean(clean):
+    def run(self):
+        clean.run(self)
+        subprocess.call( ['make', 'clean'] )
+
+
+setup(name         = 'gnuplotlib',
+      version      = '0.11',
+      author       = 'Dima Kogan',
+      author_email = 'dima@secretsauce.net',
+      url          = 'http://github.com/dkogan/gnuplotlib',
+      description  = 'Gnuplot-based plotting for numpy',
+      license      = 'LGPL-3+',
+      py_modules   = ['gnuplotlib'],
+      cmdclass     = {'clean': MoreClean})
