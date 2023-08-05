07eab036fba050a478140454ae4096d9a2ea4876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package django-unixtimestampfield/0.3.5/django-unixtimestampfield-0.3.5.tar.gz

---
+++ packages/django-unixtimestampfield/django-unixtimestampfield-0.3.5.tar.gz/django-unixtimestampfield-0.3.5/unixtimestampfield/fields.py
@@ -0,0 +1,387 @@
+# encoding: utf-8
+
+"""
+UnixTimeStampField
+
+release |release|, version |version|
+
+.. versionadded:: 1.0
+
+    Initial
+
+Contents
+--------
+
+Classes:
+
+* :class:`TimestampPatchMixin`
+* :class:`UnixTimeStampField`
+* :class:`OrdinalPatchMixin`
+* :class:`OrdinalField`
+
+Functions:
+
+Variables:
+
+Members
+-------
+
+"""
+from __future__ import unicode_literals
+
+import time
+import datetime
+
+from django.db.models import Field
+from django.utils import timezone
+from django.core import exceptions
+from django.conf import settings
+from django.forms import fields
+
+from django.utils import six
+from django.utils.translation import ugettext_lazy as _
+
+from .submiddleware import field_value_middleware
+
+
+class TimestampPatchMixin(object):
+
+    def _datetime_to_timestamp(self, v):
+        """
+        Py2 doesn't supports timestamp()
+        """
+
+        if hasattr(v, 'timestamp'):
+            return v.timestamp()
+
+        return time.mktime(v.timetuple()) + v.microsecond * 0.00001
+
+    def get_datetimenow(self):
+        """
+        get datetime now according to USE_TZ and default time
+        """
+        value = timezone.datetime.utcnow()
+        if settings.USE_TZ:
+            value = timezone.localtime(
+                timezone.make_aware(value, timezone.utc),
+                timezone.get_default_timezone()
+            )
+        return value
+
+    def get_timestampnow(self):
+        """
+        get utc unix timestamp
+        """
+        return self._datetime_to_timestamp(timezone.datetime.utcnow())
+
+    def to_timestamp(self, value):
+        """
+        from value to timestamp format(float)
+        """
+        if isinstance(value, int) or isinstance(value, float):
+            return float(value)
+
+        if isinstance(value, six.string_types):
+            try:
+                return float(value)
+            except ValueError:
+                value = self.datetime_str_to_datetime(value)
+
+        if isinstance(value, datetime.datetime):
+            if timezone.is_aware(value):
+                value = timezone.localtime(value, timezone.utc)
+            return self._datetime_to_timestamp(value)
+
+        if value is None:
+            try:
+                return float(self.default)
+            except:
+                return 0.0
+
+        raise exceptions.ValidationError(
+            "Unable to convert value: '%s' to timestamp" % value,
+            code="invalid_timestamp"
+        )
+
+    def to_naive_datetime(self, value):
+        """
+        from value to datetime with tzinfo format (datetime.datetime instance)
+        """
+        if isinstance(value, int) or isinstance(value, float):
+            value = timezone.datetime.fromtimestamp(float(value))
+            return value
+
+        if isinstance(value, six.string_types):
+            try:
+                return timezone.datetime.fromtimestamp(float(value))
+            except ValueError:
+                return self.datetime_str_to_datetime(value)
+
+        if isinstance(value, datetime.datetime):
+            return value
+
+        if value is None:
+            try:
+                return timezone.datetime.fromtimestamp(self.default)
+            except:
+                return timezone.datetime.fromtimestamp(0.0)
+
+        raise exceptions.ValidationError(
+            "Unable to convert value: '%s' to python data type" % value,
+            code="invalid_datetime"
+        )
+
+    def to_utc_datetime(self, value):
+        """
+        from value to datetime with tzinfo format (datetime.datetime instance)
+        """
+        if isinstance(value, int) or isinstance(value, float):
+            value = timezone.datetime.fromtimestamp(float(value), timezone.utc)
+            return value
+
+        if isinstance(value, six.string_types):
+            try:
+                return timezone.datetime.fromtimestamp(float(value), timezone.utc)
+            except ValueError:
+                value = self.datetime_str_to_datetime(value)
+
+        if isinstance(value, datetime.datetime):
+            if timezone.is_naive(value):
+                value = timezone.make_aware(value, timezone.utc)
+            else:
+                value = timezone.localtime(value, timezone.utc)
+            return value
+
+        if value is None:
+            try:
+                return timezone.datetime.fromtimestamp(self.default, timezone.utc)
+            except:
+                return timezone.datetime.fromtimestamp(0.0, timezone.utc)
+
+        raise exceptions.ValidationError(
+            "Unable to convert value: '%s' to python data type" % value,
+            code="invalid_datetime"
+        )
+
+    def to_default_timezone_datetime(self, value):
+        """
+        convert to default timezone datetime
+        """
+        return timezone.localtime(self.to_utc_datetime(value), timezone.get_default_timezone())
+
+    def to_datetime(self, value):
+
+        if settings.USE_TZ:
+            if settings.TIME_ZONE != 'UTC':
+                return self.to_default_timezone_datetime(value)
+            else:
+                return self.to_utc_datetime(value)
+        else:
+            return self.to_naive_datetime(value)
+
+    def datetime_str_to_datetime(self, value):
+        try:
+            if value.find('.') >= 0:
+                return timezone.datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f')
+            else:
+                return timezone.datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+        except:
+            raise exceptions.ValidationError(
+                "Unable to convert value: '%s' to datetime, please use 'YYYY-mm-dd HH:MM:SS'" % value,
+                code="invalid_timestamp"
+            )
+
+
+class UnixTimeStampField(TimestampPatchMixin, Field):
+    """
+    Copy and mimic django.db.models.fields.DatetimeField
+    Stored as float in database and used as datetime object in Python
+
+    """
+    empty_strings_allowed = False
+    description = _("Unix POSIX timestamp")
+
+    def __init__(self, verbose_name=None, name=None, auto_now=False,
+                 auto_now_add=False, round_to=6, use_numeric=False, **kwargs):
+        self.auto_now, self.auto_now_add = auto_now, auto_now_add
+        self.round_to, self.use_numeric = round_to, use_numeric
+        if auto_now or auto_now_add:
+            kwargs['editable'] = False
+            kwargs['blank'] = True
+        super(UnixTimeStampField, self).__init__(verbose_name, name, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(UnixTimeStampField, self).deconstruct()
+        if self.auto_now:
+            kwargs['auto_now'] = True
+        if self.auto_now_add:
+            kwargs['auto_now_add'] = True
+        if self.auto_now or self.auto_now_add:
+            del kwargs['editable']
+            del kwargs['blank']
+        return name, path, args, kwargs
+
+    def get_internal_type(self):
+        return "FloatField"
+
+    def pre_save(self, model_instance, add):
+        if self.auto_now or (self.auto_now_add and add):
+            value = self.get_datetimenow()
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            value = getattr(model_instance, self.attname)
+            setattr(model_instance, self.attname, field_value_middleware(self, value))
+            return value
+
+    def to_python(self, value):
+        return field_value_middleware(self, value)
+
+    def get_default(self):
+        if self.auto_now or self.auto_now_add:
+            v = self.get_datetimenow()
+        else:
+            v = 0.0
+
+        if self.has_default():
+            v = self.default
+            if callable(self.default):
+                v = self.default()
+        return self.to_python(v)
+
+    def value_to_string(self, obj):
+        val = self._get_val_from_obj(obj)
+        return '' if val is None else val
+
+    def get_prep_value(self, value):
+        value = super(UnixTimeStampField, self).get_prep_value(value)
+        return self.to_timestamp(value)
+
+    def get_db_prep_value(self, value, connection, prepared=False):
+        if not prepared:
+            value = self.get_prep_value(value)
+        return self.to_timestamp(value)
+
+    def from_db_value(self, value, expression, connection, context):
+        return field_value_middleware(self, value)
+
+    def to_timestamp(self, value):
+        return round(super(UnixTimeStampField, self).to_timestamp(value), self.round_to)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': fields.CharField}
+        defaults.update(kwargs)
+        return super(UnixTimeStampField, self).formfield(**defaults)
+
+
+class OrdinalPatchMixin(TimestampPatchMixin):
+
+    def _datetime_to_timestamp(self, v):
+        """
+        overwrite to use toordinal
+        """
+        return v.toordinal()
+
+    def get_datetimenow(self):
+        """
+        get datetime now according to USE_TZ and default time
+        """
+        value = timezone.datetime.fromordinal(timezone.datetime.utcnow().toordinal())
+        if settings.USE_TZ:
+            value = timezone.localtime(
+                timezone.make_aware(value, timezone.utc),
+                timezone.get_default_timezone()
+            )
+        return value
+
+    def to_timestamp(self, value):
+        """
+        from value to ordinal timestamp format(int)
+        """
+        if isinstance(value, int) or isinstance(value, float):
+            value = int(value)
+            if value > 0:
+                return value
+
+        if isinstance(value, six.string_types):
+            try:
+                return int(value)
+            except ValueError:
+                value = self.datetime_str_to_datetime(value)
+
+        if isinstance(value, datetime.datetime):
+            if timezone.is_aware(value):
+                value = timezone.localtime(value, timezone.utc)
+            return self._datetime_to_timestamp(value)
+
+        raise exceptions.ValidationError(
+            "Unable to convert value: '%s' to timestamp" % value,
+            code="invalid_timestamp"
+        )
+
+    def to_naive_datetime(self, value):
+        """
+        from value to datetime with tzinfo format (datetime.datetime instance)
+        """
+        if isinstance(value, int) or isinstance(value, float):
+            value = timezone.datetime.fromordinal(int(value))
+            return value
+
+        if isinstance(value, six.string_types):
+            try:
+                return timezone.datetime.fromordinal(int(value))
+            except ValueError:
+                return self.datetime_str_to_datetime(value)
+
+        if isinstance(value, datetime.datetime):
+            return value
+
+        raise exceptions.ValidationError(
+            "Unable to convert value: '%s' to python data type" % value,
+            code="invalid_datetime"
+        )
+
+    def to_utc_datetime(self, value):
+        """
+        from value to datetime with tzinfo format (datetime.datetime instance)
+        """
+        if isinstance(value, six.string_types) or isinstance(value, int) or isinstance(value, float):
+            value = self.to_naive_datetime(value)
+
+        if isinstance(value, datetime.datetime):
+            if timezone.is_naive(value):
+                value = timezone.make_aware(value, timezone.utc)
+            else:
+                value = timezone.localtime(value, timezone.utc)
+            return value
+
+        raise exceptions.ValidationError(
+            "Unable to convert value: '%s' to python data type" % value,
+            code="invalid_datetime"
+        )
+
+
+class OrdinalField(OrdinalPatchMixin, UnixTimeStampField):
+    """
+    Copy and mimic django.db.models.fields.DatetimeField
+    Stored as float in database and used as datetime object in Python
+
+    """
+    empty_strings_allowed = False
+    description = _("Ordinal timestamp")
+
+    def __init__(self, verbose_name=None, name=None, auto_now=False,
+                 auto_now_add=False, use_numeric=False, **kwargs):
+        self.auto_now, self.auto_now_add, self.use_numeric = auto_now, auto_now_add, use_numeric
+        if auto_now or auto_now_add:
+            kwargs['editable'] = False
+            kwargs['blank'] = True
+        super(UnixTimeStampField, self).__init__(verbose_name, name, **kwargs)
+
+    def get_internal_type(self):
+        return "FloatField"
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': fields.CharField}
+        defaults.update(kwargs)
+        return super(OrdinalField, self).formfield(**defaults)
---
+++ packages/django-unixtimestampfield/django-unixtimestampfield-0.3.5.tar.gz/django-unixtimestampfield-0.3.5/unixtimestampfield/tests.py
@@ -0,0 +1,312 @@
+from django.test import TestCase, override_settings
+
+from django.db import models
+from django.utils import timezone
+from django import forms
+from django.template import Template, Context
+
+from .fields import UnixTimeStampField, OrdinalField
+
+unix_0 = timezone.datetime.fromtimestamp(0.0)
+unix_0_utc = timezone.datetime.fromtimestamp(0.0, timezone.utc)
+
+ordinal_1 = timezone.datetime.fromordinal(1)
+ordinal_1_utc = timezone.make_aware(timezone.datetime.fromordinal(1), timezone.utc)
+
+
+class ForTestModel(models.Model):
+
+    created = UnixTimeStampField(auto_now_add=True)
+    modified = UnixTimeStampField(auto_now=True)
+    str_ini = UnixTimeStampField(default='0.0')
+    float_ini = UnixTimeStampField(default=0.0)
+    int_ini = UnixTimeStampField(default=0.0)
+    dt_ini = UnixTimeStampField(default=unix_0_utc)
+
+    use_numeric_field = UnixTimeStampField(use_numeric=True, default=0.0)
+    round_3_field = UnixTimeStampField(use_numeric=True, round_to=3, default=0.0)
+
+
+class TimeStampFieldTest(TestCase):
+
+    @override_settings(USE_TZ=True, TIME_ZONE='UTC')
+    def test_init_with_use_tz(self):
+        now = timezone.now()
+        expected = timezone.make_aware(timezone.datetime.utcfromtimestamp(0.0), timezone.utc)
+        t = ForTestModel.objects.create()
+
+        self.assertGreater(t.created, now)
+        self.assertGreater(t.modified, now)
+        self.assertEqual(t.str_ini, expected)
+        self.assertEqual(t.float_ini, expected)
+        self.assertEqual(t.int_ini, expected)
+
+    @override_settings(USE_TZ=True, TIME_ZONE='UTC')
+    def test_assignment_with_tz(self):
+        expected = timezone.make_aware(timezone.datetime.utcfromtimestamp(3.0), timezone.utc)
+        t = ForTestModel.objects.create()
+
+        pre_modified = t.modified
+
+        t.str_ini = '3'
+        t.float_ini = 3.0
+        t.int_ini = 3
+        t.dt_ini = timezone.datetime.fromtimestamp(3.0, timezone.utc)
+        t.use_numeric_field = 3.1111116
+        t.round_3_field = 3.1116
+        t.save()
+
+        if hasattr(t, 'refresh_from_db'):
+            t.refresh_from_db()
+        else:
+            t = ForTestModel.objects.get(id=t.id)
+
+        self.assertGreater(t.modified, pre_modified)
+        self.assertEqual(t.str_ini, expected)
+        self.assertEqual(t.float_ini, expected)
+        self.assertEqual(t.int_ini, expected)
+        self.assertEqual(t.use_numeric_field, 3.111112)
+        self.assertEqual(t.round_3_field, 3.112)
+
+    @override_settings(USE_TZ=True, TIME_ZONE='Asia/Taipei')
+    def test_init_with_different_tz(self):
+        now = timezone.now()
+        expected = timezone.localtime(
+            timezone.make_aware(timezone.datetime.utcfromtimestamp(0.0), timezone.utc),
+            timezone.pytz.timezone('Asia/Taipei')
+        )
+        t = ForTestModel.objects.create()
+
+        self.assertGreater(t.created, now)
+        self.assertGreater(t.modified, now)
+        self.assertEqual(t.str_ini, expected)
+        self.assertEqual(t.float_ini, expected)
+        self.assertEqual(t.int_ini, expected)
+
+    @override_settings(USE_TZ=False)
+    def test_init_without_tz(self):
+        now = timezone.datetime.now()
+        expected = timezone.datetime.fromtimestamp(0.0)
+        t = ForTestModel.objects.create()
+
+        self.assertGreater(t.created, now)
+        self.assertGreater(t.modified, now)
+        self.assertEqual(t.str_ini, expected)
+        self.assertEqual(t.float_ini, expected)
+        self.assertEqual(t.int_ini, expected)
+
+    @override_settings(USE_TZ=False)
+    def test_assignment_without_tz(self):
+        expected = timezone.datetime.fromtimestamp(3.0)
+        t = ForTestModel.objects.create()
+
+        pre_modified = t.modified
+
+        t.str_ini = '3'
+        t.float_ini = 3.0
+        t.int_ini = 3
+        t.dt_ini = timezone.datetime.fromtimestamp(3.0)
+        t.save()
+
+        if hasattr(t, 'refresh_from_db'):
+            t.refresh_from_db()
+        else:
+            t = ForTestModel.objects.get(id=t.id)
+
+        self.assertGreater(t.modified, pre_modified)
+        self.assertEqual(t.str_ini, expected)
+        self.assertEqual(t.float_ini, expected)
+        self.assertEqual(t.int_ini, expected)
+
+
+class ForTestModelForm(forms.ModelForm):
+    class Meta:
+        model = ForTestModel
+        fields = ['str_ini', 'float_ini', 'int_ini', 'dt_ini',
+                  'use_numeric_field', 'round_3_field']
+
+
+class FormFieldTest(TestCase):
+
+    def test_noraml(self):
+        data = {
+            'str_ini': '1999-12-11 10:23:13',
+            'float_ini': 3.0,
+            'int_ini': 3,
+            'dt_ini': 3,
+            'use_numeric_field': 0,
+            'round_3_field': 0,
+        }
+
+        tform = ForTestModelForm(data=data)
+
+        self.assertTrue(tform.is_valid())
+
+    def test_empty_form(self):
+
+        data = {}
+
+        tform = ForTestModelForm(data=data)
+
+        self.assertFalse(tform.is_valid())
+        errors = {'dt_ini': [u'This field is required.'],
+                  'float_ini': [u'This field is required.'],
+                  'int_ini': [u'This field is required.'],
+                  'round_3_field': [u'This field is required.'],
+                  'str_ini': [u'This field is required.'],
+                  'use_numeric_field': [u'This field is required.']}
+        self.assertDictEqual(tform.errors, errors)
+        self.assertEqual(tform.error_class, forms.utils.ErrorList)
+
+    def test_partial_data(self):
+
+        data = {
+            'int_ini': 0,
+            'round_3_field': 0,
+            'str_ini': '3',
+        }
+
+        tform = ForTestModelForm(data=data)
+
+        self.assertFalse(tform.is_valid())
+        errors = {'dt_ini': [u'This field is required.'],
+                  'float_ini': [u'This field is required.'],
+                  'use_numeric_field': [u'This field is required.']}
+        self.assertDictEqual(tform.errors, errors)
+        self.assertEqual(tform.error_class, forms.utils.ErrorList)
+
+    def test_invalid_data(self):
+
+        data = {
+            'str_ini': ['hello'],
+            'float_ini': 3.0,
+            'int_ini': 3,
+            'dt_ini': 3,
+            'use_numeric_field': 0,
+            'round_3_field': 0,
+        }
+
+        tform = ForTestModelForm(data=data)
+
+        self.assertFalse(tform.is_valid())
+        errors = {'str_ini': [u"Unable to convert value: '['hello']' to datetime, please use 'YYYY-mm-dd HH:MM:SS'"], }
+        self.assertDictEqual(tform.errors, errors)
+        self.assertEqual(tform.error_class, forms.utils.ErrorList)
+
+
+class ForOrdinalTestModel(models.Model):
+
+    created = OrdinalField(auto_now_add=True)
+    modified = OrdinalField(auto_now=True)
+    str_ini = OrdinalField(default='1')
+    float_ini = OrdinalField(default=1)
+    int_ini = OrdinalField(default=1)
+    dt_ini = OrdinalField(default=ordinal_1)
+
+
+class OrdinalFieldTest(TestCase):
+
+    @override_settings(USE_TZ=True, TIME_ZONE='UTC')
+    def test_init_with_utc(self):
+        today = timezone.make_aware(
+            timezone.datetime.fromordinal(timezone.now().toordinal()), timezone.utc)
+        expected = timezone.make_aware(timezone.datetime.fromordinal(1), timezone.utc)
+        m = ForOrdinalTestModel.objects.create()
+
+        self.assertEqual(m.created, today)
+        self.assertEqual(m.modified, today)
+        self.assertEqual(m.str_ini, expected)
+        self.assertEqual(m.float_ini, expected)
+        self.assertEqual(m.int_ini, expected)
+        self.assertEqual(m.dt_ini, expected)
+
+    @override_settings(USE_TZ=True, TIME_ZONE='UTC')
+    def test_assignment_with_tz(self):
+        today = timezone.make_aware(
+            timezone.datetime.fromordinal(timezone.now().toordinal()), timezone.utc)
+        expected = timezone.make_aware(timezone.datetime.fromordinal(3), timezone.utc)
+        m = ForOrdinalTestModel.objects.create()
+
+        m.str_ini = '3'
+        m.float_ini = 3.0
+        m.int_ini = 3
+        m.dt_ini = timezone.make_aware(timezone.datetime.fromordinal(3), timezone.utc)
+        m.save()
+
+        if hasattr(m, 'refresh_from_db'):
+            m.refresh_from_db()
+        else:
+            m = ForOrdinalTestModel.objects.get(id=m.id)
+
+        self.assertEqual(m.modified, today)
+        self.assertEqual(m.str_ini, expected)
+        self.assertEqual(m.float_ini, expected)
+        self.assertEqual(m.int_ini, expected)
+
+    @override_settings(USE_TZ=True, TIME_ZONE='Asia/Taipei')
+    def test_init_with_different_tz(self):
+        today = timezone.make_aware(
+            timezone.datetime.fromordinal(timezone.now().toordinal()), timezone.utc)
+        expected = timezone.localtime(
+            timezone.make_aware(timezone.datetime.fromordinal(1), timezone.utc),
+            timezone.pytz.timezone('Asia/Taipei')
+        )
+        m = ForOrdinalTestModel.objects.create()
+
+        self.assertEqual(m.created, today)
+        self.assertEqual(m.modified, today)
+        self.assertEqual(m.str_ini, expected)
+        self.assertEqual(m.float_ini, expected)
+        self.assertEqual(m.int_ini, expected)
+
+    @override_settings(USE_TZ=False)
+    def test_init_without_tz(self):
+        today = timezone.datetime.fromordinal(timezone.now().toordinal())
+        expected = timezone.datetime.fromordinal(1)
+        m = ForOrdinalTestModel.objects.create()
+
+        self.assertEqual(m.created, today)
+        self.assertEqual(m.modified, today)
+        self.assertEqual(m.str_ini, expected)
+        self.assertEqual(m.float_ini, expected)
+        self.assertEqual(m.int_ini, expected)
+
+    @override_settings(USE_TZ=False)
+    def test_assignment_without_tz(self):
+
+        today = timezone.datetime.fromordinal(timezone.now().toordinal())
+        expected = timezone.datetime.fromordinal(3)
+        m = ForOrdinalTestModel.objects.create()
+
+        m.str_ini = '3'
+        m.float_ini = 3.0
+        m.int_ini = 3
+        m.dt_ini = timezone.datetime.fromordinal(3)
+        m.save()
+
+        if hasattr(m, 'refresh_from_db'):
+            m.refresh_from_db()
+        else:
+            m = ForOrdinalTestModel.objects.get(id=m.id)
+
+        self.assertEqual(m.modified, today)
+        self.assertEqual(m.str_ini, expected)
+        self.assertEqual(m.float_ini, expected)
+        self.assertEqual(m.int_ini, expected)
+
+
+class TemplateTagsTest(TestCase):
+
+    def setUp(self):
+        self.template = Template(
+            "{% load unixtimestampfield %} "
+            "{{t.str_ini|to_datetime}} "
+            "{{t.str_ini|to_timestamp}}"
+        )
+
+    @override_settings(USE_TZ=True, TIME_ZONE='UTC')
+    def test_render(self):
+        t = ForTestModel()
+        rendered = self.template.render(Context({'t': t}))
+        self.assertIn("Jan. 1, 1970", rendered)
+        self.assertIn("0.0", rendered)
