d8769ec1a13b19d9813b381e4d7ae2ddca1d03c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package Pypsy/0.1.1/Pypsy-0.1.1.tar.gz

---
+++ packages/Pypsy/Pypsy-0.1.1.tar.gz/Pypsy-0.1.1/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='Pypsy',
+    version='0.1.1',
+    packages=['Pypsy'],
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    url='http://www.musicsensorsemotion.com',
+    license='MIT',
+    author='Brennon Bortz',
+    author_email='brennon@vt.edu',
+    description='Electrodermal activity processing and analysis'
+)
