d302916b8f4b4c05e9191692375d9941a3085bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:19 +0000

    Add package tryp-nvim/3.2.0/tryp-nvim-3.2.0.tar.gz

---
+++ packages/tryp-nvim/tryp-nvim-3.2.0.tar.gz/tryp-nvim-3.2.0/trypnv/__init__.py
@@ -0,0 +1,13 @@
+from trypnv.nvim import NvimFacade
+from trypnv.cmd import command, msg_command, json_msg_command
+from trypnv.machine import Machine, StateMachine  # type: ignore
+from trypnv.machine import PluginStateMachine  # type: ignore
+from trypnv.process import ProcessExecutor, Job  # type: ignore
+from trypnv.plugin import NvimStatePlugin, NvimPlugin
+
+in_vim = True
+
+__all__ = ['Plugin', 'command', 'NvimFacade', 'Machine', 'StateMachine',
+           'development', 'NvimStatePlugin', 'msg_command', 'ProcessExecutor',
+           'Job', 'NvimPlugin', 'PluginStateMachine', 'in_vim',
+           'json_msg_command']
