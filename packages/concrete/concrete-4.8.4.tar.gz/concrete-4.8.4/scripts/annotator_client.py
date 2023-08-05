ab84109f97beea0de2f9af5e670cc4e15f252ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package concrete/4.8.4/concrete-4.8.4.tar.gz

---
+++ packages/concrete/concrete-4.8.4.tar.gz/concrete-4.8.4/scripts/annotator_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from concrete.util import CommunicationReader, CommunicationWriter
+from concrete.util.annotator_wrapper import AnnotatorClientWrapper
+
+
+def main():
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description="Interface with a Concrete Annotator service"
+    )
+    parser.add_argument('host', help="Hostname of annotator service to which to connect.")
+    parser.add_argument('port', type=int, help="Port of annotator service to which to connect.")
+    parser.add_argument('--input', default='-', help="Input source to use. '-' for stdin; otherwise takes a path to a file.")
+    parser.add_argument('--output', default='-', help="Output source to use. '-' for stdout; otherwise takes a path to a file.")
+    args = parser.parse_args()
+
+    # Won't work on Windows...
+    # but stdin/stdout pipelines using this script in Windows have probability 0
+    input_path = '/dev/fd/0' if (args.input) == '-' else args.input
+    output_path = '/dev/fd/1' if (args.output) == '-' else args.output
+
+    reader = CommunicationReader(input_path)
+    with AnnotatorClientWrapper(args.host, args.port) as client:
+        with CommunicationWriter(output_path) as writer:
+            for (comm, _) in reader:
+                writer.write(client.annotate(comm))
+
+
+if __name__ == "__main__":
+    main()
