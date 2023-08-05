51a46b965066301164f8c0e012d4f2f2a33c3192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package nester_lewx/1.1.0/nester_lewx-1.1.0.tar.gz

---
+++ packages/nester_lewx/nester_lewx-1.1.0.tar.gz/nester_lewx-1.1.0/nester_lewx.py
@@ -0,0 +1,14 @@
+"""这是‘nester_lewx.py’模块，提供了一个名为print_lol（）的函数
+   打印列表，其中有可能包含嵌套列表。"""
+def print_lol(the_list,level):
+        """这个函数取一个位置列表，名为”the_list“，
+           这可以是任何python列表（也可以包含嵌套列表）。
+           所指定的列表中的每个数据项会（递归的）输出到屏幕，各个数据各占一行。
+           第二个参数（名为"level"）用来在遇到嵌套列表时插入制表符"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,level+1)
+                else:
+                        for tab_stop in range(level): 
+                            print("\t",end='')
+                        print(each_item)
