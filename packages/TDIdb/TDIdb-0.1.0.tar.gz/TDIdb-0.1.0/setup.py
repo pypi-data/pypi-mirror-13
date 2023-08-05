b8bff56ecbd2b3d977610c94d1a6ec5e6e18a397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:09 +0000

    Add package TDIdb/0.1.0/TDIdb-0.1.0.tar.gz

---
+++ packages/TDIdb/TDIdb-0.1.0.tar.gz/TDIdb-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='TDIdb',
+	version='0.1.0',
+	author='Kevin Lu',
+	author_email='kevin.n.lu123@gmail.com',
+	packages=['tdisql'],
+	license='LICENSE.txt',
+	url='http://pypi.python.org/pypi/TDI_DB/',
+	description='Module for accessing the TDI SQL database through Python.',
+	long_description=open('README.txt').read(),
+	install_requires=['MySQL-python'],
+)
