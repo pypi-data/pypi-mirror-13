993fc5868aa3f01d41e2b30332bff697145332fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:26 +0000

    Add package metzoo-python-sdk/0.2.3/metzoo-python-sdk-0.2.3.tar.gz

---
+++ packages/metzoo-python-sdk/metzoo-python-sdk-0.2.3.tar.gz/metzoo-python-sdk-0.2.3/metzoo/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'metzoo-python-sdk'
+__version__ = '0.2.3'
+__build__ = 0x000203
+__author__ = 'Edrans S.A.'
+__copyright__ = 'Copyright 2016 Edrans S.A.'
+
+from .agent import Agent
+from .errors import MetzooError, AgentCreationError, InvalidData, AgentDataError
