77ab7caa1eec48feb70a3a0057d7ab8362fa027b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:28 +0000

    Add package avendesora/0.0.3/avendesora-0.0.3.tar.gz

---
+++ packages/avendesora/avendesora-0.0.3.tar.gz/avendesora-0.0.3/avendesora/files.py
@@ -0,0 +1,66 @@
+#
+# READ or WRITE POSSIBLY ENCRYPTED PYTHON CODE FILES
+#
+
+from pathlib import Path
+from inform import display, error, narrate, debug, os_error
+from .preferences import (
+    SETTINGS_DIR, DEFAULT_ACCOUNTS_FILENAME, DEFAULT_LOG_FILENAME, 
+    DEFAULT_ARCHIVE_FILENAME
+)
+
+class AccountFile:
+    def __init__(self, path, gpg, generator, init=None, contents=''):
+        path = Path(path).expanduser()
+        try:
+            Path(SETTINGS_DIR).expanduser().mkdir(parents=True, exist_ok=True)
+            if init and path.exists():
+                display("%s: already exists." % path)
+                # file creation (init) requested, but file already exists
+                # don't overwrite the file, instead read it so the information 
+                # can be used to create any remaining files.
+            if init and not path.exists():
+                # create the file
+                code = contents.format(
+                    dict_hash='not implemented yet',
+                    secrets_hash='not implemented yet',
+                    charsets_hash='not implemented yet',
+                    accounts_file=DEFAULT_ACCOUNTS_FILENAME,
+                    log_file=DEFAULT_LOG_FILENAME,
+                    archive_file=DEFAULT_ARCHIVE_FILENAME,
+                    gpg_id=gpg.gpg_id,
+                    gpg_home='~/.gnupg',
+                    gpg_path='/usr/bin/gpg2',
+                    section='{''{''{''1',
+                    master_password='not implemented yet',
+                )
+                display('%s: creating.' % path)
+                if path.suffix in ['.gpg', '.asc']:
+                    narrate('encrypting.', culprit=path)
+                    # encrypt it
+                    gpg.save(Path(path), code)
+                else:
+                    narrate('not encrypting.', culprit=path)
+                    # file is not encrypted
+                    with path.open('w') as f:
+                        f.write(code)
+            else:
+                # read the file
+                if path.suffix in ['.gpg', '.asc']:
+                    # file is encrypted, decrypt it
+                    code = gpg.read(Path(path))
+                else:
+                    # file is not encrypted
+                    code = path.read_text()
+        except OSError as exception:
+            error(os_error(exception))
+
+        contents = {}
+        compiled = compile(code, str(path), 'exec')
+        exec(compiled, contents)
+        if 'master_password' in contents:
+            generator.add_missing_master(contents['master_password'])
+        self.contents = contents
+
+    def __getattr__(self, name):
+        return self.contents[name]
---
+++ packages/avendesora/avendesora-0.0.3.tar.gz/avendesora-0.0.3/avendesora/generator.py
@@ -0,0 +1,77 @@
+from .preferences import (
+    SETTINGS_DIR, CONFIG_FILENAME, CONFIG_FILE_INITIAL_CONTENTS,
+    ACCOUNTS_FILE_INITIAL_CONTENTS
+)
+from .files import AccountFile
+from .gpg import GPG
+from .dictionary import DICTIONARY
+from .account import Account
+from inform import Error, debug, terminate_if_errors
+from pathlib import Path
+
+class PasswordGenerator:
+    def __init__(self, init=False, gpg_id=None):
+        self.gpg = GPG(gpg_id)
+        self.active_account = None
+
+        # First open the config file
+        self.config = AccountFile(
+            Path(SETTINGS_DIR, CONFIG_FILENAME),
+            self.gpg,
+            self,
+            init,
+            CONFIG_FILE_INITIAL_CONTENTS,
+        )
+
+        # Now open any accounts files found
+        for filename in self.config.accounts_files:
+            AccountFile(
+                Path(SETTINGS_DIR, filename),
+                self.gpg,
+                self,
+                init,
+                ACCOUNTS_FILE_INITIAL_CONTENTS,
+            )
+        terminate_if_errors()
+
+        DICTIONARY.validate(self.config.dict_hash)
+
+    def activate_account(self, account):
+        if not account:
+            raise Error('no account specified.')
+        for each in Account.all_accounts():
+            if each.matches_exactly(account):
+                each.initialize()
+                self.active_account = each
+                return
+        raise Error('not found', culprit=account)
+
+    def get_secret(self, secret=None):
+        if not secret:
+             secret = self.active_account.get_value('default')
+        if not secret:
+             secret = 'passcode'
+        value = self.active_account.get_value(secret)
+        if value:
+            return value
+        else:
+            raise Error('not found', culprit=secret)
+
+    def find_accounts(self, target):
+        accounts = []
+        for each in Account.all_accounts():
+            if each.id_contains(target):
+                accounts.append(each)
+        return accounts
+
+    def search_accounts(self, target):
+        accounts = []
+        for each in Account.all_accounts():
+            if each.account_contains(target):
+                accounts.append(each)
+        return accounts
+
+    def add_missing_master(self, master):
+        for each in Account.all_accounts():
+            if not hasattr(each, 'master'):
+                each.master = master
