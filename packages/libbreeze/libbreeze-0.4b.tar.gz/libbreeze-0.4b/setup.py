859ff32b8c4c2dbee94c0bda468a10f8342916bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:09 +0000

    Add package libbreeze/0.4b/libbreeze-0.4b.tar.gz

---
+++ packages/libbreeze/libbreeze-0.4b.tar.gz/libbreeze-0.4b/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+# HAX - vboxsf filesystem won't allow hardlinks, so...
+del os.link
+
+def read(filen):
+    with open(os.path.join(os.path.dirname(__file__), filen), "r") as fp:
+        return fp.read()
+ 
+config = dict(
+    name="libbreeze",
+    version="0.4b",
+    description="Assemble static sites",
+    long_description=read("README.md"),
+    author="Alec Elton",
+    author_email="alec.elton@gmail.com",
+    url="https://github.com/BasementCat/breeze",
+    packages=["breeze"],
+    install_requires=[
+        'PyYAML', 'jinja2', 'markdown', 'arrow', 'libsass',
+    ],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
+
+if __name__ == "__main__":
+    setup(**config)
