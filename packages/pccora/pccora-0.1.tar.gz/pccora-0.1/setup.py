1aa9e7c3cf99ef2a00e99c7f2d05c8ad4f33b01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:28 +0000

    Add package pccora/0.1/pccora-0.1.tar.gz

---
+++ packages/pccora/pccora-0.1.tar.gz/pccora-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pccora',
+      version='0.1',
+      description='PC-CORA sounding data files parser for Python',
+      url='http://github.com/niwa/pccora',
+      author='Bruno P. Kinoshita',
+      author_email='brunodepaulak@yahoo.com.br',
+      license='MIT',
+      keywords = ['sounding file', 'radiosonde', 'vaisala', 'pccora'],
+      packages=['pccora'],
+      zip_safe=False)
