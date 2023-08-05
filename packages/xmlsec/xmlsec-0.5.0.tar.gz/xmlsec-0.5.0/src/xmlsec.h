ec089ea75290f7506018663eb88ce286e7cac27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:59 +0000

    Add package xmlsec/0.5.0/xmlsec-0.5.0.tar.gz

---
+++ packages/xmlsec/xmlsec-0.5.0.tar.gz/xmlsec-0.5.0/src/xmlsec.h
@@ -0,0 +1,7 @@
+#include <xmlsec/xmltree.h>
+#include <xmlsec/xmldsig.h>
+#include <xmlsec/xmlenc.h>
+#include <xmlsec/templates.h>
+#include <xmlsec/errors.h>
+#include <xmlsec/openssl/app.h>
+#include <xmlsec/openssl/crypto.h>
---
+++ packages/xmlsec/xmlsec-0.5.0.tar.gz/xmlsec-0.5.0/src/xmlsec/meta.py
@@ -0,0 +1,2 @@
+version = '0.5.0'
+description = 'Python bindings for the XML Security Library.'
