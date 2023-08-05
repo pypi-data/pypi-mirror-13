7703c892953edd7b76050405debbc3a53ac40959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package convertkit/0.4/convertkit-0.4.tar.gz

---
+++ packages/convertkit/convertkit-0.4.tar.gz/convertkit-0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='convertkit',
+    version='0.4',
+    description='API Client for ConvertKit v3',
+    license='BSD',
+    author='Justin Abrahms',
+    author_email='justin@abrah.ms',
+    url='https://github.com/justinabrahms/python-convertkit/',
+    packages=['convertkit'],
+    install_requires=[
+        'requests >=1.0.0'
+    ]
+)
