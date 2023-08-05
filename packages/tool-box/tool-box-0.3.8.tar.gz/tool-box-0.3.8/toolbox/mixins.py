e61d2bd76e1d96a04d3e5c140d97314ac95d6713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:52 +0000

    Add package tool-box/0.3.8/tool-box-0.3.8.tar.gz

---
+++ packages/tool-box/tool-box-0.3.8.tar.gz/tool-box-0.3.8/toolbox/mixins.py
@@ -0,0 +1,23 @@
+__author__ = 'jeff'
+
+class RegistryMixin(object):
+
+
+    _registry = None
+
+    def set_registry(self, registry):
+        self._registry = registry
+
+    def get_registry(self):
+        return self._registry
+
+
+class ConfigMixin(object):
+    def set_config(self, config):
+        self._config = config
+
+    def get_config(self):
+        return self._config
+
+    def get_global_config(self):
+        return self.get_config().get_global_config()
