6e9d98a94a6f77eec5459417ff309a358a0b32b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package Edoardo/1.2.0/Edoardo-1.2.0.tar.gz

---
+++ packages/Edoardo/Edoardo-1.2.0.tar.gz/Edoardo-1.2.0/nester.py
@@ -0,0 +1,12 @@
+"""This scode prints on the shell the items of the list,
+if there is nested item"""
+
+def print_lol(the_list, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab in range(level):
+                print("\t", end='')
+            print(each_item)
+            
