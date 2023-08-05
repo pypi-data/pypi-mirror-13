a700f50cd9d875a57cc568d29b915a4c47c6d5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:04 +0000

    Add package django-easy-select2/1.3.2/django-easy-select2-1.3.2.tar.gz

---
+++ packages/django-easy-select2/django-easy-select2-1.3.2.tar.gz/django-easy-select2-1.3.2/easy_select2/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'select2_modelform_meta', 'select2_modelform',
+    'Select2', 'Select2Multiple', 'Select2Mixin', 'apply_select2',
+]
+
+from easy_select2.utils import (
+    select2_modelform_meta, select2_modelform, apply_select2,
+)
+from easy_select2.widgets import (
+    Select2Mixin, Select2, Select2Multiple,
+)
