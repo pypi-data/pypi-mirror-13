79bd0d1bf74cc93f0618c73ef4fcb8346c3d6b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:13 +0000

    Add package vipython/0.0.1/vipython-0.0.1.tar.gz

---
+++ packages/vipython/vipython-0.0.1.tar.gz/vipython-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='vipython',
+    version='0.0.1',
+    description='Use vim with the ipython notebook',
+    license='MIT',
+    url='https://github.com/Russell91/vipython',
+    long_description='https://github.com/Russell91/vipython',
+    scripts = ['vipython_notebook'],
+)
