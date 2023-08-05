47191af81c28c6a1d10b8e9b0cbf2ec00cd63c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package dotter/0.5.0/dotter-0.5.0.tar.gz

---
+++ packages/dotter/dotter-0.5.0.tar.gz/dotter-0.5.0/dotter/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+dotter
+
+Copyright (c) 2013-2014, Friedrich Paetzke (paetzke@fastmail.fm)
+All rights reserved.
+
+"""
+from .dotter import Dotter, Program, RankType, Shape, Style, Directions
+
+__version__ = '0.5.0'
+__author__ = 'Friedrich Paetzke'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2013-2014 Friedrich Paetzke'
---
+++ packages/dotter/dotter-0.5.0.tar.gz/dotter-0.5.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+dotter
+
+Copyright (c) 2013, Friedrich Paetzke (paetzke@fastmail.fm)
+All rights reserved.
+
+"""
+from setuptools import find_packages, setup
+
+setup(name='dotter',
+      py_modules=['dotter'],
+      description='Dotter is a graphviz wrapper for Python 2 and 3',
+      long_description=(open('README.rst').read()),
+      version='0.5.0',
+      license='BSD',
+      author='Friedrich Paetzke',
+      author_email='paetzke@fastmail.fm',
+      url='https://github.com/paetzke/dotter',
+      packages=find_packages(exclude=['tests*']),
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
+      ])
