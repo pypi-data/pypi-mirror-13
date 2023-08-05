b35d1772bfa753a82dc488bb5eefbc608edaa416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:33 +0000

    Add package WASH/1.6.1/WASH-1.6.1.tar.gz

---
+++ packages/WASH/WASH-1.6.1.tar.gz/WASH-1.6.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import sys
+
+setup(name='WASH',
+		version='1.6.1', 
+		description='Wolfram|Alpha interactive shell', 
+		author='Cody Brocious', 
+		author_email='cody.brocious@gmail.com', 
+		py_modules=['wash'], 
+		entry_points=dict(
+			console_scripts=[
+				'wash = wash:main'
+			]
+		), 
+		install_requires=[
+			'beautifulsoup4', 
+			'colorama', 
+			'mako', 
+			'requests',
+		] + (['pyreadline'] if sys.platform == 'win32' else []), 
+		license='CC0', 
+		url='https://pypi.python.org/pypi/WASH'
+	)
