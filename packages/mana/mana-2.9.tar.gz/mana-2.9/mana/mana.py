64836e49426f0f5d11286932427f96cb94d8ed23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package mana/2.9/mana-2.9.tar.gz

---
+++ packages/mana/mana-2.9.tar.gz/mana-2.9/mana/mana.py
@@ -0,0 +1,250 @@
+# coding: utf-8
+"""
+mana
+~~~~
+the missing startapp command for flask
+
+Usage:
+
+    mana init <project_name>
+
+    mana sqlinit <project_name>
+
+    mana blueprint <blueprint_name>
+
+    mana deploy <project_name>
+    \__ Host IP:
+    \__ Port:
+
+Options:
+
+    --help:     help information
+    --version:  show version
+    --home:     link to the doc page
+"""
+
+import sys
+import os
+
+# project root path
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
+# operators
+from operators import _mkdir_p
+from operators import init_code
+
+# templates
+from templates.manage import _manage_basic_code, _manage_sql_code
+from templates.requirement import _requirement_code
+from templates.views import _views_basic_code, _views_blueprint_code
+from templates.forms import _forms_basic_code
+from templates.init import _init_basic_code, _init_code, _init_blueprint_code
+from templates.config import _config_sql_code
+from templates.models import _models_code
+
+# logging
+import logging
+from logging import StreamHandler, DEBUG
+# logger
+logger = logging.getLogger(__name__)
+logger.setLevel(DEBUG)
+logger.addHandler(StreamHandler())
+
+
+"""use click:)"""
+import click
+
+@click.group()
+def cli():
+    """the missing startapp command for Flask"""
+    pass
+
+
+@click.command()
+@click.argument('project_name')
+def init(project_name):
+    """
+    mana init <project_name>
+    """
+
+    # the destination path
+    dst_path = os.path.join(os.getcwd(), project_name)
+
+    # dst path exist
+    if os.path.isdir(dst_path):
+        logger.warning("path: %s exist,\nplease change the project name\n \
+                and try again!" % dst_path)
+        return
+
+    # start init
+    logger.info("start init your flask project!")
+
+    # create dst path
+    _mkdir_p(dst_path)
+
+    # create project tree
+    os.chdir(dst_path)
+
+    # create files
+    init_code('manage.py', _manage_basic_code)
+    init_code('requirement.txt', _requirement_code)
+
+    # create app/
+    app_path = os.path.join(dst_path, 'app')
+    _mkdir_p(app_path)
+
+    os.chdir(app_path)
+    # create files
+    init_code('views.py', _views_basic_code)
+    init_code('forms.py', _forms_basic_code)
+    init_code('__init__.py', _init_basic_code)
+
+    # create templates and static
+    templates_path = os.path.join(app_path, 'templates')
+    static_path = os.path.join(app_path, 'static')
+
+    logger.info("init flask project <%s> done! " % project_name)
+
+
+@click.command()
+@click.argument('project_name')
+def sqlinit(project_name):
+    """
+    mana sqlinit <project_name>
+    """
+
+    # the destination path
+    dst_path = os.path.join(os.getcwd(), project_name)
+
+    # dst path exist
+    if os.path.isdir(dst_path):
+        logger.warning("path: %s exist,\nplease change the project name\n \
+                and try again!" % dst_path)
+        return
+
+    # start init
+    logger.info("start init your flask project!")
+
+    # create dst path
+    _mkdir_p(dst_path)
+
+    # create project tree
+    os.chdir(dst_path)
+    # create files
+    init_code('manage.py', _manage_sql_code)
+    init_code('requirement.txt', _requirement_code)
+    init_code('config.py', _config_sql_code)
+
+    # create app/
+    app_path = os.path.join(dst_path, 'app')
+    _mkdir_p(app_path)
+
+    # create files
+    os.chdir(app_path)
+    init_code('models.py', _models_code)
+    init_code('__init__.py', _init_code)
+
+    # create main/
+    main_path = os.path.join(app_path, 'main')
+    _mkdir_p(main_path)
+
+    os.chdir(main_path)
+    # create files
+    init_code('views.py', _views_blueprint_code % ('main', 'main'))
+    init_code('forms.py', _forms_basic_code)
+    init_code('__init__.py', _init_blueprint_code % ('main', 'main'))
+
+    # create templates and static
+    templates_path = os.path.join(main_path, 'templates')
+    static_path = os.path.join(main_path, 'static')
+
+    logger.info("init flask project <%s> done! " % project_name)
+
+
+@click.command()
+@click.argument('blueprint_name')
+def blueprint(blueprint_name):
+    """
+    mana blueprint <blueprint_name>
+    """
+
+    # destination path
+    dst_path = os.path.join(os.getcwd(), blueprint_name)
+
+    # dst path exist
+    if os.path.isdir(dst_path):
+        logger.warning("path: %s exist,\nplease change the project name\n \
+                and try again!" % dst_path)
+        return
+
+    # create dst_path
+    _mkdir_p(dst_path)
+
+    # change dir
+    os.chdir(dst_path)
+    # create files
+    init_code('__init__.py', _init_blueprint_code % (blueprint_name, blueprint_name))
+    init_code('views.py', _views_blueprint_code % (blueprint_name, blueprint_name))
+    init_code('forms.py', _forms_basic_code)
+
+    # create templates
+    templates_path = os.path.join(dst_path, 'templates')
+    _mkdir_p(templates_path)
+    # create static
+    static_path = os.path.join(dst_path, 'static')
+    _mkdir_p(static_path)
+
+    # register auth in app
+    os.chdir(os.path.join(dst_path, '..'))
+    with open('__init__.py', 'r+') as f:
+        prev = pos = 0
+        while f.readline():
+            prev, pos = pos, f.tell()
+        f.seek(prev)
+        f.write(
+            '    from %s import %s\n    app.register_blueprint(%s, url_prefix="/%s")\n\n    return app'
+            % (
+                blueprint_name, blueprint_name,
+                blueprint_name, blueprint_name
+            )
+        )
+
+    logger.info("init flask blueprint <%s> done! " % blueprint_name)
+
+
+# @click.command()
+# @click.argument('project_name')
+# def deploy(project_name):
+#     """
+#     mana deploy <project_name>
+#     """
+#
+#     host_ip = click.prompt('\__ Host IP: ')
+#     port = click.prompt('\__ Port: ', type=int)
+#
+#     logger.info('start deploy your flask project !')
+#     init_code('wsgi.py')
+
+
+@click.command()
+def version():
+    """mana version"""
+    click.echo("mana version: 2.9 🍺 ")
+
+
+@click.command()
+def home():
+    """mana homepage"""
+    os.system('python -m webbrowser -t "http://121.43.230.104:520/mana"')
+
+
+# mana command set
+# ^o^ --> 0v0 --> {O.O}
+cli.add_command(init)
+cli.add_command(sqlinit)
+cli.add_command(blueprint)
+cli.add_command(version)
+cli.add_command(home)
+# ^o^ <-- 0v0 <-- {O.O}
---
+++ packages/mana/mana-2.9.tar.gz/mana-2.9/setup.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+"""
+    mana
+    ~~~~
+
+    the missing startapp command for Flask
+
+"""
+
+from setuptools import setup, find_packages
+import mana
+
+
+# entry_points
+entry_points = {
+    'console_scripts':[
+        'mana = mana.mana:cli'
+    ]
+}
+
+
+# requires
+with open('requirement.txt') as f:
+    requires = [exts for exts in f.read().splitlines() if exts]
+
+
+setup(
+    name='mana',
+    version='2.9',
+    packages=find_packages(),
+    url='https://github.com/neo1218/mana',
+    license='MIT',
+    author='neo1218',
+    author_email='neo1218@yeah.net',
+    description='the missing startapp command for Flask',
+    long_description=__doc__,
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=requires,
+    entry_points=entry_points,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
