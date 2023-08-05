d8a3dbd3637601970c4b3050914cde0a3a6aa698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:55 +0000

    Add package pyNam/0.4/pyNam-0.4.tar.gz

---
+++ packages/pyNam/pyNam-0.4.tar.gz/pyNam-0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyNam',
+  packages = ['pyNam'], # this must be the same as the name above
+  version = '0.4',
+  description = 'testest!',
+  author = 'Naman Jain',
+  author_email = 'namanjn07@hotmail.com',
+  url = 'https://github.com/Namanjn/namuskj', # use the URL to the github repo
+  download_url = 'https://github.com/Namanjn/PyNamu/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
