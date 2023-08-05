add08a8430033cf949445c61814231bc44db88fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package dj-utils/0.3.5/dj_utils-0.3.5-py2-none-any.whl

---
+++ packages/dj-utils/dj_utils-0.3.5-py2-none-any.whl/dj_utils/fields/__init__.py
@@ -0,0 +1,2 @@
+from .email import NullableEmailField
+from .o2o import AutoOneToOneField
---
+++ packages/dj-utils/dj_utils-0.3.5-py2-none-any.whl/dj_utils/management/commands/run_cyclictask.py
@@ -0,0 +1,14 @@
+from dj_utils import cyclictask_core
+from dj_utils.management import LoggingBaseCommand
+
+
+class Command(LoggingBaseCommand):
+    args = '<pid file path>'
+    help = 'Run cyclic tasks.'
+
+    def handle(self, *args, **options):
+        try:
+            pid_fn = args[0]
+        except IndexError:
+            pid_fn = None
+        cyclictask_core.init(pid_fn=pid_fn)
