1bd63337a4391c375f8e85d13868db24f2b8ec98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package aspose_pdf_java_for_python/1.0.0/aspose_pdf_java_for_python-1.0.0.tar.gz

---
+++ packages/aspose_pdf_java_for_python/aspose_pdf_java_for_python-1.0.0.tar.gz/aspose_pdf_java_for_python-1.0.0/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'fahadadeel'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aspose_pdf_java_for_python',
+    packages = find_packages(),
+    version = '1.0.0',
+    description = 'Aspose.Pdf Java for Python',
+    author='Fahad Adeel',
+    author_email='fahadadeel@gmail.com',
+    url='https://github.com/asposepdf/Aspose_Pdf_Java/tree/master/Plugins/Aspose_Pdf_Java_for_Python',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
