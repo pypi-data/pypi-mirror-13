15a393b37bb0e145a416bafba99421ff1777c04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:34 +0000

    Add package s3import/0.4.0/s3import-0.4.0-py2-none-any.whl

---
+++ packages/s3import/s3import-0.4.0-py2-none-any.whl/s3import/exceptions.py
@@ -0,0 +1,10 @@
+'''
+Copyright (c) 2015 Skylable Ltd.
+License: Apache 2.0, see LICENSE for more details.
+
+'''
+__all__ = ['S3ImportException']
+
+
+class S3ImportException(Exception):
+    '''General exception type of s3import.'''
---
+++ packages/s3import/s3import-0.4.0-py2-none-any.whl/s3import/tools/__init__.py
@@ -0,0 +1,7 @@
+'''
+Copyright (c) 2015 Skylable Ltd.
+License: Apache 2.0, see LICENSE for more details.
+
+'''
+from s3import.tools.converters import *
+from s3import.tools.string_helpers import *
