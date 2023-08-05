6851e0ee6ba1bbec77a6a866a1e83d3cdd7b6bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package annobase/0.0.1/annobase-0.0.1.tar.gz

---
+++ packages/annobase/annobase-0.0.1.tar.gz/annobase-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='annobase',
+      packages=['annobase'],
+      version='0.0.1',
+      descriptions='a text annotation tool that aims to provide a light-weight but comprehensive represenation for text annotation',
+      url='http://junaraki.net/software/annobase',
+      author='Jun Araki',
+      author_email='mail@junaraki.net',
+)
