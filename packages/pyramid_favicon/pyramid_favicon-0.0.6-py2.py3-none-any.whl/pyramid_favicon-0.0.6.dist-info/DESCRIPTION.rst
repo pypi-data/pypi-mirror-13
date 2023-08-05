9cf28997b4c9933e25fca7524ba52ce89740b87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:15 +0000

    Add package pyramid_favicon/0.0.6/pyramid_favicon-0.0.6-py2.py3-none-any.whl

---
+++ packages/pyramid_favicon/pyramid_favicon-0.0.6-py2.py3-none-any.whl/pyramid_favicon-0.0.6.dist-info/DESCRIPTION.rst
@@ -0,0 +1,10 @@
+``pyramid_favicon``
+===================
+
+``pyramid_favicon`` is an add-on for handling favicons in your Pyramid project. Normally,
+the favicon.ico file is stored on a web site's project root (e.g. /myproject_root/favicon.ico).
+However, with this add-on, it is possible to specify a different location of favicon.ico
+via a custom config setting (e.g. /myproject_root/some/other/location/favicon.ico).
+
+
+
---
+++ packages/pyramid_favicon/pyramid_favicon-0.0.6-py2.py3-none-any.whl/pyramid_favicon/favicon.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Serve the favicon.ico at the application root."""
+import os
+
+from pyramid.path import AssetResolver
+from pyramid.response import FileResponse
+from pyramid.security import NO_PERMISSION_REQUIRED
+from pyramid.view import view_config
+
+
+@view_config(
+    route_name='favicon',
+    permission=NO_PERMISSION_REQUIRED,
+)
+def favicon(request):
+    
+    settings = request.registry.settings
+    favicon_path = settings.get('favicon_path', None)    
+
+    a = AssetResolver()
+    if favicon_path:
+        #only if an absolute path was given; otherwise, we'll accept
+        #package path+'favicon.ico', e.g., 'myproject:static/favicon.ico'
+        if os.path.isabs(favicon_path):
+            favicon_path = os.path.join(favicon_path, 'favicon.ico')
+        resolver = a.resolve(favicon_path)        
+    else:
+        resolver = a.resolve('favicon.ico')
+    
+    icon_path = resolver.abspath()
+    
+    return FileResponse(icon_path   , request=request)
