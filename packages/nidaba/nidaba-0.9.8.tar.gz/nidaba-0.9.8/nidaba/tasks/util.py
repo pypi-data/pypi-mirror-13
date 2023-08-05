5347b14a89bd76c0b3f9a24ca86e37537a8ef5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:16 +0000

    Add package nidaba/0.9.8/nidaba-0.9.8.tar.gz

---
+++ packages/nidaba/nidaba-0.9.8.tar.gz/nidaba-0.9.8/nidaba/tasks/util.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+nidaba.tasks.util
+~~~~~~~~~~~~~~~~~
+
+Various utility tasks that either can't be classified by purpose or are one of
+a kind.
+
+"""
+
+from __future__ import unicode_literals, print_function, absolute_import
+
+from nidaba.tasks.helper import NidabaTask
+from nidaba.celery import app
+
+
+@app.task(base=NidabaTask, name=u'nidaba.util.sync')
+def sync(doc):
+    """
+    Takes ones argument and returns it. Used to synchronized stuff as
+    chaining groups is not possible with the current celery version.
+
+    Args:
+        doc: An arbitrary input argument
+
+    Returns:
+        The input argument unaltered
+    """
+    return doc
