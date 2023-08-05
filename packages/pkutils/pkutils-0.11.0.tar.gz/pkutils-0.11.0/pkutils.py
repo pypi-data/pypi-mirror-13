0dac915b6a8587e1e42ddd2a33d85963f489e4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:54 +0000

    Add package pkutils/0.11.0/pkutils-0.11.0.tar.gz

---
+++ packages/pkutils/pkutils-0.11.0.tar.gz/pkutils-0.11.0/pkutils.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+
+"""
+pkutils
+~~~~~~~
+
+Provides methods that simplify python module packaging.
+
+Examples:
+    parse requirements file::
+
+        >>> requirements = parse_requirements('requirements.txt')
+        >>> len(list(requirements)) > 0
+        True
+        >>> get_status('0.3.0') == 'Development Status :: 2 - Pre-Alpha'
+        True
+
+
+
+Attributes:
+    LICENSES (dict): available python license classifiers.
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, with_statement,
+    unicode_literals)
+
+import re
+
+from bisect import bisect
+from os import path as p
+from functools import total_ordering
+
+import semver
+
+from builtins import *
+
+__version__ = '0.11.0'
+
+__title__ = 'pkutils'
+__author__ = 'Reuben Cummings'
+__description__ = 'Python packaging utility library'
+__email__ = 'reubano@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Reuben Cummings'
+
+LICENSES = {
+    'GPL': 'License :: OSI Approved :: GNU General Public License (GPL)',
+    'MIT': 'License :: OSI Approved :: MIT License',
+    'BSD': 'License :: OSI Approved :: BSD License',
+}
+
+STATUSES = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 6 - Mature']
+
+
+@total_ordering
+class Version(object):
+    """A semver version
+
+    Args:
+        version (str): A semver valid version.
+
+    Examples:
+        >>> v1 = Version('0.3.0')
+        >>> v2 = Version('0.10.0')
+        >>> v1, v2
+        (<Version 0.3.0>, <Version 0.10.0>)
+        >>> str(v1), str(v2)
+        ('0.3.0', '0.10.0')
+        >>> str(v2) > str(v1)
+        False
+        >>> v2 > v1
+        True
+    """
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, other):
+        return semver.compare(self.value, other.value) == 0
+
+    def __lt__(self, other):
+        return semver.compare(self.value, other.value) == -1
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return '<Version %s>' % self.value
+
+
+def get_status(version, breaks=None):
+    """Categorizes a version into a development status.
+
+    Args:
+        version (str): A semver valid version.
+        breaks (seq[str]): The version cutoffs
+
+    Returns:
+        str: The version status
+
+    Examples:
+        >>> get_status('0.3.0') == 'Development Status :: 2 - Pre-Alpha'
+        True
+        >>> get_status('0.9.0') == 'Development Status :: 3 - Alpha'
+        True
+    """
+    def_breaks = ('0.5.0', '0.10.0', '1.0.0', '2.0.0')
+    breakpoints = [Version(b) for b in breaks or def_breaks]
+    return STATUSES[bisect(breakpoints, Version(version))]
+
+
+def get_url(project, user, base='https://github.com'):
+    """Gets the repo download url.
+
+    Args:
+        user (str): The username.
+        base (str): The hosting site (default: 'https://github.com').
+
+    Returns:
+        str: The url
+
+    Examples:
+        >>> get_url('pkutils', 'reubano') == (
+        ...     'https://github.com/reubano/pkutils')
+        True
+    """
+    return '%s/%s/%s' % (base, user, project)
+
+
+def get_dl_url(project, user, version, base='https://github.com', ext='tar.gz'):
+    """Gets the package download url.
+
+    Args:
+        version (str): A semver valid version.
+        user (str): The username.
+        base (str): The hosting site (default: 'https://github.com').
+        ext (str): The file extension (default: 'tar.gz').
+
+    Returns:
+        str: The download url
+
+    Examples:
+        >>> get_dl_url('pkutils', 'reubano', '0.3.0') == (
+        ...     'https://github.com/reubano/pkutils/archive/v0.3.0.tar.gz')
+        True
+    """
+    return '%s/%s/%s/archive/v%s.%s' % (base, user, project, version, ext)
+
+
+def read(filename):
+    """Reads a file.
+
+    Args:
+        filename (str): The file name.
+
+    Returns:
+        File content
+
+    Examples:
+        >>> read('README.rst').split('\\n')[0] == (
+        ...     'pkutils: a Python packaging library')
+        True
+    """
+    try:
+        with open(filename, encoding='utf-8') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+def parse_requirements(filename, dep=False):
+    """Iteratively parses requirements files. Handles `-r` and `-e` options.
+
+    Args:
+        filename (str): The file name.
+        dep (bool): Process dependency links (default: False).
+
+    Yields:
+        (str): A requirement
+
+    Examples:
+        >>> next(parse_requirements('dev-requirements.txt')) == 'semver==2.2.1'
+        True
+    """
+    try:
+        with open(filename, encoding='utf-8') as f:
+            for line in f:
+                candidate = line.strip()
+
+                if candidate.startswith('-r'):
+                    parent = p.dirname(filename)
+                    new_filename = p.join(parent, candidate[2:].strip())
+
+                    for item in parse_requirements(new_filename, dep):
+                        yield item
+                elif not dep and '#egg=' in candidate:
+                    yield re.sub('.*#egg=(.*)-(.*)', r'\1==\2', candidate)
+                elif dep and '#egg=' in candidate:
+                    yield candidate.replace('-e ', '')
+                elif not dep:
+                    yield candidate
+    except IOError:
+        yield ''
