8c228f307e7a3f82dd80de084d7f04d1d16a7859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:44 +0000

    Add package ktorrent/0.4.0/ktorrent-0.4.0.tar.gz

---
+++ packages/ktorrent/ktorrent-0.4.0.tar.gz/ktorrent-0.4.0/ktorrent/test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+import ktorrent, json
+
+class kTorrentTest(TestCase):
+
+    def testConnection(self):
+        # Check connection
+        output = ktorrent.search(search='linux')
+        self.failIfEqual(output,"Couldn't retrieve data")
+
+    def testSearch(self):
+        # search: Validate JSON
+        output = ktorrent.search(search='linux')
+        self.assertEqual( type( json.loads(output) ) is dict , True)
+
+    def testTop(self):
+        # top: Validate JSON
+        output = ktorrent.top(category='movies', page=2)
+        self.assertEqual( type( json.loads(output) ) is dict , True)
+
+if __name__ == '__main__':
+    unittest.main()
