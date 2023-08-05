8a3d9b737cfa55968469530ec5a9bdc6d17bbe85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package pandoc-tablenos/0.7.1/pandoc-tablenos-0.7.1.tar.gz

---
+++ packages/pandoc-tablenos/pandoc-tablenos-0.7.1.tar.gz/pandoc-tablenos-0.7.1/setup.py
@@ -0,0 +1,40 @@
+"""setup.py - install script for pandoc-tablenos."""
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+
+LONG_DESCRIPTION = """\
+pandoc-tablenos is a pandoc filter for numbering tables and table references.
+"""
+
+VERSION = '0.7.1'
+
+setup(
+    name='pandoc-tablenos',
+    version=VERSION,
+
+    author='Thomas J. Duck',
+    author_email='tomduck@tomduck.ca',
+    description='Table number filter for pandoc',
+    long_description=LONG_DESCRIPTION,
+    license='GPL',
+    keywords='pandoc table numbers filter',
+    url='https://github.com/tomduck/pandoc-tablenos',
+    download_url = 'https://github.com/tomduck/pandoc-tablenos/tarball/' + \
+                   VERSION,
+    
+    install_requires=['pandocfilters', 'pandoc-attributes'],
+
+    py_modules=['pandoc_tablenos'],
+    entry_points={'console_scripts':['pandoc-tablenos = pandoc_tablenos:main']},
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python'
+        ],
+)
