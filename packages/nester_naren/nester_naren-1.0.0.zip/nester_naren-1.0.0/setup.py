5365d09a8d42b321a728939ec850896d89b4955a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package nester_naren/1.0.0/nester_naren-1.0.0.zip

---
+++ packages/nester_naren/nester_naren-1.0.0.zip/nester_naren-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_naren',
+    version = '1.0.0',
+    py_modules = ['Nester'],
+    author='Narendran',
+    author_email='naren.2ndece@gmail.com',
+    description="A simple printer of nested lists"
+)
