b2f04bb24b076f4ef9fb4476e1a78c46d0a04886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package python_hangman/2.2.0/python_hangman-2.2.0-py2.py3-none-any.whl

---
+++ packages/python_hangman/python_hangman-2.2.0-py2.py3-none-any.whl/hangman/controller.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+"""
+hangman.controller
+~~~~~~~~~~~~~~~~~~
+
+This module is responsible for guiding the user through the game.
+"""
+
+
+from hangman.utils import FlashMessage, GameOver, GameWon, GameFinished
+from . import view
+from .model import Hangman
+
+
+def game_loop(game=Hangman(), flash=FlashMessage()):
+    while True:
+        try:
+            view.draw_board(game, message=flash)
+            letter = view.prompt_guess()
+            game.guess(letter)
+        except GameOver:
+            flash.game_over = True
+            flash.game_answer = game.answer
+        except GameWon:
+            flash.game_won = True
+        except ValueError as msg:
+            flash(msg)
+        except GameFinished:
+            break
+
+
+def run(game=Hangman(), flash=FlashMessage()):
+    """
+    Run ``game_loop``, handle exit.
+
+    Logic is separated from game_loop to cleanly avoid recursion limits.
+
+    :param hangman.model.Hangman game: Hangman game instance.
+    :param hangman.utils.FlashMessage flash: FlashMessage utility
+    """
+
+    # noinspection PyPep8Naming
+    GameClass, FlashClass = game.__class__, flash.__class__
+    while True:
+        try:
+            game_loop(game=game, flash=flash)
+        except KeyboardInterrupt:
+            # Exit immediately
+            return view.say_goodbye()
+
+        if not view.prompt_play_again():
+            break
+
+        # reuse classes passed in from arguments
+        game, flash = GameClass(), FlashClass()
+
+    return view.say_goodbye()
