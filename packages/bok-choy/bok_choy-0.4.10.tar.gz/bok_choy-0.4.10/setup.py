97487f48d3233cdbf8557d2f28061b28bb961da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:36 +0000

    Add package bok-choy/0.4.10/bok_choy-0.4.10.tar.gz

---
+++ packages/bok-choy/bok_choy-0.4.10.tar.gz/bok_choy-0.4.10/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.4.10'
+DESCRIPTION = 'UI-level acceptance test framework'
+REQUIREMENTS = [
+    line.strip() for line in open("requirements.txt").readlines()
+]
+
+setup(
+    name='bok_choy',
+    version=VERSION,
+    author='edX',
+    url='http://github.com/edx/bok-choy',
+    description=DESCRIPTION,
+    license='Apache 2.0',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Testing',
+                 'Topic :: Software Development :: Quality Assurance'],
+    packages=['bok_choy', 'bok_choy/a11y'],
+    package_data={'bok_choy': ['vendor/google/*.*', 'vendor/axe-core/*.*']},
+    install_requires=REQUIREMENTS,
+)
