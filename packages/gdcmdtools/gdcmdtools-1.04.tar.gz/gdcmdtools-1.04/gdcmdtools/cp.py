ce2d59cd144a4e156c3657a36bbfe73d14829a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package gdcmdtools/1.04/gdcmdtools-1.04.tar.gz

---
+++ packages/gdcmdtools/gdcmdtools-1.04.tar.gz/gdcmdtools-1.04/gdcmdtools/cp.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+import sys
+import urllib
+import requests
+import json
+import pprint
+import re
+
+import logging
+logger = logging.getLogger("gdcp")
+
+import random
+import os
+
+import json
+
+from gdcmdtools.base import GDBase
+from gdcmdtools.perm import GDPerm
+from gdcmdtools.auth import GDAuth
+
+
+class GDCp:
+
+    def __init__(self, args):
+
+        for key, value in vars(args).items():
+            setattr(self, key, value)
+
+        auth = GDAuth()
+
+        creds = auth.get_credentials()
+        self.auth_user = creds.id_token.get("email", None)
+        if creds is None:
+            raise Exception("Failed to retrieve credentials")
+        self.http = auth.get_authorized_http()
+
+        base = GDBase()
+        self.service = base.get_drive_service(self.http)
+        self.root = base.get_root()
+
+        self.file_id = base.get_id_from_url(self.file_id)
+
+    def run(self):
+
+        if self.parent_folderId is None:
+            parents = []
+        else:
+            parents = [{
+                "kind": "drive#fileLink",
+                "id": self.parent_folderId}]
+
+        body = {
+            'title': self.new_title,
+            'description': self.target_description,
+            'parents': parents}
+
+        try:
+            response = self.service.files().copy(fileId=self.file_id, body=body).execute()
+        except Exception as e:
+            logger.error(e)
+            raise
+        else:
+            return response
