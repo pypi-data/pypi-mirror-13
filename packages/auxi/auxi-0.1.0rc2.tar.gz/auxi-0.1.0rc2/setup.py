104bd254ebe0b20b2b5141454c80a35f6478c9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:38 +0000

    Add package auxi/0.1.0rc2/auxi-0.1.0rc2.tar.gz

---
+++ packages/auxi/auxi-0.1.0rc2.tar.gz/auxi-0.1.0rc2/setup.py
@@ -0,0 +1,33 @@
+# USAGE: you probably want 'setup.py install' - but execute 'setup.py --help'
+# for all the details.
+
+# from setuptools import setup, Extension, Command
+
+from distutils.core import setup
+
+# build the distribution
+setup(name="auxi",
+      version="0.1.0rc2",
+      description="A toolkit to help metallurgical process engineers to rapidly do day-to-day calculations.",
+      long_description=open('README.txt').read(),
+      author="Ex Mente (Pty) Ltd",
+      author_email="dev@ex-mente.co.za",
+      maintainer="Ex Mente (Pty) Ltd",
+      maintainer_email="dev@ex-mente.co.za",
+      url="https://github.com/Ex-Mente/auxi.0",
+      download_url="https://pypi.python.org/pypi/auxi",
+      license='LICENSE.txt',
+      keywords="metallurgy,chemistry,modelling,simulation,thermochemistry,engineering,mass balance,energy balance",
+      platforms="Ubunutu 14.04,Ubuntu 15.04",
+      package_dir={'auxi': 'auxi'},
+      packages=["auxi", "auxi.core",
+                "auxi.modelling", "auxi.modelling.business", "auxi.modelling.accounting.financial", "auxi.modelling.accounting.stock", "auxi.simulation",
+                "auxi.tools", "auxi.tools.chemistry"],
+      package_data={'auxi.core': ['*.so*','*.so.1.54.0*'],
+                    'auxi.modelling.accounting.financial': ['*.a', '*.so*', '*_report.py'],
+                    'auxi.modelling.accounting.stock': ['*.a', '*.so*', '*_report.py'],
+                    'auxi.modelling.business': ['*.a', '*.so*', '*_report.py'],
+                    'auxi.simulation': ['*.py', r'io/*'],
+                    'auxi.tools.chemistry': ['*.so*','*.so.1.54.0*', r'data/*'],
+                    'auxi' : [r'*.txt']}
+      )
