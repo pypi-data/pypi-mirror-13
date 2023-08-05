d7c69206143dd07ca52be2b56ace39ad7216ae99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:21 +0000

    Add package nester_435/1.3.0/nester_435-1.3.0.zip

---
+++ packages/nester_435/nester_435-1.3.0.zip/nester_435-1.3.0/nester_435.py
@@ -0,0 +1,14 @@
+"""nester_435 fornece uma função chamada print_lol() que imprime os dados de
+uma lista. Esta função aceita como argumentos de entrada uma lista, True or
+False como opções de indentação, no caso de listas aninhadas, e level, que
+deve ser um integer com a quantidade de espaço em cada indentação"""
+
+def print_lol(the_list, indent = False, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
