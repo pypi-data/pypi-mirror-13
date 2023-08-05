8748d22cabdc33ee4f9f26e2e67ada4d6d8b4ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:08 +0000

    Add package code-checker/0.2.1/code-checker-0.2.1.tar.gz

---
+++ packages/code-checker/code-checker-0.2.1.tar.gz/code-checker-0.2.1/codechecker/__init__.py
@@ -0,0 +1,10 @@
+"""Code checker application.
+
+Exports:
+
+- :mod:`codechecker.checkers` - base checker tasks and builders
+- :mod:`codechecker.scripts` - console scripts logic
+- :mod:`codechecker.git` - get git repository informations
+- :mod:`codechecker.worker` - execute checkers tasks
+- :mod:`codechecker.checkers_spec` - define concrete checkers
+"""
