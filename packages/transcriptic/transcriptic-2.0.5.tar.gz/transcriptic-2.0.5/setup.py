6e03fff09dfedc3a2ea51dcc9fdb110ef7b7e1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:40 +0000

    Add package transcriptic/2.0.5/transcriptic-2.0.5.tar.gz

---
+++ packages/transcriptic/transcriptic-2.0.5.tar.gz/transcriptic-2.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='transcriptic',
+    description='Transcriptic CLI & Python Client Library',
+    url='https://github.com/transcriptic/transcriptic',
+    version='2.0.5',
+    packages=['transcriptic', 'transcriptic.analysis'],
+    install_requires=[
+        'Click>=5.1',
+        'requests',
+        'autoprotocol>=2.5',
+        'pandas>=0.16',
+        'matplotlib>=1.4',
+        'scikit-learn>=0.16',
+        'scipy>=0.16',
+        'numpy>=1.10',
+        'plotly>=1.8',
+        'future>=0.15'
+    ],
+    entry_points='''
+        [console_scripts]
+        transcriptic=transcriptic.cli:cli
+    '''
+)
