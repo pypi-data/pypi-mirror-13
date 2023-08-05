43e9f24f2d7bd86cac8e414ce38236376f640598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package DSDownload/1.4.1.0/DSDownload-1.4.1.0.tar.gz

---
+++ packages/DSDownload/DSDownload-1.4.1.0.tar.gz/DSDownload-1.4.1.0/setup.py
@@ -0,0 +1,22 @@
+
+from DSDownload.config import version, description, name
+from distutils.core import setup
+
+setup(
+  name = name,
+  packages = [name], 
+  version = version,
+  description = description,
+  author = 'Diego Siqueira',
+  author_email = 'dieg0@live.com',
+  url = 'https://github.com/DiSiqueira/DSDownload', 
+  download_url = 'https://github.com/DiSiqueira/DSDownload/tarball/'+version, 
+  keywords = ['download', 'thread', 'speed', 'resume', 'multi', 'simple'],
+  classifiers = [],
+  license='MIT',
+  entry_points = {
+          'console_scripts': [
+              'dsdownload = DSDownload.DSDownload:main',                  
+          ],              
+      },
+)
