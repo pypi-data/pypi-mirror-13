d787a8ffc7edf11055efcb45c0553df263b44d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package ndio/0.0.20/ndio-0.0.20.tar.gz

---
+++ packages/ndio/ndio-0.0.20.tar.gz/ndio-0.0.20/setup.py
@@ -0,0 +1,43 @@
+import ndio
+
+VERSION = ndio.version
+"""
+roll with
+git tag VERSION
+git push --tags
+python setup.py sdist upload -r pypi
+"""
+
+from distutils.core import setup
+setup(
+    name = 'ndio',
+    packages = [
+        'ndio',
+        'ndio.convert',
+        'ndio.ramon',
+        'ndio.remote',
+        'ndio.utils'
+    ],
+    version = VERSION,
+    description = 'A Python library for neuroscience data access and manipulation.',
+    author = 'Jordan Matelsky',
+    author_email = 'j6k4m8@gmail.com',
+    url = 'https://github.com/openconnectome/ndio',
+    download_url = 'https://github.com/openconnectome/ndio/tarball/' + VERSION,
+    keywords = [
+        'brain',
+        'medicine',
+        'microscopy',
+        'neuro',
+        'neurodata',
+        'neurodata.io',
+        'neuroscience',
+        'ndio',
+        'ocpy',
+        'ocp',
+        'ocp.me',
+        'connectome',
+        'connectomics'
+    ],
+    classifiers = [],
+)
