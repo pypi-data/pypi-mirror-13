ad146e2bed49c059a1b3a4067c77f2c8854060cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:25 +0000

    Add package insteon-hub/0.4.4/insteon-hub-0.4.4.tar.gz

---
+++ packages/insteon-hub/insteon-hub-0.4.4.tar.gz/insteon-hub-0.4.4/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+from insteon.version import VERSION
+
+REQUIRES = [
+    'requests>=2,<3',
+    'pyyaml>=3.11,<4'
+]
+
+setup(
+    name = "insteon-hub",
+    version = VERSION,
+    author = "Dean Galvin",
+    author_email = "deangalvin3@gmail.com",
+    description = ("A python package that interacts with the Insteon Hub Cloud API"),
+    license = "MIT",
+    keywords = ["insteon", "hub", "cloud"],
+    url = "https://github.com/FreekingDean/insteon-hub",
+    packages=['insteon'],
+    include_package_data=True,
+    install_requires=REQUIRES,
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Development Status :: 3 - Alpha",
+        "Topic :: Home Automation",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        "License :: OSI Approved :: MIT License",
+    ],
+)
