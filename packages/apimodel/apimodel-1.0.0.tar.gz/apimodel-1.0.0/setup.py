4be65eb09d7507e9e24f89278761da730c95c381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:39 +0000

    Add package apimodel/1.0.0/apimodel-1.0.0.tar.gz

---
+++ packages/apimodel/apimodel-1.0.0.tar.gz/apimodel-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='apimodel',
+    packages=['apimodel'],
+    version='1.0.0',
+    description='API-backed Django-style model',
+    author='Ethan McCreadie',
+    author_email='ethanmcc@gmail.com',
+    url='http://github.com/ethanmcc/apimodel',
+    keywords=['python', 'django', 'rest', 'api', 'model'],
+)
