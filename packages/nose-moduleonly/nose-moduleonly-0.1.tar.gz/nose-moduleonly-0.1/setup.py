87b5787c393bcaeea4b54217156715753faf563b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package nose-moduleonly/0.1/nose-moduleonly-0.1.tar.gz

---
+++ packages/nose-moduleonly/nose-moduleonly-0.1.tar.gz/nose-moduleonly-0.1/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nose-moduleonly',
+    version='0.1',
+    description="",
+    long_description="""""",
+    author='Craig de Stigter',
+    author_email='craig.ds@gmail.com',
+    license="BSD",
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=['Nose'],
+    url='https://github.com/craigds/nose-moduleonly',
+    include_package_data=True,
+    entry_points = {
+        'nose.plugins.0.10': [
+            'moduleonly = moduleonly.plugin:ModuleOnly',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing'
+        ],
+    )
