167d2817cb67977dbb831a2054288c08db223580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package ticket_auth/0.1.3/ticket_auth-0.1.3.tar.gz

---
+++ packages/ticket_auth/ticket_auth-0.1.3.tar.gz/ticket_auth-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="ticket_auth",
+    version='0.1.3',
+    description='Ticket authentication system similar to mod_auth_tkt used by Apache',
+    setup_requires=['setuptools-markdown'],
+    long_description=open('README.rst').read(),
+    packages=['ticket_auth'],
+    author='Gnarly Chicken',
+    author_email='gnarlychicken@gmx.com',
+    test_suite='tests',
+    url='https://github.com/gnarlychicken/ticket_auth',
+    license='MIT',
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Internet :: WWW/HTTP :: Session',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ],
+    keywords='mod_auth_tkt authentication session ticket')
