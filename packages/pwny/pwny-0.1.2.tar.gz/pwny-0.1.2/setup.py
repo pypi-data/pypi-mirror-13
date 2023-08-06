31d87a042c2f9a7d00445dcbdbee7866d3d051ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package pwny/0.1.2/pwny-0.1.2.tar.gz

---
+++ packages/pwny/pwny-0.1.2.tar.gz/pwny-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name="pwny",
+    version="0.1.2",
+    author="André Kupka",
+    author_email="kupka@in.tum.de",
+    description="A small binary exploitation framework.",
+    license="MIT",
+    keywords="binary exploitation pwning pwny",
+    url="http://packages.python.org/pwny",
+    packages=["pwny"],
+    long_description="",
+    classifiers=[]
+)
