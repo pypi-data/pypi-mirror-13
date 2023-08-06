228f10a05ab021fca50eea23f3a245690bd364d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:25 +0000

    Add package nesterjnam/1.2.0/nesterjnam-1.2.0.zip

---
+++ packages/nesterjnam/nesterjnam-1.2.0.zip/nesterjnam-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'nesterjnam',
+    version='1.2.0',
+    py_modules=['nesterjnam'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='a simple printer of nested lists',
+    )
