806810cc7d90877ad0c0de2aa399ad481e4714c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:07 +0000

    Add package pyleri/1.0.6/pyleri-1.0.6.tar.gz

---
+++ packages/pyleri/pyleri-1.0.6.tar.gz/pyleri-1.0.6/pyleri/tokens.py
@@ -0,0 +1,30 @@
+'''Tokens class.
+
+:copyright: 2015, Jeroen van der Heijden (Transceptor Technology)
+'''
+from .elements import NamedElement
+
+
+class Tokens(NamedElement):
+
+    __slots__ = ('_tokens',)
+
+    def __init__(self, tokens):
+        self._tokens = tokens.split()
+        self._tokens.sort(key=len, reverse=True)
+
+    def __repr__(self):
+        return ', '.join(self._tokens)
+
+    def _get_node_result(self, root, tree, rule, s, node):
+        for token in self._tokens:
+            if s.startswith(token):
+                root._append_tree(tree, node, node.start + len(token))
+                return True, node.end
+
+        root._expecting.update(self, node.start)
+
+        return False, node.start
+
+    def _run_export_js(self, js_identation, ident, classes):
+        return 'Tokens(\'{}\')'.format(' '.join(self._tokens))
