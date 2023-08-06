aa81f9a9fe72acceb7cc6a12b5df9261074d05e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:48 +0000

    Add package cmajor/0.1/cmajor-0.1.tar.gz

---
+++ packages/cmajor/cmajor-0.1.tar.gz/cmajor-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'cmajor',
+    py_modules = ['cmajor'],
+    version = '0.1',
+    description = 'hello, cmajor',
+    author = 'kuga',
+    author_email = 'kugacola@gmail.com',
+    url = 'http://cmajor.me',
+    keywords = ['cmajor'],
+)
