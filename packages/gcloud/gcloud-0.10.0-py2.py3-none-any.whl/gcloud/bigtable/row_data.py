dfb818dc9fc904d0a8176f900deac978fff3976a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:46 +0000

    Add package gcloud/0.10.0/gcloud-0.10.0-py2.py3-none-any.whl

---
+++ packages/gcloud/gcloud-0.10.0-py2.py3-none-any.whl/gcloud/bigtable/row_data.py
@@ -0,0 +1,111 @@
+# Copyright 2016 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Container for Google Cloud Bigtable Cells and Streaming Row Contents."""
+
+
+import six
+
+from gcloud._helpers import _datetime_from_microseconds
+from gcloud._helpers import _to_bytes
+
+
+class Cell(object):
+    """Representation of a Google Cloud Bigtable Cell.
+
+    :type value: bytes
+    :param value: The value stored in the cell.
+
+    :type timestamp: :class:`datetime.datetime`
+    :param timestamp: The timestamp when the cell was stored.
+
+    :type labels: list
+    :param labels: (Optional) List of strings. Labels applied to the cell.
+    """
+
+    def __init__(self, value, timestamp, labels=()):
+        self.value = value
+        self.timestamp = timestamp
+        self.labels = list(labels)
+
+    @classmethod
+    def from_pb(cls, cell_pb):
+        """Create a new cell from a Cell protobuf.
+
+        :type cell_pb: :class:`._generated.bigtable_data_pb2.Cell`
+        :param cell_pb: The protobuf to convert.
+
+        :rtype: :class:`Cell`
+        :returns: The cell corresponding to the protobuf.
+        """
+        timestamp = _datetime_from_microseconds(cell_pb.timestamp_micros)
+        if cell_pb.labels:
+            return cls(cell_pb.value, timestamp, labels=cell_pb.labels)
+        else:
+            return cls(cell_pb.value, timestamp)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return (other.value == self.value and
+                other.timestamp == self.timestamp and
+                other.labels == self.labels)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class PartialRowData(object):
+    """Representation of partial row in a Google Cloud Bigtable Table.
+
+    These are expected to be updated directly from a
+    :class:`._generated.bigtable_service_messages_pb2.ReadRowsResponse`
+
+    :type row_key: bytes
+    :param row_key: The key for the row holding the (partial) data.
+    """
+
+    def __init__(self, row_key):
+        self._row_key = row_key
+        self._cells = {}
+        self._committed = False
+        self._chunks_encountered = False
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return (other._row_key == self._row_key and
+                other._committed == self._committed and
+                other._chunks_encountered == self._chunks_encountered and
+                other._cells == self._cells)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def to_dict(self):
+        """Convert the cells to a dictionary.
+
+        This is intended to be used with HappyBase, so the column family and
+        column qualiers are combined (with ``:``).
+
+        :rtype: dict
+        :returns: Dictionary containing all the data in the cells of this row.
+        """
+        result = {}
+        for column_family_id, columns in six.iteritems(self._cells):
+            for column_qual, cells in six.iteritems(columns):
+                key = (_to_bytes(column_family_id) + b':' +
+                       _to_bytes(column_qual))
+                result[key] = cells
+        return result
---
+++ packages/gcloud/gcloud-0.10.0-py2.py3-none-any.whl/gcloud/bigtable/test_row_data.py
@@ -0,0 +1,178 @@
+# Copyright 2016 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest2
+
+
+class TestCell(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.bigtable.row_data import Cell
+        return Cell
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTargetClass()(*args, **kwargs)
+
+    def _from_pb_test_helper(self, labels=None):
+        import datetime
+        from gcloud._helpers import _EPOCH
+        from gcloud.bigtable._generated import bigtable_data_pb2 as data_pb2
+
+        timestamp_micros = 18738724000  # Make sure millis granularity
+        timestamp = _EPOCH + datetime.timedelta(microseconds=timestamp_micros)
+        value = b'value-bytes'
+
+        if labels is None:
+            cell_pb = data_pb2.Cell(value=value,
+                                    timestamp_micros=timestamp_micros)
+            cell_expected = self._makeOne(value, timestamp)
+        else:
+            cell_pb = data_pb2.Cell(value=value,
+                                    timestamp_micros=timestamp_micros,
+                                    labels=labels)
+            cell_expected = self._makeOne(value, timestamp, labels=labels)
+
+        klass = self._getTargetClass()
+        result = klass.from_pb(cell_pb)
+        self.assertEqual(result, cell_expected)
+
+    def test_from_pb(self):
+        self._from_pb_test_helper()
+
+    def test_from_pb_with_labels(self):
+        labels = [u'label1', u'label2']
+        self._from_pb_test_helper(labels)
+
+    def test_constructor(self):
+        value = object()
+        timestamp = object()
+        cell = self._makeOne(value, timestamp)
+        self.assertEqual(cell.value, value)
+        self.assertEqual(cell.timestamp, timestamp)
+
+    def test___eq__(self):
+        value = object()
+        timestamp = object()
+        cell1 = self._makeOne(value, timestamp)
+        cell2 = self._makeOne(value, timestamp)
+        self.assertEqual(cell1, cell2)
+
+    def test___eq__type_differ(self):
+        cell1 = self._makeOne(None, None)
+        cell2 = object()
+        self.assertNotEqual(cell1, cell2)
+
+    def test___ne__same_value(self):
+        value = object()
+        timestamp = object()
+        cell1 = self._makeOne(value, timestamp)
+        cell2 = self._makeOne(value, timestamp)
+        comparison_val = (cell1 != cell2)
+        self.assertFalse(comparison_val)
+
+    def test___ne__(self):
+        value1 = 'value1'
+        value2 = 'value2'
+        timestamp = object()
+        cell1 = self._makeOne(value1, timestamp)
+        cell2 = self._makeOne(value2, timestamp)
+        self.assertNotEqual(cell1, cell2)
+
+
+class TestPartialRowData(unittest2.TestCase):
+
+    def _getTargetClass(self):
+        from gcloud.bigtable.row_data import PartialRowData
+        return PartialRowData
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTargetClass()(*args, **kwargs)
+
+    def test_constructor(self):
+        row_key = object()
+        partial_row_data = self._makeOne(row_key)
+        self.assertTrue(partial_row_data._row_key is row_key)
+        self.assertEqual(partial_row_data._cells, {})
+        self.assertFalse(partial_row_data._committed)
+        self.assertFalse(partial_row_data._chunks_encountered)
+
+    def test___eq__(self):
+        row_key = object()
+        partial_row_data1 = self._makeOne(row_key)
+        partial_row_data2 = self._makeOne(row_key)
+        self.assertEqual(partial_row_data1, partial_row_data2)
+
+    def test___eq__type_differ(self):
+        partial_row_data1 = self._makeOne(None)
+        partial_row_data2 = object()
+        self.assertNotEqual(partial_row_data1, partial_row_data2)
+
+    def test___ne__same_value(self):
+        row_key = object()
+        partial_row_data1 = self._makeOne(row_key)
+        partial_row_data2 = self._makeOne(row_key)
+        comparison_val = (partial_row_data1 != partial_row_data2)
+        self.assertFalse(comparison_val)
+
+    def test___ne__(self):
+        row_key1 = object()
+        partial_row_data1 = self._makeOne(row_key1)
+        row_key2 = object()
+        partial_row_data2 = self._makeOne(row_key2)
+        self.assertNotEqual(partial_row_data1, partial_row_data2)
+
+    def test___ne__committed(self):
+        row_key = object()
+        partial_row_data1 = self._makeOne(row_key)
+        partial_row_data1._committed = object()
+        partial_row_data2 = self._makeOne(row_key)
+        self.assertNotEqual(partial_row_data1, partial_row_data2)
+
+    def test___ne__cells(self):
+        row_key = object()
+        partial_row_data1 = self._makeOne(row_key)
+        partial_row_data1._cells = object()
+        partial_row_data2 = self._makeOne(row_key)
+        self.assertNotEqual(partial_row_data1, partial_row_data2)
+
+    def test_to_dict(self):
+        cell1 = object()
+        cell2 = object()
+        cell3 = object()
+
+        family_name1 = u'name1'
+        family_name2 = u'name2'
+        qual1 = b'col1'
+        qual2 = b'col2'
+        qual3 = b'col3'
+
+        partial_row_data = self._makeOne(None)
+        partial_row_data._cells = {
+            family_name1: {
+                qual1: cell1,
+                qual2: cell2,
+            },
+            family_name2: {
+                qual3: cell3,
+            },
+        }
+
+        result = partial_row_data.to_dict()
+        expected_result = {
+            b'name1:col1': cell1,
+            b'name1:col2': cell2,
+            b'name2:col3': cell3,
+        }
+        self.assertEqual(result, expected_result)
