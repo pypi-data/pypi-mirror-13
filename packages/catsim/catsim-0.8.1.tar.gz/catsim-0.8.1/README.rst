742624d8fa8cb9cdb008c836d761ed79f689c168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:29 +0000

    Add package catsim/0.8.1/catsim-0.8.1.tar.gz

---
+++ packages/catsim/catsim-0.8.1.tar.gz/catsim-0.8.1/README.rst
@@ -0,0 +1,49 @@
+.. image:: https://travis-ci.org/douglasrizzo/catsim.svg?branch=master
+    :target: https://travis-ci.org/douglasrizzo/catsim:
+    :alt: Build Status
+
+.. image:: https://coveralls.io/repos/github/douglasrizzo/catsim/badge.svg?branch=master
+    :target: https://coveralls.io/github/douglasrizzo/catsim?branch=master
+    :alt: Test Coverage
+.. image:: https://badge.fury.io/py/catsim.svg
+    :target: https://badge.fury.io/py/catsim
+    :alt: Latest Version
+.. image:: https://landscape.io/github/douglasrizzo/catsim/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/douglasrizzo/catsim/master
+   :alt: Code Health
+
+Introduction
+------------
+
+**catsim** is a computerized adaptive testing simulator written in Python 3.4. It allow for the simulation of computerized adaptive tests, selecting different test initialization rules, item selection rules, proficiency reestimation methods and stopping criteria.
+
+Computerized adaptive tests are educational evaluations, usually taken by examinees in a computer or some other digital means, in which the examinee's proficiency is evaluated after the response of each item. The new proficiency is then used to select a new item, closer to the examinee's real proficiency. This method of test application has several advantages compared to the traditional paper-and-pencil method, since high-proficiency examinees are not required to answer all the easy items in a test, answering only the items that actually give some information regarding his or hers true knowledge of the subject at matter. A similar, but inverse effect happens for those examinees of low proficiency level.
+
+*catsim* allows users to simulate the application of a computerized adaptive test, given a sample of examinees, represented by their proficiency levels, and an item bank, represented by their parameters according to some Item Response Theory model.
+
+Basic Usage
+-----------
+
+1. Have an `item matrix <item_matrix.rst>`_;
+2. Have a sample, or a number of examinees;
+3. Create a `initializer <initialization.rst>`_, an item `selector <selection.rst>`_, a proficiency `estimator <estimation.rst>`_ and a `stopping criterion <stopping.rst>`_;
+4. Pass them to a `simulator <simulation.rst>`_ and start the simulation.
+
+Optional:
+
+5. Access the simulator's properties to get specifics of the results;
+6. `Plot <plot.rst>`_ your results.
+
+ .. code-block:: python
+     :linenos:
+
+     from catsim.initialization import RandomInitializer
+     from catsim.selection import MaxInfoSelector
+     from catsim.reestimation import HillClimbingEstimator
+     from catsim.stopping import MaxItemStopper
+     from catsim.cat import generate_item_bank
+     initializer = RandomInitializer()
+     selector = MaxInfoSelector()
+     estimator = HillClimbingEstimator()
+     stopper = MaxItemStopper(20)
+     Simulator(generate_item_bank(100), 10).simulate(initializer, selector, estimator, stopper)
---
+++ packages/catsim/catsim-0.8.1.tar.gz/catsim-0.8.1/catsim/testing.py
@@ -0,0 +1,119 @@
+import unittest
+import numpy
+from catsim import irt
+from catsim import plot
+from catsim.simulation import Simulator
+from catsim.cat import generate_item_bank
+from catsim.stopping import MaxItemStopper, MinErrorStopper
+from catsim.selection import MaxInfoSelector, ClusterSelector
+from catsim.initialization import RandomInitializer, FixedPointInitializer
+from catsim.reestimation import HillClimbingEstimator, BinarySearchEstimator, DifferentialEvolutionEstimator, FMinEstimator
+
+
+def test_item_bank_generation():
+    for items in [
+        generate_item_bank(5, '1PL'), generate_item_bank(5, '2PL'), generate_item_bank(5, '3PL'),
+        generate_item_bank(
+            5,
+            '3PL',
+            corr=0
+        )
+    ]:
+        irt.validate_item_bank(items, raise_err=True)
+
+    items = numpy.zeros((100))
+    irt.validate_item_bank(items)
+    items = irt.normalize_item_bank(items)
+    irt.validate_item_bank(items, raise_err=True)
+
+
+def test_plots():
+    from matplotlib.pyplot import close
+    initializer = RandomInitializer()
+    selector = MaxInfoSelector()
+    estimator = HillClimbingEstimator()
+    stopper = MaxItemStopper(20)
+    s = Simulator(generate_item_bank(100), 10)
+    s.simulate(initializer, selector, estimator, stopper)
+
+    for item in s.items[0:10]:
+        yield plot.item_curve, item[0], item[1], item[2], 'Test plot', 'icc', None, False
+        yield plot.item_curve, item[0], item[1], item[2], 'Test plot', 'iic', None, False
+        yield plot.item_curve, item[0], item[1], item[2], 'Test plot', 'both', None, False
+        close('all')
+
+    plot.gen3D_dataset_scatter(items=s.items, show=False)
+    plot.test_progress(
+        title='Test progress',
+        simulator=s,
+        index=0,
+        info=True,
+        see=True,
+        reliability=True,
+        show=False
+    )
+
+    # close all plots after testing
+    close('all')
+
+
+def test_simulations():
+    examinees = 10
+    initializers = [
+        RandomInitializer('uniform',
+                          (-5, 5)
+                          # ), RandomInitializer(
+                          #     'normal', (0, 1)
+                          ),
+        FixedPointInitializer(0)
+    ]
+    selectors = [MaxInfoSelector()]
+    estimators = [
+        HillClimbingEstimator(), BinarySearchEstimator(), DifferentialEvolutionEstimator((-8, 8)),
+        FMinEstimator()
+    ]
+    stoppers = [MaxItemStopper(20), MinErrorStopper(.4)]
+
+    for initializer in initializers:
+        for selector in selectors:
+            for estimator in estimators:
+                for stopper in stoppers:
+                    items = generate_item_bank(5000)
+                    yield one_simulation, items, examinees, initializer, selector, estimator, stopper
+
+
+def test_cism():
+    from sklearn.cluster import KMeans
+
+    examinees = 10
+    initializers = [
+        RandomInitializer('uniform',
+                          (-5, 5)
+                          # ), RandomInitializer(
+                          #     'normal', (0, 1)
+                          ),
+        FixedPointInitializer(0)
+    ]
+    estimators = [
+        HillClimbingEstimator(), BinarySearchEstimator(), DifferentialEvolutionEstimator((-8, 8)),
+        FMinEstimator()
+    ]
+    stoppers = [MaxItemStopper(20), MinErrorStopper(.4)]
+
+    for initializer in initializers:
+        for estimator in estimators:
+            for stopper in stoppers:
+                items = generate_item_bank(5000)
+                clusters = KMeans(n_clusters=8).fit_predict(items)
+                ClusterSelector.weighted_cluster_infos(0, items, clusters)
+                ClusterSelector.avg_cluster_params(items, clusters)
+                selector = ClusterSelector(clusters=clusters, r_max=.2)
+                yield one_simulation, items, examinees, initializer, selector, estimator, stopper
+
+
+def one_simulation(items, examinees, initializer, selector, estimator, stopper):
+    Simulator(items, examinees).simulate(initializer, selector, estimator, stopper)
+
+
+if __name__ == '__main__':
+    unittest.main()
