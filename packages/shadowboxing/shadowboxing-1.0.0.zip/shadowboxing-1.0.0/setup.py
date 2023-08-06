142c923cb4eb73ebd1aef2c8401fcfc65a5f2e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:37 +0000

    Add package shadowboxing/1.0.0/shadowboxing-1.0.0.zip

---
+++ packages/shadowboxing/shadowboxing-1.0.0.zip/shadowboxing-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup    
+
+setup(
+        name        ='shadowboxing',
+        version     ='1.0.0',
+        py_modules  =['nester'],
+        author      ='hfpython',
+        author_email='shadowbox@126.com',
+        url         ='http://www.headfirstlabs.com',
+        description ='A simple printer of nested lists',
+        
+      )
