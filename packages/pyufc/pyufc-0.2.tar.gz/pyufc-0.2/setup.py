141b07cb2323cd39bb08e2fb7f7381370e1615d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:52 +0000

    Add package pyufc/0.2/pyufc-0.2.tar.gz

---
+++ packages/pyufc/pyufc-0.2.tar.gz/pyufc-0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='pyufc',
+    version='0.2',
+    author='Eric Hamiter',
+    author_email='ehamiter@gmail.com',
+    description='A Python wrapper for API access to the UFC fighter roster',
+    url='https://github.com/ehamiter/pyufc',
+    packages=['pyufc'],
+    install_requires=[
+        'beautifulsoup4==4.4.0',
+        'requests==2.7.0'
+    ],
+    license='MIT',
+    keywords=['ufc', 'mma', 'mixed martial arts', 'fighting', 'fighters'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
