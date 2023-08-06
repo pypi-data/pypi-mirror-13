521ddcde548bf80a64ccf5d6a4f02618fb1526b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:20 +0000

    Add package nesterhs/1.0.0/nesterhs-1.0.0.tar.gz

---
+++ packages/nesterhs/nesterhs-1.0.0.tar.gz/nesterhs-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nesterhs',
+        version = '1.0.0',
+        py_modules = ['nesterhs'],
+        author = 'hspython',
+        author_email = 'admin@heatspotter.com',
+        url = 'http://www.heatspotter.com',
+        description = 'A simple printer of nested lists',
+    )        
