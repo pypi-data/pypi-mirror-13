bf7a53c7e1248f3c42faf9308b122e0052023b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:09 +0000

    Add package l/0.6.1/l-0.6.1.tar.gz

---
+++ packages/l/l-0.6.1.tar.gz/l-0.6.1/l/cli.py
@@ -0,0 +1,79 @@
+from sys import stdout
+
+import click
+
+from l.core import columnized, ls, one_per_line, recurse, flat
+from l.project import project
+
+
+VARIADIC = -1  # click uses -1 for nargs which is very hard to read
+
+
+class Project(click.ParamType):
+    name = "project"
+    convert = lambda self, value, param, ctx : project(value)
+
+
+PROJECT = Project()
+
+
+def run(all, paths, recurse, output, stdout=stdout):
+    """
+    Project-oriented directory and file information lister.
+
+    """
+
+    contents = [
+        path_and_children
+        for path in paths or (project("."),)
+        for path_and_children in recurse(path=path)
+    ]
+    stdout.write(output(contents))
+
+
+I_hate_everything = [
+    click.command(context_settings=dict(help_option_names=["-h", "--help"])),
+    click.option(
+        "-1", "--one-per-line", "output",
+        flag_value=one_per_line,
+        help="Force output to be one entry per line. "
+            "Note that unlike ls, when recursively listing directories, "
+            "also forces output to not be grouped by subdirectory.",
+    ),
+    click.option(
+        "--many-per-line", "output",
+        flag_value=columnized,
+        default=True,
+        help="Show human-readable, labelled output.",
+    ),
+    click.option(
+        "-a", "--all",
+        flag_value="all",
+        help="Like -A, but also display '.' and '..'",
+    ),
+    click.option(
+        "-A", "--almost-all", "all",
+        flag_value="almost",
+        help="Do not ignore entries that start with '.'",
+    ),
+    click.option(
+        "-R", "--recursive", "recurse",
+        flag_value=recurse,
+        help="Recursively list the project.",
+    ),
+    click.option(
+        "--no-recursive", "recurse",
+        flag_value=flat,
+        default=True,
+        help="Do not recursively list the project.",
+    ),
+    click.argument(
+        "paths",
+        nargs=VARIADIC,
+        type=PROJECT,
+    ),
+]
+
+main = run
+for add_cli_thing_to in reversed(I_hate_everything):
+    main = add_cli_thing_to(main)
---
+++ packages/l/l-0.6.1.tar.gz/l-0.6.1/l/core.py
@@ -0,0 +1,60 @@
+def ls(path):
+    return [
+        child for child in path.children()
+        if not child.basename().startswith(".")
+    ]
+
+
+def ls_almost_all(path):
+    return path.listdir()
+
+
+def columnized(paths):
+    if len(paths) == 1:
+        (_, children), = paths
+        return _tabularized(children)
+    return "\n".join(_labelled(sorted(paths)))
+
+
+def _labelled(parents_and_children):
+    return (
+        "{parent.path}:\n{children}".format(
+            parent=parent,
+            children=_tabularized(children),
+        )
+        for parent, children in parents_and_children
+    )
+
+
+def _tabularized(children):
+    return "  ".join(sorted(child.basename() for child in children)) + "\n"
+
+
+def one_per_line(parents_and_children):
+    if len(parents_and_children) == 1:
+        paths = (
+            child.basename()
+            for _, children in parents_and_children
+            for child in sorted(children)
+        )
+    else:
+        paths = sorted(
+            child.path
+            for _, children in parents_and_children
+            for child in children
+        )
+
+    return "\n".join(paths) + "\n"
+
+
+def recurse(path):
+    working = [path]
+    while working:
+        path = working.pop()
+        children = ls(path=path)
+        yield path, children
+        working.extend(child for child in children if child.isdir())
+
+
+def flat(path):
+    return [(path, ls(path=path))]
