21cb39b68b02179c7c4153fbbbd3032c90839a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:16 +0000

    Add package ScoreiT/1.0/ScoreiT-1.0.zip

---
+++ packages/ScoreiT/ScoreiT-1.0.zip/ScoreiT-1.0/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+setup(
+    name="ScoreiT",
+    version="1.0",
+    description="cricket score notifier",
+    url="https://github.com/DeveshAgrawal1/ScoreiT",
+    author="Devesh Agrawal",
+    author_email="dagrawal1096@gmail.com",
+    license='MIT',
+    packages=["ScoreiT"],
+    keywords=["cricket"," score"," "," "," "],
+    install_requires=["requests","BeautifulSoup"," "," "," "],
+    scripts=["src/ScoreiT"],
+    zip_safe=True
+    )
+    
