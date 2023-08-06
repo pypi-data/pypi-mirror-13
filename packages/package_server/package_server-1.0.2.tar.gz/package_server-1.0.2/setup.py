df8494fabe6da09d6e891d52a573f26747c91adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:05 +0000

    Add package package_server/1.0.2/package_server-1.0.2.tar.gz

---
+++ packages/package_server/package_server-1.0.2.tar.gz/package_server-1.0.2/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='package_server',
+      version='1.0.2',
+      description='Server program for eYRC+ 2015',
+      author='Sanam Shakya, Sudeep Rajput',
+      py_modules=['package_server'])
