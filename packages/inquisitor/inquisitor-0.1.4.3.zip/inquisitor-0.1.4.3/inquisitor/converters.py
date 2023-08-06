823801f2c334308824936d7131acc0692edc1fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:09 +0000

    Add package inquisitor/0.1.4.3/inquisitor-0.1.4.3.zip

---
+++ packages/inquisitor/inquisitor-0.1.4.3.zip/inquisitor-0.1.4.3/inquisitor/converters.py
@@ -0,0 +1,35 @@
+import pandas
+import datetime
+
+
+class PandasConverter(object):
+
+    def convert_data(self, data):
+        """
+        Convert data to pandas DataFrame
+        Args:
+            data (dict): dict
+
+        Returns:
+            pandas.DataFrame
+
+        """
+        return pandas.DataFrame(
+            {data['ticker']: data['data']['values']},
+            index=map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'), data['data']['dates']),
+            dtype=float
+        )
+
+    def convert_results(self, results):
+        """
+        Convert results to pandas DataFrame
+        Args:
+            results (dict): results from api response
+
+        Returns:
+            pandas.DataFrame: pandas.DataFrame data
+        """
+        dataframe = pandas.DataFrame()
+        for data in results:
+            dataframe = pandas.concat([dataframe, self.convert_data(data)], axis=1)
+        return dataframe
