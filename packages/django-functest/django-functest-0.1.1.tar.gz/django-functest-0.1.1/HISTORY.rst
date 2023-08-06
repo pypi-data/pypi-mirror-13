d4ed5e65502827f37b3b76053f914f6a6156b95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:29 +0000

    Add package django-functest/0.1.1/django-functest-0.1.1.tar.gz

---
+++ packages/django-functest/django-functest-0.1.1.tar.gz/django-functest-0.1.1/HISTORY.rst
@@ -0,0 +1,14 @@
+.. :changelog:
+
+History
+-------
+
+0.1.1 (in development)
+++++++++++++++++++++++
+
+* Fixed packaging bug that caused wheels to fail on Python 3.
+
+0.1.0
++++++
+
+* First release on PyPI.
---
+++ packages/django-functest/django-functest-0.1.1.tar.gz/django-functest-0.1.1/django_functest/__init__.py
@@ -0,0 +1,7 @@
+from .funcselenium import FuncSeleniumMixin
+from .funcwebtest import FuncWebTestMixin
+from .utils import AdminLoginMixin, ShortcutLoginMixin
+
+__version__ = '0.1.1'
+
+__all__ = ['FuncWebTestMixin', 'FuncSeleniumMixin', 'ShortcutLoginMixin', 'AdminLoginMixin']
