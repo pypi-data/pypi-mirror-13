bde9d503089ed7805080e2ddd815338994ac1fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:08 +0000

    Add package django-lazy-choices/0.1.3/django-lazy-choices-0.1.3.tar.gz

---
+++ packages/django-lazy-choices/django-lazy-choices-0.1.3.tar.gz/django-lazy-choices-0.1.3/src/lazychoices/__init__.py
@@ -0,0 +1,3 @@
+from .models import LazyChoiceField, LazyChoiceModelMixin  # noqa
+
+__version__ = '0.1.3'
---
+++ packages/django-lazy-choices/django-lazy-choices-0.1.3.tar.gz/django-lazy-choices-0.1.3/src/lazychoices/forms/__init__.py
@@ -0,0 +1,2 @@
+from .fields import LazyChoiceField  # noqa
+from .models import LazyChoiceModelForm  # noqa
