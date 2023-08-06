526fea1c800c0ad5e2f66c8338276ae9ca724ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:10 +0000

    Add package django-import-data/0.3.0/django_import_data-0.3.0-py2.py3-none-any.whl

---
+++ packages/django-import-data/django_import_data-0.3.0-py2.py3-none-any.whl/import_data/management/commands/json_to_xml.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+import process_xslt
+
+class Command(BaseCommand):
+    help = 'Converts JSON file to XML and prints the output'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', help='URL to fetch source JSON')
+
+    def handle(self, *args, **options):
+        etree, encoding = process_xslt.load_source_by_url(options['url'])
+        process_xslt.print_xml(etree, encoding)
---
+++ packages/django-import-data/django_import_data-0.3.0-py2.py3-none-any.whl/import_data/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from django.conf import settings
+
+# class ConfigTests(TestCase):
+    # def test_xslt_files_dir_config(self):
+        # self.assertIn('XSLT_FILES_DIR', settings, '')
