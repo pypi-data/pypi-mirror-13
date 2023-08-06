2c70894366ceb2591e5673745903ea7b43814029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:32 +0000

    Add package twittermetrics/0.1.4/twittermetrics-0.1.4.zip

---
+++ packages/twittermetrics/twittermetrics-0.1.4.zip/twittermetrics-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='twittermetrics',
+      version='0.1.4',
+      description='Twitter search for popular words and sentiment analysis',
+      url='https://github.com/dennokorir/project-mining-twitter.git',
+      author='Denis Korir',
+      author_email='dennokorir@gmail.com',
+      license='MIT',
+      packages=['twittermetrics'],
+      install_requires=[
+          'twitter','prettytable','requests','collections'
+      ],
+      zip_safe=False)
+
