e425f68d0f9123d0d52321816e928c7bac663537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:06 +0000

    Add package nbstripout/0.2.2/nbstripout-0.2.2.tar.gz

---
+++ packages/nbstripout/nbstripout-0.2.2.tar.gz/nbstripout-0.2.2/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup_requires = [
+    'pytest-runner'
+]
+
+tests_require = [
+    'pytest',
+    'pytest-flake8',
+    'cram'
+]
+
+setup(name='nbstripout',
+      version='0.2.2',
+
+      author='Min RK',
+      author_email='benjaminrk@gmail.com',
+
+      maintainer='Florian, Rathgeber, Michael McNeil Forbes',
+      maintainer_email='florian.rathgeber@gmail.com, michael.forbes+python@gmail.com',
+      url='https://github.com/kynan/nbstripout',
+
+      license="License :: OSI Approved :: MIT License",
+
+      description='Strips outputs from Jupyter and IPython notebooks',
+      long_description=long_description,
+      py_modules=['pytest_cram', 'nbstripout'],
+      entry_points={
+          'console_scripts': [
+              'nbstripout = nbstripout:main'
+          ],
+          'pytest11': [
+              'pytest_cram = pytest_cram',
+          ]
+      },
+
+      setup_requires=setup_requires,
+      tests_require=tests_require,
+
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Other Environment",
+          "Framework :: IPython",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Version Control",
+      ])
