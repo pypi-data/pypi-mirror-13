973e353506eccdaaeff2de83e4a743a5062b7be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:28 +0000

    Add package pydisque/0.1.2/pydisque-0.1.2.tar.gz

---
+++ packages/pydisque/pydisque-0.1.2.tar.gz/pydisque-0.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.1.2'
+DESCRIPTION = 'disque client'
+
+setup(
+    name='pydisque',
+    version=VERSION,
+    description=DESCRIPTION,
+    author='ybrs',
+    license='MIT',
+    url="http://github.com/ybrs/pydisque",
+    author_email='aybars.badur@gmail.com',
+    packages=['pydisque'],
+    install_requires=['redis', 'six'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
