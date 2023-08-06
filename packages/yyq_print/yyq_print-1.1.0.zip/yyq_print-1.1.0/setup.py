15d9b5af01db6588932cf52ee1044b6a975e1dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:20 +0000

    Add package yyq_print/1.1.0/yyq_print-1.1.0.zip

---
+++ packages/yyq_print/yyq_print-1.1.0.zip/yyq_print-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'yyq_print',
+    version = '1.1.0',
+    py_modules = ['yyq_print'],
+    author = 'yeyinqi',
+    author_email = '2868989685@qq.com',
+    url = 'http://www.hp.com',
+    description = 'a simple printer of nested lest',
+    )
