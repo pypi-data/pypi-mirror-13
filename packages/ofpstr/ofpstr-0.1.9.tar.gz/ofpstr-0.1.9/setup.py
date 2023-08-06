448ff66f5db961bfe7ac13469acc0bcff362acc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:20 +0000

    Add package ofpstr/0.1.9/ofpstr-0.1.9.tar.gz

---
+++ packages/ofpstr/ofpstr-0.1.9.tar.gz/ofpstr-0.1.9/setup.py
@@ -0,0 +1,14 @@
+try:
+	from setuptools import setup
+except:
+	from distutils.core import setup
+
+setup(name='ofpstr',
+	version='0.1.9',
+	description='Openflow stringer library',
+	long_description=open("README.rst").read(),
+	author='Hiroaki Kawai',
+	author_email='hiroaki.kawai@gmail.com',
+	url='https://github.com/hkwi/ofpstr/',
+	packages=['ofpstr']
+)
