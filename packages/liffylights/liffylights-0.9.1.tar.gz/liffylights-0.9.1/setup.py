c2c1092912d20ea124c6b4e6b1bd59c8ba49b12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:17 +0000

    Add package liffylights/0.9.1/liffylights-0.9.1.tar.gz

---
+++ packages/liffylights/liffylights-0.9.1.tar.gz/liffylights-0.9.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='liffylights',
+    py_modules=['liffylights'],
+    version='0.9.1',
+    description='LIFX Python library',
+    author='TangoAlpha',
+    author_email='liffylights@attwood.org',
+    url='https://github.com/TangoAlpha/liffylights',
+    license='MIT',
+    keywords=['homeassistant'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
