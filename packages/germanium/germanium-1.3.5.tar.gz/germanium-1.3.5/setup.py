173f3bc761cac56fb1cd66e14f95a3721a733c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:35 +0000

    Add package germanium/1.3.5/germanium-1.3.5.tar.gz

---
+++ packages/germanium/germanium-1.3.5.tar.gz/germanium-1.3.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='germanium',
+    version='1.3.5',
+    description='The germanium project',
+    long_description = 'The germanium project',
+    author='Bogdan Mustiata',
+    author_email='bogdan.mustiata@gmail.com',
+    license='BSD',
+
+    install_requires=['selenium'],
+    packages=['germanium'],
+    package_data={'germanium': ['*.js']}
+)
