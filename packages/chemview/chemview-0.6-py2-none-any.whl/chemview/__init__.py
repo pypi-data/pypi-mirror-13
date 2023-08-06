0b62af80da523d5a2b87b49fbf5e863a0625aec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:29 +0000

    Add package chemview/0.6/chemview-0.6-py2-none-any.whl

---
+++ packages/chemview/chemview-0.6-py2-none-any.whl/chemview/__init__.py
@@ -0,0 +1,6 @@
+from .install import enable_notebook
+from .widget import RepresentationViewer
+from .viewer import MolecularViewer, TrajectoryControls
+from .trajectory import TrajectoryViewer
+
+__version__ = '0.6'
