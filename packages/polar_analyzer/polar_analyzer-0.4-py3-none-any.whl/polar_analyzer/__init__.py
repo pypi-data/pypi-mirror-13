aecec4ca4877c1f471164deebc06b32d23963328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:11 +0000

    Add package polar_analyzer/0.4/polar_analyzer-0.4-py3-none-any.whl

---
+++ packages/polar_analyzer/polar_analyzer-0.4-py3-none-any.whl/polar_analyzer/__init__.py
@@ -0,0 +1,10 @@
+import nltk.tokenize
+from collections import Counter
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+from nltk.corpus import wordnet
+from nltk import WordPunctTokenizer
+from nltk.tokenize import RegexpTokenizer
+import re
+import statistics
+from polar_analyzer import polarAnalyzer
