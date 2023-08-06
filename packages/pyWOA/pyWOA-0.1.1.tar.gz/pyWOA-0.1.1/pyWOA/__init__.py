c000a5d119e27061d6d69c068319e0115d5494f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:05 +0000

    Add package pyWOA/0.1.1/pyWOA-0.1.1.tar.gz

---
+++ packages/pyWOA/pyWOA-0.1.1.tar.gz/pyWOA-0.1.1/pyWOA/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Guilherme Castelao'
+__email__ = 'guilherme@castelao.net'
+__version__ = '0.1.1'
+
+
+from pyWOA.woa import WOA
+#from WOA.woa import woa_profile
+#from WOA.woa import woa_profile_from_dap
+#from WOA.woa import woa_track_from_file
