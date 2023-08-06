385be7dd514877c8fe224f04d8bd5f5762daefa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:45 +0000

    Add package blowdrycss/0.1.5/blowdrycss-0.1.5-py3.4.egg

---
+++ packages/blowdrycss/blowdrycss-0.1.5-py3.4.egg/blowdrycss/unit_tests/test_fileRegexMap.py
@@ -0,0 +1,58 @@
+# builtins
+from unittest import TestCase, main
+# custom
+from blowdrycss.classparser import FileRegexMap
+from blowdrycss.utilities import unittest_file_path
+
+__author__ = 'chad nelson'
+__project__ = 'blowdrycss'
+
+
+class TestFileRegexMap(TestCase):
+    def test_is_valid_extension_true(self):
+        file_paths = [unittest_file_path('test_aspx', 'test.aspx'), unittest_file_path('test_jinja', 'test.jinja2')]
+        for _path in file_paths:
+            file_regex_map = FileRegexMap(file_path=_path)
+            self.assertTrue(file_regex_map.is_valid_extension(), msg=_path)
+
+    def test_is_valid_extension_whitespace(self):
+        file_paths = [unittest_file_path('test_aspx', 'test.aspx  '), unittest_file_path('test_jinja', 'test.jinja2 ')]
+        for _path in file_paths:
+            file_regex_map = FileRegexMap(file_path=_path)
+            self.assertTrue(file_regex_map.is_valid_extension(), msg=_path)
+
+    def test_is_valid_extension_false(self):
+        wrong_extensions = ['.wrong', '.squirrel', '.incorrect']
+        file_path = unittest_file_path('test_aspx', 'test.aspx')
+        for wrong_extension in wrong_extensions:
+            file_regex_map = FileRegexMap(file_path=file_path)
+            file_regex_map.extension = wrong_extension
+            self.assertFalse(file_regex_map.is_valid_extension())
+
+    def test_is_valid_extension_raises_OSError(self):
+        file_paths = [
+            unittest_file_path('test_files', 'test.wrong'), unittest_file_path('test_files', 'test.incorrect')
+        ]
+        for _path in file_paths:
+            self.assertRaises(OSError, FileRegexMap, _path)
+
+    def test_regexes(self):
+        expected_dicts = [
+            {
+                'sub_regex': r'<%.*?%>',
+                'findall_regex': r'class="(.*?)"',
+            },
+            {
+                'sub_regex': r'{.*?}?}',
+                'findall_regex': r'class="(.*?)"',
+            },
+        ]
+        file_paths = [unittest_file_path('test_aspx', 'test.aspx'), unittest_file_path('test_jinja', 'test.jinja2')]
+        for i, _path in enumerate(file_paths):
+            file_regex_map = FileRegexMap(file_path=_path)
+            actual_dict = file_regex_map.regex_dict
+            self.assertEqual(actual_dict, expected_dicts[i], msg=_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/blowdrycss/blowdrycss-0.1.5-py3.4.egg/blowdrycss/unit_tests/test_write_blowdrycss_settings_dot_py.py
@@ -0,0 +1,88 @@
+# builtins
+from unittest import TestCase, main
+from os import getcwd, path, remove, chdir
+# custom
+from blowdrycss.settingsbuilder import write_blowdrycss_settings_dot_py
+
+
+class TestWrite_blowdrycss_settings_dot_py(TestCase):
+    def test_write_blowdrycss_settings_dot_py(self):
+        settings_file = 'blowdrycss_settings.py'
+
+        # Identical section of code from blowdrycss.py for test purposes.
+        cwd = getcwd()
+
+        # The if/else logic is required for unit testing
+        if cwd.endswith('unit_tests'):                              # Allows running of pycharm unittest.
+            if path.isfile(settings_file):                          # Remove it if it exists.
+                remove(settings_file)
+
+            write_blowdrycss_settings_dot_py()
+            self.assertTrue(path.isfile('blowdrycss_settings.py'))  # test file existence
+        else:                                                       # python setup.py test
+            chdir(path.join('blowdrycss', 'unit_tests'))
+
+            if path.isfile(settings_file):                          # Remove it if it exists.
+                remove(settings_file)
+
+            write_blowdrycss_settings_dot_py()
+            self.assertTrue(path.isfile('blowdrycss_settings.py'))  # test file existence
+            chdir(cwd)                                              # Reset current working directory.
+
+        # Import from the current folder.
+        import blowdrycss.unit_tests.blowdrycss_settings as test_settings    # python setup.py test
+
+        # test directory settings
+        cwd = getcwd()
+        self.assertEqual(
+            test_settings.markdown_directory, path.join(cwd, 'docs', 'markdown'),
+            msg=test_settings.markdown_directory + '\t' + path.join(cwd, 'docs', 'markdown')
+        )
+        self.assertEqual(test_settings.project_directory, path.join(cwd, 'examplesite'))
+        self.assertEqual(test_settings.css_directory, path.join(cwd, 'examplesite', 'css'))
+        self.assertEqual(test_settings.docs_directory, path.join(cwd, 'docs'))
+
+        # test file_type settings
+        self.assertTrue(test_settings.file_types == ('*.html', ))
+
+        # test accessibility of true settings
+        true_settings = [
+            test_settings.html_docs, test_settings.timing_enabled, test_settings.human_readable,
+            test_settings.minify, test_settings.media_queries_enabled, test_settings.use_em,
+            test_settings.hide_css_errors,
+        ]
+        for true_setting in true_settings:
+            self.assertTrue(true_setting)
+
+        # test accessibility of false settings
+        false_settings = [test_settings.markdown_docs, test_settings.rst_docs]
+        for false_setting in false_settings:
+            self.assertFalse(false_setting)
+
+        # test base, px_to_em, and breakpoints
+        self.assertTrue(test_settings.base == 16)
+
+        self.assertTrue(test_settings.xxsmall == (test_settings.px_to_em(0), test_settings.px_to_em(120)))
+        self.assertTrue(test_settings.xsmall == (test_settings.px_to_em(121), test_settings.px_to_em(240)))
+        self.assertTrue(test_settings.small == (test_settings.px_to_em(241), test_settings.px_to_em(480)))
+        self.assertTrue(test_settings.medium == (test_settings.px_to_em(481), test_settings.px_to_em(720)))
+        self.assertTrue(test_settings.large == (test_settings.px_to_em(721), test_settings.px_to_em(1024)))
+        self.assertTrue(test_settings.xlarge == (test_settings.px_to_em(1025), test_settings.px_to_em(1366)))
+        self.assertTrue(test_settings.xxlarge == (test_settings.px_to_em(1367), test_settings.px_to_em(1920)))
+        self.assertTrue(test_settings.giant == (test_settings.px_to_em(1921), test_settings.px_to_em(2560)))
+        self.assertTrue(test_settings.xgiant == (test_settings.px_to_em(2561), test_settings.px_to_em(2800)))
+        self.assertTrue(test_settings.xxgiant == (test_settings.px_to_em(2801), test_settings.px_to_em(10**6)))
+
+        # Clean up. Removed created file.
+        if path.isfile(settings_file):                          # Remove it if it exists.
+            remove(settings_file)
+
+        if not cwd.endswith('unit_tests'):                              # Allows running of pycharm unittest.
+            chdir(path.join('blowdrycss', 'unit_tests'))
+            if path.isfile(settings_file):                          # Remove it if it exists.
+                remove(settings_file)
+            chdir(cwd)
+
+
+if __name__ == '__main__':
+    main()
