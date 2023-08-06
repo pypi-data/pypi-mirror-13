06623244f7520084f1090b0ff5b0b7c965a885da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:52 +0000

    Add package jaychou/0.1/jaychou-0.1.tar.gz

---
+++ packages/jaychou/jaychou-0.1.tar.gz/jaychou-0.1/jaychou.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+print('Hello大家好，我是周杰伦')
---
+++ packages/jaychou/jaychou-0.1.tar.gz/jaychou-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'jaychou',
+    py_modules = ['jaychou'],
+    version = '0.1',
+    description = 'Hello大家好，我是周杰伦',
+    author = 'kuga',
+    author_email = 'kugacola@gmail.com',
+    url = 'http://zhoujielun.me',
+    keywords = ['jay', 'jay chou', 'music', 'legend', 'incomparable'],
+)
