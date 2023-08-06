111f9c801d5d8da681a83534a0e2cd8fca1378ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:26 +0000

    Add package chenjingnester/1.0.0.1/chenjingnester-1.0.0.1.zip

---
+++ packages/chenjingnester/chenjingnester-1.0.0.1.zip/chenjingnester-1.0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='chenjingnester',
+	version='1.0.0.1',
+	py_modules=['chenjingnester'],
+	author='chenjing',
+	author_email='1402752916@qq.com',
+	url='http://',
+	description='no',
+)
