e739eb0f0bfe5b7295795e73b3bddbdd92655f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:50 +0000

    Add package hyperop/1.1/hyperop-1.1.tar.gz

---
+++ packages/hyperop/hyperop-1.1.tar.gz/hyperop-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# Load the version string
+exec(open('hyperop/_version.py').read())
+
+setup(
+    name="hyperop",
+    packages=['hyperop'],
+    version=__version__,
+    download_url='https://github.com/thoppe/python-hyperoperators/tarball/1.1',
+    author="Travis Hoppe",
+    author_email="travis.hoppe+hyperop@gmail.com",
+    description=(
+        "Hyperoperators (succession, addition, multiplication, exponentiation, tetration and higher) in python."),
+    license = "Creative Commons Attribution-ShareAlike 4.0 International License",
+    keywords = ["math", "hyperoperators", "uparrow", "large-numbers", ],
+    url="https://github.com/thoppe/python-hyperoperators",
+    test_suite="tests",
+)
