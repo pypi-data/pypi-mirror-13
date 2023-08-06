840281cd0a5466c5b431a80e6db44e63bbab49ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:39 +0000

    Add package sailunester/1.0.0/sailunester-1.0.0.zip

---
+++ packages/sailunester/sailunester-1.0.0.zip/sailunester-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(  name = 'sailunester',
+        version = '1.0.0',
+        py_modules = ['sailunester'],
+        author = 'sailu',
+        author_email = 'svgolconda@gmail.com',
+        #url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
