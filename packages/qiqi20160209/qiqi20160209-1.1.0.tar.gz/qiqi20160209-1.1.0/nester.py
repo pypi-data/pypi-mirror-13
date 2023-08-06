48d0d63ebb9e2ffcdac773467c4893b6f17708ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:45 +0000

    Add package qiqi20160209/1.1.0/qiqi20160209-1.1.0.tar.gz

---
+++ packages/qiqi20160209/qiqi20160209-1.1.0.tar.gz/qiqi20160209-1.1.0/nester.py
@@ -0,0 +1,12 @@
+'''this is moudle memory
+'''
+def print_lol(the_list,indent=False,level=0):
+    '''this is the def memory'''
+    for each_item in the_list:
+         if isinstance(each_item,list):
+             print_lol(each_item,indent,level+1)
+         else:
+             if indent=True:
+                 for stepx in range(level):
+                 print ("\t",end="")
+             print(each_item)
---
+++ packages/qiqi20160209/qiqi20160209-1.1.0.tar.gz/qiqi20160209-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='qiqi20160209',
+    version='1.1.0',
+    py_modules=['nester'],
+    author='qiqi',
+    author_email='qqq@qq.com',
+    url='http://www.qiqi.com',
+    description='my first python list'
+    )
