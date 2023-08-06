d4b8c595521edbdb8ec6726ecd21a37c55faa723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:12 +0000

    Add package cliquery/1.6.4/cliquery-1.6.4-py2-none-any.whl

---
+++ packages/cliquery/cliquery-1.6.4-py2-none-any.whl/cliquery/compat.py
@@ -0,0 +1,56 @@
+"""Functions for Python 2 and 3 compatibility"""
+
+import sys
+
+
+SYS_VERSION = sys.version_info[0]
+if SYS_VERSION == 2:
+    from HTMLParser import HTMLParser
+    def unescape(x):
+        """Convert character references (e.g. &gt;, &#62;,..) to unicode"""
+        return HTMLParser().unescape(x)
+
+    def iteritems(dct):
+        """Python 2 dictionary iteritems()"""
+        return dct.iteritems()
+
+    def iterkeys(dct):
+        """Python 2 dictionary iterkeys()"""
+        return dct.iterkeys()
+
+    def itervalues(dct):
+        """Python 2 dictionary itervalues()"""
+        return dct.itervalues()
+
+    def uni(word):
+        """Python 2 utf-8 encode"""
+        return word.encode('utf-8')
+
+    def asc(word):
+        """Python 2 ascii-ignore encode"""
+        return word.encode('ascii', 'ignore')
+else:
+    from html.parser import HTMLParser
+    def unescape(x):
+        """Convert character references (e.g. &gt;, &#62;,..) to unicode"""
+        return HTMLParser().unescape(x)
+
+    def iteritems(dct):
+        """Python 3 dictionary iteritems()"""
+        return iter(dct.items())
+
+    def iterkeys(dct):
+        """Python 3 dictionary iterkeys()"""
+        return iter(dct.keys())
+
+    def itervalues(dct):
+        """Python 3 dictionary itervalues()"""
+        return iter(dct.values())
+
+    def uni(word):
+        """Python 3 utf-8 encode"""
+        return word
+
+    def asc(word):
+        """Python 3 ascii-ignore encode"""
+        return word
