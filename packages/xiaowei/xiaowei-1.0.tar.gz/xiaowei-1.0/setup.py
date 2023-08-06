5f4feff5720328818274262a5d48e7e4d9fa006d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package xiaowei/1.0/xiaowei-1.0.tar.gz

---
+++ packages/xiaowei/xiaowei-1.0.tar.gz/xiaowei-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		name         = 'xiaowei',
+		version      = '1.0',
+		py_modules   = ['myftp'],
+		author       = 'xiaowei',
+		author_email = '15623xxx.@qq.com',
+		url          = 'http://www.example.com',
+		description  = 'just like a ftp server ',
+       )
