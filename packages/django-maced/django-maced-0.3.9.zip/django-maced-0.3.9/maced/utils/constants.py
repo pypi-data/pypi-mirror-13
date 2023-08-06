a46d302c9238d55f84afea32cd956d25e9f16f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package django-maced/0.3.9/django-maced-0.3.9.zip

---
+++ packages/django-maced/django-maced-0.3.9.zip/django-maced-0.3.9/maced/utils/constants.py
@@ -0,0 +1,5 @@
+ACTION_TYPES = ["add", "edit", "merge", "delete"]  # Action types of "clone" and "info" will be added later.
+                                                   # "info" is not really an action, but for the sake of ease of use,
+                                                   # it will be considered one.
+VALID_INPUT_TYPES = ["maced", "text", "color", "select"]
+VALID_SELECT_TYPES = ["object", "string"]
