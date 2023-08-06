a208fc5276316f102bc0c9589bf2ae31f99d31e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:16 +0000

    Add package qipipe/5.6.4/qipipe-5.6.4.tar.gz

---
+++ packages/qipipe/qipipe-5.6.4.tar.gz/qipipe-5.6.4/qipipe/qiprofile/update.py
@@ -0,0 +1,25 @@
+from qiprofile_rest_client.helpers import database
+from qiprofile_rest_client.model.subject import Subject
+from qiprofile_rest_client.model.imaging import Session
+from . import (clinical, imaging)
+
+
+def update(project, collection, subject, session, spreadsheet):
+    """
+    Updates the qiprofile database from the clinical spreadsheet and
+    XNAT database for the given session.
+    
+    :param project: the XNAT project name
+    :param collection: the image collection name
+    :param subject: the subject number
+    :param session: the XNAT session number
+    :param spreadsheet: the spreadsheet input file location
+    :param modeling_technique: the modeling technique
+    """
+    # Get or create the subject database subject.
+    key = dict(project=project, collection=collection, number=subject)
+    sbj = database.get_or_create(Subject, key)
+    # Update the clinical information from the XLS input.
+    clinical.update(sbj, spreadsheet)
+    # Update the imaging information from XNAT.
+    imaging.update(sbj, session)
