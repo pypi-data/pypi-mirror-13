c85f119868b540d265fa459d63b17625bbecb745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:03 +0000

    Add package dazzyjong/1.3.0/dazzyjong-1.3.0.tar.gz

---
+++ packages/dazzyjong/dazzyjong-1.3.0.tar.gz/dazzyjong-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'dazzyjong',
+    version     = '1.3.0',
+    py_modules  = ['dazzyjong'],
+    author      = 'dazzyjong',
+    author_email= 'dazzyjong@gmail.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
