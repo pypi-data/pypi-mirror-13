266d729213f8bf93021b3b83f83d3445823d275a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:12 +0000

    Add package mine/0.4.1/mine-0.4.1-py3-none-any.whl

---
+++ packages/mine/mine-0.4.1-py3-none-any.whl/mine/config.py
@@ -0,0 +1,22 @@
+"""Data structures for all settings."""
+
+import logging
+
+import yorm
+
+from .application import Applications
+from .computer import Computers
+
+
+log = logging.getLogger(__name__)
+
+
+@yorm.attr(applications=Applications)
+@yorm.attr(computers=Computers)
+class ProgramConfig(yorm.types.AttributeDictionary):
+    """Dictionary of program configuration settings."""
+
+    def __init__(self):
+        super().__init__()
+        self.applications = Applications()
+        self.computers = Computers()
