ca602afd13f5cb793318413ee9186676da25d03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:49 +0000

    Add package petermiya/1.0.0/petermiya-1.0.0.zip

---
+++ packages/petermiya/petermiya-1.0.0.zip/petermiya-1.0.0/petermiya.py
@@ -0,0 +1,7 @@
+"""描述我和我家团团在决策去不去吃饭时的状态，1是去，0是不去了"""
+#detailed description
+def peter_miya(peter=1,miya=1):
+    if (peter and miya):
+        print("出门咯！")
+    else:
+        print("都怪你")
---
+++ packages/petermiya/petermiya-1.0.0.zip/petermiya-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='petermiya',
+    version='1.0.0',
+    py_modules=['petermiya'],
+    author = "peter pan",
+    author_email = "397063058@qq.com",
+    description = "to my dearing"
+      )   
