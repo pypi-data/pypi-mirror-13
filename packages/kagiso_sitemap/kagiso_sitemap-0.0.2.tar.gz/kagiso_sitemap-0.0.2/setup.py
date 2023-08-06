8a57124eab8cb28f50c5d89dc00bec0b86c32705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:19 +0000

    Add package kagiso_sitemap/0.0.2/kagiso_sitemap-0.0.2.tar.gz

---
+++ packages/kagiso_sitemap/kagiso_sitemap-0.0.2.tar.gz/kagiso_sitemap-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='kagiso_sitemap',
+    version='0.0.2',
+    author='Kagiso Media',
+    author_email='development@kagiso.io',
+    description='Kagiso Sitemap',
+    url='https://github.com/Kagiso-Future-Media/kagiso_sitemap',
+    packages=find_packages(),
+    install_requires=[
+        'wagtail>=1.3.1',
+    ]
+)
