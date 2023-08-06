80303028023847d69d950e652f33bbbc88e8f84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:56 +0000

    Add package storjkademlia/0.5/storjkademlia-0.5.tar.gz

---
+++ packages/storjkademlia/storjkademlia-0.5.tar.gz/storjkademlia-0.5/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from kademlia import version
+
+setup(
+    name="storjkademlia",
+    version=version,
+    description=("Kademlia is a distributed hash table for "
+                 "decentralized peer-to-peer computer networks."),
+    long_description=open("README.markdown").read(),
+    author="Brian Muller",
+    author_email="bamuller@gmail.com",
+    license="MIT",
+    test_suite="kademlia.tests",
+    url="https://github.com/Storj/storjkademlia",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines(),
+    tests_require=open("test_requirements.txt").readlines(),
+    classifiers=[
+        # "Development Status :: 1 - Planning",
+        # "Development Status :: 2 - Pre-Alpha",
+        # "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
