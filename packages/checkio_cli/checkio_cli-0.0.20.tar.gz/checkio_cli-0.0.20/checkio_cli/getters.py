a3e3a2ee16854faa1e4f702867f7c937c88d7807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:32 +0000

    Add package checkio_cli/0.0.20/checkio_cli-0.0.20.tar.gz

---
+++ packages/checkio_cli/checkio_cli-0.0.20.tar.gz/checkio_cli-0.0.20/checkio_cli/getters.py
@@ -0,0 +1,186 @@
+import os
+import sys
+import git
+import re
+import shutil
+from distutils.dir_util import copy_tree
+import logging
+
+from checkio_docker.parser import MissionFilesCompiler
+from checkio_cli.folder import Folder
+from checkio_docker.client import DockerClient
+from checkio_cli.config import settings
+
+PY3 = sys.version_info[0] == 3
+if PY3:
+    raw_input = input
+
+
+RE_REPO_BRANCH = re.compile('(.+?)\@([\w\-\_]+)$')
+
+
+class GetterExeption(Exception):
+    pass
+
+
+class TemplateWasntFound(GetterExeption):
+    def __init__(self, template, folders):
+        self.template = template
+        self.folders = folders
+
+    def __str__(self):
+        return 'Template "{}"" wasn\'t found in folder(s) "{}"'.format(
+            self.template, '","'.join(self.folders)
+        )
+
+
+class MissionFolderExistsAlready(GetterExeption):
+    def __init__(self, folder):
+        self.folder = folder
+
+    def __str__(self):
+        return 'Mission folder "{}" exists already'.format(self.folder)
+
+
+def make_mission_from_template(mission, template, force_remove=False):
+    template_full_path = None
+    for template_folder in settings.TEMPLATES_FOLDERS:
+        template_full_path = os.path.join(template_folder, template)
+        if os.path.exists(template_full_path):
+            break
+        else:
+            template_full_path = None
+
+    if template_full_path is None:
+        raise TemplateWasntFound(template, settings.TEMPLATES_FOLDERS)
+
+    folder = Folder(mission)
+    mission_folder = folder.mission_folder()
+    if os.path.exists(mission_folder):
+        if force_remove:
+            shutil.rmtree(mission_folder)
+        else:
+            raise MissionFolderExistsAlready(mission_folder)
+
+    os.mkdir(mission_folder)
+
+    from distutils.dir_util import copy_tree
+    copy_tree(os.path.join(template_full_path, 'source'), mission_folder)
+
+    GG = {}
+    exec(open(os.path.join(template_full_path, 'run.py')).read(), GG)
+    GG['run'](mission)
+
+    folder.mission_config_write({
+        'type': 'local',
+        'url': mission_folder
+    })
+
+
+def mission_git_init(mission, original_url):
+    folder = Folder(mission)
+    mission_folder = folder.mission_folder()
+    logging.info('Init git repository for folder %s', mission_folder)
+    repo = git.Repo.init(mission_folder)
+    for root, dirs, files in os.walk(mission_folder):
+        if root.endswith('.git') or '/.git/' in root:
+            continue
+
+        for file_name in files:
+            abs_file_name = os.path.join(root, file_name)
+            logging.debug('Add file to local git repository %s', abs_file_name)
+            repo.index.add([abs_file_name])
+
+    repo.index.commit("initial commit")
+    origin = repo.create_remote('origin', original_url)
+    origin.push(repo.refs)
+    origin.fetch()
+    repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master)
+    folder.mission_config_write({
+        'type': 'git',
+        'url': original_url
+    })
+
+
+def mission_git_getter(url, slug):
+    # TODO: checkout into mission solder
+    # compile it
+    # build docker
+    # prepare cli interface
+    folder = Folder(slug)
+    destination_path = folder.mission_folder()
+
+    logging.info('Getting a new mission through the git...')
+    logging.info('from %s to %s', url, destination_path)
+
+    if os.path.exists(destination_path):
+        answer = raw_input('Folder {} exists already.'
+                           ' Do you want to overwite it? [y]/n :'.format(destination_path))
+        if answer is '' or answer.lower().startswith('y'):
+            shutil.rmtree(destination_path)
+        else:
+            return
+
+    re_ret = re.search(RE_REPO_BRANCH, url)
+    if re_ret:
+        checkout_url, branch = re_ret.groups()
+    else:
+        checkout_url = url
+        branch = 'master'
+
+    logging.debug('URL info: checkioout url:%s branch:%s', url, branch)
+
+    try:
+        git.Repo.clone_from(checkout_url, destination_path, branch=branch)
+    except git.GitCommandError as e:
+        raise Exception(u"{}, {}".format(e or '', e.stderr))
+    folder.mission_config_write({
+        'type': 'git',
+        'url': url
+    })
+    print('Prepare mission {} from {}'.format(slug, url))
+
+
+def logging_sys(command):
+    logging.debug('Sys: %s', command)
+    os.system(command)
+
+
+def rebuild_native(slug):
+    folder = Folder(slug)
+    logging.info('Building virtualenv in %s', folder.native_env_folder_path())
+    if os.path.exists(folder.native_env_folder_path()):
+        shutil.rmtree(folder.native_env_folder_path())
+
+    logging_sys("virtualenv --system-site-packages -p python3 " + folder.native_env_folder_path())
+    logging_sys("{pip3} install -r {requirements}".format(
+        pip3=folder.native_env_bin('pip3'),
+        requirements=folder.referee_requirements()
+    ))
+    logging_sys("{pip3} install -r {requirements}".format(
+        pip3=folder.native_env_bin('pip3'),
+        requirements=folder.interface_cli_requirements()
+    ))
+
+
+def rebuild_mission(slug):
+    folder = Folder(slug)
+    docker = DockerClient()
+    verification_folder_path = folder.container_verification_folder_path()
+    logging.info("Build docker image %s from %s", folder.image_name(), verification_folder_path)
+    if os.path.exists(verification_folder_path):
+        shutil.rmtree(verification_folder_path)
+
+    copy_tree(folder.verification_folder_path(), verification_folder_path)
+    docker.build(name_image=folder.image_name(), path=verification_folder_path)
+
+
+def recompile_mission(slug):
+    folder = Folder(slug)
+    compiled_path = folder.compiled_folder_path()
+    logging.info("Relink folder to %s", compiled_path)
+    if os.path.exists(compiled_path):
+        shutil.rmtree(compiled_path)
+
+    mission_source = MissionFilesCompiler(compiled_path)
+    mission_source.compile(source_path=folder.mission_folder(), use_link=True)
---
+++ packages/checkio_cli/checkio_cli-0.0.20.tar.gz/checkio_cli-0.0.20/checkio_cli/interfaces/ifolder.py
@@ -0,0 +1,211 @@
+import sys
+from argparse import OPTIONAL
+
+from checkio_cli.config import settings, tools
+from checkio_cli.config.initial_config import console_interactive
+from checkio_cli.getters import mission_git_getter, rebuild_mission, recompile_mission,\
+    rebuild_native, make_mission_from_template, mission_git_init, TemplateWasntFound,\
+    MissionFolderExistsAlready
+
+from checkio_cli.initial import init_path_file, init_home_file
+from checkio_cli.testing import execute_referee
+
+PY3 = sys.version_info[0] == 3
+if PY3:
+    raw_input = input
+
+
+def use_config(parser):
+    parser.add_argument('name', nargs=OPTIONAL)
+    parser.add_argument('value', nargs=OPTIONAL)
+
+    def run(options):
+        if not options.name:
+            return console_interactive()
+
+        if options.value:
+            tools.set_value(options.name, options.value)
+        else:
+            value = settings.get_value(options.name)
+            print("Value is {}".format(value))
+
+    parser.set_defaults(func=run)
+
+
+def use_active(parser):
+    parser.add_argument('mission', nargs=OPTIONAL)
+    parser.add_argument('interpreter', nargs=OPTIONAL)
+
+    def run(options):
+        if options.mission is None:
+            print('Mission: {}\nInterpreter: {}'.format(
+                settings.MISSION, settings.INTERPRETER
+            ))
+            return
+        if options.mission != '-':
+            tools.set_value('mission', options.mission)
+        if options.interpreter:
+            tools.set_value('interpreter', options.interpreter)
+
+    parser.set_defaults(func=run)
+
+
+def use_get_git(parser):
+    parser.add_argument('url')
+    # TODO: make slug parameter optional
+    parser.add_argument('mission')
+    parser.add_argument('--without-container', action='store_true', default=False,
+                        help='start process without using container')
+
+    def run(options):
+        mission_git_getter(options.url, options.mission)
+        recompile_mission(options.mission)
+        if not options.without_container:
+            rebuild_mission(options.mission)
+        init_home_file(options.mission, settings.INTERPRETER)
+        rebuild_native(options.mission)
+        tools.set_value('mission', options.mission)
+    parser.set_defaults(func=run)
+
+
+def use_compile_mission(parser):
+    parser.add_argument('mission', nargs=OPTIONAL)
+
+    def run(options):
+        if options.mission:
+            tools.set_value('mission', options.mission)
+        recompile_mission(options.mission)
+        rebuild_native(options.mission)
+    parser.set_defaults(func=run)
+
+
+def use_build_mission(parser):
+    parser.add_argument('mission', nargs=OPTIONAL)
+
+    def run(options):
+        if options.mission:
+            tools.set_value('mission', options.mission)
+        rebuild_mission(options.mission)
+    parser.set_defaults(func=run)
+
+
+def use_build_native_env(parser):
+    parser.add_argument('mission', nargs=OPTIONAL)
+
+    def run(options):
+        if options.mission:
+            tools.set_value('mission', options.mission)
+        rebuild_native(options.mission)
+    parser.set_defaults(func=run)
+
+
+def use_init(parser):
+    parser.add_argument('filename', nargs=OPTIONAL)
+    parser.add_argument('mission', nargs=OPTIONAL)
+    parser.add_argument('interpreter', nargs=OPTIONAL)
+
+    def run(options):
+        if options.filename is None:
+            return init_home_file(settings.MISSION, settings.INTERPRETER)
+        if '.' in options.filename:
+            mission, interpreter = tools.set_mi(options.mission, options.interpreter)
+            return init_path_file(options.filename, mission, interpreter)
+        # --------------------mission-slug, ----interpreter-name
+        mission, interpreter = tools.set_mi(options.filename, options.mission)
+        return init_home_file(mission, interpreter)
+    parser.set_defaults(func=run)
+
+
+def use_referee(parser, command):
+    parser.add_argument('mission', nargs=OPTIONAL, default=settings.MISSION)
+    parser.add_argument('interpreter', nargs=OPTIONAL, default=settings.INTERPRETER)
+    parser.add_argument('--without-container', action='store_true', default=False,
+                        help='start process without using container')
+    parser.add_argument('--interface-child', action='store_true', default=False,
+                        help='run interface as a child process')
+    parser.add_argument('--interface-only', action='store_true', default=False,
+                        help='out of two preocesses run interface only')
+    parser.add_argument('--referee-only', action='store_true', default=False,
+                        help='out of two preocesses run interface only')
+
+    def run(options, command=command):
+        mission, interpreter = tools.set_mi(options.mission, options.interpreter)
+        execute_referee(command, mission, interpreter,
+                        without_container=options.without_container,
+                        interface_child=options.interface_child,
+                        interface_only=options.interface_only,
+                        referee_only=options.referee_only)
+    parser.set_defaults(func=run)
+
+
+def use_create_mission(parser):
+    parser.add_argument('mission')
+    parser.add_argument('origin', nargs=OPTIONAL)
+    parser.add_argument('--template', nargs=OPTIONAL, default='simpleio')
+    parser.add_argument('--without-container', action='store_true', default=False,
+                        help='start process without using container')
+
+    def run(options):
+        try:
+            make_mission_from_template(options.mission, options.template)
+        except TemplateWasntFound as e:
+            print(e)
+            return
+        except MissionFolderExistsAlready as e:
+            print(e)
+            answer = raw_input('Would you like to remove this folder? y/n').strip().lower()
+            if answer == 'n':
+                return
+            if answer in ['', 'y']:
+                make_mission_from_template(options.mission, options.template, force_remove=True)
+
+        if options.origin:
+            mission_git_init(options.mission, options.origin)
+
+        recompile_mission(options.mission)
+        if not options.without_container:
+            rebuild_mission(options.mission)
+        init_home_file(options.mission, settings.INTERPRETER)
+        rebuild_native(options.mission)
+        tools.set_value('mission', options.mission)
+
+    parser.set_defaults(func=run)
+
+
+def use_git_link_mission(parser):
+    parser.add_argument('mission')
+    parser.add_argument('origin')
+
+    def run(options):
+        mission_git_init(options.mission, options.origin)
+        tools.set_value('mission', options.mission)
+
+    parser.set_defaults(func=run)
+
+
+def use(parser):
+    subparsers = parser.add_subparsers()
+    use_create_mission(subparsers.add_parser('create-mission', help='Create mission folder'))
+    use_git_link_mission(subparsers.add_parser('git-link-mission',
+                                               help='Link a mission folder with'
+                                                    ' git repository'))
+
+    use_config(subparsers.add_parser('config', help='Configure working folder'))
+    use_active(subparsers.add_parser('active', help='Activate mission and interpreter'))
+
+    use_get_git(subparsers.add_parser('get-git',
+                                      help='Download and prepare mission from git-repo'))
+    use_compile_mission(subparsers.add_parser('compile-mission',
+                                              help='Collect all sources in one place'))
+    use_build_mission(subparsers.add_parser('build-mission', help='Prepare a docker image'))
+    use_build_native_env(subparsers.add_parser('build-native-env',
+                                               help='Prepare a ENV for natove run'))
+
+    use_init(subparsers.add_parser('init', help='Initial code'))
+    for name in ('run', 'check', 'battle'):
+        use_referee(subparsers.add_parser(name, help='{} a solution from home folder'.format(name)),
+                    name)
+    parser.add_argument('--logging', action='store_true', default=False,
+                        help='Input file this data for task')
+
+    return parser
