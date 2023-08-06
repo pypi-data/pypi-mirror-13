9c49b1c864c88f910e20d66ac4068b6221bbf68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:05 +0000

    Add package css-html-prettify/1.8.0/css-html-prettify-1.8.0.tar.bz2

---
+++ packages/css-html-prettify/css-html-prettify-1.8.0.tar.bz2/css-html-prettify-1.8.0/setup.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#
+# To generate DEB package from Python Package:
+# sudo pip3 install stdeb
+# python3 setup.py --verbose --command-packages=stdeb.command bdist_deb
+#
+#
+# To generate RPM package from Python Package:
+# sudo apt-get install rpm
+# python3 setup.py bdist_rpm --verbose --fix-python --binary-only
+#
+#
+# To generate EXE MS Windows from Python Package (from MS Windows only):
+# python3 setup.py bdist_wininst --verbose
+#
+#
+# To generate PKGBUILD ArchLinux from Python Package (from PyPI only):
+# sudo pip3 install git+https://github.com/bluepeppers/pip2arch.git
+# pip2arch.py PackageNameHere
+#
+#
+# To Upload to PyPI by executing:
+# python3 setup.py register
+# python3 setup.py bdist_egg sdist --formats=bztar,gztar,zip upload --sign
+
+
+"""Setup.py for Python, as Generic as possible."""
+
+
+import logging as log
+import os
+import re
+import sys
+from copy import copy
+from tempfile import gettempdir
+
+from setuptools import setup
+
+
+##############################################################################
+# EDIT HERE
+
+
+MODULE_PATH = os.path.join(os.path.dirname(__file__), "css-html-prettify.py")
+DESCRIPTION = ("StandAlone Async single-file cross-platform Unicode-ready "
+               "Python3 Prettifier Beautifier for the Web.")
+REQUIREMENTS_FILE = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+
+##############################################################################
+# Dont touch below
+
+
+try:
+    with open(str(MODULE_PATH), "r", encoding="utf-8-sig") as source_code_file:
+        SOURCE = source_code_file.read()
+except:
+    with open(str(MODULE_PATH),  "r") as source_code_file:
+        SOURCE = source_code_file.read()
+
+
+def make_logger(name=str(os.getpid())):
+    """Build and return a Logging Logger."""
+    if not sys.platform.startswith("win") and sys.stderr.isatty():
+        def add_color_emit_ansi(fn):
+            """Add methods we need to the class."""
+            def new(*args):
+                """Method overload."""
+                if len(args) == 2:
+                    new_args = (args[0], copy(args[1]))
+                else:
+                    new_args = (args[0], copy(args[1]), args[2:])
+                if hasattr(args[0], 'baseFilename'):
+                    return fn(*args)
+                levelno = new_args[1].levelno
+                if levelno >= 50:
+                    color = '\x1b[31;5;7m\n '  # blinking red with black
+                elif levelno >= 40:
+                    color = '\x1b[31m'  # red
+                elif levelno >= 30:
+                    color = '\x1b[33m'  # yellow
+                elif levelno >= 20:
+                    color = '\x1b[32m'  # green
+                elif levelno >= 10:
+                    color = '\x1b[35m'  # pink
+                else:
+                    color = '\x1b[0m'  # normal
+                try:
+                    new_args[1].msg = color + str(new_args[1].msg) + ' \x1b[0m'
+                except Exception as reason:
+                    print(reason)  # Do not use log here.
+                return fn(*new_args)
+            return new
+        # all non-Windows platforms support ANSI Colors so we use them
+        log.StreamHandler.emit = add_color_emit_ansi(log.StreamHandler.emit)
+    else:
+        log.debug("Colored Logs not supported on {0}.".format(sys.platform))
+    log_file = os.path.join(gettempdir(), str(name).lower().strip() + ".log")
+    log.basicConfig(level=-1, filemode="w", filename=log_file,
+                    format="%(levelname)s:%(asctime)s %(message)s %(lineno)s")
+    log.getLogger().addHandler(log.StreamHandler(sys.stderr))
+    adrs = "/dev/log" if sys.platform.startswith("lin") else "/var/run/syslog"
+    try:
+        handler = log.handlers.SysLogHandler(address=adrs)
+    except Exception:
+        log.warning("Unix SysLog Server not found,ignored Logging to SysLog.")
+    else:
+        log.addHandler(handler)
+    log.debug("Logger created with Log file at: {0}.".format(log_file))
+    return log
+
+
+# Should be all UTF-8 for best results
+def make_root_check_and_encoding_debug():
+    """Debug and Log Encodings and Check for root/administrator,return Boolean.
+
+    >>> make_root_check_and_encoding_debug()
+    True
+    """
+    log.info(__doc__)
+    log.debug("STDIN Encoding: {0}.".format(sys.stdin.encoding))
+    log.debug("STDERR Encoding: {0}.".format(sys.stderr.encoding))
+    log.debug("STDOUT Encoding:{}".format(getattr(sys.stdout, "encoding", "")))
+    log.debug("Default Encoding: {0}.".format(sys.getdefaultencoding()))
+    log.debug("FileSystem Encoding: {0}.".format(sys.getfilesystemencoding()))
+    log.debug("PYTHONIOENCODING Encoding: {0}.".format(
+        os.environ.get("PYTHONIOENCODING", None)))
+    os.environ["PYTHONIOENCODING"] = "utf-8"
+    if not sys.platform.startswith("win"):  # root check
+        if not os.geteuid():
+            log.critical("Runing as root is not Recommended,NOT Run as root!.")
+            return False
+    return True
+
+
+def set_process_name_and_cpu_priority(name):
+    """Set process name and cpu priority.
+
+    >>> set_process_name_and_cpu_priority("test_test")
+    True
+    """
+    try:
+        os.nice(19)  # smooth cpu priority
+        return True
+    except Exception:
+        return False  # this may fail on windows and its normal, so be silent.
+
+
+def find_this(search, source=SOURCE):
+    """Take a string and a filename path string and return the found value."""
+    log.debug("Searching for {what}.".format(what=search))
+    if not search or not source:
+        log.warning("Not found on source: {what}.".format(what=search))
+        return ""
+    return str(re.compile(r".*__{what}__ = '(.*?)'".format(
+        what=search), re.S).match(source).group(1)).strip().replace("'", "")
+
+
+def parse_requirements(path=REQUIREMENTS_FILE):
+    """Rudimentary parser for the requirements.txt file.
+
+    We just want to separate regular packages from links to pass them to the
+    'install_requires' and 'dependency_links' params of the 'setup()'.
+    """
+    log.debug("Parsing Requirements from file {what}.".format(what=path))
+    pkgs, links = ["pip"], []
+    if not os.path.isfile(path):
+        return pkgs, links
+    try:
+        requirements = map(str.strip, path.splitlines())
+    except Exception as reason:
+        log.warning(reason)
+        return pkgs, links
+    for req in requirements:
+        if not req:
+            continue
+        if 'http://' in req.lower() or 'https://' in req.lower():
+            links.append(req)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
+            pkgs.append('{package}=={ver}'.format(package=name, ver=version))
+        else:
+            pkgs.append(req)
+    log.debug("Requirements found: {what}.".format(what=(pkgs, links)))
+    return pkgs, links
+
+
+make_logger()
+make_root_check_and_encoding_debug()
+set_process_name_and_cpu_priority("setup_py")
+install_requires_list, dependency_links_list = parse_requirements()
+log.info("Starting build of setuptools.setup().")
+
+
+##############################################################################
+# EDIT HERE
+
+
+setup(
+
+    name="css-html-prettify",
+    version=find_this("version"),
+
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+
+    url=find_this("url"),
+    license=find_this("license"),
+
+    author=find_this("author"),
+    author_email=find_this("email"),
+    maintainer=find_this("author"),
+    maintainer_email=find_this("email"),
+
+    include_package_data=True,
+    zip_safe=True,
+
+    extras_require={"pip": ["pip"]},
+    tests_require=['pip'],
+    requires=['pip', 'beautifulsoup4'],
+
+    install_requires=install_requires_list,
+    dependency_links=dependency_links_list,
+
+    scripts=["css-html-prettify.py"],
+
+    keywords=['CSS', 'HTML', 'Prettify', 'CSS3', 'HTML5', 'Web', 'Beautify'],
+
+    classifiers=[
+
+        'Development Status :: 5 - Production/Stable',
+
+        'Environment :: Console',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Other Audience',
+
+        'Natural Language :: English',
+
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+
+        'Topic :: Software Development',
+
+    ],
+)
+
+
+log.info("Finished build of setuptools.setup().")
