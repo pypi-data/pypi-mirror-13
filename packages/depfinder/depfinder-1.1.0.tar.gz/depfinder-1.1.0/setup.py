e9e70367e4155b9d8f5e21215a0bac041198aebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:42 +0000

    Add package depfinder/1.1.0/depfinder-1.1.0.tar.gz

---
+++ packages/depfinder/depfinder-1.1.0.tar.gz/depfinder-1.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+VERSION = '1.1.0'
+
+setup(name='depfinder',
+      version=VERSION,
+      author='Eric Dill',
+      author_email='thedizzle@gmail.com',
+      py_modules=['depfinder'],
+      description='Find all the imports in your library',
+      url='http://github.com/ericdill/depfinder',
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      install_requires=['stdlib_list', 'setuptools'],
+      license='GPLv3',
+      entry_points = {"console_scripts": ['depfinder = depfinder:cli']},
+)
