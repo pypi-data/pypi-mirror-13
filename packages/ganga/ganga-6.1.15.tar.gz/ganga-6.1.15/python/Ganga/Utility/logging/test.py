4d18bb1840a1fa761a12cda1df7cb93ae9d3076c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:40 +0000

    Add package ganga/6.1.15/ganga-6.1.15.tar.gz

---
+++ packages/ganga/ganga-6.1.15.tar.gz/ganga-6.1.15/python/Ganga/Utility/logging/test.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+from . import test
+import Ganga.Utility.logging
+Ganga.Utility.logging.config['Ganga.Utility.logging'] = 'DEBUG'
+
+logger = Ganga.Utility.logging.getLogger()
+
+import sys
+print(sys.path)
+
+print(logger.name)
+
+logger.info('info test')
+logger.error('error test')
+logger.warning('warning test')
+
+# Ganga.Utility.logging.bootstrap()
