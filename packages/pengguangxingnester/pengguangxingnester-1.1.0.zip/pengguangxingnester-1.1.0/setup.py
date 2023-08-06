3d11e177b9cf39df85616e1c161a53b022d26702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:26 +0000

    Add package pengguangxingnester/1.1.0/pengguangxingnester-1.1.0.zip

---
+++ packages/pengguangxingnester/pengguangxingnester-1.1.0.zip/pengguangxingnester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name        = 'pengguangxingnester',
+	version     = '1.1.0',
+	py_modules  = ['pengguangxingnester'],
+	author      = 'peng',
+	url         = 'http://127.0.0.1',
+	description = 'A simple printer of nested lists',
+	)
