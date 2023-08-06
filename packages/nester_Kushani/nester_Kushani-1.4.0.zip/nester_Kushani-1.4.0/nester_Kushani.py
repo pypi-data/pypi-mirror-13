1fd4fe0bb06b804b0b0da8656063709e181fe84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:28 +0000

    Add package nester_Kushani/1.4.0/nester_Kushani-1.4.0.zip

---
+++ packages/nester_Kushani/nester_Kushani-1.4.0.zip/nester_Kushani-1.4.0/nester_Kushani.py
@@ -0,0 +1,54 @@
+"""This is the “nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists."""
+
+
+"""This function takes a positional argument called “the_list", which is any
+Python list (of, possibly, nested lists). Each data item in the provided list
+is (recursively) printed to the screen on its own line."""
+import sys
+
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item, indent, level+1,fh)
+                else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t", end='',file=fh)
+                        print(each_item,file=fh)
+
+		
+        
+        
+
+        
+	
+
+
+
+
+# function 1
+"""
+def print_lol(the_list):
+     
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item)
+		else:
+			print(each_item)
+
+
+"""
+
+"""
+movies = [
+"The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+["Graham Chapman",
+["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print_lol(movies,0)
+"""
+"""
+names = ['John', 'Eric', ['Cleese', 'Idle'], 'Michael', ['Palin']]
+print_lol(names,True,0)
+"""
