2c298ed4f50670099e5d44305a1737511311b8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:48 +0000

    Add package autoprotocol/2.7.0/autoprotocol-2.7.0.tar.gz

---
+++ packages/autoprotocol/autoprotocol-2.7.0.tar.gz/autoprotocol-2.7.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='autoprotocol',
+    url='http://github.com/autoprotocol/autoprotocol-python',
+    author='Vanessa Biggers',
+    description='Python library for generating Autoprotocol',
+    author_email="vanessa@transcriptic.com",
+    version='2.7.0',
+    test_suite='test',
+    packages=['autoprotocol']
+)
