da30df4fefc4f2fa41d0678c95930caf0b448d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package bdmd/1.0.0/bdmd-1.0.0.tar.gz

---
+++ packages/bdmd/bdmd-1.0.0.tar.gz/bdmd-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name='bdmd',
+	version='1.0.0',
+	py_modules=['bdmd'],
+	authon='zhangn',
+	url='http://www.pandaTV.com',
+	description='A great dba,a great coder!',
+)
