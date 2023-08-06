20cb3ba2d15874247f1ebdf1fa5d5a5d4949f62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:54 +0000

    Add package easytrader/0.3.4/easytrader-0.3.4.tar.gz

---
+++ packages/easytrader/easytrader-0.3.4.tar.gz/easytrader-0.3.4/easytrader/api.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+from .helpers import disable_log
+from .httrader import HTTrader
+from .yhtrader import YHTrader
+from .yjbtrader import YJBTrader
+
+
+def use(broker, debug=True):
+    """用于生成特定的券商对象
+    :param broker:券商名支持 ['ht', 'HT', '华泰’] ['yjb', 'YJB', ’佣金宝'] ['yh', 'YH', '银河']
+    :return the class of trader
+
+    Usage::
+
+        >>> import easytrader
+        >>> user = easytrader.use('ht')
+        >>> user.prepare('ht.json')
+    """
+    if not debug:
+        disable_log()
+    if broker.lower() in ['ht', 'HT', '华泰']:
+        return HTTrader()
+    if broker.lower() in ['yjb', 'YJB', '佣金宝']:
+        return YJBTrader()
+    if broker.lower() in ['yh', 'YH', '银河']:
+        return YHTrader()
