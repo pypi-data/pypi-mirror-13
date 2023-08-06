8af65295e458940fe094a72271b0e6f01e8c181f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:40 +0000

    Add package django-admin-resumable-js/1.2/django-admin-resumable-js-1.2.tar.gz

---
+++ packages/django-admin-resumable-js/django-admin-resumable-js-1.2.tar.gz/django-admin-resumable-js-1.2/admin_resumable/views.py
@@ -0,0 +1,69 @@
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from admin_resumable.files import ResumableFile
+from django.core.exceptions import ImproperlyConfigured
+from django.core.files.storage import get_storage_class
+from django.contrib.admin.views.decorators import staff_member_required
+
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+
+def get_chunks_subdir():
+    return getattr(settings, 'ADMIN_RESUMABLE_SUBDIR', 'admin_uploaded')
+
+
+def get_chunks_dir():
+    chunks_subdir = get_chunks_subdir()
+    media_root = getattr(settings, 'MEDIA_ROOT', None)
+    if not media_root:
+        raise ImproperlyConfigured(
+            'You must set settings.MEDIA_ROOT')
+    chunks_dir = os.path.join(media_root, chunks_subdir)
+    ensure_dir(chunks_dir)
+    return chunks_dir
+
+
+def get_storage():
+    """
+    Looks at the ADMIN_RESUMABLE_STORAGE setting and returns
+    an instance of the storage class specified.
+
+    Defaults to django.core.files.storage.FileSystemStorage.
+
+    Any custom storage class used here must either be a subclass of
+    django.core.files.storage.FileSystemStorage, or accept a location
+    init parameter.
+    """
+    return get_storage_class(getattr(
+        settings, 'ADMIN_RESUMABLE_STORAGE',
+        'django.core.files.storage.FileSystemStorage'
+    ))(location=get_chunks_dir())
+
+
+@staff_member_required
+def admin_resumable(request):
+    storage = get_storage()
+    if request.method == 'POST':
+        chunk = request.FILES.get('file')
+        r = ResumableFile(storage, request.POST)
+        if not r.chunk_exists:
+            r.process_chunk(chunk)
+        if r.is_complete:
+            actual_filename = storage.save(r.filename, r.file)
+            r.delete_chunks()
+            return HttpResponse(get_chunks_subdir() + "/" + actual_filename)
+        return HttpResponse('chunk uploaded')
+    elif request.method == 'GET':
+        r = ResumableFile(storage, request.GET)
+        if not r.chunk_exists:
+            return HttpResponse('chunk not found', status=404)
+        if r.is_complete:
+            actual_filename = storage.save(r.filename, r.file)
+            r.delete_chunks()
+            return HttpResponse(get_chunks_subdir() + "/" + actual_filename)
+        return HttpResponse('chunk exists')
