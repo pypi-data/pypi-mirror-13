c9c82dc37380875c7f44e21341ba5172d52cb990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:45 +0000

    Add package hzc_nester/1.1.0/hzc_nester-1.1.0.zip

---
+++ packages/hzc_nester/hzc_nester-1.1.0.zip/hzc_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'hzc_nester',
+        version      = '1.1.0',
+        py_modules   = ['hzc_nester'],
+        author       = 'minatsu',
+        author_email = '1375390735@qq.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
