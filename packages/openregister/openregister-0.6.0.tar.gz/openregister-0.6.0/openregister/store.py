0bd3573bd611eaba76388b74b4989bf38d15cb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:45 +0000

    Add package openregister/0.6.0/openregister-0.6.0.tar.gz

---
+++ packages/openregister/openregister-0.6.0.tar.gz/openregister-0.6.0/openregister/store.py
@@ -0,0 +1,26 @@
+class Store(object):
+
+    """Interface for storage of Items."""
+    def __init__(self, register="unknown"):
+        pass
+
+    def put(self, item):
+        """Store item"""
+        raise NotImplementedError
+
+    def get(self, hash):
+        """Retrieve item"""
+        raise NotImplementedError
+
+    def add(self, item, timestamp=None):
+        """Add item as a new entry"""
+        raise NotImplementedError
+
+    def get_latest(self, name=None):
+        raise NotImplementedError
+
+    def find(self, query={}, page=1, page_size=50):
+        raise NotImplementedError
+
+    def find_all(self, query={}):
+        raise NotImplementedError
