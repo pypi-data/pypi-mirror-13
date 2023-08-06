e7e9f6aed96c38ae64f4f2cd4a18527c4da31b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package PearPyPac/0.0.5/PearPyPac-0.0.5.tar.gz

---
+++ packages/PearPyPac/PearPyPac-0.0.5.tar.gz/PearPyPac-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "PearPyPac",
+    version = "0.0.5",
+    license = "MIT",
+    packages=['PearPyPac'],
+    description = 'pear library',
+    author = 'Hiroki Umatani',
+    author_email = 'h_umatani@pear.chat',
+    url = 'https://github.com/HirokiUmatani/PearPackage',
+    keywords = ['pear', 'db', 'mysql', 'cgi', 'get', 'post', 'utility'],
+)
