45d884242926256894e326d30ff2f4a0aa164b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package el320_nester/1.3.0/el320_nester-1.3.0.zip

---
+++ packages/el320_nester/el320_nester-1.3.0.zip/el320_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'el320_nester',
+    version      = '1.3.0',
+    py_modules   = ['el320_nester'],
+    author       = 'eleanor',
+    description  = 'A simple program to list the contents of nested lists',
+    author_email = 'sudipti320@gmail.com',
+    
+)	
