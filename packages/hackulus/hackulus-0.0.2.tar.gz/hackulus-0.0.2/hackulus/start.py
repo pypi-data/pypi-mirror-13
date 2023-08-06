22de9465685ea3c79b548f873b3e6390edf1edbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:07 +0000

    Add package hackulus/0.0.2/hackulus-0.0.2.tar.gz

---
+++ packages/hackulus/hackulus-0.0.2.tar.gz/hackulus-0.0.2/hackulus/start.py
@@ -0,0 +1,42 @@
+import mongoengine as mongo
+from flask import Flask, request, jsonify
+from flask.templating import render_template
+from hackulus.config import *
+from hackulus.utils import Interpreter
+from hackulus.models import Hacker
+
+app_db = mongo.connect('hackulus')
+app = Flask(__name__)
+
+app_shell = Interpreter()
+
+@app.route("/")
+def home():
+    hackers = Hacker.objects.all()
+    return render_template('home.html', hackers=hackers)
+
+@app.route('/shell/<language>', methods=['GET','POST'])
+def shell(language):
+    """ Takes in 'querystring' from POST request and then
+        passes it as an argument into a function of form f(querystring)
+        that returns a string """
+    if request.method == 'POST':
+#         print request.form['querystring']
+#         print app_shell.input(request.form['querystring'])
+        retval = app_shell.input(request.form['querystring'])
+        print(retval)
+        return jsonify(retval)
+    else:
+        app_shell.set_language(language)
+        return render_template('shell.html', language=language, APP_CONFIG=APP_CONFIG)
+
+@app.route("/login")
+def login():
+    return "Yolo"
+
+@app.route('/logout')
+def logout():
+    return "Byebye"
+
+if __name__ == "__main__":
+    app.run(host='192.168.0.73', port=8000)
