c144b6dfa482cfc9743fafab953cf627967e496a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package basegateway/0.1/basegateway-0.1.tar.gz

---
+++ packages/basegateway/basegateway-0.1.tar.gz/basegateway-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name = 'basegateway',
+  version = '0.1',
+  py_modules = ['basegateway'],
+  description = 'A base gateway to make api calls',
+  author = 'Snapsheet',
+  author_email = 'technotifications@snapsheet.me',
+  url = 'https://github.com/bodyshopbidsdotcom/basegateway',
+  download_url = 'https://github.com/bodyshopbidsdotcom/basegateway/tarball/0.1',
+  keywords = ['api', 'gateway', 'http', 'REST'],
+  classifiers = [
+    "Topic :: Internet :: WWW/HTTP",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
