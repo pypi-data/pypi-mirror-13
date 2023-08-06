db5f5f86d2722ff4a47e05391ab3dc1d9944e2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:42 +0000

    Add package edocuments/0.1.0/edocuments-0.1.0.tar.gz

---
+++ packages/edocuments/edocuments-0.1.0.tar.gz/edocuments-0.1.0/edocuments/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from yaml import load
+from autoupgrade import AutoUpgrade
+from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QApplication
+
+from edocuments.main_widget import MainWindow
+
+CONFIG_FILENAME = "edocuments.yaml"
+
+if 'APPDATA' in os.environ:
+    CONFIG_PATH = os.path.join(os.environ['APPDATA'], CONFIG_FILENAME)
+elif 'XDG_CONFIG_HOME' in os.environ:
+    CONFIG_PATH = os.path.join(os.environ['XDG_CONFIG_HOME'], CONFIG_FILENAME)
+else:
+    CONFIG_PATH = os.path.join(os.environ['HOME'], '.config', CONFIG_FILENAME)
+
+config = {}
+root_folder = None
+settings = None
+
+
+def gui_main():
+    global config, root_folder, settings
+    with open(CONFIG_PATH) as f:
+        config = load(f.read())
+    root_folder = "%s/%s/" % (
+        os.path.expanduser('~'),
+        config.get("root_folder"),
+    )
+    settings = QSettings("org", "edocuments")
+
+    app = QApplication(sys.argv)
+    mw = MainWindow()
+    if settings.value("geometry") is not None:
+        mw.restoreGeometry(settings.value("geometry"))
+    if settings.value("state") is not None:
+        mw.restoreState(settings.value("state"))
+
+    au = AutoUpgrade('edocuments')
+    if au.check():
+        msg = QMessageBox(self)
+        msg.setWindowTitle("eDocuments - Upgrade")
+        msg.setText("A new version is available")
+        msg.setInformativeText("Do you want to do anupdate and restart?")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        ret = msg.exec()
+        if ret == QMessageBox.Yes:
+            au.upgrade(dependencies=True)
+            au.restart()
+
+    mw.show()
+    app.exec()
+    settings.setValue("geometry", mw.saveGeometry())
+    settings.setValue("state", mw.saveState())
+    settings.sync()
