c803f4040a54688157310cb952e0c6c29d0c8968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package pygnurl/1.0.1/pygnurl-1.0.1.zip

---
+++ packages/pygnurl/pygnurl-1.0.1.zip/pygnurl-1.0.1/readline.py
@@ -0,0 +1,4 @@
+"""Importing this module enables command line editing using GNU
+readline.
+"""
+from pygnurl.modules.readline import *  # pylint: disable=wildcard-import
---
+++ packages/pygnurl/pygnurl-1.0.1.zip/pygnurl-1.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+"""Packaging script for pygnurl."""
+
+from setuptools import setup
+
+
+def get_long_description():
+    """Return the contents of README.rst."""
+    with open('README.rst') as readme:
+        return readme.read()
+
+
+setup(
+    name='pygnurl',
+    version='1.0.1',
+    description='Dynamic GNU Readline interface',
+    long_description=get_long_description(),
+    author='evanunderscore',
+    author_email='evanunderscore@gmail.com',
+    url='https://pypi.python.org/pypi/pygnurl',
+    license='GNU General Public License v2',
+    packages=['pygnurl', 'pygnurl.modules', 'pygnurl.examples'],
+    py_modules=['readline'],
+    extras_require={
+        ':sys_platform=="win32"': ['colorama>=0.3.5'],
+    },
+    tests_require=[
+        'coverage',
+        'nose',
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+    ],
+    keywords='gnu readline bindings',
+)
