c3e667114e321f1a264b01b4b24486c19bb44121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package galgo/0.0.2/galgo-0.0.2.tar.gz

---
+++ packages/galgo/galgo-0.0.2.tar.gz/galgo-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+REQUIRES = ['argtools', 'pysam', 'numpy']
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+if os.path.exists(README):
+    long_description = open(README).read() + '\n\n'
+else:
+    long_description = 'A collection of algorithms and tools for genome analysis'
+VERSION = '0.0.2'
+
+setup(name='galgo',
+      version=VERSION,
+      install_requires=REQUIRES,
+      description='A collection of algorithms and tools for genome analysis',
+      long_description=long_description,
+      classifiers=['Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics'],
+      keywords='genome bio-informatics fasta',
+      author='Takahiro Mimori',
+      author_email='takahiro.mimori@gmail.com',
+      py_modules=['galgo'],
+      package_data = {'': ['README.md']},
+      url='https://github.com/m1m0r1/galgo',
+)
