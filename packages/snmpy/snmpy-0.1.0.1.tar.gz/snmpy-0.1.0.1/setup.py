4ae253369132e83151e68ead72886e4218d47a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package snmpy/0.1.0.1/snmpy-0.1.0.1.tar.gz

---
+++ packages/snmpy/snmpy-0.1.0.1.tar.gz/snmpy-0.1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+setup(
+    name='snmpy',
+    version='0.1.0.1',
+    description='Simple pysnmp wrapper.',
+    long_description='The pysnmp library is somewhat tricky to work with. This package makes reading and writing SNMP OIDs much easier.',
+    url='https://github.com/SimplicityGuy/snmpy',
+    author='Robert Wlodarczyk',
+    author_email='robert@simplicityguy.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Monitoring',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='snmp monitoring',
+    install_requires=['pysnmp'],
+    platforms='any',
+)
