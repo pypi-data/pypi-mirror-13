e166fb09d3289830824cf561cb4e2c2b461f0cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package pysimpled/1.0.1/pysimpled-1.0.1.tar.gz

---
+++ packages/pysimpled/pysimpled-1.0.1.tar.gz/pysimpled-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pysimpled',
+    version='1.0.1',
+    package_dir={'pysimpled': "sources/pysimpled"},
+    packages=['pysimpled'],
+    url='https://github.com/bsimpson888/pysimpled',
+    license='Apache License',
+    author='Marco Bartel',
+    author_email='bsimpson888@gmail.com',
+    description='Simple to use context managers for server side scripts like daemons'
+)
