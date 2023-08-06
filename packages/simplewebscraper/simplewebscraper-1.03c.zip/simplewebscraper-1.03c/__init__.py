4b4becd5c792bed8de54dd922cd7fb598a1e9f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package simplewebscraper/1.03c/simplewebscraper-1.03c.zip

---
+++ packages/simplewebscraper/simplewebscraper-1.03c.zip/simplewebscraper-1.03c/__init__.py
@@ -0,0 +1,29 @@
+"""
+This is the simplewebscraper package.
+"""
+
+from connection import Connect
+
+
+class Scraper(Connect):
+    def __init__(self, log="simplescraper.log"):
+        from logger import get_logger
+        logger = get_logger(log, maxbytes=2147483648)
+        Connect.__init__(self, logger)
+
+
+class Browser(object):
+    from cookies import Chrome, Firefox
+    Chrome = Chrome
+    Firefox = Firefox
+
+
+class ProxyPool(object):
+    from proxy_aggregators import Hidester
+    Hidester = Hidester
+
+
+class HTTPMethod(object):
+    from enumerations import HTTPMethods
+    GET = HTTPMethods.GET
+    POST = HTTPMethods.POST
