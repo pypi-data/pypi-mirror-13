0bd4150884b225bce7a33d0ac988c63df55ffe6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:17 +0000

    Add package symcws/1.4/symcws-1.4.tar.gz

---
+++ packages/symcws/symcws-1.4.tar.gz/symcws-1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'symcws',
+  packages = ['symcws'],
+  version = '1.4',
+  description = 'A Python library for the Symantec Website Security SOAP API',
+  author = 'Tobias Zatti',
+  author_email = 'tobias_zatti@symantec.com',
+  url = 'https://github.com/tzatti/symcws',
+  download_url = 'https://github.com/tzatti/symcws/tarball/1.4',
+  keywords = ['SOAP', 'API', 'Symantec', 'Reseller', 'Website Security'],
+  classifiers = []
+)
