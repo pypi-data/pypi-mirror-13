eaec01e707e0095584cc2cabad3e2582cd6e566b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:41 +0000

    Add package pyspacewar/0.9.8/pyspacewar-0.9.8.tar.gz

---
+++ packages/pyspacewar/pyspacewar-0.9.8.tar.gz/pyspacewar-0.9.8/src/pyspacewar/version.py
@@ -0,0 +1,24 @@
+"""
+PySpaceWar version number tracker.
+"""
+
+version = "0.9.8"
+
+
+def get_git_revision(unknown='', format=' (git %s)'):
+    """Return the latest revision number of the files in the package."""
+    import os
+    import subprocess
+    package_root = os.path.dirname(__file__)
+    try:
+        p = subprocess.Popen(['git', 'describe', '--always'],
+                             cwd=package_root,
+                             stdout=subprocess.PIPE)
+    except OSError:
+        return unknown
+    else:
+        return format % p.communicate()[0].strip()
+
+
+if 'dev' in version:
+    version += get_git_revision(format='+git.%s')
