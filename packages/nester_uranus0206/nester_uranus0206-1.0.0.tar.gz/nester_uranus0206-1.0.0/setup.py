ef5be60f3d324fc671dec6c6bd2648f69b3d487f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:58 +0000

    Add package nester_uranus0206/1.0.0/nester_uranus0206-1.0.0.tar.gz

---
+++ packages/nester_uranus0206/nester_uranus0206-1.0.0.tar.gz/nester_uranus0206-1.0.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(
+        name = 'nester_uranus0206',
+        version = '1.0.0',
+        py_modules = ['nester_uranus0206'],
+        author = 'uranus0206',
+        author_email = 'uranus0206@gmail.com',
+        url = '',
+        description = 'A simple printer of nested lists',
+        )
