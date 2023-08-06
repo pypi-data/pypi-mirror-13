f546cc6dc33c2d6c5a8bd3f2d2e7002a6b6f14e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:34 +0000

    Add package simplewebscraper/1.42b0/simplewebscraper-1.42b0.zip

---
+++ packages/simplewebscraper/simplewebscraper-1.42b0.zip/simplewebscraper-1.42b0/src/enumerations.py
@@ -0,0 +1,13 @@
+class HTTPMethods(object):
+    GET = 1
+    POST = 2
+    PUT = 3
+    DELETE = 4
+    HEAD = 5
+    OPTIONS = 6
+    CONNECT = 7
+
+
+class OperatingSystem(object):
+    LINUX = 1
+    WINDOWS = 2
