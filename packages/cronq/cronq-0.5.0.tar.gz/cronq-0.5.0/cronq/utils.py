02de178386d02eb5d57056256b4b20d5db568ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package cronq/0.5.0/cronq-0.5.0.tar.gz

---
+++ packages/cronq/cronq-0.5.0.tar.gz/cronq-0.5.0/cronq/utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import datetime
+import logging
+import os
+
+
+def setup_logging():
+    if os.getenv('LOGGING_FILE_CONFIG'):
+        logging.config.fileConfig(os.getenv('LOGGING_FILE_CONFIG'))
+    else:
+        logformat = "[%(asctime)s] %(name)s [pid:%(process)d] - %(levelname)s - %(message)s"  # noqa
+        datefmt = "%Y-%m-%d %H:%M:%S"
+        logging.basicConfig(level=logging.DEBUG,
+                            format=logformat,
+                            datefmt=datefmt)
+
+
+def split_command(string):
+    commands = string.strip().split(';')
+    ret = []
+    for command in commands:
+        ret.extend(command.strip().split(' && '))
+    return ret
+
+
+def task_status(first, last=None):
+    if last is None:
+        return first['status']
+
+    if last['status'] == 'finished':
+        if int(last['return_code']) == 0:
+            return 'succeeded'
+        return 'failed'
+    return last['status']
+
+
+def took(first_time, last_time):
+    if type(first_time) is not datetime.datetime:
+        return ""
+
+    if type(last_time) is not datetime.datetime:
+        return ""
+
+    elapsed_time = last_time - first_time
+    return int(elapsed_time.total_seconds())
