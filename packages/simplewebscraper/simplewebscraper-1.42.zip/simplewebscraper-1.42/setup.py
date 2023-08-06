515f3eaeb1f05a53b86f791c6cdbdf5e012519d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:32 +0000

    Add package simplewebscraper/1.42/simplewebscraper-1.42.zip

---
+++ packages/simplewebscraper/simplewebscraper-1.42.zip/simplewebscraper-1.42/setup.py
@@ -0,0 +1,32 @@
+import os
+# from distutils.core import setup
+from setuptools import setup
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='simplewebscraper',
+    version='1.042',
+    license='lgpl',
+    packages=['simplewebscraper'],
+    package_dir={'simplewebscraper': 'src'},
+    install_requires = ["requests[security]"],
+    package_data={'': ['README.rst']},
+    author='Alexander Ward',
+    author_email='alexander.ward1@gmail.com',
+    maintainer_email = 'alexander.ward1@gmail.com',
+    description='Python library that makes web scraping very simple.',
+    long_description=read('README.rst'),
+    url='https://github.com/alexanderward/simplewebscraper',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Operating System :: OS Independent',
+    ]
+)
