c3133a0c7ce5da4660a3c096fb897183e8bf2d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:34 +0000

    Add package basespaceapp/0.0.5/basespaceapp-0.0.5.tar.gz

---
+++ packages/basespaceapp/basespaceapp-0.0.5.tar.gz/basespaceapp-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup  # , find_packages
+
+setup(name='basespaceapp',
+      version='0.0.5',
+      description='template for basespace native app.',
+      url='https://github.com/alaindomissy/basespaceapp',
+      author='Alain Domissy',
+      author_email='alaindomissy@gmail.com',
+      license='MIT',
+      # packages=find_packages(exclude=["tests"]),
+      packages=['basespaceapp'],
+      install_requires=[
+            'six'
+      ],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+      ],
+      zip_safe=False)
