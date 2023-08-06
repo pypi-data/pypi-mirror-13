fa947c947bfe68b0361eb7a1ba93942207aac2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package mammoth/0.3.29/mammoth-0.3.29.tar.gz

---
+++ packages/mammoth/mammoth-0.3.29.tar.gz/mammoth-0.3.29/mammoth/document_matchers.py
@@ -0,0 +1,25 @@
+import collections
+
+
+def paragraph(style_id=None, style_name=None, numbering=None):
+    return ParagraphMatcher(style_id, style_name, numbering)
+
+
+ParagraphMatcher = collections.namedtuple("ParagraphMatcher", ["style_id", "style_name", "numbering"])
+ParagraphMatcher.element_type = "paragraph"
+
+
+def run(style_id=None, style_name=None):
+    return RunMatcher(style_id, style_name)
+
+
+RunMatcher = collections.namedtuple("RunMatcher", ["style_id", "style_name"])
+RunMatcher.element_type = "run"
+
+
+class underline(object):
+    element_type = "underline"
+
+
+class strikethrough(object):
+    element_type = "strikethrough"
---
+++ packages/mammoth/mammoth-0.3.29.tar.gz/mammoth-0.3.29/mammoth/lists.py
@@ -0,0 +1,43 @@
+import sys
+
+
+def collect(values):
+    result = []
+    _collect(values, result)
+    return result
+
+
+def _collect(values, result):
+    for value in values:
+        if isinstance(value, list):
+            _collect(value, result)
+        elif value is not None:
+            result.append(value)
+
+def unique(values):
+    output = []
+    seen = set()
+    for value in values:
+        if value not in seen:
+            seen.add(value)
+            output.append(value)
+    return output
+
+
+def flat_map(func, values):
+    return [
+        element
+        for value in values
+        for element in func(value)
+    ]
+
+
+if sys.version_info[0] == 2:
+    map = map
+    filter = filter
+else:
+    import builtins
+    def map(*args, **kwargs):
+        return list(builtins.map(*args, **kwargs))
+    def filter(*args, **kwargs):
+        return list(builtins.filter(*args, **kwargs))
