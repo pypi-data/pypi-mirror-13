ae9ff93ef62649116efb8bddc4878ed53b3de938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:09 +0000

    Add package gns3-net-converter/1.3.0/gns3-net-converter-1.3.0.tar.gz

---
+++ packages/gns3-net-converter/gns3-net-converter-1.3.0.tar.gz/gns3-net-converter-1.3.0/README.rst
@@ -0,0 +1,35 @@
+gns3-converter: GNS3 Topology Converter
+***************************************
+
+The original code is from Daniel Lintott:
+https://github.com/dlintott/gns3-converter
+
+The GNS3 team forked it in order to update it for the last versions of GNS3.
+
+.. image:: https://travis-ci.org/gns3/gns3-converter.svg?branch=master
+    :target: https://travis-ci.org/gns3/gns3-converter
+
+.. image:: https://img.shields.io/coveralls/gns3/gns3-converter/master.svg
+    :target: https://coveralls.io/r/gns3/gns3-converter?branch=master 
+
+.. image:: https://img.shields.io/pypi/v/gns3-converter.svg
+    :target: https://pypi.python.org/pypi/gns3-converter
+
+.. image:: https://img.shields.io/pypi/l/gns3-converter.svg
+    :target: https://pypi.python.org/pypi/gns3-converter
+
+GNS3 Converter is designed to convert old ini-style GNS3 topologies (<=0.8.7)
+to the newer version v1+ JSON format for use in GNS3 v1+
+
+The converter will convert all IOS, Cloud and VirtualBox devices to the new
+format. It will also convert all QEMU based devices (QEMU VM, ASA, PIX, JUNOS &
+IDS). VPCS nodes will be converted to cloud devices due to lack of information
+the 0.8.7 topology files.
+
+For topologies containing snapshots, the snapshots will also be converted to
+the new format automatically.
+
+Documentation
+=============
+Current documentation for gns3-converter can be found here:
+ http://gns3-converter.readthedocs.org/en/latest/
---
+++ packages/gns3-net-converter/gns3-net-converter-1.3.0.tar.gz/gns3-net-converter-1.3.0/setup.py
@@ -0,0 +1,64 @@
+import os.path
+import sys
+import warnings
+from setuptools import setup
+
+executables = []
+setup_options = {}
+if sys.platform == 'win32':
+    try:
+        from cx_Freeze import setup, Executable
+
+        executables.append(Executable("gns3-converter.py"))
+    except ImportError:
+        warnings.warn('Module "cx_Freeze" not found. Skipping exe file creation.')
+
+    setup_options = {
+        'build_exe': {
+            'namespace_packages': 'gns3converter',
+            'packages': 'gns3converter',
+            'zip_includes': [
+                (
+                    'gns3converter/configspec',
+                    os.path.join('gns3converter', 'configspec')
+                )
+            ],
+            'include_msvcr': True
+        }
+    }
+
+setup(
+    name='gns3-net-converter',
+    version=__import__('gns3converter').__version__,
+    packages=['gns3converter'],
+    url='https://github.com/gns3/gns3-converter',
+    license='GPLv3+',
+    author='GNS3 Team',
+    author_email='developers@gns3.net',
+    description='Official fork by GNS3 team of the gns3 converter.'
+                'Convert old ini-style GNS3 topologies (<=0.8.7) to the '
+                'newer version 1+ JSON format',
+    long_description=open("README.rst", "r").read(),
+    test_suite='tests',
+    install_requires=['configobj'],
+    package_data={'gns3converter': ['configspec']},
+    entry_points={
+        'console_scripts': ['gns3-converter = gns3converter.main:main']
+    },
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License '
+        'v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Education',
+        'Topic :: Utilities'
+    ]
+)
