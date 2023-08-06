5a7b60a259d90ec0bf0da59c475d8f1f5d0a015b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:14 +0000

    Add package bacpypes/0.13.7/bacpypes-0.13.7-py2.7.egg

---
+++ packages/bacpypes/bacpypes-0.13.7-py2.7.egg/bacpypes/__init__.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+"""BACnet Python Package"""
+
+#
+#   Platform Check
+#
+
+import sys as _sys
+import warnings as _warnings
+
+_supported_platforms = ('linux2', 'win32', 'darwin')
+
+if _sys.platform not in _supported_platforms:
+    _warnings.warn("unsupported platform", RuntimeWarning)
+
+#
+#   Communications Core Modules
+#
+
+from . import comm
+from . import task
+from . import singleton
+
+#
+#   Link Layer Modules
+#
+
+from . import pdu
+from . import vlan
+
+#
+#   Network Layer Modules
+#
+
+from . import npdu
+from . import netservice
+
+#
+#   Virtual Link Layer Modules
+#
+
+from . import bvll
+from . import bvllservice
+from . import bsll
+from . import bsllservice
+
+#
+#   Application Layer Modules
+#
+
+from . import primitivedata
+from . import constructeddata
+from . import basetypes
+
+from . import object
+
+from . import apdu
+
+from . import app
+from . import appservice
+
+#
+#   Analysis
+#
+
+from . import analysis
