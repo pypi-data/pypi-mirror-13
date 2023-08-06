40f795af6dd673765eafbfd03e05ef953d2b5194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:02 +0000

    Add package xust1119/1.3.0/xust1119-1.3.0.zip

---
+++ packages/xust1119/xust1119-1.3.0.zip/xust1119-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'xust1119',
+	version = '1.3.0',
+	py_modules = ["nester_lee"],
+	author = "xust1119",
+	author_email = 'xust1118@163.com',
+	url = 'www.xust.edu.cn',
+	description = "a test"
+	)
