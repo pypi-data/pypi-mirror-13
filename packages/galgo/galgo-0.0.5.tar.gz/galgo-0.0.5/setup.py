0ee77728b71b8a234b97ccaf40d5e71287ca60ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:24 +0000

    Add package galgo/0.0.5/galgo-0.0.5.tar.gz

---
+++ packages/galgo/galgo-0.0.5.tar.gz/galgo-0.0.5/setup.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+REQUIRES = ['argtools', 'pysam', 'numpy']
+try:
+    import ordereddict
+except ImportError:
+    REQUIRES.append('ordereddict')
+try:
+    import counter
+except ImportError:
+    REQUIRES.append('counter')
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+if os.path.exists(README):
+    long_description = open(README).read() + '\n\n'
+else:
+    long_description = 'A collection of algorithms and tools for genome analysis'
+VERSION = '0.0.5'
+
+setup(name='galgo',
+      version=VERSION,
+      install_requires=REQUIRES,
+      description='A collection of algorithms and tools for genome analysis',
+      long_description=long_description,
+      classifiers=['Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics'],
+      keywords='genome bio-informatics fasta',
+      author='Takahiro Mimori',
+      author_email='takahiro.mimori@gmail.com',
+      #py_modules=['galgo'],
+      package_data = {'': ['README.md']},
+      package_dir = {'galgo': 'galgo'},
+      #scripts = ['bin/galgo'],
+      entry_points={'console_scripts': ['galgo = galgo.__main__']},
+      url='https://github.com/m1m0r1/galgo',
+)
