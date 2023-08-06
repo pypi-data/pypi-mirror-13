e0fa626d3aee8bfc19f99f33c39408014f2c56a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:10 +0000

    Add package openunipay/0.1.9/openunipay-0.1.9-py3-none-any.whl

---
+++ packages/openunipay/openunipay-0.1.9-py3-none-any.whl/openunipay/ali_pay/security.py
@@ -0,0 +1,42 @@
+import os
+import rsa
+import base64
+from django.conf import settings
+from urllib.parse import quote_plus
+
+def sign(data):
+    privateKey = _load_private_key()
+    signBytes = rsa.sign(data.encode(), privateKey, 'SHA-1')
+    signStr = str(base64.b64encode(signBytes), 'utf-8')
+    return quote_plus(signStr)
+
+
+def verify(valueDict):
+    signStr = valueDict['sign']
+    del valueDict['sign']
+    if valueDict.has_key('sign_type'):
+        del valueDict['sign_type']
+    tempStr = _compose_sign_str(valueDict)
+    aliPayKey = _load_ali_pub_key()
+    result = rsa.verify(tempStr, signStr, aliPayKey)
+    return result 
+
+def _load_private_key():
+    keyfilePath = os.path.join(settings.ALIPAY['rsa_private_key_pem'])
+    with open(keyfilePath) as keyFile:
+        return rsa.PrivateKey.load_pkcs1(keyFile.read().encode('latin_1'))
+    
+    
+def _load_ali_pub_key():
+    keyfilePath = os.path.join(settings.ALIPAY['ali_public_key_pem'])
+    with open(keyfilePath) as keyFile:
+        return rsa.PublicKey.load_pkcs1_openssl_pem(keyFile.read().encode('latin_1'))
+
+def _compose_sign_str(valueDict):
+    temp = []
+    for key in valueDict:
+        if not valueDict[key]:
+            continue
+        temp.append('{}="{}"'.format(key, valueDict[key]))
+    tempStr = '&'.join(temp)
+    return tempStr
