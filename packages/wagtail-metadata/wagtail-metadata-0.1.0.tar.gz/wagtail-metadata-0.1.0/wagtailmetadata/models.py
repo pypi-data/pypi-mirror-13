c241e98a8cdc32213e584ed12312dd4662efeeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:11 +0000

    Add package wagtail-metadata/0.1.0/wagtail-metadata-0.1.0.tar.gz

---
+++ packages/wagtail-metadata/wagtail-metadata-0.1.0.tar.gz/wagtail-metadata-0.1.0/wagtailmetadata/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.wagtailcore.models import Page, Site
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+TWITTER_CARD_TYPES = [
+    ('summary', 'Summary card'),
+    ('summary_large_image', 'Summary card with large image'),
+]
+
+
+class SiteMetadataPreferences(models.Model):
+    site = models.OneToOneField(Site, unique=True, db_index=True, editable=False)
+    site_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL)
+    site_description = models.TextField()
+
+    # Twitter settings
+    card_type = models.CharField(max_length=128, choices=TWITTER_CARD_TYPES)
+
+    general_panels = [
+        MultiFieldPanel([
+            ImageChooserPanel('site_image'),
+            FieldPanel('site_description')
+        ], heading='General')
+    ]
+
+    twitter_panels = [
+        MultiFieldPanel([
+            FieldPanel('card_type')
+        ], heading='Twitter')
+    ]
+
+
+class MetadataPageMixin(models.Model):
+    search_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL)
+
+    promote_panels = [
+        MultiFieldPanel([
+            FieldPanel('slug'),
+            FieldPanel('seo_title'),
+            FieldPanel('show_in_menus'),
+            FieldPanel('search_description'),
+            ImageChooserPanel('search_image'),
+        ], ugettext_lazy('Common page configuration')),
+    ]
+
+    class Meta:
+        abstract = True
---
+++ packages/wagtail-metadata/wagtail-metadata-0.1.0.tar.gz/wagtail-metadata-0.1.0/wagtailmetadata/templatetags/wagtailmetadata_tags.py
@@ -0,0 +1,50 @@
+from django import template
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.html import format_html
+from wagtail.wagtailcore.models import Site
+from wagtailmetadata.models import SiteMetadataPreferences
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def meta_tags(context):
+    instance = context['self']
+    request = context['request']
+    page = context['self']
+    site = Site.find_for_request(request)
+    try:
+        global_settings = SiteMetadataPreferences.objects.get(site=site)
+    except SiteMetadataPreferences.DoesNotExist:
+        return format_html('<!-- Please define your global metadata settings -->')
+
+    instance.meta_image = instance.search_image or global_settings.site_image
+    instance.meta_description = instance.search_description or global_settings.site_description
+
+    instance.meta_image = instance.meta_image.get_rendition(filter='original')
+    instance.meta_image.full_url = request.build_absolute_uri(instance.meta_image.url)
+
+    page.absolute_url = request.build_absolute_uri(page.url)
+    return render_to_string('wagtailmetadata/parts/tags.html', {
+        'instance': instance,
+        'page': page,
+        'site_name': settings.WAGTAIL_SITE_NAME,
+        'global_settings': global_settings,
+    })
+
+
+@register.simple_tag()
+def tot(first, second):
+    """
+    This or that, here shortened to 'tot'
+    will display a variable if it exists,
+    and if it doesn't, will display the second
+    variable passed through, and finally, none.
+    """
+    if first:
+        return first
+    elif second:
+        return second
+    else:
+        return None
