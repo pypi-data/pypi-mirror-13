da9c1fbde47dc2150c8ca146657b5d959469f6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:44 +0000

    Add package xmldirector.plonecore/1.5.0/xmldirector.plonecore-1.5.0.zip

---
+++ packages/xmldirector.plonecore/xmldirector.plonecore-1.5.0.zip/xmldirector.plonecore-1.5.0/xmldirector/plonecore/service.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.plonecore
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+
+import fs.errors
+import zope.interface
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from xmldirector.plonecore.interfaces import IWebdavSettings
+from xmldirector.plonecore.interfaces import IWebdavHandle
+from xmldirector.plonecore.fswrapper import get_fs_wrapper
+
+
+class WebdavHandle(object):
+
+    zope.interface.implements(IWebdavHandle)
+
+    def get_handle(self):
+        """ Return WebDAV handle """
+
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IWebdavSettings)
+
+        root_url = settings.connector_url
+        username = settings.connector_username
+        password = settings.connector_password or ''
+
+        if settings.connector_dexterity_subpath:
+            url = '{}/{}'.format(root_url, settings.connector_dexterity_subpath)
+        else:
+            url = root_url
+        try:
+            return get_fs_wrapper(url, credentials=dict(username=username,
+                                                        password=password))
+        except fs.errors.ResourceNotFoundError:
+            root_handle = get_fs_wrapper(root_url, credentials=dict(username=username,
+                                                                    password=password))
+            root_handle.makedir(settings.connector_dexterity_subpath, True, True)
+            return get_fs_wrapper(url, credentials=dict(username=username,
+                                                        password=password))
+
+
+WebdavHandleUtility = WebdavHandle()
