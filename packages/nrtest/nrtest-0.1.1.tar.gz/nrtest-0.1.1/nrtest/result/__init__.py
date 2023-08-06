3d4c0f0211853a84f07fa554bcd166c6a6e2751b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:27 +0000

    Add package nrtest/0.1.1/nrtest-0.1.1.tar.gz

---
+++ packages/nrtest/nrtest-0.1.1.tar.gz/nrtest-0.1.1/nrtest/result/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .factory import create
+
+# classes in the following submodules will be registered to the factory
+from .default import *
+from .null import *
+from .topas import *
