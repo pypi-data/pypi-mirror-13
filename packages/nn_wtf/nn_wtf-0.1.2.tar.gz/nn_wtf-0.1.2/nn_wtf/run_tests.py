8e0ad3bf4f81c9cdd95ed969efa21b884ddb550c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:33 +0000

    Add package nn_wtf/0.1.2/nn_wtf-0.1.2.tar.gz

---
+++ packages/nn_wtf/nn_wtf-0.1.2.tar.gz/nn_wtf-0.1.2/nn_wtf/run_tests.py
@@ -0,0 +1,9 @@
+from nn_wtf.tests.neural_network_graph_test import NeuralNetworkGraphTest
+from nn_wtf.tests.images_labels_data_set_test import ImagesLabelsDataSetTest
+from nn_wtf.tests.input_data_test import InputDataTest
+
+import unittest
+
+__author__ = 'Lene Preuss <lene.preuss@gmx.net>'
+
+unittest.main()
