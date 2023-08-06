e031cfb6f94cce1e29c5fa0a6a49f12e3cd190ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:02 +0000

    Add package about/5.0.0/about-5.0.0.tar.gz

---
+++ packages/about/about-5.0.0.tar.gz/about-5.0.0/about/about.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+metadata = dict(
+  __name__        = "about",
+  __version__     = "5.0.0",
+  __license__     = "MIT License",  
+  __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>",
+  __url__         = "https://warehouse.python.org/project/about",
+  __summary__     = "Software Metadata for Humans",
+  __keywords__    = "Python / 2.7, OS independent, software development"
+)
+
+globals().update(metadata)
+
+__all__ = metadata.keys()
