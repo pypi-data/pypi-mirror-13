c8b23000f34a423def1721ad1ad490dd8e7a5a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:15 +0000

    Add package fate/0.92/fate-0.92.tar.gz

---
+++ packages/fate/fate-0.92.tar.gz/fate-0.92/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name == 'mbcs')
+    codecs.register(func)
+
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = '0.92'
+
+setup(
+    name='fate',
+    version=version,
+    install_requires=requirements,
+    author='Karan Sharma',
+    author_email='karansharma1295@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/mr-karan/fate/',
+    license='MIT',
+    description='Browse FontAwesome Icons in your terminal',
+    long_description=long_description,
+    scripts=['bin/fate'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Text Processing :: Fonts',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
