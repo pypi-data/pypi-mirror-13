ef947eb5e7ec4c7c05f0e8824bda63a934725438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:41 +0000

    Add package traclus_impl/0.7/traclus_impl-0.7.zip

---
+++ packages/traclus_impl/traclus_impl-0.7.zip/traclus_impl-0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='traclus_impl',
+      version='0.7',
+      description='First pass implementation of traclus algorithm',
+      url='http://github.com/apolcyn/traclus_impl.git',
+      author='AP',
+      author_email='alexpolcyn65@gmail.com',
+      license='MIT',
+      packages=['traclus_impl', 'traclus_impl.partitioning', \
+       'traclus_impl.traclus_dbscan'], 
+      install_requires=[
+          'polypaths_planar_override',
+      ],
+      zip_safe=False)
