24c6283078d115d4c24b510829b22bd08d23c83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:01 +0000

    Add package anidado/1.3.0/anidado-1.3.0.zip

---
+++ packages/anidado/anidado-1.3.0.zip/anidado-1.3.0/anidado.py
@@ -0,0 +1,18 @@
+﻿"""Este módulo es nester.py y provee una función llamada print_lol que permite imprimir en
+pantalla una lista que dentro de sus items contenga listas anidadas y dentro de esas listas
+anidadas contenga otras listas anidadas y así sucesivamente..."""
+
+def print_lol(the_list,level=-1):
+        """Esta función valida que cada ítem que contiene la lista no sea una lista y si es
+        una lista se llama a sí mismo para validar la siguiente lista en caso de lo
+        contrario imprime los ítems en pantalla, un segundo argunmento es opcional para dar
+        un espacio (tabulador) o varios a cada lista dependiedo de su nivel de anidación"""
+        
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,level+1)
+                else:
+                        for num in range(level):
+                                print("\t",end="")
+                        print(each_item)
+		
---
+++ packages/anidado/anidado-1.3.0.zip/anidado-1.3.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(
+	name		= 'anidado',
+	version		= '1.3.0',
+	py_modules	= ['anidado'],
+	author		= 'kyblin',
+	author_email	= 'kyblin@gamil.com',
+	url		= 'http://',
+	description	= 'Una simple forma de imprimir listas anidadas',
+     )
