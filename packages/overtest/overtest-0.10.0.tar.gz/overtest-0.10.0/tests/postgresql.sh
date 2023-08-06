b6a2380dca26c718432cfc46c0db4401147621ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:20 +0000

    Add package overtest/0.10.0/overtest-0.10.0.tar.gz

---
+++ packages/overtest/overtest-0.10.0.tar.gz/overtest-0.10.0/tests/postgresql.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+source bin/overtest postgresql
+
+testEnvironment ()
+{
+    assertEquals 9824 ${OVERTEST_POSTGRESQL_PORT}
+    assertEquals postgresql ${OVERTEST_DAEMON}
+    assertEquals "postgresql://localhost/template1?host=${OVERTEST_POSTGRESQL_DATA}&port=${OVERTEST_POSTGRESQL_PORT}" ${OVERTEST_POSTGRESQL_URL}
+    assertEquals ${OVERTEST_URL} ${OVERTEST_POSTGRESQL_URL}
+}
+
+
+testPSQL ()
+{
+    psql template1 -c "CREATE TABLE FOOBAR();"
+}
