34b82022e58c2e0f9cb58b73555bde55d6400823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:57 +0000

    Add package prosopopee/0.1/prosopopee-0.1.tar.gz

---
+++ packages/prosopopee/prosopopee-0.1.tar.gz/prosopopee-0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
+setup(name='prosopopee',
+      version='0.1',
+      description='excposure.co clone in a static web generating tool',
+      author='Laurent Peuch',
+      long_description=read_md('README.md'),
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/prosopopee',
+      install_requires=open("./requirements.txt", "r").read().split(),
+      packages=['prosopopee'],
+      py_modules=[],
+      license= 'GPLv3+',
+      scripts=[],
+      entry_points={
+        'console_scripts': ['prosopopee = prosopopee.prosopopee:main']
+      },
+      keywords='',
+      include_package_data=True,
+      package_data={
+            'prosopopee': ['static/css/*', 'static/js/*', 'static/img/*', 'templates/*.html', 'templates/sections/*'],
+        },
+     )
