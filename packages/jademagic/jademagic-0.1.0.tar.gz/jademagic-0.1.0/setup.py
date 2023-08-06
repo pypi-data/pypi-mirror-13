bbbe90af512595fec636fb8fbf877655658206f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package jademagic/0.1.0/jademagic-0.1.0.tar.gz

---
+++ packages/jademagic/jademagic-0.1.0.tar.gz/jademagic-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    """
+    Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top
+    level README file and 2) it's easier to type in the README file than to put
+    a raw string in below ...
+    """
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name="jademagic",
+    version="0.1.0",
+    author="Nicholas Bollweg",
+    author_email="nick.bollweg@gmail.com",
+    description="a Jade ne Pug magic for IPython notebooks",
+    license="BSD",
+    keywords="IPython Jade Pug Jupyter",
+    url="http://github.com/bollwyvl/jademagic",
+    py_modules=["jademagic"],
+    long_description=read("README.rst"),
+    classifiers=[
+        "Topic :: Utilities",
+        "Framework :: IPython",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Testing",
+    ],
+    install_requires=[
+        "pyjade",
+    ]
+)
