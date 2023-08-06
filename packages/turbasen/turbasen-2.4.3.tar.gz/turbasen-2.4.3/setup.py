d564f1f007d3cb5ed7b7e1f0b92d48de5522ad96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:54 +0000

    Add package turbasen/2.4.3/turbasen-2.4.3.tar.gz

---
+++ packages/turbasen/turbasen-2.4.3.tar.gz/turbasen-2.4.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+name = 'turbasen'
+VERSION = '2.4.3'
+
+setup(
+    name=name,
+    packages=[name],
+    version=VERSION,
+    description='Client for Nasjonal Turbase REST API',
+    long_description='See https://github.com/Turbasen/turbasen.py/blob/master/README.md',
+    author='Ali Kaafarani',
+    author_email='ali.kaafarani@turistforeningen.no',
+    url='https://github.com/Turbasen/turbasen.py',
+    download_url='https://github.com/Turbasen/turbasen.py/tarball/v%s' % (VERSION),
+    keywords=['turbasen', 'nasjonalturbase', 'turistforening', 'rest-api'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Norwegian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['requests>=2.9,<2.10'],
+    extras_require={
+        'dev': ['pytest>=2.8,<2.9'],
+    }
+)
