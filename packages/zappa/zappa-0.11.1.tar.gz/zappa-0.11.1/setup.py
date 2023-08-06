a5f55809e48cafa470da3f97e27a1ecb015a5581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:17 +0000

    Add package zappa/0.11.1/zappa-0.11.1.tar.gz

---
+++ packages/zappa/zappa-0.11.1.tar.gz/zappa-0.11.1/setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from setuptools import setup
+
+# Set external files
+try:
+    from pypandoc import convert
+    README = convert('README.md', 'rst')     
+except ImportError:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+setup(
+    name='zappa',
+    version='0.11.1',
+    packages=['zappa'],
+    install_requires=required,  
+    include_package_data=True,
+    license='MIT License',
+    description='Serverless WSGI With AWS Lambda + API Gateway',
+    long_description=README,
+    url='https://github.com/Miserlou/Zappa',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
