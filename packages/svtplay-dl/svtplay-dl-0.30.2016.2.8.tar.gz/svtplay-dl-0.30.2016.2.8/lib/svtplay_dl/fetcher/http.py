1fcfa1f38d3fcc06b626bf20d8dddb2d9a5e84bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:52 +0000

    Add package svtplay-dl/0.30.2016.2.8/svtplay-dl-0.30.2016.2.8.tar.gz

---
+++ packages/svtplay-dl/svtplay-dl-0.30.2016.2.8.tar.gz/svtplay-dl-0.30.2016.2.8/lib/svtplay_dl/fetcher/http.py
@@ -0,0 +1,39 @@
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+from __future__ import absolute_import
+import time
+
+from svtplay_dl.output import progress, output # FIXME use progressbar() instead
+from svtplay_dl.fetcher import VideoRetriever
+
+
+class HTTP(VideoRetriever):
+    def name(self):
+        return "http"
+
+    def download(self):
+        """ Get the stream from HTTP """
+        data = self.http.request("get", self.url, stream=True)
+        try:
+            total_size = data.headers['content-length']
+        except KeyError:
+            total_size = 0
+        total_size = int(total_size)
+        bytes_so_far = 0
+
+        file_d = output(self.options, "mp4")
+        if hasattr(file_d, "read") is False:
+            return
+
+        lastprogress = 0
+        for i in data.iter_content(8192):
+            bytes_so_far += len(i)
+            file_d.write(i)
+            if self.options.output != "-" and not self.options.silent:
+                now = time.time()
+                if lastprogress + 1 < now:
+                    lastprogress = now
+                    progress(bytes_so_far, total_size)
+
+        if self.options.output != "-":
+            file_d.close()
---
+++ packages/svtplay-dl/svtplay-dl-0.30.2016.2.8.tar.gz/svtplay-dl-0.30.2016.2.8/lib/svtplay_dl/service/picsearch.py
@@ -0,0 +1,61 @@
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+from __future__ import absolute_import
+import re
+import json
+import copy
+
+from svtplay_dl.service import Service, OpenGraphThumbMixin
+from svtplay_dl.fetcher.rtmp import RTMP
+from svtplay_dl.fetcher.hds import hdsparse
+from svtplay_dl.error import ServiceError
+
+
+class Picsearch(Service, OpenGraphThumbMixin):
+    supported_domains = ['dn.se', 'mobil.dn.se', 'di.se']
+
+    def get(self):
+        data = self.get_urldata()
+
+        if self.exclude(self.options):
+            yield ServiceError("Excluding video")
+            return
+
+        ajax_auth = re.search(r"picsearch_ajax_auth = '(\w+)'", data)
+        if not ajax_auth:
+            ajax_auth = re.search(r'screen9-ajax-auth="([^"]+)"', data)
+            if not ajax_auth:
+                yield ServiceError("Cant find token for video")
+                return
+        mediaid = re.search(r"mediaId = '([^']+)';", self.get_urldata())
+        if not mediaid:
+            mediaid = re.search(r'media-id="([^"]+)"', self.get_urldata())
+            if not mediaid:
+                mediaid = re.search(r'screen9-mid="([^"]+)"', self.get_urldata())
+                if not mediaid:
+                    yield ServiceError("Cant find media id")
+                    return
+        jsondata = self.http.request("get", "http://csp.picsearch.com/rest?jsonp=&eventParam=1&auth=%s&method=embed&mediaid=%s" % (ajax_auth.group(1), mediaid.group(1))).text
+        jsondata = json.loads(jsondata)
+        if "playerconfig" not in jsondata["media"]:
+            yield ServiceError(jsondata["error"])
+            return
+        if "live" in jsondata["media"]["playerconfig"]["clip"]:
+            self.options.live = jsondata["media"]["playerconfig"]["clip"]
+        playlist = jsondata["media"]["playerconfig"]["playlist"][1]
+        if "bitrates" in playlist:
+            files = playlist["bitrates"]
+            server = jsondata["media"]["playerconfig"]["plugins"]["bwcheck"]["netConnectionUrl"]
+
+            for i in files:
+                self.options.other = "-y '%s'" % i["url"]
+                yield RTMP(copy.copy(self.options), server, i["height"])
+        if "provider" in playlist:
+            if playlist["provider"] != "rtmp":
+                if "live" in playlist:
+                    self.options.live = playlist["live"]
+                if playlist["url"].endswith(".f4m"):
+                    streams = hdsparse(self.options, self.http.request("get", playlist["url"], params={"hdcore": "3.7.0"}), playlist["url"])
+                    if streams:
+                        for n in list(streams.keys()):
+                            yield streams[n]
