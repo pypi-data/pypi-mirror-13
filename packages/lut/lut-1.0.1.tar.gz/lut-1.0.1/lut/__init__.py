ec40a4981eb497351688b49d29cff338bc33ef0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:14 +0000

    Add package lut/1.0.1/lut-1.0.1.tar.gz

---
+++ packages/lut/lut-1.0.1.tar.gz/lut-1.0.1/lut/__init__.py
@@ -0,0 +1,1 @@
+from lut import load, Lut
---
+++ packages/lut/lut-1.0.1.tar.gz/lut-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='lut',
+    packages=['lut'],
+    version='1.0.1',
+    description='Multi-component interpolation lookup table.',
+    author='Remik Ziemlinski',
+    author_email='first.last@gmail.com',
+    license='GPLv3',
+    scripts=['scripts/lut'],
+    url='https://www.github.com/rsmz/lut',
+    download_url='https://github.com/rsmz/lut/archive/lut-1.0.1.tar.gz',
+)
