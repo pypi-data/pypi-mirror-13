62d513b25ca34af2695263e65d980b2c119831c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:08 +0000

    Add package anidado/1.0.0/anidado-1.0.0.zip

---
+++ packages/anidado/anidado-1.0.0.zip/anidado-1.0.0/anidado.py
@@ -0,0 +1,12 @@
+﻿"""Este módulo es nester.py y provee una función llamada print_lol que permite imprimir en pantalla una lista que dentro
+de sus items contenga listas anidadas y dentro de esas listas anidadas contenga otras listas anidadas y así sucesivamente..."""
+
+def print_lol(the_list):
+        """Esta función valida que cada ítem que contiene la lista no sea una lista y si es una lista se llama a sí mismo
+        para validarla siguiente lista en caso de lo contrario imprime los ítems en pantalla"""
+        
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item)
+                else: print(each_item)
+		
