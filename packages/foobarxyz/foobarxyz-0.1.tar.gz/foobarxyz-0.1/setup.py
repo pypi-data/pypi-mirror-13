4a70966e4c91e1d39c4ac7ae37fc604e5f3f486a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:44 +0000

    Add package foobarxyz/0.1/foobarxyz-0.1.tar.gz

---
+++ packages/foobarxyz/foobarxyz-0.1.tar.gz/foobarxyz-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='foobarxyz',
+    version='0.1',
+    description='Foo bar',
+    url='http://example.com/',
+    author='Changneng Chen',
+    author_email='changneng@example.com',
+    license='MIT',
+    packages=['foobarxyz'],
+    zip_safe=False
+)
+
