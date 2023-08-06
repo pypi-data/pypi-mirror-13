fefa16be3551400537d5c155d14effd651211554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:50 +0000

    Add package nesterfaund/1.2.0/nesterfaund-1.2.0.tar.gz

---
+++ packages/nesterfaund/nesterfaund-1.2.0.tar.gz/nesterfaund-1.2.0/nester.py
@@ -0,0 +1,16 @@
+"""
+nester module, provide a function named print_lol()
+"""
+def print_lol(the_list, level=0):
+    """
+    print_lol(the_list, level), the_list can be any python list
+    it also can be a nested list
+    """
+
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
