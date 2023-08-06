36eb6817e3ef2e4b2fa68af1a5d109fb8608eada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:59 +0000

    Add package gmusicapi-scripts/0.2.0/gmusicapi_scripts-0.2.0.win-amd64.zip

---
+++ packages/gmusicapi-scripts/gmusicapi_scripts-0.2.0.win-amd64.zip/Python35/Scripts/gmsync-script.py
@@ -0,0 +1,10 @@
+#!C:\Python35\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'gmusicapi-scripts==0.2.0','console_scripts','gmsync'
+__requires__ = 'gmusicapi-scripts==0.2.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('gmusicapi-scripts==0.2.0', 'console_scripts', 'gmsync')()
+    )
