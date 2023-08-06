ada60c9ae9c88f26486b3c24c1d8e5fa2667fe3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:22 +0000

    Add package nexter0805/1.3.0/nexter0805-1.3.0.zip

---
+++ packages/nexter0805/nexter0805-1.3.0.zip/nexter0805-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nexter0805',
+    version = '1.3.0',
+    py_modules = ['nexter0805'],
+    author = 'hgtkpython',
+    author_email = '2868989685@qq.com',
+    url = 'http://www.hp.com',
+    description = 'a simple printer of nested lest',
+    )
