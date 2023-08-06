5ca16e57f8dda1b3f618422bb934a37cc7b415e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:22 +0000

    Add package robberslang/0.1/robberslang-0.1.tar.gz

---
+++ packages/robberslang/robberslang-0.1.tar.gz/robberslang-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='robberslang',
+	version='0.1',
+	description='String to robbers lang',
+	url='http://bit.ly/1WJ6b77',
+	author='Ivar Matstoms',
+	author_email='ivar.matstoms@gmail.com',
+	license='GPL',
+	scripts=['bin/robberslang'],
+	zip_safe=False)
