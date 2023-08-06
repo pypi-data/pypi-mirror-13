e1fc8e4af6e5466387f47bcb09683b4eedd15abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:50 +0000

    Add package bug/0.1/bug-0.1.tar.gz

---
+++ packages/bug/bug-0.1.tar.gz/bug-0.1/bug.py
@@ -0,0 +1,1 @@
+print('Always with you :)')
---
+++ packages/bug/bug-0.1.tar.gz/bug-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'bug',
+    py_modules = ['bug'],
+    version = '0.1',
+    description = 'Always with you :)',
+    author = 'kuga',
+    author_email = 'kugacola@gmail.com',
+    url = 'http://luffy.me',
+    keywords = ['bug'],
+)
