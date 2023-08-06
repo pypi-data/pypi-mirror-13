dde29845207154184a20e4b6c5265f422be72a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:21 +0000

    Add package plone.formwidget.namedfile/1.0.14/plone.formwidget.namedfile-1.0.14.tar.gz

---
+++ packages/plone.formwidget.namedfile/plone.formwidget.namedfile-1.0.14.tar.gz/plone.formwidget.namedfile-1.0.14/plone/formwidget/namedfile/tests.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from z3c.form import testing
+import doctest
+import unittest
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            'widget.rst',
+            setUp=testing.setUp,
+            tearDown=testing.tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            encoding='utf-8'
+        ),
+    ))
