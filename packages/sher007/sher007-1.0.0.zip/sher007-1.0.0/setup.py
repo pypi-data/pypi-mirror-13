8789177d68fc5818f033addd4dfd118fa9972750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:27 +0000

    Add package sher007/1.0.0/sher007-1.0.0.zip

---
+++ packages/sher007/sher007-1.0.0.zip/sher007-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'sher007',
+		version = '1.0.0',
+		py_modules = ['nester'],
+		author = 'sherlock',
+		author_email = '1837129387@qq.com',
+	    url = 'http://www.headfirstlabs.com',
+	    description = 'A simple printer of nested lists',
+	)
