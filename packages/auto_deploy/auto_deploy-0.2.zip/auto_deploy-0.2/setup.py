135c7df8a7672b261722a458fe6a4fd3b17893f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:50 +0000

    Add package auto_deploy/0.2/auto_deploy-0.2.zip

---
+++ packages/auto_deploy/auto_deploy-0.2.zip/auto_deploy-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'auto_deploy',
+  packages = ['auto_deploy'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Appengine Autodeploy Script',
+  author = 'Kent Astudillo',
+  author_email = 'kent@sym.com',
+  url = 'https://github.com/killuabone/appengine-deployer', # use the URL to the github repo
+  download_url = 'https://github.com/killuabone/appengine-deployer/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
