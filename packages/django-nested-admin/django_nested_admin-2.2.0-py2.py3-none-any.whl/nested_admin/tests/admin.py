e14a7dcf4b4efb7d7ae5e112275a2205c55f4740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:56 +0000

    Add package django-nested-admin/2.2.0/django_nested_admin-2.2.0-py2.py3-none-any.whl

---
+++ packages/django-nested-admin/django_nested_admin-2.2.0-py2.py3-none-any.whl/nested_admin/tests/admin.py
@@ -0,0 +1,83 @@
+from django.contrib import admin
+from nested_admin import NestedStackedInline, NestedTabularInline, NestedAdmin
+
+from .models import (
+    StackedGroup, StackedSection, StackedItem,
+    TabularGroup, TabularSection, TabularItem,
+    TopLevel, LevelOne, LevelTwo, LevelThree)
+
+
+class StackedItemInline(NestedStackedInline):
+
+    model = StackedItem
+    extra = 0
+    sortable_field_name = "position"
+    inline_classes = ("collapse open",)
+
+
+class StackedSectionInline(NestedStackedInline):
+
+    model = StackedSection
+    extra = 0
+    sortable_field_name = "position"
+
+    inlines = [StackedItemInline]
+    inline_classes = ("collapse open",)
+
+
+class StackedGroupAdmin(NestedAdmin):
+
+    inlines = [StackedSectionInline]
+
+
+class TabularItemInline(NestedTabularInline):
+
+    model = TabularItem
+    extra = 0
+    sortable_field_name = "position"
+
+
+class TabularSectionInline(NestedTabularInline):
+
+    model = TabularSection
+    extra = 0
+    sortable_field_name = "position"
+    inlines = [TabularItemInline]
+
+
+class TabularGroupAdmin(NestedAdmin):
+
+    inlines = [TabularSectionInline]
+
+
+class LevelThreeInline(NestedStackedInline):
+
+    model = LevelThree
+    extra = 1
+    inline_classes = ("collapse open grp-collapse grp-open",)
+
+
+class LevelTwoInline(NestedStackedInline):
+
+    model = LevelTwo
+    extra = 1
+    inlines = [LevelThreeInline]
+    inline_classes = ("collapse open grp-collapse grp-open",)
+
+
+class LevelOneInline(NestedStackedInline):
+
+    model = LevelOne
+    extra = 1
+    inlines = [LevelTwoInline]
+    inline_classes = ("collapse open grp-collapse grp-open",)
+
+
+class TopLevelAdmin(NestedAdmin):
+
+    inlines = [LevelOneInline]
+
+
+admin.site.register(StackedGroup, StackedGroupAdmin)
+admin.site.register(TabularGroup, TabularGroupAdmin)
+admin.site.register(TopLevel, TopLevelAdmin)
