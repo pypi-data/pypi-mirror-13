0c63b249fd244e4b1405072b93f936b26076a4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:04 +0000

    Add package jasmin/0.8.post13/jasmin-0.8.post13.tar.gz

---
+++ packages/jasmin/jasmin-0.8.post13.tar.gz/jasmin-0.8.post13/misc/doc/sources/apis/ja-http/example_send_gsm0338.php
@@ -0,0 +1,13 @@
+<?php
+// Sending simple message using PHP
+// http://jasminsms.com
+
+$baseurl = 'http://127.0.0.1:1401/send'
+
+$params = '?username=nathalie'
+$params.= '&password=secret'
+$params.= '&to='.urlencode('+24206155423')
+$params.= '&content='.urlencode('Hello world !')
+
+$response = file_get_contents($baseurl.$params);
+?>
