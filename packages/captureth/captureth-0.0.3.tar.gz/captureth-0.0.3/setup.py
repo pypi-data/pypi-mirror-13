64e506d657aa8bb4a91a729c43482a858de1e98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:51 +0000

    Add package captureth/0.0.3/captureth-0.0.3.tar.gz

---
+++ packages/captureth/captureth-0.0.3.tar.gz/captureth-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='captureth',
+      version='0.0.3',
+      url='http://github.com/Consensys/pycaptureth/',
+      description='Capture msgs from pyethapp',
+      license='MIT',
+      packages=['captureth'])
