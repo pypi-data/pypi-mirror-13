dbebb3d7ac8d36082978f2138fe2b41312bab5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package staffjoy/0.2/staffjoy-0.2.tar.gz

---
+++ packages/staffjoy/staffjoy-0.2.tar.gz/staffjoy-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "staffjoy",
+    packages = ["staffjoy"],
+    version = "0.2",
+    description = "Staffjoy API Wrapper in Python",
+    author = "Philip Thomas",
+    author_email = "help@staffjoy.com",
+    url = "https://github.com/staffjoy/client_python",
+    download_url = "https://github.com/StaffJoy/client_python/archive/0.2.tar.gz",
+    keywords = ["staffjoy-api", "staffjoy", "staff joy"],
+)
