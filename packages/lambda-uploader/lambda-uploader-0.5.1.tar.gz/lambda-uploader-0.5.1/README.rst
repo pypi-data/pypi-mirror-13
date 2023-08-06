cd283101d58e491fcf484f85b68168a97e689a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:29 +0000

    Add package lambda-uploader/0.5.1/lambda-uploader-0.5.1.tar.gz

---
+++ packages/lambda-uploader/lambda-uploader-0.5.1.tar.gz/lambda-uploader-0.5.1/README.rst
@@ -0,0 +1,105 @@
+lambda-uploader
+===============
+
+Provides a quick command line utility for packaging and publishing
+Python AWS Lambda functions. This is a work in progress and pull
+requests are always welcome.
+
+Installation
+~~~~~~~~~~~~
+
+The latest release of lambda-uploader can be installed via pip:
+
+::
+
+    pip install lambda-uploader
+
+An alternative install method would be manually installing it leveraging
+``setup.py``:
+
+::
+
+    git clone https://github.com/rackerlabs/lambda-uploader
+    cd lambda-uploader
+    python setup.py install
+
+Configuration File
+~~~~~~~~~~~~~~~~~~
+
+The lambda uploader expects a directory with, at a minimum, your lambda
+function and a lambda.json file. It is not necessary to set requirements
+in your config file since the lambda uploader will also check for and
+use a requirements.txt file.
+
+Example lambda.json file:
+
+.. code:: json
+
+    {
+      "name": "myFunction",
+      "description": "It does things",
+      "region": "us-east-1",
+      "handler": "function.lambda_handler",
+      "role": "arn:aws:iam::00000000000:role/lambda_basic_execution",
+      "requirements": ["pygithub"],
+      "ignore": [
+        "circle.yml",
+        ".git",
+        "/*.pyc"
+      ],
+      "timeout": 30,
+      "memory": 512
+    }
+
+Command Line Usage
+~~~~~~~~~~~~~~~~~~
+
+To package and upload simply run the command from within your lambda
+directory or with the directory as an option.
+
+.. code:: shell
+
+    lambda-uploader ./myfunc
+
+To specify an alternative profile that has been defined in
+``~/.aws/credentials`` use the ``--profile`` parameter.
+
+.. code:: shell
+
+    lambda-uploader --profile=alternative-profile
+
+To specify an alternative, prexisting virtualenv use the
+``--virtualenv`` parameter.
+
+.. code:: shell
+
+    lambda-uploader --virtualenv=~/.virtualenv/my_custom_virtualenv
+
+To omit using a virtualenv use the ``--no-virtualenv`` parameter.
+
+.. code:: shell
+
+    lambda-uploader --no-virtualenv
+
+If you would prefer to upload another way you can tell the uploader to
+ignore the upload. This will create a package and leave it in the
+project directory.
+
+.. code:: shell
+
+    lambda-uploader --no-upload ./myfunc
+
+To publish a version without an alias you would pass the the publish
+flag.
+
+.. code:: shell
+
+    lambda-uploader -p ./myfunc
+
+If you would like to alias your upload you can pass the alias with the
+alias flag. The function description will be used when an
+alias-description is not provided.
+
+.. code:: shell
+
+    lambda-uploader --alias myAlias --alias-description 'My alias description' ./myfunc
---
+++ packages/lambda-uploader/lambda-uploader-0.5.1.tar.gz/lambda-uploader-0.5.1/test/test_package.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import pytest
+
+from shutil import rmtree
+from os import path
+from lambda_uploader import package
+
+TESTING_TEMP_DIR = '.testing_temp'
+
+
+def setup_module(module):
+    print('calling setup')
+    os.mkdir(TESTING_TEMP_DIR)
+
+
+def teardown_module(module):
+    print('calling teardown')
+    rmtree(TESTING_TEMP_DIR)
+
+
+def test_package_zip_location():
+    pkg = package.Package(TESTING_TEMP_DIR)
+    assert pkg.zip_file == '.testing_temp/lambda_function.zip'
+
+
+def test_package_clean_workspace():
+    temp_workspace = path.join(TESTING_TEMP_DIR,
+                               package.TEMP_WORKSPACE_NAME)
+    os.mkdir(temp_workspace)
+
+    pkg = package.Package(TESTING_TEMP_DIR)
+    pkg.clean_workspace()
+    assert path.isdir(temp_workspace) == False
+
+
+def test_prepare_workspace():
+    temp_workspace = path.join(TESTING_TEMP_DIR,
+                               package.TEMP_WORKSPACE_NAME)
+
+    reqs = ['pytest']
+    pkg = package.Package(TESTING_TEMP_DIR, requirements=reqs)
+    pkg.prepare_workspace()
+    pkg.prepare_virtualenv()
+    assert path.isdir(temp_workspace)
+    assert path.isdir(path.join(temp_workspace, 'venv'))
+    if sys.platform == 'win32' or sys.platform == 'cygwin':
+        assert path.isfile(path.join(temp_workspace, "venv\\Scripts\\pip.exe"))
+    else:
+        assert path.isfile(path.join(temp_workspace, 'venv/bin/pip'))
+
+
+def test_install_requirements():
+    reqs = ['pytest']
+    temp_workspace = path.join(TESTING_TEMP_DIR,
+                               package.TEMP_WORKSPACE_NAME)
+
+    pkg = package.Package(TESTING_TEMP_DIR, requirements=reqs)
+    pkg.prepare_virtualenv()
+
+    site_packages = path.join(temp_workspace,
+                              'venv/lib/python2.7/site-packages')
+    if sys.platform == 'win32' or sys.platform == 'cygwin':
+        site_packages = path.join(temp_workspace, "venv\\lib\\site-packages")
+
+    assert path.isdir(path.join(site_packages, '_pytest'))
+
+
+def test_default_virtualenv():
+    temp_workspace = path.join(TESTING_TEMP_DIR,
+                               package.TEMP_WORKSPACE_NAME)
+    reqs = ['pytest']
+    pkg = package.Package(TESTING_TEMP_DIR, requirements=reqs)
+    pkg.prepare_virtualenv()
+    # ensure we picked a real venv path if using default behavior
+    assert pkg._pkg_venv == ("%s/venv" % temp_workspace)
+
+
+def test_existing_virtualenv():
+    venv_dir = "virtualenv_test"
+    temp_virtualenv = path.join(TESTING_TEMP_DIR, venv_dir)
+    os.mkdir(temp_virtualenv)
+
+    pkg = package.Package(TESTING_TEMP_DIR, temp_virtualenv)
+    pkg.prepare_virtualenv()
+
+    assert pkg._pkg_venv == temp_virtualenv
+
+
+def test_bad_existing_virtualenv():
+    pkg = package.Package(TESTING_TEMP_DIR, 'abc')
+    with pytest.raises(Exception):
+        pkg.prepare_virtualenv()
+
+
+def test_omit_virtualenv():
+    pkg = package.Package(TESTING_TEMP_DIR, False)
+    pkg.prepare_virtualenv()
+    assert pkg._pkg_venv is False
+
+    with pytest.raises(Exception):
+        pkg.build_new_virtualenv()
+
+
+def test_package():
+    pkg = package.Package(TESTING_TEMP_DIR)
+    pkg.package()
+    assert path.isfile(path.join(TESTING_TEMP_DIR, 'lambda_function.zip'))
+
+
+def test_package_name():
+    pkg = package.Package(TESTING_TEMP_DIR, zipfile_name='test.zip')
+    pkg.package()
+    assert path.isfile(path.join(TESTING_TEMP_DIR, 'test.zip'))
