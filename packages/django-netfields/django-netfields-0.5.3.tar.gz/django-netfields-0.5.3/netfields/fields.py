b4e5df2290dd7a6b32b79e274526f01a43e4c6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:50 +0000

    Add package django-netfields/0.5.3/django-netfields-0.5.3.tar.gz

---
+++ packages/django-netfields/django-netfields-0.5.3.tar.gz/django-netfields-0.5.3/netfields/fields.py
@@ -0,0 +1,205 @@
+from ipaddress import ip_interface, ip_network
+from netaddr import EUI
+from netaddr.core import AddrFormatError
+
+from django import VERSION
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.six import with_metaclass
+
+from netfields.managers import NET_OPERATORS, NET_TEXT_OPERATORS
+from netfields.forms import InetAddressFormField, CidrAddressFormField, MACAddressFormField
+from netfields.mac import mac_unix_common
+
+
+class _NetAddressField(models.Field):
+    empty_strings_allowed = False
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = self.max_length
+        super(_NetAddressField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            return value
+
+        try:
+            value = unicode(value)
+        except NameError:
+            pass
+
+        try:
+            return self.python_type()(value)
+        except (AddrFormatError, ValueError) as e:
+            raise ValidationError(e)
+
+    def get_prep_lookup(self, lookup_type, value):
+        if not value:
+            return None
+
+        if (lookup_type in NET_OPERATORS and
+                NET_OPERATORS[lookup_type] not in NET_TEXT_OPERATORS):
+            if (lookup_type.startswith('net_contained') or
+                    lookup_type.endswith('prefixlen')) and value is not None:
+                # Argument will be CIDR
+                return str(value)
+            return self.get_prep_value(value)
+
+        return super(_NetAddressField, self).get_prep_lookup(
+            lookup_type, value)
+
+    def get_prep_value(self, value):
+        if not value:
+            return None
+
+        return str(self.to_python(value))
+
+    def get_db_prep_lookup(self, lookup_type, value, connection,
+            prepared=False):
+        if not value:
+            return []
+
+        if (lookup_type in NET_OPERATORS and
+                NET_OPERATORS[lookup_type] not in NET_TEXT_OPERATORS):
+            return [value] if prepared else [self.get_prep_value(value)]
+
+        return super(_NetAddressField, self).get_db_prep_lookup(
+            lookup_type, value, connection=connection, prepared=prepared)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': self.form_class()}
+        defaults.update(kwargs)
+        return super(_NetAddressField, self).formfield(**defaults)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(_NetAddressField, self).deconstruct()
+        if self.max_length is not None:
+            kwargs['max_length'] = self.max_length
+        return name, path, args, kwargs
+
+if VERSION < (1, 8):
+    # SubFieldBase has been deprecated in Django 1.8
+    # see https://docs.djangoproject.com/en/1.9/releases/1.8/#subfieldbase
+    class InetAddressField(with_metaclass(models.SubfieldBase, _NetAddressField)):
+        description = "PostgreSQL INET field"
+        max_length = 39
+
+        def __init__(self, *args, **kwargs):
+            self.store_prefix_length = kwargs.pop('store_prefix_length', True)
+            super(InetAddressField, self).__init__(*args, **kwargs)
+
+        def db_type(self, connection):
+            return 'inet'
+
+        def to_python(self, value):
+            if not value:
+                return value
+
+            try:
+                value = unicode(value)
+            except NameError:
+                pass
+
+            try:
+                if self.store_prefix_length:
+                    return ip_interface(value)
+                else:
+                    return ip_interface(value).ip
+            except ValueError as e:
+                raise ValidationError(e)
+
+        def form_class(self):
+            return InetAddressFormField
+else:
+    class InetAddressField(_NetAddressField):
+        description = "PostgreSQL INET field"
+        max_length = 39
+
+        def __init__(self, *args, **kwargs):
+            self.store_prefix_length = kwargs.pop('store_prefix_length', True)
+            super(InetAddressField, self).__init__(*args, **kwargs)
+
+        def from_db_value(self, value, expression, connection, context):
+            return self.to_python(value)
+
+        def db_type(self, connection):
+            return 'inet'
+
+        def to_python(self, value):
+            if not value:
+                return value
+
+            try:
+                value = unicode(value)
+            except NameError:
+                pass
+
+            try:
+                if self.store_prefix_length:
+                    return ip_interface(value)
+                else:
+                    return ip_interface(value).ip
+            except ValueError as e:
+                raise ValidationError(e)
+
+        def form_class(self):
+            return InetAddressFormField
+
+if VERSION < (1, 8):
+    # SubFieldBase has been deprecated in Django 1.8
+    # see https://docs.djangoproject.com/en/1.9/releases/1.8/#subfieldbase
+    class CidrAddressField(with_metaclass(models.SubfieldBase, _NetAddressField)):
+        description = "PostgreSQL CIDR field"
+        max_length = 43
+
+        def db_type(self, connection):
+            return 'cidr'
+
+        def python_type(self):
+            return ip_network
+
+        def form_class(self):
+            return CidrAddressFormField
+else:
+    class CidrAddressField(_NetAddressField):
+        description = "PostgreSQL CIDR field"
+        max_length = 43
+
+        def from_db_value(self, value, expression, connection, context):
+            return self.to_python(value)
+
+        def db_type(self, connection):
+            return 'cidr'
+
+        def python_type(self):
+            return ip_network
+
+        def form_class(self):
+            return CidrAddressFormField
+
+
+class MACAddressField(models.Field):
+    description = "PostgreSQL MACADDR field"
+
+    def db_type(self, connection):
+        return 'macaddr'
+
+    def to_python(self, value):
+        if not value:
+            return value
+
+        try:
+            return EUI(value, dialect=mac_unix_common)
+        except AddrFormatError as e:
+            raise ValidationError(e)
+
+    def get_prep_value(self, value):
+        if not value:
+            return None
+
+        return str(self.to_python(value))
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': MACAddressFormField}
+        defaults.update(kwargs)
+        return super(MACAddressField, self).formfield(**defaults)
---
+++ packages/django-netfields/django-netfields-0.5.3.tar.gz/django-netfields-0.5.3/netfields/lookups.py
@@ -0,0 +1,137 @@
+from django.core.exceptions import FieldError
+from django.db.models import Lookup, Transform, IntegerField
+from django.db.models.lookups import EndsWith, IEndsWith, StartsWith, IStartsWith, Regex, IRegex
+from netfields.fields import InetAddressField, CidrAddressField
+
+
+class InvalidLookup(Lookup):
+    """
+    Emulate Django 1.9 error for unsupported lookups
+    """
+    def as_sql(self, qn, connection):
+        raise FieldError("Unsupported lookup '%s'" % self.lookup_name)
+
+
+class InvalidSearchLookup(Lookup):
+    """
+    Emulate Django 1.9 error for unsupported search lookup
+    """
+    lookup_name = 'search'
+
+    def as_sql(self, qn, connection):
+        raise NotImplementedError("Full-text search is not implemented for this database backend")
+
+
+class NetFieldDecoratorMixin(object):
+    def process_lhs(self, qn, connection, lhs=None):
+        lhs = lhs or self.lhs
+        lhs_string, lhs_params = qn.compile(lhs)
+        if isinstance(lhs.source if hasattr(lhs, 'source') else lhs.output_field, InetAddressField):
+            lhs_string = 'HOST(%s)' % lhs_string
+        elif isinstance(lhs.source if hasattr(lhs, 'source') else lhs.output_field, CidrAddressField):
+            lhs_string = 'TEXT(%s)' % lhs_string
+        return lhs_string, lhs_params
+
+
+class EndsWith(NetFieldDecoratorMixin, EndsWith):
+    pass
+
+
+class IEndsWith(NetFieldDecoratorMixin, IEndsWith):
+    pass
+
+
+class StartsWith(NetFieldDecoratorMixin, StartsWith):
+    pass
+
+
+class IStartsWith(NetFieldDecoratorMixin, IStartsWith):
+    pass
+
+
+class Regex(NetFieldDecoratorMixin, Regex):
+    pass
+
+
+class IRegex(NetFieldDecoratorMixin, IRegex):
+    pass
+
+
+class NetContains(Lookup):
+    lookup_name = 'net_contains'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s >> %s' % (lhs, rhs), params
+
+
+class NetContained(Lookup):
+    lookup_name = 'net_contained'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s << %s' % (lhs, rhs), params
+
+
+class NetContainsOrEquals(Lookup):
+    lookup_name = 'net_contains_or_equals'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s >>= %s' % (lhs, rhs), params
+
+
+class NetContainedOrEqual(Lookup):
+    lookup_name = 'net_contained_or_equal'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s <<= %s' % (lhs, rhs), params
+
+
+class Family(Transform):
+    lookup_name = 'family'
+
+    def as_sql(self, compiler, connection):
+        lhs, params = compiler.compile(self.lhs)
+        return "family(%s)" % lhs, params
+
+    @property
+    def output_field(self):
+        return IntegerField()
+
+
+class _PrefixlenMixin(object):
+    def process_lhs(self, qn, connection, lhs=None):
+        lhs = lhs or self.lhs
+        lhs_string, lhs_params = qn.compile(lhs)
+        lhs_string = 'MASKLEN(%s)' % lhs_string
+        return lhs_string, lhs_params
+
+
+class MaxPrefixlen(_PrefixlenMixin, Lookup):
+    lookup_name = 'max_prefixlen'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s <= %s' % (lhs, rhs), params
+
+
+class MinPrefixlen(_PrefixlenMixin, Lookup):
+    lookup_name = 'min_prefixlen'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s >= %s' % (lhs, rhs), params
