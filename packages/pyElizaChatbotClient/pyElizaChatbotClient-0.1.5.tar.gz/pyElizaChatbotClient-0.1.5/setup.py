f0a30b8e2c8443b46c4620be76c39fc5a654c208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package pyElizaChatbotClient/0.1.5/pyElizaChatbotClient-0.1.5.tar.gz

---
+++ packages/pyElizaChatbotClient/pyElizaChatbotClient-0.1.5.tar.gz/pyElizaChatbotClient-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyElizaChatbotClient',
+    version='0.1.5',
+    author="Luis Fernando D'Haro",
+    author_email='luisdhe@i2r.a-star.edu.sg',
+    license='LICENSE.txt',
+    packages=find_packages(exclude=['contrib', 'docs']),
+    include_package_data = True,
+    package_data={'test': 'test/pyElizaChatbotClient.py'},
+    url='http://workshop.colips.org/re-wochat/',
+    description='Basic stand-alone/websocket client for using in the Re-WoChat Shared task.',
+    long_description=open('README.txt').read(),
+    keywords='chatbots eliza re-wochat shared-task',
+    install_requires=[
+        "nltk >= 3.1",
+        "petl >= 1.1.0",
+        "future >= 0.15.2",
+    ],
+)
+
