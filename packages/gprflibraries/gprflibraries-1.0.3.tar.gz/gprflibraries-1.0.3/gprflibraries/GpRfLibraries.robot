f0b1a70f0b2f65c52f9c78d8e0421ed98b18c213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:55 +0000

    Add package gprflibraries/1.0.3/gprflibraries-1.0.3.tar.gz

---
+++ packages/gprflibraries/gprflibraries-1.0.3.tar.gz/gprflibraries-1.0.3/gprflibraries/GpRfLibraries.robot
@@ -0,0 +1,8 @@
+*** Settings ***
+Library  Selenium2Library
+Resource  resources/GpCommonKeywords.robot
+Library  libraries/gp_random.py
+Library  libraries/gp_csv.py
+Library  libraries/gp_common.py
+
+Documentation  Main file that is to be imported into your project. This file includes all libraries and resources.
---
+++ packages/gprflibraries/gprflibraries-1.0.3.tar.gz/gprflibraries-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from gprflibraries.version import ver
+
+setup(
+  name = 'gprflibraries',
+  packages = ['gprflibraries', 'gprflibraries.libraries', 'gprflibraries.resources'],
+  include_package_data = True,
+  package_data = {'': ['*.robot']},
+  version = ver,
+  keywords = [],
+  classifiers = [],
+)
