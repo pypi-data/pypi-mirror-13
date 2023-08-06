47421bfab082ac70cf6712aa8c2b9691875e634f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:20 +0000

    Add package cheesepi/1.2/cheesepi-1.2.tar.gz

---
+++ packages/cheesepi/cheesepi-1.2.tar.gz/cheesepi-1.2/cheesepi/tasks/__init__.py
@@ -0,0 +1,18 @@
+
+from Task import Task
+from Dummy import Dummy
+from Beacon import Beacon
+from Upload import Upload
+from Upgradecode import Upgradecode
+from Updatetasks import Updatetasks
+from Status import Status
+from Ping import Ping
+from Httping import Httping
+from Traceroute import Traceroute
+from Dash import Dash
+from DNS import DNS
+from Throughput import Throughput
+from iPerf import iPerf
+from Wifi import Wifi
+
+
