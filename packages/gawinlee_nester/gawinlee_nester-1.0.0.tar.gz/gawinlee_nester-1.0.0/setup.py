dfae1e3d6b2c5de37dbe4edbd912c07e2d55e32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package gawinlee_nester/1.0.0/gawinlee_nester-1.0.0.tar.gz

---
+++ packages/gawinlee_nester/gawinlee_nester-1.0.0.tar.gz/gawinlee_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name         = 'gawinlee_nester',
+    version      = '1.0.0',
+    py_module    = ['gawinlee_nester'],
+    author       = 'gawinlee',
+    author_email = 'gawinlee@gmail.com',
+    url          = '',
+    description  = 'A simple printer of nested lists',
+      )
