ed1443f2dffcc5349554557ed6347f5e6780c138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:02 +0000

    Add package Pyckson/0.4.1/Pyckson-0.4.1.tar.gz

---
+++ packages/Pyckson/Pyckson-0.4.1.tar.gz/Pyckson-0.4.1/pyckson/helpers.py
@@ -0,0 +1,34 @@
+import re
+
+from pyckson.enum import EnumParser, DefaultEnumParser
+from pyckson.model import PycksonModel
+from pyckson.const import PYCKSON_ATTR, PYCKSON_MODEL, PYCKSON_ENUM_PARSER, BASIC_TYPES
+
+
+def is_pyckson(obj_type):
+    return getattr(obj_type, PYCKSON_ATTR, False)
+
+
+def get_model(obj_or_class) -> PycksonModel:
+    if type(obj_or_class) is not type:
+        return get_model(obj_or_class.__class__)
+    if not is_pyckson(obj_or_class):
+        raise ValueError('{} has no pyckson info'.format(obj_or_class))
+    return getattr(obj_or_class, PYCKSON_MODEL)
+
+
+def camel_case_name(python_name):
+    return re.sub('_([a-z])', lambda match: match.group(1).upper(), python_name)
+
+
+def get_enum_parser(obj_or_class) -> EnumParser:
+    if not hasattr(obj_or_class, PYCKSON_ENUM_PARSER):
+        return DefaultEnumParser(obj_or_class)
+    return getattr(obj_or_class, PYCKSON_ENUM_PARSER)
+
+
+def is_base_type(obj):
+    for btype in BASIC_TYPES:
+        if isinstance(obj, btype):
+            return True
+    return False
