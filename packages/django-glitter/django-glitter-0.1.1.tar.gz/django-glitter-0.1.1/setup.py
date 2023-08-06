bb010be4141244bd281270fbd459a522cd2b5f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:16 +0000

    Add package django-glitter/0.1.1/django-glitter-0.1.1.tar.gz

---
+++ packages/django-glitter/django-glitter-0.1.1.tar.gz/django-glitter-0.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    'Django>=1.8,<1.9',
+    'django-mptt>=0.7,<0.8',
+    'django-mptt-admin>=0.3,<0.4',
+    'sorl-thumbnail>=12.2',
+]
+
+
+setup(
+    name='django-glitter',
+    version='0.1.1',
+    description='Glitter for Django',
+    long_description=open('README.rst').read(),
+    url='https://github.com/blancltd/django-glitter',
+    maintainer='Blanc LTD',
+    maintainer_email='studio@blanc.ltd.uk',
+    platforms=['any'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='Proprietary',
+    install_requires=install_requires,
+)
