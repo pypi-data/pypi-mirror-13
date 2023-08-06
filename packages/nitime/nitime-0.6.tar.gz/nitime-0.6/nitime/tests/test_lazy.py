8ef45aaa7d317d72987b3f4c1078bce18ce16c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package nitime/0.6/nitime-0.6.tar.gz

---
+++ packages/nitime/nitime-0.6.tar.gz/nitime-0.6/nitime/tests/test_lazy.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import nitime.lazyimports as l
+import numpy.testing as npt
+import numpy.testing.decorators as dec
+
+# The next test requires nitime.lazyimports.disable_lazy_imports to have been
+# set to false, otherwise the lazy import machinery is disabled and all imports
+# happen at l.LazyImport calls which become equivalent to regular import
+# statements
+@dec.skipif(l.disable_lazy_imports)
+def test_lazy():
+    mlab = l.LazyImport('matplotlib.mlab')
+    # repr for mlab should be <module 'matplotlib.mlab' will be lazily loaded>
+    assert 'lazily loaded' in repr(mlab)
+    # accessing mlab's attribute will cause an import of mlab
+    npt.assert_equal(mlab.dist(1969,2011), 42.0)
+    # now mlab should be of class LoadedLazyImport an repr(mlab) should be
+    # <module 'matplotlib.mlab' from # '.../matplotlib/mlab.pyc>
+    assert 'lazily loaded' not in repr(mlab)
+
+# A known limitation of our lazy loading implementation is that, when it it is
+# enabled, reloading the module raises an ImportError, and it also does not
+# actually perform a reload, as demonstrated by this test.
+@dec.skipif(l.disable_lazy_imports)
+def test_lazy_noreload():
+    "Reloading of lazy modules causes ImportError"
+    mod = l.LazyImport('sys')
+    # accessing module dictionary will trigger an import
+    len(mod.__dict__)
+    # do not use named tuple feature for Python 2.6 compatibility
+    major, minor = sys.version_info[:2]
+    if major == 2:
+        npt.assert_raises(ImportError, reload, mod)
+    elif major == 3:
+        import imp
+        if minor == 2:
+            npt.assert_raises(ImportError, imp.reload, mod)
+        elif minor == 3:
+            npt.assert_raises(TypeError, imp.reload, mod)
