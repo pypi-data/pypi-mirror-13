b48297c5f2ff0645d01f7fd0b62d45c39ddf7164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:06 +0000

    Add package xfork/0.37/xfork-0.37.tar.gz

---
+++ packages/xfork/xfork-0.37.tar.gz/xfork-0.37/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+setup(
+    name='xfork',
+    version='0.37',
+    description='submitting cpu-bound tasks to processes and io-bound tasks to threads',
+    author='Sven R. Kunze',
+    author_email='srkunze@mail.de',
+    url='https://github.com/srkunze/fork',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+
+    py_modules=['fork'],
+    install_requires=['futures'] if sys.version_info[0] == 2 else []
+)
