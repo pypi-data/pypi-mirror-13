6e8ec70b0af8cd8c278d8956f8a90997bd442326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:08 +0000

    Add package straitlets/0.2.2/straitlets-0.2.2-py2-none-any.whl

---
+++ packages/straitlets/straitlets-0.2.2-py2-none-any.whl/straitlets/test_utils.py
@@ -0,0 +1,54 @@
+"""
+Testing utilities.
+"""
+from contextlib import contextmanager
+
+import pytest
+from six import iteritems
+
+from .serializable import Serializable
+
+
+def multifixture(g):
+    """
+    Decorator for turning a generator into a "parameterized" fixture that emits
+    the generated values.
+    """
+    fixture_values = list(g())
+
+    @pytest.fixture(params=fixture_values)
+    def _fixture(request):
+        return request.param
+
+    return _fixture
+
+
+def check_attributes(obj, attrs):
+    for key, value in iteritems(attrs):
+        assert getattr(obj, key) == value
+
+
+def assert_serializables_equal(left, right, skip=()):
+    assert type(left) == type(right)
+    assert set(left.trait_names()) == set(right.trait_names())
+    for name in left.trait_names():
+        if name in skip:
+            continue
+        left_attr = getattr(left, name)
+        right_attr = getattr(right, name)
+        assert type(left_attr) == type(right_attr)
+        if isinstance(left_attr, Serializable):
+            assert_serializables_equal(left_attr, right_attr)
+        else:
+            assert left_attr == right_attr
+
+
+@contextmanager
+def removed_keys(dict_, keys):
+    popped = {}
+    for key in keys:
+        popped[key] = dict_.pop(key)
+    try:
+        yield
+    finally:
+        dict_.update(popped)
---
+++ packages/straitlets/straitlets-0.2.2-py2-none-any.whl/straitlets/traits.py
@@ -0,0 +1,187 @@
+"""
+Enhanced versions of IPython's traitlets.
+
+Adds the following additional behavior:
+
+- Strict construction/validation of config attributes.
+- Serialization to/from dictionaries containing only primitives.
+- More strict handling of default values than traitlets' built-in behavior.
+"""
+import inspect
+
+import traitlets as tr
+
+from .to_primitive import to_primitive, can_convert_to_primitive
+
+
+class SerializableTrait(tr.TraitType):
+
+    # Override IPython's default values with Undefined so that default values
+    # must be passed explicitly to trait instances.
+    default_value = tr.Undefined
+
+    def example(self, value):
+        return self.tag(example=value)
+
+    def instance_init(self, obj):
+        super(SerializableTrait, self).instance_init(obj)
+        # If we were tagged with an example, make sure it's actually a valid
+        # example.
+        example = self._static_example_value()
+        if example is not tr.Undefined:
+            self._validate(obj, example)
+
+    def _static_example_value(self):
+        return self.metadata.get('example', self.default_value)
+
+    example_value = property(_static_example_value)
+
+
+class Integer(SerializableTrait, tr.Integer):
+    pass
+
+
+class Float(SerializableTrait, tr.Float):
+    pass
+
+
+class Unicode(SerializableTrait, tr.Unicode):
+    pass
+
+
+class Bool(SerializableTrait, tr.Bool):
+    pass
+
+
+# Different traitlets container types use different values for `default_value`.
+# Figure out what to use by inspecting the signatures of __init__.
+def _get_default_value_sentinel(t):
+    # traitlets Tuple does a kwargs.pop rather than specifying the value in its
+    # signature.
+    if t is tr.Tuple:
+        return tr.Undefined
+    argspec = inspect.getargspec(t.__init__)
+    for name, value in zip(reversed(argspec.args), reversed(argspec.defaults)):
+        if name == 'default_value':
+            return value
+
+    raise TypeError(  # pragma: nocover
+        "Can't find default value sentinel for type %s" % t
+    )
+
+_NOTPASSED = object()
+_TRAITLETS_CONTAINER_TYPES = frozenset([tr.List, tr.Set, tr.Dict, tr.Tuple])
+_DEFAULT_VALUE_SENTINELS = {
+    t: _get_default_value_sentinel(t) for t in _TRAITLETS_CONTAINER_TYPES
+}
+
+
+class _ContainerMixin(object):
+
+    def __init__(self, default_value=_NOTPASSED, **kwargs):
+        # traitlets' Container base class converts default_value into args and
+        # kwargs to pass to a factory type and sets those values to (), {} when
+        # default is None or Undefined.  They do this so that not every List
+        # trait shares the same list object as a default value, but each
+        # subclass mucks with the default value in slightly different ways, and
+        # all of them interpret 'default_value not passed' as 'construct an
+        # empty instance', which we don't think is a sane choice of default.
+        #
+        # Rather than trying to intercept all the different ways that traitlets
+        # overrides default values, we just mark whether we've seen an explicit
+        # default value in our constructor, and our make_dynamic_default
+        # function yields Undefined if this wasn't specified.
+        self._have_explicit_default_value = (default_value is not _NOTPASSED)
+        if not self._have_explicit_default_value:
+            # Different traitlets use different values in their __init__
+            # signatures to signify 'not passed'.  Find the correct value to
+            # forward by inspecting our method resolution order.
+            for type_ in type(self).mro():
+                if type_ in _TRAITLETS_CONTAINER_TYPES:
+                    default_value = _DEFAULT_VALUE_SENTINELS[type_]
+                    break
+            else:  # pragma: nocover
+                raise tr.TraitError(
+                    "_ContainerMixin applied to unknown type %s" % type(self)
+                )
+
+        return super(_ContainerMixin, self).__init__(
+            default_value=default_value,
+            **kwargs
+        )
+
+    def validate(self, obj, value):
+        # Ensure that the value is coercible to a primitive.
+        to_primitive(value)
+        return super(_ContainerMixin, self).validate(obj, value)
+
+    def make_dynamic_default(self):
+        if not self._have_explicit_default_value:
+            return None
+        return super(_ContainerMixin, self).make_dynamic_default()
+
+
+class Set(SerializableTrait, _ContainerMixin, tr.Set):
+    pass
+
+
+class List(SerializableTrait, _ContainerMixin, tr.List):
+    pass
+
+
+class Dict(SerializableTrait, _ContainerMixin, tr.Dict):
+    # For reasons I don't understand, the base Dict constructor intercepts a
+    # default value of Undefined and replaces it with {}.  If None is passed to
+    # the Dict constructor, then we get the desired behavior of barfing unless
+    # a default is explicitly provided.
+    def __init__(self, default_value=None, **kwargs):
+        super(Dict, self).__init__(default_value=default_value, **kwargs)
+
+
+class Tuple(SerializableTrait, _ContainerMixin, tr.Tuple):
+    pass
+
+
+class Enum(SerializableTrait, tr.Enum):
+
+    def __init__(self, *args, **kwargs):
+        super(Enum, self).__init__(*args, **kwargs)
+        for value in self.values:
+            if not can_convert_to_primitive(type(value)):
+                raise TypeError(
+                    "Can't convert Enum value %s to a primitive." % value
+                )
+
+
+class Instance(SerializableTrait, tr.Instance):
+
+    def init(self):
+        self._resolve_classes()
+        if not can_convert_to_primitive(self.klass):
+            raise TypeError(
+                "Can't convert instances of %s to primitives." % (
+                    self.klass.__name__,
+                )
+            )
+        super(Instance, self).init()
+
+    def validate(self, obj, value):
+        from .serializable import Serializable
+        if issubclass(self.klass, Serializable) and isinstance(value, dict):
+            value = self.klass.from_dict(value)
+        return super(Instance, self).validate(obj, value)
+
+    @property
+    def example_value(self):
+        """
+        If we're an instance of a Serializable, fall back to its
+        `example_instance()` method.
+        """
+        from .serializable import Serializable
+        inst = self._static_example_value()
+        if inst is tr.Undefined and issubclass(self.klass, Serializable):
+            return self.klass.example_instance()
+        return inst
+
+    # Override the base class.
+    make_dynamic_default = None
