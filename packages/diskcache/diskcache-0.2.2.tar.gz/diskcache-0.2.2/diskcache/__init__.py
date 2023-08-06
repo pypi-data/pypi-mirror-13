5cae943c4a1e50b73fb795952340681f3e506b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:34 +0000

    Add package diskcache/0.2.2/diskcache-0.2.2.tar.gz

---
+++ packages/diskcache/diskcache-0.2.2.tar.gz/diskcache-0.2.2/diskcache/__init__.py
@@ -0,0 +1,11 @@
+"DiskCache: disk and file-based cache."
+
+from .core import Disk, Cache, EVICTION_POLICY, DEFAULT_SETTINGS, EmptyDirWarning
+# from .djangocache import DjangoCache
+
+__title__ = 'diskcache'
+__version__ = '0.2.2'
+__build__ = 0x000202
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Grant Jenks'
