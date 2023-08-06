dfc8efde0b5102253b1caca72244907b739f43f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:02 +0000

    Add package django-scarface/3.0/django-scarface-3.0.tar.gz

---
+++ packages/django-scarface/django-scarface-3.0.tar.gz/django-scarface-3.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+reqs =['boto>=2.34.0',]
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-scarface',
+    version='3.0',
+    packages=['scarface'],
+    include_package_data=True,
+    license='MIT License',
+    description='Send push notifications to mobile devices using Amazon SNS.',
+    long_description=README,
+    url='http://www.dreipol.ch/',
+    download_url='ttps://github.com/dreipol/django-scarface/releases/tarball/3.0',
+    author='dreipol GmbH',
+    author_email='dev@dreipol.ch',
+    keywords=['aws', 'push notification', 'django'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=reqs,
+)
