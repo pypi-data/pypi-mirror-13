35d10c8120ea6776161c8bd2f0173eee58d6bdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:50 +0000

    Add package PearPyPac/0.1.7/PearPyPac-0.1.7.tar.gz

---
+++ packages/PearPyPac/PearPyPac-0.1.7.tar.gz/PearPyPac-0.1.7/PearPyPac/CGI.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+import cgi
+import json
+
+
+class PearCGI:
+    def __init__(self):
+        return
+
+    def isMethodPOST(self):
+        if(os.environ["REQUEST_METHOD"] == "POST"):
+            return True
+        else:
+            return False
+
+    def isMethodGET(self):
+        if(os.environ["REQUEST_METHOD"] == "GET"):
+            return True
+        else:
+            return False
+
+    def getParameter(self):
+        parameter = cgi.FieldStorage()
+        return parameter
+
+    def isValue(self, data, key):
+        if key not in data:
+            False
+        else:
+            True
+
+    def jsonEncode(self, data):
+            results = json.dumps(data, indent=4)
+            return results
