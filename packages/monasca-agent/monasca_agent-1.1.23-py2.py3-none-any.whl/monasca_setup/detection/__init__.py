657138fc47d9b76d367c9a3682a850af42cd909f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package monasca-agent/1.1.23/monasca_agent-1.1.23-py2.py3-none-any.whl

---
+++ packages/monasca-agent/monasca_agent-1.1.23-py2.py3-none-any.whl/monasca_setup/detection/__init__.py
@@ -0,0 +1,8 @@
+# (C) Copyright 2015 Hewlett Packard Enterprise Development Company LP
+
+from args_plugin import ArgsPlugin
+from plugin import Plugin
+from service_plugin import ServicePlugin
+from utils import find_process_cmdline
+from utils import find_process_name
+from utils import watch_process
