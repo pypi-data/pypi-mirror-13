4d66bbd2bbf1ba5c188c34004853ec65375410a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:08 +0000

    Add package spm1d/0.3.2.3/spm1d-0.3.2.3.tar.gz

---
+++ packages/spm1d/spm1d-0.3.2.3.tar.gz/spm1d-0.3.2.3/README.md
@@ -0,0 +1,20 @@
+spm1d
+=====
+
+One-Dimensional <b>Statistical Parametric Mapping</b> in Python and MATLAB.
+ 
+ 
+spm1d uses <b>Random Field Theory</b> expectations regarding the behavior of smooth, one-dimensional Gaussian fields to make statistical inferences regarding a set of one-dimensional continua.
+
+Software documentation is hosted at:
+[www.spm1d.org](http://spm1d.org)
+
+MATLAB source code is available at:
+https://github.com/0todd0000/spm1dmatlab
+
+
+Issues
+------
+
+Please report software bugs or other problems by searching existing issues or creating a new issue [here](https://github.com/0todd0000/spm1d/issues).
+
---
+++ packages/spm1d/spm1d-0.3.2.3.tar.gz/spm1d-0.3.2.3/spm1d/__init__.py
@@ -0,0 +1,31 @@
+
+
+'''
+spm1D: A Python package for 1D Statistical Parametric Mapping
+
+Useful references include:
+
+[1] Statistical Parametric Mapping: The Analysis of Functional Brain Images
+Karl Friston, John Ashburner, Stefan Kiebel, Thomas Nichols (Editors)
+Academic Press, 2006
+
+[2] Wellcome Trust Centre for Neuroimaging SPM homepage:
+www.fil.ion.ucl.ac.uk/spm/
+
+[3] Pataky TC (2010). Generalized n-dimensional field analysis using
+statistical parametric mapping. Journal of Biomechanics.
+
+
+Copyright (C) 2016  Todd Pataky
+Version: 0.3.2.3 (2016/01/29)
+'''
+
+
+__version__ = '0.3.2.3 (2016/01/29)'
+
+
+__all__ = ['io', 'plot', 'stats', 'util']
+
+
+import data, io, plot, stats, util
+
