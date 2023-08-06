231f4526efaeb4dea5b76f418346095178b5640d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:46 +0000

    Add package py_bonemat_abaqus/1.0.7/py_bonemat_abaqus-1.0.7.win-amd64.zip

---
+++ packages/py_bonemat_abaqus/py_bonemat_abaqus-1.0.7.win-amd64.zip/Anaconda2/Scripts/py_bonemat_abaqus-script.py
@@ -0,0 +1,10 @@
+#!C:\Anaconda2\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'py-bonemat-abaqus==1.0.7','console_scripts','py_bonemat_abaqus'
+__requires__ = 'py-bonemat-abaqus==1.0.7'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('py-bonemat-abaqus==1.0.7', 'console_scripts', 'py_bonemat_abaqus')()
+    )
