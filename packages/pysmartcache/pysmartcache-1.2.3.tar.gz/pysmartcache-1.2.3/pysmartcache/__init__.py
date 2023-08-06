d148c6cd738965bc0ae2beda1cd0de4006f69820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:38 +0000

    Add package pysmartcache/1.2.3/pysmartcache-1.2.3.tar.gz

---
+++ packages/pysmartcache/pysmartcache-1.2.3.tar.gz/pysmartcache-1.2.3/pysmartcache/__init__.py
@@ -0,0 +1,23 @@
+from pysmartcache.clients import CacheClient, MemcachedClient, RedisClient
+from pysmartcache.engine import CacheEngine, cache
+from pysmartcache.exceptions import (ImproperlyConfigured, UniqueRepresentationNotFound, InvalidTypeForUniqueRepresentation,
+                                     CacheClientNotFound)
+from pysmartcache.object_representations import UniqueRepresentation, get_unique_representation
+from pysmartcache.settings import PySmartCacheSettings
+from pysmartcache.utils import depth_getattr
+
+__all__ = [
+    'CacheClient',
+    'MemcachedClient',
+    'RedisClient',
+    'CacheClientNotFound',
+    'CacheEngine',
+    'cache',
+    'ImproperlyConfigured',
+    'UniqueRepresentationNotFound',
+    'InvalidTypeForUniqueRepresentation',
+    'UniqueRepresentation',
+    'get_unique_representation',
+    'PySmartCacheSettings',
+    'depth_getattr',
+]
---
+++ packages/pysmartcache/pysmartcache-1.2.3.tar.gz/pysmartcache-1.2.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+VERSION = '1.2.3'
+
+
+setup(
+    name='pysmartcache',
+    packages=['pysmartcache', ],
+    version=VERSION,
+    author='Filipe Waitman',
+    author_email='filwaitman@gmail.com',
+    install_requires=[x.strip() for x in open('requirements.txt').readlines()],
+    tests_require=[x.strip() for x in open('requirements_test.txt').readlines()],
+    url='https://github.com/filwaitman/pysmartcache',
+    download_url='https://github.com/filwaitman/pysmartcache/tarball/{}'.format(VERSION),
+    keywords=['cache', 'caching', 'memcached', 'redis', 'memoization', 'memoize'],
+    test_suite='tests',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ]
+)
