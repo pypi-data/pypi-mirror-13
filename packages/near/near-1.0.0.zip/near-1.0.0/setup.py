30188bd0d2c96c56cfb6cc4afd5c0ee951a157ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:08 +0000

    Add package near/1.0.0/near-1.0.0.zip

---
+++ packages/near/near-1.0.0.zip/near-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'near',
+    version = '1.0.0',
+    py_modules = ['near'],
+    author = 'lee',
+    author_email = 'lic8086939@163.com',
+    url = '...',
+    description = '迭代显示列表数据',
+    
+	)
