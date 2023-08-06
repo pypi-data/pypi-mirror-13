d53028e595f47443f67a47ac317baf88b80b3560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:39 +0000

    Add package spiget/0.1.2/spiget-0.1.2.tar.gz

---
+++ packages/spiget/spiget-0.1.2.tar.gz/spiget-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+# from pip.req import parse_requirements
+
+# reqs = parse_requirements('requirements.txt', session=False)
+# reqs = [str(ir.req) for ir in reqs]
+#
+
+def main():
+    setup(
+        name='spiget',
+        version='0.1.2',
+        packages=[
+            'spiget'
+        ],
+        url='https://github.com/TechnicalBro/spiget',
+        license='',
+        author='Brandon Curtis',
+        author_email='freebird.brandon@gmail.com',
+        description='Spiget.org API Interaction',
+        download_url='https://github.com/TechnicalBro/spiget/tarball/0.1.1',
+        keywords=['spigotmc', 'spiget', 'minecraft', 'bukkit'],
+        install_requires=[
+            "requests==2.9.1"
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
