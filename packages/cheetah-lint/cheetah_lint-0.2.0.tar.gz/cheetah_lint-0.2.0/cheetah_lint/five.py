4ecfd0127550bf0891078f205614e03aa4970bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:06 +0000

    Add package cheetah-lint/0.2.0/cheetah_lint-0.2.0.tar.gz

---
+++ packages/cheetah-lint/cheetah_lint-0.2.0.tar.gz/cheetah_lint-0.2.0/cheetah_lint/five.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# pylint:disable=undefined-variable
+"""five: six, redux"""
+
+PY2 = (str is bytes)
+PY3 = (str is not bytes)
+
+# provide a symettrical `text` type to `bytes`
+if PY2:
+    text = unicode  # flake8: noqa # pragma: no cover
+else:
+    text = str  # pragma: no cover
