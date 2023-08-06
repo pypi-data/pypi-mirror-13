6879dee146b2b50990d57b57f11780afb1f7f870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:21 +0000

    Add package nbimporter/0.2/nbimporter-0.2.tar.gz

---
+++ packages/nbimporter/nbimporter-0.2.tar.gz/nbimporter-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='nbimporter',
+      version='0.2',
+      description='Import IPython notebooks as modules',
+      author='Adam Schwartz',
+      author_email='adam@anschwa.com',
+      py_modules=['nbimporter', 'display_nb'],
+      url='https://github.com/grst/nbimporter',
+ )
