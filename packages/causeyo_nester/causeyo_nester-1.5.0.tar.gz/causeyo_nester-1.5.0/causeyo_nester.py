2147d4509caae567971bc56b30258ebb6bf5eab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:55 +0000

    Add package causeyo_nester/1.5.0/causeyo_nester-1.5.0.tar.gz

---
+++ packages/causeyo_nester/causeyo_nester-1.5.0.tar.gz/causeyo_nester-1.5.0/causeyo_nester.py
@@ -0,0 +1,15 @@
+import sys
+"""This is multiline comment
+nester.py module"""
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
+    """this function takes one positional
+    argument and checks if it is a list-type
+    and indent check"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='', file=fh)
+            print(each_item, file=fh)
---
+++ packages/causeyo_nester/causeyo_nester-1.5.0.tar.gz/causeyo_nester-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'causeyo_nester',
+        version = '1.5.0',
+        py_modules = ['causeyo_nester'],
+        author = 'causeyo',
+        author_email = 'wifonik@gmail.com',
+        url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested list',
+   )
