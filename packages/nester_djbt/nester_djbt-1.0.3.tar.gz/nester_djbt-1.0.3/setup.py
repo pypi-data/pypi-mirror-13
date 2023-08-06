a54e1365a9e6c230de22f3a8b38b49ebec448550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:10 +0000

    Add package nester_djbt/1.0.3/nester_djbt-1.0.3.tar.gz

---
+++ packages/nester_djbt/nester_djbt-1.0.3.tar.gz/nester_djbt-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    
+name = 'nester_djbt',
+    
+version = '1.0.3',
+    py_modules = ['nester'],
+    author = 'dirkjanbreeuwer',
+    author_email = 'dirkjanbreeuwer@gmail.com',
+    url = 'http://www.dirkjanbreeuwer.com',
+    description = 'A simple printer of nested lists',
+    )
