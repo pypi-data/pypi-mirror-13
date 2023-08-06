dc15ec2afdad2b55d5d72947d147d245c01cd041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:48 +0000

    Add package nester_wing/1.1.2/nester_wing-1.1.2.zip

---
+++ packages/nester_wing/nester_wing-1.1.2.zip/nester_wing-1.1.2/nester_wing.py
@@ -0,0 +1,11 @@
+﻿#这是"nester_wing.py"模块
+#兼容性API
+def print_lol(the_list,level=0):
+  for each_outher in the_list:
+    if isinstance(each_outher,list):
+      print_lol(each_outher,level+1)
+    else:
+      for tab_stop in range(level):
+        print('\t',end='')
+      print(each_outher)
+
---
+++ packages/nester_wing/nester_wing-1.1.2.zip/nester_wing-1.1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='nester_wing',
+  version='1.1.2',
+  py_modules=['nester_wing'],
+  author='gui_jia',
+  author_email='gui_jia@wo.cn',
+  url='http://www.gzmeo.com',
+  description='wing',)
