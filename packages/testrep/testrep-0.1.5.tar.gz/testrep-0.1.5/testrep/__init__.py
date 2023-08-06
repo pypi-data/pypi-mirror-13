7ec7e67cd73d8f67747d9e33fc2a53b1155259eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:39 +0000

    Add package testrep/0.1.5/testrep-0.1.5.tar.gz

---
+++ packages/testrep/testrep-0.1.5.tar.gz/testrep-0.1.5/testrep/__init__.py
@@ -0,0 +1,20 @@
+import math
+
+def AreYouErnesto(name):
+	if(name == "Ernesto"):
+		return True
+	return False
+
+class vecstuff():
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+
+	def normalvector(self):
+		unit = sqrt(pow(self.x)+pow(self.y)+pow(self.z))
+		return self.x/unit, self.y/unit, self.z/unit
+
+	def length(self):
+		return sqrt(pow(self.x)+pow(self.y)+pow(self.z))
+			
