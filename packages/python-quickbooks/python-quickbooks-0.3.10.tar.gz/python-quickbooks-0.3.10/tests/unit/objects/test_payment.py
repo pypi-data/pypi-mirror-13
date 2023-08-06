6a041e1fce74d466a5a04674176e81af914f23a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:08 +0000

    Add package python-quickbooks/0.3.10/python-quickbooks-0.3.10.tar.gz

---
+++ packages/python-quickbooks/python-quickbooks-0.3.10.tar.gz/python-quickbooks-0.3.10/tests/unit/objects/test_payment.py
@@ -0,0 +1,21 @@
+import unittest
+
+from quickbooks.objects.payment import PaymentLine, Payment
+
+
+class PaymentLineTests(unittest.TestCase):
+    def test_unicode(self):
+        payment_line = PaymentLine()
+        payment_line.LineNum = 1
+        payment_line.Description = "Product Description"
+        payment_line.Amount = 100
+
+        self.assertEquals(str(payment_line), "[1] Product Description 100")
+
+
+class PaymentTests(unittest.TestCase):
+    def test_unicode(self):
+        payment = Payment()
+        payment.TotalAmt = 1000
+
+        self.assertEquals(str(payment), '1000')
