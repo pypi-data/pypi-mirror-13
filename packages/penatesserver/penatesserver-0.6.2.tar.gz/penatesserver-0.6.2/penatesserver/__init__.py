137d4f5f11560f26c77acd681776feb4e44141ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:03 +0000

    Add package penatesserver/0.6.2/penatesserver-0.6.2.tar.gz

---
+++ packages/penatesserver/penatesserver-0.6.2.tar.gz/penatesserver-0.6.2/penatesserver/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+__author__ = 'flanker'
+__version__ = '0.6.2'
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
