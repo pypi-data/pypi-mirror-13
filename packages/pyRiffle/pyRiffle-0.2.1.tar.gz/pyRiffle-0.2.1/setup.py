a77bcc2bb088dbf144771c006d30c13d1473d32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:07 +0000

    Add package pyRiffle/0.2.1/pyRiffle-0.2.1.tar.gz

---
+++ packages/pyRiffle/pyRiffle-0.2.1.tar.gz/pyRiffle-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+
+setup(
+    name="pyRiffle",
+    version="0.2.1",
+    description="Riffle client libraries for interacting over a fabric",
+    author="Exis",
+    url="http://www.exis.io",
+    license="MIT",
+
+    packages=["riffle"],
+    include_package_data=True,
+
+    install_requires=[
+        'greenlet'
+    ],
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
