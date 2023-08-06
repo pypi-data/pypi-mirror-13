a36b66cdd66de0083846c5a9dbcc4a8f187d46da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:41 +0000

    Add package sshagentmux/0.0.1/sshagentmux-0.0.1.tar.gz

---
+++ packages/sshagentmux/sshagentmux-0.0.1.tar.gz/sshagentmux-0.0.1/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2015, IBM
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sshagentmux',
+    version='0.0.1',
+    packages=find_packages(),
+    license='Apache2',
+    description = 'SSH Agent Authorization Proxy and Multiplexer',
+    author = 'Dustin Lundquist',
+    author_email = 'dustin@null-ptr.net',
+    url = 'https://github.com/blueboxgroup/sshagentmux',
+    download_url = 'https://github.com/blueboxgroup/sshagentmux/tarball/v0.0.1',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'authorization_proxy = sshagentmux.authorization_proxy:main',
+            'sshagentmux = sshagentmux.ssh_agent_mux:main'
+        ],
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+    ],
+)
+
