d428b3fdde2fc9c9b8b0249bd65148591dabe283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:29 +0000

    Add package suspect/0.0.5/suspect-0.0.5.tar.gz

---
+++ packages/suspect/suspect-0.0.5.tar.gz/suspect-0.0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='suspect',
+        version='0.0.5',
+        packages=find_packages(),
+        url='https://github.com/bennyrowland/suspect.git',
+        license='MIT',
+        author='bennyrowland',
+        author_email='bennyrowland@mac.com',
+        description='',
+        entry_points={
+        },
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 2 - Pre-Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering :: Medical Science Apps.',
+            'Topic :: Scientific/Engineering :: Physics',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: MIT License',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+        install_requires=['numpy', 'pytest', 'mock']
+)
---
+++ packages/suspect/suspect-0.0.5.tar.gz/suspect-0.0.5/suspect/processing/__init__.py
@@ -0,0 +1,1 @@
+from . import frequency_correction, channel_combination
