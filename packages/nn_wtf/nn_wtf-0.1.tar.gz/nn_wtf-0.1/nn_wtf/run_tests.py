d7118586687377594947579b1622c72b34efffcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:23 +0000

    Add package nn_wtf/0.1/nn_wtf-0.1.tar.gz

---
+++ packages/nn_wtf/nn_wtf-0.1.tar.gz/nn_wtf-0.1/nn_wtf/run_tests.py
@@ -0,0 +1,9 @@
+from tests.neural_network_graph_test import NeuralNetworkGraphTest
+from tests.images_labels_data_set_test import ImagesLabelsDataSetTest
+from tests.input_data_test import InputDataTest
+
+import unittest
+
+__author__ = 'Lene Preuss <lene.preuss@gmx.net>'
+
+unittest.main()
---
+++ packages/nn_wtf/nn_wtf-0.1.tar.gz/nn_wtf-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+__author__ = 'Lene Preuss <lene.preuss@gmail.com>'
+
+
+setup(
+    name='nn_wtf',
+    version='0.1',
+    description='Neural Networks Wrapper for TensorFlow',
+    url='http://github.com/lene/nn-wtf',
+    author='Lene Preuss',
+    author_email='lene.preuss@gmail.com',
+    license='Apache V2',
+    packages=['nn_wtf'],
+    zip_safe=False
+)
