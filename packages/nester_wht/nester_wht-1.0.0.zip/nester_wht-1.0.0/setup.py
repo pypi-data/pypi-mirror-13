af55f844a1123eb52ea825b463ead9eecff2f997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package nester_wht/1.0.0/nester_wht-1.0.0.zip

---
+++ packages/nester_wht/nester_wht-1.0.0.zip/nester_wht-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name	='nester_wht',
+		version 	='1.0.0',
+		py_modules=['nester'],
+		author 	='WarHeat',
+		author_email='wht_thu@outlook.com',
+		url 	='http://whtqh.github.io/',
+		description='HeHe',
+                )
