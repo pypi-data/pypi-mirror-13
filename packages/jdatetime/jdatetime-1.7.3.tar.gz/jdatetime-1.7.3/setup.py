b3bf1978338997277a1ef05e8fd6d69093796896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:57 +0000

    Add package jdatetime/1.7.3/jdatetime-1.7.3.tar.gz

---
+++ packages/jdatetime/jdatetime-1.7.3.tar.gz/jdatetime-1.7.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='jdatetime',
+        version='1.7.3',
+        packages=['jdatetime',],
+        license='Python Software Foundation License',
+        keywords='Jalali implementation of Python datetime',
+        platforms='any',
+        description=("Jalali datetime binding for python"),
+        url="https://github.com/slashmili/python-jalali",
+        long_description=open('README.rst').read()
+)
