58dc6677f6e18fdf4e269d78a67dc49ea1fe62c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:37 +0000

    Add package plone.app.controlpanel/3.0.4/plone.app.controlpanel-3.0.4.tar.gz

---
+++ packages/plone.app.controlpanel/plone.app.controlpanel-3.0.4.tar.gz/plone.app.controlpanel-3.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+version = '3.0.4'
+
+setup(name='plone.app.controlpanel',
+      version=version,
+      description="Formlib-based controlpanels for Plone.",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+        ],
+      keywords='plone controlpanel formlib',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='https://pypi.python.org/pypi/plone.app.controlpanel',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'Products.CMFPlone',
+      ],
+      extras_require={
+        'test': [
+        ]
+      }
+      )
