457a9cdcba07bdd367127c8ec99ac774b6dd8d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:44 +0000

    Add package watson-developer-cloud/0.7.0/watson-developer-cloud-0.7.0.tar.gz

---
+++ packages/watson-developer-cloud/watson-developer-cloud-0.7.0.tar.gz/watson-developer-cloud-0.7.0/examples/natural_language_classifier_v1.py
@@ -0,0 +1,21 @@
+import json
+from os.path import join, dirname
+from watson_developer_cloud import NaturalLanguageClassifierV1 as NaturalLanguageClassifier
+
+
+natural_language_classifier = NaturalLanguageClassifier(username='YOUR SERVICE USERNAME',
+                                                        password='YOUR SERVICE PASSWORD')
+
+classifiers = natural_language_classifier.list()
+print(json.dumps(classifiers, indent=2))
+
+# create a classifier
+# with open('../resources/weather_data_train.csv', 'rb') as training_data:
+#     print(json.dumps(natural_language_classifier.create(training_data=training_data, name='weather2'), indent=2))
+
+# replace 47C164-nlc-243 with your classifier id
+status = natural_language_classifier.status('47C164-nlc-243')
+print (json.dumps(status, indent=2))
+
+classes = natural_language_classifier.classify('47C164-nlc-243', 'How hot will it be tomorrow?')
+print(json.dumps(classes, indent=2))
