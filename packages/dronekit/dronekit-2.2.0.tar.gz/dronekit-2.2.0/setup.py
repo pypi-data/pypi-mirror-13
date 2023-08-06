dbeb990b2b167fcb08f85ef5568b9f17301d16dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:34 +0000

    Add package dronekit/2.2.0/dronekit-2.2.0.tar.gz

---
+++ packages/dronekit/dronekit-2.2.0.tar.gz/dronekit-2.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension
+import platform
+
+version = '2.2.0'
+
+setup(name='dronekit',
+      zip_safe=True,
+      version=version,
+      description='Python language bindings for the DroneApi',
+      long_description='Python language bindings for the DroneApi',
+      url='https://github.com/dronekit/dronekit-python',
+      author='3D Robotics',
+      install_requires=[
+          'pymavlink>=1.1.62',
+          'requests>=2.5.0,<=2.99999',
+      ],
+      author_email='tim@3drobotics.com, kevinh@geeksville.com',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering',
+      ],
+      license='apache',
+      packages=[
+          'dronekit', 'dronekit.cloud', 'dronekit.test'
+      ],
+      ext_modules=[])
