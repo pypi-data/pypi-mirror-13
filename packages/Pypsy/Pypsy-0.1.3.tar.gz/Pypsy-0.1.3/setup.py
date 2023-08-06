a0aa8aa88ce82f10f411fe4baa9274f16a1f2fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:51 +0000

    Add package Pypsy/0.1.3/Pypsy-0.1.3.tar.gz

---
+++ packages/Pypsy/Pypsy-0.1.3.tar.gz/Pypsy-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pypsy',
+    version='0.1.3',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'sklearn'
+    ],
+    url='http://www.musicsensorsemotion.com',
+    license='MIT',
+    author='Brennon Bortz',
+    author_email='brennon@vt.edu',
+    description='Electrodermal activity processing and analysis'
+)
