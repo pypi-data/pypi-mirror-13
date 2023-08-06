1292fb86953a23186b849075df3968886be6083d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:48 +0000

    Add package kuga/0.1/kuga-0.1.tar.gz

---
+++ packages/kuga/kuga-0.1.tar.gz/kuga-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'kuga',
+    py_modules = ['kuga'],
+    version = '0.1',
+    description = 'Now is better than never.',
+    author = 'kuga',
+    author_email = 'kugacola@gmail.com',
+    url = 'http://kuga.me',
+    keywords = ['kuga'],
+)
