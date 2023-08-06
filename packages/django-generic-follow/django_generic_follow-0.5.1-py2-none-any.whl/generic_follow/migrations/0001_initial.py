b049766a4d1f0d1bdebaab277e1a5bdd843cf28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:22 +0000

    Add package django-generic-follow/0.5.1/django_generic_follow-0.5.1-py2-none-any.whl

---
+++ packages/django-generic-follow/django_generic_follow-0.5.1-py2-none-any.whl/generic_follow/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Follow',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('target_object_id', models.PositiveIntegerField()),
+                ('target_content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='follow',
+            unique_together=set([('user', 'target_content_type', 'target_object_id')]),
+        ),
+    ]
