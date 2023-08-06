0a70f6bb3a57a6892ff68978f40494e7c5afc4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package odoo-data/0.1/odoo-data-0.1.tar.gz

---
+++ packages/odoo-data/odoo-data-0.1.tar.gz/odoo-data-0.1/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = 'odoo-data',
+    packages = ['tests'],
+    version = '0.1',
+    description = 'Odoo XML data files library',
+    author = 'Will Earp',
+    author_email = 'will.earp@icloud.com',
+    url = 'https://github.com/wearp/odoo-data',
+    download_url = 'https://github.com/wearp/odoo-data/tarball/0.1',
+    keywords = ['odoo', 'xml', 'data', 'files'],
+)
