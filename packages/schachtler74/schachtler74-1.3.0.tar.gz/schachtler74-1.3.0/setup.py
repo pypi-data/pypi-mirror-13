ad64ff6be6fb28dc980b69d6db868173b3bd1c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:11 +0000

    Add package schachtler74/1.3.0/schachtler74-1.3.0.tar.gz

---
+++ packages/schachtler74/schachtler74-1.3.0.tar.gz/schachtler74-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+            name = 'schachtler74',
+            version = '1.3.0',
+            py_modules = ['schachtler'],
+            author = 'david74heine',
+            author_email = 'david.heine@swissonline.ch',
+            url = 'http://headfirstlabs.com',
+            description = 'Einfache Ausgabe geschachtelter Listen'
+        )
