82061ffbca2fd8928a51a10110f289dd61790180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:34 +0000

    Add package pinax-lms-activities/0.7.0/pinax_lms_activities-0.7.0-py2.py3-none-any.whl

---
+++ packages/pinax-lms-activities/pinax_lms_activities-0.7.0-py2.py3-none-any.whl/pinax/lms/activities/hooks.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+from django.contrib import messages
+
+
+class ActivitiesDefaultHookSet(object):
+
+    def activity_class_path(self, slug):
+        return settings.PINAX_LMS_ACTIVITIES_ACTIVITIES.get(slug)
+
+    def all_activities(self):
+        return settings.PINAX_LMS_ACTIVITIES_ACTIVITIES.items()
+
+    def success_message(self, request, activity):
+        if activity.repeatable:
+            messages.success(request, "{} activity completed. You may repeat it again at any time.".format(activity.title))
+        else:
+            messages.success(request, "{} activity completed.".format(activity.title))
+
+    def already_completed_message(self, request, activity):
+        messages.info(request, "{} activity already completed.".format(activity.title))
+
+
+class HookProxy(object):
+
+    def __getattr__(self, attr):
+        return getattr(settings.PINAX_LMS_ACTIVITIES_HOOKSET, attr)
+
+
+hookset = HookProxy()
