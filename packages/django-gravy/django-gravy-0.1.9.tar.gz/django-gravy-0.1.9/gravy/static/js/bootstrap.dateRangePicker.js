ca387544a84f3ee1075cb6d1f3662cf98b8fd5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package django-gravy/0.1.9/django-gravy-0.1.9.tar.gz

---
+++ packages/django-gravy/django-gravy-0.1.9.tar.gz/django-gravy-0.1.9/gravy/static/js/bootstrap.dateRangePicker.js
@@ -0,0 +1,86 @@
+;(function($, window, document, undefined) {
+
+    var pluginName = 'dateRangePicker',
+        defaults = {
+            warnStartPast: false,
+            warnDuration: false
+        };
+
+    var DateRangePicker = function(element, options) {
+        this.element = element;
+        this.options = $.extend({}, defaults, options, $(element).data());
+        this.init();
+    };
+
+    DateRangePicker.prototype = {
+
+        init: function() {
+            this.$element = $(this.element);
+            this.$start = this.$element.find('input[type="text"]').first();
+            this.$end = this.$element.find('input[type="text"]').last();
+            this.$buttons = this.$element.find('button[data-value]');
+            this.initEvents();
+            return this;
+        },
+
+        initEvents: function() {
+            setInterval($.proxy(this.validate, this), 200);
+            this.$buttons.click($.proxy(this.click, this));
+        },
+
+        validate: function(e) {
+            var start = this.$start.data('DateTimePicker').date();
+            var end = this.$end.data('DateTimePicker').date();
+            var start_warn = 0;
+            var end_warn = 0;
+            if (this.options.warnStartPast && start) {
+                if (start.add(this.options.warnStartPast, 'seconds') < moment()) {
+                    start_warn++;
+                }
+            }
+            if (this.options.warnDuration && start && end) {
+                if (start.add(this.options.warnDuration, 'seconds') < end) {
+                    start_warn++;
+                    end_warn++;
+                }
+            }
+            if (start_warn && !this.$start.hasClass('warning')) {
+                this.$start.addClass('warning');
+            }
+            if (!start_warn && this.$start.hasClass('warning')) {
+                this.$start.removeClass('warning');
+            }
+            if (end_warn && !this.$end.hasClass('warning')) {
+                this.$end.addClass('warning');
+            }
+            if (!end_warn && this.$end.hasClass('warning')) {
+                this.$end.removeClass('warning');
+            }
+        },
+
+        click: function(e) {
+            var $button = $(e.target);
+            var start = this.$start.data('DateTimePicker').date();
+            if (start == null) {
+                start = moment();
+                this.$start.data('DateTimePicker').date(start);
+            }
+            var end = start.add($button.data('value'), 's');
+            this.$end.data('DateTimePicker').date(end);
+            e.preventDefault();
+            return false;
+        }
+    };
+
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if (!$.data(this, pluginName)) {
+                $.data(this, pluginName, new DateRangePicker(this, options));
+            }
+        });
+    };
+
+    $('[data-toggle="' + pluginName + '"]')[pluginName]();
+
+})(jQuery, window, document);
+
