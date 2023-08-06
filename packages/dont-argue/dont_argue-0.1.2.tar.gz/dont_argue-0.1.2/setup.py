685ba71d70de0fe1c7ab1396deb7dcb0ca8cd2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:43 +0000

    Add package dont-argue/0.1.2/dont_argue-0.1.2.tar.gz

---
+++ packages/dont-argue/dont_argue-0.1.2.tar.gz/dont_argue-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name = 'dont_argue',
+    packages = ['dont_argue'],
+    package_data={ 'dont_argue': ['README.md'], },
+    version = '0.1.2',
+    description = 'Dead-simple command line argument parsing',
+    long_description=long_description,
+    author = 'Chris Penner',
+    author_email = 'christopher.penner@gmail.com',
+    url = 'https://github.com/ChrisPenner/dont-argue',
+    download_url = 'https://github.com/ChrisPenner/dont-argue/releases/tag/v0.1.2', 
+    license = 'MIT',
+    keywords = ['command line', 'argument', 'parsing', 'argparse'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+
+        'Topic :: Software Development :: Libraries',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
