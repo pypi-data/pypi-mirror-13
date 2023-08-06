25bf22daaa40b11a6a082ac0323e0d3b028a162d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:41 +0000

    Add package pyntc/0.0.1/pyntc-0.0.1.tar.gz

---
+++ packages/pyntc/pyntc-0.0.1.tar.gz/pyntc-0.0.1/pyntc/data_model/key_maps/eos_key_maps.py
@@ -0,0 +1,20 @@
+BASIC_FACTS_KM = {
+    'model': 'modelName',
+    'os_version': 'internalVersion',
+    'serial_number': 'serialNumber'
+}
+
+
+INTERFACES_KM = {
+    'speed': 'bandwidth',
+    'duplex': 'duplex',
+    'vlan': ['vlanInformation', 'vlanId'],
+    'state': 'linkStatus',
+    'description': 'description',
+}
+
+VLAN_KM = {
+    'state': 'state',
+    'name': 'name',
+    'id': 'vlan_id',
+}
