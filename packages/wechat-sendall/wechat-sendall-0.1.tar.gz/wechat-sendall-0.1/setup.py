247f57fc0f795585598e261c6bafd9f4ca805c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:35 +0000

    Add package wechat-sendall/0.1/wechat-sendall-0.1.tar.gz

---
+++ packages/wechat-sendall/wechat-sendall-0.1.tar.gz/wechat-sendall-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wechat-sendall',
+  packages = ['wechat-sendall'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Send custom message to your friend on wechat',
+  author = 'Alex',
+  author_email = 'lifeng1519@gmail.com',
+  url = 'https://github.com/vonnyfly/wechat-sendall', # use the URL to the github repo
+  download_url = 'https://github.com/vonnyfly/wechat-sendall/tarball/1.0', # I'll explain this in a second
+  keywords = ['wechat', 'send', 'robot'], # arbitrary keywords
+  classifiers = [],
+)
