c89ebee83a036652c4338820169fadb8f6697f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:14 +0000

    Add package tonynester/1.1.0/tonynester-1.1.0.zip

---
+++ packages/tonynester/tonynester-1.1.0.zip/tonynester-1.1.0/tonynester.py
@@ -0,0 +1,13 @@
+'''This is the "nester.py" module and it provides one funciont.....'''
+
+def print_lol(the_list, level):
+    '''This is function takes one positional argument called "the list", which
+     is any python list'''
+    
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print('\t', end='')
+            print(each_item)
