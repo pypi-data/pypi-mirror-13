957a050cf9b75758cab6feccdb3ec02a2d77cd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package itolapi/1.3.0/itolapi-1.3.0.tar.gz

---
+++ packages/itolapi/itolapi-1.3.0.tar.gz/itolapi-1.3.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+try:
+    readme = open("README.rst")
+    long_description = str(readme.read())
+finally:
+    readme.close()
+
+setup(name='itolapi',
+      version='1.3.0',
+      description='API for interacting with itol.embl.de',
+      long_description=long_description,
+      url='http://github.com/albertyw/itolapi',
+      author='Albert Wang',
+      author_email='albertyw@mit.edu',
+      license='MIT',
+      packages=['itolapi'],
+      install_requires=[
+          'requests>=2.0, <3.0',
+      ],
+      scripts=['itolapi/Itol.py', 'itolapi/ItolExport.py'],
+      test_suite="tests",
+      tests_require=[
+          'mock>=1.0.1',
+          'tox',
+          'codecov',
+      ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ],
+      )
