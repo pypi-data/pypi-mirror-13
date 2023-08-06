db1e8ab533d713e6fed9eaf6bd48608fc0e7aa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:14 +0000

    Add package polar_analyzer/0.6/polar_analyzer-0.6.zip

---
+++ packages/polar_analyzer/polar_analyzer-0.6.zip/polar_analyzer-0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='polar_analyzer',
+      version='0.6',
+      description='maps positive and negative semantic values to words in a string or a text file.',
+      url='http://github.com/sherifEwis/polar_analyzer/',
+      author='Flying Circus',
+      author_email='sherifewis@tickletock.com',
+      license='MIT',
+      packages=['polar_analyzer'],
+      install_requires=[
+          'statistics',
+          'nltk',
+          'matplotlib'
+      ],
+      zip_safe=False)
