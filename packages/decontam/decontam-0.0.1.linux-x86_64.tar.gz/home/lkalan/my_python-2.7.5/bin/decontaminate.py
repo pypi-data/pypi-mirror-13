647b3b6c0a9f6c1387f95db3c4e42d03443c109e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:25 +0000

    Add package decontam/0.0.1/decontam-0.0.1.linux-x86_64.tar.gz

---
+++ packages/decontam/decontam-0.0.1.linux-x86_64.tar.gz/home/lkalan/my_python-2.7.5/bin/decontaminate.py
@@ -0,0 +1,3 @@
+#!/home/lkalan/my_python-2.7.5/bin/python
+from decontamlib.main import human_filter_main
+human_filter_main()
---
+++ packages/decontam/decontam-0.0.1.linux-x86_64.tar.gz/home/lkalan/my_python-2.7.5/bin/make_index.py
@@ -0,0 +1,3 @@
+#!/home/lkalan/my_python-2.7.5/bin/python
+from decontamlib.main import make_index_main
+make_index_main()
