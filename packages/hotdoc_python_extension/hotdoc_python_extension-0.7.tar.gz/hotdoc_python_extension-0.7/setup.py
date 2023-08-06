04f94c4a135ba1708f1a080bdd2839debb2f5a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:51 +0000

    Add package hotdoc_python_extension/0.7/hotdoc_python_extension-0.7.tar.gz

---
+++ packages/hotdoc_python_extension/hotdoc_python_extension-0.7.tar.gz/hotdoc_python_extension-0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "hotdoc_python_extension",
+    version = "0.7",
+    keywords = "python ast sphinx napoleon hotdoc",
+    url='https://github.com/hotdoc/hotdoc_python_extension',
+    author_email = 'mathieu.duponchelle@opencreed.com',
+    license = 'LGPL',
+    description = ("An extension for hotdoc that parses python using the"
+        " standard ast module and napoleon from sphinx"),
+    author = "Mathieu Duponchelle",
+    packages = find_packages(),
+    package_data = {
+        '': ['*.html'],
+    },
+    entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_python_extension.python_extension:get_extension_classes'},
+    install_requires = [
+        'docutils==0.12',
+        'pypandoc==1.1.3',
+    ]
+)
