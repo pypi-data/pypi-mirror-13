5f210304f35c3d21f6d030c4209d8711c1305895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:50 +0000

    Add package kaaedit/0.50.0/kaaedit-0.50.0.tar.gz

---
+++ packages/kaaedit/kaaedit-0.50.0.tar.gz/kaaedit-0.50.0/kaa/ui/pastelines/pastelinesmode.py
@@ -0,0 +1,41 @@
+import kaa
+from kaa import document, command
+from kaa.ui.multiline import multilinemode
+from kaa.theme import Theme, Style
+from kaa.filetype.default import keybind
+from kaa.commands import editorcommand
+from kaa.keyboard import *
+
+class PasteLinesMode(multilinemode.MultilineMode):
+
+    auto_indent = False
+
+
+    def close(self):
+        super().close()
+        self.target = None
+
+    def on_add_window(self, wnd):
+        super().on_add_window(wnd)
+
+        kaa.app.messagebar.set_message("Paste text and hit alt+Enter")
+
+    @command.commandid('multiline.done')
+    @command.norec
+    @command.norerun
+    def paste_lines(self, wnd):
+        f, t = self.document.marks['multiline']
+        s = self.document.gettext(f, t)
+
+        self.target.document.mode.put_string(self.target, s)
+
+        kaa.app.messagebar.set_message("{} letters inserted".format(t - f))
+
+        self.multiline_done(wnd)
+
+    @classmethod
+    def build(cls, target):
+        doc = super().build()
+        doc.mode.target = target
+
+        return doc
