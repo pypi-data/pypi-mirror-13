b2d975e5913c60e9f79a87af749847cb79de081f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package pyknight/1.0/pyknight-1.0.tar.gz

---
+++ packages/pyknight/pyknight-1.0.tar.gz/pyknight-1.0/pkgA/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def fun():
+    print "hello world"
+
+if __name__ == '__main__':
+    fun()
---
+++ packages/pyknight/pyknight-1.0.tar.gz/pyknight-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyknight',
+    packages = ['pkgA'],
+    scripts = ['exec'],
+    version = '1.0',
+    description = 'My first project',
+    author = 'chairco',
+    author_email = 'chairco@gmail.com',
+    url = 'https://github.com/chairco/pyknight',
+    download_url = 'https://github.com/chairco/pyknight/tarball/v1.0',
+    keywords = ['pyknight'],
+    classifiers = [],
+)
