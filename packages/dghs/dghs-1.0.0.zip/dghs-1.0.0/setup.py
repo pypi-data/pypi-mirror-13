bd07844a7abc9a7fb6ba1014a35eda56d2c7f1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:36 +0000

    Add package dghs/1.0.0/dghs-1.0.0.zip

---
+++ packages/dghs/dghs-1.0.0.zip/dghs-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+     name='dghs',
+     version='1.0.0',
+     py_modules=['dghs'],
+     author='bbclyj',
+     author_email='bbclyj@163.com',
+     description='First try',
+    )
