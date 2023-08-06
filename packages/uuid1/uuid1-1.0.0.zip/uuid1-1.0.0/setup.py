9f732731d29e2e72269dbe5318f523c723dd20ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:28 +0000

    Add package uuid1/1.0.0/uuid1-1.0.0.zip

---
+++ packages/uuid1/uuid1-1.0.0.zip/uuid1-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'uuid1',
+  packages = ['uuid1'],
+  version = '1.0.0',
+  description = 'Library that generates time sortable UUIDs',
+  long_description = '',
+  author = 'David Fierro',
+  author_email = 'fierro85@gmail.com',
+  url = 'https://github.com/touchvie/python-uuid1',
+  download_url = 'https://github.com/touchvie/python-uuid1/tarball/0.1.0',
+  keywords = ['uuid', 'uuid1', 'TimeUUID'],
+  classifiers = [],
+  license = 'MIT'
+)
