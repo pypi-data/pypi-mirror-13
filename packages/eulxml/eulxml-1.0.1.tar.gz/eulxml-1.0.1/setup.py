4d7845a999ee0dd4a39d2a5d6876f0eca400426e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:18 +0000

    Add package eulxml/1.0.1/eulxml-1.0.1.tar.gz

---
+++ packages/eulxml/eulxml-1.0.1.tar.gz/eulxml-1.0.1/setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+from distutils.command.build_py import build_py
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+import eulxml
+
+class build_py_with_ply(build_py):
+    '''Use ply to generate parsetab and lextab modules.'''
+
+    def run(self, *args, **kwargs):
+        # importing this forces ply to generate parsetab/lextab
+        import eulxml.xpath.core
+        build_py.run(self, *args, **kwargs)
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Text Processing :: Markup :: XML',
+]
+
+LONG_DESCRIPTION = None
+try:
+    # read the description if it's there
+    with open('README.rst') as desc_f:
+        LONG_DESCRIPTION = desc_f.read()
+except:
+    pass
+
+dev_requirements = [
+    'sphinx>=1.3.5',
+    'coverage',
+    'Django<1.9',
+    'rdflib>=3.0',
+    'mock',
+    'nose',
+    'tox',
+]
+# NOTE: dev requirements should be duplicated in pip-dev-req.txt
+# for generating documentation on readthedocs.org
+
+# unittest2 should only be included for py2.6
+if sys.version_info < (2, 7):
+    dev_requirements.append('unittest2')
+
+
+setup(
+    cmdclass={'build_py': build_py_with_ply},
+
+    name='eulxml',
+    version=eulxml.__version__,
+    author='Emory University Libraries',
+    author_email='libsysdev-l@listserv.cc.emory.edu',
+    url='https://github.com/emory-libraries/eulxml',
+    license='Apache License, Version 2.0',
+    packages=find_packages(),
+
+    setup_requires=[
+        'ply>=3.8',
+    ],
+    install_requires=[
+        'ply',
+        'lxml',
+        'six>=1.10',
+    ],
+    extras_require={
+        'django': ['Django<1.9'],
+        'rdf': ['rdflib>=3.0'],
+        'dev': dev_requirements
+    },
+
+    description='XPath-based XML data binding, with Django form support',
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+)
