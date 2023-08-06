cb26142e2fa3b935d098b175e509221a3701ec90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:33 +0000

    Add package django_shark/0.0.1/django_shark-0.0.1.zip

---
+++ packages/django_shark/django_shark-0.0.1.zip/django_shark-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name = "django_shark",
+    packages = ["shark_core"],
+    version = "0.0.1",
+    description = "Django based bootstrap web framework",
+    author = "Bart Jellema",
+    author_email = "b@rtje.net",
+    url = "https://pypi.python.org/pypi/shark/0.0.1",
+    keywords = ["django", "bootstrap", "framework"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+Coming soon.
+"""
+)
