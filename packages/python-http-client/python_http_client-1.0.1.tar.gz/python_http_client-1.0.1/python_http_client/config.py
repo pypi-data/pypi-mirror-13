6613903e8674befbcf4501739b61d754ca9c9100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:55 +0000

    Add package python-http-client/1.0.1/python_http_client-1.0.1.tar.gz

---
+++ packages/python-http-client/python_http_client-1.0.1.tar.gz/python_http_client-1.0.1/python_http_client/config.py
@@ -0,0 +1,15 @@
+import os
+
+
+class Config(object):
+    """Allow variables assigned in .env available using
+       os.environ.get('VAR_NAME')"""
+    def __init__(self):
+        base_path = os.path.join(os.path.dirname(__file__), os.pardir)
+        if os.path.exists(base_path + '/.env'):
+            file = open(base_path + '/.env')
+            for line in file:
+                var = line.strip().split('=')
+                if len(var) == 2:
+                    os.environ[var[0]] = var[1]
+            file.close()
