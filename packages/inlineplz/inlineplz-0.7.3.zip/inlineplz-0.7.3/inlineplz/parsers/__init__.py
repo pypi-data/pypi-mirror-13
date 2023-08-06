65022cdd1b48fbca7e89c8926a9db3f1bc54a09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:38 +0000

    Add package inlineplz/0.7.3/inlineplz-0.7.3.zip

---
+++ packages/inlineplz/inlineplz-0.7.3.zip/inlineplz-0.7.3/inlineplz/parsers/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from inlineplz.parsers.prospector import ProspectorParser
+from inlineplz.parsers.eslint import ESLintParser
+from inlineplz.parsers.jscs import JSCSParser
+from inlineplz.parsers.jshint import JSHintParser
+from inlineplz.parsers.jsonlint import JSONLintParser
+from inlineplz.parsers.yamllint import YAMLLintParser
+from inlineplz.parsers.rstlint import RSTLintParser
+from inlineplz.parsers.markdownlint import MarkdownLintParser
+
+
+PARSERS = {
+    'prospector': ProspectorParser,
+    'eslint': ESLintParser,
+    'jshint': JSHintParser,
+    'jscs': JSCSParser,
+    'jsonlint': JSONLintParser,
+    'yamllint': YAMLLintParser,
+    'rstlint': RSTLintParser,
+    'markdownlint': MarkdownLintParser
+}
