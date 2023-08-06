cc3f16aa1bf1146f61a98387b27b837464f0d09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:54 +0000

    Add package schiene/0.14/schiene-0.14.tar.gz

---
+++ packages/schiene/schiene-0.14.tar.gz/schiene-0.14/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'schiene',
+  packages = ['schiene'],
+  version = '0.14',
+  license = 'MIT',
+  description = 'schiene is a Python library for interacting with Bahn.de',
+  author = 'Kevin Kennell',
+  author_email = 'kevin@fileperms.org',
+  install_requires=[
+        'requests>=2.7.0',
+        'beautifulsoup4>=4.4.0'
+  ],
+  url = 'https://github.com/kennell/schiene',
+  download_url = 'https://github.com/kennell/schiene/tarball/0.13',
+  keywords = ['bahn', 'api'],
+  classifiers = [],
+)
