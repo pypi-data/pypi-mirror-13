a58a7138530cc625928116a0f3417edecb615a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:19 +0000

    Add package FukuML/0.0.8/FukuML-0.0.8.tar.gz

---
+++ packages/FukuML/FukuML-0.0.8.tar.gz/FukuML-0.0.8/FukuML/__init__.py
@@ -0,0 +1,19 @@
+#encoding=utf8
+
+__version__ = '0.0.8'
+__all__ = [
+    'MLBase',
+    'PLA',
+    'PLA.BinaryClassifier',
+    'PocketPLA',
+    'PocketPLA.BinaryClassifier',
+    'LinearRegression',
+    'LinearRegression.LinearRegression',
+    'LinearRegression.BinaryClassifier',
+    'LinearRegression.Accelerator',
+    'LogisticRegression.LogisticRegression',
+    'LogisticRegression.BinaryClassifier',
+    'LogisticRegression.MultiClassifier',
+    'Utility',
+    'Utility.DatasetLoader'
+]
