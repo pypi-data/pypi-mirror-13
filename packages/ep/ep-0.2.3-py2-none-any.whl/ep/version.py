961916277e004d0acab7ceca652cdfb93ad9410a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:30 +0000

    Add package ep/0.2.3/ep-0.2.3-py2-none-any.whl

---
+++ packages/ep/ep-0.2.3-py2-none-any.whl/ep/version.py
@@ -0,0 +1,16 @@
+import semver
+
+
+def match(version, spec_string):
+    """
+    Match a version to a specification string.
+
+    :param spec_string:
+        A comma-separated list of semver matchers, e.g. ">=2.7.0,<3.0.0"
+
+    """
+    try:
+        specs = map(str.strip, spec_string.split(','))
+        return all(map(lambda s: semver.match(version, s), specs))
+    except:
+        return False
