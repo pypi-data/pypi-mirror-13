8b695baf3cc8a38863c7993c23a5f6f4182d20ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:12 +0000

    Add package Janson_lol/1.1.3/Janson_lol-1.1.3.zip

---
+++ packages/Janson_lol/Janson_lol-1.1.3.zip/Janson_lol-1.1.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Janson_lol',
+    version = '1.1.3',
+    py_modules = ['Janson_lol'],
+    author = 'Janson',
+    author_email = 'fish_eleven@126.com',
+    url = 'http://www.126.com',
+    description = 'My first python code',
+    )
