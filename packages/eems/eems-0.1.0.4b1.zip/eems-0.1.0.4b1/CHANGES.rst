2dd08f79e9bcef3eabae94729bd6dba72aad3917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:49 +0000

    Add package eems/0.1.0.4b1/eems-0.1.0.4b1.zip

---
+++ packages/eems/eems-0.1.0.4b1.zip/eems-0.1.0.4b1/CHANGES.rst
@@ -0,0 +1,43 @@
+=========
+Changelog
+=========
+
+All major versions including are listed below.
+
+0.1.0.4b1
+~~~~~~~~~
+
+- Beginners Guide (Documentation)
+- Removed parameter *check* in object *Temp()*
+- Added console short commands *-i* and *-d* for interval and duration
+- Small bug fixes
+
+0.1.0.3b1
+~~~~~~~~~
+
+- Add function to prepare system configurations for DS18B20 sensors
+- Bug fixes in eems console script
+
+0.1.0.2b1
+~~~~~~~~~
+
+- Changed main function names and adapted expected parameters
+- Documentation updated
+- Log file format changed from .log to .txt
+- Updated csv export file structure
+- Moved csv handling to separated module
+
+0.1.0.1b1
+~~~~~~~~~
+
+Open beta for testing issues.
+
+0.0.4.3a1
+~~~~~~~~~
+
+Closed alpha version.
+
+0.0.3.dev1
+~~~~~~~~~~
+
+Development version.
---
+++ packages/eems/eems-0.1.0.4b1.zip/eems-0.1.0.4b1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+from eems import __project__, __version__, __author__
+
+
+setup(
+    name=__project__,
+    version=__version__,
+    description='eems - easy energy monitoring system',
+    long_description='An easy application to establish an energy monitoring '
+                     'system for raspberry pi and ds18b20 temperature sensors.',
+    url='https://github.com/enricoba/eems',
+    author=__author__,
+    author_email='henrik.baran@online.de, a.hoehn@mailbox.org',
+    license='MIT',
+    platforms='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Education',
+        'Intended Audience :: Manufacturing',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='easy energy monitoring system raspberrry pi',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    package_data={
+        'eems': ['data/*.txt'],
+    },
+    entry_points={
+        'console_scripts': ['eems = eems.scripts:main']
+    }
+)
