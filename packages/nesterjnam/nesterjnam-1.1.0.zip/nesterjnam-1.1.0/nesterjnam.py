040ca40971905834556669216d89c24d09637868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:46 +0000

    Add package nesterjnam/1.1.0/nesterjnam-1.1.0.zip

---
+++ packages/nesterjnam/nesterjnam-1.1.0.zip/nesterjnam-1.1.0/nesterjnam.py
@@ -0,0 +1,12 @@
+'''파이썬 연습용 모듈 배포'''
+
+def print_lol(LIST,index):
+    '''리스트 내의 리스트 있을때 출력 함수
+    index인자 사용하여 들여쓰기 수준 결정'''
+    for in_item in LIST:
+        if isinstance(in_item,list):
+            print_lol(in_item,index+1)
+        else:
+            for i in range(index):
+                print('\t',end='')
+            print(in_item)
