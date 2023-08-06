924ee33c29bd8f6208192273b818d492200f1160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:15 +0000

    Add package cold-start-recommender/0.4.1/cold-start-recommender-0.4.1.tar.gz

---
+++ packages/cold-start-recommender/cold-start-recommender-0.4.1.tar.gz/cold-start-recommender-0.4.1/csrec/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.4.1'
+__author__ = 'elegans.io Ltd'
+__license__ = 'GPL v2'
+__maintainer__ = 'elegans-io'
+__email__ = 'info@elegans.io'
+
+from csrec.factory_dal import Dal
+from csrec.recommender import Recommender
