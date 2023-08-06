2109afae00690dcde4c54dff6ae7ce88875a1acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:19 +0000

    Add package gandi.cli/0.17/gandi.cli-0.17.tar.gz

---
+++ packages/gandi.cli/gandi.cli-0.17.tar.gz/gandi.cli-0.17/gandi/cli/commands/account.py
@@ -0,0 +1,17 @@
+""" Account namespace commands. """
+
+from gandi.cli.core.cli import cli
+from gandi.cli.core.utils import output_account
+from gandi.cli.core.params import pass_gandi
+
+
+@cli.command()
+@pass_gandi
+def info(gandi):
+    """Display information about hosting account.
+    """
+    output_keys = ['handle', 'credit']
+
+    account = gandi.account.all()
+    output_account(gandi, account, output_keys)
+    return account
