1ce0fea8ac9b073f15be2f1e016e0dd04a427d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:24 +0000

    Add package dark-matter/1.0.19/dark-matter-1.0.19.tar.gz

---
+++ packages/dark-matter/dark-matter-1.0.19.tar.gz/dark-matter-1.0.19/dark/database.py
@@ -0,0 +1,9 @@
+from mysql import connector
+
+from os import environ
+
+
+def getDatabaseConnection():
+    return connector.connect(
+        host='localhost', user=environ.get('DBI_USER', environ['USER']),
+        password=environ['DBI_PASSWORD'], database='ncbi_taxonomy')
