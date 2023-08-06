a7ead28356d3b135b2141b2aa02eb7c7d189ee96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:11 +0000

    Add package Flask-uWSGI-WebSocket/0.5.1/Flask-uWSGI-WebSocket-0.5.1.tar.gz

---
+++ packages/Flask-uWSGI-WebSocket/Flask-uWSGI-WebSocket-0.5.1.tar.gz/Flask-uWSGI-WebSocket-0.5.1/flask_uwsgi_websocket/__init__.py
@@ -0,0 +1,34 @@
+'''
+Flask-uWSGI-WebSocket
+---------------------
+High-performance WebSockets for your Flask apps powered by `uWSGI <http://uwsgi-docs.readthedocs.org/en/latest/>`_.
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '0.5.1'
+__license__ = 'MIT'
+__author__  = 'Zach Kelling'
+
+from ._uwsgi import uwsgi
+from .websocket import *
+from .async import *
+
+class GeventNotInstalled(Exception):
+    pass
+
+try:
+    from ._gevent import *
+except ImportError:
+    class GeventWebSocket(object):
+        def __init__(self, *args, **kwargs):
+            raise GeventNotInstalled("Gevent must be installed to use GeventWebSocket. Try: `pip install gevent`.")
+
+class AsyncioNotAvailable(Exception):
+    pass
+
+try:
+    from ._asyncio import *
+except ImportError:
+    class AsyncioWebSocket(object):
+        def __init__(self, *args, **kwargs):
+            raise AsyncioNotAvailable("Asyncio should be enabled at uwsgi compile time. Try: `UWSGI_PROFILE=asyncio pip install uwsgi`.")
