336d8bc4df9cee06c99b2e19d6d265d16dcf3a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:07 +0000

    Add package staffjoy/0.10/staffjoy-0.10.tar.gz

---
+++ packages/staffjoy/staffjoy-0.10.tar.gz/staffjoy-0.10/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="staffjoy",
+      packages=find_packages(),
+      version="0.10",
+      description="Staffjoy API Wrapper in Python",
+      author="Philip Thomas",
+      author_email="help@staffjoy.com",
+      license="MIT",
+      url="https://github.com/staffjoy/client_python",
+      download_url="https://github.com/StaffJoy/client_python/archive/0.10.tar.gz",
+      keywords=["staffjoy-api", "staffjoy", "staff joy"],
+      install_requires=["requests[security]"], )
