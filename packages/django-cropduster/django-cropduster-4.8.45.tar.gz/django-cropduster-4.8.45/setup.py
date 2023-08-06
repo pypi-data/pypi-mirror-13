75a190f113c4429b0278856761ade80157b6b34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:03 +0000

    Add package django-cropduster/4.8.45/django-cropduster-4.8.45.tar.gz

---
+++ packages/django-cropduster/django-cropduster-4.8.45.tar.gz/django-cropduster-4.8.45/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup_kwargs = {}
+
+try:
+    setup_kwargs['long_description'] = open('README.rst').read()
+except IOError:
+    pass
+
+setup(
+    name='django-cropduster',
+    version='4.8.45',
+    author='The Atlantic',
+    author_email='programmers@theatlantic.com',
+    url='https://github.com/theatlantic/django-cropduster',
+    description='Django image uploader and cropping tool',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Pillow',
+        'python-xmp-toolkit',
+        'django-generic-plus>=1.2.16',
+        'six>=1.7.0',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+    **setup_kwargs)
