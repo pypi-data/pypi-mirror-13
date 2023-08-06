49b721fd1726d6a5270a0fc28f44628a8f8bf0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:36 +0000

    Add package tulipgui-python/4.8.1/tulipgui_python-4.8.1-cp27-cp27m-win_amd64.whl

---
+++ packages/tulipgui-python/tulipgui_python-4.8.1-cp27-cp27m-win_amd64.whl/tulipgui_python-4.8.1.data/purelib/tulipogl/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import platform
+
+from tulip import *
+
+_tulipOglNativeLibsPath = os.path.dirname(__file__) + '/native/'
+
+sys.path.append(_tulipOglNativeLibsPath)
+
+if platform.system() == 'Windows':
+  os.environ['PATH'] = _tulipOglNativeLibsPath + ';' + os.environ['PATH']
+
+import _tulipogl
+
+sys.path.pop()
+
+class tlpogl(_tulipogl.tlpogl):
+  pass
+
+if not sys.argv[0] == 'tulip':
+  # when the tulipogl module is installed from the Python Packaging Index
+  # modify the TulipBitmapDir variable as it is different from the default one
+  bitmapDir = os.path.dirname(__file__) + '/share/bitmaps/'
+  if os.path.isdir(bitmapDir):
+    tlp.TulipBitmapDir = bitmapDir
+    tlp.TulipViewSettings.instance().setDefaultFontFile(tlp.TulipBitmapDir + 'font.ttf')
+
+tlp.loadTulipPluginsFromDir(_tulipOglNativeLibsPath + 'plugins')
+
+__all__ = ['tlpogl']
