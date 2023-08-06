b55feeef7ba865a6dd703830151f6e7af7d19ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:23 +0000

    Add package piep/0.8.0/piep-0.8.0.tar.gz

---
+++ packages/piep/piep-0.8.0.tar.gz/piep-0.8.0/README.rst
@@ -0,0 +1,26 @@
+piep
+====
+
+Bringing the power of python to stream editing
+----------------------------------------------
+
+| Zero install feed:
+| http://gfxmonk.net/dist/0install/piep.xml
+|
+| Online documentation:
+| http://gfxmonk.net/dist/doc/piep/
+|
+| Source Code / Issues:
+| https://github.com/gfxmonk/piep/
+|
+| Cheese shop entry:
+| http://pypi.python.org/pypi/piep
+
+HACKING
+-------
+
+Please add tests for any new functionality.
+
+Run the tests with ``make test``. If you don't want to do it that way (perhaps you don't
+have make or zero-install), it's basically ``nosetests --with-doctest --exe``, once you've
+made sure your local version of ``piep`` is on ``$PYTHONPATH``.
---
+++ packages/piep/piep-0.8.0.tar.gz/piep-0.8.0/test/test_helper.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import itertools
+import contextlib
+import shutil
+import tempfile
+import subprocess
+
+from piep import main
+
+def run(*args):
+	args = list(args)
+	stdin = itertools.imap(str, args.pop())
+	old_stdin = sys.stdin
+	try:
+		sys.stdin = stdin
+		return [str(line) for line in main.run(*main.parse_args(args))]
+	finally:
+		sys.stdin = old_stdin
+
+def run_full(*args):
+	args = list(args)
+	stdin = args.pop()
+	proc = subprocess.Popen(['python', main.__file__] + args, stdin=subprocess.PIPE,
+			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out, err = proc.communicate(stdin)
+	if proc.returncode != 0:
+		raise AssertionError("Command failed\nout: %s\nerr: %s" % (out, err))
+	return out
+
+
+@contextlib.contextmanager
+def cwd(path):
+	old_cwd = os.getcwd()
+	os.chdir(path)
+	try:
+		yield
+	finally:
+		os.chdir(old_cwd)
+
+@contextlib.contextmanager
+def temp_cwd():
+	path = tempfile.mkdtemp()
+	try:
+		with cwd(path):
+			yield
+	finally:
+		shutil.rmtree(path)
