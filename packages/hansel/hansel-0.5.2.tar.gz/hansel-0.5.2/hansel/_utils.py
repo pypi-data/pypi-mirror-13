15637c06c48cdef08e92459cddc9c6ef4ed2f89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:38 +0000

    Add package hansel/0.5.2/hansel-0.5.2.tar.gz

---
+++ packages/hansel/hansel-0.5.2.tar.gz/hansel-0.5.2/hansel/_utils.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Crumb manipulation utilities
+"""
+import os
+import os.path as op
+
+from   six import string_types
+
+
+def _dict_popitems(adict, **kwargs):
+    if not adict:
+        return
+
+    if not kwargs:
+        return
+
+    _ = [adict.pop(k) for k in kwargs if k in adict]
+
+
+def _get_path(crumb_path):
+    """ Return the path string from `crumb_path`.
+    Parameters
+    ----------
+    crumb_path: str or Crumb
+
+    Returns
+    -------
+    path: str
+    """
+    if hasattr(crumb_path, '_path'):
+        crumb_path = crumb_path._path
+
+    if not isinstance(crumb_path, string_types):
+        raise TypeError("Expected `crumb_path` to be a {}, got {}.".format(string_types, type(crumb_path)))
+
+    return crumb_path
+
+
+def _is_crumb_arg(crumb_arg, start_end_syms=('{', '}')):
+    """ Returns True if `crumb_arg` is a well formed crumb argument, i.e.,
+    is a string that starts with `start_sym` and ends with `end_sym`. False otherwise."""
+    if not isinstance(crumb_arg, string_types):
+        return False
+
+    start_sym, end_sym = start_end_syms
+    return crumb_arg.startswith(start_sym) and crumb_arg.endswith(end_sym)
+
+
+def _arg_params(arg, start_end_syms=('{', '}'), reg_sym=':'):
+    """ Return the name and the regex of the argument given its crumb representation.
+    Parameters
+    ----------
+    arg_crumb: str
+
+    start_end_syms: 2-tuple of str
+        The strings that indicate the start and end of a crumb argument
+
+    reg_sym: str
+        The string that separate the crumb argument name from the
+        crumb argument regular expression.
+
+    Returns
+    -------
+    arg_name: str
+
+    arg_regex: str
+    """
+    arg_content = _arg_content(arg, start_end_syms=start_end_syms)
+
+    if reg_sym in arg_content:
+        return tuple(arg_content.split(reg_sym))
+    else:
+        return arg_content, None
+
+
+def _arg_name(arg, start_end_syms=('{', '}'), reg_sym=':'):
+    """ Return the name of the argument given its crumb representation.
+    Parameters
+    ----------
+    arg_crumb: str
+
+    start_end_syms: 2-tuple of str
+        The strings that indicate the start and end of a crumb argument
+
+    reg_sym: str
+        The string that separate the crumb argument name from the
+        crumb argument regular expression.
+
+    Returns
+    -------
+    arg_name: str
+    """
+    arg_content = _arg_content(arg, start_end_syms=start_end_syms)
+
+    if reg_sym in arg_content:
+        return arg_content.split(reg_sym)[0]
+    else:
+        return arg_content
+
+
+def _arg_regex(arg, start_end_syms=('{', '}'), reg_sym=':'):
+    """ Return the name of the argument given its crumb representation.
+    Parameters
+    ----------
+    arg_crumb: str
+
+    start_end_syms: 2-tuple of str
+        The strings that indicate the start and end of a crumb argument
+
+    reg_sym: str
+        The string that separate the crumb argument name from the
+        crumb argument regular expression.
+
+    Returns
+    -------
+    arg_name: str
+    """
+    arg_content = _arg_content(arg, start_end_syms=start_end_syms)
+
+    if reg_sym in arg_content:
+        return arg_content.split(reg_sym)[1]
+    else:
+        return None
+
+
+def _arg_content(arg, start_end_syms=('{', '}')):
+    """ Return the name of the argument given its crumb representation.
+    Parameters
+    ----------
+    arg_crumb: str
+
+    start_end_syms: 2-tuple of str
+        The strings that indicate the start and end of a crumb argument
+
+    Returns
+    -------
+    arg_name: str
+    """
+    if not _is_crumb_arg(arg):
+        raise ValueError("Expected an well formed crumb argument, "
+                         "got {}.".format(arg))
+
+    start_sym, end_sym = start_end_syms
+    return arg[len(start_sym):-len(end_sym)]
+
+
+def _arg_format(arg_name, start_end_syms=('{', '}'), reg_sym=':', regex=None):
+    """ Return the crumb argument for its string `format()` representation.
+    Parameters
+    ----------
+    arg_name: str
+
+    Returns
+    -------
+    arg_format: str
+    """
+    start_sym, end_sym = start_end_syms
+
+    arg_fmt = start_sym + arg_name
+    if regex is not None:
+        arg_fmt += reg_sym + regex
+    arg_fmt += end_sym
+
+    return arg_fmt
+
+
+def is_valid(crumb_path, start_end_syms=('{', '}')):
+    """ Return True if `crumb_path` is a well formed path with crumb arguments,
+    False otherwise.
+    Parameters
+    ----------
+    crumb_path: str
+
+    Returns
+    -------
+    is_valid: bool
+    """
+    crumb_path = _get_path(crumb_path)
+
+    start_sym, end_sym = start_end_syms
+
+    splt = crumb_path.split(op.sep)
+    for crumb in splt:
+        if op.isdir(crumb):
+            continue
+
+        if _is_crumb_arg(crumb, start_end_syms=start_end_syms):
+            crumb = _arg_name(crumb, start_end_syms=start_end_syms)
+
+        if start_sym in crumb or end_sym in crumb:
+            return False
+
+    return True
+
+
+def has_crumbs(crumb_path, start_end_syms=('{', '}')):
+    """ Return True if the `crumb_path.split(op.sep)` has item which is a crumb argument
+    that starts with '{' and ends with '}'."""
+    crumb_path = _get_path(crumb_path)
+
+    splt = crumb_path.split(op.sep)
+    for i in splt:
+        if _is_crumb_arg(i, start_end_syms=start_end_syms):
+            return True
+
+    return False
+
+
+def _replace(crumb_path, start_end_syms=('{', '}'), regexes=None, **kwargs):
+    """ Return `crumb_path` where every crumb argument found in `kwargs` has been
+    replaced by the given value in `kwargs."""
+    if not kwargs:
+        return crumb_path
+
+    if regexes is None:
+        regexes = {}
+
+    for k in kwargs:
+        karg = _arg_format(k, start_end_syms=start_end_syms, regex=regexes.get(k, None))
+        if k not in crumb_path:
+            raise KeyError("Could not find argument {} in"
+                           " `path` {}.".format(k, crumb_path))
+
+        crumb_path = crumb_path.replace(karg, kwargs[k])
+
+    return crumb_path
+
+
+def _split(crumb_path, start_end_syms=('{', '}')):
+    """ Split `crumb_path` in two parts, the first is the base folder without any crumb argument
+        and the second is the rest of `crumb_path` beginning with the first crumb argument.
+        If `crumb_path` has no crumb arguments or starts with a crumb argument, return `crumb_path`.
+    """
+    crumb_path = _get_path(crumb_path)
+
+    if not has_crumbs(crumb_path, start_end_syms=start_end_syms):
+        return crumb_path
+
+    if not is_valid(crumb_path, start_end_syms=start_end_syms):
+        raise ValueError('Crumb path {} is not valid.'.format(crumb_path))
+
+    start_sym, _ = start_end_syms
+    if crumb_path.startswith(start_sym):
+        return crumb_path
+
+    idx = crumb_path.find(start_sym)
+    base = crumb_path[0:idx]
+    if base.endswith(op.sep):
+        base = base[:-1]
+
+    rest = crumb_path[idx:]
+
+    return base, rest
+
+
+def _touch(crumb_path, exist_ok=True, start_end_syms=('{', '}')):
+    """ Create a leaf directory and all intermediate ones
+    using the non crumbed part of `crumb_path`.
+    If the target directory already exists, raise an IOError
+    if exist_ok is False. Otherwise no exception is raised.
+    Parameters
+    ----------
+    crumb_path: str
+
+    exist_ok: bool
+        Default = True
+
+    Returns
+    -------
+    nupath: str
+        The new path created.
+    """
+    if has_crumbs(crumb_path, start_end_syms=start_end_syms):
+        nupath = _split(crumb_path, start_end_syms=start_end_syms)[0]
+    else:
+        nupath = crumb_path
+
+    if op.exists(nupath) and not exist_ok:
+        raise IOError("Folder {} already exists.".format(nupath))
+
+    try:
+        os.makedirs(nupath)
+    except:
+        raise
+    else:
+        return nupath
+
+
+def _split_exists(crumb_path, start_end_syms=('{', '}')):
+    """ Return True if the part without crumb arguments of `crumb_path`
+    is an existing path or a symlink, False otherwise.
+    Returns
+    -------
+    exists: bool
+    """
+    if has_crumbs(crumb_path):
+        rpath = _split(crumb_path, start_end_syms=start_end_syms)[0]
+    else:
+        rpath = str(crumb_path)
+
+    return op.exists(rpath) or op.islink(rpath)
---
+++ packages/hansel/hansel-0.5.2.tar.gz/hansel-0.5.2/hansel/utils.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Utilities to make crumbs
+"""
+import re
+import os
+import os.path as op
+import operator
+import fnmatch
+from   copy        import deepcopy
+from   collections import Mapping
+from   functools   import partial, reduce
+from   itertools   import product
+
+
+def remove_duplicates(lst):
+    """ Return a sorted lst of non-duplicated elements from `lst`.
+
+    Parameters
+    ----------
+    lst: sequence of any
+
+    Returns
+    -------
+    fslst:
+        Filtered and sorted `lst` with non duplicated elements of `lst`.
+    """
+    return sorted(list(set(lst)))
+
+
+def remove_ignored(ignore, strs):
+    """ Remove from `strs` the matches to the `fnmatch` (glob) patterns and
+    return the result in a list."""
+    nustrs = deepcopy(strs)
+    for ign in ignore:
+        nustrs = [item for item in nustrs if not fnmatch.fnmatch(item, ign)]
+
+    return nustrs
+
+
+def fnmatch_filter(pattern, items, *args):
+    """ Return the items from `items` that match the fnmatch expression in `pattern`.
+    Parameters
+    ----------
+    pattern: str
+        Regular expression
+
+    items: list of str
+        The items to be checked
+
+    args: ignored
+
+    Returns
+    -------
+    matches: list of str
+        Matched items
+    """
+    return [item for item in items if fnmatch.fnmatch(item, pattern)]
+
+
+def regex_match_filter(pattern, items, *args):
+    """ Return the items from `items` that match the regular expression in `pattern`.
+    Parameters
+    ----------
+    pattern: str
+        Regular expression
+
+    items: list of str
+        The items to be checked
+
+    args: re.compile arguments
+
+    Returns
+    -------
+    matches: list of str
+        Matched items
+    """
+    test = re.compile(pattern, *args)
+    return [s for s in items if test.match(s)]
+
+
+def list_children(path, just_dirs=False):
+    """ Return the immediate elements (files and folders) in `path`.
+    Parameters
+    ----------
+    path: str
+
+    just_dirs: bool
+        If True will return only folders.
+
+    ignore: sequence of str
+        Sequence of glob patterns to ignore from the listing.
+
+    re: str
+        Regular expression that the result items must match.
+
+    Returns
+    -------
+    paths: list of str
+    """
+    if not op.exists(path):
+        raise IOError("Expected an existing path, but could not"
+                      " find {}.".format(path))
+
+    if op.isfile(path):
+        if just_dirs:
+            vals = []
+        else:
+            vals = [path]
+    else:
+        if just_dirs: # this means we have to list only folders
+            vals = [d for d in os.listdir(path) if op.isdir(op.join(path, d))]
+        else:   # this means we have to list files
+            vals = os.listdir(path)
+
+    return vals
+
+
+def list_subpaths(path, just_dirs=False, ignore=None, pattern=None,
+                  filter_func=fnmatch_filter, filter_args=None):
+    """ Return the immediate elements (files and folders) in `path`.
+    Parameters
+    ----------
+    path: str
+
+    just_dirs: bool
+        If True will return only folders.
+
+    ignore: sequence of str
+        Sequence of glob patterns to ignore from the listing.
+
+    pattern: str
+        Regular expression that the result items must match.
+
+    filter_func: func
+        The function to match the patterns.
+        Must have as arguments: (pattern, paths) and return
+        a subset of paths.
+
+    filter_args: filter func arguments
+        Arguments for the filter function.
+
+    Returns
+    -------
+    paths: list of str
+    """
+    paths = list_children(path, just_dirs=just_dirs)
+
+    if ignore and ignore is not None:
+        paths = remove_ignored(ignore, paths)
+
+    if pattern and pattern is not None:
+        if filter_args is None:
+            filter_args = ()
+
+        paths = filter_func(pattern, paths, *filter_args)
+
+    return paths
+
+
+class ParameterGrid(object):
+    """
+    Picked from sklearn: https://github.com/scikit-learn/scikit-learn
+
+    Grid of parameters with a discrete number of values for each.
+    Can be used to iterate over parameter value combinations with the
+    Python built-in function iter.
+
+    Read more in the :ref:`User Guide <grid_search>`.
+    Parameters
+    ----------
+    param_grid : dict of string to sequence, or sequence of such
+        The parameter grid to explore, as a dictionary mapping estimator
+        parameters to sequences of allowed values.
+        An empty dict signifies default parameters.
+        A sequence of dicts signifies a sequence of grids to search, and is
+        useful to avoid exploring parameter combinations that make no sense
+        or have no effect. See the examples below.
+    Examples
+    --------
+    >>> from sklearn.grid_search import ParameterGrid
+    >>> param_grid = {'a': [1, 2], 'b': [True, False]}
+    >>> list(ParameterGrid(param_grid)) == (
+    ...    [{'a': 1, 'b': True}, {'a': 1, 'b': False},
+    ...     {'a': 2, 'b': True}, {'a': 2, 'b': False}])
+    True
+    >>> grid = [{'kernel': ['linear']}, {'kernel': ['rbf'], 'gamma': [1, 10]}]
+    >>> list(ParameterGrid(grid)) == [{'kernel': 'linear'},
+    ...                               {'kernel': 'rbf', 'gamma': 1},
+    ...                               {'kernel': 'rbf', 'gamma': 10}]
+    True
+    >>> ParameterGrid(grid)[1] == {'kernel': 'rbf', 'gamma': 1}
+    True
+    """
+
+    def __init__(self, param_grid):
+        if isinstance(param_grid, Mapping):
+            # wrap dictionary in a singleton list to support either dict
+            # or list of dicts
+            param_grid = [param_grid]
+        self.param_grid = param_grid
+
+    def __iter__(self):
+        """Iterate over the points in the grid.
+        Returns
+        -------
+        params : iterator over dict of string to any
+            Yields dictionaries mapping each estimator parameter to one of its
+            allowed values.
+        """
+        for p in self.param_grid:
+            # Always sort the keys of a dictionary, for reproducibility
+            items = sorted(p.items())
+            if not items:
+                yield {}
+            else:
+                keys, values = zip(*items)
+                for v in product(*values):
+                    params = dict(zip(keys, v))
+                    yield params
+
+    def __len__(self):
+        """Number of points on the grid."""
+        # Product function that can handle iterables (np.product can't).
+        product = partial(reduce, operator.mul)
+        return sum(product(len(v) for v in p.values()) if p else 1
+                   for p in self.param_grid)
+
+    def __getitem__(self, ind):
+        """Get the parameters that would be ``ind``th in iteration
+        Parameters
+        ----------
+        ind : int
+            The iteration index
+        Returns
+        -------
+        params : dict of string to any
+            Equal to list(self)[ind]
+        """
+        # This is used to make discrete sampling without replacement memory
+        # efficient.
+        for sub_grid in self.param_grid:
+            # XXX: could memoize information used here
+            if not sub_grid:
+                if ind == 0:
+                    return {}
+                else:
+                    ind -= 1
+                    continue
+
+            # Reverse so most frequent cycling parameter comes first
+            keys, values_lists = zip(*sorted(sub_grid.items())[::-1])
+            sizes = [len(v_list) for v_list in values_lists]
+            product = partial(reduce, operator.mul)
+            total = product(sizes)
+
+            if ind >= total:
+                # Try the next grid
+                ind -= total
+            else:
+                out = {}
+                for key, v_list, n in zip(keys, values_lists, sizes):
+                    ind, offset = divmod(ind, n)
+                    out[key] = v_list[offset]
+                return out
+
+        raise IndexError('ParameterGrid index out of range')
