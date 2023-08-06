a6a139899a3d8ed2b7fb9a1c84af8260f7625183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:16 +0000

    Add package buckingham/0.2/buckingham-0.2.tar.gz

---
+++ packages/buckingham/buckingham-0.2.tar.gz/buckingham-0.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='buckingham',
+      version='0.2',
+      description='A library for unit conversion and error propagation',
+      author='Massimo DiPierro',
+      author_email='massimo.dipierro@gmail.com',
+      long_description=open("README.md").read(),
+      url='https://github.com/mdipierro/buckingham',
+      install_requires=[],
+      py_modules=["buckingham"],
+      license= 'BSD',
+      package_data = {'': ['README.md']},
+      keywords='unit conversion and error propagation',
+      )
