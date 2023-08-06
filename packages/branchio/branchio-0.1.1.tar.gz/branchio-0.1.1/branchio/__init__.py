d8c640f1dea415d2b33c5885f5758c36d31e91e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:01 +0000

    Add package branchio/0.1.1/branchio-0.1.1.tar.gz

---
+++ packages/branchio/branchio-0.1.1.tar.gz/branchio-0.1.1/branchio/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+# BranchIo
+# Copyright Headquarters HQ
+# See LICENSE for details.
+
+"""
+Branch.io Api Library
+"""
+__version__ = '0.1.0'
+__author__ = 'Eric Chapman'
+__license__ = 'MIT'
+
+from .client import Client
+from .defines import *
