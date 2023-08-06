3f1888a398b00fa59d769af2f7922c1e27ad6943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:25 +0000

    Add package geeksdownloader/1.0/geeksdownloader-1.0.tar.gz

---
+++ packages/geeksdownloader/geeksdownloader-1.0.tar.gz/geeksdownloader-1.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="geeksdownloader",
+	version="1.0",
+	description="Download all GeeksforGeeks articles/posts of a particular topic/company within a minute",
+	url="https://github.com/madhuradlakha/GeeksforGeeks_Downloader",
+	author="Madhur Adlakha",
+	author_email="madhuradlakha@yahoo.co.in",
+	license='MIT',
+	packages=["geeksdownloader"],
+	scripts=["bin/geeksdownloader"],
+	zip_safe=False)
