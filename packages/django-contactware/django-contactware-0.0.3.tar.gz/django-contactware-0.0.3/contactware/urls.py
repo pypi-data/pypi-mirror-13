6ab08b597a012a0ba72242872510ebe529f91d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:13 +0000

    Add package django-contactware/0.0.3/django-contactware-0.0.3.tar.gz

---
+++ packages/django-contactware/django-contactware-0.0.3.tar.gz/django-contactware-0.0.3/contactware/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls import patterns
+
+from .views import *
+
+
+urlpatterns = [
+
+    url(
+        r'sent$',
+        ContactMessageSentView.as_view(),
+        name='contactware_confirmation_view',
+    ),
+
+    url(
+        r'',
+        ContactFormView.as_view(),
+        name='contactware_form_view',
+    ),
+
+]
