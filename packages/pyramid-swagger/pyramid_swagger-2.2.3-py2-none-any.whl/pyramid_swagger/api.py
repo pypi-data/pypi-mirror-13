6180c80103277c0c404b96681fe890568a4db5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:35 +0000

    Add package pyramid-swagger/2.2.3/pyramid_swagger-2.2.3-py2-none-any.whl

---
+++ packages/pyramid-swagger/pyramid_swagger-2.2.3-py2-none-any.whl/pyramid_swagger/api.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Module for automatically serving /api-docs* via Pyramid.
+"""
+import simplejson
+
+from pyramid_swagger.model import PyramidEndpoint
+
+
+# TODO: document that this is now a public interface
+def register_api_doc_endpoints(config, endpoints, base_path='/api-docs'):
+    """Create and register pyramid endpoints to service swagger api docs.
+    Routes and views will be registered on the `config` at `path`.
+
+    :param config: a pyramid configuration to register the new views and routes
+    :type  config: :class:`pyramid.config.Configurator`
+    :param endpoints: a list of endpoints to register as routes and views
+    :type  endpoints: a list of :class:`pyramid_swagger.model.PyramidEndpoint`
+    :param base_path: the base path used to register api doc endpoints.
+        Defaults to `/api-docs`.
+    :type  base_path: string
+    """
+    for endpoint in endpoints:
+        path = base_path.rstrip('/') + endpoint.path
+        config.add_route(endpoint.route_name, path)
+        config.add_view(
+            endpoint.view,
+            route_name=endpoint.route_name,
+            renderer=endpoint.renderer)
+
+
+def build_swagger_12_endpoints(resource_listing, api_declarations):
+    """
+    :param resource_listing: JSON representing a Swagger 1.2 resource listing
+    :type resource_listing: dict
+    :param api_declarations: JSON representing Swagger 1.2 api declarations
+    :type api_declarations: dict
+    :rtype: iterable of :class:`pyramid_swagger.model.PyramidEndpoint`
+    """
+    yield build_swagger_12_resource_listing(resource_listing)
+
+    for name, filepath in api_declarations.items():
+        with open(filepath) as input_file:
+            yield build_swagger_12_api_declaration(
+                name, simplejson.load(input_file))
+
+
+def build_swagger_12_resource_listing(resource_listing):
+    """
+    :param resource_listing: JSON representing a Swagger 1.2 resource listing
+    :type resource_listing: dict
+    :rtype: :class:`pyramid_swagger.model.PyramidEndpoint`
+    """
+    def view_for_resource_listing(request):
+        # Thanks to the magic of closures, this means we gracefully return JSON
+        # without file IO at request time.
+        return resource_listing
+
+    return PyramidEndpoint(
+        path='',
+        route_name='pyramid_swagger.swagger12.api_docs',
+        view=view_for_resource_listing,
+        renderer='json')
+
+
+def build_swagger_12_api_declaration(resource_name, api_declaration):
+    """
+    :param resource_name: The `path` parameter from the resource listing for
+        this resource.
+    :type resource_name: string
+    :param api_declaration: JSON representing a Swagger 1.2 api declaration
+    :type api_declaration: dict
+    :rtype: :class:`pyramid_swagger.model.PyramidEndpoint`
+    """
+    # NOTE: This means our resource paths are currently constrained to be valid
+    # pyramid routes! (minus the leading /)
+    route_name = 'pyramid_swagger.swagger12.apidocs-{0}'.format(resource_name)
+    return PyramidEndpoint(
+        path='/{0}'.format(resource_name),
+        route_name=route_name,
+        view=build_swagger_12_api_declaration_view(api_declaration),
+        renderer='json')
+
+
+def build_swagger_12_api_declaration_view(api_declaration_json):
+    """Thanks to the magic of closures, this means we gracefully return JSON
+    without file IO at request time.
+    """
+    def view_for_api_declaration(request):
+        # Note that we rewrite basePath to always point at this server's root.
+        return dict(
+            api_declaration_json,
+            basePath=str(request.application_url),
+        )
+    return view_for_api_declaration
+
+
+def build_swagger_20_swagger_dot_json(config):
+
+    def view_for_swagger_dot_json(request):
+        spec = config.registry.settings['pyramid_swagger.schema20']
+        return spec.spec_dict
+
+    return PyramidEndpoint(
+        path='/swagger.json',
+        route_name='pyramid_swagger.swagger20.api_docs',
+        view=view_for_swagger_dot_json,
+        renderer='json')
