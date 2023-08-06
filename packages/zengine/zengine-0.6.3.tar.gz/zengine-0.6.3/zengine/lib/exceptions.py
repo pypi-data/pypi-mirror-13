10f53dfa14c01713b3c41fda241cd4cb4d2fc192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:07 +0000

    Add package zengine/0.6.3/zengine-0.6.3.tar.gz

---
+++ packages/zengine/zengine-0.6.3.tar.gz/zengine-0.6.3/zengine/lib/exceptions.py
@@ -0,0 +1,34 @@
+from falcon.errors import *
+
+
+
+class SuspiciousOperation(Exception):
+    """The user did something suspicious"""
+
+
+class SuspiciousMultipartForm(SuspiciousOperation):
+    """Suspect MIME request in multipart form data"""
+    pass
+
+
+class SuspiciousFileOperation(SuspiciousOperation):
+    """A Suspicious filesystem operation was attempted"""
+    pass
+
+
+class DisallowedHost(SuspiciousOperation):
+    """HTTP_HOST header contains invalid value"""
+    pass
+
+
+class PermissionDenied(Exception):
+    """The user did not have permission to do that"""
+    pass
+
+
+class ViewDoesNotExist(Exception):
+    """The requested view does not exist"""
+    pass
+
+class ZengineError(Exception):
+    pass
---
+++ packages/zengine/zengine-0.6.3.tar.gz/zengine-0.6.3/zengine/views/auth.py
@@ -0,0 +1,72 @@
+# -*-  coding: utf-8 -*-
+"""Authentication views"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+import falcon
+
+from pyoko import fields
+from zengine.forms.json_form import JsonForm
+from zengine.views.base import SimpleView
+
+
+class LoginForm(JsonForm):
+    """
+    Simple login form
+    """
+    username = fields.String("Username")
+    password = fields.String("Password", type="password")
+
+
+def logout(current):
+    """
+    Log out view.
+    Simply deletes the session object
+
+    Args:
+        current: :attr:`~zengine.engine.WFCurrent` object.
+    """
+    current.session.delete()
+
+
+def dashboard(current):
+    """
+    Dashboard view. Not implemented yet!!!
+
+    Args:
+        current: :attr:`~zengine.engine.WFCurrent` object.
+    """
+    current.output["msg"] = "Success"
+
+
+class Login(SimpleView):
+    """
+    Class based login view.
+    Displays login form at ``show`` stage,
+    does the authentication at ``do`` stage.
+    """
+    def do_view(self):
+        """
+        Authenticate user with given credentials.
+        """
+        try:
+            auth_result = self.current.auth.authenticate(
+                self.current.input['username'],
+                self.current.input['password'])
+            self.current.task_data['login_successful'] = auth_result
+        except:
+            self.current.log.exception("Wrong username or another error occurred")
+            self.current.task_data['login_successful'] = False
+        if not self.current.task_data['login_successful']:
+            self.current.response.status = falcon.HTTP_403
+
+    def show_view(self):
+        """
+        Show :attr:`LoginForm` form.
+        """
+        self.current.output['forms'] = LoginForm(current=self.current).serialize()
+
+
+
