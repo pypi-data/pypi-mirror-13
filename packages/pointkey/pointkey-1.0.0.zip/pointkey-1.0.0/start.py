ae913339739ae0d99c6b5a02057d7f897ad40412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package pointkey/1.0.0/pointkey-1.0.0.zip

---
+++ packages/pointkey/pointkey-1.0.0.zip/pointkey-1.0.0/start.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name            = 'pointkey',
+    version         = '1.0.0',
+    py_modules      = ['dotkey'],
+    author          = 'choi',
+    author_email    = 'choi@gmail.com',
+    url             = '',
+    description     = 'A simple printer of nested lists',
+)
+
