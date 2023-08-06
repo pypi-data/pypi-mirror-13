283c7abc85a04848a6ccfaa8e3028e15d95dd030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:36 +0000

    Add package DjamBase/1.0.13/DjamBase-1.0.13.tar.gz

---
+++ packages/DjamBase/DjamBase-1.0.13.tar.gz/DjamBase-1.0.13/setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+
+packages = ["DjamBase", "DjamBase.packages.requests"]
+
+long_description = open("README.txt").read()
+
+setup(
+    name = "DjamBase",
+    packages = packages,
+    version = "1.0.13",
+    author = "Eric James Foster",
+    author_email = "maniphestival@gmail.com",
+    description = "A Thin JamBase API Client library for Django/Python Applications.",
+    requires = ["requests (>=2.9.1)"],
+    license = "MIT",
+    url = "https://pypi.python.org/pypi/DjamBase",
+    keywords = "JamBase search event events concert concerts music bands artists HTTP query api festival list",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        ],
+    long_description = long_description
+)
