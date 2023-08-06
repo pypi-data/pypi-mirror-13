b62b853d413143ccedf932c109cce445a7b44cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package geolang/0.1.2/geolang-0.1.2.tar.gz

---
+++ packages/geolang/geolang-0.1.2.tar.gz/geolang-0.1.2/geolang/__init__.py
@@ -0,0 +1,17 @@
+from geolang.geolang import (
+					__author__,
+					__version__,
+					_KA_ALPHABET,
+					_LAT_ALPHABET,
+					KA2KA,
+					LAT2LAT,
+					KA2LAT,
+					LAT2KA,
+					UNI2LAT,
+					_2KA,
+					_2LAT,
+					encode_slugify,
+					GeoLangToolKit,
+				)
+# from geolang.geolang import *
+from .unicode import unicode
