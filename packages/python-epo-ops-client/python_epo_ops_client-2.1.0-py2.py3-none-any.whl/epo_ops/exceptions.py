b528a46c5a6103107719eb3257b3b431a5db4222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:45 +0000

    Add package python-epo-ops-client/2.1.0/python_epo_ops_client-2.1.0-py2.py3-none-any.whl

---
+++ packages/python-epo-ops-client/python_epo_ops_client-2.1.0-py2.py3-none-any.whl/epo_ops/exceptions.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from requests.exceptions import HTTPError
+
+log = logging.getLogger(__name__)
+
+
+# Query errors
+class InvalidDate(ValueError):
+    """User used an invalid date."""
+
+
+class MissingRequiredValue(ValueError):
+    """User did not supply a required value."""
+
+
+# Number service error
+class InvalidNumberConversion(ValueError):
+    """Invalid number conversion request."""
+
+
+# OPS quota errors
+class AnonymousQuotaPerMinuteExceeded(HTTPError):
+    """Anonymous per minute quota exceeded."""
+
+
+class AnonymousQuotaPerDayExceeded(HTTPError):
+    """Anonymous per day quota exceeded."""
+
+
+class IndividualQuotaPerHourExceeded(HTTPError):
+    """Anonymous per day quota exceeded."""
+
+
+class RegisteredQuotaPerWeekExceeded(HTTPError):
+    """Anonymous per day quota exceeded."""
