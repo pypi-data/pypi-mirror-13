70b281e5742dc8c386ef7e471b28fb1a747149cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:59 +0000

    Add package supra/1.0.0/supra-1.0.0.tar.gz

---
+++ packages/supra/supra-1.0.0.tar.gz/supra-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+# version (shape.function.path)
+setup(
+	name='supra',
+	version='1.0.0',
+	py_modules=['supra'],
+	url='https://github.com/luismoralesp/supra',
+	author='Luis Miguel Morales Pajaro',
+	author_email='luismiguel.mopa@gmail.com'
+)
