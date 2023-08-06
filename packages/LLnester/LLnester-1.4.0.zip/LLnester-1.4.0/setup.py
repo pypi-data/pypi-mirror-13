fb5f7e8b69872d1781b18818c69cf122d72fbf45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:04 +0000

    Add package LLnester/1.4.0/LLnester-1.4.0.zip

---
+++ packages/LLnester/LLnester-1.4.0.zip/LLnester-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'LLnester',                                #当前发布的名称 
+    version = '1.4.0',                                #模块版本
+    py_modules = ['llnester'],                        #对应模块的名称
+    author = 'CC',                                    #模块的作者
+    author_email = 'chengchao.24@163.com',            #作者的email
+    url = 'chengchao.24@163.com',                     #作者的个人首页
+    description = 'A simple printer of the txt file and format the txt file ', #模块功能描述
+    )
