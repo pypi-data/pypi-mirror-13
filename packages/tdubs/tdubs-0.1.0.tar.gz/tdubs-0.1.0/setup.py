2db15617390f5862335c36b4188dcb47d4cbe68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:45 +0000

    Add package tdubs/0.1.0/tdubs-0.1.0.tar.gz

---
+++ packages/tdubs/tdubs-0.1.0.tar.gz/tdubs-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tdubs',
+    version='0.1.0',
+    url='https://github.com/blaix/tdubs',
+    author='Justin Blake',
+    author_email='justin@blaix.com',
+    description='A test double library',
+    packages=find_packages(),
+    license='MIT (Expat)',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Testing',
+    ],
+)
