243b0af7eee8124bc5c35b8f31a30e714389749c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package nester_wing/1.0.0/nester_wing-1.0.0.zip

---
+++ packages/nester_wing/nester_wing-1.0.0.zip/nester_wing-1.0.0/nester_wing.py
@@ -0,0 +1,10 @@
+﻿#这是"nester_wing.py"模块
+#提供了一个 print_lol() 函数,
+#这个函数的作用是打印列表,
+#其中有可能包含嵌套列表
+def print_lol(the_list):
+  for each_outher in the_list:
+    if isinstance(each_outher,list):
+      print_lol(each_outher)
+    else:
+      print(each_outher)
---
+++ packages/nester_wing/nester_wing-1.0.0.zip/nester_wing-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='nester_wing',
+  version='1.0.0',
+  py_modules=['nester_wing'],
+  author='hfpython',
+  author_email='hfpython@headfirstlads.com',
+  url='http://www.headfirstlads.com',
+  description='A simple printer of nested lists',)
