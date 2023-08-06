c8e5178f3775cab402e08e58aa5699626827b63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:11 +0000

    Add package piscan/0.1.47/piscan-0.1.47.tar.gz

---
+++ packages/piscan/piscan-0.1.47.tar.gz/piscan-0.1.47/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='piscan',
+      version='0.1.47',
+      description='RXWave.com Uniden DMA Police Scanner RaspberryPi agent',
+      url='http://www.rxwave.com',
+      author='Dave Kolberg',
+      author_email='dave.kolberg@gmail.com',
+      license='MIT',
+      packages=['piscan'],
+      install_requires=['pika','boto','flask'],
+      zip_safe=False,
+      include_package_data=True,
+      package_dir={'piscan': 'piscan/data'},
+      package_data={
+        '' : ['setup.sh'],
+        'data' : ['*'],
+      },
+
+)
+
+
