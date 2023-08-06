285e48bf7e8976db94126d4a8ced1f777ac88cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:39 +0000

    Add package kprint/1.0.0/kprint-1.0.0.zip

---
+++ packages/kprint/kprint-1.0.0.zip/kprint-1.0.0/kprint.py
@@ -0,0 +1,4 @@
+def printList(datas):
+    if(isinstance(datas,list)):
+        for item in datas:
+            print(item)
---
+++ packages/kprint/kprint-1.0.0.zip/kprint-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = "kprint",
+    version = "1.0.0",
+    py_modules=["kprint"],
+    author="konghao",
+    author_email="konghao@qq.com",
+    url="http://www.konghao.org",
+    description="test print list"
+)
