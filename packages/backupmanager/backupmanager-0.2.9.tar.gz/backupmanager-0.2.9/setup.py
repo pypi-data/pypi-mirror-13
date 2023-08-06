0c916acfde56e1ee47ad99459fcc020dcb8f5f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:50 +0000

    Add package backupmanager/0.2.9/backupmanager-0.2.9.tar.gz

---
+++ packages/backupmanager/backupmanager-0.2.9.tar.gz/backupmanager-0.2.9/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+        name='backupmanager',
+        version='0.2.9',
+        packages=['backupmanager', 'backupmanager.tools'],
+        url='https://github.com/MartijnBraam/backupmanager',
+        license='MIT',
+        author='Martijn Braam',
+        author_email='martijn@brixit.nl',
+        description='A wrapper to unify configuration/running backup tools',
+        install_requires=[
+            'humanize',
+            'plumbum',
+            'PyYAML',
+            'tabulate'
+        ],
+        entry_points={
+            'console_scripts': [
+                'backupmanager = backupmanager.__main__:main'
+            ]
+        },
+        include_package_data=True,
+        package_data={
+            '': ['*.dist', '*.service', '*.timer']
+        },
+        keywords=["backup", "linux", "manager"],
+        classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Development Status :: 4 - Beta",
+            "Operating System :: POSIX :: Linux",
+            "License :: OSI Approved :: MIT License"
+        ],
+)
