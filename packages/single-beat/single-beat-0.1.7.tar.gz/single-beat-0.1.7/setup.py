5a761d67fdb369ed43c578a5863f6de2b66637a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:26 +0000

    Add package single-beat/0.1.7/single-beat-0.1.7.tar.gz

---
+++ packages/single-beat/single-beat-0.1.7.tar.gz/single-beat-0.1.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='single-beat',
+    version='0.1.7',
+    long_description=__doc__,
+    description='ensures only one instance of your process across your servers',
+    url='https://github.com/ybrs/single-beat',
+    packages=['singlebeat'],
+    zip_safe=True,
+    install_requires=[
+        'pyuv >= 0.10,<1.0.0',
+        'redis >= 2.9.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'single-beat = singlebeat.beat:run_process',
+        ],
+    }
+)
