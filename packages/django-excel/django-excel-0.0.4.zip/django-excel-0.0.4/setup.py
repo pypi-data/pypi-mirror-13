4446c6a7223b3cd7a0ba5edf51da0217bc26fb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:06 +0000

    Add package django-excel/0.0.4/django-excel-0.0.4.zip

---
+++ packages/django-excel/django-excel-0.0.4.zip/django-excel-0.0.4/setup.py
@@ -0,0 +1,67 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+with open("README.rst", 'r') as readme:
+    README_txt = readme.read()
+
+dependencies = [
+    'pyexcel>=0.2.0',
+    'pyexcel-webio>=0.0.6',
+    'Django>=1.7.1',
+]
+
+extras = {
+    'xls': ['pyexcel-xls>=0.1.0'],
+    'xlsx': ['pyexcel-xlsx>=0.1.0'],
+    'ods': ['pyexcel-ods3>=0.1.0'],
+}
+
+
+setup(
+    name='django-excel',
+    author='C. W.',
+    version='0.0.4',
+    author_email='wangc_2011 (at) hotmail.com',
+    url='https://github.com/pyexcel/django-excel',
+    description='A django middleware that provides one application programming interface to read and write data in different excel file formats',
+    install_requires=dependencies,
+    extras_require=extras,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    long_description=README_txt,
+    zip_safe=False,
+    tests_require=['nose'],
+    keywords=[
+        'excel',
+        'python',
+        'pyexcel',
+        'API',
+        'Django'
+    ],
+    license='New BSD',
+    classifiers=[
+        'Topic :: Office/Business',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
