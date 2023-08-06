2450eeb3d2d3e3e6bb6f8051e7ffafc82acd4201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:32 +0000

    Add package GDM/0.8.2/GDM-0.8.2.tar.gz

---
+++ packages/GDM/GDM-0.8.2.tar.gz/GDM-0.8.2/gdm/__init__.py
@@ -0,0 +1,27 @@
+"""Package for GDM."""
+
+import sys
+
+__project__ = 'GDM'
+__version__ = '0.8.2'
+
+CLI = 'gdm'
+PLUGIN = 'deps'
+NAME = "Git Dependency Manager"
+VERSION = __project__ + ' v' + __version__
+DESCRIPTION = "A language-agnostic \"dependency manager\" using Git."
+
+PYTHON_VERSION = 3, 4
+
+if sys.version_info < PYTHON_VERSION:  # pragma: no cover (manual test)
+    exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
+
+try:
+    # pylint: disable=wrong-import-position
+    from .commands import install
+    from .commands import update
+    from .commands import display as list  # pylint: disable=redefined-builtin
+    from .commands import lock
+    from .commands import delete as uninstall
+except ImportError:  # pragma: no cover (manual test)
+    pass
