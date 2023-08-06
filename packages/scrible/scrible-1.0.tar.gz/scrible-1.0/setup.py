032542c7ef600952c03a7b0eec4d82e779a9be1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:16 +0000

    Add package scrible/1.0/scrible-1.0.tar.gz

---
+++ packages/scrible/scrible-1.0.tar.gz/scrible-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='scrible',
+      version='1.0',
+      long_description=readme(),
+      description='Note taking application',
+      url='https://github.com/jaxtreme01/Scrible',
+      author='Jack Mwangi',
+      author_email='jackmwa94@gmail.com',
+      keywords='notes taking app scrible scribbler scribble',
+      license='MIT',
+      entry_points={
+          'console_scripts': ['scrible = scrible.notes.scrible:main'],
+      },
+      download_url = 'https://github.com/jaxtreme01/Scrible/tarball/0.1',
+      packages=['scrible'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=['docopt', 'clint', 'requests', 'python-firebase'
+                        'colorama','pyfiglet','termcolor'],
+      include_package_data=True,
+      zip_safe=False)
+
