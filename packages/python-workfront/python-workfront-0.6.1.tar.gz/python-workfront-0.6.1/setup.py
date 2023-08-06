8561fae6d0e92b44499420b94ba13cb2c3477f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package python-workfront/0.6.1/python-workfront-0.6.1.tar.gz

---
+++ packages/python-workfront/python-workfront-0.6.1.tar.gz/python-workfront-0.6.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-workfront',
+    version='0.6.1',
+    author='Chris Withers',
+    author_email='chris@withers.org',
+    license='MIT',
+    description="Python library for accessing the Workfront REST api",
+    long_description="Python library for accessing the Workfront REST api",
+    url='http://workfront.readthedocs.org',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    extras_require=dict(
+        test=['nose', 'nose-cov', 'testfixtures', 'mock', 'coveralls'],
+        build=['sphinx', 'pkginfo', 'setuptools-git']
+    )
+)
---
+++ packages/python-workfront/python-workfront-0.6.1.tar.gz/python-workfront-0.6.1/tests/helpers.py
@@ -0,0 +1,112 @@
+import json
+from io import StringIO
+from workfront.six import text_type, string_types
+from workfront.six.moves.urllib.error import HTTPError
+from workfront.six.moves.urllib.parse import parse_qs
+from workfront.session import  PY34
+
+from testfixtures import compare, Replacer
+
+from workfront import Session
+from workfront.meta import APIVersion, Object, Field
+
+
+class MockResponse(StringIO):
+    def __init__(self, content, code):
+        super(MockResponse, self).__init__(text_type(content))
+        self.code = code
+
+
+class MockHTTPError(HTTPError):
+    def __init__(self, content, code):
+        self.read = StringIO(text_type(content)).read
+        self.code = code
+
+
+class MockOpen(dict):
+
+    added = 0
+    calls = 0
+
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def decode(self, params):
+        if isinstance(params, bytes):
+            params = params.decode('utf-8')
+        bits = []
+        for key, value in parse_qs(params).items():
+            if isinstance(value, list):
+                if len(value) == 1:
+                    value = value[0]
+                else:
+                    value = tuple(value)
+            if isinstance(value, string_types) and value[0]=='{':
+                value = json.dumps(json.loads(value), sort_keys=True)
+            bits.append((key, value))
+        return tuple(sorted(bits))
+
+    def __call__(self, request, context=None):
+        params = self.decode(request.data)
+        url = request.get_full_url()
+        key = self.calls, url, params, context
+        try:
+            response, code = self[key]
+        except KeyError:  # pragma: no cover
+            parts = []
+            for expected in sorted(self.keys()):
+                parts.append(compare(expected, actual=key, raises=False))
+            if parts:
+                raise AssertionError('\n'.join(parts))
+            else:
+                raise KeyError(key)
+
+        self.calls += 1
+        if isinstance(response, Exception):
+            raise response
+        return MockResponse(response, code)
+
+    def add(self, url, response, params='', code=200, ssl_context=None):
+        # Python 3.4 doesn't support ssl contexts, so just ignore them:
+        if PY34:
+            ssl_context = None
+
+        if not url.startswith('http'):
+            url = self.base_url + url
+        self[self.added,
+             url,
+             self.decode(params),
+             ssl_context] = response, code
+        self.added += 1
+
+    def assert_called(self, times):
+        compare(self.added, expected=times)
+
+
+class MockOpenHelper(object):
+
+    base = 'https://test.attask-ondemand.com/attask/api/unsupported'
+
+    def setUp(self):
+        self.replace = Replacer()
+        self.addCleanup(self.replace.restore)
+        self.server = MockOpen(self.base)
+        self.replace('workfront.six.moves.urllib.request.urlopen', self.server)
+
+
+class TestObjectHelper(MockOpenHelper):
+
+    base = 'https://test.attask-ondemand.com/attask/api/test'
+
+    def setUp(self):
+        super(TestObjectHelper, self).setUp()
+        self.replace('workfront.session.Session.version_registry', {})
+        test_api = APIVersion('test')
+        class TestObject(Object):
+            code='TEST'
+            field_one = Field('fieldOne')
+            field_two = Field('fieldTwo')
+        test_api.register(TestObject)
+        Session.register(test_api)
+        self.session = Session('test', api_version='test')
+        self.api = self.session.api
