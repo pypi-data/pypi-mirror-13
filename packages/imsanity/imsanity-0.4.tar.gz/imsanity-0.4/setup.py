86cd9813121652f81b3ac673e5add30b57f49ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:06 +0000

    Add package imsanity/0.4/imsanity-0.4.tar.gz

---
+++ packages/imsanity/imsanity-0.4.tar.gz/imsanity-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='imsanity',
+    packages=['imsanity'],  # this must be the same as the name above
+    version='0.4',
+    description='Import sanity. Adds the first parent directory containing .imsanity to PYTHONPATH.',
+    author='Vadim Graboys',
+    author_email='dimva13@gmail.com',
+    url='https://github.com/vadimg/imsanity',
+    download_url='https://github.com/vadimg/imsanity/tarball/0.4',
+    keywords=['import', 'sanity', 'imsanity'],
+    classifiers=[],
+)
