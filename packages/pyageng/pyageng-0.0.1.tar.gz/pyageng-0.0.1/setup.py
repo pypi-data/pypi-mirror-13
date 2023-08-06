238fe3793e58f6ee6cfbfc4d7f55562c14ad9664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:53 +0000

    Add package pyageng/0.0.1/pyageng-0.0.1.tar.gz

---
+++ packages/pyageng/pyageng-0.0.1.tar.gz/pyageng-0.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+import pyageng
+
+setup(name='pyageng',
+      version = pyageng.__version__,
+      description='Code for the book "Measurements and Data Analysis for Agricultural Engineers using Python"',
+      author='Matti Pastell',
+      author_email='matti.pastell@helsinki.fi',
+      url='http://pyageng.mpastell.com',
+      long_description =\
+"""
+Python for Agricultural Engineers
+---------------------------------
+
+This Python package is used in my (not quite finished) book  " Measurements and Data Analysis for Agricultural Engineers using Python".
+You can read the current version book online at: `<http://pyageng.mpastell.com/book>`_
+""",
+
+      packages=['pyageng'],
+      license='LICENSE.txt',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+        ]
+)
