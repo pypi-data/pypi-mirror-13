aeb752e37837d4d6ee4a35539295270fa531ddb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package radical.synapse/0.14/radical.synapse-0.14.tar.gz

---
+++ packages/radical.synapse/radical.synapse-0.14.tar.gz/radical.synapse-0.14/src/radical/synapse/atoms/__init__.py
@@ -0,0 +1,16 @@
+
+
+from constants import UNKNOWN, COMPUTE, MEMORY, STORAGE, NETWORK
+
+from base      import AtomBase
+from compute   import Compute
+from memory    import Memory
+from storage   import Storage
+from network   import Network
+
+from _atoms    import atom_compute_asm 
+from _atoms    import atom_compute
+from _atoms    import atom_memory
+from _atoms    import atom_storage 
+from _atoms    import atom_network 
+
---
+++ packages/radical.synapse/radical.synapse-0.14.tar.gz/radical.synapse-0.14/src/radical/synapse/atoms/constants.py
@@ -0,0 +1,19 @@
+
+
+__author__    = "Andre Merzky"
+__copyright__ = "Copyright 2013, The SAGA Project"
+__license__   = "LGPL.v3"
+
+
+
+# ------------------------------------------------------------------------------
+#
+# Atom Types
+#
+UNKNOWN = 'unknown'
+COMPUTE = 'compute'
+COMPUTE = 'compute_asm'
+MEMORY  = 'memory' 
+STORAGE = 'storage'
+NETWORK = 'network'
+
