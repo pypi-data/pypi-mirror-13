4ecd664afdbeac68ec3a24615b037cfa37205055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:25 +0000

    Add package pyinfra/0.1.dev9/pyinfra-0.1.dev9.tar.gz

---
+++ packages/pyinfra/pyinfra-0.1.dev9.tar.gz/pyinfra-0.1.dev9/pyinfra/api/inventory.py
@@ -0,0 +1,152 @@
+# pyinfra
+# File: pyinfra/api/inventory.py
+# Desc: represents a pyinfra inventory
+
+from .host import Host
+from .attrs import AttrData
+
+
+class Inventory(object):
+    '''
+    Represents a collection of target hosts. Stores and provides access to group data,
+    host data and default data for these hosts.
+
+    Args:
+        hostnames_data: tuple of ``(hostnames, data)``
+        ssh_user: default SSH user
+        ssh_port: default SSH port
+        ssh_key: default SSH key filename
+        ssh_key_password: default password for the SSH key
+        ssh_password: default SSH password
+        **groups: map of group names -> ``(hostnames, data)``
+    '''
+
+    state = None
+
+    def __init__(
+        self, hostnames_data,
+        ssh_user=None, ssh_port=None, ssh_key=None,
+        ssh_key_password=None, ssh_password=None, **groups
+    ):
+        hostnames, data = hostnames_data
+
+        self.connected_hosts = set()
+        self.groups = {}
+        self.host_data = {}
+        self.group_data = {}
+
+        # In CLI mode these are --user, --key, etc
+        self.override_data = AttrData({
+            'ssh_user': ssh_user,
+            'ssh_key': ssh_key,
+            'ssh_key_password': ssh_key_password,
+            'ssh_port': ssh_port or 22,
+            'ssh_password': ssh_password
+        })
+
+        self.data = AttrData(data)
+
+        # Loop groups and build map of hostname -> groups
+        hostnames_to_groups = {}
+        for group_name, (group_hostnames, group_data) in groups.iteritems():
+            group_name = group_name.lower()
+            self.group_data[group_name] = AttrData(group_data)
+
+            for hostname in group_hostnames:
+                hostname = hostname[0] if isinstance(hostname, tuple) else hostname
+                hostnames_to_groups.setdefault(hostname, []).append(group_name)
+
+        # Build the actual Host instances
+        hosts = {}
+        for hostname in hostnames:
+            host_data = {}
+            if isinstance(hostname, tuple):
+                hostname, host_data = hostname
+
+            self.host_data[hostname] = AttrData(host_data)
+
+            host = Host(self, hostname, hostnames_to_groups.get(hostname))
+            hosts[hostname] = host
+
+            # Push into any groups
+            for groupname in hostnames_to_groups.get(hostname, []):
+                self.groups.setdefault(groupname, []).append(host)
+
+        self.hosts = hosts
+
+    def __getitem__(self, key):
+        '''
+        Get individual hosts from the inventory by hostname.
+        '''
+
+        return self.hosts.get(key)
+
+    def __getattr__(self, key):
+        '''
+        Get groups (lists of hosts) from the inventory by name.
+        '''
+
+        return self.groups.get(key)
+
+    def __len__(self):
+        '''
+        Returns a list of all hosts, connected or not.
+        '''
+
+        return len(self.hosts)
+
+    def __iter__(self):
+        '''
+        Iterates over inventory hosts. Uses connected hosts only when they exist - in that
+        sense can be seen as the "active" list of hosts during a deploy.
+        '''
+
+        for host in self.hosts.values():
+            if not self.connected_hosts:
+                yield host
+
+            elif host.ssh_hostname in self.connected_hosts:
+                yield host
+
+    def get_data(self):
+        '''
+        Get the base/all data attached to this inventory.
+        '''
+
+        return self.data
+
+    def get_override_data(self):
+        '''
+        Get override data for this inventory.
+        '''
+
+        return self.override_data
+
+    def get_host_data(self, hostname):
+        '''
+        Get data for a single host in this inventory.
+        '''
+
+        return self.host_data[hostname]
+
+    def get_group_data(self, group):
+        '''
+        Get data for a single group in this inventory.
+        '''
+
+        return self.group_data.get(group, {})
+
+    def get_groups_data(self, groups):
+        '''
+        Gets aggregated data from a list of groups. Vars are collected in order so, for
+        any groups which define the same var twice, the last group's value will hold.
+        '''
+
+        data = {}
+
+        for group in groups:
+            data.update(
+                self.get_group_data(group).dict()
+            )
+
+        return AttrData(data)
