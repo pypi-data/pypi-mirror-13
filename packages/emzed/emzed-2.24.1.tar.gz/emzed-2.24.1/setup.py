f9f22a22647e11d7f2c6d21093a5926c55d28f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:34 +0000

    Add package emzed/2.24.1/emzed-2.24.1.tar.gz

---
+++ packages/emzed/emzed-2.24.1.tar.gz/emzed-2.24.1/setup.py
@@ -0,0 +1,51 @@
+import sys
+
+from setuptools import setup, find_packages
+
+# no import emzed here, causes trouble when installing on win, as missing packages
+# are needed when importing emzed
+version = (2, 24, 1)
+
+
+install_requires = ["emzed_optimizations>=0.5.0",
+                    "guidata<=1.6.2",
+                    "guiqwt<=2.3.2",
+                    "requests",
+                    "ipython==0.10",
+                    # "spyder==2.1.13",
+                    "dill",
+                    "sphinx",
+                    "html2text",
+                    "pandas",
+                    "dill",
+                    "pyopenms",
+                    "pyRserve==0.8.2",
+                    "pytest",
+                    "scikit-learn",
+                    "pycryptodome<=3.3",   # wheel for 3.4.0 was broken, 3.3 does the job !
+                    ]
+
+# if we install pyreadline on ubuntu14 we run into trouble:
+if sys.platform == "win32":
+    install_requires += ["pyreadline"]
+elif sys.platform != "linux2":
+    install_requires += ["readline"]
+
+
+setup(name="emzed",
+      packages=find_packages(exclude=["tests", "sandbox"]),
+      version="%d.%d.%d" % version,
+      keywords=["alpha", ],
+      description="Rewrite of emzed framework for LCMS data analysis",
+      entry_points={
+          "gui_scripts": ["emzed.workbench = emzed.workbench.main:main",
+                          "emzed.inspect = emzed.cmdline:inspect",
+                          ],
+          "console_scripts": ["emzed.console = emzed.console:main",
+                              "emzed.workbench.debug = emzed.workbench.main:main",
+                              ]
+      },
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      )
