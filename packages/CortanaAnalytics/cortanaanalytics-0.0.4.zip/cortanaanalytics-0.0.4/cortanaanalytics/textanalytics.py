a9e0dd131be6f95ead78dd7bca99c10f379121d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:16 +0000

    Add package CortanaAnalytics/0.0.4/cortanaanalytics-0.0.4.zip

---
+++ packages/CortanaAnalytics/cortanaanalytics-0.0.4.zip/cortanaanalytics-0.0.4/cortanaanalytics/textanalytics.py
@@ -0,0 +1,76 @@
+﻿#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation 
+# All rights reserved. 
+# 
+# Distributed under the terms of the MIT License
+#-------------------------------------------------------------------------
+import requests
+
+class TextAnalytics:
+    def __init__(self, account_key):
+        '''
+        account_key (str): account_key provided at https://datamarket.azure.com/account/keys
+        '''
+        self.auth = ('', account_key)
+
+    def get_sentiment(self, text):
+        '''
+        text (str)
+        '''
+        API_URL = Uris.get_sentiment
+        response = requests.get(API_URL, params = { 'text':text }, auth=self.auth)
+        return response.json()['Score']
+
+    def get_sentiment_batch(self, text_blocks):
+        '''
+        text_blocks ([{"Text":'string' Id:0},...])
+        '''
+        data = { "Inputs" : text_blocks }
+        API_URL = Uris.get_sentiment_batch
+        response = requests.post(API_URL, json=data, auth=self.auth)
+        return response.json()['SentimentBatch']
+
+    def get_key_phrases(self, text):
+        '''
+        text (str)
+        '''
+        API_URL = Uris.get_key_phrases
+        response = requests.get(API_URL, params = { 'text':text }, auth=self.auth)
+        return response.json()['KeyPhrases']
+
+    def get_key_phrases_batch(self, text_blocks):
+        '''
+        text_blocks ([{"Text":'string' Id:0},...])
+        '''
+        data = { "Inputs" : text_blocks }
+        API_URL = Uris.get_key_phrases_batch
+        response = requests.post(API_URL, json=data, auth=self.auth)
+        return response.json()['KeyPhrasesBatch']
+
+    def get_language(self, text, number_of_languages_to_detect=1):
+        '''
+        text (str)
+        '''
+        API_URL = Uris.get_language
+        params = { 'text':text, 'NumberOfLanguagesToDetect':number_of_languages_to_detect }
+        response = requests.get(API_URL, params = params, auth=self.auth)
+        return response.json()['DetectedLanguages']
+
+    def get_language_batch(self, text_blocks):
+        '''
+        text_blocks ([{"Text":'string' Id:0},...])
+        '''
+        data = { "Inputs" : text_blocks }
+        API_URL = Uris.get_language_batch
+        response = requests.post(API_URL, json=data, auth=self.auth)
+        print(response.json())
+        return response.json()['LanguageBatch']
+
+class Uris:
+    root = 'https://api.datamarket.azure.com/data.ashx/amla/text-analytics/v1/'
+    get_sentiment = root + 'GetSentiment'
+    get_sentiment_batch = root + 'GetSentimentBatch'
+    get_key_phrases = root + 'GetKeyPhrases'
+    get_key_phrases_batch = root + 'GetKeyPhrasesBatch'
+    get_language = root + 'GetLanguage'
+    get_language_batch = root + 'GetLanguageBatch'
---
+++ packages/CortanaAnalytics/cortanaanalytics-0.0.4.zip/cortanaanalytics-0.0.4/setup.py
@@ -0,0 +1,51 @@
+﻿#!/usr/bin/env python
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation 
+# All rights reserved. 
+# 
+# Distributed under the terms of the MIT License
+#-------------------------------------------------------------------------
+
+from setuptools import setup
+import os
+
+# To build:
+# python setup.py sdist
+# python setup.py bdist_wheel
+#
+# To install:
+# python setup.py install
+#
+# To register (only needed once):
+# python setup.py register
+#
+# To upload:
+# python setup.py sdist upload
+# python setup.py bdist_wheel upload
+
+with open('README', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cortanaanalytics',
+    version='0.0.4',
+    description='Python wrappers of Cortana Analytics services',
+    long_description=long_description,
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='ptvshelp@microsoft.com',
+    url='https://github.com/crwilcox/cortanaanalytics',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=['cortanaanalytics'],
+    install_requires=[
+        'requests',
+    ],
+    zip_safe = False,
+)
