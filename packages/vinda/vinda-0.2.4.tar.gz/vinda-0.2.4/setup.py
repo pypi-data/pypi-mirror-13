262a43ae732b480001d9992bd87692d57e675608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:19 +0000

    Add package vinda/0.2.4/vinda-0.2.4.tar.gz

---
+++ packages/vinda/vinda-0.2.4.tar.gz/vinda-0.2.4/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name = 'vinda',
+    packages = ['vinda'],
+    version = '0.2.4',
+    description = '`Help users construct their index pages rapidly',
+    long_description = readme,
+    author = 'Sicheng Luo',
+    author_email = 'i@lsich.com',
+    url = 'https://github.com/luosch/vinda',
+    keywords = ['vinda', 'index', 'html'],
+    license = 'MIT',
+    include_package_data = True,
+    package_data = {
+        '': ['*.html'],
+    },
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
