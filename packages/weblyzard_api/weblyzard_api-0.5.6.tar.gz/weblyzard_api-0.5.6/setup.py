54ca551a73c2f44f895c7ac48d9146ea2fd9e4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package weblyzard_api/0.5.6/weblyzard_api-0.5.6.tar.gz

---
+++ packages/weblyzard_api/weblyzard_api-0.5.6.tar.gz/weblyzard_api-0.5.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from setuptools.command import sdist
+
+setup (
+    name = 'weblyzard_api',
+    version = '0.5.6',
+    description= ' Web services for weblyzard',
+    author = 'Heinz-Peter Lang and Albert Weichselbraun',
+    author_email = 'lang@weblyzard.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = ['eWRT>=0.9.1.12', 
+                        'nose',
+                        'lxml',
+                        'requests',
+			            'pytest'],
+    dependency_links = ['git+https://github.com/weblyzard/ewrt.git#egg=eWRT-0.9.1.12'],
+    zip_safe = False,
+    scripts = ['src/weblyzard_api/client/openrdf/wl_upload_repository.py',]
+)
