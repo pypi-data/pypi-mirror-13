3060077af4cd1eedc6bfe4a633bc5fe441ef6aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package kaaedit/0.52.0/kaaedit-0.52.0.tar.gz

---
+++ packages/kaaedit/kaaedit-0.52.0.tar.gz/kaaedit-0.52.0/kaa/ui/git/commitdlgmode.py
@@ -0,0 +1,22 @@
+import locale
+import kaa
+from kaa import document, command
+from kaa.ui.multiline import multilinemode
+from kaa.theme import Theme, Style
+from kaa.filetype.default import keybind
+from kaa.commands import editorcommand
+from kaa.keyboard import *
+
+class CommitDialogMode(multilinemode.MultilineMode):
+
+    CAPTION = 'Hit alt+Enter to commit'
+
+    commit_callback = None
+    def callback(self, s):
+        self.conn.send(b'done')
+        self.conn.close()
+        self.conn = None
+        if s is not None:
+            open(self.commitmsg, 'w', encoding=locale.getpreferredencoding()).write(s)
+
+        self.commit_callback()
