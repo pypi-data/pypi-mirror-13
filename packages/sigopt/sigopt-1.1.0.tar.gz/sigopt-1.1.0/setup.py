d7aedc9ef04d887add448f1076ec2a6fbb58053a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:37 +0000

    Add package sigopt/1.1.0/sigopt-1.1.0.tar.gz

---
+++ packages/sigopt/sigopt-1.1.0.tar.gz/sigopt-1.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# keep this in sync with requirements.txt
+install_requires=['requests==2.5.1','simplejson>=3.5.2','six==1.10.0']
+
+setup(
+  name='sigopt',
+  version='1.1.0',
+  description='SigOpt Python API Client',
+  author='SigOpt',
+  author_email='support@sigopt.com',
+  url='https://sigopt.com/',
+  packages=['sigopt'],
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ]
+)
