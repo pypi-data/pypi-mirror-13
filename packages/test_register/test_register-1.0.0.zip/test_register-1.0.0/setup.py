ee07e16072b69cc863bd85fac43a1f3d5fef5ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:02 +0000

    Add package test_register/1.0.0/test_register-1.0.0.zip

---
+++ packages/test_register/test_register-1.0.0.zip/test_register-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'test_register',
+	version			= '1.0.0',
+	py_modules		= ['test_register'],
+	author			= 'ugiugi',
+	author_email	= 'hyeonukkim1990@gmail.com',
+	description		= 'just test'
+	
+)
