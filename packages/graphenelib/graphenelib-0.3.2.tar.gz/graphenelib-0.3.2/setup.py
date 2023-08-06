4f002c3f27c1b598f977089dfc3921d33ef5ecf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:10 +0000

    Add package graphenelib/0.3.2/graphenelib-0.3.2.tar.gz

---
+++ packages/graphenelib/graphenelib-0.3.2.tar.gz/graphenelib-0.3.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.3.2'
+
+setup(name='graphenelib',
+      version=VERSION,
+      description='Python library for graphene-based blockchains',
+      long_description=open('README.md').read(),
+      download_url='https://github.com/xeroc/python-graphenelib/tarball/' + VERSION,
+      author='Fabian Schuh',
+      author_email='<Fabian@BitShares.eu>',
+      maintainer='Fabian Schuh',
+      maintainer_email='<Fabian@BitShares.eu>',
+      url='http://www.github.com/xeroc/python-graphene',
+      keywords=['bitshares', 'muse', 'library', 'api',
+                'rpc', 'coin', 'tradingbot', 'exchange'],
+      packages=["grapheneapi",
+                "graphenebase",
+                "grapheneextra",
+                "grapheneexchange",
+                ],
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 3',
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Financial and Insurance Industry',
+                   'Topic :: Office/Business :: Financial',
+                   ]
+      )
