3e22130f595a37a67c35122b90e185a9f84548c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package Flask-BearyChat/0.2.0/Flask-BearyChat-0.2.0.tar.gz

---
+++ packages/Flask-BearyChat/Flask-BearyChat-0.2.0.tar.gz/Flask-BearyChat-0.2.0/flask_bearychat/__init__.py
@@ -0,0 +1,13 @@
+"""
+    flask_bearychat
+    ~~~~~~~~~~~~~~~
+
+    A Flask extension to help interact with BearyChat.
+
+    :copyright: (c) 2015 Yaoda Liu.
+    :license: MIT
+"""
+from .bearychat import BearyChat
+
+__all__ = ['BearyChat']
+__version__ = '0.2.0'
