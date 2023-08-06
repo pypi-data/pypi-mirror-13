3a2d389628f7a1041eedfb682d49c28a1f8b7ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:32 +0000

    Add package wanghuanranmyfun89757/1.0.0/wanghuanranmyfun89757-1.0.0.zip

---
+++ packages/wanghuanranmyfun89757/wanghuanranmyfun89757-1.0.0.zip/wanghuanranmyfun89757-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'wanghuanranmyfun89757',
+		version = '1.0.0',
+		py_modules = ['fun1'],
+	#	author = 'sherlock',
+	#	author_email = '1837129387@qq.com',
+	#    url = 'http://www.headfirstlabs.com',
+	#    description = 'A simple printer of nested lists',
+	)
