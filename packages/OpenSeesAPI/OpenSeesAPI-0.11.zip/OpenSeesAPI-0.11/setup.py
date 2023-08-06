8496b9676c6956f98fc23392f2f1fe5023ba0086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package OpenSeesAPI/0.11/OpenSeesAPI-0.11.zip

---
+++ packages/OpenSeesAPI/OpenSeesAPI-0.11.zip/OpenSeesAPI-0.11/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='OpenSeesAPI',
+      version='0.11',
+      # install_requires=[
+      # "numpy",
+      # "matplotlib",
+      # ],
+      description='A OpenSees Wrapper in Python',
+      url='http://github.com/nassermarafi/OpenSeesAPI',
+      author='Nasser Marafi',
+      author_email='marafi@uw.edu',
+      license='MIT',
+      packages=['OpenSeesAPI'],
+      zip_safe=False)
