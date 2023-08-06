5ca7621a82cc729788004ada171b956130e2f6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package githubgateway/0.3/githubgateway-0.3.tar.gz

---
+++ packages/githubgateway/githubgateway-0.3.tar.gz/githubgateway-0.3/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+  name = 'githubgateway',
+  version = '0.3',
+  py_modules = ['githubgateway'],
+  description = 'A class to make api calls to github',
+  author = 'Snapsheet',
+  author_email = 'technotifications@snapsheet.me',
+  url = 'https://github.com/bodyshopbidsdotcom/githubgateway',
+  download_url = 'https://github.com/bodyshopbidsdotcom/githubgateway/tarball/0.3',
+  keywords = ['api', 'gateway', 'http', 'REST'],
+  install_requires = [
+    'basegateway==0.7'
+  ],
+  classifiers = [
+    "Topic :: Internet :: WWW/HTTP",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
