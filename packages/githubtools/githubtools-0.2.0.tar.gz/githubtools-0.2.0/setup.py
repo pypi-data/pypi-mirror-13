8d206927e01f5e4a384e26c47f7e204fd3b45774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:31 +0000

    Add package githubtools/0.2.0/githubtools-0.2.0.tar.gz

---
+++ packages/githubtools/githubtools-0.2.0.tar.gz/githubtools-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='githubtools',
+      version='0.2.0',
+      author='nricklin',
+      author_email='nricklin@digitalglobe.com',
+      license='MIT',
+      description='Some github tools',
+      platforms="Posix; MacOS X; Windows",
+      install_requires=[
+          "PyGithub==1.26.0",
+          "docopt==0.6.2"
+      ],
+      packages=find_packages(),
+      py_modules=['githubtools.commit_status','githubtools.commit_status','githubtools.merge_pull_request'],
+      include_package_data=True,
+      entry_points={
+          'console_scripts': [
+              'github-commit-status = githubtools.commit_status:main',
+              'github-create-pull-request = githubtools.commit_status:main',
+              'github-merge-pull-request = githubtools.merge_pull_request:main',
+              ]
+      }
+    )
