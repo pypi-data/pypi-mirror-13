94cd2f1d84439ce24f0fdd33b01dc7a7d17baaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:35 +0000

    Add package pycryptopp/0.7.1.869544967005693312591928092448767568728501330214/pycryptopp-0.7.1.869544967005693312591928092448767568728501330214.tar.gz

---
+++ packages/pycryptopp/pycryptopp-0.7.1.869544967005693312591928092448767568728501330214.tar.gz/pycryptopp-0.7.1.869544967005693312591928092448767568728501330214/src-cryptopp/extraversion.h
@@ -0,0 +1,8 @@
+
+/* This _version.py is generated from git metadata by the pycryptopp
+ * setup.py. The main version number is taken from the most recent release
+ * tag. If some patches have been added since the last release, this will
+ * have a -NN "build number" suffix, or else a -rNN "revision number" suffix.
+ */
+
+#define CRYPTOPP_EXTRA_VERSION "pycryptopp-0.7.1.869544967005693312591928092448767568728501330214"
---
+++ packages/pycryptopp/pycryptopp-0.7.1.869544967005693312591928092448767568728501330214.tar.gz/pycryptopp-0.7.1.869544967005693312591928092448767568728501330214/src/pycryptopp/_version.py
@@ -0,0 +1,8 @@
+
+# This is the version of this tree, as created by pycryptopp's setup.py from the
+# git information: the main version number is taken from the most recent
+# release tag. If some patches have been added since the last release, this
+# will have a -NN "build number" suffix, followed by -gXXX "revid" suffix.
+
+__pkgname__ = "pycryptopp"
+__version__ = "0.7.1.869544967005693312591928092448767568728501330214"
