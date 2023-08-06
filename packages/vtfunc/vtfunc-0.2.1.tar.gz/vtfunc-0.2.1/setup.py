9577d2db29fc3632d86b2eef0c4dca6f17a05391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:23 +0000

    Add package vtfunc/0.2.1/vtfunc-0.2.1.tar.gz

---
+++ packages/vtfunc/vtfunc-0.2.1.tar.gz/vtfunc-0.2.1/setup.py
@@ -0,0 +1,28 @@
+import glob
+import os
+
+from distutils.core import setup, Extension
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    import warnings
+    raise RuntimeError('Cython must be installed to build vtfunc.')
+
+python_source = 'vtfunc.pyx'
+pysqlite = glob.glob('pysqlite/*.c')
+
+extension = Extension(
+    'vtfunc',
+    define_macros=[('MODULE_NAME', '"vtfunc"')],
+    libraries=['sqlite3'],
+    sources=[python_source] + pysqlite)
+
+setup(
+    name='vtfunc',
+    version='0.2.1',
+    description='',
+    url='https://github.com/coleifer/sqlite-vtfunc',
+    author='Charles Leifer',
+    author_email='',
+    ext_modules=cythonize(extension),
+)
