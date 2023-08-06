220f19a925ea9fbba078f3ebc935e13120cd265e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:33 +0000

    Add package mx_datagenerator/0.4.2/mx_datagenerator-0.4.2.tar.gz

---
+++ packages/mx_datagenerator/mx_datagenerator-0.4.2.tar.gz/mx_datagenerator-0.4.2/mx_datagen/data_source/company.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""Companies data"""
+#TODO: Add more prefix, sufix, descriptions and that kind of stuff
+
+prefix = ('Compañia', 'Despacho', 'Fondeadora', 'Desarrolladora')
+
+sufix = ('Corp', 'Inc', 'Co', 'Corporation', 'Incorporated', 'Compañía')
+
+description = ('de software', 'Marquez')
---
+++ packages/mx_datagenerator/mx_datagenerator-0.4.2.tar.gz/mx_datagenerator-0.4.2/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+path = '%s/data_source/' % os.path.dirname(os.path.realpath(__file__))
+
+setup(name='mx_datagenerator',
+      version='0.4.2',
+      description='Generador de datos específicamente creado para México',
+      url='https://github.com/ericking97/MX_datagen.git',
+      author='ericking97',
+      author_email='ericking97@hotmail.com',
+      license='MIT',
+      packages=find_packages(),
+      data_files=[(path, ['mx_datagen/data_source/CP.json'])],
+      zip_safe=False)
