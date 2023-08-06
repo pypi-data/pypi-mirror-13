1640f7e03d90f1adfd39570c9b1adcf25ea446aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package asianet-tracker/0.2/asianet-tracker-0.2.tar.gz

---
+++ packages/asianet-tracker/asianet-tracker-0.2.tar.gz/asianet-tracker-0.2/tracker/migrations/0002_auto_20160202_1738.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='tracker',
+            name='path',
+            field=models.TextField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='tracker',
+            name='user_agent',
+            field=models.TextField(null=True, blank=True),
+        ),
+    ]
