8e22cbfbc7abb5326641212cd3989460306d8b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package python-u2flib-server/4.0.0a0/python-u2flib-server-4.0.0a0.tar.gz

---
+++ packages/python-u2flib-server/python-u2flib-server-4.0.0a0.tar.gz/python-u2flib-server-4.0.0a0/test/test_attestation.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2013 Yubico AB
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from u2flib_server.attestation.metadata import MetadataProvider
+from u2flib_server.attestation.resolvers import create_resolver
+from u2flib_server.attestation.data import YUBICO
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+import json
+import unittest
+
+ATTESTATION_CERT = """
+MIICGzCCAQWgAwIBAgIEdaP2dTALBgkqhkiG9w0BAQswLjEsMCoGA1UEAxMjWXViaWNvIFUyRiBS
+b290IENBIFNlcmlhbCA0NTcyMDA2MzEwIBcNMTQwODAxMDAwMDAwWhgPMjA1MDA5MDQwMDAwMDBa
+MCoxKDAmBgNVBAMMH1l1YmljbyBVMkYgRUUgU2VyaWFsIDE5NzM2Nzk3MzMwWTATBgcqhkjOPQIB
+BggqhkjOPQMBBwNCAAQZo35Damtpl81YdmcbhEuXKAr7xDcQzAy5n3ftAAhtBbu8EeGU4ynfSgLo
+nckqX6J2uXLBppTNE3v2bt+Yf8MLoxIwEDAOBgorBgEEAYLECgECBAAwCwYJKoZIhvcNAQELA4IB
+AQC9LbiNPgs0sQYOHAJcg+lMk+HCsiWRlYVnbT4I/5lnqU907vY17XYAORd432bU3Nnhsbkvjz76
+kQJGXeNAF4DPANGGlz8JU+LNEVE2PWPGgEM0GXgB7mZN5Sinfy1AoOdO+3c3bfdJQuXlUxHbo+nD
+pxxKpzq9gr++RbokF1+0JBkMbaA/qLYL4WdhY5NvaOyMvYpO3sBxlzn6FcP67hlotGH1wU7qhCeh
++uur7zDeAWVh7c4QtJOXHkLJQfV3Z7ZMvhkIA6jZJAX99hisABU/SSa5DtgX7AfsHwa04h69AAAW
+DUzSk3HgOXbUd1FaSOPdlVFkG2N2JllFHykyO3zO
+""".replace('\n', '').decode('base64')
+
+
+class AttestationTest(unittest.TestCase):
+
+    def test_resolver(self):
+        resolver = create_resolver(YUBICO)
+        cert = x509.load_der_x509_certificate(ATTESTATION_CERT, default_backend())
+        metadata = resolver.resolve(cert)
+        assert metadata.identifier == '2fb54029-7613-4f1d-94f1-fb876c14a6fe'
+
+    def test_provider(self):
+        provider = MetadataProvider()
+        cert = x509.load_der_x509_certificate(ATTESTATION_CERT, default_backend())
+        attestation = provider.get_attestation(cert)
+
+        assert attestation.trusted
+
+    def test_versioning_newer(self):
+        resolver = create_resolver(YUBICO)
+        newer = json.loads(json.dumps(YUBICO))
+        newer['version'] = newer['version'] + 1
+        newer['trustedCertificates'] = []
+
+        resolver.add_metadata(newer)
+
+        cert = x509.load_der_x509_certificate(ATTESTATION_CERT, default_backend())
+        metadata = resolver.resolve(cert)
+
+        assert metadata is None
+
+    def test_versioning_older(self):
+        resolver = create_resolver(YUBICO)
+        newer = json.loads(json.dumps(YUBICO))
+        newer['trustedCertificates'] = []
+
+        resolver.add_metadata(newer)
+
+        cert = x509.load_der_x509_certificate(ATTESTATION_CERT, default_backend())
+        metadata = resolver.resolve(cert)
+
+        assert metadata.identifier == '2fb54029-7613-4f1d-94f1-fb876c14a6fe'
---
+++ packages/python-u2flib-server/python-u2flib-server-4.0.0a0.tar.gz/python-u2flib-server-4.0.0a0/test/test_u2f_v2.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2013 Yubico AB
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from u2flib_server import u2f_v2 as u2f
+from soft_u2f_v2 import SoftU2FDevice
+import unittest
+
+APP_ID = 'http://www.example.com/appid'
+FACET = 'https://www.example.com'
+FACETS = [FACET]
+
+
+class U2fV2Test(unittest.TestCase):
+
+    def test_register_soft_u2f(self):
+        token = SoftU2FDevice()
+
+        request = u2f.start_register(APP_ID)
+        response = token.register(request.json, FACET)
+
+        device, cert = u2f.complete_register(request, response)
+        assert device
+
+    def test_authenticate_soft_u2f(self):
+        token = SoftU2FDevice()
+        request = u2f.start_register(APP_ID)
+        response = token.register(request.json, FACET)
+        device, cert = u2f.complete_register(request, response)
+
+        challenge1 = u2f.start_authenticate(device)
+        challenge2 = u2f.start_authenticate(device)
+
+        response2 = token.getAssertion(challenge2.json, FACET)
+        response1 = token.getAssertion(challenge1.json, FACET)
+
+        assert u2f.verify_authenticate(device, challenge1, response1)
+        assert u2f.verify_authenticate(device, challenge2, response2)
+
+        try:
+            u2f.verify_authenticate(device, challenge1, response2)
+        except:
+            pass
+        else:
+            assert False, "Incorrect validation should fail!"
+
+        try:
+            u2f.verify_authenticate(device, challenge2, response1)
+        except:
+            pass
+        else:
+            assert False, "Incorrect validation should fail!"
+
+    def test_wrong_facet(self):
+        token = SoftU2FDevice()
+        request = u2f.start_register(APP_ID)
+        response = token.register(request.json, "http://wrongfacet.com")
+
+        try:
+            u2f.complete_register(request, response, FACETS)
+        except:
+            pass
+        else:
+            assert False, "Incorrect facet should fail!"
+
+        response2 = token.register(request.json, FACET)
+        device, cert = u2f.complete_register(request, response2)
+
+        challenge = u2f.start_authenticate(device)
+        response = token.getAssertion(challenge.json, "http://notright.com")
+
+        try:
+            u2f.verify_authenticate(device, challenge, response, FACETS)
+        except:
+            pass
+        else:
+            assert False, "Incorrect facet should fail!"
