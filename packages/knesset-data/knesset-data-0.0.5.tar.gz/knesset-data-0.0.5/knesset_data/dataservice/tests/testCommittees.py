10b16b88e45150e7bd8e0d27a761e136b04c7511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:10 +0000

    Add package knesset-data/0.0.5/knesset-data-0.0.5.tar.gz

---
+++ packages/knesset-data/knesset-data-0.0.5.tar.gz/knesset-data-0.0.5/knesset_data/dataservice/tests/testCommittees.py
@@ -0,0 +1,21 @@
+import unittest
+from knesset_data.dataservice.committees import Committee, CommitteeMeeting
+from datetime import datetime
+
+
+class TestCommittees(unittest.TestCase):
+
+    def test_committee(self):
+        committee = Committee.get(1)
+        committees = Committee.get_page(order_by=('id', 'asc'))
+        self.assertEqual(committee.name, committees[0].name)
+
+    def test_committee_meeting(self):
+        meetings = CommitteeMeeting.get(1, datetime(2016, 1, 1), datetime(2016, 1, 5))
+        self.assertTrue(isinstance(meetings[0].datetime, datetime))
+
+    def test_active_committees(self):
+        committees = Committee.get_all_active_committees()
+        committee = Committee.get(committees[0].id)
+        self.assertEqual(committee.name, committees[0].name)
+        self.assertTrue(committee.portal_link!=None and committee.portal_link != '' and committee.end_date==None)
