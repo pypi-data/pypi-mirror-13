ba5a37a67d185b7b9b95b2810b22b1e3557e2e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:17 +0000

    Add package crackqcli/0.4.1/crackqcli-0.4.1.tar.gz

---
+++ packages/crackqcli/crackqcli-0.4.1.tar.gz/crackqcli-0.4.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.4.1'
+
+setup(name='crackqcli',
+      packages=['crackqcli'],
+      version=version,
+      description="Hashcrack Crackq command-line client",
+      long_description="""\
+Hashcrack Crackq command-line client for submitting hashes to the Crackq. Supported hash formats: NTLM, MD5, SHA1, WPA/WPA2, PDF, descrypt, md5crypt, PHPass, MYSQL""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='hashcrack crackq password bruteforce hash NTLM MD5 SHA1 WPA WPA2 DESCRYPT MD5CRYPT PDF Wordpress Joomla phpBB3 MYSQL',
+      author='Hashcrack',
+      author_email='support@hashcrack.org',
+      scripts=['crackqcli/crackqcli.py'],
+      url='http://hashcrack.org',
+      license='GPLv3',
+      include_package_data=False,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
