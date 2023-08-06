f803349cacd7302e4bb5b3b8d3994e7b00c2febb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:00 +0000

    Add package san/1.2.0/san-1.2.0.zip

---
+++ packages/san/san-1.2.0.zip/san-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'san',
+    version = '1.2.0',
+    py_modules = ['san'],
+    author = 'lee',
+    author_email = 'lic8086939@163.com',
+    url = '...',
+    description = '...',
+    
+	)
