0cf16f77e3153284677ba600c15a12200f305e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:11 +0000

    Add package dj-extensions/0.1.6/dj-extensions-0.1.6.zip

---
+++ packages/dj-extensions/dj-extensions-0.1.6.zip/dj-extensions-0.1.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name="dj-extensions",
+    version="0.1.6",
+    description="Yet another Django extension with set of generic reusable, pluggable mixins",
+    long_description=readme(),
+    keywords="django views mixins",
+    author="Jahan Balasubramaniam",
+    author_email="jahan.unofficial@gmail.com",
+    url="https://github.com/jahan01/dj-extensions",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['Django >= 1.7'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Environment :: Plugins",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Framework :: Django",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+)
