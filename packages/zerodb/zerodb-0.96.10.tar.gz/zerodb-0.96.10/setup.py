c39a47d2764b62e1945335c2b9f101a391dbc93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:12 +0000

    Add package zerodb/0.96.10/zerodb-0.96.10.tar.gz

---
+++ packages/zerodb/zerodb-0.96.10.tar.gz/zerodb-0.96.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = [
+    'BTrees',
+    'zope.component',
+    'zodbpickle',
+    'ZODB',
+    'zope.index',
+    'repoze.catalog',
+    'cachetools',
+    'lz4-cffi',
+    'zc.zlibstorage',
+    'pycryptodome',
+    'click',
+    'flask-cors',
+    'flask',
+    'requests',
+    'jsonpickle',
+    'pyelliptic',
+    'ecdsa']
+
+setup(
+    name="zerodb",
+    version="0.96.10",
+    description="End-to-end encrypted database",
+    author="ZeroDB Inc.",
+    author_email="michael@zerodb.io",
+    license="AGPLv3",
+    url="http://zerodb.io",
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+)
