efccf2631158eab4b0eacf67162d067c85abe1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package ticketrenderer/0.1.4/ticketrenderer-0.1.4.tar.gz

---
+++ packages/ticketrenderer/ticketrenderer-0.1.4.tar.gz/ticketrenderer-0.1.4/ticketrenderer/tests.py
@@ -0,0 +1,104 @@
+import unittest
+from datetime import datetime
+
+from .ticketrenderer import TicketRenderer
+
+
+class TestTicketRenderer(unittest.TestCase):
+
+    def setUp(self):
+        self.media_url = 'http://media'
+        self.css_url = 'http://static/ticket.css'
+
+    def test_render(self):
+        """
+        TicketRenderer should render a ticket
+        """
+
+        html = '{{picture}} {{code}} {{datetime | datetimeformat}} ' \
+               '{{textvariable_1}} {{imagevariable_2}} ' \
+               '{{image_3}}'
+        texts = ['Titi', 'Vicky', 'Benni']
+        text_variables = [{'id': '1', 'items': texts}]
+
+        items = [
+            {
+                'id': '1',
+                'name': 'image1'
+
+            },
+            {
+                'id': '2',
+                'name': 'image2'
+            }
+        ]
+        image_variables = [{'id': '2', 'items': items}]
+        images = [{'id': '3', 'name': 'image3'}]
+        template = {
+            'html': html,
+            'images': images,
+            'image_variables': image_variables,
+            'text_variables': text_variables,
+            'title': 'title',
+            'subtitle': 'subtitle'
+        }
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
+        code = 'SJ98H'
+        date = datetime(2016, 01, 01)
+        picture = 'http://path/to/picture'
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture)
+        self.assertIn("http://path/to/picture", rendered)
+        self.assertIn(code, rendered)
+        self.assertIn("http://static/ticket.css", rendered)
+        self.assertTrue("Titi" in rendered or "Vicky" in rendered or "Benni" in rendered)
+        self.assertTrue("http://media/images/image1" in rendered or "http://media/images/image2" in rendered)
+        self.assertTrue("http://media/images/image3" in rendered)
+
+    def test_set_date_format(self):
+        """
+        Ticket renderer should handle datetimeformat filter
+        """
+        html = '{{datetime | datetimeformat("%Y/%m/%d")}}'
+        template = {
+            'html': html,
+            'images': [],
+            'image_variables': [],
+            'text_variables': [],
+            'title': '',
+            'subtitle': ''
+        }
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
+        code = 'SJ98H'
+        date = datetime(2010, 01, 01)
+        picture = 'http://path/to/picture'
+        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture)
+        assert "2010/01/01" in rendered_html
+
+
+    def test_encode_non_unicode_character(self):
+        """
+        Ticket renderer should encode non unicode character
+        """
+        html = u"Du texte avec un accent ici: é"
+        template = {
+            'html': html,
+            'images': [],
+            'image_variables': [],
+            'text_variables': [],
+            'title': '',
+            'subtitle': ''
+        }
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
+        code = 'SJ98H'
+        date = datetime(2010, 01, 01)
+        picture = 'http://path/to/picture'
+        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture)
+        assert u'Du texte avec un accent ici: é' in rendered_html
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
---
+++ packages/ticketrenderer/ticketrenderer-0.1.4.tar.gz/ticketrenderer-0.1.4/ticketrenderer/ticketrenderer.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+
+import random
+from jinja2 import Environment
+
+
+def datetimeformat(value, format='%Y-%m-%d'):
+    """
+    Jinja filter used to format date
+    :param value:
+    :param format:
+    :return:
+    """
+    return value.strftime(format)
+
+JINJA_ENV = Environment()
+JINJA_ENV.filters['datetimeformat'] = datetimeformat
+
+
+class TicketRenderer(object):
+
+    def __init__(self, ticket_template, media_url, css_url):
+        """
+        :param ticket_template: a ticket template used to configure a photobooth
+        :param media_url: base url to fetch images
+        :param css_url: url to find css files
+        :return:
+        """
+        self.template = ticket_template
+        self.media_url = media_url
+        self.css_url = css_url
+
+    def render(self, picture, code, date):
+
+        context = {
+            'title': self.template['title'],
+            'description': self.template['description'],
+            'picture': picture,
+            'datetime': date,
+            'code': code
+        }
+
+        for image in self.template['images']:
+            image_url = self.get_image_url(image['name'])
+            context['image_%s' % image['id']] = image_url
+
+        for image_variable in self.template['image_variables']:
+            choice = random.choice(image_variable['items'])
+            uid = 'imagevariable_%s' % image_variable['id']
+            context[uid] = self.get_image_url(choice['name'])
+
+        for text_variable in self.template['text_variables']:
+            choice = random.choice(text_variable['items'])
+            uid = 'textvariable_%s' % text_variable['id']
+            context[uid] = choice
+
+        template = JINJA_ENV.from_string(self.with_layout(self.template['html']))
+        return template.render(context)
+
+    def get_image_url(self, image_name):
+        return '%s/images/%s' % (self.media_url, image_name)
+
+    def with_layout(self, html):
+        """
+        add layout
+        """
+        base = u"""<!doctype html>
+        <html class="figure figure-ticket-container">
+            <head>
+                <meta charset="utf-8">
+                <link rel="stylesheet" href="{css_url}">
+            </head>
+            <body class="figure figure-ticket-container">
+                <div class="figure figure-ticket">
+                    {content}
+                </div>
+            </body>
+        </html>"""
+        return base.format(css_url=self.css_url, content=html)
+
+
