56ef4491d05e3879c96d59982fb8973b1597a86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:54 +0000

    Add package squery-pg/1.0/squery-pg-1.0.zip

---
+++ packages/squery-pg/squery-pg-1.0.zip/squery-pg-1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+import squery_pg as pkg
+
+
+VERSION = pkg.__version__
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='squery-pg',
+    version=VERSION,
+    license='BSD',
+    packages=['squery_pg'],
+    include_package_data=True,
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        'psycopg2',
+        'gevent',
+        'sqlize-pg',
+    ],
+)
