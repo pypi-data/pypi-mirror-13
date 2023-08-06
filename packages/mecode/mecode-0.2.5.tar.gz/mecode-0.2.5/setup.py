13f1147a51bf0e2cda6b35296452f536aa70c3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:37 +0000

    Add package mecode/0.2.5/mecode-0.2.5.tar.gz

---
+++ packages/mecode/mecode-0.2.5.tar.gz/mecode-0.2.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+INFO = {'name': 'mecode',
+        'version': '0.2.5',
+        'description': 'Simple GCode generator',
+        'author': 'Jack Minardi',
+        'author_email': 'jack@minardi.org',
+        }
+
+setup(
+    name=INFO['name'],
+    version=INFO['version'],
+    description=INFO['description'],
+    author=INFO['author'],
+    author_email=INFO['author_email'],
+    packages=find_packages(),
+    url='https://github.com/jminardi/mecode',
+    download_url='https://github.com/jminardi/mecode/tarball/master',
+    keywords=['gcode', '3dprinting', 'cnc', 'reprap'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'pyserial',
+        'numpy',
+    ],
+)
