4f351f9febd5949c97c514f8807190883108f878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package django-scrub-pii/1.0/django-scrub-pii-1.0.tar.gz

---
+++ packages/django-scrub-pii/django-scrub-pii-1.0.tar.gz/django-scrub-pii-1.0/HISTORY.rst
@@ -0,0 +1,9 @@
+Changelog
+=========
+    
+
+1.0 (2016-01-29)
+----------------
+
+- Initial release, basic support for built in field types, especially on postgres. Limited sqlite support.
+  [MatthewWilkes]
---
+++ packages/django-scrub-pii/django-scrub-pii-1.0.tar.gz/django-scrub-pii-1.0/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name='django-scrub-pii',
+    version='1.0',
+    description='A django add-on that allows models to be decorated with information about which fields contain '
+                'sensitive information, and an associated management command that creates a script to remove that'
+                ' information.',
+    long_description=(read('README.rst') +
+                      read('HISTORY.rst') +
+                      read('LICENSE')),
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author='Matthew Wilkes',
+    author_email='matt@matthewwilkes.name',
+    url='',
+    license='BSD',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        'django==1.8',
+    ],
+    extras_require={
+        'test': [
+            'nose',
+            'nose-selecttests',
+            'coverage',
+            'unittest2',
+            'flake8',
+        ],
+        'development': [
+            'zest.releaser',
+            'check-manifest',
+        ],
+    },
+    entry_points="""
+    """,
+    include_package_data=True,
+    zip_safe=False,
+)
