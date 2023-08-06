da5e486f9356d619c91663cb86ee81c30854d49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:12 +0000

    Add package pyupset/0.1.1.post7/PyUpSet-0.1.1.post7.tar.gz

---
+++ packages/pyupset/PyUpSet-0.1.1.post7.tar.gz/PyUpSet-0.1.1.post7/setup.py
@@ -0,0 +1,25 @@
+__author__ = 'leo@opensignal.com'
+from distutils.core import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+setup(name='PyUpSet',
+      version='0.1.1.post7',
+      description='Python implementation of the UpSet visualisation suite by Lex et al.',
+      author = 'Leonardo Baldassini',
+      author_email= 'leo@opensignal.com',
+      url='https://github.com/ImSoErgodic/py-upset',
+      license='MIT',
+      classifiers= [
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Science/Research',
+      'Topic :: Scientific/Engineering :: Visualization',
+      'Programming Language :: Python :: 3'],
+      install_requires=['pandas', 'matplotlib', 'numpy'],
+      packages=['pyupset'],
+      package_dir={'pyupset':'src/pyupset'},
+      package_data={'pyupset':['src/data/test_data_dict.pckl']})
