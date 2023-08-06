67e744ba6a9e4106a0a0df7a79bb119aad7529b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:43 +0000

    Add package alex_chi2/0.1.4/alex_chi2-0.1.4.zip

---
+++ packages/alex_chi2/alex_chi2-0.1.4.zip/alex_chi2-0.1.4/setup.py
@@ -0,0 +1,15 @@
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+import numpy.distutils.misc_util
+
+
+setup(name='alex_chi2',
+      version='0.1.4',
+      description='Sum Squared Error calculator in c',
+      author='Alexander Templeton',
+      author_email='blakmatr@gmail.com',
+      url='https://nepenthe.me/alex_chi2',
+      long_description='''
+Demo package demonstrating actual c code being modulized''',
+      ext_modules=[Extension("_alex_chi2", ["_alex_chi2.cpp", "alex_chi2.cpp"])],
+      include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
