8a355e21c707ef304c6b49b219a28d9daa3d5491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package markii/0.3.6/markii-0.3.6.tar.gz

---
+++ packages/markii/markii-0.3.6.tar.gz/markii-0.3.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="markii",
+    version="0.3.6",
+    description="MarkII is a development-mode error handler for Python web applications.",
+    long_description="https://github.com/Bogdanp/markii",
+    packages=["markii", "markii.frameworks"],
+    install_requires=["jinja2"],
+    include_package_data=True,
+    author="Bogdan Popa",
+    author_email="popa.bogdanp@gmail.com",
+    url="https://github.com/Bogdanp/markii",
+    keywords=["web", "errors", "debugging"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
