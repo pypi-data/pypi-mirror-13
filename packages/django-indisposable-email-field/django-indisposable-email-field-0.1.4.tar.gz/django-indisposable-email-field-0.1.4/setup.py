37e89f364368566bc6860e8b15711c2a80e114c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:18 +0000

    Add package django-indisposable-email-field/0.1.4/django-indisposable-email-field-0.1.4.tar.gz

---
+++ packages/django-indisposable-email-field/django-indisposable-email-field-0.1.4.tar.gz/django-indisposable-email-field-0.1.4/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+setup(
+        name='django-indisposable-email-field',
+        version='0.1.4',
+        packages=['indisposable_email_field'],
+        include_package_data=True,
+        url='https://github.com/sinnwerkstatt/django-indisposable-email-field',
+        license='Apache License 2.0',
+        author='Andreas Nüßlein',
+        author_email='andreas.nuesslein@sinnwerkstatt.com',
+        description='A Django form-Field that will check if the given email-address is a Disposable Email Address and block it accordingly.',
+        setup_requires=['django'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+        ]
+)
