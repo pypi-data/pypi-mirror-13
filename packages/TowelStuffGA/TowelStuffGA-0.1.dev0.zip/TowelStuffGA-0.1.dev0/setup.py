22585e2fee6279b196b8fd01b23e6b2d3f1dfedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:24 +0000

    Add package TowelStuffGA/0.1.dev0/TowelStuffGA-0.1.dev0.zip

---
+++ packages/TowelStuffGA/TowelStuffGA-0.1.dev0.zip/TowelStuffGA-0.1.dev0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TowelStuffGA',
+    version='0.1.dev0',
+    url='where to get it',
+    author='R. Bergeron',
+    author_email='rickboro@msn.com',
+    
+    packages=['towelstuff',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read()
+)
