0838074cc6170b34ed986280f7982c8a0f27d705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:46 +0000

    Add package briefkasten_watchdog/0.2.1/briefkasten_watchdog-0.2.1.zip

---
+++ packages/briefkasten_watchdog/briefkasten_watchdog-0.2.1.zip/briefkasten_watchdog-0.2.1/README.rst
@@ -0,0 +1,1 @@
+Note that for non-source installations you *will* need to provide a path to the configuration file when calling ``bin/watchdog``, i.e. ``bin/watchdog watchdog.ini``
---
+++ packages/briefkasten_watchdog/briefkasten_watchdog-0.2.1.zip/briefkasten_watchdog-0.2.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+version = '0.2.1'
+
+setup(
+    name='briefkasten_watchdog',
+    version=version,
+    description='Perform functional testing of a Briefkasten instance',
+    long_description="""
+        Part of the `ZeitOnline Briefkasten <https://github.com/ZeitOnline/briefkasten>`_ project,
+        this allows administrators to perform a fully functional test of their running instances.
+        See the `documentation <https://github.com/ZeitOnline/briefkasten#monitoring>`_ for details.""",
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        'License :: OSI Approved :: BSD License',
+    ],
+    packages=['briefkasten_watchdog'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'imapclient',
+        'zope.testbrowser',
+        'pyquery',
+        'pyramid_mailer',
+    ],
+    extras_require={
+        'development': [
+            'devpi-client',
+        ],
+    },
+    entry_points="""
+        [console_scripts]
+        watchdog = briefkasten_watchdog:main
+    """,
+)
