624c7ffabf0d69c13ecb8555043bf4fbeefe9b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:35 +0000

    Add package CortanaAnalytics/0.0.3/cortanaanalytics-0.0.3.zip

---
+++ packages/CortanaAnalytics/cortanaanalytics-0.0.3.zip/cortanaanalytics-0.0.3/setup.py
@@ -0,0 +1,46 @@
+﻿#!/usr/bin/env python
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation 
+# All rights reserved. 
+# 
+# Distributed under the terms of the MIT License
+#-------------------------------------------------------------------------
+
+from setuptools import setup
+
+# To build:
+# python setup.py sdist
+# python setup.py bdist_wheel
+#
+# To install:
+# python setup.py install
+#
+# To register (only needed once):
+# python setup.py register
+#
+# To upload:
+# python setup.py sdist upload
+# python setup.py bdist_wheel upload
+
+setup(
+    name='cortanaanalytics',
+    version='0.0.3',
+    description='Wrappers of Cortana Analytics services',
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='ptvshelp@microsoft.com',
+    url='https://github.com/crwilcox/cortanaanalytics',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=['cortanaanalytics'],
+    install_requires=[
+        'requests',
+    ],
+    zip_safe = False,
+)
