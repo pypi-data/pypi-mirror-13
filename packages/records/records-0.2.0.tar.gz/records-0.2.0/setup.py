5be23cb4bcc3d255f336c9738e91ecb81fdca890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:27 +0000

    Add package records/0.2.0/records-0.2.0.tar.gz

---
+++ packages/records/records-0.2.0.tar.gz/records-0.2.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+from codecs import open
+
+from setuptools import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py register')
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload --universal')
+    sys.exit()
+
+requires = ['psycopg2', 'tablib']
+version = '0.2.0'
+
+def read(f):
+    return open(f, encoding='utf-8').read()
+
+setup(
+    name='records',
+    version=version,
+    description='SQL for Humans',
+    long_description=read('README.rst') + '\n\n' + read('HISTORY.rst'),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.org',
+    url='https://github.com/kennethreitz/records',
+    py_modules=['records'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    license='ISC',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+    )
+)
