48aab1443bbebae5f8bc954d6148e5596b05db2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:46 +0000

    Add package dexcell_api_v3/0.1.9/dexcell_api_v3-0.1.9.tar.gz

---
+++ packages/dexcell_api_v3/dexcell_api_v3-0.1.9.tar.gz/dexcell_api_v3-0.1.9/dexma_api_v3/factory/location_factory/leaf.py
@@ -0,0 +1,22 @@
+__author__ = 'dcortes'
+
+from schematics.types import StringType, FloatType
+from schematics.types.compound import ListType, ModelType
+
+from dexma_api_v3.factory.reference_device import ReferenceDevice
+from dexma_api_v3.factory.location_factory.location_info_factory.address import Address
+from dexma_api_v3.factory.base_entity import BaseEntity
+from dexma_api_v3.factory.custom_types.unsigned_long import UnsignedLong
+
+
+class Leaf(BaseEntity):
+
+    name = StringType(required=True)
+    type = StringType(required=True)
+    parent =  ModelType(BaseEntity, required=True)
+    area = UnsignedLong(required=True)
+    summer_temp = FloatType()
+    winter_temp = FloatType()
+    activity = StringType(required=True)
+    reference_devices = ListType(ModelType(ReferenceDevice, required=True))
+    address = ModelType(Address, required=True)
