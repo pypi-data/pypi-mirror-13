b3c4ad348ba3f218199f4ad78b8c60a30efc069d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:22 +0000

    Add package uranium/0.2.28/uranium-0.2.28-py2.py3-none-any.whl

---
+++ packages/uranium/uranium-0.2.28-py2.py3-none-any.whl/uranium/__init__.py
@@ -0,0 +1,14 @@
+# an import is required to support this encoding in
+# libraries such as requests
+import encodings.idna
+
+# we import setuptools to ensure it's provided
+# when attempting to use uranium within a sandbox.
+import setuptools
+
+from .remote import get_remote_script
+from .decorators import task_requires
+from .rules import rule
+from .app_globals import current_build
+
+__all__ = ["get_remote_script", "task_requires", "rule", "current_build"]
