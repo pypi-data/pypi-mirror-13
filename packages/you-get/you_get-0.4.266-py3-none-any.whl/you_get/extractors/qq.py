fbaeaa594fc66ea4ee68863d0bd3b4b50a141cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:25 +0000

    Add package you-get/0.4.266/you_get-0.4.266-py3-none-any.whl

---
+++ packages/you-get/you_get-0.4.266-py3-none-any.whl/you_get/extractors/qq.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+__all__ = ['qq_download']
+
+from ..common import *
+
+def qq_download_by_vid(vid, title, output_dir='.', merge=True, info_only=False):
+    api = "http://vv.video.qq.com/geturl?otype=json&vid=%s" % vid
+    content = get_html(api)
+    output_json = json.loads(match1(content, r'QZOutputJson=(.*)')[:-1])
+    url = output_json['vd']['vi'][0]['url']
+    _, ext, size = url_info(url, faker=True)
+
+    print_info(site_info, title, ext, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir=output_dir, merge=merge)
+
+def qq_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    if 'iframe/player.html' in url:
+        vid = match1(url, r'\bvid=(\w+)')
+        # for embedded URLs; don't know what the title is
+        title = vid
+    else:
+        content = get_html(url)
+        vid = match1(content, r'vid\s*:\s*"\s*([^"]+)"')
+        title = match1(content, r'title\s*:\s*"\s*([^"]+)"')
+        # try to get the right title for URLs like this:
+        # http://v.qq.com/cover/p/ps6mnfqyrfo7es3.html?vid=q0181hpdvo5
+        title = matchall(content, [r'title\s*:\s*"\s*([^"]+)"'])[-1]
+
+    qq_download_by_vid(vid, title, output_dir, merge, info_only)
+
+site_info = "QQ.com"
+download = qq_download
+download_playlist = playlist_not_supported('qq')
