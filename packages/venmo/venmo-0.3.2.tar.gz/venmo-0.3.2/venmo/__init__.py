cb8dcea62dcc2bc302715ae4c87b7e3568043ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package venmo/0.3.2/venmo-0.3.2.tar.gz

---
+++ packages/venmo/venmo-0.3.2.tar.gz/venmo-0.3.2/venmo/__init__.py
@@ -0,0 +1,11 @@
+import atexit
+
+from venmo import cookies, singletons
+
+__version__ = '0.3.2'
+
+
+def exit_handler():
+    cookies.save(singletons.session().cookies)
+
+atexit.register(exit_handler)
