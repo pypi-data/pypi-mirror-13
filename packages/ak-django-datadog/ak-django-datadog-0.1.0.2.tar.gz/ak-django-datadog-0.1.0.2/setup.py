1a49ce32dbeca3b53cc1cdc57ef200bf1a2a1f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:20 +0000

    Add package ak-django-datadog/0.1.0.2/ak-django-datadog-0.1.0.2.tar.gz

---
+++ packages/ak-django-datadog/ak-django-datadog-0.1.0.2.tar.gz/ak-django-datadog-0.1.0.2/setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+reqs = []
+if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
+    reqs.append("simplejson>=2.0.9")
+
+setup(
+    name='ak-django-datadog',
+    version='0.1.0.2',
+    packages=[
+        'datadog',
+        'datadog.middleware'
+    ],
+    include_package_data=True,
+    license='BSD',
+    description=(
+        'Simple Django middleware for submitting timings and exceptions to'
+        ' Datadog.'),
+    long_description=README,
+    author='Conor Branagan',
+    author_email='conor.branagan@gmail.com',
+    install_requires=reqs.extend([
+        'dogapi==1.2.1'
+    ])
+)
