7e6d9cb4e5c22424b9031db690e33a62655530fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:52 +0000

    Add package lw_nester/1.3.0/lw_nester-1.3.0.zip

---
+++ packages/lw_nester/lw_nester-1.3.0.zip/lw_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name = 'lw_nester',
+	version = '1.3.0',
+	py_modules = ['nester'],
+	author = 'Liang Wei',
+	author_email = '591683556@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+       )
