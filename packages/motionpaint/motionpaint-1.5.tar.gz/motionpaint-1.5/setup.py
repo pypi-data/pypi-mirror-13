6e0c865645b1a54c81189e2c3a4cd5378c277cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:27 +0000

    Add package motionpaint/1.5/motionpaint-1.5.tar.gz

---
+++ packages/motionpaint/motionpaint-1.5.tar.gz/motionpaint-1.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='motionpaint',
+      version='1.5',
+      description='Visualizing motion with Python, Opencv, and Qt.',
+      author='clime',
+      author_email='clime@redhat.com',
+      url='http://github.com/clime',
+      license='GPLv2+',
+      py_modules=['motionpaint'])
