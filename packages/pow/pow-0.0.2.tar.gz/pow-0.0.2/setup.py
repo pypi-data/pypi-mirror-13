857933d1177f927cb6c7011b58b6a480e655641e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:37 +0000

    Add package pow/0.0.2/pow-0.0.2.tar.gz

---
+++ packages/pow/pow-0.0.2.tar.gz/pow-0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='pow',
+    version='0.0.2',
+    description=(
+        "Command line text snippets, inspired by Zach Holman's **boom**."),
+    url='https://github.com/doctaphred/pow',
+    author='Frederick Wagner',
+    author_email='doctaphred@gmail.com',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: MacOS :: MacOS X',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        ],
+    keywords=['command line', 'text', 'snippet', 'snippets', 'clipboard'],
+    py_modules=['pow'],
+    install_requires=[
+        'colorama==0.3.6',
+        'docopt==0.6.2',
+        ],
+    extras_require={
+        'test': ['flake8'],
+        },
+    entry_points={
+        'console_scripts': [
+            'pow = pow:main',
+            ],
+        },
+    )
