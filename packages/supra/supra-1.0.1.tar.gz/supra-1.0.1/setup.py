9b64efb6dcd430675567f89fc8f6dcc11b807b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:50 +0000

    Add package supra/1.0.1/supra-1.0.1.tar.gz

---
+++ packages/supra/supra-1.0.1.tar.gz/supra-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+# version (shape.function.path)
+setup(
+	name='supra',
+	version='1.0.1',
+	py_modules=['supra'],
+	url='https://github.com/luismoralesp/supra',
+	author="Luis Miguel Morales Pajaro",
+	author_email="luismiguel.mopa@gmail.com",
+	licence="Creative Common",
+	description="It's an easy JSON services generator",
+	platform="Linux"
+)
