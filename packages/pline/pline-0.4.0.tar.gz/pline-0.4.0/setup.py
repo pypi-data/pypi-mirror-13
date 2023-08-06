7479a251efe4f1e493b966b43f5b901956e53d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:56 +0000

    Add package pline/0.4.0/pline-0.4.0.tar.gz

---
+++ packages/pline/pline-0.4.0.tar.gz/pline-0.4.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+NAME    = 'pline'
+VERSION = '0.4.0'
+AUTHOR  = 'amancevice'
+EMAIL   = 'smallweirdnum@gmail.com'
+DESC    = 'AWS Data Pipeline Wrapper'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Utilities" ]
+REQUIRES = ["boto3>=1.2.3"]
+TEST_REQUIRES = ["mock", "nose"]
+
+setup(
+    name                 = NAME,
+    version              = VERSION,
+    author               = AUTHOR,
+    author_email         = EMAIL,
+    packages             = [ NAME ],
+    package_data         = { "%s" % NAME : ['README.md'] },
+    include_package_data = True,
+    url                  = 'http://www.smallweirdnumber.com',
+    description          = DESC,
+    long_description     = read('README.md'),
+    classifiers          = CLASSIFIERS,
+    install_requires     = REQUIRES,
+    test_requires        = TEST_REQUIRES,
+    test_suite           = "nose.collector" )
