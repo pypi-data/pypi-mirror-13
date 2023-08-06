f746a348bceb52c7d196f2fe59db50a3d8469a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:05 +0000

    Add package lj_nester/1.3.0/lj_nester-1.3.0.zip

---
+++ packages/lj_nester/lj_nester-1.3.0.zip/lj_nester-1.3.0/nester.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""This is the "nester.py" module and it provides one function called print_lol()"""
+
+def print_lol(the_list, indent=False, level=0):
+    """This function takes one positional argument called "the_list"."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t"),
+            print(each_item)
+
---
+++ packages/lj_nester/lj_nester-1.3.0.zip/lj_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'lj_nester',
+        version = '1.3.0',
+        py_modules = ['nester'],
+        author = 'ljpython',
+        author_email = 'auzyze@gmail.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists'
+    )
