56220fe68e78d59a828495258d9254c81a41e045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package coala/0.4.1.dev20160207153706/coala-0.4.1.dev20160207153706-py3-none-any.whl

---
+++ packages/coala/coala-0.4.1.dev20160207153706-py3-none-any.whl/bears/python/PyUnusedCodeBear.py
@@ -0,0 +1,19 @@
+import autoflake
+
+from coalib.bearlib.abstractions.CorrectionBasedBear import CorrectionBasedBear
+
+
+class PyUnusedCodeBear(CorrectionBasedBear):
+    GET_REPLACEMENT = staticmethod(
+        lambda file: (autoflake.fix_code(''.join(file)).splitlines(True), []))
+    RESULT_MESSAGE = "This file contains unused source code."
+
+    def run(self, filename, file):
+        """
+        Detects unused code. This functionality is limited to:
+
+        - Unneeded pass statements.
+        - Unneeded builtin imports. (Others might have side effects.)
+        """
+        for result in self.retrieve_results(filename, file):
+            yield result
