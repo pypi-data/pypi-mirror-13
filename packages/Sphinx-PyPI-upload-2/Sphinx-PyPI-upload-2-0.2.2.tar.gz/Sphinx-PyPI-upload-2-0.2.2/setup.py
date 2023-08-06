8aa4bc2460594cc9d801bf4af7d1803bf8bc3a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:55 +0000

    Add package Sphinx-PyPI-upload-2/0.2.2/Sphinx-PyPI-upload-2-0.2.2.tar.gz

---
+++ packages/Sphinx-PyPI-upload-2/Sphinx-PyPI-upload-2-0.2.2.tar.gz/Sphinx-PyPI-upload-2-0.2.2/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+
+setup(
+    name='Sphinx-PyPI-upload-2',
+    version='0.2.2',
+    author='Rick van Hattem',
+    author_email='wolph@wol.ph',
+    url='https://github.com/WoLpH/sphinx-pypi-upload',
+    description='Setuptools command for uploading Sphinx documentation to the '
+                'PyPI',
+    long_description=read('README.rst'),
+    license='BSD',
+    classifiers=[
+        'Topic :: Documentation',
+        'Framework :: Setuptools Plugin',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    platforms='any',
+    py_modules=['sphinx_pypi_upload'],
+    entry_points={
+        'distutils.commands': [
+            'upload_sphinx = sphinx_pypi_upload:UploadDoc',
+        ],
+    },
+)
