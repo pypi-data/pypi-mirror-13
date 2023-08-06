a5a206af85ab0aed08e7938f35dcbc163cd5298e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:23 +0000

    Add package django-functest/0.1.0/django-functest-0.1.0.tar.gz

---
+++ packages/django-functest/django-functest-0.1.0.tar.gz/django-functest-0.1.0/django_functest/__init__.py
@@ -0,0 +1,7 @@
+from .funcselenium import FuncSeleniumMixin
+from .funcwebtest import FuncWebTestMixin
+from .utils import AdminLoginMixin, ShortcutLoginMixin
+
+__version__ = '0.1.0'
+
+__all__ = ['FuncWebTestMixin', 'FuncSeleniumMixin', 'ShortcutLoginMixin', 'AdminLoginMixin']
