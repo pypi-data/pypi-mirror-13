9cc88fbd6a44ee8907162068e796bb83b62f09b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:19 +0000

    Add package 0805nexter/1.2.0/0805nexter-1.2.0.zip

---
+++ packages/0805nexter/0805nexter-1.2.0.zip/0805nexter-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = '0805nexter',
+    version = '1.2.0',
+    py_modules = ['0805nexter'],
+    author = 'hgtkpython',
+    author_email = '2868989685@qq.com',
+    url = 'http://www.hp.com',
+    description = 'a simple printer of nested lest',
+    )
