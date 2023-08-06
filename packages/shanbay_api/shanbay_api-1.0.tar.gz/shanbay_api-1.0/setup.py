cbf9680c47de8aa46dc397324c9cd41456cf88d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:19 +0000

    Add package shanbay_api/1.0/shanbay_api-1.0.tar.gz

---
+++ packages/shanbay_api/shanbay_api-1.0.tar.gz/shanbay_api-1.0/setup.py
@@ -0,0 +1,63 @@
+"""Shanbay API framework
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='shanbay_api',
+    version='1.0',
+    description='A pip library for construct shanbay api',
+    long_description=long_description,
+
+    url='http://www.shanbay.com/',
+
+    author='Li Hao',
+    author_email='hao.li@shanbay.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='api rest_framework development',
+
+    # You can just specify the pckages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['utils']),
+    install_requires=['djangorestframework'],
+)
