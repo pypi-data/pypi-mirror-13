0b0fcfdd607f1a50e182c81455f3b269dd436687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:25 +0000

    Add package edgy.project/0.1.7/edgy.project-0.1.7-py3-none-any.whl

---
+++ packages/edgy.project/edgy.project-0.1.7-py3-none-any.whl/edgy/project/feature/nosetests.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from . import Feature
+
+
+class NosetestsFeature(Feature):
+    def configure(self):
+        self.dispatcher.add_listener('edgy.project.feature.make.on_generate', self.on_make_generate)
+        self.dispatcher.add_listener('edgy.project.feature.python.on_generate', self.on_python_generate)
+
+    def on_make_generate(self, event):
+        makefile = event.makefile
+        makefile.add_target('test', '''
+            $(VIRTUALENV_PATH)/bin/nosetests -q --with-doctest --with-coverage --cover-package={name}
+        '''.format(name=event.package_name), deps=('install', ), phony=True)
+
+    def on_python_generate(self, event):
+        event.files['requirements'] = '\n'.join((event.files['requirements'], 'nose >=1.3,<1.4', 'coverage >=3.7,<3.8' ))
+
+
+__feature__ = NosetestsFeature
