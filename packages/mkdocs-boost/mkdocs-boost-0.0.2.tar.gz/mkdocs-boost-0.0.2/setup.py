e3805736511456f309930615d52bae354ad7ff83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:22 +0000

    Add package mkdocs-boost/0.0.2/mkdocs-boost-0.0.2.tar.gz

---
+++ packages/mkdocs-boost/mkdocs-boost-0.0.2.tar.gz/mkdocs-boost-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+
+
+setup(
+    name="mkdocs-boost",
+    version=VERSION,
+    url='https://github.com/pfultz2/mkdocs-boost',
+    license='boost',
+    description='Boost theme for mkdocs',
+    author='Paul Fultz II',
+    author_email='pfultz2@yahoo.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'boost = mkdocs_boost',
+        ]
+    },
+    zip_safe=False
+)
