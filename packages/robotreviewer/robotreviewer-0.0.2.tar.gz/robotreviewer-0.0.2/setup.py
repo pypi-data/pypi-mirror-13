8449822240d1cf3ea961af498839b30aaa9a6805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:32 +0000

    Add package robotreviewer/0.0.2/robotreviewer-0.0.2.tar.gz

---
+++ packages/robotreviewer/robotreviewer-0.0.2.tar.gz/robotreviewer-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='robotreviewer',
+      version='0.0.2',
+      description='Automatic extraction of data from clinical trial reports',
+      url='https://github.com/ijmarshall/robotreviewer',
+      author='Edward Banner',
+      author_email='edward.banner@gmail.com',
+      license='GPL',
+      packages=['robotreviewer'],
+      zip_safe=False)
