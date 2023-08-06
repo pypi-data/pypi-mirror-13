e61f25be54752d6734d14bc005ee4d1c11ebd199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:20 +0000

    Add package kagiso_sitemap/0.0.4/kagiso_sitemap-0.0.4.tar.gz

---
+++ packages/kagiso_sitemap/kagiso_sitemap-0.0.4.tar.gz/kagiso_sitemap-0.0.4/kagiso_sitemap/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url
+from django.contrib.sitemaps.views import index as sitemap_index, sitemap
+
+from .sitemaps import KagisoSitemap
+
+sitemaps = {'kagiso': KagisoSitemap()}
+
+urlpatterns = patterns(
+    '',
+    url(r'^sitemap\.xml$', sitemap_index, {'sitemaps': sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$',
+        sitemap, {
+            'sitemaps': sitemaps,
+            'template_name': 'kagiso_sitemap/kagiso_sitemap.xml'
+        }),
+)
