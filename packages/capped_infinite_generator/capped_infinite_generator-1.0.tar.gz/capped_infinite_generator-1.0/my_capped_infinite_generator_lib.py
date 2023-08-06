ae8c5516da42cddb6c8b385dbd2d6c93de29cddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:54 +0000

    Add package capped_infinite_generator/1.0/capped_infinite_generator-1.0.tar.gz

---
+++ packages/capped_infinite_generator/capped_infinite_generator-1.0.tar.gz/capped_infinite_generator-1.0/my_capped_infinite_generator_lib.py
@@ -0,0 +1,4 @@
+def my_inf2(n, m):
+	while n < m:
+		yield n+1
+		n += 1
---
+++ packages/capped_infinite_generator/capped_infinite_generator-1.0.tar.gz/capped_infinite_generator-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='capped_infinite_generator', 
+	description='from exercises of the Biocomputing II module',
+	author='Rocio Enriquez-Gasca',
+	author_email='Rocio.Enriquez@mpi-muenster.mpg.de',
+	version='1.0',
+	py_modules=['my_capped_infinite_generator_lib'],
+)
