a04a9d42b84eed7348b159d44ff0e42e222f6206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:35 +0000

    Add package ievv-opensource/0.2.23/ievv_opensource-0.2.23.tar.gz

---
+++ packages/ievv-opensource/ievv_opensource-0.2.23.tar.gz/ievv_opensource-0.2.23/ievv_opensource/ievvtasks_development/management/commands/ievvtasks_makemessages.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.core import management
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run makemessages for the languages specified in the ' \
+           'IEVVTASKS_MAKEMESSAGES_LANGUAGE_CODES setting.'
+
+    def __build_python_translations(self):
+        ignore = getattr(settings, 'IEVVTASKS_MAKEMESSAGES_IGNORE', [
+            'static/*'
+        ])
+        management.call_command('makemessages',
+                                locale=settings.IEVVTASKS_MAKEMESSAGES_LANGUAGE_CODES,
+                                ignore=ignore)
+
+    def __build_javascript_translations(self):
+        ignore = getattr(settings, 'IEVVTASKS_MAKEMESSAGES_JAVASCRIPT_IGNORE', [
+            'node_modules/*',
+            'bower_components/*'
+        ])
+        management.call_command('makemessages',
+                                domain='djangojs',
+                                locale=settings.IEVVTASKS_MAKEMESSAGES_LANGUAGE_CODES,
+                                ignore=ignore)
+
+    def handle(self, *args, **options):
+        self.__build_python_translations()
+        if getattr(settings, 'IEVVTASKS_MAKEMESSAGES_BUILD_JAVASCRIPT_TRANSLATIONS', False):
+            self.__build_javascript_translations()
