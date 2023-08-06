e7a2a8140b01c498aaf34d8ca9d64cafa8b52f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:18 +0000

    Add package exersice/1.4.0/exersice-1.4.0.zip

---
+++ packages/exersice/exersice-1.4.0.zip/exersice-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='exersice',
+    version='1.4.0',
+    py_modules=['exersice'],
+    author='sky_tz',
+    author_email='935967926@qq.com',
+    url='https://pypi.python.org/pypi?:action=login',
+    description='A spider for exersice.com',
+    )
