2f7cd3aeee3ffedbec6d7e2e212ad752198b920a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:27 +0000

    Add package eems/0.0.3.dev1/eems-0.0.3.dev1.zip

---
+++ packages/eems/eems-0.0.3.dev1.zip/eems-0.0.3.dev1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eems',
+    version='0.0.3.dev1',
+    description='An easy application to establish an energy monitoring system.',
+    long_description='test',
+    url='https://github.com/enricoba/eems',
+    author='Henrik Baran, Aurofree Hoehn',
+    author_email='henrik.baran@online.de, a.hoehn@mailbox.org',
+    license='MIT',
+    platforms='',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Education',
+        'Intended Audience :: Manufacturing',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='easy energy monitoring system',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    entry_points={
+        'console_scripts': [
+            'eems = eems.scripts:main'
+        ]
+    }
+)
