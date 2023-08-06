70bec925538d1d436038f328b0a7e99607ec3467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:12 +0000

    Add package arbitools/0.91/arbitools-0.91.tar.gz

---
+++ packages/arbitools/arbitools-0.91.tar.gz/arbitools-0.91/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="arbitools",
+      version="0.91",
+      description="Chess Arbiter Tools",
+      license="GPL",
+      author="David Gonzalez Gandara",
+      author_email="mrrookes@member.fsf.org",
+      url="http://www.ourenxadrez.org/arbitools.htm",
+      packages=['arbitools', 'tests'],
+      package_data={'arbitools':['data/*']},
+      install_requires={'xlwt', 'xlrd', 'lxml', 'Click'},
+      entry_points='''
+          [console_scripts]
+          arbitools-run=arbitools-run:standings
+      ''',
+      scripts=['arbitools-standings.py', 'arbitools-update.py', 'arbitools-gui.py', 'arbitools-purge.py', 'arbitools-add.py']
+)
