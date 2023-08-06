40c194b7e43fb6e7b7e6bb424e13519d852a532b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package twe_parser/0.0.5/twe_parser-0.0.5.tar.gz

---
+++ packages/twe_parser/twe_parser-0.0.5.tar.gz/twe_parser-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "twe_parser",
+    version = "0.0.5",
+    author = "Asahi Himura",
+    author_email = "himura@nitolab.com",
+    description = ("TWE-Lite's serial responce Parser"),
+    license = "MIT",
+    keywords = "TWE-Lite",
+    url = "https://github.com/nc30/twe_parser",
+    download_url = 'https://github.com/nc30/twe_parser/tarball/0.0.5',
+    packages = ["twe_parser"],
+    classifiers = [],
+)
