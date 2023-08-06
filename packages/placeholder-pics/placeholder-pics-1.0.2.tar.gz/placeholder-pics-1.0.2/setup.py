c8ff6fba35b12c0f4ad5dcd6468a568326b39ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:56 +0000

    Add package placeholder-pics/1.0.2/placeholder-pics-1.0.2.tar.gz

---
+++ packages/placeholder-pics/placeholder-pics-1.0.2.tar.gz/placeholder-pics-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='placeholder-pics',
+    version='1.0.2',
+    description="Easily generate placeholder pictures for your users.",
+    url="https://github.com/kylefox/placeholder-pics",
+    author="Kyle Fox",
+    author_email="kylefox@gmail.com",
+    license="MIT",
+    packages=['placeholder_pics'],
+    install_requires=['Pillow'],
+    zip_safe=False,
+)
