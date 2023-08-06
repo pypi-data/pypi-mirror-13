298f630807482c23028c5876f17dcebe3cf1e960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:26 +0000

    Add package usergrid/0.0.9/usergrid-0.0.9.tar.gz

---
+++ packages/usergrid/usergrid-0.0.9.tar.gz/usergrid-0.0.9/usergrid/management_templates.py
@@ -0,0 +1,7 @@
+
+__author__ = 'Jeff West @ ApigeeCorporation'
+
+magement_org_url_template = "{api_url}/management/organizations/{org_name}"
+management_org_list_apps_url_template = "{api_url}/management/organizations/{org_name}/applications"
+management_app_url_template = "{api_url}/management/organizations/{org_name}/applications/{app_name}"
+org_token_url_template = "{api_url}/management/token"
