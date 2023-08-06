750f592c066c63e837846d32c5f000c547888053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package django-functest/0.1.2/django_functest-0.1.2-py2-none-any.whl

---
+++ packages/django-functest/django_functest-0.1.2-py2-none-any.whl/django_functest/tests/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Thing',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('big', models.BooleanField(default=False)),
+                ('clever', models.BooleanField(default=False)),
+                ('element_type', models.CharField(max_length=1, choices=[(b'e', b'Earth'), (b'w', b'Water'), (b'a', b'Air'), (b'f', b'Fire')])),
+                ('count', models.IntegerField(default=0)),
+                ('description', models.TextField(blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
