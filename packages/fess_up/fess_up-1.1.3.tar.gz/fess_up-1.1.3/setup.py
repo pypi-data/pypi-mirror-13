f99d8de7dffdbdb5b7896e1569fb3f227b9dce58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package fess_up/1.1.3/fess_up-1.1.3.tar.gz

---
+++ packages/fess_up/fess_up-1.1.3.tar.gz/fess_up-1.1.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = "fess_up",
+    version = "1.1.3",
+    author = "Hugh Nowlan",
+    author_email = "nosmo@nosmo.me",
+    description = "DNS record scraper",
+    long_description = """Fess Up is an unintelligent DNS record
+guesser. It offers both a library and a command line tool to scan
+specific domains for (currently) one layer of subdomains. The scan is
+dictionary based and it will attempt to avoid superfluous queries by
+basing subsequent queries off of previously discovered records.
+""",
+    license = "Hacktivismo Enhanced-Source Software License Agreement",
+    keywords = "dns zone scraper",
+    url = "http://github.com/nosmo/fess_up",
+    packages=['fess_up', 'fess_up.dnsnames'],
+    install_requires=['PyYAML'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: Utilities",
+        ],
+    scripts = ["bin/fess_up"],
+    )
