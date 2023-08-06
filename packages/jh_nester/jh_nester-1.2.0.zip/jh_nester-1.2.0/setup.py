9730aa9c81ff860b021fd9075ba62dc8a303ed00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:33 +0000

    Add package jh_nester/1.2.0/jh_nester-1.2.0.zip

---
+++ packages/jh_nester/jh_nester-1.2.0.zip/jh_nester-1.2.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+
+        name            = 'jh_nester',
+        version         = '1.2.0',
+        py_modules      = ['jh_nester'],
+        author          = 'hfpython',
+        author_email    = 'hfpython@headfirstlabs.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = 'A simple printer fo nested lists',
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
