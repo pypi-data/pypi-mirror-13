bc8d74cb57575b9e3fcf5c9b944b223111569f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package django-logging-json/1.4/django-logging-json-1.4.tar.gz

---
+++ packages/django-logging-json/django-logging-json-1.4.tar.gz/django-logging-json-1.4/django_logging/handlers.py
@@ -0,0 +1,99 @@
+import json
+import gzip
+import time
+from logging import StreamHandler
+from logging.handlers import RotatingFileHandler
+from .log_object import LogObject, ErrorLogObject, SqlLogObject
+
+
+class AppFileHandler(RotatingFileHandler):
+    def emit(self, record):
+        if not isinstance(record.msg, LogObject)\
+                and not isinstance(record.msg, ErrorLogObject)\
+                and not isinstance(record.msg, dict):
+            return
+        return super(AppFileHandler, self).emit(record)
+
+    def format(self, record):
+        created = int(record.created)
+        message = record.msg if isinstance(record.msg, dict) else record.msg.to_dict
+        data = {record.levelname: {created: message}}
+        return json.dumps(data, sort_keys=True)
+
+    def rotation_filename(self, default_name):
+        return '{}-{}.gz'.format(default_name, time.strftime('%Y%m%d'))
+
+    def rotate(self, source, dest):
+        with open(source, 'rb+') as fh_in:
+            with gzip.open(dest, 'wb') as fh_out:
+                fh_out.writelines(fh_in)
+            fh_in.seek(0)
+            fh_in.truncate()
+
+
+class DebugFileHandler(RotatingFileHandler):
+    def emit(self, record):
+        if not isinstance(record.msg, LogObject) and \
+                not isinstance(record.msg, ErrorLogObject)\
+                and not isinstance(record.msg, dict):
+            return super(DebugFileHandler, self).emit(record)
+
+    def format(self, record):
+        created = int(record.created)
+        message = record.msg if isinstance(record.msg, dict) else record.msg.to_dict
+        data = {record.levelname: {created: message}}
+        return json.dumps(data, sort_keys=True)
+
+    def rotation_filename(self, default_name):
+        return '{}-{}.gz'.format(default_name, time.strftime('%Y%m%d'))
+
+    def rotate(self, source, dest):
+        with open(source, 'rb+') as fh_in:
+            with gzip.open(dest, 'wb') as fh_out:
+                fh_out.writelines(fh_in)
+            fh_in.seek(0)
+            fh_in.truncate()
+
+
+class ConsoleHandler(StreamHandler):
+    def emit(self, record):
+        return super(ConsoleHandler, self).emit(record)
+
+    def format(self, record):
+        if isinstance(record.msg, LogObject) or isinstance(record.msg, SqlLogObject):
+            created = int(record.created)
+            message = {record.levelname: {created: record.msg.to_dict}}
+
+            return json.dumps(message, sort_keys=True)
+        elif isinstance(record.msg, ErrorLogObject):
+            return str(record.msg)
+        elif isinstance(record.msg, dict):
+            created = int(record.created)
+            message = {record.levelname: {created: record.msg}}
+            return json.dumps(message, sort_keys=True)
+        else:
+            return super(ConsoleHandler, self).format(record)
+
+
+class SQLFileHandler(RotatingFileHandler):
+    def emit(self, record):
+        if not isinstance(record.msg, SqlLogObject):
+            return
+        return super(SQLFileHandler, self).emit(record)
+
+    def format(self, record):
+        created = int(record.created)
+        message = {record.levelname: {created: record.msg.to_dict}}
+
+        return json.dumps(message, sort_keys=True)
+
+    def rotation_filename(self, default_name):
+        return '{}-{}.gz'.format(default_name, time.strftime('%Y%m%d'))
+
+    def rotate(self, source, dest):
+        with open(source, 'rb+') as fh_in:
+            with gzip.open(dest, 'wb') as fh_out:
+                fh_out.writelines(fh_in)
+            fh_in.seek(0)
+            fh_in.truncate()
+
