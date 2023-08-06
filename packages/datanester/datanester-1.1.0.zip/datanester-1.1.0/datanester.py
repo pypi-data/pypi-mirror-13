f8feccc81d9eb76ec725e2a32bd880cc84366b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package datanester/1.1.0/datanester-1.1.0.zip

---
+++ packages/datanester/datanester-1.1.0.zip/datanester-1.1.0/datanester.py
@@ -0,0 +1,16 @@
+"""This is "datanester.py" module, and it provides one function called data_nest()
+which prints lists that may or may not include nested lists."""
+
+def data_nest(data_list,level):
+    """This function takes a positional argument called "data_list" , which is any
+        Python list (of,possibly,nested lists). Each data item in the provided list
+        is (recursively) printed to the screen on its own line."""
+    for data in data_list:
+        if isinstance(data,list):
+            data_nest(data,level+1)
+        else:
+            for data_tab in range(level):
+                print("\t",end='')
+            print(data)
+            
+#lists = ["xyz","lol",["is","my",["name"]]]
---
+++ packages/datanester/datanester-1.1.0.zip/datanester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'datanester',
+        version      = '1.1.0',
+        py_modules   = ['datanester'],
+        author       = 'tycoonabhay',
+        author_email = 'abhay.chauhan_1995@hotmail.com',
+        url          = 'http://www.codeshot.in',
+        description  = 'A simple printing of nested lists',
+     )
