8e03cee363e99f2b73c832753a021377d51fd4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:04 +0000

    Add package openerp_proxy/0.6.3/openerp_proxy-0.6.3.tar.gz

---
+++ packages/openerp_proxy/openerp_proxy-0.6.3.tar.gz/openerp_proxy-0.6.3/openerp_proxy/orm/object.py
@@ -0,0 +1,150 @@
+import six
+from extend_me import ExtensibleByHashType
+
+from ..utils import (AttrDict,
+                     DirMixIn)
+
+
+__all__ = ('Object', 'get_object')
+
+
+ObjectType = ExtensibleByHashType._('Object', hashattr='name')
+
+
+def get_object(client, name):
+    """ Create new Object instance.
+
+        :param client: Client instance to bind this object to
+        :type client: Client
+        :param name: name of object. Ex. 'sale.order'
+        :type name: str
+        :return: Created Object instance
+        :rtype: Object
+    """
+    cls = ObjectType.get_class(name, default=True)
+    return cls(client, name)
+
+
+@six.python_2_unicode_compatible
+class Object(six.with_metaclass(ObjectType, DirMixIn)):
+    """ Base class for all Objects
+
+        Provides simple interface to remote osv.osv objects::
+
+            erp = Client(...)
+            sale_obj = Object(erp, 'sale.order')
+            sale_obj.search([('state','not in',['done','cancel'])])
+
+        To create new instance - use *get_object* function, it implements
+        all extensions magic, whic is highly used in this project
+
+        It is posible to create extension only to specific object.
+        Example could be found in ``plugins/module_utils.py`` file.
+
+    """
+
+    def __init__(self, service, object_name):
+        self._service = service
+        self._obj_name = object_name
+
+        self._columns_info = None
+
+    @property
+    def name(self):
+        """ Name of the object
+        """
+        return self._obj_name
+
+    @property
+    def service(self):
+        """ Object service instance
+        """
+        return self._service
+
+    @property
+    def client(self):
+        """ Client instance, this object is relatedto
+        """
+        return self.service.client
+
+    # Overriden to add some standard method to be available in introspection
+    # Useful for IPython auto completition
+    def __dir__(self):
+        res = super(Object, self).__dir__()
+        res.extend(['read', 'search', 'write', 'unlink', 'create'])
+        return res
+
+    def __getattr__(self, name):
+        def method_wrapper(object_name, method_name):
+            """ Wraper around Odoo objects's methods.
+
+                for internal use.
+                It is used in Object class.
+            """
+            def wrapper(*args, **kwargs):
+                return self.service.execute(object_name, method_name, *args, **kwargs)
+            name = str('%s:%s' % (object_name, method_name))
+            wrapper.__name__ = name
+            return wrapper
+
+        # Private methods are not available to be called via RPC
+        if name.startswith('_'):
+            raise AttributeError("Private methods are not exposed to RPC. (attr: %s)" % name)
+
+        setattr(self, name,  method_wrapper(self.name, name))
+        return getattr(self, name)
+
+    def __str__(self):
+        return u"Object ('%s')" % self.name
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.client == other.client
+
+    def _get_columns_info(self):
+        """ Calculates columns info
+        """
+        return AttrDict(self.fields_get())
+
+    @property
+    def columns_info(self):
+        """ Reads information about fields available on model
+        """
+        if self._columns_info is None:
+            self._columns_info = self._get_columns_info()
+
+        return self._columns_info
+
+    def resolve_field_path(self, field):
+        """ Resolves dot-separated field path to tuple list of tuples (model, field_name)
+
+            :param str field: dot-separated field path to resolve
+
+            For example::
+
+                sale_obj = client['sale.order']
+                sale_obj.resolve_field_path('partner_id.country_id.name')
+
+            will be resoved to::
+
+                [('sale.order', 'partner_id', 'res.partner'),
+                 ('res.partner', 'country_id', 'res.country'),
+                 ('res.country', 'name', False)]
+
+        """
+        field_path = field.split('.')
+        res = []
+
+        model = self.name
+        cinfo = self.client[model].columns_info
+        f = field_path.pop(0)
+        res.append((model, f, cinfo[f].get('relation', False)))
+
+        while field_path:
+            model = cinfo[f]['relation']
+            cinfo = self.client[model].columns_info
+            f = field_path.pop(0)
+            res.append((model, f, cinfo[f].get('relation', False)))
+        return res
---
+++ packages/openerp_proxy/openerp_proxy-0.6.3.tar.gz/openerp_proxy-0.6.3/openerp_proxy/plugins/module_utils.py
@@ -0,0 +1,82 @@
+from ..plugin import Plugin
+from ..orm.object import Object
+
+
+# Overriden to add shortcut for module update
+class ModuleObject(Object):
+    """ Add shortcut method to upgrade module
+    """
+
+    # Apply this extension only to 'ir.module.module' Object / Model
+    class Meta:
+        name = 'ir.module.module'
+
+    def upgrade(self, ids):
+        """ Immediatly upgrades module
+        """
+        res = self.button_immediate_upgrade(ids)
+        self.client.clean_caches()  # because new models may appear in DB, so registered_objects shoud be refreshed
+        return res
+
+    def install(self, ids):
+        """ Immediatly install module
+        """
+        res = self.button_immediate_install(ids)
+        self.client.clean_caches()  # because new models may appear in DB, so registered_objects shoud be refreshed
+        return res
+
+
+class ModuleUtils(Plugin):
+    """ Utility plugin to simplify module management
+
+        Allows to acces module objects like::
+
+            db.plugins.module_utils.m_stock
+
+        which is equivalent to::
+
+            db.get_obj('ir.module.module').search_records([('name','=','stock')])[0]
+
+        Also autocomplete in IPython supported for this syntax
+    """
+
+    class Meta:
+        name = "module_utils"
+
+    def __init__(self, *args, **kwargs):
+        super(ModuleUtils, self).__init__(*args, **kwargs)
+        self._modules = None
+
+    @property
+    def modules(self):
+        """ Returns dictionary of modules registered in system.
+
+            Result dict is like: ``{'module_name': module_inst}``
+
+            where *module_inst* is *Record* instance for this module
+        """
+        if self._modules is None:
+            self._modules = {m.name: m for m in self.client['ir.module.module'].search_records([])}
+        return self._modules
+
+    def update_module_list(self):
+        """ Update module list
+
+            If there are some modules added to server,
+            update list, to be able to installe them.
+        """
+        self._modules = None
+        return self.client['ir.module.module'].update_list()
+
+    def __dir__(self):
+        res = dir(super(ModuleUtils, self))
+        res.extend(['m_' + i for i in self.modules.keys()])
+        return res
+
+    def __getitem__(self, name):
+        return self.modules[name]
+
+    def __getattr__(self, name):
+        if name.startswith('m_') and name[2:] in self.modules:
+            return self.modules[name[2:]]
+        raise AttributeError("No attribute %s in object %s" % (name, self))
