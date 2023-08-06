4f70ee6b44c29ebc67997f9386403eb72ac8ef45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:53 +0000

    Add package kugou/0.1/kugou-0.1.tar.gz

---
+++ packages/kugou/kugou-0.1.tar.gz/kugou-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'kugou',
+    py_modules = ['kugou'],
+    version = '0.1',
+    description = 'hello kugou',
+    author = 'kuga',
+    author_email = 'kugacola@gmail.com',
+    url = 'http://hellokugou.com',
+    keywords = ['kugou', 'hello', 'hello kugou'],
+)
