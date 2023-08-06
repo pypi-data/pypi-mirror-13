431dd8191677d9462beb4ee1b66e508009836414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:32 +0000

    Add package biannester/1.2.0/biannester-1.2.0.zip

---
+++ packages/biannester/biannester-1.2.0.zip/biannester-1.2.0/biannester.py
@@ -0,0 +1,14 @@
+"""This is the"biannester.py" module and it provides one function called print_lol()
+   which prints lists that may or may not include nested lists."""
+def print_lol(the_list,level=0):
+    """This function"""
+    for each_item in the_list:
+        if isinstance (each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
+				
+	
+   
