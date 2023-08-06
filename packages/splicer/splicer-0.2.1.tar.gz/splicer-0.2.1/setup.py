ed3410937aee3202eef3cfa4c7fd23e12e50b1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:48 +0000

    Add package splicer/0.2.1/splicer-0.2.1.tar.gz

---
+++ packages/splicer/splicer-0.2.1.tar.gz/splicer-0.2.1/setup.py
@@ -0,0 +1,40 @@
+import os.path
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README_PATH = os.path.join(HERE, 'README.md')
+try:
+    README = open(README_PATH).read()
+except IOError:
+    README = ''
+
+setup(
+  name='splicer',
+
+  version="0.2.1",
+  description='the world is a database now you can query it with SQL',
+  long_description=README,
+  author='Scott Robertson',
+  author_email='scott@triv.io',
+  url='http://github.com/trivio/splicer',
+  classifiers=[
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: 3.4",
+      "Programming Language :: Python :: 3.5",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development",
+  ],
+  packages = find_packages(),
+
+  install_requires=[
+    'codd',
+    'zipper',
+  ]
+)
