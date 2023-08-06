5bb0bba3d149c1cd3e0ae7d9f801808ac4f44095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:24 +0000

    Add package features/0.5.5/features-0.5.5.zip

---
+++ packages/features/features-0.5.5.zip/features-0.5.5/setup.py
@@ -0,0 +1,42 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='features',
+    version='0.5.5',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Feature set algebra for linguistics',
+    keywords='lattice morphology phonology learning fca',
+    license='MIT',
+    url='http://github.com/xflr6/features',
+    packages=find_packages(),
+    package_data={'features': ['config.ini']},
+    zip_safe=False,
+    install_requires=[
+        'concepts>=0.7, <1.0',
+        'fileconfig>=0.5, <1.0',
+        'graphviz>=0.3, <1.0',
+    ],
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
