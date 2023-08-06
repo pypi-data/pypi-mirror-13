cd41e18425e9c2f2838b23e53b90c6116bf7be08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:13 +0000

    Add package pymse/0.1.1/pymse-0.1.1.tar.gz

---
+++ packages/pymse/pymse-0.1.1.tar.gz/pymse-0.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:  Yeison Cardona --<yeison.eng@gmail.com>
+  Purpose:
+  Created: 13/02/16
+"""
+
+from setuptools import setup, find_packages
+
+setup(name = "pymse",
+      version = "0.1.1",
+      packages = find_packages(),
+      include_package_data=True,
+      description = "Calculates multiscale entropy (MSE) of one or multiple data sets.",
+      description_file = "README.md",
+
+      author = "M. Costa",
+      author_email = "mcosta@fsa.harvard.edu",
+      maintainer = "Yeison Cardona",
+      maintainer_email = "yeison.eng@gmail.com",
+
+      url = "http://www.physionet.org/",
+      download_url = "",
+
+      license = "BSD 3-Clause",
+      install_requires = ["numpy"],
+      keywords = 'mse',
+
+      classifiers=[#list of classifiers in https://pypi.python.org/pypi?:action=list_classifiers
+
+                   ],
+      )
