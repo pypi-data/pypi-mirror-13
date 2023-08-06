a2ed7e2ceac2720fb9d015e5208f4aa03050fb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:31 +0000

    Add package neronet/0.2.2/neronet-0.2.2.tar.gz

---
+++ packages/neronet/neronet-0.2.2.tar.gz/neronet-0.2.2/neronet/core.py
@@ -0,0 +1,259 @@
+# core.py
+#
+# Core class and function definitions
+
+import os
+import sys
+import datetime
+import socket
+import pickle
+import time
+#import psutil
+from signal import signal, SIGTERM, SIGQUIT
+from traceback import print_exc
+from copy import deepcopy
+
+TIME_OUT = 5.0
+"""float: how long the socket waits before failing when sending data
+"""
+
+USER_DATA_DIR = '~/.neronet' # Remember to os.path.expanduser
+USER_DATA_DIR_ABS = os.path.expanduser(USER_DATA_DIR)
+
+MANDATORY_FIELDS = set(['run_command_prefix', 'main_code_file', 'parameters', 
+                        'parameters_format'])
+OPTIONAL_FIELDS = set(['logoutput', 'collection', 'required_files',
+                        'conditions'])
+AUTOMATIC_FIELDS = set(['path', 'time_created', 'time_modified', 'state', 
+                        'cluster_id'])
+
+class Experiment(object):
+    """ 
+    Attributes:
+        experiment_id (str): Unique identifier to the experiment
+        run_command_prefix (str): The run command of the experiment
+        main_code_file (str): The code file to be run
+        required_files (str): Other files required by the experiments
+        logoutput (str): File where the experiment outputs information
+        parameters (dict): The Experiment parameters
+        parameters_format (str): The format of the experiment parameters
+        collection (str): The collection the experiment is part of
+        conditions (dict): Special condition for the experiment to do stuff
+        state (list of tuples): The states of the experiment with timestamp
+        cluster_id (str): The ID of the cluster where the experiment is run
+        time_created (str): Timestamp of when the experiment was created
+        time_modified (str): Timestamp of when the experiment was modified
+        last
+        path (str): Path to the experiment folder
+    """
+
+    class State:
+        #none = 'none'
+        defined = 'defined'
+        submitted = 'submitted'
+        submitted_to_kid = 'submitted_to_kid'
+        terminated = 'terminated'
+        running = 'running'
+        finished = 'finished'
+
+    def __init__(self, experiment_id, run_command_prefix, main_code_file,
+                    parameters, parameters_format, path, required_files=None,
+                    logoutput='output.log', collection=None, conditions=None):
+        now = datetime.datetime.now()
+        fields = {'run_command_prefix': run_command_prefix,
+                    'main_code_file': main_code_file,
+                    'required_files': required_files if required_files else [],
+                    'logoutput': logoutput,
+                    'parameters': parameters,
+                    'parameters_format': parameters_format,
+                    'collection': collection,
+                    'conditions': conditions,
+                    'path': path,
+                    'time_created': now,
+                    'time_modified': now,
+                    'states_info': [(Experiment.State.defined, now)],
+                    'cluster_id': None}
+        #MAGIC: Creates the attributes for the experiment class
+        self.__dict__['_fields'] = fields
+        #super(Experiment, self).__setattr__('_fields', fields)
+        super(Experiment, self).__setattr__('_experiment_id', experiment_id)
+    
+    def get_action(self, logrow):
+        for key in self._fields['conditions']:
+            action = self._fields['conditions'][key].get_action(logrow)
+            if action != 'no action':
+                return (action, key)
+        return ('no action', '')
+
+    def __getattr__(self, attr):
+        """Getter for the experiment class hides the inner dictionary"""
+        #Gets the inner dictionary
+        fields = super(Experiment, self).__getattribute__('_fields')
+        if attr in fields:
+            return fields[attr]
+        #Gets hidden attributes by adding _
+        if attr == 'id':
+            attr = 'experiment_id'
+        elif attr == 'state':
+            return fields['states_info'][-1][0]
+        elif attr == 'state_info':
+            return fields['states_info'][-1]
+        return super(Experiment, self).__getattribute__('_' + attr)
+
+    def __setattr__(self, attr, value):
+        """Setter for the experiment class
+        
+        Raises:
+            AttributeError: if the attribute doesn't exist
+        """
+        #Gets the inner dictionary
+        fields = super(Experiment, self).__getattribute__('_fields')
+        if attr == 'id' or attr == 'experiment_id':
+            super(Experiment, self).__setattr__('_experiment_id', value)
+        elif attr in fields or attr in ('log_output', ):
+            fields[attr] = value
+        else:
+            raise AttributeError('Experiment has no attribute named "%s"!' % attr)
+
+    @property 
+    def callstring(self):
+        rcmd = self._fields['run_command_prefix']
+        code_file = self._fields['main_code_file']
+        parameters = self._fields['parameters']
+        param_format = self._fields['parameters_format']
+        parameters_string = param_format.format(**parameters)
+        callstring = ' '.join([rcmd, code_file, parameters_string])
+        return callstring
+
+    def update_state(self, state):
+        """ Updates the state
+        """
+        self._fields['states_info'].append((state, datetime.datetime.now()))
+
+    def as_dict(self):
+        """ Returns the experiment as a dictionary
+        """
+        return {self._experiment_id: deepcopy(self._fields)}
+
+    def as_gen(self):
+        """Creates a generate that generates info about the experiment
+        Yields:
+            str: A line of experiment status
+        """
+        yield "%s\n" % self._experiment_id
+        yield "  Run command: %s\n" % self._fields['run_command_prefix']
+        yield "  Main code file: %s\n" % self._fields['main_code_file']
+        params = self._fields['parameters_format'].format( \
+            **self._fields['parameters'])
+        yield "  Parameters: %s\n" % params
+        yield "  Parameters format: %s\n" % self._fields['parameters_format']
+        if self._fields['collection']:
+            yield "  Collection: %s\n" % self._fields['collection']
+        yield "  State: %s\n" % self.state
+        yield "  Last modified: %s\n" % self._fields['time_modified']
+
+    def __str__(self):
+        return "%s %s" % (self._experiment_id, self._fields['state'][-1][0])
+
+def osrun(cmd):
+    print('> %s' % (cmd))
+    os.system(cmd)
+
+def get_hostname():
+    return read_file('/etc/hostname').strip()
+
+def time_now():
+    return datetime.datetime.now() #.strftime('%H:%M:%S %d-%m-%Y')
+
+def write_file(filepath, data):
+    text = str(data)
+    with open(filepath, 'w') as f:
+        f.write(text)
+
+def read_file(filepath, default=None):
+    result = default
+    try:
+        with open(filepath, 'r') as f:
+            result = ''
+            for line in f:
+                result += line
+    except IOError as e:
+        print(e)
+        pass
+    return result
+
+class Logger:
+
+    """A class to simplify logging."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def log(self, msg):
+        """prints datetime, process name, process message"""
+        # Print to stdout in a clear format
+        print('%s %s: %s' % (datetime.datetime.now(), self.name, msg))
+
+
+class Socket:
+
+    """A class to simplify socket usage."""
+
+    def __init__(self, host, port):
+        # Save key attributes
+        self.host = host
+        self.port = port
+
+    def send_data(self, data):
+        """Create a socket, send data over it, and close it"""
+        # Create a TCP/IP socket
+        sock = socket.socket()
+        sock.settimeout(TIMEOUT)
+        # Connect to the mother
+        #self.logger.log('Connecting to (%s, %s)...' % (self.host, self.port))
+        sock.connect((self.host, self.port))
+        # Send data
+        #self.logger.log('Sending data "%s"...' % (data))
+        sock.sendall(pickle.dumps(data, -1))
+        # Close socket
+        #self.logger.log('Closing socket...')
+        sock.close()
+        
+WARNING_FIELDS = set(['variablename', 'killvalue', 'comparator', 'when', \
+                        'action'])
+
+class ExperimentWarning:
+    
+    def __init__(self, name, variablename, killvalue, comparator, when, action):
+        self.name = name.strip()
+        self.varname = variablename.strip()
+        self.killvalue = killvalue
+        self.comparator = comparator.strip()
+        self.when = when.strip()
+        self.action = action.strip()
+        self.start_time = datetime.datetime.now()
+            
+    def get_action(self, logrow):
+        logrow = logrow.strip()
+        varlen = len(self.varname)
+        check_condition = True
+        if 'time' in self.when:
+            time_when = float(self.when[4:].strip())
+            time_passed = datetime.datetime.now() - self.start_time
+            time_passed_sec = time_passed.days * 86400 + time_passed.seconds
+            time_passed_min = time_passed_sec / 60
+            if time_passed_min < time_when:
+                check_condition = False
+        if check_condition and logrow[:varlen].strip() == self.varname:
+            varvalue = logrow[varlen:].strip()
+            try:
+                varvalue = float(varvalue)
+            except:
+                return 'no action'
+            if any( [self.comparator == 'gt' and varvalue > self.killvalue,
+                self.comparator == 'lt' and varvalue < self.killvalue,
+                self.comparator == 'eq' and varvalue == self.killvalue,
+                self.comparator == 'geq' and varvalue >= self.killvalue,
+                self.comparator == 'leq' and varvalue <= self.killvalue] ):
+                return self.action
+        return 'no action'
---
+++ packages/neronet/neronet-0.2.2.tar.gz/neronet-0.2.2/neronet/neromum.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""This module defines Neromum.
+"""
+
+# TODO: need database parsing
+
+import os
+import sys
+import pickle
+import glob
+import time
+
+import neronet.core
+import neronet.daemon
+import neronet.nerokid
+
+class Neromum(neronet.daemon.Daemon):
+
+    """A class to specify the Neromum object.
+
+    Runs in the cluster and manages and monitors all the nodes.
+
+    Gets the experiment as the 1st command line argument
+    Experiment parameters from 2nd onwards.
+
+    Attrs:
+        exp_dict (dict): A dict of all experiments submitted to this mum by
+            experiment ID.
+    """
+
+    def __init__(self):
+        super(Neromum, self).__init__('neromum')
+        self.exp_dict = {}
+        self.add_query('list_exps', self.qry_list_exps)
+        self.add_query('exp_update', self.qry_exp_update)
+
+    def qry_list_exps(self): # primarily for debugging
+        """List all experiments submitted to this mum."""
+        msg = 'Experiments:\n'
+        for exp in self.exp_dict.values():
+            msg += '- %s  %s  %s\n' % (exp.id, exp.state, exp.path)
+        self._reply['msgbody'] = msg
+        self._reply['rv'] = 0
+
+    def qry_exp_update(self, exp_id, state, log_output):
+        """Save information from Nerokids' experiment data updates."""
+        exp = self.exp_dict[exp_id]
+        # Extract log output update information
+        for log_path, new_text in exp.log_output.items():
+            # Initialize buffers for any new log path
+            if log_path not in exp.log_output:
+                exp.log_output[log_path] = ''
+            # Append the new log output
+            exp.log_output[log_path] += new_text
+        # Update experiment state info
+        exp.update_state(state)
+        neronet.core.write_file(os.path.join(neronet.core.USER_DATA_DIR_ABS,
+                'experiments', exp.id, 'exp.pickle'), pickle.dumps(exp))
+        # Debugging
+        if exp.state == neronet.core.Experiment.State.finished:
+            self.log('Experiment "%s" has finished!' % (exp.id))
+        self._reply['rv'] = 0
+
+    def ontimeout(self):
+        """Load and start any unstarted received experiments."""
+        # Load all experiments into the dict that have not yet been loaded
+        for exp_file in glob.glob(os.path.join(neronet.core.USER_DATA_DIR_ABS,
+                'experiments/*/exp.pickle')):
+            #self.log('Checking exp "%s"...' % (exp_file))
+            exp_id = os.path.basename(os.path.dirname(exp_file))
+            if exp_id not in self.exp_dict:
+                exp = pickle.loads(neronet.core.read_file(exp_file))
+                self.log('New experiment detected: "%s" (%s)...' % (exp_id, exp.state))
+                self.exp_dict[exp_id] = exp
+        # Start an experiment if there is any to start
+        for exp in self.exp_dict.values():
+            if exp.state == neronet.core.Experiment.State.submitted:
+                # Initialize the log output container
+                exp.log_output = {}
+                # TODO: Allow sbatch launch
+                # Launch experiment in the local (umanaged) node
+                self.log('Launching experiment "%s"...' % (exp.id))
+                nerokid = neronet.daemon.QueryInterface(neronet.nerokid.Nerokid(exp.id))
+                # Start the kid daemon
+                nerokid.start()
+                # Wait until it gets initialized
+                time.sleep(2.0)
+                # Configure it
+                nerokid.query('configure', host=self._host, port=self._port)
+                exp.update_state(neronet.core.Experiment.State.submitted_to_kid)
+                return # pace submission by launching only one at a time
+        # Compute the number of finished experiments
+        finished_count = 0
+        for exp in self.exp_dict.values():
+            if exp.state == neronet.core.Experiment.State.finished:
+                finished_count += 1
+        # Exit if all known (submitted) experiments are finished
+        if finished_count == len(self.exp_dict):
+            self.log('All %d experiments are finished. Quitting...' % (finished_count))
+            self._doquit = True
+
+def main():
+    """Create a CLI interface object and process CLI arguments."""
+    cli = neronet.daemon.Cli(Neromum())
+    cli.parse_arguments()
+
+#if __name__ == '__main__':
+#    main()
