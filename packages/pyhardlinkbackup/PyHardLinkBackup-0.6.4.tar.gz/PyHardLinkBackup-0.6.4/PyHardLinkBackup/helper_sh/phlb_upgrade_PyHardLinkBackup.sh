e91395a0de4115a6ccad75e56aa6740134ed34be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:41 +0000

    Add package pyhardlinkbackup/0.6.4/PyHardLinkBackup-0.6.4.tar.gz

---
+++ packages/pyhardlinkbackup/PyHardLinkBackup-0.6.4.tar.gz/PyHardLinkBackup-0.6.4/PyHardLinkBackup/helper_sh/phlb_upgrade_PyHardLinkBackup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ENV_PATH=~/PyHardLinkBackup
+CURRENT_DIR=$(pwd)
+
+set -e
+
+ACTIVATE_SCRIPT=${ENV_PATH}/bin/activate
+echo "Activate venv with: ${ACTIVATE_SCRIPT}"
+source ${ACTIVATE_SCRIPT}
+
+set -x
+
+pip install --upgrade pip
+pip install --upgrade PyHardLinkBackup
+
+phlb helper ${ENV_PATH}
