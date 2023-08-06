dd5cce50a4948e08871fcfff2ad4672e6ba203fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package pandas-profiling/1.0.0a2/pandas_profiling-1.0.0a2-py2.py3-none-any.whl

---
+++ packages/pandas-profiling/pandas_profiling-1.0.0a2-py2.py3-none-any.whl/pandas_profiling/__init__.py
@@ -0,0 +1,36 @@
+
+import codecs
+from .templates import wrapper_html
+from .base import describe, to_html
+
+NO_OUTPUTFILE = "pandas_profiling.no_outputfile"
+DEFAULT_OUTPUTFILE = "pandas_profiling.default_outputfile"
+
+
+class ProfileReport(object):
+    html = ''
+    file = None
+
+    def __init__(self, df):
+        description_set = describe(df)
+        self.html = to_html(df.head(),
+                            description_set)
+
+    def to_file(self, outputfile=DEFAULT_OUTPUTFILE):
+
+        if outputfile != NO_OUTPUTFILE:
+            if outputfile == DEFAULT_OUTPUTFILE:
+                outputfile = 'profile_' + str(hash(self)) + ".html"
+
+            self.file = codecs.open(outputfile, 'w+b', encoding='utf8')
+            self.file.write(wrapper_html % self.html)
+            self.file.close()
+
+    def _repr_html_(self):
+        return self.html
+
+    def __str__(self):
+        return "Output written to file " + str(self.file.name)
+
+
+
