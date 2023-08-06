b1230876a5f29294acf567bfa32cea947ca1b80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:13 +0000

    Add package tinyschachtler/1.3.0/tinyschachtler-1.3.0.tar.gz

---
+++ packages/tinyschachtler/tinyschachtler-1.3.0.tar.gz/tinyschachtler-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name		= 'tinyschachtler' ,
+		version		= '1.3.0' ,
+		py_modules	= ['schachtler'] ,
+		author		= 'tiny' ,
+		author_email= 'kickoke@gmail.com' ,
+		url			= 'https://github.com/kickoke' ,
+		description	= 'Einfache Ausgabe geschachtelter Listen' ,
+	)
