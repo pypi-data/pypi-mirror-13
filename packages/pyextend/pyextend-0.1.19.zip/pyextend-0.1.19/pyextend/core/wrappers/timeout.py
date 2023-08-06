90ba557f08a45d0a286d17840654f1b3d9f39894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:33 +0000

    Add package pyextend/0.1.19/pyextend-0.1.19.zip

---
+++ packages/pyextend/pyextend-0.1.19.zip/pyextend-0.1.19/pyextend/core/wrappers/timeout.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+"""
+    pyextend.core.wrappers.timeout
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    pyextend core wrappers timeout wrapper
+
+    :copyright: (c) 2016 by Vito.
+    :license: GNU, see LICENSE for more details.
+"""
+import signal
+import functools
+from . import system as sys
+
+# class TimeoutError(Exception): pass
+
+
+def timeout(seconds, error_message=None):
+    """Timeout checking just for Linux-like platform, not working in Windows platform."""
+    def decorated(func):
+        result = ""
+
+        def _handle_timeout(signum, frame):
+            errmsg = error_message or 'TimeoutError: the action <%s> is timeout, %s seconds!' % (func.__name__, seconds)
+
+            global result
+            result = None
+            raise TimeoutError(errmsg)
+
+        @sys.platform(sys.UNIX_LIKE, case_false_wraps=func)
+        def wrapper(*args, **kwargs):
+            global result
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+                return result
+
+        return functools.wraps(func)(wrapper)
+
+    return decorated
