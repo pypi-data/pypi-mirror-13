b4a14ee5b595be22baf09bd3bd05d945a75e78fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package python-csv/0.0.8/python-csv-0.0.8.tar.gz

---
+++ packages/python-csv/python-csv-0.0.8.tar.gz/python-csv-0.0.8/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+    name = 'python-csv',
+    packages = ['pcsv'],
+    version = "0.0.8",
+    description = 'Python tools for manipulating csv files',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/python-csv",
+    download_url = 'https://github.com/jasontrigg0/python-csv/tarball/0.0.8',
+    scripts=[
+        "pcsv/pcsv",
+        "pcsv/pagg",
+        "pcsv/pgraph",
+        "pcsv/pjoin",
+        "pcsv/plook",
+        "pcsv/psort",
+        "pcsv/pset",
+        "pcsv/ptable",
+        "pcsv/pindent",
+        "pcsv/any2csv"
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "xlrd"
+    ],
+    keywords = [],
+    classifiers = [],
+)
