1fb566dc39a68b0333051cb8e175b8680782a758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package tahmid_nester/1.2.0/tahmid_nester-1.2.0.zip

---
+++ packages/tahmid_nester/tahmid_nester-1.2.0.zip/tahmid_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'tahmid_nester',
+    version= '1.2.0',
+    py_module= ['tahmid_nester'],
+    author='Tahmid Rahman',
+    author_gmail='tahmid131@gmail.com',
+    url='http://www.gmail.com',
+    description = 'A simple printer of nested lists',
+    )
