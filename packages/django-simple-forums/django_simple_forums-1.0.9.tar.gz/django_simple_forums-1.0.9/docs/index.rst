b1504eee838009fafb314e6f86843f656e2ea426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:43 +0000

    Add package django-simple-forums/1.0.9/django_simple_forums-1.0.9.tar.gz

---
+++ packages/django-simple-forums/django_simple_forums-1.0.9.tar.gz/django_simple_forums-1.0.9/docs/index.rst
@@ -0,0 +1,26 @@
+.. Simple Forums documentation master file, created by
+   sphinx-quickstart on Tue Feb 16 20:02:49 2016.
+
+.. include:: ../README.rst
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   overview
+   installation
+   configuration
+
+
+.. include:: changelog.rst
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
+
---
+++ packages/django-simple-forums/django_simple_forums-1.0.9.tar.gz/django_simple_forums-1.0.9/simple_forums/views.py
@@ -0,0 +1,98 @@
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+from simple_forums import forms, models
+from simple_forums.utils import thread_detail_url
+
+try:
+    from django.contrib.auth.mixins import LoginRequiredMixin
+except ImportError:
+    from simple_forums.compatability.mixins import LoginRequiredMixin
+
+
+class ThreadCreateView(LoginRequiredMixin, generic.edit.FormView):
+    """ View for creating new threads """
+
+    template_name = 'simple_forums/thread_create.html'
+    form_class = forms.ThreadCreationForm
+
+    def form_valid(self, form):
+        """ Save form if it is valid """
+        thread = form.save(self.request.user)
+
+        return HttpResponseRedirect(thread_detail_url(thread=thread))
+
+
+class ThreadDetailView(generic.DetailView):
+    """ View for getting a thread's details """
+
+    model = models.Thread
+    pk_url_kwarg = 'thread_pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(ThreadDetailView, self).get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated():
+            context['reply_form'] = forms.ThreadReplyForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """ Create a new reply to the current thread """
+        if not request.user.is_authenticated():
+            raise PermissionDenied()
+
+        self.object = self.get_object()
+
+        form = forms.ThreadReplyForm(request.POST)
+
+        if form.is_valid():
+            form.save(request.user, self.object)
+
+            return HttpResponseRedirect(thread_detail_url(thread=self.object))
+
+        context = self.get_context_data()
+        context['reply_form'] = form
+
+        return render(request, self.get_template_names(), context)
+
+
+class ThreadListView(generic.ListView):
+    """ View for listing threads """
+
+    model = models.Thread
+
+    def get_context_data(self, **kwargs):
+        context = super(ThreadListView, self).get_context_data(**kwargs)
+
+        sticky_threads = self._get_base_queryset().filter(sticky=True)
+        context['sticky_thread_list'] = sticky_threads
+
+        topic = get_object_or_404(
+            models.Topic,
+            pk=self.kwargs.get('topic_pk'))
+        context['topic'] = topic
+
+        return context
+
+    def _get_base_queryset(self):
+        """ Retrieve all threads associated with the given topic """
+        topic = get_object_or_404(
+            models.Topic,
+            pk=self.kwargs.get('topic_pk'))
+
+        return self.model.objects.filter(topic=topic)
+
+    def get_queryset(self):
+        """ Return all non-sticky threads """
+        return self._get_base_queryset() \
+            .exclude(sticky=True) \
+            .order_by('-time_last_activity')
+
+
+class TopicListView(generic.ListView):
+    """ View for listing topics """
+
+    model = models.Topic
