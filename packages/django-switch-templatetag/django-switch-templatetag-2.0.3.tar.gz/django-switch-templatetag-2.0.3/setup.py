f16659fd5674e0c54d8df97f7ae8ccb3adc38583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package django-switch-templatetag/2.0.3/django-switch-templatetag-2.0.3.tar.gz

---
+++ packages/django-switch-templatetag/django-switch-templatetag-2.0.3.tar.gz/django-switch-templatetag-2.0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-switch-templatetag',
+
+    url="https://chris-lamb.co.uk/projects/django-switch-templatetag",
+    version='2.0.3',
+    description="Simple switch tag for Django templates.",
+
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+
+    packages=find_packages(),
+    include_package_data=True,
+)
