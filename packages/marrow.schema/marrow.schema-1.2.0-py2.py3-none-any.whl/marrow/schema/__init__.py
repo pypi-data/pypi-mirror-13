4a4d73cb87549b83a6576ce5ce258449f38e4303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:02 +0000

    Add package marrow.schema/1.2.0/marrow.schema-1.2.0-py2.py3-none-any.whl

---
+++ packages/marrow.schema/marrow.schema-1.2.0-py2.py3-none-any.whl/marrow/schema/__init__.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+from .release import version as __version__
+from .meta import Element
+from .declarative import Container, DataAttribute, Attribute, CallbackAttribute
+from .exc import Concern
+from .util import Attributes
