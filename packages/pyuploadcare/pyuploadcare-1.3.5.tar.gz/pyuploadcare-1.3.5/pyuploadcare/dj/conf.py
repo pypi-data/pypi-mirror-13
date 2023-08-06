4509591f83b7a7597c54ce5e051db07aa8017511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:52 +0000

    Add package pyuploadcare/1.3.5/pyuploadcare-1.3.5.tar.gz

---
+++ packages/pyuploadcare/pyuploadcare-1.3.5.tar.gz/pyuploadcare-1.3.5/pyuploadcare/dj/conf.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+from .. import conf
+
+
+if not hasattr(settings, 'UPLOADCARE'):
+    raise ImproperlyConfigured('UPLOADCARE setting must be set')
+
+if 'pub_key' not in settings.UPLOADCARE:
+    raise ImproperlyConfigured('UPLOADCARE setting must have pub_key')
+if 'secret' not in settings.UPLOADCARE:
+    raise ImproperlyConfigured('UPLOADCARE setting must have secret')
+
+
+conf.pub_key = settings.UPLOADCARE['pub_key']
+conf.secret = settings.UPLOADCARE['secret']
+conf.user_agent_name = 'pyuploadcare-django'
+
+if 'cdn_base' in settings.UPLOADCARE:
+    conf.cdn_base = settings.UPLOADCARE['cdn_base']
+
+
+widget_version = settings.UPLOADCARE.get('widget_version', '2.5.5')
+widget_variant = settings.UPLOADCARE.get('widget_variant', 'full.min')
+
+hosted_url = 'https://ucarecdn.com/widget/{version}/uploadcare/uploadcare.{variant}.js'.format(
+    version=widget_version,
+    variant=widget_variant,
+)
+
+local_url = 'uploadcare/uploadcare.{variant}.js'.format(
+    variant=widget_variant)
+
+use_hosted_assets = settings.UPLOADCARE.get(
+    'use_hosted_assets',
+    getattr(settings, 'PYUPLOADCARE_USE_HOSTED_ASSETS', True)
+)
+
+if use_hosted_assets:
+    uploadcare_js = hosted_url
+else:
+    uploadcare_js = settings.UPLOADCARE.get(
+        'widget_url',
+        getattr(settings, 'PYUPLOADCARE_WIDGET_URL', local_url)
+    )
+
+upload_base_url = settings.UPLOADCARE.get(
+    'upload_base_url',
+    getattr(settings, 'PYUPLOADCARE_UPLOAD_BASE_URL', None)
+)
