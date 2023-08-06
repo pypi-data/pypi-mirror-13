3354b5b8261905312c24ae6a7dfac0425889a5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:15 +0000

    Add package virtenviro/0.7.8/virtenviro-0.7.8.zip

---
+++ packages/virtenviro/virtenviro-0.7.8.zip/virtenviro-0.7.8/virtenviro/admin/urls.py
@@ -0,0 +1,46 @@
+# ~*~ coding: utf-8 ~*~
+from django.conf.urls import url, include
+from django.conf import settings
+from django import __version__
+
+if __version__.startswith('1.9'):
+    app_name = 'vadmin'
+
+    urlpatterns = [
+        url(r'^$', 'virtenviro.admin.views.index', name='home'),
+    ]
+
+    if 'virtenviro.content' in settings.INSTALLED_APPS:
+        urlpatterns += [
+            url(r'^content/$', 'virtenviro.content.admin_views.content_page', name='content'),
+            url(r'^content/(?P<page_id>\d+)/$', 'virtenviro.content.admin_views.content_page_edit',
+                name='content_page_edit'),
+            url(r'^content/add/$', 'virtenviro.content.admin_views.content_page_add', name='content_page_add'),
+        ]
+
+    if 'virtenviro.shop' in settings.INSTALLED_APPS:
+        urlpatterns += [
+            url(r'^shop/$', 'virtenviro.admin.views.shop', name='vadmin_shop'),
+            url(r'^shop/import_yml/$', 'virtenviro.shop.views.import_yml', name='vadmin_import_yml'),
+        ]
+else:
+    from django.conf.urls import patterns
+
+    urlpatterns = patterns('',
+                           url(r'^$', 'virtenviro.admin.views.index', name='home'), )
+
+    if 'virtenviro.content' in settings.INSTALLED_APPS:
+        urlpatterns += patterns('',
+                                url(r'^content/$', 'virtenviro.content.admin_views.content_page', name='content'),
+                                url(r'^content/(?P<page_id>\d+)/$', 'virtenviro.content.admin_views.content_page_edit',
+                                    name='content_page_edit'),
+                                url(r'^content/add/$', 'virtenviro.content.admin_views.content_page_add',
+                                    name='content_page_add'),
+                                )
+
+    if 'virtenviro.shop' in settings.INSTALLED_APPS:
+        urlpatterns += patterns('',
+                                url(r'^shop/$', 'virtenviro.admin.views.shop', name='vadmin_shop'),
+                                url(r'^shop/import_yml/$', 'virtenviro.shop.views.import_yml',
+                                    name='vadmin_import_yml'),
+                                )
