02f34cce8830852df95ee01ff8b0bccfa0fe2c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:10 +0000

    Add package myfibo/1.0.0/myfibo-1.0.0.zip

---
+++ packages/myfibo/myfibo-1.0.0.zip/myfibo-1.0.0/set.py
@@ -0,0 +1,11 @@
+from distutils.core	import setup
+setup(
+    name         ='myfibo',
+    version      ='1.0.0',
+    py_modules   =['myfibo'],
+    author       ='liucancheng',
+    author_email ='122645966@qq.com',
+    url          ='http://www.headfirstlabs.com',
+    description  ='a simple fibo',
+    )
+ 
