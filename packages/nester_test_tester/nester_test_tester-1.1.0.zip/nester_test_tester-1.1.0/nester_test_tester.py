a0cd3a152092e0c06990598da4ea9548d7de946f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package nester_test_tester/1.1.0/nester_test_tester-1.1.0.zip

---
+++ packages/nester_test_tester/nester_test_tester-1.1.0.zip/nester_test_tester-1.1.0/nester_test_tester.py
@@ -0,0 +1,11 @@
+'''This is the "nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists.'''
+
+def print_lol(the_list, tab):
+   for each_item in the_list:
+       if isinstance(each_item, list):
+               print_lol(each_item, tab+1)
+       else:
+          for ntab in range(tab):
+             print("/t",end='')
+          print(each_item)
