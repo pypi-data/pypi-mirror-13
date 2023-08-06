83f7db14b5b9115f27c848ebb7cf8797af8af6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:51 +0000

    Add package nester_wing/1.1.3/nester_wing-1.1.3.zip

---
+++ packages/nester_wing/nester_wing-1.1.3.zip/nester_wing-1.1.3/nester_wing.py
@@ -0,0 +1,19 @@
+﻿#这是"nester.py"模块
+#提供了一个 print_lol() 函数,
+#这个函数的作用是打印列表,
+
+#其中有可能包含嵌套列表
+
+#添加列表内缩进功能
+
+#兼容性API
+#添加indent属性,可对是否打印进行选择
+def print_lol(the_list,indent=False,level=0):
+  for each_outher in the_list:
+    if isinstance(each_outher,list):
+      print_lol(each_outher,indent,level+1)
+    else:
+      if indent:
+        for tab_stop in range(level):
+          print('\t',end='')
+      print(each_outher)
---
+++ packages/nester_wing/nester_wing-1.1.3.zip/nester_wing-1.1.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='nester_wing',
+  version='1.1.3',
+  py_modules=['nester_wing'],
+  author='gui_jia',
+  author_email='gui_jia@wo.cn',
+  url='http://www.gzmeo.com',
+  description='I am wing and the e-mail is:<gui_jia@wo.cn>,nice to meet you',)
