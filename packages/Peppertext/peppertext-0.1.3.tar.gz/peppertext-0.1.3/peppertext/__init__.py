0fa8f71ef93225064c1513e120d1496fb3a6b174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:22 +0000

    Add package Peppertext/0.1.3/peppertext-0.1.3.tar.gz

---
+++ packages/Peppertext/peppertext-0.1.3.tar.gz/peppertext-0.1.3/peppertext/__init__.py
@@ -0,0 +1,2 @@
+from .base import selector, resolve, register, register_selector, \
+        EntityField, DateFormatField, SimpleURLField, Hypertext
---
+++ packages/Peppertext/peppertext-0.1.3.tar.gz/peppertext-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='peppertext',
+    version="0.1.3",
+    description='Declarative hypertext client',
+    author='Kyungil Choi',
+    author_email='hanpama@gmail.com',
+    url='https://github.com/hanpama/peppertext',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    install_requires=[
+        "pyquery>=1.2.9",
+        "requests>=2.8.1",
+        "six",
+    ],
+    zip_safe=False,
+)
