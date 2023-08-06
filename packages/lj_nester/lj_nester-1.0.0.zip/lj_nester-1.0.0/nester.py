fd586f94c17ea1607200713eeb768201d6b43d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:02 +0000

    Add package lj_nester/1.0.0/lj_nester-1.0.0.zip

---
+++ packages/lj_nester/lj_nester-1.0.0.zip/lj_nester-1.0.0/nester.py
@@ -0,0 +1,10 @@
+"""This is the "nester.py" module and it provides one function called print_lol()"""
+
+def print_lol(the_list):
+    """This function takes one positional argument called "the_list"."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
