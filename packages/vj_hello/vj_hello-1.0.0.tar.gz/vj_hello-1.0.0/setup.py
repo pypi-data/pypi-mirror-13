fe46bf795ef3881f8ce5ca7a9aaa29cc4fcb0a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:28 +0000

    Add package vj_hello/1.0.0/vj_hello-1.0.0.tar.gz

---
+++ packages/vj_hello/vj_hello-1.0.0.tar.gz/vj_hello-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="vj_hello",
+    version="1.0.0",
+    py_modules=['vj_hello'],
+    author='vijay',
+    author_email='vijayktechno@gmail.com',
+    url='',
+    description='say hello to all'
+)
---
+++ packages/vj_hello/vj_hello-1.0.0.tar.gz/vj_hello-1.0.0/vj_hello.py
@@ -0,0 +1,6 @@
+"""this function is to say hello to list of names"""
+def say_hello_to_all(names):
+    for name in names:
+        print("hello "+name)
+
+        
