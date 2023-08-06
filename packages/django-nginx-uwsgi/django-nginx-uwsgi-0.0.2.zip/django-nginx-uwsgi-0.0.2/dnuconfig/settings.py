c887baf0dccc6a0acf57ef6bb256ca5e43e5857e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:19 +0000

    Add package django-nginx-uwsgi/0.0.2/django-nginx-uwsgi-0.0.2.zip

---
+++ packages/django-nginx-uwsgi/django-nginx-uwsgi-0.0.2.zip/django-nginx-uwsgi-0.0.2/dnuconfig/settings.py
@@ -0,0 +1,9 @@
+#~*~ coding: utf-8 ~*~
+import os
+
+NGINX_DIR = os.path.dirname('/srv/nginx/')
+VASSALS_DIR = os.path.dirname('/srv/vassals/')
+SOCKS_DIR = os.path.dirname('/srv/socks/')
+DJANGO_SETTINGS_MODULE = 'settings'
+LOGS_DIR = os.path.dirname('/srv/logs/')
+MODULE_TO_RUN = 'wsgi'
