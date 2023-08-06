1b53c526408e5b221de8daa0de3ca8dc20a841e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:35 +0000

    Add package ka-lite/0.16a1/ka-lite-0.16a1.zip

---
+++ packages/ka-lite/ka-lite-0.16a1.zip/ka-lite-0.16a1/kalite/i18n/tests/__init__.py
@@ -0,0 +1,2 @@
+from base import *
+from browser_tests import *
---
+++ packages/ka-lite/ka-lite-0.16a1.zip/ka-lite-0.16a1/kalite/main/api_resources.py
@@ -0,0 +1,143 @@
+import json
+from tastypie import fields
+from tastypie.resources import ModelResource, Resource
+from tastypie.exceptions import NotFound
+from django.conf.urls import url
+from django.conf import settings
+
+from .models import VideoLog, ExerciseLog, AttemptLog, ContentLog, ContentRating
+
+from kalite.distributed.api_views import get_messages_for_api_calls
+from kalite.topic_tools.settings import CHANNEL
+from kalite.topic_tools.content_models import get_assessment_item_data
+from kalite.shared.api_auth.auth import UserObjectsOnlyAuthorization
+from kalite.facility.api_resources import FacilityUserResource
+
+
+
+class ExerciseLogResource(ModelResource):
+
+    user = fields.ForeignKey(FacilityUserResource, 'user')
+
+    class Meta:
+        queryset = ExerciseLog.objects.all()
+        resource_name = 'exerciselog'
+        filtering = {
+            "exercise_id": ('exact', 'in', ),
+            "user": ('exact', ),
+        }
+        authorization = UserObjectsOnlyAuthorization()
+
+    def obj_create(self, bundle, **kwargs):
+        is_admin = getattr(bundle.request, "is_admin", False)
+        user = getattr(bundle.request, "user", None)
+        if is_admin:
+            if user and getattr(user, 'is_superuser', False):
+                return None
+        return super(ExerciseLogResource, self).obj_create(bundle, **kwargs)
+
+
+class AttemptLogResource(ModelResource):
+
+    user = fields.ForeignKey(FacilityUserResource, 'user')
+
+    class Meta:
+        queryset = AttemptLog.objects.all().order_by("-timestamp")
+        resource_name = 'attemptlog'
+        filtering = {
+            "exercise_id": ('exact', ),
+            "user": ('exact', ),
+            "context_type": ('exact', 'in', ),
+        }
+        ordering = [
+            "timestamp",
+        ]
+        authorization = UserObjectsOnlyAuthorization()
+
+    def obj_create(self, bundle, **kwargs):
+        is_admin = getattr(bundle.request, "is_admin", False)
+        user = getattr(bundle.request, "user", None)
+        if is_admin:
+            if user and getattr(user, 'is_superuser', False):
+                return None
+        return super(AttemptLogResource, self).obj_create(bundle, **kwargs)
+
+
+class ContentLogResource(ModelResource):
+
+    user = fields.ForeignKey(FacilityUserResource, 'user')
+
+    class Meta:
+        queryset = ContentLog.objects.all()
+        resource_name = 'contentlog'
+        filtering = {
+            "content_id": ('exact', 'in', ),
+            "user": ('exact', ),
+        }
+        authorization = UserObjectsOnlyAuthorization()
+
+
+class VideoLogResource(ModelResource):
+
+    user = fields.ForeignKey(FacilityUserResource, 'user')
+
+    class Meta:
+        queryset = VideoLog.objects.all()
+        resource_name = 'videolog'
+        filtering = {
+            "video_id": ('exact', 'in', ),
+            "user": ('exact', ),
+        }
+        authorization = UserObjectsOnlyAuthorization()
+
+
+class AssessmentItemResource(ModelResource):
+    class Meta:
+        resource_name = 'assessment_item'
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<id>[\w\d_.-]+)/$" % self._meta.resource_name,
+                self.wrap_view('dispatch_detail'),
+                name="api_dispatch_detail"),
+        ]
+
+
+    def obj_get(self, bundle, **kwargs):
+        id = kwargs.get("id")
+        assessment_item = get_assessment_item_data(channel=getattr(bundle.request, "channel", "khan"), language=getattr(bundle.request, "language", "en"), assessment_item_id=id)
+        if assessment_item:
+            return AssessmentItem(**assessment_item)
+        else:
+            raise NotFound('AssessmentItem with id %s not found' % id)
+
+
+    def obj_create(self, bundle, **kwargs):
+        raise NotImplemented("Operation not implemented yet for assessment_items.")
+
+    def obj_update(self, bundle, **kwargs):
+        raise NotImplemented("Operation not implemented yet for assessment_items.")
+
+    def obj_delete_list(self, bundle, **kwargs):
+        raise NotImplemented("Operation not implemented yet for assessment_items.")
+
+    def obj_delete(self, bundle, **kwargs):
+        raise NotImplemented("Operation not implemented yet for assessment_items.")
+
+    def rollback(self, bundles):
+        raise NotImplemented("Operation not implemented yet for assessment_items.")
+
+
+class ContentRatingResource(ModelResource):
+
+    user = fields.ForeignKey(FacilityUserResource, 'user')
+
+    class Meta:
+        resource_name = 'content_rating'
+        queryset = ContentRating.objects.all()
+        filtering = {
+            "content_id": ('exact', 'in', ),
+            "content_kind": ('exact', 'in', ),
+            "user": ('exact', ),
+        }
+        authorization = UserObjectsOnlyAuthorization()
