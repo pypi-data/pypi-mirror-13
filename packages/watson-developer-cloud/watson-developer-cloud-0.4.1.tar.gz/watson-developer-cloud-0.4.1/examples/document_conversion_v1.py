00e032e98c22092462a189674e1ac9358b94f699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:53 +0000

    Add package watson-developer-cloud/0.4.1/watson-developer-cloud-0.4.1.tar.gz

---
+++ packages/watson-developer-cloud/watson-developer-cloud-0.4.1.tar.gz/watson-developer-cloud-0.4.1/examples/document_conversion_v1.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import json
+from os.path import join, dirname
+from watson_developer_cloud import DocumentConversionV1 as DocumentConversion
+
+
+document_conversion = DocumentConversion(username='YOUR SERVICE USERNAME',
+                                         password='YOUR SERVICE PASSWORD',
+                                         version='2016-02-09')
+
+with open(join(dirname(__file__), '../resources/sample-docx.docx'), 'rb') as document:
+    config = {'conversion_target': DocumentConversion.ANSWER_UNITS}
+    print(json.dumps(document_conversion.convert_document(document=document, config=config), indent=2))
