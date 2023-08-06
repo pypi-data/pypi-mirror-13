814ddf9078ac4a646b46b34e48ffa6357f4fc997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package passpie/1.3.1/passpie-1.3.1.tar.gz

---
+++ packages/passpie/passpie-1.3.1.tar.gz/passpie-1.3.1/passpie/database.py
@@ -0,0 +1,117 @@
+from datetime import datetime
+import logging
+import os
+import shutil
+
+from tinydb import TinyDB, Storage, where, Query
+import yaml
+
+from .utils import mkdir_open
+from .history import Repository
+from .credential import split_fullname, make_fullname
+
+
+class PasspieStorage(Storage):
+    extension = ".pass"
+
+    def __init__(self, path):
+        super(PasspieStorage, self).__init__()
+        self.path = path
+
+    def delete(self, credentials):
+        for cred in credentials:
+            dirname, filename = cred["name"], cred["login"] + self.extension
+            credpath = os.path.join(self.path, dirname, filename)
+            os.remove(credpath)
+            if not os.listdir(os.path.dirname(credpath)):
+                shutil.rmtree(os.path.dirname(credpath))
+
+    def read(self):
+        elements = []
+        for rootdir, dirs, files in os.walk(self.path):
+            filenames = [f for f in files if f.endswith(self.extension)]
+            for filename in filenames:
+                docpath = os.path.join(rootdir, filename)
+                with open(docpath) as f:
+                    elements.append(yaml.load(f.read()))
+
+        return {"_default":
+                {idx: elem for idx, elem in enumerate(elements, start=1)}}
+
+    def write(self, data):
+        deleted = [c for c in self.read()["_default"].values()
+                   if c not in data["_default"].values()]
+        self.delete(deleted)
+
+        for eid, cred in data["_default"].items():
+            dirname, filename = cred["name"], cred["login"] + self.extension
+            credpath = os.path.join(self.path, dirname, filename)
+            with mkdir_open(credpath, "w") as f:
+                f.write(yaml.dump(dict(cred), default_flow_style=False))
+
+
+class Database(TinyDB):
+
+    def __init__(self, config, storage=PasspieStorage):
+        self.config = config
+        self.path = config['path']
+        self.repo = Repository(self.path,
+                               autopull=config.get('autopull'),
+                               autopush=config.get('autopush'))
+        PasspieStorage.extension = config['extension']
+        super(Database, self).__init__(self.path, storage=storage)
+
+    def has_keys(self):
+        return os.path.exists(os.path.join(self.path, '.keys'))
+
+    def credential(self, fullname):
+        login, name = split_fullname(fullname)
+        Credential = Query()
+        if login is None:
+            creds = self.get(Credential.name == name)
+        else:
+            creds = self.get((Credential.login == login) & (Credential.name == name))
+        return creds
+
+    def add(self, fullname, password, comment):
+        login, name = split_fullname(fullname)
+        if login is None:
+            logging.error('Cannot add credential with empty login. use "@<name>" syntax')
+            return None
+        credential = dict(fullname=fullname,
+                          name=name,
+                          login=login,
+                          password=password,
+                          comment=comment,
+                          modified=datetime.now())
+        self.insert(credential)
+        return credential
+
+    def update(self, fullname, values):
+        values['fullname'] = make_fullname(values["login"], values["name"])
+        values['modified'] = datetime.now()
+        self.table().update(values, (where("fullname") == fullname))
+
+    def credentials(self, fullname=None):
+        if fullname:
+            login, name = split_fullname(fullname)
+            Credential = Query()
+            if login is None:
+                creds = self.search(Credential.name == name)
+            else:
+                creds = self.search((Credential.login == login) & (Credential.name == name))
+        else:
+            creds = self.all()
+        return sorted(creds, key=lambda x: x["name"] + x["login"])
+
+    def remove(self, fullname):
+        self.table().remove(where('fullname') == fullname)
+
+    def matches(self, regex):
+        Credential = Query()
+        credentials = self.search(
+            Credential.name.matches(regex) |
+            Credential.login.matches(regex) |
+            Credential.comment.matches(regex)
+        )
+        return sorted(credentials, key=lambda x: x["name"] + x["login"])
---
+++ packages/passpie/passpie-1.3.1.tar.gz/passpie-1.3.1/tests/test_config.py
@@ -0,0 +1,147 @@
+import passpie.config
+import yaml
+
+
+def mock_open():
+    try:
+        from mock import mock_open as mopen
+    except:
+        from unittest.mock import mock_open as mopen
+    return mopen()
+
+
+def test_config_read_opens_path_and_load_yaml_content(mocker):
+    config_file = mocker.patch('passpie.config.open', mock_open(), create=True)()
+    mock_yaml = mocker.patch('passpie.config.yaml')
+
+    passpie.config.read('path')
+    assert mock_yaml.load.called
+    mock_yaml.load.assert_called_once_with(config_file.read())
+
+
+def test_config_read_logs_debug_when_config_file_not_found_and_returns_default(mocker):
+    mocker.patch('passpie.config.open', side_effect=IOError, create=True)
+    mock_logging = mocker.patch('passpie.config.logging')
+
+    result = passpie.config.read('path')
+    assert result == passpie.config.DEFAULT
+    assert mock_logging.debug.called
+    mock_logging.debug.assert_called_once_with('config file "path" not found')
+
+
+def test_config_read_logs_error_when_config_file_malformed_found_and_returns_empty(mocker):
+    mocker.patch('passpie.config.open', side_effect=yaml.scanner.ScannerError, create=True)
+    mock_logging = mocker.patch('passpie.config.logging')
+
+    result = passpie.config.read('path')
+    assert result == {}
+    assert mock_logging.error.called
+    mock_logging.error.assert_called_once_with('Malformed user configuration file: path')
+
+
+def test_config_load_gets_settings_from_local_config_when_exists(mocker):
+    expected_config = {
+        'short_commands': True
+    }
+    mocker.patch('passpie.config.os.path.exists', return_value=True)
+    mocker.patch('passpie.config.read', return_value=expected_config)
+
+    configuration = passpie.config.load()
+    assert configuration is not None
+    assert configuration['short_commands'] is expected_config['short_commands']
+
+def test_config_load_overrides_global_config_with_local_config_vars(mocker):
+    global_config = {
+        'genpass_pattern': "[\w]{16}",
+        'short_commands': True
+    }
+    local_config = {
+        'genpass_pattern': "[\w]{32}"
+    }
+    mocker.patch('passpie.config.os.path.exists', return_value=True)
+    mocker.patch('passpie.config.read', return_value=local_config)
+    mocker.patch('passpie.config.read_global_config', return_value=global_config)
+
+    configuration = passpie.config.load()
+    assert configuration is not None
+    assert configuration['genpass_pattern'] == local_config['genpass_pattern']
+    assert configuration['short_commands'] == global_config['short_commands']
+
+def test_config_load_has_default_config_loaded(mocker):
+    default_config = passpie.config.DEFAULT
+    global_config = {
+        'genpass_pattern': "[\w]{16}",
+        'short_commands': True
+    }
+    local_config = {
+        'genpass_pattern': "[\w]{32}"
+    }
+    mocker.patch('passpie.config.os.path.exists', return_value=True)
+    mocker.patch('passpie.config.read', return_value=local_config)
+    mocker.patch('passpie.config.read_global_config', return_value=global_config)
+
+    configuration = passpie.config.load()
+    assert configuration is not None
+    assert configuration['path'] == default_config['path']
+    assert configuration['key_length'] == default_config['key_length']
+    assert configuration['table_format'] == default_config['table_format']
+
+def test_config_load_has_default_config_loaded(mocker):
+    default_config = passpie.config.DEFAULT
+    global_config = {
+        'genpass_pattern': "[\w]{16}",
+        'short_commands': True
+    }
+    local_config = {
+        'genpass_pattern': "[\w]{32}"
+    }
+    mocker.patch('passpie.config.os.path.exists', return_value=True)
+    mocker.patch('passpie.config.read', return_value=local_config)
+    mocker.patch('passpie.config.read_global_config', return_value=global_config)
+
+    configuration = passpie.config.load()
+    assert configuration is not None
+    assert configuration['path'] == default_config['path']
+    assert configuration['key_length'] == default_config['key_length']
+    assert configuration['table_format'] == default_config['table_format']
+
+
+def test_config_load_with_overrides_override_loaded_config(mocker):
+    global_config = {
+        'genpass_pattern': "[\w]{16}",
+        'short_commands': True
+    }
+    local_config = {
+        'genpass_pattern': "[\w]{32}"
+    }
+
+    short_commands=False
+    genpass_pattern=10
+    mocker.patch('passpie.config.os.path.exists', return_value=True)
+    mocker.patch('passpie.config.read', return_value=local_config)
+    mocker.patch('passpie.config.read_global_config', return_value=global_config)
+
+    overrides = dict(short_commands=short_commands, genpass_pattern=genpass_pattern)
+    configuration = passpie.config.load(**overrides)
+    assert configuration is not None
+    assert configuration['genpass_pattern'] == genpass_pattern
+    assert configuration['short_commands'] == short_commands
+
+
+def test_config_create_adds_an_empty_dot_config_file_to_path_when_default_false(mocker):
+    config_file = mocker.patch('passpie.config.open', mock_open(), create=True)()
+    mock_yaml_dump = mocker.patch('passpie.config.yaml.dump')
+    overrides = {}
+    passpie.config.create('path', overrides)
+
+    config_file.write.assert_called_once_with(mock_yaml_dump(overrides, default_flow_style=False))
+
+
+def path_is_repo_url(mocker):
+    assert is_repo_url('https://foo@example.com/user/repo.git')
+    assert is_repo_url('https://github.com/marcwebbie/passpie.git')
+    assert is_repo_url('git@github.com:marcwebbie/passpie.git')
+    assert not is_repo_url('http://example.com')
+    assert not is_repo_url(None)
+    assert not is_repo_url('')
+    assert not is_repo_url('++++++++++++++')
