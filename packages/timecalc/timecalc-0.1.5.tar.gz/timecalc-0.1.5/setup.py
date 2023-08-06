1e3d42a27a073ae747d8d7738d902858e628bc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package timecalc/0.1.5/timecalc-0.1.5.tar.gz

---
+++ packages/timecalc/timecalc-0.1.5.tar.gz/timecalc-0.1.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name="timecalc",
+    version="0.1.5",
+    description="a tool to make the imitation of Lyubishchev and his time billing method easier",
+    author="Xin YUE",
+    author_email="yuecn41@gmail.com",
+    url="https://github.com/yuex/timecalc",
+    package_dir={'': 'src'},
+    py_modules=['timecalc'],
+    install_requires=[
+        "PyYAML==3.11",
+    ],
+    entry_points={
+        'console_scripts': ['timecalc=timecalc:main'],
+    },
+)
