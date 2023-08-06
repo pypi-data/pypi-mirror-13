6a5db82fe9215427e5382c23e59a997d092f79a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:11 +0000

    Add package DSImgur/0.1.0.2/DSImgur-0.1.0.2.tar.gz

---
+++ packages/DSImgur/DSImgur-0.1.0.2.tar.gz/DSImgur-0.1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+  name = 'DSImgur',
+  version = '0.1.0.2',
+  description = 'Easily download images, Albums, Galleries and entire Profiles from Imgur. The most powerful Imgur Downloader!! You can use as program or as module!',
+  url = 'https://github.com/DiSiqueira/DSImgur',
+  author = 'Diego Siqueira',
+  author_email = 'dieg0@live.com',
+  license = 'MIT',
+  package_dir = { 'DSImgur' : 'src' },
+  packages = [ 'DSImgur' ],
+  zip_safe = False, 
+  keywords = ['download', 'thread', 'speed', 'resume', 'multi', 'simple'],
+  entry_points = 
+  {
+      'console_scripts': 
+      [
+          'dsimgur = DSImgur:main',
+      ],
+  },
+  install_requires = ['DSDownload']
+)
