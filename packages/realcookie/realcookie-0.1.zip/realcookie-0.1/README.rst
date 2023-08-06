a59231cf972e98bb563946eb4d9d0ac1245d0c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:09 +0000

    Add package realcookie/0.1/realcookie-0.1.zip

---
+++ packages/realcookie/realcookie-0.1.zip/realcookie-0.1/README.rst
@@ -0,0 +1,19 @@
+Benolot's cookie API, python version
+------------------------------------
+
+This package gives you three functions, add, gib, and count, explained below:
+
+add
+  The 'add' function takes a cookie image url as argument and saves that image to a database folder.
+gib
+  The 'gib' function takes no argumens and returns the path to one of the images in the previously mentioned databse folder, randomely picked.
+count
+  The 'count' function returns the number of images currently in the database folder.
+
+
+To use, simply do::
+
+    >>> import realcookie
+    >>> a = realcookie.cookie()
+    >>> a.add('some cookie image url')
+    >>> a.gib()
---
+++ packages/realcookie/realcookie-0.1.zip/realcookie-0.1/realcookie/realcookie.py
@@ -0,0 +1,32 @@
+import random
+import os
+import urllib.request
+
+
+class cookie:
+
+    def __init__(self):
+        self.folder = 'cookies'
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
+        self.img_number = len(os.listdir(self.folder))
+
+    # returns the path to a random cookie in the cookies folder
+    def gib(self):
+        a = random.randint(1, self.img_number + 1)
+        path = self.folder + '/' + str(a) + '.png'
+        return path
+
+    # dowloads a cookie from image url to the cookies file    
+    def add(self, img_url):
+        try:
+            filename = os.listdir(self.folder)
+            last_name = filename[-1].replace('.png', '')
+            new_name = str(int(last_name) + 1) + '.png'
+        except IndexError:
+            new_name = '1.png'
+        urllib.request.urlretrieve(img_url, 'cookies/' + new_name)
+
+    # returns the number of cookie images in the folder
+    def count(self):
+        return self.img_number
