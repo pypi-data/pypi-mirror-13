30a4300f8bd73d482baea4c462c7cd1dcd025576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:45 +0000

    Add package qiqi20160209/1.0.0/qiqi20160209-1.0.0.tar.gz

---
+++ packages/qiqi20160209/qiqi20160209-1.0.0.tar.gz/qiqi20160209-1.0.0/nester.py
@@ -0,0 +1,9 @@
+'''this is moudle memory
+'''
+def print_lol(the_list):
+    '''this is the def memory'''
+    for each_item in the_list:
+         if isinstance(each_item,list):
+             print_lol(each_item)
+         else:
+            print(each_item)
