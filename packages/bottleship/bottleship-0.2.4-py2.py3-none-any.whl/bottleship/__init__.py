8a447d46ab95c4376905f40da086c056460ff641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:21 +0000

    Add package bottleship/0.2.4/bottleship-0.2.4-py2.py3-none-any.whl

---
+++ packages/bottleship/bottleship-0.2.4-py2.py3-none-any.whl/bottleship/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Oscar Martinez'
+__email__ = 'omtinez@gmail.com'
+__version__ = '0.2.4'
+__all__ = ['BottleShip', 'data_encode', 'data_decode', 'data_is_encoded']
+
+from bottle import *
+from .bottleship import BottleShip, data_encode, data_decode, data_is_encoded
