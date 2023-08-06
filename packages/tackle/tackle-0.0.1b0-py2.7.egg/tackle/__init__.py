1c231070dfb0e780005126bc3059b8839f623975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:56 +0000

    Add package tackle/0.0.1b0/tackle-0.0.1b0-py2.7.egg

---
+++ packages/tackle/tackle-0.0.1b0-py2.7.egg/tackle/__init__.py
@@ -0,0 +1,23 @@
+
+from util import cached_property
+from wsgi import (
+    WSGIService,
+    WSGIApplication,
+    WSGIRequestHandler as RequestHandler,
+    sendfile
+)
+
+from middleware import (
+    Middleware,
+    RedirectionMiddleware,
+    StaticFileMiddleware,
+    Shortener
+)
+
+__version__ = '0.0.1b'
+__author__ = [
+    'Sam Briesemeister <sam.briesemeister@gmail.com>'
+]
+
+__license__ = "Python Software Foundation"
+
