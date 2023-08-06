310bee7a07990408ea483e4d5e61d72cabdca26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package ChineseTone/0.1.1/ChineseTone-0.1.1.tar.gz

---
+++ packages/ChineseTone/ChineseTone-0.1.1.tar.gz/ChineseTone-0.1.1/ChineseTone/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+"""
+@author:   letian
+@homepage: http://www.letiantian.me
+@github:   https://github.com/someus/
+"""
+
+from __future__ import absolute_import
+
+__author__   = 'letian'
+__version__  = '0.1.0'
+
+from .chinesetone import PinyinFormat, PinyinResource, PinyinException, PinyinHelper, ChineseHelper
