3fcbb3a0dcb3137a286d002928a28886de80e747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:48 +0000

    Add package qiqi20160209/1.4.2/qiqi20160209-1.4.2.tar.gz

---
+++ packages/qiqi20160209/qiqi20160209-1.4.2.tar.gz/qiqi20160209-1.4.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='qiqi20160209',
+    version='1.4.2',
+    py_modules=['nester'],
+    author='qiqi',
+    author_email='qqq@qq.com',
+    url='http://www.qiqi.com',
+    description='my first python list'
+    )
