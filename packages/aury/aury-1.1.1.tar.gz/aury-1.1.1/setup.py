97381535374cb509ba82e6e7137d365d78341cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:35 +0000

    Add package aury/1.1.1/aury-1.1.1.tar.gz

---
+++ packages/aury/aury-1.1.1.tar.gz/aury-1.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aury",
+    version="1.1.1",
+    author="Fabien Devaux",
+    author_email="fdev31@gmail.com",
+    license="MIT",
+    platform="all",
+    description="AUR (archlinux) packages maintenance made easy via PyPi lookup",
+    long_description=open('README.md').read(),
+    scripts=['aury'],
+    packages=find_packages(),
+    url='https://github.com/fdev31/aury',
+    zip_safe=True,
+    keywords=['packaging', 'archlinux', 'automate', 'aur', 'pacman'],
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Archiving :: Packaging',
+    ]
+)
+
