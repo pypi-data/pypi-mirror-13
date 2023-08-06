8e798dc9e6fb61956c9c42437059882c6ecdfbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:28 +0000

    Add package mashape-analytics/1.0.8/mashape-analytics-1.0.8.tar.gz

---
+++ packages/mashape-analytics/mashape-analytics-1.0.8.tar.gz/mashape-analytics-1.0.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '1.0.8'
+
+setup(
+  name='mashape-analytics',
+  version=version,
+  description='Python Agent for Mashape Analytics',
+  author='Mashape',
+  author_email='opensource@mashape.com',
+  url='https://github.com/Mashape/analytics-agent-python',
+  download_url = 'https://github.com/Mashape/analytics-agent-python/archive/v{0}.zip'.format(version),
+  license='MIT',
+  packages=find_packages(exclude=['tests']),
+  zip_safe=False,
+  install_requires=[
+    'pytz==2015.2',
+    'pyzmq==14.5.0',
+    'six==1.9.0',
+    'ujson==1.33',
+    'Werkzeug==0.10.4'
+  ],
+  include_package_data=True,
+  classifiers=[
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+  ]
+)
