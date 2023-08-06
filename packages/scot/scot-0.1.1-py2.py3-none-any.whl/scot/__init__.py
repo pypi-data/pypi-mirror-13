8c199a64d43449be8b0116f15ce060071fa62e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:50 +0000

    Add package scot/0.1.1/scot-0.1.1-py2.py3-none-any.whl

---
+++ packages/scot/scot-0.1.1-py2.py3-none-any.whl/scot/__init__.py
@@ -0,0 +1,25 @@
+# Released under The MIT License (MIT)
+# http://opensource.org/licenses/MIT
+# Copyright (c) 2013 SCoT Development Team
+
+""" SCoT: The Source Connectivity Toolbox
+"""
+
+from __future__ import absolute_import
+
+from . import config
+
+# default backend
+# TODO: set default backend in config
+from . import backend_builtin
+
+from .ooapi import Workspace
+
+from .connectivity import Connectivity
+
+from . import datatools
+
+__all__ = ['Workspace', 'Connectivity', 'datatools']
+__version__ = "0.1.1"
+
+backends = ['backend_builtin', 'backend_sklearn']
---
+++ packages/scot/scot-0.1.1-py2.py3-none-any.whl/scot/backend_builtin.py
@@ -0,0 +1,50 @@
+# Released under The MIT License (MIT)
+# http://opensource.org/licenses/MIT
+# Copyright (c) 2013 SCoT Development Team
+
+""" Use internally implemented functions as backend.
+"""
+
+import scipy as sp
+
+from . import config, datatools, pca, csp
+from .var import VAR
+from .external.infomax_ import infomax
+
+
+def wrapper_infomax(data):
+    """ Call Infomax (from MNE) for ICA calculation.
+    """
+    u = infomax(datatools.cat_trials(data)).T
+    m = sp.linalg.pinv(u)
+    return m, u
+
+
+def wrapper_pca(x, reducedim):
+    """ Call SCoT's PCA algorithm.
+    """
+    c, d = pca.pca(datatools.cat_trials(x),
+                   subtract_mean=False, reducedim=reducedim)
+    y = datatools.dot_special(x, c)
+    return c, d, y
+
+
+def wrapper_csp(x, cl, reducedim):
+    c, d = csp.csp(x, cl, numcomp=reducedim)
+    y = datatools.dot_special(x, c)
+    return c, d, y
+
+
+backend = {
+    'ica': wrapper_infomax,
+    'pca': wrapper_pca,
+    'csp': wrapper_csp,
+    'var': VAR
+}
+
+
+def activate():
+    config.backend = backend
+
+
+activate()
