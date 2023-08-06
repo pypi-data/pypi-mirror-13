28cb96a3be8635b0a22c3a4a3047c9e97bf2e0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:49 +0000

    Add package focli/0.8/focli-0.8.tar.gz

---
+++ packages/focli/focli-0.8.tar.gz/focli-0.8/focli/focli.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import argparse
+import json
+import os
+
+from focli.foline import FoliPrint
+from focli import exceptions
+
+CONFIG_PATH = "~/.focli"
+
+
+def main():
+    parser = CliParser()
+    ns = parser.parse_argv()
+    try:
+        if ns.add:
+            ret = add_bookmark(ns)
+        elif ns.delete:
+            ret = del_bookmark(ns)
+        elif ns.list_bookmarks:
+            ret = list_bookmarks()
+        else:
+            ret = show_stops(ns)
+    except exceptions.FoliStopNameException as e:
+        print(str(e.message))
+        ret = 1
+    if ret != 0:
+        parser.print_help()
+    return ret
+
+
+def read_config():
+    try:
+        path = os.path.expanduser(CONFIG_PATH)
+        with open(path, 'r') as fh:
+            return json.load(fh)
+    except IOError:
+        return dict()
+
+
+def write_config(config):
+    try:
+        path = os.path.expanduser(CONFIG_PATH)
+        with open(path, 'w') as fh:
+            json.dump(config, fh)
+    except TypeError:
+        raise
+
+
+def add_bookmark(ns):
+    if len(ns.stopnumber):
+        config = read_config()
+        config[ns.stopnumber[0]] = ns.stopname
+        write_config(config)
+        print("Bookmark for stop {0} added.".format(ns.stopnumber[0]))
+        return 0
+    else:
+        print("Please provide stop number to bookmark")
+        return 1
+
+
+def del_bookmark(ns):
+    if len(ns.stopnumber):
+        config = read_config()
+        if str(ns.stopnumber[0]) in config.keys():
+            config.pop(str(ns.stopnumber[0]), None)
+            write_config(config)
+            print("Bookmark for stop {0} deleted.".format(ns.stopnumber[0]))
+            return 0
+        print("No bookmark for stop {0}.".format(ns.stopnumber[0]))
+        return 1
+    else:
+        print("Please provide stop number to remove.")
+        return 1
+
+
+def list_bookmarks():
+    config = read_config()
+    if config:
+        print("Saved bookmarks:")
+        for bm in config.keys():
+            print("{0} - {1}".format(bm, config[bm]))
+    return 0
+
+
+def show_stops(ns):
+    try:
+        if len(ns.stopnumber):
+            f_print = FoliPrint(ns.stopnumber)
+            f_print.print_lines()
+            return 0
+        else:
+            config = read_config()
+            if config:
+                f_print = FoliPrint(named_stops=config)
+                f_print.print_lines()
+                return 0
+            else:
+                return 1
+    except exceptions.FoliServerException as e:
+        print(e.message)
+        return 1
+
+
+class CliParser:
+    def parse_argv(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-a", dest="add", action="store_true",
+                                 default=False, help="Add line to bookmarks")
+        self.parser.add_argument("-d", dest="delete", action="store_true",
+                                 default=False,
+                                 help="Remove line from bookmarks")
+        self.parser.add_argument("-l", dest="list_bookmarks",
+                                 action="store_true",
+                                 default=False,
+                                 help="List saved bookmarks")
+        self.parser.add_argument("-n", dest="stopname", action="store",
+                                 help="Custom name for the stop to bookmark")
+        self.parser.add_argument("stopnumber", nargs="*",
+                                 help="Stop number to show / add / delete")
+        return self.parser.parse_args()
+
+    def print_help(self):
+        self.parser.print_help()
---
+++ packages/focli/focli-0.8.tar.gz/focli-0.8/focli/foline.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import datetime
+import json
+import math
+
+import requests
+from six.moves import range
+from focli.folitable import FoliTable
+from focli import exceptions
+from blessings import Terminal
+
+
+class FoliPrint:
+    def __init__(self, flines=[], named_stops=dict()):
+        self.stops = []
+        if flines:
+            for l in flines:
+                new_stop = FoliStop(l)
+                new_stop.run()
+                self.stops.append(new_stop)
+        elif named_stops:
+            for l in named_stops.keys():
+                new_stop = FoliStop(l)
+                new_stop.stop_name = named_stops[l]
+                new_stop.run()
+                self.stops.append(new_stop)
+
+    def print_lines(self):
+        tables_handled = 0
+        timetables = self.get_tables()
+        t_per_line = self.fits_line()
+        while len(timetables) > tables_handled:
+            if (len(timetables)-tables_handled) < t_per_line:
+                handle = len(timetables)-tables_handled
+            else:
+                handle = t_per_line
+            p_list = timetables[int(tables_handled):int(tables_handled+handle)]
+            height = max(h.num_lines() for h in p_list)
+            for i in range(height):
+                line = " ".join([t.get_row(i) for t in p_list])
+                print(line)
+            tables_handled += handle
+
+    def fits_line(self):
+        term = Terminal()
+        return math.floor(term.width/33)
+
+    def get_tables(self):
+        tables = []
+        for s in self.stops:
+            t = FoliTable(s.stop_name)
+            journeys = self.filter_list(s.journeys)
+            for jo in journeys:
+                f_timediff = jo.timediff-jo.time
+                line_time = jo.time + f_timediff
+                f_time = "{0:02d}:{1:02d}:{2:02d}".format(line_time.hour,
+                                                         line_time.minute,
+                                                         line_time.second)
+                t.add_row(f_time, jo.name, jo.timediff_min_sec(),
+                          f_timediff.seconds)
+            tables.append(t)
+        return tables
+
+    def filter_list(self, flist, start_min=-5, end_min=60):
+        now = datetime.datetime.now()
+        start_delta = datetime.timedelta(minutes=start_min)
+        end_delta = datetime.timedelta(minutes=end_min)
+        start = now + start_delta
+        end = now + end_delta
+        return [f for f in flist if f.time > start and f.time < end]
+
+
+class FoliLine:
+    def __init__(self, name, ltime, realtime=False,
+                 cancelled=False, ontime=True, timediff=0):
+        if timediff == 0:
+            timediff = ltime
+        self.name = name
+        self.time = ltime
+        self.realtime = realtime
+        self.cancelled = cancelled
+        self.ontime = ontime
+        self.timediff = timediff
+
+    def timediff_min_sec(self):
+        """ Get timediff in format 1m48s """
+        tdiff = self.timediff - self.time
+        minutes, seconds = divmod(tdiff.seconds, 60)
+        if minutes == 0 and seconds == 0:
+            return "-"
+        if minutes:
+            return "{0}m{1:02d}s".format(minutes, seconds)
+        return "{0:02d}s".format(seconds)
+
+
+class FoliStop:
+    def __init__(self, stopnr="157"):
+        self.stop_name = ""
+        self.stopnr = self.normalize_stopnr(stopnr)
+        self.url = ("http://data-western.foli.fi/stops/{0}".format(
+            self.stopnr))
+
+    def run(self):
+        """ Perform request, and populate journey data table """
+        try:
+            data = json.loads(self.get_data())
+        except ValueError:
+            raise exceptions.FoliParseDataException(
+                "Got bad data from url: {0}".format(self.url))
+        if not self.stop_name:
+            self.stop_name = self.stopnr
+        self.journeys = []
+        try:
+            for jo in data['result']:
+                jo_time = jo['aimeddeparturetime']
+                line_time = datetime.datetime.fromtimestamp(jo_time)
+
+                new_line = FoliLine(jo['lineref'], line_time,
+                                    realtime=True)
+                if 'delay' in jo.keys():
+                    new_line.ontime = False
+                    new_line.timediff = datetime.datetime.fromtimestamp(
+                        jo['expecteddeparturetime'])
+                self.journeys.append(new_line)
+
+        except KeyError:
+            raise exceptions.FoliParseDataError("Error while parsing data"
+                                                " for line {0}".format(
+                                                    self.stopnr))
+
+    def normalize_stopnr(self, nr):
+        """ Simple validation and normalization of stop nr,
+            raise FoliStopNameException if b0rked
+        """
+        try:
+            if nr[0].lower() == "t":
+                retnr = "T{0}".format(int(nr[1:]))
+            else:
+                retnr = "{0}".format(int(nr))
+        except ValueError:
+            raise exceptions.FoliStopNameException(
+                "{0} is not a valid stop id".format(nr))
+        return retnr
+
+    def get_data(self):
+        req = requests.get(self.url)
+        if req.status_code == 200:
+            return req.text
+        elif req.status_code == 404:
+            raise exceptions.FoliServerException(
+                "Server response was: 404 - Not Found, '{0}'"
+                " doesn't seem like a valid stop id".format(self.stopnr))
+        else:
+            raise exceptions.FoliServerException(
+                "Server responded with status {0}".format(req.status_code))
