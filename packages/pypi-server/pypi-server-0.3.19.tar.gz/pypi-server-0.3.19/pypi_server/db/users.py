059e277992e8a84bd8d71c80aa40fdeac2be3ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:07 +0000

    Add package pypi-server/0.3.19/pypi-server-0.3.19.tar.gz

---
+++ packages/pypi-server/pypi-server-0.3.19.tar.gz/pypi-server-0.3.19/pypi_server/db/users.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import peewee as p
+from playhouse.fields import PasswordField as PasswordFieldBase, PasswordHash, gensalt, hashpw
+from pypi_server.db import Model
+
+
+class PasswordField(PasswordFieldBase):
+    def db_value(self, value):
+        if isinstance(value, p.unicode_type):
+            value = value.encode('utf-8')
+
+        salt = gensalt(self.bcrypt_iterations)
+        return value if value is None else hashpw(value, salt)
+
+
+
+class Users(Model):
+    login = p.CharField(max_length=255, unique=True, index=True, null=False)
+    password = PasswordField(iterations=10, null=False)
+    disabled = p.BooleanField(default=False, null=False)
+    is_admin = p.BooleanField(default=False, null=False)
+    email = p.CharField(index=True)
+
+    @classmethod
+    def check(cls, login, password):
+        q = cls.select(cls.id, cls.password).where(
+            cls.disabled == False,
+            cls.login == str(login),
+        )
+
+        user = q.limit(1)
+        user = user[0] if user else None
+
+        if user and user.password.check_password(password):
+            return cls.get(id=user.id)
+        else:
+            raise p.DoesNotExist("User doesn't exists")
