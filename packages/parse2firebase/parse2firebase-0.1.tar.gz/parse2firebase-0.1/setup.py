b62e939b3fe693e46ab9f72410524b35da475f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:48 +0000

    Add package parse2firebase/0.1/parse2firebase-0.1.tar.gz

---
+++ packages/parse2firebase/parse2firebase-0.1.tar.gz/parse2firebase-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='parse2firebase',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['parse2firebase']                  # The name of your scipt, and also the command you'll be using for calling it
+)
