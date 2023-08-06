e93bd44d9a46272fb9186ec051db0d4cd1e7c249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:41 +0000

    Add package nester_kypaik/1.1.2/nester_kypaik-1.1.2.tar.gz

---
+++ packages/nester_kypaik/nester_kypaik-1.1.2.tar.gz/nester_kypaik-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_kypaik',
+	version = '1.1.2',
+	py_modules = ['nester_kypaik'],
+	author = 'kypaik',
+	author_email = 'maxvision21@naver.com',
+	url = 'http://andykathy.com',
+	description = 'A simple printer of nested lists',
+	)
