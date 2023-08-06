c9aff164f5cdcd35ccc8ad31fefbcecfb03a8412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:10 +0000

    Add package delphixpy/1.7.0.0/delphixpy-1.7.0.0.tar.gz

---
+++ packages/delphixpy/delphixpy-1.7.0.0.tar.gz/delphixpy-1.7.0.0/delphixpy/v1_4_1/web/__init__.py
@@ -0,0 +1,58 @@
+#
+# Copyright 2016 by Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Package "web"
+"""
+# pylint: disable=W0403,W0611,W0622
+API_VERSION = "1.4.1"
+
+import session
+import alert
+import jetstream
+import source
+import data
+import registration
+import group
+import network
+import capacity
+import analytics
+import job
+import environment
+import repository
+import delphix
+import service
+import fault
+import toolkit
+import about
+import replication
+import database
+import system
+import sourceconfig
+import permission
+import user
+import storage
+import policy
+import namespace
+import host
+import action
+import role
+import connectivity
+import authorization
+import timeflow
+import notification
+import snapshot
+import domain
