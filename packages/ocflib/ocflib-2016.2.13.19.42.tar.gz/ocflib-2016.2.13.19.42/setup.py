80ff7893eea3d5bef15f40deebca90e57ce3cd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package ocflib/2016.2.13.19.42/ocflib-2016.2.13.19.42.tar.gz

---
+++ packages/ocflib/ocflib-2016.2.13.19.42.tar.gz/ocflib-2016.2.13.19.42/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+
+try:
+    with open('.version') as f:
+        VERSION = f.readline().strip()
+except IOError:
+    VERSION = 'unknown'
+
+setup(
+    name='ocflib',
+    version=VERSION,
+    author='Open Computing Facility',
+    author_email='help@ocf.berkeley.edu',
+    description='libraries for account and server management',
+    url='https://www.ocf.berkeley.edu',
+    packages=find_packages(),
+    package_data={
+        'ocflib.account': ['rc/*'],
+    },
+    install_requires=(
+        'cached_property',
+        'colorama',
+        'cracklib',
+        'dnspython3',
+        'ldap3',
+        'paramiko',
+        # pexpect 3.3 (currently the latest) breaks with multiprocessing/celery
+        # https://github.com/pexpect/pexpect/issues/86
+        'pexpect<3.2.999',
+        'pycrypto',
+        # TODO: Remove this version restriction after upgrade to jessie
+        #   Currently pymysql breaks with Python 3.2.x (rt#4267)
+        'pymysql<0.6.7',
+        'pysnmp',
+        'pyyaml',
+        'redis',
+        'requests',
+        'sqlalchemy',
+    )
+)
