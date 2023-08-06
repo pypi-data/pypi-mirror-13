2da9f105539ca79921b0d66987cd638df3f80414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:23 +0000

    Add package prefetch/0.0/prefetch-0.0.tar.gz

---
+++ packages/prefetch/prefetch-0.0.tar.gz/prefetch-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Ionel Cristian Maries',
+    author_email='contact@ionelmc.ro',
+    description='Generic model related data prefetch framework for Django.',
+    install_requires=['django-prefetch'],
+    long_description='''Use `django-prefetch <https://pypi.python.org/pypi/django-prefetch/>`_ instead.''',
+    maintainer='Ionel Cristian Maries',
+    maintainer_email='contact@ionelmc.ro',
+    name='prefetch',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/django-prefetch/',
+    version="0.0",
+    zip_safe=False,
+)
