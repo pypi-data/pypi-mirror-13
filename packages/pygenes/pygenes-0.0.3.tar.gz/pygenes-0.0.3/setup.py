46a2b68e3bb8b74d25137c5f5c155183a5573961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:06 +0000

    Add package pygenes/0.0.3/pygenes-0.0.3.tar.gz

---
+++ packages/pygenes/pygenes-0.0.3.tar.gz/pygenes-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+ext_modules = [
+    Extension(
+        'pygenes',
+        ['src/pygenes.cpp'],
+        language='c++',
+        include_dirs=['src'],
+        libraries=['boost_python', 'boost_serialization'],
+    )
+]
+
+setup(
+    name='pygenes',
+    version='0.0.3',
+    description='On-disk gene database searchable using an interval tree',
+    author='Andrew McPherson',
+    author_email='andrew.mcpherson@gmail.com',
+    url='http://bitbucket.org/dranew/pygenes',
+    download_url='https://bitbucket.org/dranew/pygenes/get/v0.0.3.tar.gz',
+    keywords=['bioinformatics'],
+    classifiers=[],
+    ext_modules=ext_modules,
+)
+
