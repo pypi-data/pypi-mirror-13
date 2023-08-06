b6edff9ab852f68fc061508d2442ec26e0ec0e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:11 +0000

    Add package slash/0.20.0/slash-0.20.0.tar.gz

---
+++ packages/slash/slash-0.20.0.tar.gz/slash-0.20.0/tests/test_exception_marks.py
@@ -0,0 +1,21 @@
+from slash.utils.exception_mark import ExceptionMarker, mark_exception, get_exception_mark
+
+def test_marker():
+    exc = AttributeError()
+    marker = ExceptionMarker('mark')
+    assert not marker.is_exception_marked(exc)
+    marker.mark_exception(exc)
+    assert marker.is_exception_marked(exc)
+
+def test_marker_class():
+
+    class CustomException(Exception):
+        pass
+    mark_exception(CustomException, 'a', 'b')
+    assert get_exception_mark(CustomException, 'a') == 'b'
+
+    e = CustomException()
+    mark_exception(e, 'c', 'd')
+    assert get_exception_mark(e, 'a') == 'b'
+    assert get_exception_mark(e, 'c') == 'd'
+    assert get_exception_mark(CustomException, 'c') is None
