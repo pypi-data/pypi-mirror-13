ba14aaed291f17d53894e9ec70ed89203ceb9f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:41 +0000

    Add package django-tracware/0.0.1/django-tracware-0.0.1.tar.gz

---
+++ packages/django-tracware/django-tracware-0.0.1.tar.gz/django-tracware-0.0.1/tracware/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+from django.conf.urls import patterns
+
+from .views import *
+
+
+urlpatterns = [
+
+    url(
+        r'^toggler/$',
+        TracToggleAjaxView.as_view(),
+        name='tracware_toggler_ajax_url'
+    ),
+
+    url(
+        r'^delete/(?P<pk>\d+)$',
+        TracDeleteAjaxView.as_view(),
+        name='tracware_delete_trac'
+    ),
+
+]
