080467f669fc1e0a5ffc5d03c48ca40c7832c9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:15 +0000

    Add package gd_opt/1.1.0/gd_opt-1.1.0.zip

---
+++ packages/gd_opt/gd_opt-1.1.0.zip/gd_opt-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+      name='gd_opt',
+      version='1.1.0',
+      py_modules=['gd_opt'],
+      author='lipd',
+      author_email='pandeng.li@163.com',
+      description='a simple gd code for optimization',
+
+
+      )
