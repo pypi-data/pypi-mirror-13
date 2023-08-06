5b062495ed41b94eae31d12a5884b08f85e0c597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package nester_wing/1.1.1/nester_wing-1.1.1.zip

---
+++ packages/nester_wing/nester_wing-1.1.1.zip/nester_wing-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='nester_wing',
+  version='1.1.1',
+  py_modules=['nester_wing'],
+  author='hfpython',
+  author_email='hfpython@headfirstlads.com',
+  url='http://www.headfirstlads.com',
+  description='wing',)
