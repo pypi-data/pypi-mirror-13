9ed000db698bf124e6c4814e106e473dd050f3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package django-markdownx/1.4/django_markdownx-1.4-py2.7.egg

---
+++ packages/django-markdownx/django_markdownx-1.4-py2.7.egg/markdownx/views.py
@@ -0,0 +1,42 @@
+import markdown
+
+from django.views.generic.edit import View, FormView
+from django.http import HttpResponse, JsonResponse
+
+from .forms import ImageForm
+from .settings import MARKDOWNX_MARKDOWN_EXTENSIONS, MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS
+
+
+class MarkdownifyView(View):
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse(
+            markdown.markdown(
+                request.POST['content'],
+                extensions=MARKDOWNX_MARKDOWN_EXTENSIONS,
+                extension_configs=MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS))
+
+
+class ImageUploadView(FormView):
+
+    template_name = "dummy.html"
+    form_class = ImageForm
+    success_url = '/'
+
+    def form_invalid(self, form):
+        response = super(ImageUploadView, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        image_path = form.save()
+        response = super(ImageUploadView, self).form_valid(form)
+
+        if self.request.is_ajax():
+            data = {}
+            data['image_path'] = image_path
+            return JsonResponse(data)
+        else:
+            return response
