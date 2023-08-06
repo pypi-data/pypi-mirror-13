84c6c741116b425728cde174d3598e1a9482ebcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:19 +0000

    Add package twtxt/1.0.1/twtxt-1.0.1.tar.gz

---
+++ packages/twtxt/twtxt-1.0.1.tar.gz/twtxt-1.0.1/twtxt/parser.py
@@ -0,0 +1,47 @@
+"""
+    twtxt.parser
+    ~~~~~~~~~~~~
+
+    This module implements the parser for twtxt files.
+
+    :copyright: (c) 2016 by buckket.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import logging
+from datetime import timezone
+
+import dateutil.parser
+
+from twtxt.types import Tweet
+
+logger = logging.getLogger(__name__)
+
+
+def make_aware(dt):
+    """Appends tzinfo and assumes UTC, if datetime object has no tzinfo already."""
+    return dt if dt.tzinfo else dt.replace(tzinfo=timezone.utc)
+
+
+def parse_iso8601(string):
+    """Parse string using dateutil.parser."""
+    return make_aware(dateutil.parser.parse(string))
+
+
+def parse_string(string, source):
+    """Parses a multi-line string and returns extracted :class:`Tweet` objects."""
+    tweets = []
+
+    for line in string:
+        try:
+            parts = line.partition("\t")
+            created_at = parse_iso8601(parts[0])
+            text = parts[2].lstrip().rstrip()
+
+            tweet = Tweet(text, created_at, source)
+            tweets.append(tweet)
+
+        except (ValueError, OverflowError) as e:
+            logger.debug(e)
+
+    return tweets
