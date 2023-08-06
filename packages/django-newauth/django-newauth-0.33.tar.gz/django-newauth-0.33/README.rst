2f0f86cec4dff33e9d587b0e608588f7dc3fdee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:58 +0000

    Add package django-newauth/0.33/django-newauth-0.33.tar.gz

---
+++ packages/django-newauth/django-newauth-0.33.tar.gz/django-newauth-0.33/README.rst
@@ -0,0 +1,9 @@
+django-newauth
+===================
+
+See the docs at: https://github.com/beproud/django-newauth/tree/master/docs/en/source
+
+Run tests using setup.py::
+
+    $ python setup.py test
+
---
+++ packages/django-newauth/django-newauth-0.33.tar.gz/django-newauth-0.33/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#:coding=utf-8:
+
+from setuptools import setup, find_packages
+
+setup (
+    name='django-newauth',
+    version='0.33',
+    description='Authentication for Django done right',
+    author='BeProud',
+    author_email='project@beproud.jp',
+    url='https://github.com/beproud/django-newauth/',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Plugins',
+      'Framework :: Django',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Programming Language :: Python',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    test_suite='tests.main',
+    zip_safe=False,
+)
