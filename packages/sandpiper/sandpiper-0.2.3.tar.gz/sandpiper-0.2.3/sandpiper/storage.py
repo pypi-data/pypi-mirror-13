121be972376a52d49f5c40fd908bac391f2e06e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:18 +0000

    Add package sandpiper/0.2.3/sandpiper-0.2.3.tar.gz

---
+++ packages/sandpiper/sandpiper-0.2.3.tar.gz/sandpiper-0.2.3/sandpiper/storage.py
@@ -0,0 +1,22 @@
+from .adapter           import InMemory
+from .adapter.exception import NotSupported
+from .mixin             import DictionaryAccess
+
+class Storage(DictionaryAccess):
+    def __init__(self, driver=InMemory()):
+        self.driver = driver
+
+    def get(self, key):
+        return self.driver.get(key)
+
+    def set(self, key, value):
+        self.driver.set(key, value)
+
+    def remove(self, key):
+        self.driver.remove(key)
+
+    def find(self, *args, **kwargs):
+        try:
+            return self.driver.find(*args, **kwargs)
+        except NotSupported as e:
+            return []
