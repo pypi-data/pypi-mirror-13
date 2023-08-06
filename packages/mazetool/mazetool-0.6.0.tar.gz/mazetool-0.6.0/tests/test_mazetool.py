34f061481d4b737e034ea86816dbb829596c6569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package mazetool/0.6.0/mazetool-0.6.0.tar.gz

---
+++ packages/mazetool/mazetool-0.6.0.tar.gz/mazetool-0.6.0/tests/test_mazetool.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_mazetool
+----------------------------------
+
+Tests for `mazetool` module.
+"""
+
+import unittest
+
+from mazetool import mazetool
+
+
+class TestMazetool(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
