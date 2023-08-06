f3309c3b1fbbe5c0043579bff750f4772dde5b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:17 +0000

    Add package traclus_impl/0.2/traclus_impl-0.2.zip

---
+++ packages/traclus_impl/traclus_impl-0.2.zip/traclus_impl-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='traclus_impl',
+      version='0.2',
+      description='First pass implementation of traclus algorithm',
+      url='http://github.com/apolcyn/traclus_impl.git',
+      author='AP',
+      author_email='alexpolcyn65@gmail.com',
+      license='MIT',
+      packages=['traclus_impl'],
+      install_requires=[
+          'planar',
+      ],
+      zip_safe=False)
