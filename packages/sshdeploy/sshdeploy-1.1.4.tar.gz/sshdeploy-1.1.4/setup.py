babfaae297eea5f7cb43ae25e78e73c138aa3f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package sshdeploy/1.1.4/sshdeploy-1.1.4.tar.gz

---
+++ packages/sshdeploy/sshdeploy-1.1.4.tar.gz/sshdeploy-1.1.4/setup.py
@@ -0,0 +1,76 @@
+from setuptools import setup
+from textwrap import dedent
+
+# create README file by combining sshdeploy/manual.rst with the following 
+# installation instructions
+install = dedent("""\
+    Installation
+    ============
+
+    If you plan to use SSH Deploy without modifying it, the preferred way to 
+    install it for multiple users is::
+
+       pip install --update sshdeploy
+
+    Doing so generally requires root permissions. Alternately, you can install it 
+    just for yourself using::
+
+       pip install --user --update sshdeploy
+
+    This installs sshdeploy into ~/.local and so does not require root permissions.
+
+    If you would like to change the program, you should first clone it's source 
+    repository and then install it::
+
+       git clone https://github.com/KenKundert/sshdeploy.git
+       cd sshdeploy
+       python setup.py install --user
+""")
+with open('sshdeploy/manual.rst') as f:
+    manual = f.read()
+with open('README.rst', 'w') as f:
+    readme ='\n\n'.join([manual, install])
+    f.write(readme)
+
+setup(
+    name='sshdeploy',
+    version='1.1.4',
+    description="Generates and distributes SSH keys.",
+    long_description=readme,
+    author="Ken Kundert",
+    author_email='sshdeploy@nurdletech.com',
+    url='http://nurdletech.com/linux-utilities/sshdeploy',
+    download_url='https://github.com/kenkundert/sshdeploy/tarball/master',
+    entry_points = {
+        'console_scripts': [
+            'sshdeploy=sshdeploy.main:main',
+        ],
+    },
+    zip_safe = False,
+    packages=['sshdeploy'],
+    package_data={'sshdeploy': ['manual.rst']},
+    license='GPLv3+',
+    install_requires=[
+        'arrow',
+        'docopt',
+        'inform',
+        'pexpect',
+        'shlib',
+        #'abraxas', -- not yet in pypi
+    ],
+    keywords=[
+        'ssh',
+        'keys',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+)
+# vim: set sw=4 sts=4 et:
---
+++ packages/sshdeploy/sshdeploy-1.1.4.tar.gz/sshdeploy-1.1.4/sshdeploy/utils.py
@@ -0,0 +1,53 @@
+from shlib import Run, rm, to_path
+from inform import comment, debug, display, error, fmt, narrate, os_error
+from arrow import now
+
+# today's date
+date = now().format('YYYY-MM-DD')
+
+# run a command
+def run(cmd, stdin=None, modes=None):
+    comment('    running:', *cmd)
+    Run(cmd, stdin=stdin, modes=modes)
+
+# run an sftp command
+def run_sftp(server, cmds):
+    cmd = ['sftp', '-q', '-b', '-', server]
+    comment(fmt('    sftp {server}:'), '; '.join(cmds))
+    try:
+        Run(cmd, stdin='\n'.join(cmds), modes='sOEW')
+    except KeyboardInterrupt:
+        display('Continuing')
+
+# test access to host
+def test_access(host):
+    try:
+        narrate(fmt('Testing connection to {host}.'))
+        payload = fmt('test payload for {host}')
+        ref = to_path('.ref')
+        test = to_path('.test')
+        ref.write_text(payload)
+        rm(test)
+        run_sftp(host, [
+            fmt('put {ref}'),
+            fmt('get {ref} {test}'),
+            fmt('rm {ref}')
+        ])
+        if test.read_text() == payload:
+            comment('connection successful.', culprit=host)
+        else:
+            error('cannot connect.', culprit=host)
+    except OSError as err:
+        error('cannot connect.', culprit=host)
+    rm(ref, test)
+
+# clean host
+def clean(host):
+    try:
+        narrate(fmt('Cleaning {host}.'))
+        run_sftp(host, ['rm .ssh/*.provisional'])
+    except OSError as err:
+        if 'no such file or directory' in str(err).lower():
+            comment(os_error(err))
+        else:
+            error('cannot connect.', culprit=host)
