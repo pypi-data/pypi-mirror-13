a311d9f6fb189d93124e584708ed7bfae3e88356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:49 +0000

    Add package Disinfect/0.1.3/Disinfect-0.1.3.tar.gz

---
+++ packages/Disinfect/Disinfect-0.1.3.tar.gz/Disinfect-0.1.3/src/disinfect/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division, print_function
+
+from .disinfect import (
+    Field,
+    MappedValueError,
+    Mapping,
+    MultiValueError,
+    Test,
+    test_and_return,
+    validate,
+    sanitize,
+)
+
+__version__ = '0.1.3'
+
+__package__ = 'disinfect'
+__title__ = 'Disinfect'
+__description__ = 'Disinfect: Destroy bad input.'
+__uri__ = 'https://github.com/corverdevelopment/Disinfect/'
+
+__author__ = 'Nils Corver'
+__email__ = 'nils@corverdevelopment.nl'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2016 Corver Development B.V.'
+
+__all__ = [
+    'Field',
+    'MappedValueError',
+    'Mapping',
+    'MultiValueError',
+    'Test',
+    'test_and_return',
+    'validate',
+    'sanitize',
+]
