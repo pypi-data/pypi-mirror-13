dab3f2a71bddb9efe11549185b1ca1e2893dc676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:42 +0000

    Add package poolmanager/0.0.1/poolmanager-0.0.1.tar.gz

---
+++ packages/poolmanager/poolmanager-0.0.1.tar.gz/poolmanager-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+
+with io.open('README.md') as f:
+    long_description = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
+requires = open(os.path.join(here, 'requirements.txt')).read().split('\n')
+
+setup(name='poolmanager',
+      version='0.0.1',
+      description='Simple pool manager',
+      long_description=long_description,
+      classifiers=[],
+      keywords='',
+      author='Loic Gasser',
+      author_email='loicgasser4@gmail.com',
+      license='MIT',
+      url='https://github.com/loicgasser/poolmanager',
+      packages=find_packages(exclude=['tests']),
+      package_dir={'poolmanager': 'poolmanager'},
+      package_data={'poolmanager': ['*.md']},
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      install_requires=requires
+      )
+      
+
