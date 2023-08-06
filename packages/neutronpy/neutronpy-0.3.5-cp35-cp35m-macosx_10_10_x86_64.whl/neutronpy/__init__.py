52b24e23afd97c2e17098d0ffea309f7c0494642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:41 +0000

    Add package neutronpy/0.3.5/neutronpy-0.3.5-cp35-cp35m-macosx_10_10_x86_64.whl

---
+++ packages/neutronpy/neutronpy-0.3.5-cp35-cp35m-macosx_10_10_x86_64.whl/neutronpy/__init__.py
@@ -0,0 +1,16 @@
+r'''NeutronPy: open source python library for neutron scattering data analysis
+
+'''
+from __future__ import absolute_import
+
+__version__ = '0.3.5'
+
+from . import constants
+from . import core as tools
+from . import resolution
+from . import form_facs
+from . import functions
+from . import models
+from . import spurion
+from . import lattice
+from .core import *  # pylint: disable=wildcard-import
---
+++ packages/neutronpy/neutronpy-0.3.5-cp35-cp35m-macosx_10_10_x86_64.whl/neutronpy/models.py
@@ -0,0 +1,19 @@
+r'''Physical Models
+'''
+
+def harmonic_oscillator():
+    r'''Standard equation for a harmonic oscillator
+    '''
+    pass
+
+
+def acoustic_phonon():
+    r'''Standard equation for the dispersion of an acoustic phonon
+    '''
+    pass
+
+
+def optical_phonon():
+    r'''Standard equation for the dispersion of an optical phonon
+    '''
+    pass
