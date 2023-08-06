0ebac0115466ba985639e19740e81bebe17a8e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:24 +0000

    Add package replication-monitor/0.1.1/replication-monitor-0.1.1.tar.gz

---
+++ packages/replication-monitor/replication-monitor-0.1.1.tar.gz/replication-monitor-0.1.1/replication_monitor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+try:
+    import gi
+except Exception as ex:
+    print('Unable to load the GTK+ gi module.\n'
+          'Is GTK+ installed on your operating system and the gi module installed into Python?\n'
+          'The error reported is: ' + str(ex))
+    sys.exit(1)
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+# if we are running as a module make sure relative imports still work
+if __name__ != '__main__':
+    sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+from src.builder import Builder
+from ui.main_window import MainWindow
+
+def main():
+    ui_path = os.path.dirname(os.path.realpath(__file__))
+    ui_path = os.path.join(ui_path, 'ui/replication_monitor.glade')
+    builder = Builder(ui_path)
+    win = MainWindow(builder)
+    Gtk.main()
+
+if __name__ == '__main__':
+    main()
