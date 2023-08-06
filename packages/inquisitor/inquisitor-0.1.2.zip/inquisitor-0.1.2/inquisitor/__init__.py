4bde8a652d3af95cc807d9ad03d32876d331bc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:59 +0000

    Add package inquisitor/0.1.2/inquisitor-0.1.2.zip

---
+++ packages/inquisitor/inquisitor-0.1.2.zip/inquisitor-0.1.2/inquisitor/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from .api import ApiException, Inquisitor
+from .converters import PandasConverter
---
+++ packages/inquisitor/inquisitor-0.1.2.zip/inquisitor-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 08 21:53:57 2015
+
+@author: OriolAndres
+"""
+
+from distutils.core import setup
+setup(
+  name = 'inquisitor',
+  packages = ['inquisitor'],
+  version = '0.1.2',
+  description = 'Python class to fetch data from inquirim.com',
+  author = 'Oriol Andres',
+  author_email = 'oriol@inquirim.com',
+  url = 'https://github.com/inquirim/inquisitor',
+  download_url = 'https://github.com/inquirim/inquisitor/tarball/0.1',
+  keywords = ['data', 'economics','finance','api'],
+  classifiers = ["Programming Language :: Python",
+                 "Development Status :: 4 - Beta",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Intended Audience :: Science/Research",
+                 "Topic :: Office/Business :: Financial :: Spreadsheet"],
+)
