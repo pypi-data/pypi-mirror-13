8e27ac58172646d9daba2fc2919e103b9bd9005b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:20 +0000

    Add package hf_jk_nester/1.4.0/hf_jk_nester-1.4.0.tar.gz

---
+++ packages/hf_jk_nester/hf_jk_nester-1.4.0.tar.gz/hf_jk_nester-1.4.0/hf_jk_nester.py
@@ -0,0 +1,30 @@
+"""
+
+This is the "hf_jk_nester" module, it provides a function
+called print_lol() which prints lists that may or may not
+include nested lists.
+
+Date: 19.02.2016
+Author: Jaap Kroesschell
+
+"""
+import sys
+
+def print_lol(ownlist, indent=False, level=0, output=sys.stdout):
+    """ This function takes a positional argument called "ownList",
+    which is any Python list (of possibly, nested lists).
+     Each data item in the provided list is (recursively)
+     printed to the screen on its own line.
+     A second argument enables tabbed printing in case of nested lists.
+     A third argument called "level" is used to insert tab-stops
+     when a nested list is encountered.
+     A fouth argument sends output optional to a specified file. """
+
+    for eachItem in ownlist:
+        if isinstance(eachItem, list):
+            print_lol(eachItem, indent, level+1, file=output)
+        else:
+            if indent == True:
+                for tab_stop in range(level):
+                    print('\t', end='', file=output)
+            print(eachItem, file=output)
