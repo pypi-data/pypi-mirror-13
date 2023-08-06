71f2b16daf9892bb73c905167194251774fd9efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:44 +0000

    Add package nester_mgrd/1.0.0/nester_mgrd-1.0.0.zip

---
+++ packages/nester_mgrd/nester_mgrd-1.0.0.zip/nester_mgrd-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester_mgrd',
+    version     = '1.0.0',
+    py_modules  = ['nester_mgrd'],
+    author      = 'mgrdiez',
+    author_email= 'mgrdiez@seouxs.com',
+    url         = 'http://www.seouxs.com',
+    description = 'A simple printed of nested lists',
+    )
