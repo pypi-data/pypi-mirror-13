89fd2f7f3beefcfb3e83f80cdfa2b3decd5990be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:29 +0000

    Add package egnester/1.1.0/egnester-1.1.0.tar.gz

---
+++ packages/egnester/egnester-1.1.0.tar.gz/egnester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name 			= 'egnester',
+		version			= '1.1.0',
+		py_modules		= ['egnester'],
+		author			= 'ekramGaleti',
+		author_email	= 'enigmaekkui@gmail.com',
+		url				= 'www.perfectgirls.net',
+		description		= 'Lists in Lists in Listes.......and so on',
+)
