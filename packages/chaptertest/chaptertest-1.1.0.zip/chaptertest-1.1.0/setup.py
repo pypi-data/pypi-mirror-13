8289fe2ac849962bdf62fe7d9e2fd53fef94c666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:51 +0000

    Add package chaptertest/1.1.0/chaptertest-1.1.0.zip

---
+++ packages/chaptertest/chaptertest-1.1.0.zip/chaptertest-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+       name='chaptertest',
+       version='1.1.0',
+       py_modules=['chapter2'],
+       author='wendy_python',
+       authon_email='lyy20091019@163.com',
+       
+    )
