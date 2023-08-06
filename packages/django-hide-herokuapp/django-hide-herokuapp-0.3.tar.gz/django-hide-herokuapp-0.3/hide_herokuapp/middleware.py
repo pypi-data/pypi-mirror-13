d01afe48e8a2de4863207a0741a8418b0f7eb0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:34 +0000

    Add package django-hide-herokuapp/0.3/django-hide-herokuapp-0.3.tar.gz

---
+++ packages/django-hide-herokuapp/django-hide-herokuapp-0.3.tar.gz/django-hide-herokuapp-0.3/hide_herokuapp/middleware.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+class HideHerokuappFromRobotsMiddleware(object):
+    def process_response(self, request, response):
+        """
+        Add the header to prevent sites under *.herokuapp.com from being indexed.
+        """
+        http_host = request.get_host()
+
+        if http_host and 'herokuapp' in http_host:
+            response['X-Robots-Tag'] = 'noindex, nofollow'
+
+        return response
