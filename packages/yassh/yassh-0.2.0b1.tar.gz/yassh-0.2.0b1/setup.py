412bcd5221e9776cadf5779affe5cc54dac9b91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:46 +0000

    Add package yassh/0.2.0b1/yassh-0.2.0b1.tar.gz

---
+++ packages/yassh/yassh-0.2.0b1.tar.gz/yassh-0.2.0b1/setup.py
@@ -0,0 +1,44 @@
+import os
+import re
+from setuptools import setup
+
+HERE = os.path.dirname(__file__)
+
+
+def _readme():
+    with open(os.path.join(HERE, 'README.rst')) as f:
+        return f.read()
+
+
+def _version():
+    with open(os.path.join(HERE, 'yassh', '__init__.py')) as f:
+        return re.search(r"__version__ = '([^']+)'", f.read()).group(1)
+
+setup(
+    name='yassh',
+
+    version=_version(),
+
+    description='A process remote launcher.',
+    long_description=_readme(),
+
+    url='https://github.com/Enyx-SA/yassh',
+
+    author='David Keller',
+    author_email='david.keller@enyx.com',
+
+    license='BSD',
+
+    keywords='process launcher',
+
+    install_requires=[
+        'pexpect',
+    ],
+
+    extras_require={
+        'test': ['sure', 'behave', 'coverage']
+    },
+
+    packages=['yassh'],
+    platforms='UNIX'
+)
