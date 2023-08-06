fe4add6a726c4efb8601ed00adf719837dda578f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:16 +0000

    Add package tmy_nester/1.1.0/tmy_nester-1.1.0.zip

---
+++ packages/tmy_nester/tmy_nester-1.1.0.zip/tmy_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name            = 'tmy_nester',
+version         = '1.1.0',
+py_modules      = ['tmy_nester'],
+author          = 'cybercatmobile',
+author_email    = 'cybercatmobile@yahoo.com',
+url             = 'http://www.headfirstlabs.com',
+description     = 'A simple printer of nested lists',
+)
