a0ca70c3a6099f70810a7f393368d3723468e705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:50 +0000

    Add package one-piece/0.1/one-piece-0.1.tar.gz

---
+++ packages/one-piece/one-piece-0.1.tar.gz/one-piece-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'one-piece',
+    py_modules = ['onepiece'],
+    version = '0.1',
+    description = 'onepiece',
+    author = 'kuga',
+    author_email = 'kugacola@gmail.com',
+    url = 'http://kuga.me',
+    keywords = ['onepiece'],
+)
