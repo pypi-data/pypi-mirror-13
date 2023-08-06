cced6a6ca0583df50082b3e7733f3acf732a2a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:00 +0000

    Add package logmatic-python/0.0.7/logmatic-python-0.0.7.tar.gz

---
+++ packages/logmatic-python/logmatic-python-0.0.7.tar.gz/logmatic-python-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='logmatic-python',
+    version='0.0.7',
+    author='Logmatic.io support team',
+    author_email='support@logmatic.io',
+    packages = ['logmatic'],
+    scripts=[],
+    url='https://github.com/logmatic/logmatic-python',
+    license='MIT',
+    long_description=open('README.md').read(),
+    description='Python plugin to send logs to Logmatic.io',
+    install_requires = ['python-json-logger']
+)
