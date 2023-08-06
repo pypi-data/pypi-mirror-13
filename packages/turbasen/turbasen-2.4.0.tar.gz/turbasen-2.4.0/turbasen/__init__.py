d49d35827e5e0fd38a00a4ec5b4975d7e6d69870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:46 +0000

    Add package turbasen/2.4.0/turbasen-2.4.0.tar.gz

---
+++ packages/turbasen/turbasen-2.4.0.tar.gz/turbasen-2.4.0/turbasen/__init__.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Import the models we want directly available through the root module
+from .models import \
+    Gruppe, \
+    Omrade, \
+    Sted
+
+# Make configure directly available through the root module
+from .settings import configure
+
+# Make handle_available directly available through the root module
+from .events import handle_event
