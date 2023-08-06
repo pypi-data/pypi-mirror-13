5c56bde5eca5471291eb843918b3e9a645e8f4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:14 +0000

    Add package geneview/0.0.1.dev8/geneview-0.0.1.dev8-py2.7.egg

---
+++ packages/geneview/geneview-0.0.1.dev8-py2.7.egg/geneview/__init__.py
@@ -0,0 +1,11 @@
+from . import gwas
+from . import util
+from .palette import *
+
+from .ext.miscplot import puppyplot, palplot
+
+# Set aesthetic parameters in one step and make the font size as 1.2 times
+# in the default setting in ``plotting_context()`` in ``.palette._rcmod``
+set_all(font_scale=1.2) # this function is in the ``.palette`` package
+
+__version__ = '0.0.1.dev8'
