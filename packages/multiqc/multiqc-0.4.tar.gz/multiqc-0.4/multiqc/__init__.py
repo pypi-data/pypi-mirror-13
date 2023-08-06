bb0a31ba0075c6cba7c4d274f20797a7c0d3a30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:13 +0000

    Add package multiqc/0.4/multiqc-0.4.tar.gz

---
+++ packages/multiqc/multiqc-0.4.tar.gz/multiqc-0.4/multiqc/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import logging
+import pkgutil
+from pkg_resources import get_distribution
+
+logger = logging.getLogger(__name__)
+
+__version__ = get_distribution("multiqc").version
+
+from multiqc.utils import config
+from multiqc.modules.base_module import BaseMultiqcModule
+
+config.version = __version__
