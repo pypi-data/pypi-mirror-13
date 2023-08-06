f18cffd7e1a0d6c81692e957bb5c405ef805d2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:46 +0000

    Add package anyconfig/0.5.0/anyconfig-0.5.0.tar.gz

---
+++ packages/anyconfig/anyconfig-0.5.0.tar.gz/anyconfig-0.5.0/anyconfig/globals.py
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2013 - 2015 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+"""anyconfig globals.
+"""
+import anyconfig.init
+
+
+AUTHOR = "Satoru SATOH <ssat@redhat.com>"
+VERSION = "0.5.0"
+
+LOGGER = anyconfig.init.LOGGER
+
+# vim:sw=4:ts=4:et:
