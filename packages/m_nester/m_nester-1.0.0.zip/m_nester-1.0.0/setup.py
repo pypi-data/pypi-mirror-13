c37167c01ac0c7bc2e76e84de5ee747ab0233ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:09 +0000

    Add package m_nester/1.0.0/m_nester-1.0.0.zip

---
+++ packages/m_nester/m_nester-1.0.0.zip/m_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name  = 'm_nester',
+        version  = '1.0.0',
+        py_modules = ['m_nester'],
+        author = 'yangyanlong',
+        author_email = 'yangyanlongedu@163.com',
+        url  = 'g:\m_nester',
+      )
