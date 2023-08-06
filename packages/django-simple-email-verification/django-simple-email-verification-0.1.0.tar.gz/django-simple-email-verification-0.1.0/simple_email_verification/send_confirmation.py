344125ff86ef06f3977c83faae1e0ed93bda8ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:32 +0000

    Add package django-simple-email-verification/0.1.0/django-simple-email-verification-0.1.0.tar.gz

---
+++ packages/django-simple-email-verification/django-simple-email-verification-0.1.0.tar.gz/django-simple-email-verification-0.1.0/simple_email_verification/send_confirmation.py
@@ -0,0 +1,21 @@
+from socket import gaierror
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from simple_email_verification import get_settings
+
+settings = get_settings()
+
+
+class EmailMessage:
+
+    def __init__(self, **kwargs):
+        self.subject = settings.get('VERIFICATION_EMAIL_SUBJECT')
+        self.plain = render_to_string(settings.get('VERIFICATION_EMAIL_PLAINTEXT'), kwargs)
+        self.html = render_to_string(settings.get('VERIFICATION_EMAIL_HTML'), kwargs)
+
+    def send(self, sender, recipients):
+        try:
+            send_mail(self.subject, self.plain, sender, recipients, html_message=self.html)
+        except gaierror as e:
+            # gaierror raised if SMTP hostname does not resolve or is incorrect
+            return e
