255b70ba89d81b749e8bfbf6f6403e36759fb172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:17 +0000

    Add package com.tr.nester/1.6.0/com.tr.nester-1.6.0.zip

---
+++ packages/com.tr.nester/com.tr.nester-1.6.0.zip/com.tr.nester-1.6.0/nester.py
@@ -0,0 +1,17 @@
+"""这是nester模块，这个模块的功能是打印出一个列表里的所有内容（包括嵌套的内容）"""
+import sys
+def print_lol(the_list,indent=False,level=0,fn=sys.stdout):
+    """函数需要一个列表参数，当列表有嵌套的时候，需要迭代的调用该函数
+	level参数提供了列表内部结构的展示，缺省值是0
+	indent参数默认值为False，是是否缩进的开关
+	fn参数控制数据输出到哪里，缺省值是屏幕"""
+    for each_item in the_list:
+        if(isinstance(each_item,list)):
+           print_lol(each_item,indent,level+1,fn)
+        else:
+            if indent: 
+                for number in range(level):
+                        print("\t",end='',file=fn)
+            print(each_item,file=fn)
+           
+           
---
+++ packages/com.tr.nester/com.tr.nester-1.6.0.zip/com.tr.nester-1.6.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name        = 'com.tr.nester',
+		version     = '1.6.0',
+		py_modules  = ['nester'],
+		author      = 'zhoupansheng',
+		author_email= 'zhoupansheng666@163.com',
+		url         = 'http://www.hao123.com',
+		description = '迭代显示列表数据，增加数据输出方式，默认为打印显示到屏幕上'
+	)
