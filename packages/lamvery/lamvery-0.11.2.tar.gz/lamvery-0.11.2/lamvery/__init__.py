28e263d3bbee6b427f147c7b092d79c03073155e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:36 +0000

    Add package lamvery/0.11.2/lamvery-0.11.2.tar.gz

---
+++ packages/lamvery/lamvery-0.11.2.tar.gz/lamvery-0.11.2/lamvery/__init__.py
@@ -0,0 +1,11 @@
+"""
+lamvery
+----
+Function based deploy and management tool for AWS Lambda.
+"""
+
+__version__ = '0.11.2'
+
+import importlib
+
+secret = importlib.import_module('lamvery.secret')
