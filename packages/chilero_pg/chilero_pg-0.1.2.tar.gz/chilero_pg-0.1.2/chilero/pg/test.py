e4f26e1279553f8457b6d14386f15836fd016d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:20 +0000

    Add package chilero_pg/0.1.2/chilero_pg-0.1.2.tar.gz

---
+++ packages/chilero_pg/chilero_pg-0.1.2.tar.gz/chilero_pg-0.1.2/chilero/pg/test.py
@@ -0,0 +1,54 @@
+import asyncio
+import random
+import sys
+
+from chilero.pg.utils import create_database, drop_database
+from chilero.web.test import WebTestCase
+
+from .application import Application
+
+
+class TestCase(WebTestCase):
+    application = Application
+    settings = {}
+
+    @asyncio.coroutine
+    def initialize_application(self):
+        return self.application(
+            routes=self.routes,
+            settings=self.settings,
+            loop=self.loop
+        )
+
+    @classmethod
+    def setUpClass(cls):
+        version = '{}{}{}'.format(
+            sys.version_info.major,
+            sys.version_info.minor,
+            sys.version_info.micro,
+        )
+
+        dyn_db_url = 'postgres://postgres@localhost/test_{version}'.format(
+            version=version
+        )
+
+        db_url = '{base}_{name}_{rand}'.format(
+            base=cls.settings.get('db_url') or dyn_db_url,
+            name=cls.__name__,
+            rand=random.randrange(1000, 100000)
+        )
+
+        cls._database_name = db_url
+        cls._initialize_db()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._destroy_db()
+
+    @classmethod
+    def _initialize_db(cls):
+        create_database(cls._database_name)
+
+    @classmethod
+    def _destroy_db(cls):
+        drop_database(cls._database_name)
