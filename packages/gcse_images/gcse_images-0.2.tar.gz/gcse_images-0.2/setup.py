5abc5a1588af188279d9d7fa9261e81c05709647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package gcse_images/0.2/gcse_images-0.2.tar.gz

---
+++ packages/gcse_images/gcse_images-0.2.tar.gz/gcse_images-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gcse_images',
+      version='0.2',
+      description='Lightweight utility for using Google Custom Search API '
+                  'for image searches.',
+      url='http://github.com/kjchavez/gcse-images',
+      author='Kevin Chavez',
+      author_email='kevin.j.chavez@gmail.com',
+      license='MIT',
+      packages=['gcse_images'],
+      zip_safe=False)
