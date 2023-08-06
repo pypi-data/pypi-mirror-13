95318a44e818560de770f86aa10e63192abc8a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:14 +0000

    Add package dfvfs/20160221/dfvfs-20160221.tar.gz

---
+++ packages/dfvfs/dfvfs-20160221.tar.gz/dfvfs-20160221/dfvfs/lib/errors.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""The Virtual File System (VFS) error classes."""
+
+
+class Error(Exception):
+  """Class that defines a generic error."""
+
+
+class AccessError(Error):
+  """Class that defines access errors."""
+
+
+class BackEndError(Error):
+  """Class that defines back-end errors."""
+
+
+class CacheFullError(Error):
+  """Class that defines cache full errors."""
+
+
+class FileFormatError(Error):
+  """Class that defines file format errors."""
+
+
+class MountPointError(Error):
+  """Class that defines mount point errors."""
+
+
+class PathSpecError(Error):
+  """Class that defines path specification errors."""
+
+
+class VolumeSystemError(Error):
+  """Class that defines volume system errors."""
