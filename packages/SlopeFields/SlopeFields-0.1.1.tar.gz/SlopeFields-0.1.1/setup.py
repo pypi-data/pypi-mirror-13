6e83a8a4851fca9350dc8c2926d8f486c7c0904d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:18 +0000

    Add package SlopeFields/0.1.1/SlopeFields-0.1.1.tar.gz

---
+++ packages/SlopeFields/SlopeFields-0.1.1.tar.gz/SlopeFields-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='SlopeFields',
+    version='0.1.1',
+    author='David Dworken',
+    author_email='david@daviddworken.com',
+    packages=['SlopeFields'],
+    url='http://pypi.python.org/pypi/SlopeFields/',
+    license='LICENSE.txt',
+    description='A Slope Field generator and grapher. ',
+    long_description=open('README.rst').read(),
+    scripts=['SlopeFields/slopeFields'],
+    install_requires=[
+        "argparse",
+        "matplotlib",
+        "numpy",
+    ],
+)
