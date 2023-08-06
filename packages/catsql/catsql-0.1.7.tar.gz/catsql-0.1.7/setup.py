099aa2253738e379248a34c7971f64a0e4669d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package catsql/0.1.7/catsql-0.1.7.tar.gz

---
+++ packages/catsql/catsql-0.1.7.tar.gz/catsql-0.1.7/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="catsql",
+      version="0.1.7",
+      author="Paul Fitzpatrick",
+      author_email="paulfitz@alum.mit.edu",
+      description="Display quick view from sql databases",
+      packages=['catsql', 'catsql.daffsql'],
+      entry_points={
+          "console_scripts": [
+              "catsql=catsql.main:main",
+              "patchsql=catsql.patch:main"
+          ]
+      },
+      install_requires=[
+          "SQLAlchemy>=1.0.11",
+          "python-magic",
+          "unicodecsv",
+	  "daff>=1.3.14",
+	  "psycopg2"
+      ],
+      url="https://github.com/paulfitz/catsql"
+)
