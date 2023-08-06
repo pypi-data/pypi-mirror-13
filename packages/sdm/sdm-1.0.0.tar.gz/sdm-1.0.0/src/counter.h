431e1c1ea7d393e909f2cd607a17249463506188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:38 +0000

    Add package sdm/1.0.0/sdm-1.0.0.tar.gz

---
+++ packages/sdm/sdm-1.0.0.tar.gz/sdm-1.0.0/src/counter.h
@@ -0,0 +1,29 @@
+#ifndef SDM_COUNTER_H
+#define SDM_COUNTER_H
+
+typedef int counter_t;
+
+struct counter_s {
+	unsigned int bits;
+	unsigned int sample;
+	int fd;
+	char *filename;
+	counter_t **counter;
+	counter_t *data;
+};
+
+int counter_init(struct counter_s *this, unsigned int bits, unsigned int sample);
+int counter_init_file(char *filename, struct counter_s *this);
+void counter_free(struct counter_s *this);
+void counter_print_summary(struct counter_s *this);
+
+void counter_print(struct counter_s *this, unsigned int index);
+
+int counter_add_bitstring(struct counter_s *this, unsigned int index, bitstring_t *bs);
+int counter_add_counter(struct counter_s *c1, unsigned int idx1, struct counter_s *c2, unsigned int idx2);
+
+int counter_to_bitstring(struct counter_s *this, unsigned int index, bitstring_t *bs);
+
+int counter_create_file(char *filename, unsigned int bits, unsigned int sample);
+
+#endif
