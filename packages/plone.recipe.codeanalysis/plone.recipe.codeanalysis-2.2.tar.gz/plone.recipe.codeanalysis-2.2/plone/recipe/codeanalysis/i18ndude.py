0c91c8baf51a62b7ba6743d2902e54afc5c711b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:31 +0000

    Add package plone.recipe.codeanalysis/2.2/plone.recipe.codeanalysis-2.2.tar.gz

---
+++ packages/plone.recipe.codeanalysis/plone.recipe.codeanalysis-2.2.tar.gz/plone.recipe.codeanalysis-2.2/plone/recipe/codeanalysis/i18ndude.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from plone.recipe.codeanalysis.analyser import Analyser
+from plone.recipe.codeanalysis.analyser import console_factory
+
+
+class I18NDude(Analyser):
+
+    name = 'find-untranslated'
+    title = 'Translations'
+
+    @property
+    def cmd(self):
+        cmd = []
+        files = self.find_files('.*\.pt')
+
+        if files:
+            cmd.append(self.options.get('i18ndude-bin') or '')
+            cmd.append('find-untranslated')
+            cmd.extend(files)
+
+        return cmd
+
+
+def console_script(options):
+    console_factory(I18NDude, options)
