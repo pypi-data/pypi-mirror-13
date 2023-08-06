58053468d3f4200d9d239be7a7810459e3c18bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:23 +0000

    Add package schema-migrations/0.1.1/schema-migrations-0.1.1.tar.gz

---
+++ packages/schema-migrations/schema-migrations-0.1.1.tar.gz/schema-migrations-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='schema-migrations',
+    version='0.1.1',
+    packages=['schema_migrations'],
+    url='',
+    license='MIT License',
+    author='Darwin Monroy',
+    author_email='contact@darwinmonroy.com',
+    description='PostgreSQL Schema Migrations',
+    install_requires=[
+        'psycopg2'
+    ]
+)
