1e832fb08719e4a66cc66f9a7ff19b6a385e72ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package decred_hash/1.0/decred_hash-1.0.tar.gz

---
+++ packages/decred_hash/decred_hash-1.0.tar.gz/decred_hash-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+
+blake_hash_module = Extension('blake_hash',
+                               sources = ['blakemodule.c',
+                                          'blake.c',
+					  'sphlib/blake.c'],
+                               include_dirs=['.', './sphlib'])
+
+setup (name = 'decred_hash',
+       version = '1.0',
+       description = 'Bindings for blake proof of work compatible with Decred.',
+       maintainer = 'kefkius',
+       maintainer_email = 'kefkius@mail.com',
+       url = 'https://github.com/kefkius/decred-hash',
+       ext_modules = [blake_hash_module])
