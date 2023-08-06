dac2adfc33b17e24fe9456e7d42959b174695dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:55 +0000

    Add package flexx/0.3/flexx-0.3-py2.py3-none-any.whl

---
+++ packages/flexx/flexx-0.3-py2.py3-none-any.whl/flexx/__init__.py
@@ -0,0 +1,35 @@
+"""
+Flexx is a pure Python toolkit for creating
+graphical user interfaces (GUI's), that uses web technology for its
+rendering. You can use Flexx to create desktop applications, web
+applications, and (if designed well) export an app to a standalone HTML
+document. It also works in the Jupyter notebook.
+
+Being pure Python and cross platform, it should work anywhere where
+there's Python and a browser. To run apps in desktop-mode, we recommend having Firefox
+installed.
+
+Flexx has a modular design, consisting of a few subpackages, which can
+also be used by themselves:
+
+* ui - the widgets
+* app - the event loop and server
+* react - reactive programming (how information flows through your program)
+* pyscript - Python to JavaScript transpiler
+* webruntime - to launch a runtime
+
+For more information, see http://flexx.readthedocs.org.
+"""
+
+__version__ = '0.3'
+
+
+# Assert compatibility
+import sys
+if sys.version_info[0] == 2:  # pragma: no cover
+    if sys.version_info < (2, 7):
+        raise RuntimeError('Flexx needs at least Python 2.7')
+    if type(b'') == type(''):  # noqa - will be str and unicode after conversion
+        raise RuntimeError('Flexx can only run on Python 2.x after 3to2 conversion.')
+
+del sys
