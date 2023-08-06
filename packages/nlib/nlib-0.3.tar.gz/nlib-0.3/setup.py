f7d9743590b8a37f3d47701ab9674dd1eb962599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package nlib/0.3/nlib-0.3.tar.gz

---
+++ packages/nlib/nlib-0.3.tar.gz/nlib-0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='nlib',
+      version='0.3',
+      description='nlib is a collection of numeric/scientific algorithms which do not required numpy',
+      author='Massimo DiPierro',
+      author_email='massimo.dipierro@gmail.com',
+      url='https://github.com/mdipierro/nlib',
+      install_requires=[],
+      py_modules=["nlib"],
+      license= 'BSD',
+      keywords='numerical',
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
