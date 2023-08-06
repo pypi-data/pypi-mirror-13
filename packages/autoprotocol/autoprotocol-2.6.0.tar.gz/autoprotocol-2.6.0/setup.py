5d12cf2830c30b09e1290fae76f13d9584852b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:20 +0000

    Add package autoprotocol/2.6.0/autoprotocol-2.6.0.tar.gz

---
+++ packages/autoprotocol/autoprotocol-2.6.0.tar.gz/autoprotocol-2.6.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='autoprotocol',
+    url='http://github.com/autoprotocol/autoprotocol-python',
+    author='Tali Herzka',
+    description='Python library for generating Autoprotocol',
+    author_email="tali@transcriptic.com",
+    version='2.6.0',
+    test_suite='test',
+    packages=['autoprotocol']
+)
