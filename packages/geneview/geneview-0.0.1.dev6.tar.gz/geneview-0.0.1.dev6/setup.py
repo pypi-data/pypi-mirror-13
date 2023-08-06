82c90bfe5ec11eeca124b631f36650d03aaff595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:34 +0000

    Add package geneview/0.0.1.dev6/geneview-0.0.1.dev6.tar.gz

---
+++ packages/geneview/geneview-0.0.1.dev6.tar.gz/geneview-0.0.1.dev6/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# Copyright (C) 2016 Shujia Huang <huangshujia9@gmail.com>
+import os
+# temporarily redirect config directory to prevent matplotlib importing
+# testing that for writeable directory which results in sandbox error in
+# certain easy_install versions
+os.environ["MPLCONFIGDIR"] = "."
+
+DESCRIPTION = "Geneview: A python package for genomics data visualization"
+LONG_DESCRIPTION = """\
+Geneview is a library for making attractive and informative genomics graphics in Python. It is built on top of matplotlib and tightly integrated with the PyData stack, including support for numpy data structures.
+
+Some of the features that geneview offers are
+
+- Functions for visualizing univariate and bivariate distributions or for comparing them between subsets of data
+
+- Tools that fit and visualize linear regression models for different kinds of independent and dependent variables
+
+- High-level abstractions for structuring grids of plots that let you easily build complex visualizations
+"""
+
+DISTNAME = 'geneview'
+MAINTAINER = 'Shujia Huang'
+MAINTAINER_EMAIL = 'huangshujia9@gmail.com'
+URL = 'https://github.com/ShujiaHuang/geneview'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/ShujiaHuang/geneview'
+VERSION = '0.0.1.dev6'
+
+
+try:
+    from setuptools import setup, find_packages
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+if __name__ == "__main__":
+
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=[
+              'numpy',
+              'matplotlib',
+              'Cycler',  # option, rarely use
+          ],
+          packages=find_packages(),
+          classifiers=[
+             'Intended Audience :: Science/Research',
+             'Programming Language :: Python :: 2.7',
+             'Programming Language :: Python :: 3.3',
+             'Programming Language :: Python :: 3.4',
+             'License :: OSI Approved :: BSD License',
+             'Topic :: Scientific/Engineering :: Bio-Informatics',
+             'Topic :: Scientific/Engineering :: Visualization',
+             'Topic :: Multimedia :: Graphics',
+             'Operating System :: POSIX',
+             'Operating System :: Unix',
+             'Operating System :: MacOS'],
+          )
