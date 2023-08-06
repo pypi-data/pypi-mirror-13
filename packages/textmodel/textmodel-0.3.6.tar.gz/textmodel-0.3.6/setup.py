439da3ce2a6195571efc1d6a811f0dbccb402122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:33 +0000

    Add package textmodel/0.3.6/textmodel-0.3.6.tar.gz

---
+++ packages/textmodel/textmodel-0.3.6.tar.gz/textmodel-0.3.6/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: latin-1 -*-
+
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='textmodel',
+      version='0.3.6',
+      description = \
+          'A data type for storing and manipulating rich text data. ' \
+          'It aims to be fast and efficient and it is suited even for ' \
+          'very long texts.',
+      long_description = long_description,
+      author='C. Ecker',
+      author_email='textmodelview@gmail.com',
+      url='https://pypi.python.org/pypi/textmodel/',
+      license='BSD',
+      packages=['textmodel'],
+      platforms = ['any'],
+     )
+
