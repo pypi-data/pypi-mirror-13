6292217f39debef915c40bede6822d5d3d906169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:17 +0000

    Add package swagger-schema/0.0.1/swagger_schema-0.0.1-py2.py3-none-any.whl

---
+++ packages/swagger-schema/swagger_schema-0.0.1-py2.py3-none-any.whl/swagger_schema/__init__.py
@@ -0,0 +1,7 @@
+from .operation import Operation
+from .paths import Paths
+from .path import Path
+from .swagger import Swagger
+from .info import Info
+from .response import Response
+from .responses import Responses
