acf7cb114f1ffe75db118af69ef6db58ca927d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:15 +0000

    Add package db-migrator/0.0.2/db-migrator-0.0.2.tar.gz

---
+++ packages/db-migrator/db-migrator-0.0.2.tar.gz/db-migrator-0.0.2/dbmigrator/cli.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2015, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+
+import argparse
+import os
+import sys
+
+from . import commands, utils
+
+
+DEFAULTS = {
+    }
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='DB Migrator')
+
+    parser.add_argument('--migrations-directory')
+
+    parser.add_argument('--config')
+
+    parser.add_argument('--db-connection-string',
+                        help='a psycopg2 db connection string')
+
+    subparsers = parser.add_subparsers(help='commands')
+    commands.load_cli(subparsers)
+
+    args = parser.parse_args(argv)
+    args = vars(args)
+
+    if args.get('config') and os.path.exists(args['config']):
+        utils.get_settings_from_config(args['config'], [
+            'migrations-directory',
+            'db-connection-string',
+            ], args)
+    utils.get_settings_from_entry_points(args)
+
+    for name, value in DEFAULTS.items():
+        if not args.get(name):
+            args[name] = value
+
+    if 'cmmd' not in args:
+        parser.print_help()
+        return parser.error('command missing')
+
+    args['migrations_directory'] = os.path.relpath(
+        args['migrations_directory'])
+
+    return args['cmmd'](**args)
---
+++ packages/db-migrator/db-migrator-0.0.2.tar.gz/db-migrator-0.0.2/dbmigrator/commands/generate.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2015, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+
+import datetime
+import os
+
+
+__all__ = ('cli_loader',)
+
+
+def cli_command(migration_name='', **kwargs):
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = '{}_{}.py'.format(timestamp, migration_name)
+    directory = kwargs['migrations_directory']
+    path = os.path.join(directory, filename)
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    with open(path, 'w') as f:
+        f.write("""\
+# -*- coding: utf-8 -*-
+
+def up(cursor):
+    # TODO migration code
+    pass
+
+
+def down(cursor):
+    # TODO rollback code
+    pass
+""")
+    print('Generated migration script "{}"'.format(path))
+
+
+def cli_loader(parser):
+    parser.add_argument('migration_name')
+    return cli_command
