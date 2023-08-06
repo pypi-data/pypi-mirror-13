d5e239015c9c411ae99a680c6069b5a86c92784d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:59 +0000

    Add package hyuki-cvs-graph/0.1.0/hyuki-cvs-graph-0.1.0.tar.gz

---
+++ packages/hyuki-cvs-graph/hyuki-cvs-graph-0.1.0.tar.gz/hyuki-cvs-graph-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+__VERSION__ = '0.1.0'
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+]
+
+setup(
+    name='hyuki-cvs-graph',
+    version=__VERSION__,
+    description='view graph for working log',
+    long_description=open('README.rst').read(),
+    author='Yassu',
+    author_email='mathyassu@gmail.com',
+    url='https://github.com/yassu/hyuki-cvs-graph',
+    classifiers=classifiers,
+    entry_points="""
+       [console_scripts]
+       hyuki-graph = hyuki_graph.hyuki_graph:main
+    """,
+)
+
