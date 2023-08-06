de8945ddde0ed63c278442a8e32c72f2f726e0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:22 +0000

    Add package timecalc/0.1.3/timecalc-0.1.3.tar.gz

---
+++ packages/timecalc/timecalc-0.1.3.tar.gz/timecalc-0.1.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name="timecalc",
+    version="0.1.3",
+    description="a tool to summarize your bills of time",
+    author="Xin YUE",
+    author_email="yuecn41@gmail.com",
+    url="https://github.com/yuex/timecalc",
+    package_dir={'': 'src'},
+    py_modules=['timecalc'],
+    install_requires=[
+        "PyYAML==3.11",
+    ],
+    entry_points={
+        'console_scripts': ['timecalc=timecalc:main'],
+    },
+)
