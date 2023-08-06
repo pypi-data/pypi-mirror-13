6dc885140128514d928d55c7d5591d846884e4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:47 +0000

    Add package iterm2-color-scheme/0.1.8/iterm2-color-scheme-0.1.8.tar.gz

---
+++ packages/iterm2-color-scheme/iterm2-color-scheme-0.1.8.tar.gz/iterm2-color-scheme-0.1.8/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='iterm2-color-scheme',
+    version=(open(os.path.join(os.path.dirname(__file__),
+                               'iterm2_color_scheme',
+                               'version.txt'))
+             .read().strip()),
+    author='Dan Davison',
+    author_email='dandavison7@gmail.com',
+    description="Command-line color scheme selector for iTerm2",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'iterm2-color-scheme = iterm2_color_scheme.iterm2_color_scheme:main',
+        ],
+    },
+    install_requires = [
+        'py-getch',
+    ],
+)
