2cbe02d1422eb41cd6f5fc85dc99c6b4437186c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:26 +0000

    Add package civicjson/0.1.0/civicjson-0.1.0-py2.py3-none-any.whl

---
+++ packages/civicjson/civicjson-0.1.0-py2.py3-none-any.whl/tests/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of civicjson.
+# https://github.com/DCgov/civic.json-cli
+
+# Licensed under the CC0-1.0 license:
+# https://creativecommons.org/publicdomain/zero/1.0/
+# Copyright (c) 2016, Emanuel Feld <elefbet@gmail.com>
+
+from unittest import TestCase as PythonTestCase
+
+
+class TestCase(PythonTestCase):
+    pass
