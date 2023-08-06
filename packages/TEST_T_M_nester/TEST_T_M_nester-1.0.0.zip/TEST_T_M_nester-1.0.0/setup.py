51c6b1f33f26162d42cea9c689baf7d7ebcbc578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:05 +0000

    Add package TEST_T_M_nester/1.0.0/TEST_T_M_nester-1.0.0.zip

---
+++ packages/TEST_T_M_nester/TEST_T_M_nester-1.0.0.zip/TEST_T_M_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          =  'TEST_T_M_nester',
+    version       =  '1.0.0',
+    py_modules    =  ['TEST_T_M_nester'],
+    author        =  'TopologyMaster',
+    author_email  =  '543173106@qq.com',
+    url           =  'http://user.qzone.qq.com/543173106',
+    description   =  'A simple printer of nested lists',
+    )
