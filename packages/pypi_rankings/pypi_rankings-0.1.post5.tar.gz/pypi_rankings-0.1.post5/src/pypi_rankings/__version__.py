cf056bda623deb113a702438be2fd28535baf26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package pypi_rankings/0.1.post5/pypi_rankings-0.1.post5.tar.gz

---
+++ packages/pypi_rankings/pypi_rankings-0.1.post5.tar.gz/pypi_rankings-0.1.post5/src/pypi_rankings/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.post5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@github.com:wiggin15/pypi_rankings.git'
+__git_head_hash__ = 'afa6fd9c2b38caac4d86be100ba6ed54e42b8761'
+__git_head_subject__ = 'disable requests warnings'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-01-29 16:06:47'
