cd2e188cbfc62ac0176fe572823df0732f0dbfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:46 +0000

    Add package typhon/0.2.2/typhon-0.2.2.tar.gz

---
+++ packages/typhon/typhon-0.2.2.tar.gz/typhon-0.2.2/typhon/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.2.2'
+
+from . import arts
+from . import cm
+from . import files
+from . import utils
+
+
+def _runtest():
+    """Run all tests."""
+    from os.path import dirname
+    import nose
+    loader = nose.loader.TestLoader(workingDir=dirname(__file__))
+    return nose.run(testLoader=loader)
+
+test = _runtest
