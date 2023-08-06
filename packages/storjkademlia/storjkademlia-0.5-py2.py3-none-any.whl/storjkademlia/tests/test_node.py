46a339fa5e99cf5a7def061737a8edb09b5cb1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:56 +0000

    Add package storjkademlia/0.5/storjkademlia-0.5-py2.py3-none-any.whl

---
+++ packages/storjkademlia/storjkademlia-0.5-py2.py3-none-any.whl/storjkademlia/tests/test_node.py
@@ -0,0 +1,58 @@
+import os
+import hashlib
+import binascii
+from past.builtins import long
+
+from twisted.trial import unittest
+
+from storjkademlia.node import Node, NodeHeap
+from storjkademlia.tests.utils import mknode
+
+
+class NodeTest(unittest.TestCase):
+    def test_longID(self):
+        rid = hashlib.sha1(os.urandom(32)).digest()
+        n = Node(rid)
+        self.assertEqual(n.long_id, long(binascii.hexlify(rid), 16))
+
+    def test_distanceCalculation(self):
+        ridone = hashlib.sha1(os.urandom(32))
+        ridtwo = hashlib.sha1(os.urandom(32))
+
+        shouldbe = long(ridone.hexdigest(), 16) ^ long(ridtwo.hexdigest(), 16)
+        none = Node(ridone.digest())
+        ntwo = Node(ridtwo.digest())
+        self.assertEqual(none.distanceTo(ntwo), shouldbe)
+
+
+class NodeHeapTest(unittest.TestCase):
+    def test_maxSize(self):
+        n = NodeHeap(mknode(intid=0), 3)
+        self.assertEqual(0, len(n))
+
+        for d in range(10):
+            n.push(mknode(intid=d))
+        self.assertEqual(3, len(n))
+
+        self.assertEqual(3, len(list(n)))
+
+    def test_iteration(self):
+        heap = NodeHeap(mknode(intid=0), 5)
+        nodes = [mknode(intid=x) for x in range(10)]
+        for index, node in enumerate(nodes):
+            heap.push(node)
+        for index, node in enumerate(heap):
+            self.assertEqual(index, node.long_id)
+            self.assertTrue(index < 5)
+
+    def test_remove(self):
+        heap = NodeHeap(mknode(intid=0), 5)
+        nodes = [mknode(intid=x) for x in range(10)]
+        for node in nodes:
+            heap.push(node)
+
+        heap.remove([nodes[0].id, nodes[1].id])
+        self.assertEqual(len(list(heap)), 5)
+        for index, node in enumerate(heap):
+            self.assertEqual(index + 2, node.long_id)
+            self.assertTrue(index < 5)
