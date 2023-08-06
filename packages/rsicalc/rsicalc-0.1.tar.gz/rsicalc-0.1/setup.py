ec8a42f6149663436cd96dba92e09f9e4872871d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:42 +0000

    Add package rsicalc/0.1/rsicalc-0.1.tar.gz

---
+++ packages/rsicalc/rsicalc-0.1.tar.gz/rsicalc-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rsicalc',
+      version='0.1',
+      description='Relative Strength Index Calculator',
+      author='Vivek Ayer',
+      author_email='ayer@pobox.com',
+      url='https://github.com/vsayer/rsicalc',
+      scripts=['bin/rsicalc'],
+      )
