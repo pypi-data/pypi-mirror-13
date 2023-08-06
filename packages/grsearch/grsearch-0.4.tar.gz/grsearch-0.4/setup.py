7c1115d7ed9694cc0af4b72f456f6703b0f52cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:38 +0000

    Add package grsearch/0.4/grsearch-0.4.tar.gz

---
+++ packages/grsearch/grsearch-0.4.tar.gz/grsearch-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='grsearch',
+	version='0.4',
+	author='herrersystem',
+	author_email='herrersystem@mailoo.org',
+	url="http://github.com/herrersystem/grsearch",
+	keywords='grammatical search find',
+	description='find keyword within string chars or files.',
+	license='MIT',
+	packages=["grsearch"],
+	classifiers=[
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
+	],
+)
