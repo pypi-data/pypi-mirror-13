b4e946be530aea7ebb5617cc028ad7c38c9b7fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:26 +0000

    Add package calculate_lu/1.0.1/calculate_lu-1.0.1.tar.gz

---
+++ packages/calculate_lu/calculate_lu-1.0.1.tar.gz/calculate_lu-1.0.1/calculate_lu/help.py
@@ -0,0 +1,6 @@
+def sum(*values):
+    s = 0
+    for v in values:
+        i = int(v)
+        s = s + i
+    print(s)
---
+++ packages/calculate_lu/calculate_lu-1.0.1.tar.gz/calculate_lu-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'calculate_lu',
+    version = '1.0.1',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
