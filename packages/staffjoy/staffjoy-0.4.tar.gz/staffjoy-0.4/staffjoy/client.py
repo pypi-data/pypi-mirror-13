0ffc70bcf4115f18d8e71d3a6ab1a0bf4d5a6d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:37 +0000

    Add package staffjoy/0.4/staffjoy-0.4.tar.gz

---
+++ packages/staffjoy/staffjoy-0.4.tar.gz/staffjoy-0.4/staffjoy/client.py
@@ -0,0 +1,37 @@
+from .resource import Resource
+from .resources.organization import Organization
+from .resources.cron import Cron
+from .resources.user import User
+from .resources.plan import Plan
+
+
+class Client(Resource):
+    def get_organizations(self, limit=25, offset=0, **kwargs):
+        return Organization.get_all(parent=self,
+                                    limit=limit,
+                                    offset=offset,
+                                    **kwargs)
+
+    def get_organization(self, id):
+        return Organization.get(parent=self, id=id)
+
+    def create_organization(self, **kwargs):
+        return Organization.create(parent=self, **kwargs)
+
+    def cron(self):
+        """Internal only - cron job manual timer"""
+        return Cron.get_all(parent=self)
+
+    def get_users(self, limit=25, offset=0, **kwargs):
+
+        # Some supported filters: filterbyUsername, filterByEmail
+        return User.get_all(parent=self, limit=limit, offset=offset, **kwargs)
+
+    def get_user(self, id):
+        return User.get(parent=self, id=id)
+
+    def get_plans(self, **kwargs):
+        return Plan.get_all(parent=self, **kwargs)
+
+    def get_plan(self, id):
+        return Plan.get(parent=self, id=id)
