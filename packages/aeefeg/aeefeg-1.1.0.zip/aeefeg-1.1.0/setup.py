42972297a0854d3750a4f2142469360af0c392bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:26 +0000

    Add package aeefeg/1.1.0/aeefeg-1.1.0.zip

---
+++ packages/aeefeg/aeefeg-1.1.0.zip/aeefeg-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'aeefeg', 
+        version      = '1.1.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['aeefeg'],
+        author       = 'ermytung',
+        author_email = '2073539597@qq.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
