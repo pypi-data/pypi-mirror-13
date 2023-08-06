a79f7aa335627c503fba0c0a242c9a94110efbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:41 +0000

    Add package metapensiero.sqlalchemy.proxy/3.2/metapensiero.sqlalchemy.proxy-3.2.tar.gz

---
+++ packages/metapensiero.sqlalchemy.proxy/metapensiero.sqlalchemy.proxy-3.2.tar.gz/metapensiero.sqlalchemy.proxy-3.2/src/metapensiero/sqlalchemy/proxy/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# :Project:   metapensiero.sqlalchemy.proxy
+# :Created:   lun 08 apr 2013 11:22:14 CEST
+# :Author:    Lele Gaifax <lele@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: © 2013, 2016 Lele Gaifax
+#
