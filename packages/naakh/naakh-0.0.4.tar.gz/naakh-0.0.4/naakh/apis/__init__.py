ffb92a2361c998655ead5062ec377352b8ff4075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:46 +0000

    Add package naakh/0.0.4/naakh-0.0.4.tar.gz

---
+++ packages/naakh/naakh-0.0.4.tar.gz/naakh-0.0.4/naakh/apis/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .translation_api import TranslationApi
+from .translatedtext_api import TranslatedtextApi
+from .tones_api import TonesApi
+from .oauth_api import OauthApi
+from .topics_api import TopicsApi
+from .translate_api import TranslateApi
+from .languages_api import LanguagesApi
---
+++ packages/naakh/naakh-0.0.4.tar.gz/naakh-0.0.4/naakh/models/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# import models into model package
+from .translation_request_creation_payload import TranslationRequestCreationPayload
+from .translation_request import TranslationRequest
+from .access_token_creation_payload import AccessTokenCreationPayload
+from .topics_resource import TopicsResource
+from .translated_text_update_payload import TranslatedTextUpdatePayload
+from .tones_resource import TonesResource
+from .languages_resource import LanguagesResource
+from .language import Language
+from .topic import Topic
+from .tone import Tone
+from .meta import Meta
+from .access_token_response_payload import AccessTokenResponsePayload
+from .error import Error
+from .naakh_error import NaakhError
+from .simple_translation_request import SimpleTranslationRequest
+from .translated_text import TranslatedText
+from .translation_progress import TranslationProgress
+from .metadata import Metadata
