87f0da240dd39c94dc240854b80eaf3fe69b3a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package virtenviro/0.7.10/virtenviro-0.7.10.zip

---
+++ packages/virtenviro/virtenviro-0.7.10.zip/virtenviro-0.7.10/virtenviro/content/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import *
+from django.conf import settings
+
+
+SLUG_REGEXP = '[0-9A-Za-z-_.//]+'
+if settings.APPEND_SLASH:
+    regexp = r'^(?P<slug>%s)/$' % SLUG_REGEXP
+else:
+    regexp = r'^(?P<slug>%s)/$' % SLUG_REGEXP
+
+urlpatterns = patterns('',
+    url(r'^$', 'virtenviro.content.views.home', name='home'),
+    url(r'^[a-z0-9-_/]+$', 'virtenviro.content.views.view', name='page'),
+)
+
