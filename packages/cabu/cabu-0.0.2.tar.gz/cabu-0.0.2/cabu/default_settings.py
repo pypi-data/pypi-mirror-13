d01d8a5e39e01d1399ca37a812d97222ce590c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:34 +0000

    Add package cabu/0.0.2/cabu-0.0.2.tar.gz

---
+++ packages/cabu/cabu-0.0.2.tar.gz/cabu-0.0.2/cabu/default_settings.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+DEBUG = False
+CABU_TEST = None
+LOGGER_LEVEL = 'error'
+LOGGER_PATH = ''
+
+CABU_SETTINGS = 'cabu.default_settings'
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'admin'
+
+DRIVER_NAME = None
+DRIVER_BINARY_PATH = None
+# DRIVER_BINARY_PATH = '/Users/t.leveque/Applications/Firefox.app/Contents/MacOS/firefox'
+DRIVER_WINDOWS_WIDTH = 1024
+DRIVER_WINDOWS_HEIGHT = 768
+DRIVER_PAGE_TIMEOUT = 30  # In seconds.
+
+FTP_HOST = None
+FTP_LOGIN = None
+FTP_PASSWORD = None
+
+HEADLESS = True  # Run the Browser in Xvfd
+HEADERS = None
+
+IO_CONCURRENCY = False  # If True, Python version should be superior or equal to py34
