433e8b2ec00a137cd4e972bd4e8cfb4a025d323b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:42 +0000

    Add package onenew/1.3.0/onenew-1.3.0.tar.gz

---
+++ packages/onenew/onenew-1.3.0.tar.gz/onenew-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'onenew',
+	version		= '1.3.0',
+	py_modules	= ['onenew'],
+	author		= 'minhtoan',
+	author_email= 'phamminhtoan304@gmail.com',
+	url			= 'facebook.com/phamminhtoan304',
+	description	= 'a simple printer of nested list',
+)
