134fa84912a629462d6a0d0517c2fcfec3a0ff30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:15 +0000

    Add package pytsite/0.56.1/PytSite-0.56.1.tar.gz

---
+++ packages/pytsite/PytSite-0.56.1.tar.gz/PytSite-0.56.1/pytsite/threading/__init__.py
@@ -0,0 +1,27 @@
+"""PytSite Threading.
+"""
+import threading as _threading
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+__lock = _threading.Lock()
+__r_lock = _threading.RLock()
+
+
+def get_lock():
+    return __lock
+
+
+def get_r_lock():
+    return __r_lock
+
+
+def create_thread(target, **kwargs) -> _threading.Thread:
+    return _threading.Thread(target=target, kwargs=kwargs)
+
+
+def active_count() -> int:
+    return _threading.active_count()
