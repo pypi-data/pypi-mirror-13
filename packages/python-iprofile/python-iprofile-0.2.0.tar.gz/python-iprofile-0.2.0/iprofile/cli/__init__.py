e9e7ad22257a045844f0ff8319eb58a54aa3242e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:37 +0000

    Add package python-iprofile/0.2.0/python-iprofile-0.2.0.tar.gz

---
+++ packages/python-iprofile/python-iprofile-0.2.0.tar.gz/python-iprofile-0.2.0/iprofile/cli/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .activate import *  # noqa
+from .active import *  # noqa
+from .add import *  # noqa
+from .config import *  # noqa
+from .clear import *  # noqa
+from .create import *  # noqa
+from .deactivate import *  # noqa
+from .delete import *  # noqa
+from .djshell import *  # noqa
+from .init import *  # noqa
+from .list import *  # noqa
+from .magic import *  # noqa
+from .save import *  # noqa
+from .shell import *  # noqa
