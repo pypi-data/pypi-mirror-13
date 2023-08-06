11c24df7b9ab77558d839028066a08a4d8f9f47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:28 +0000

    Add package LLnester/1.0.0/LLnester-1.0.0.zip

---
+++ packages/LLnester/LLnester-1.0.0.zip/LLnester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'LLnester',                                #当前发布的名称 
+    version = '1.0.0',                                #模块版本
+    py_modules = ['llnester'],                        #对应模块的名称
+    author = 'CC',                                    #模块的作者
+    author_email = 'chengchao.24@163.com',            #作者的email
+    url = 'chengchao.24@163.com',                     #作者的个人首页
+    description = 'A simple printer of nested lists', #模块功能描述
+    )
