556c2e2a278010866ba8ae8b9ea7e68587678412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:50 +0000

    Add package nsot_sync/0.4.2/nsot_sync-0.4.2.tar.gz

---
+++ packages/nsot_sync/nsot_sync-0.4.2.tar.gz/nsot_sync-0.4.2/nsot_sync/commands/facter.py
@@ -0,0 +1,16 @@
+import json
+import click
+from nsot_sync.drivers import facter
+
+
+@click.command()
+@click.pass_context
+def cli(ctx):
+    '''The facter driver can add attributes to created resources from facter'''
+    driver = facter.FacterDriver(click_ctx=ctx)
+    if ctx.obj['NOOP']:
+        click.echo(json.dumps(driver.get_resources()))
+        return
+
+    driver.ensure_attrs()
+    driver.handle_resources()
