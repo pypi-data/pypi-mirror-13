9557f0ef02bf393b8d761568c09a9026570108a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:25 +0000

    Add package crispy-forms-foundation/0.5.4/crispy-forms-foundation-0.5.4.tar.gz

---
+++ packages/crispy-forms-foundation/crispy-forms-foundation-0.5.4.tar.gz/crispy-forms-foundation-0.5.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crispy-forms-foundation',
+    version=__import__('crispy_forms_foundation').__version__,
+    description=__import__('crispy_forms_foundation').__doc__,
+    long_description=open('README.rst').read(),
+    author='David Thenon',
+    author_email='sveetch@gmail.com',
+    url='http://pypi.python.org/pypi/crispy-forms-foundation',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.4",
+        "Framework :: Django :: 1.5",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        'django-crispy-forms >= 1.4'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
