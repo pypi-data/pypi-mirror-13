e7d1e64e4e975be2b9744fa1ece7db8afafac149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:37 +0000

    Add package django-scrub-pii/1.1.2/django_scrub_pii-1.1.2-py2.py3-none-any.whl

---
+++ packages/django-scrub-pii/django_scrub_pii-1.1.2-py2.py3-none-any.whl/tests/testapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from scrubpii import allow_sensitive_fields
+
+
+with allow_sensitive_fields():
+    @python_2_unicode_compatible
+    class Person(models.Model):
+        first_name = models.CharField(max_length=30)
+        last_name = models.CharField(max_length=30)
+        biography = models.TextField()
+        date_of_birth = models.DateField()
+        email = models.EmailField()
+
+        def __str__(self):
+            return "{0} {1}".format(self.first_name, self.last_name)
+
+        class Meta:
+            sensitive_fields = {'biography', 'last_name', 'first_name', 'email', 'date_of_birth'}
+
+
+@python_2_unicode_compatible
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Person)
+
+    def __str__(self):
+        return "{0} by {1}".format(self.title, self.author)
+
+
+with allow_sensitive_fields():
+    # Test that importing from another models file works, as recommended by README
+    from .sensitive_models import *  # noqa
