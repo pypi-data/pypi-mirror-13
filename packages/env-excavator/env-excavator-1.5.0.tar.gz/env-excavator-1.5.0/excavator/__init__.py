c76a0b04339a7c1931d22fc5d343a742fd7c0bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:11 +0000

    Add package env-excavator/1.5.0/env-excavator-1.5.0.tar.gz

---
+++ packages/env-excavator/env-excavator-1.5.0.tar.gz/env-excavator-1.5.0/excavator/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.5.0'
+
+from .utils import (  # NOQA
+    env_string,
+    env_bool,
+    env_list,
+    env_int,
+    env_float,
+    env_timestamp,
+    env_iso8601,
+    get,
+)
