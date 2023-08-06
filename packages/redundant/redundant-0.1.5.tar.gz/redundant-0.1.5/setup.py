6b0edfb39185516560d9778fdd7ced4223b1da9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:59 +0000

    Add package redundant/0.1.5/redundant-0.1.5.tar.gz

---
+++ packages/redundant/redundant-0.1.5.tar.gz/redundant-0.1.5/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(name='redundant',
+      version='0.1.5',
+      description='Project technical debt analysis tool',
+      author='Calvin Spealman',
+      author_email='calvin@caktusgroup.com',
+      url='https://www.github.com/caktus/redundant',
+      packages=['redundant'],
+      include_package_data=True,
+      scripts=['scripts/redundant'],
+      long_description=long_description,
+)
