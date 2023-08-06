ffeadb0243b72c0bb6fccd540d4ae85a18fceaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:57 +0000

    Add package collective.searchandreplace/6.0.2/collective.searchandreplace-6.0.2.tar.gz

---
+++ packages/collective.searchandreplace/collective.searchandreplace-6.0.2.tar.gz/collective.searchandreplace-6.0.2/collective/searchandreplace/browser/searchreplacetable.py
@@ -0,0 +1,43 @@
+# -*- coding: us-ascii -*-
+from collective.searchandreplace.interfaces import ISearchReplaceUtility
+from zope.component import getUtility
+from zope.publisher.browser import BrowserView
+
+
+class SearchReplaceTable(BrowserView):
+    """ View class for search and replace preivew table widget."""
+
+    def getItems(self):
+        """ Get preview items """
+        # Set search parameters
+        srutil = getUtility(ISearchReplaceUtility)
+        stext = None
+        if 'form.findWhat' in self.request:
+            stext = self.request['form.findWhat']
+        if not stext:
+            return []
+        if 'form.searchSubfolders' in self.request:
+            subfolders = True
+        else:
+            subfolders = False
+        if 'form.matchCase' in self.request:
+            mcase = True
+        else:
+            mcase = False
+        # Get search results
+        results = srutil.searchObjects(
+            self.context,
+            stext,
+            searchSubFolders=subfolders,
+            matchCase=mcase)
+        return results
+
+    def getRelativePath(self, path):
+        """ Get a relative path """
+        cpath = '/'.join(self.context.getPhysicalPath())
+        rpath = path[len(cpath):]
+        if rpath:
+            rpath = '.' + rpath
+        else:
+            rpath = './'
+        return rpath
