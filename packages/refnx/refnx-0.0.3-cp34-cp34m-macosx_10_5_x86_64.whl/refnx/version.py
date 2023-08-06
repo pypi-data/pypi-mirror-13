d1c1be035369380539b1b0d54f612bf9907a0a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:13 +0000

    Add package refnx/0.0.3/refnx-0.0.3-cp34-cp34m-macosx_10_5_x86_64.whl

---
+++ packages/refnx/refnx-0.0.3-cp34-cp34m-macosx_10_5_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.0.3'
+version = '0.0.3'
+full_version = '0.0.3'
+git_revision = '0b12b01cebadc9bd033d87c376b3652d9ceb8d06'
+release = True
+if not release:
+    version = full_version
