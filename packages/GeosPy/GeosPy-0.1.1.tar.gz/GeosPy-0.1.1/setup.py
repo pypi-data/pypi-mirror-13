50bf2fcefac7bd47c46db7f2dde4cdf9ef0e650e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:22 +0000

    Add package GeosPy/0.1.1/GeosPy-0.1.1.tar.gz

---
+++ packages/GeosPy/GeosPy-0.1.1.tar.gz/GeosPy-0.1.1/setup.py
@@ -0,0 +1,46 @@
+"""
+  Copyright (c) 2015, Tyler Finethy
+
+  All rights reserved. See LICENSE file for details
+"""
+# Setup config from http://docs.cython.org
+from distutils.extension import Extension
+from distutils.core import setup
+# Import Cython
+from Cython.Build import cythonize
+# Import numpy
+import numpy as np
+
+# extensions to compile
+extensions=[
+    Extension("GeosPy/utilities/*.pyx",
+        ["GeosPy/utilities/*.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-w"]),
+    Extension("GeosPy/models/*.pyx",
+        ["GeosPy/models/*.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-w"]),
+    Extension("GeosPy/*.pyx",
+        ["GeosPy/*.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-w"])
+    ]
+
+# full config
+config = {
+    'description': 'GeosPy',
+    'author': 'Tyler Finethy',
+    'url': 'https://github.com/tylfin/GeosPy',
+    'download_url': 'https://github.com/tylfin/GeosPy',
+    'author_email': 'tylfin@gmail.com',
+    'version': '0.1.1',
+    'packages': ['GeosPy', 'GeosPy.utilities', 'GeosPy.models'],
+    'install_requires' : ['Cython', 'numpy', 'scipy'],
+    'scripts': [],
+    'name': 'GeosPy',
+    'ext_modules':cythonize(extensions)
+}
+
+# setting up
+setup(**config)
