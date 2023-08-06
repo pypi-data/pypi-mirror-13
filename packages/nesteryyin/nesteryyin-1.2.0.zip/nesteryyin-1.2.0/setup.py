b4f1e314a0c2f6a073859e61e93ccc78d0a5d4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:16 +0000

    Add package nesteryyin/1.2.0/nesteryyin-1.2.0.zip

---
+++ packages/nesteryyin/nesteryyin-1.2.0.zip/nesteryyin-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name         = 'nesteryyin',
+		version      = '1.2.0',
+		py_module    = ['nesteryyin'],
+		author       = 'alexyin',
+		author_email = 'geniustalent2@163.com',
+		url          = 'http://www.headfirstlabs.com',
+		description  = 'A simple printer of nested lists'
+		)
