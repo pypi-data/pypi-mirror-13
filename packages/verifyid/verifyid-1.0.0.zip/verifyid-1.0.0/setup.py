018547980056e6000defd5fe6f379b3604417679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:14 +0000

    Add package verifyid/1.0.0/verifyid-1.0.0.zip

---
+++ packages/verifyid/verifyid-1.0.0.zip/verifyid-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='verifyid',
+    version='1.0.0',
+    packages=["verifyid"],
+    description='verify Taiwanese identy number',
+    author='Jamie Chuang',
+    author_email='jamiechuang1228@gmail.com',
+    url='https://github.com/ChuangYuMing/verifyid',
+    keywords=['verify', 'id', 'taiwan'],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    include_package_data=True,
+)
