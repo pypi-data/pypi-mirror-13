713af49b5fbd1c4b52f329aa91c19c2cd84cb8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:09 +0000

    Add package tnewnester/3.4.4/tnewnester-3.4.4.zip

---
+++ packages/tnewnester/tnewnester-3.4.4.zip/tnewnester-3.4.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name ='tnewnester',
+    version ='3.4.4',
+    py_modules =['G:\\newnester\\tnewnester'],
+    author ='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url= 'http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
+    
+    
