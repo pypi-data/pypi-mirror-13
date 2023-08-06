ec2cee5d6aa75113e670177188a4871386790fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package cos2/0.1.1/cos2-0.1.1.tar.gz

---
+++ packages/cos2/cos2-0.1.1.tar.gz/cos2-0.1.1/examples/bucket.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import  cos2
+import uuid
+if __name__ == '__main__':
+
+    endpoint = 'cos-beta.chinac.com'
+    accessKeyId = '9af33aad368b482f92db43d7431087f9'
+    accessKeySecret = '854c49419e4e4b64949a29420bb007c5'
+
+    bucketName = 'my-first-cos-bucket-' + str(uuid.uuid1())
+    objectName = 'my-first-cos-object.txt'
+
+# print bucketName
+    print '==========================================='
+    print 'Getting Started with COS SDK for Python'
+    print '===========================================\n'
+
+# Init COS Python SDK
+    auth = cos2.Auth(accessKeyId, accessKeySecret)
+    bucket = cos2.Bucket(auth, endpoint, bucketName)
+
+# Create a new COS PublicRead bucket
+    print 'Creating bucket ' + bucketName
+# bucket.create_bucket(cos2.BUCKET_ACL_PUBLIC_READ)
+    bucket.create_bucket()
+
+# List the buckets in your account
+    print 'Listing buckets'
+    service = cos2.Service(auth, endpoint, connect_timeout=30)
+    for bucketInfo in service.list_buckets().buckets:
+        print ' - ' + bucketInfo.name
+
+# Upload an object to your bucket
+    print 'Uploading a new object to COS from memory'
+    content = 'Thank you for using COS SDK for Python'
+    bucket.put_object(objectName, content)
+
+# Determine whether an object residents in your bucket
+    exist = bucket.object_exists(objectName)
+    print exist
+
+
+
---
+++ packages/cos2/cos2-0.1.1.tar.gz/cos2-0.1.1/tests/test_multipart.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import cos2
+
+from common  import *
+from cos2 import utils
+
+
+
+class TestMultipart(CosTestCase):
+    def test_multipart(self):
+        key = self.random_key()
+        content = random_bytes(100 * 1024)
+
+        parts = []
+        upload_id = self.bucket.init_multipart_upload(key).upload_id
+
+        result = self.bucket.upload_part(key, upload_id, 1, content)
+        parts.append(cos2.models.PartInfo(1, result.etag))
+
+        self.bucket.complete_multipart_upload(key, upload_id, parts)
+
+        result = self.bucket.get_object(key)
+        self.assertEqual(utils.content_md5(content), utils.content_md5(result.read()))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
