12c1bd06b8ed6ca06ca37bb88ea27c25d2a1c6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:32 +0000

    Add package molo.servicedirectory/1.1.0/molo.servicedirectory-1.1.0.tar.gz

---
+++ packages/molo.servicedirectory/molo.servicedirectory-1.1.0.tar.gz/molo.servicedirectory-1.1.0/molo/servicedirectory/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home,
+        name='home'),
+
+    url(r'^location-search/$', views.location_search,
+        name='location-search'),
+
+    url(r'^location-results/$', views.location_results,
+        name='location-results'),
+
+    url(r'^service-results/$', views.service_results,
+        name='service-results'),
+
+    url(r'^service-detail/(?P<service_id>[0-9]+)/$', views.service_detail,
+        name='service-detail'),
+]
