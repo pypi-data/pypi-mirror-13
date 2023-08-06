8e083a4a08fdb02dfb9109148a88283c996e37e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:59 +0000

    Add package papavisor/0.0.1a0/papavisor-0.0.1a0.tar.gz

---
+++ packages/papavisor/papavisor-0.0.1a0.tar.gz/papavisor-0.0.1a0/CHANGES.rst
@@ -0,0 +1,8 @@
+CHANGES
+=======
+
+0.0.1a (2016-02-10)
+-------------------
+
+- papavisor was born.
+  [pcdummy]
---
+++ packages/papavisor/papavisor-0.0.1a0.tar.gz/papavisor-0.0.1a0/setup.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""papavisor package."""
+
+import os
+from setuptools import setup
+
+# The choices for the Trove Development Status line:
+# Development Status :: 5 - Production/Stable
+# Development Status :: 4 - Beta
+# Development Status :: 3 - Alpha
+
+
+doclines = __doc__.split("\n")
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="papavisor",
+    version='0.0.1a',
+    author="Webmeisterei GmbH",
+    maintainer="Rene´ Jochum",
+    maintainer_email="rene@webmeisterei.com",
+    author_email="rene@webmeisterei.com",
+    url="https://github.com/webmeisterei/papavisor",
+    license="MIT",
+    platforms=["any"],
+    description=doclines[0],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Topic :: System :: Boot',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
+    ],
+    keywords="papavisor supervisor supervisord",
+    long_description=(
+        read("README.rst") + "\n\n" +
+        "Changelog\n" +
+        "==============\n\n" +
+        read("CHANGES.rst")
+    ),
+
+    install_requires=[
+        'aiohttp',
+        'setuptools',
+    ],
+    extras_require={
+    },
+    include_package_data=True,
+    zip_safe=False,
+    packages=['papavisor'],
+    package_dir={'papavisor': 'src/papavisor'},
+    scripts=[
+        'src/papavisor/scripts/papavisor',
+        'src/papavisor/scripts/apapavisor',
+    ],
+    data_files=[
+        ('/etc/papavisor', [
+                'config/00_defaults.json',
+                'config/apapavisor.sh'
+            ],
+         ),
+    ],
+)
