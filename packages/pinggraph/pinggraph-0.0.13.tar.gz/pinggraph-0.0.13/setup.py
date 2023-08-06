53ab8fa2614f4e296ac1062d7468dfde7fc33f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:36 +0000

    Add package pinggraph/0.0.13/pinggraph-0.0.13.tar.gz

---
+++ packages/pinggraph/pinggraph-0.0.13.tar.gz/pinggraph-0.0.13/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pinggraph',
+    version='0.0.13',
+    packages=['gping'],
+    url='https://github.com/orf/gping',
+    license='',
+    author='Orf',
+    author_email='tom@tomforb.es',
+    description='Ping, but with a graph. Visit the Github page for more info',
+    requires=['colorama'],
+    install_requires=['colorama'],
+    entry_points={
+        "console_scripts": [
+            "gping=gping.pinger:run"
+        ]
+    }
+)
