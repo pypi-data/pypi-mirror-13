f5c16bfd2c3f187efc1c85d3b5954b7b309ffa1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:56 +0000

    Add package P_nester/1.3.0/P_nester-1.3.0.zip

---
+++ packages/P_nester/P_nester-1.3.0.zip/P_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'P_nester',
+version = '1.3.0',
+py_modules = ['P_nester'],
+author = 'peter031',
+author_email='peter031@gmail.com',
+url='http://www.headfirstlabs.com',
+description='A sample printer of nested lists',
+)
