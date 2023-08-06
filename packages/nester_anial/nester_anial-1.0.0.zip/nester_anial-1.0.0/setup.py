55008c72618b942775ff8db832dd04104e274ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:21 +0000

    Add package nester_anial/1.0.0/nester_anial-1.0.0.zip

---
+++ packages/nester_anial/nester_anial-1.0.0.zip/nester_anial-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name = 'nester_anial',
+	version = '1.0.0',
+	py_modules = ['nester_anial'],
+	author = 'alisowska',
+	author_email = 'anna-lisowska@outlook.com',
+	description = 'A simple printer of nested list'
+	)
