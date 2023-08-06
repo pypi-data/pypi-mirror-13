609fd485d7dae6e712232a9dac00c5a013908a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:50 +0000

    Add package Hatak_Statics/0.2.5/Hatak_Statics-0.2.5.tar.gz

---
+++ packages/Hatak_Statics/Hatak_Statics-0.2.5.tar.gz/Hatak_Statics-0.2.5/setup.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'hatak>=0.2.1',
+    'hatak_jinja2',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='Hatak_Statics',
+        version='0.2.5',
+        description='Statics plugin for Hatak.',
+        license='Apache License 2.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        namespace_packages=['haplugin'],
+        install_requires=install_requires,
+        zip_safe=False,
+        package_data={
+            '': ['statics/templates/*.jinja2'],
+        }
+    )
