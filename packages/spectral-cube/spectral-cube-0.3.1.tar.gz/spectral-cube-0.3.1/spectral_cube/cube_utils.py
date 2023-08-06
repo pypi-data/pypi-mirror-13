930bd9e35024a6ca8a84630657799be5737b2cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:16 +0000

    Add package spectral-cube/0.3.1/spectral-cube-0.3.1.tar.gz

---
+++ packages/spectral-cube/spectral-cube-0.3.1.tar.gz/spectral-cube-0.3.1/spectral_cube/cube_utils.py
@@ -0,0 +1,232 @@
+from __future__ import print_function, absolute_import, division
+
+import numpy as np
+from astropy.wcs import (WCSSUB_SPECTRAL, WCSSUB_LONGITUDE, WCSSUB_LATITUDE)
+from . import wcs_utils
+from astropy import log
+
+def _fix_spectral(wcs):
+    """
+    Attempt to fix a cube with an invalid spectral axis definition.  Only uses
+    well-known exceptions, e.g. CTYPE = 'VELOCITY'.  For the rest, it will try
+    to raise a helpful error.
+    """
+
+    axtypes = wcs.get_axis_types()
+
+    types = [a['coordinate_type'] for a in axtypes]
+
+    if wcs.naxis not in (3,4):
+        raise TypeError("The WCS has {0} axes of types {1}".format(len(types),
+                                                                   types))
+
+    # sanitize noncompliant headers
+    if 'spectral' not in types:
+        log.warn("No spectral axis found; header may be non-compliant.")
+        for ind,tp in enumerate(types):
+            if tp not in ('celestial','stokes'):
+                if wcs.wcs.ctype[ind] in wcs_utils.bad_spectypes_mapping:
+                    wcs.wcs.ctype[ind] = wcs_utils.bad_spectypes_mapping[wcs.wcs.ctype[ind]]
+
+    return wcs
+
+def _split_stokes(array, wcs):
+    """
+    Given a 4-d data cube with 4-d WCS (spectral cube + stokes) return a
+    dictionary of data and WCS objects for each Stokes component
+
+    Parameters
+    ----------
+    array : `~numpy.ndarray`
+        The input 3-d array with two position dimensions, one spectral
+        dimension, and a Stokes dimension.
+    wcs : `~astropy.wcs.WCS`
+        The input 3-d WCS with two position dimensions, one spectral
+        dimension, and a Stokes dimension.
+    """
+
+    if array.ndim not in (3,4):
+        raise ValueError("Input array must be 3- or 4-dimensional for a"
+                         " STOKES cube")
+
+    if wcs.wcs.naxis != 4:
+        raise ValueError("Input WCS must be 4-dimensional for a STOKES cube")
+
+    wcs = _fix_spectral(wcs)
+
+    # reverse from wcs -> numpy convention
+    axtypes = wcs.get_axis_types()[::-1]
+
+    types = [a['coordinate_type'] for a in axtypes]
+
+    try:
+        # Find stokes dimension
+        stokes_index = types.index('stokes')
+    except ValueError:
+        # stokes not in list, but we are 4d
+        if types.count('celestial') == 2 and types.count('spectral') == 1:
+            if None in types:
+                stokes_index = types.index(None)
+                log.warn("FITS file has no STOKES axis, but it has a blank"
+                         " axis type at index {0} that is assumed to be "
+                         "stokes.".format(4-stokes_index))
+            else:
+                for ii,tp in enumerate(types):
+                    if tp not in ('celestial', 'spectral'):
+                        stokes_index = ii
+                        stokes_type = tp
+
+                log.warn("FITS file has no STOKES axis, but it has an axis"
+                         " of type {1} at index {0} that is assumed to be "
+                         "stokes.".format(4-stokes_index, stokes_type))
+        else:
+            raise IOError("There are 4 axes in the data cube but no STOKES "
+                          "axis could be identified")
+
+    # TODO: make the stokes names more general
+    stokes_names = ["I", "Q", "U", "V"]
+
+    stokes_arrays = {}
+
+    wcs_slice = wcs_utils.drop_axis(wcs, wcs.naxis - 1 - stokes_index)
+
+    if array.ndim == 4:
+        for i_stokes in range(array.shape[stokes_index]):
+
+            array_slice = [i_stokes if idim == stokes_index else slice(None)
+                           for idim in range(array.ndim)]
+
+            stokes_arrays[stokes_names[i_stokes]] = array[array_slice]
+    else:
+        # 3D array with STOKES as a 4th header parameter
+        stokes_arrays['I'] = array
+
+    return stokes_arrays, wcs_slice
+
+
+def _orient(array, wcs):
+    """
+    Given a 3-d spectral cube and WCS, swap around the axes so that the
+    spectral axis cube is the first in Numpy notation, and the last in WCS
+    notation.
+
+    Parameters
+    ----------
+    array : `~numpy.ndarray`
+        The input 3-d array with two position dimensions and one spectral
+        dimension.
+    wcs : `~astropy.wcs.WCS`
+        The input 3-d WCS with two position dimensions and one spectral
+        dimension.
+    """
+
+    if array.ndim != 3:
+        raise ValueError("Input array must be 3-dimensional")
+
+    if wcs.wcs.naxis != 3:
+        raise ValueError("Input WCS must be 3-dimensional")
+
+    wcs = _fix_spectral(wcs)
+
+    # reverse from wcs -> numpy convention
+    axtypes = wcs.get_axis_types()[::-1]
+
+    types = [a['coordinate_type'] for a in axtypes]
+
+    nums = [None if a['coordinate_type'] != 'celestial' else a['number']
+            for a in axtypes]
+
+    if 'stokes' in types:
+        raise ValueError("Input WCS should not contain stokes")
+
+    t = [types.index('spectral'), nums.index(1), nums.index(0)]
+    result_array = array.transpose(t)
+
+    result_wcs = wcs.sub([WCSSUB_LONGITUDE, WCSSUB_LATITUDE, WCSSUB_SPECTRAL])
+
+    return result_array, result_wcs
+
+
+def slice_syntax(f):
+    """
+    This decorator wraps a function that accepts a tuple of slices.
+
+    After wrapping, the function acts like a property that accepts
+    bracket syntax (e.g., p[1:3, :, :])
+
+    Parameters
+    ----------
+    f : function
+    """
+
+    def wrapper(self):
+        result = SliceIndexer(f, self)
+        result.__doc__ = f.__doc__
+        return result
+
+    wrapper.__doc__ = slice_doc.format(f.__doc__ or '',
+                                       f.__name__)
+
+    result = property(wrapper)
+    return result
+
+slice_doc = """
+{0}
+
+Notes
+-----
+Supports efficient Numpy slice notation,
+like ``{1}[0:3, :, 2:4]``
+"""
+
+
+class SliceIndexer(object):
+
+    def __init__(self, func, _other):
+        self._func = func
+        self._other = _other
+
+    def __getitem__(self, view):
+        return self._func(self._other, view)
+
+    def __iter__(self):
+        raise Exception("You need to specify a slice (e.g. ``[:]`` or "
+                        "``[0,:,:]`` in order to access this property.")
+
+
+# TODO: make this into a proper configuration item
+# TODO: make threshold depend on memory?
+MEMORY_THRESHOLD=1e8
+
+def is_huge(cube):
+    if cube.size < MEMORY_THRESHOLD:  # smallish
+        return False
+    else:
+        return True
+
+
+def iterator_strategy(cube, axis=None):
+    """
+    Guess the most efficient iteration strategy
+    for iterating over a cube, given its size and layout
+
+    Parameters
+    ----------
+    cube : SpectralCube instance
+        The cube to iterate over
+    axis : [0, 1, 2]
+        For reduction methods, the axis that is
+        being collapsed
+
+    Returns
+    -------
+    strategy : ['cube' | 'ray' | 'slice']
+        The recommended iteration strategy.
+        *cube* recommends working with the entire array in memory
+        *slice* recommends working with one slice at a time
+        *ray*  recommends working with one ray at a time
+    """
+    # pretty simple for now
+    if cube.size < 1e8:  # smallish
+        return 'cube'
+    return 'slice'
---
+++ packages/spectral-cube/spectral-cube-0.3.1.tar.gz/spectral-cube-0.3.1/spectral_cube/io/fits.py
@@ -0,0 +1,177 @@
+from __future__ import print_function, absolute_import, division
+
+import warnings
+
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.extern import six
+from astropy.utils import OrderedDict
+from astropy.io.fits.hdu.hdulist import fitsopen as fits_open
+
+import numpy as np
+import datetime
+try:
+    from .. import version
+    SPECTRAL_CUBE_VERSION = version.version
+except ImportError:
+    # We might be running py.test on a clean checkout
+    SPECTRAL_CUBE_VERSION = 'dev'
+
+from .. import SpectralCube, StokesSpectralCube, LazyMask
+from .. import cube_utils
+
+
+def first(iterable):
+    return next(iter(iterable))
+
+
+# FITS registry code - once Astropy includes a proper extensible I/O base
+# class, we can use that instead. The following code takes care of
+# interpreting string input (filename), HDU, and HDUList.
+
+def is_fits(input, **kwargs):
+    """
+    Determine whether input is in FITS format
+    """
+    if isinstance(input, six.string_types):
+        if input.lower().endswith(('.fits', '.fits.gz',
+                                   '.fit', '.fit.gz',
+                                   '.fits.Z', '.fit.Z')):
+            return True
+    elif isinstance(input, (fits.HDUList, fits.PrimaryHDU, fits.ImageHDU)):
+        return True
+    else:
+        return False
+
+
+def read_data_fits(input, hdu=None, **kwargs):
+    """
+    Read an array and header from an FITS file.
+
+    Parameters
+    ----------
+    input : str or compatible `astropy.io.fits` HDU object
+        If a string, the filename to read the table from. The
+        following `astropy.io.fits` HDU objects can be used as input:
+        - :class:`~astropy.io.fits.hdu.table.PrimaryHDU`
+        - :class:`~astropy.io.fits.hdu.table.ImageHDU`
+        - :class:`~astropy.io.fits.hdu.hdulist.HDUList`
+    hdu : int or str, optional
+        The HDU to read the table from.
+    """
+
+    if isinstance(input, fits.HDUList):
+
+        # Parse all array objects
+        arrays = OrderedDict()
+        for ihdu, hdu_item in enumerate(input):
+            if isinstance(hdu_item, (fits.PrimaryHDU, fits.ImageHDU)):
+                arrays[ihdu] = hdu_item
+
+        if len(arrays) > 1:
+            if hdu is None:
+                hdu = first(arrays)
+                warnings.warn("hdu= was not specified but multiple arrays"
+                              " are present, reading in first available"
+                              " array (hdu={0})".format(hdu))
+
+            # hdu might not be an integer, so we first need to convert it
+            # to the correct HDU index
+            hdu = input.index_of(hdu)
+
+            if hdu in arrays:
+                array_hdu = arrays[hdu]
+            else:
+                raise ValueError("No array found in hdu={0}".format(hdu))
+
+        elif len(arrays) == 1:
+            array_hdu = arrays[first(arrays)]
+        else:
+            raise ValueError("No arrays found")
+
+    elif isinstance(input, (fits.PrimaryHDU, fits.ImageHDU)):
+
+        array_hdu = input
+
+    else:
+
+        hdulist = fits_open(input, **kwargs)
+
+        try:
+            return read_data_fits(hdulist, hdu=hdu)
+        finally:
+            hdulist.close()
+
+    return array_hdu.data, array_hdu.header
+
+
+def load_fits_cube(input, hdu=0, meta=None, **kwargs):
+    """
+    Read in a cube from a FITS file using astropy.
+
+    Parameters
+    ----------
+    input: str or HDU
+        The FITS cube file name or HDU
+    hdu: int
+        The extension number containing the data to be read
+    meta: dict
+        Metadata (can be inherited from other readers, for example)
+    """
+
+    data, header = read_data_fits(input, hdu=hdu, **kwargs)
+
+    if meta is None:
+        meta = {}
+
+    if 'BUNIT' in header:
+        meta['BUNIT'] = header['BUNIT']
+
+    wcs = WCS(header)
+
+    if wcs.wcs.naxis == 3:
+
+        data, wcs = cube_utils._orient(data, wcs)
+
+        mask = LazyMask(np.isfinite, data=data, wcs=wcs)
+        assert data.shape == mask._data.shape
+        cube = SpectralCube(data, wcs, mask, meta=meta, header=header)
+        assert cube._data.shape == cube._mask._data.shape
+
+    elif wcs.wcs.naxis == 4:
+
+        data, wcs = cube_utils._split_stokes(data, wcs)
+
+        stokes_data = {}
+        for component in data:
+            comp_data, comp_wcs = cube_utils._orient(data[component], wcs)
+            comp_mask = LazyMask(np.isfinite, data=comp_data, wcs=comp_wcs)
+            stokes_data[component] = SpectralCube(comp_data, wcs=comp_wcs,
+                                                  mask=comp_mask, meta=meta,
+                                                  header=header)
+
+        cube = StokesSpectralCube(stokes_data)
+
+    else:
+
+        raise Exception("Data should be 3- or 4-dimensional")
+
+    return cube
+
+
+def write_fits_cube(filename, cube, overwrite=False,
+                    include_origin_notes=True):
+    """
+    Write a FITS cube with a WCS to a filename
+    """
+
+    if isinstance(cube, SpectralCube):
+        hdu = cube.hdu
+        now = datetime.datetime.strftime(datetime.datetime.now(),
+                                         "%Y/%m/%d-%H:%M:%S")
+        hdu.header.add_history("Written by spectral_cube v{version} on "
+                               "{date}".format(version=SPECTRAL_CUBE_VERSION,
+                                               date=now))
+        hdu.writeto(filename, clobber=overwrite)
+    else:
+        raise NotImplementedError()
