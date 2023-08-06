d1b7ee0c2b807adce680b89636b77da0771ef0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:13 +0000

    Add package tornado_default_pack/0.1dev/tornado_default_pack-0.1dev.tar.gz

---
+++ packages/tornado_default_pack/tornado_default_pack-0.1dev.tar.gz/tornado_default_pack-0.1dev/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tornado_default_pack',
+    author='Andrey Puzuk',
+    author_email='frayandy@gmail.com',
+    url='https://github.com/andyfray/tornado_default_pack',
+    version='0.1dev',
+    packages=['tornado_default_pack'],
+    license='Open license',
+    long_description='long dis',
+    description='short dis',
+    keywords=['testing', 'logging', 'example'],
+)
