a2c47a60e45a67f54a9a5d8d4017343ea5571ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:28 +0000

    Add package woost/1.0.1/woost-1.0.1.tar.gz

---
+++ packages/woost/woost-1.0.1.tar.gz/woost-1.0.1/woost/extensions/googleanalytics/strings.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+from cocktail.translations import translations
+
+translations.define("Document.ga_tracking_enabled",
+    ca = u"Habilitar seguiment amb Google Analytics",
+    es = u"Habilitar seguimiento con Google Analytics",
+    en = u"Track with Google Analytics"
+)
+
