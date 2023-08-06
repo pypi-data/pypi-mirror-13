55e44ea02520d6242323d785c9f4a01be11f78d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package psyplot/0.1.2/psyplot-0.1.2.tar.gz

---
+++ packages/psyplot/psyplot-0.1.2.tar.gz/psyplot-0.1.2/psyplot/__init__.py
@@ -0,0 +1,20 @@
+import datetime as dt
+import logging as _logging
+from .warning import warn, critical, disable_warnings
+from .config.rcsetup import rcParams
+from .data import (
+    ArrayList, InteractiveArray, InteractiveList, open_dataset, open_mfdataset)
+
+__version__ = "0.1.2"
+__author__ = "Philipp Sommer (philipp.sommer@unil.ch)"
+
+
+logger = _logging.getLogger(__name__)
+logger.debug(
+    "%s: Initializing psyplot, version %s",
+    dt.datetime.now().isoformat(), __version__)
+logger.debug("Logging configuration file: %s", config.logcfg_path)
+logger.debug("Configuration file: %s", config.config_path)
+
+
+rcParams.HEADER += "\n\npsyplot version: " + __version__
