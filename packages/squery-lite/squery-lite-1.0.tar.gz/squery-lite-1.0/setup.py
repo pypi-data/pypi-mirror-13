0baa4bbec0e34d4271bdf1aa99aaa6897c776be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:54 +0000

    Add package squery-lite/1.0/squery-lite-1.0.tar.gz

---
+++ packages/squery-lite/squery-lite-1.0.tar.gz/squery-lite-1.0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+import squery_lite as pkg
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+VERSION = pkg.__version__
+
+setup(
+    name='squery-lite',
+    version=VERSION,
+    license='BSD',
+    packages=[pkg.__name__],
+    include_package_data=True,
+    long_description=read('README.rst'),
+    install_requires=[
+        'pytz',
+        'sqlize',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
