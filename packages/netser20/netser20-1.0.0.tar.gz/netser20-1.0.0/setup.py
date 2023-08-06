3055880c120447556d6ead148fda4c6e00d37e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:26 +0000

    Add package netser20/1.0.0/netser20-1.0.0.tar.gz

---
+++ packages/netser20/netser20-1.0.0.tar.gz/netser20-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'netser20',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'fallenAngel',
+	author_email = 'ashutoshbhadoria@outlook.com',
+	url = 'http://www.ashutoshbhadoria.wordpress.com',
+	description = 'A simple printer of nested lists',
+)
