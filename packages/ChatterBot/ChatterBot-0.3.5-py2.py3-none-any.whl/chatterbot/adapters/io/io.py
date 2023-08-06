1eb4ec2a2753fa8e52c7d3f0fde5b9e6a5321711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:22 +0000

    Add package ChatterBot/0.3.5/ChatterBot-0.3.5-py2.py3-none-any.whl

---
+++ packages/ChatterBot/ChatterBot-0.3.5-py2.py3-none-any.whl/chatterbot/adapters/io/io.py
@@ -0,0 +1,22 @@
+from chatterbot.adapters import Adapter
+from chatterbot.adapters.exceptions import AdapterNotImplementedError
+
+
+class IOAdapter(Adapter):
+    """
+    This is an abstract class that represents the interface
+    that all input-output adapters should implement.
+    """
+
+    def process_input(self):
+        """
+        Returns data retrieved from the input source.
+        """
+        raise AdapterNotImplementedError()
+
+    def process_response(self, input_value):
+        """
+        Takes an input value.
+        Returns an output value.
+        """
+        raise AdapterNotImplementedError()
