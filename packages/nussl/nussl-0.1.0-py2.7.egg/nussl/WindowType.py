c421992e8a543441d7e18f65a238989fc04d4cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:14 +0000

    Add package nussl/0.1.0/nussl-0.1.0-py2.7.egg

---
+++ packages/nussl/nussl-0.1.0-py2.7.egg/nussl/WindowType.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class WindowType:
+    RECTANGULAR = 'Rectangular'
+    HAMMING = 'Hamming'
+    HANNING = 'Hanning'
+    BLACKMAN = 'Blackman'
+    DEFAULT = HAMMING
+    def __init__(self):
+        pass
---
+++ packages/nussl/nussl-0.1.0-py2.7.egg/nussl/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import FftUtils
+from WindowType import WindowType
+from WindowAttributes import WindowAttributes
+from AudioSignal import AudioSignal
+from Duet import Duet
+from Nmf import Nmf, DistanceType
+from Repet import Repet, RepetType
+
+
+__version__ = '0.1.0'
+
+__title__ = 'nussl'
+__description__ = 'A flexible sound source separation library.'
+__uri__ = 'https://github.com/interactiveaudiolab/nussl'
+
+__author__ = 'C. Grief, E. Manilow, F. Pishdadian'
+__email__ = 'ethanmanilow2015@u.northwestern.edu'
+
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (c) 2016 Interactive Audio Lab'
