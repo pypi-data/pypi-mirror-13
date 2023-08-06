b301cdbd1cfaa1bb966f2ba6ca4491da1a82fdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:51 +0000

    Add package dou_movies/1.1.0/dou_movies-1.1.0.zip

---
+++ packages/dou_movies/dou_movies-1.1.0.zip/dou_movies-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name    ='dou_movies',
+    version ='1.1.0',
+    py_modules=['movies'],
+    author  ='dou',
+    author_email='1366.qq.com',
+    url='http://www.head.com',
+    description='a simple printer of nested lists',
+)
