ee129c8f5dff60087188c7f506566998b414ac57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:33 +0000

    Add package disccip1_nester/1.2.0/disccip1_nester-1.2.0.tar.gz

---
+++ packages/disccip1_nester/disccip1_nester-1.2.0.tar.gz/disccip1_nester-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name            = 'disccip1_nester',
+        version         = '1.2.0',
+        py_modules      = ['nester'],
+        author          = 'disccip',
+        author_email    = 'disccip@gmail.com',
+        url             = 'none',
+        description     = 'A simple program',
+     )
+
+        
