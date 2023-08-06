8ca1fff90a6021ba976344cdd57fadc597aacb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:40 +0000

    Add package nester_logistex/1.3.0/nester_logistex-1.3.0.zip

---
+++ packages/nester_logistex/nester_logistex-1.3.0.zip/nester_logistex-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = 'nester_logistex',
+    version = '1.3.0',
+    py_modules = ['nester_logistex'],
+    author = 'logistex',
+    author_email = 'logistex@hywoman.ac.kr',
+    url = 'http://www.hywoman.ac.kr',
+    description = 'A simple printer of nested lists'
+    )
