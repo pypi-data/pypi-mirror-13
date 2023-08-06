1055b29bddc04ab380e04438ff3fee848db8b34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:32 +0000

    Add package python-auto-mock/0.1.1/python-auto-mock-0.1.1.tar.gz

---
+++ packages/python-auto-mock/python-auto-mock-0.1.1.tar.gz/python-auto-mock-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='python-auto-mock',
+    version='0.1.1',
+    description='A module to easily mock code while testing.',
+    long_description=(read('README.rst')),
+    url='https://github.com/aaaustin10/Python-Auto-Mock',
+    license='MIT',
+    author='Austin Stewart',
+    author_email='austinfstewart@gmail.com',
+    py_modules=['auto_mock'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
