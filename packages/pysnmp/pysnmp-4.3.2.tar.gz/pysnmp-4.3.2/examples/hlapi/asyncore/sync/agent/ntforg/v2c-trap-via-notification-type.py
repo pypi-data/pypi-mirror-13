245cc1eb9c1361f3614148d325ce9e6875d109d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package pysnmp/4.3.2/pysnmp-4.3.2.tar.gz

---
+++ packages/pysnmp/pysnmp-4.3.2.tar.gz/pysnmp-4.3.2/examples/hlapi/asyncore/sync/agent/ntforg/v2c-trap-via-notification-type.py
@@ -0,0 +1,35 @@
+"""
+SNMPv2c TRAP via NOTIFICATION-TYPE
+++++++++++++++++++++++++++++++++++
+
+Initialize TRAP message contents from variables specified
+in *NOTIFICATION-TYPE* SMI macro.
+
+* SNMPv2c
+* with community name 'public'
+* over IPv4/UDP
+* send TRAP notification
+* with TRAP ID 'coldStart' specified as a MIB symbol
+* include managed object information specified as a MIB symbol
+
+Functionally similar to:
+
+| $ snmptrap -v2c -c public demo.snmplabs.com 12345 1.3.6.1.4.1.20408.4.1.1.2
+
+"""#
+from pysnmp.hlapi import *
+
+errorIndication, errorStatus, errorIndex, varBinds = next(
+    sendNotification(SnmpEngine(),
+                     CommunityData('public'),
+                     UdpTransportTarget(('demo.snmplabs.com', 162)),
+                     ContextData(),
+                     'trap',
+                     NotificationType(
+                         ObjectIdentity('SNMPv2-MIB', 'coldStart')
+                     )
+    )
+)
+
+if errorIndication:
+    print(errorIndication)
