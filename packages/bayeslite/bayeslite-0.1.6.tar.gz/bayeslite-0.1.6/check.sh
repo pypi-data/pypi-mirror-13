3d686bf8611c844b8a2178148722aa4c1ac325a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package bayeslite/0.1.6/bayeslite-0.1.6.tar.gz

---
+++ packages/bayeslite/bayeslite-0.1.6.tar.gz/bayeslite-0.1.6/check.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -Ceu
+
+: ${PYTHON:=python}
+
+root=`cd -- "$(dirname -- "$0")" && pwd`
+
+(
+    set -Ceu
+    cd -- "${root}"
+    rm -rf build
+    "$PYTHON" setup.py build
+    export BAYESDB_WIZARD_MODE=1
+    export BAYESDB_DISABLE_VERSION_CHECK=1
+    if [ $# -eq 0 ]; then
+        # By default, when running all tests, skip tests that have
+        # been marked for continuous integration by using __ci_ in
+        # their names.  (git grep __ci_ to find these.)
+        ./pythenv.sh "$PYTHON" -m pytest -k "not __ci_" \
+                     tests shell/tests
+    elif [ "docker" = "$1" ]; then
+        docker build -f tests/Dockerfile -t bayeslite-test .
+    else
+        # If args are specified, run all tests, including continuous
+        # integration tests, for the selected components.
+        ./pythenv.sh "$PYTHON" -m pytest "$@"
+    fi
+)
