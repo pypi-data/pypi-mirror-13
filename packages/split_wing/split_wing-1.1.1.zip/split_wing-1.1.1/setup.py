035b05f31ce81468028af60038da9f03aa6ba06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:24 +0000

    Add package split_wing/1.1.1/split_wing-1.1.1.zip

---
+++ packages/split_wing/split_wing-1.1.1.zip/split_wing-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='split_wing',
+  version='1.1.1',
+  py_modules=['split_wing'],
+  author='gui_jia',
+  author_email='gui_jia@wo.cn',
+  url='http://www.gzmeo.com',
+  description='I am wing and the e-mail is:<gui_jia@wo.cn>',)
---
+++ packages/split_wing/split_wing-1.1.1.zip/split_wing-1.1.1/split_wing.py
@@ -0,0 +1,22 @@
+﻿#只能处理一个异常
+#if语句可读性差
+
+#文件格式发生改变要修改代码
+
+import os
+
+
+os.chdir('d:\py_wing')
+data = open('rilegou.txt')
+
+for each_line in data:
+  #if each_line.find(":")!=-1:
+  #if not each_line.find(":")==-1:
+  try:
+    (role,line_spoken)= each_line.split(":",1)
+    print(role,end='')
+    print(" : ",end='')
+    print(line_spoken,end='')
+  except:    
+    pass
+data.close()
