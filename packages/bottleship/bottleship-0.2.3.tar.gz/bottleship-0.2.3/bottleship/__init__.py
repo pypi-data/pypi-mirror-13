82fc7131469a58abda0e726999966bf1d52a9925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:15 +0000

    Add package bottleship/0.2.3/bottleship-0.2.3.tar.gz

---
+++ packages/bottleship/bottleship-0.2.3.tar.gz/bottleship-0.2.3/bottleship/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Oscar Martinez'
+__email__ = 'omtinez@gmail.com'
+__version__ = '0.2.3'
+__all__ = ['BottleShip', 'data_encode', 'data_decode', 'data_is_encoded', 'parse_auth', 'static_file', 'redirect', 'request', 'HTTPResponse']
+
+from .bottleship import BottleShip, data_encode, data_decode, data_is_encoded, parse_auth, static_file, redirect, request, HTTPResponse
