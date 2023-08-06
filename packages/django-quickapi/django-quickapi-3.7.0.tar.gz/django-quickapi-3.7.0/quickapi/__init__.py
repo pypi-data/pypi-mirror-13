72cb90b7fb32e56d3f0e9994c85ae677bb40e815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:01 +0000

    Add package django-quickapi/3.7.0/django-quickapi-3.7.0.tar.gz

---
+++ packages/django-quickapi/django-quickapi-3.7.0.tar.gz/django-quickapi-3.7.0/quickapi/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+#   Copyright 2012-2015 Grigoriy Kramarenko <root@rosix.ru>
+#
+#   This file is part of QuickAPI.
+#
+#   QuickAPI is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU Affero General Public License
+#   as published by the Free Software Foundation, either version 3 of
+#   the License, or (at your option) any later version.
+#
+#   QuickAPI is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public
+#   License along with QuickAPI. If not, see
+#   <http://www.gnu.org/licenses/>.
+#
+
+VERSION = (3, 7, 0)
+
+def get_version(*args, **kwargs):
+    return '%d.%d.%d' % VERSION
+
+def get_docs_version(*args, **kwargs):
+    return '%d.%d' % VERSION[:2]
+
+__version__ = get_version()
+
+default_app_config = 'quickapi.apps.AppConfig'
