4a989448ed707bfe8dd3af6f6ca111d1fd021a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:55 +0000

    Add package yassh/0.5.0b1/yassh-0.5.0b1.tar.gz

---
+++ packages/yassh/yassh-0.5.0b1.tar.gz/yassh-0.5.0b1/yassh/__init__.py
@@ -0,0 +1,41 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2016 EnyxSA
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import logging
+
+from .reactor import Reactor
+from .remote_run import RemoteRun, remote_run
+from .remote_copy import RemoteCopy, remote_copy
+from .local_run import LocalRun, local_run
+from .exceptions import AlreadyStartedException
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = ['RemoteRun', 'remote_run',
+           'RemoteCopy', 'remote_copy',
+           'LocalRun', 'local_run',
+           'Reactor',
+           'AlreadyStartedException']
+
+__version__ = '0.5.0b1'
