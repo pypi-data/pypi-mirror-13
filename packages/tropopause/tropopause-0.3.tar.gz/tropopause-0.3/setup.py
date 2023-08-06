9fa59fe73c1bb6d9cd3587c80bfeb095cefd09f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:03 +0000

    Add package tropopause/0.3/tropopause-0.3.tar.gz

---
+++ packages/tropopause/tropopause-0.3.tar.gz/tropopause-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='tropopause',
+      version='0.3',
+      description='Extra utilities for troposphere',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      url='http://github.com/adragomir/tropopause',
+      author='Andrei Dragomir',
+      author_email='adragomir@gmail.com',
+      license='MIT',
+      packages=['tropopause'],
+      install_requires=[
+          'troposphere',
+          'awacs'
+      ],
+      zip_safe=True)
