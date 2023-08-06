83ef0081ce0d3ab14ae0f6aca3e2b102358649b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:45 +0000

    Add package infi.projector/0.6.46/infi.projector-0.6.46.tar.gz

---
+++ packages/infi.projector/infi.projector-0.6.46.tar.gz/infi.projector-0.6.46/src/infi/projector/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.46"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(HEAD detached at v0.6.46)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '6bd4a7c9351b2c84c32fe2c09004437c6d077327'
+__git_head_subject__ = 'Finished Release v0.6.46'
+__git_head_message__ = '* develop:\n  update default isolated python version in skeleton buildout.cfg\n  fix release on Windows\n  fix initial commit on Windows\n  update buildout.cfg\n  initial Python 3 compatibility (WIP!)\n  small refactor\n  pep8\n  ditch gitflow\n  ditch infi.pyutils dependency\n  HOSTDEV-2314 freezing/unfreezing now honors == dependencies as eggs under development-scripts\n  CINDER-86 fixing tests\n  CINDER-86 fixing tests\n  CINDER-86 fixing tests\n  CINDER-86 fixing tests\n  CINDER-86 requirements unfreeze will now keep == dependencies in versions section\n  fix tests'
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-02-15 13:53:33'
